33babb3603ba655d715456652b3cbb92a28e606b
owen <owen@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2002-04-18 14:58:01 +0000

    made color chooser work
    git-svn-id: http://chianti.ucsd.edu/svn@4717 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/dialogs/VisualPropertiesDialog.java
+++ cytoscape/trunk/src/cytoscape/dialogs/VisualPropertiesDialog.java
@@ -20,6 +20,8 @@ import java.awt.event.*;
 import javax.swing.event.*;
 import java.text.NumberFormat;
 
+import javax.swing.colorchooser.*;
+
 import cytoscape.data.*;
 import cytoscape.vizmap.*;
 import cytoscape.dialogs.NewSlider;
@@ -54,6 +56,11 @@ public VisualPropertiesDialog (Frame parentFrame, String title, AttributeMapper 
   gridbag.setConstraints(applyButton,c);
   mainPanel.add (applyButton);
 
+  JDialog ccdd = new ColorChooserDemoDialog(this);
+  ccdd.pack ();
+  ccdd.setLocationRelativeTo (this);
+  ccdd.setVisible (true);
+
   ///////////////////////////////////////////
 
   setContentPane (mainPanel);
@@ -80,4 +87,48 @@ public class ApplyAction extends AbstractAction {
 
 } // QuitAction
 //-----------------------------------------------------------------------------
+
+
+
+public class ColorChooserDemoDialog extends JDialog {
+    public ColorChooserDemoDialog(JDialog parentDialog) {
+        super(parentDialog, "ColorChooserDemoDialog");
+
+        //Set up the banner at the top of the window
+        final JLabel banner = new JLabel("Welcome to the Tutorial Zone!",
+                                         JLabel.CENTER);
+        banner.setForeground(Color.yellow);
+        banner.setBackground(Color.blue);
+        banner.setOpaque(true);
+        banner.setFont(new Font("SansSerif", Font.BOLD, 24));
+        banner.setPreferredSize(new Dimension(100, 65));
+
+        JPanel bannerPanel = new JPanel(new BorderLayout());
+        bannerPanel.add(banner, BorderLayout.CENTER);
+        bannerPanel.setBorder(BorderFactory.createTitledBorder("Banner"));
+
+        //Set up color chooser for setting text color
+        final JColorChooser tcc = new JColorChooser(banner.getForeground());
+        tcc.getSelectionModel().addChangeListener(
+            new ChangeListener() {
+                public void stateChanged(ChangeEvent e) {
+                    Color newColor = tcc.getColor();
+		    System.out.println("hello!");
+                    banner.setForeground(newColor);
+                }
+            }
+        );
+        tcc.setBorder(BorderFactory.createTitledBorder(
+                                             "Choose Text Color"));
+
+        //Add the components to the demo frame
+        Container contentPane = getContentPane();
+        contentPane.add(bannerPanel, BorderLayout.CENTER);
+        contentPane.add(tcc, BorderLayout.SOUTH);
+    }
+
+}
+
+
 } // class VisualPropertiesDialog
+
