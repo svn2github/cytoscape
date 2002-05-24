031e0046d682ead1f3fb764314f352096bba928a
owen <owen@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2002-05-24 21:36:42 +0000

    tabbed correctly, removed more unused functions
    git-svn-id: http://chianti.ucsd.edu/svn@4878 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/dialogs/EdgeTextPanel.java
+++ cytoscape/trunk/src/cytoscape/dialogs/EdgeTextPanel.java
@@ -41,41 +41,41 @@ public EdgeTextPanel (GraphObjAttributes edgeAttribs,
 		      Frame parentFrame,
 		      MutableString writeHere)
 {
-  super ();
-  this.aMapper = aMapper;
-  this.parentFrame = parentFrame;
-  attributeNames = edgeAttribs.getAttributeNames ();
-  edgeKey = writeHere;
-  attribKey = new MutableString("");
-  theMap = null;
-  useThisMap=false;
-
-  this.setLayout (new GridLayout(1,2,10,10));
-
-  DefaultComboBoxModel boxModel = new DefaultComboBoxModel();
-  for (int i=0; i < attributeNames.length; i++)
-      boxModel.addElement(new String(attributeNames [i]));
-  theBox = new JComboBox(boxModel);
-
-  theBox.addActionListener(new BoxAction(edgeKey));
-  if(boxModel.getSize()==1) {
-      theBox.setSelectedIndex(0);
-      edgeKey.setString((String)boxModel.getElementAt(0));
-  }
-  else
-      theBox.setSelectedItem(edgeKey.getString());
-
-  this.add(theBox);
-
-  JButton pickThisAttribute = new JButton ("Define Mapping");
-  pickThisAttribute.addActionListener (new ColorToDiscreteListener());
-  this.add(pickThisAttribute);
-
+    super ();
+    this.aMapper = aMapper;
+    this.parentFrame = parentFrame;
+    attributeNames = edgeAttribs.getAttributeNames ();
+    edgeKey = writeHere;
+    attribKey = new MutableString("");
+    theMap = null;
+    useThisMap=false;
+    
+    this.setLayout (new GridLayout(1,2,10,10));
+    
+    DefaultComboBoxModel boxModel = new DefaultComboBoxModel();
+    for (int i=0; i < attributeNames.length; i++)
+	boxModel.addElement(new String(attributeNames [i]));
+    theBox = new JComboBox(boxModel);
+    
+    theBox.addActionListener(new BoxAction(edgeKey));
+    if(boxModel.getSize()==1) {
+	theBox.setSelectedIndex(0);
+	edgeKey.setString((String)boxModel.getElementAt(0));
+    }
+    else
+	theBox.setSelectedItem(edgeKey.getString());
+    
+    this.add(theBox);
+    
+    JButton pickThisAttribute = new JButton ("Define Mapping");
+    pickThisAttribute.addActionListener (new ColorToDiscreteListener());
+    this.add(pickThisAttribute);
+    
 } // EdgeTextPanel ctor
-
-public Map getMap() {
-    return theMap;
-}
+    
+    public Map getMap() {
+	return theMap;
+    }
     public void updateMapperScalableArrows() {
 	TreeMap tempSM = new TreeMap();
 	Set allKeys = theMap.keySet();
@@ -93,213 +93,198 @@ public Map getMap() {
 	    EdgeArrowColor.removeThenAddEdgeColor(aMapper,keyString,c);
 	}
     }
-public boolean getWhetherToUseTheMap() {
-    return useThisMap;
-}
-public void setWhetherToUseTheMap(boolean b) {
-    useThisMap = b;
-}
-
-public class SharedListSelectionHandler implements ListSelectionListener {
-    MutableString ms;
-    public SharedListSelectionHandler(MutableString ms) {
-	super();
-	this.ms = ms;
+    public boolean getWhetherToUseTheMap() {
+	return useThisMap;
     }
-    public void valueChanged(ListSelectionEvent e) {
-	JList jl = (JList)e.getSource();
-	ms.setString(jl.getSelectedValue().toString());
+    public void setWhetherToUseTheMap(boolean b) {
+	useThisMap = b;
     }
-}
-
-public class BoxAction extends AbstractAction {
-    MutableString ms;
-    public BoxAction(MutableString ms) {
-	super();
-	this.ms = ms;
+    
+    public class SharedListSelectionHandler implements ListSelectionListener {
+	MutableString ms;
+	public SharedListSelectionHandler(MutableString ms) {
+	    super();
+	    this.ms = ms;
+	}
+	public void valueChanged(ListSelectionEvent e) {
+	    JList jl = (JList)e.getSource();
+	    ms.setString(jl.getSelectedValue().toString());
+	}
     }
-    public void actionPerformed(ActionEvent e) {
-	JComboBox jcb = (JComboBox)e.getSource();
-	ms.setString((String)jcb.getSelectedItem());
-	//System.out.println(ms.getString());
+
+    public class BoxAction extends AbstractAction {
+	MutableString ms;
+	public BoxAction(MutableString ms) {
+	    super();
+	    this.ms = ms;
+	}
+	public void actionPerformed(ActionEvent e) {
+	    JComboBox jcb = (JComboBox)e.getSource();
+	    ms.setString((String)jcb.getSelectedItem());
+	    //System.out.println(ms.getString());
+	}
     }
-}
-
-public class ColorToDiscreteListener extends AbstractAction {
-    public void actionPerformed (ActionEvent e) {
-	//System.out.println("define " + edgeKey.getString());
-	ColorToDiscreteDialog ctdDialog =
-	    new ColorToDiscreteDialog();
+    
+    public class ColorToDiscreteListener extends AbstractAction {
+	public void actionPerformed (ActionEvent e) {
+	    //System.out.println("define " + edgeKey.getString());
+	    ColorToDiscreteDialog ctdDialog =
+		new ColorToDiscreteDialog();
+	}
     }
-}
-
-public class MapStringListener extends AbstractAction {
-    JLabel label;
-    Map theMap;
-    Component component;
-    String key;
-    String title;
-    public MapStringListener(Component component,
-			     Map theMap, String key,
-			     JLabel label, String title) {
-	this.label = label;
-	this.theMap = theMap;
-	this.key = key;
-	this.component = component;
-	this.title = title;
+    
+    public class MapStringListener extends AbstractAction {
+	JLabel label;
+	Map theMap;
+	Component component;
+	String key;
+	String title;
+	public MapStringListener(Component component,
+				 Map theMap, String key,
+				 JLabel label, String title) {
+	    this.label = label;
+	    this.theMap = theMap;
+	    this.key = key;
+	    this.component = component;
+	    this.title = title;
+	}
+	public void actionPerformed (ActionEvent e) {
+	    MutableColor mc = new MutableColor((Color)theMap.get(key));
+	    GeneralColorDialogListener gcdl =
+		new GeneralColorDialogListener(component,
+					       mc,
+					       label,
+					       title);
+	    gcdl.popup();
+	    theMap.put(key,mc.getColor());
+	}
     }
-    public void actionPerformed (ActionEvent e) {
-	MutableColor mc = new MutableColor((Color)theMap.get(key));
-	GeneralColorDialogListener gcdl =
-	    new GeneralColorDialogListener(component,
-					   mc,
-					   label,
-					   title);
-	gcdl.popup();
-	theMap.put(key,mc.getColor());
-    }
-}
-
-public class ColorToDiscreteDialog extends JDialog {
-    JPanel extScrollPanel;
-    JPanel intScrollPanel;
-    public ColorToDiscreteDialog() {
-	super(parentFrame, true);
-	setTitle("Colors for " + edgeKey.getString() + " types");
-
-	intScrollPanel = new JPanel(new GridLayout(0,2));
-	String controllingAttribName =
-	    aMapper.getControllingDomainAttributeName(VizMapperCategories.EDGE_COLOR);
-
-	//System.out.println(">" + edgeKey.getString() + "<  vs  >"
-	//		   + controllingAttribName + "<");
-	if(controllingAttribName.equals(edgeKey.getString())) {
-	    DiscreteMapper dmColor =
-		(DiscreteMapper)
-		aMapper.getValueMapper(VizMapperCategories.EDGE_COLOR);
+    
+    public class ColorToDiscreteDialog extends JDialog {
+	JPanel extScrollPanel;
+	JPanel intScrollPanel;
+	public ColorToDiscreteDialog() {
+	    super(parentFrame, true);
+	    setTitle("Colors for " + edgeKey.getString() + " types");
 	    
-	    //Map valueMapColor = dmColor.getValueMap();
-	    // make a copy.
-	    //Map valueMapColor = new HashMap(dmColor.getValueMap());
-	    if(theMap==null || !theMapKey.equals(controllingAttribName)) {
-		System.out.println("In controller, creating");
-		theMap = new HashMap(dmColor.getValueMap());
-		theMapKey = controllingAttribName;
+	    intScrollPanel = new JPanel(new GridLayout(0,2));
+	    String controllingAttribName =
+		aMapper.getControllingDomainAttributeName(VizMapperCategories.EDGE_COLOR);
+	    
+	    //System.out.println(">" + edgeKey.getString() + "<  vs  >"
+	    //		   + controllingAttribName + "<");
+	    if(controllingAttribName.equals(edgeKey.getString())) {
+		DiscreteMapper dmColor =
+		    (DiscreteMapper)
+		    aMapper.getValueMapper(VizMapperCategories.EDGE_COLOR);
+		
+		//Map valueMapColor = dmColor.getValueMap();
+		// make a copy.
+		//Map valueMapColor = new HashMap(dmColor.getValueMap());
+		if(theMap==null || !theMapKey.equals(controllingAttribName)) {
+		    System.out.println("In controller, creating");
+		    theMap = new HashMap(dmColor.getValueMap());
+		    theMapKey = controllingAttribName;
+		}
+		Set allKeys = theMap.keySet();
+		Iterator keyIter = allKeys.iterator();
+		
+		for(;keyIter.hasNext();) {
+		    Object keyObject = keyIter.next();
+		    String keyString = (String)keyObject;
+		    //System.out.println(" key: " + keyString);
+		    JButton tempButton = new JButton(keyString);
+		    JLabel tempLabel = new JLabel("     ");
+		    tempLabel.setOpaque(true);
+		    tempLabel.setBackground((Color)theMap.get(keyString));
+		    String tempTitle = edgeKey.getString() + " / " + keyString;
+		    tempButton.addActionListener(new
+			MapStringListener(ColorToDiscreteDialog.this,
+					  theMap,
+					  keyString,
+					  tempLabel,
+					  tempTitle));
+		    intScrollPanel.add(tempButton);
+		    intScrollPanel.add(tempLabel);
+		}
 	    }
-	    Set allKeys = theMap.keySet();
-	    Iterator keyIter = allKeys.iterator();
-
-	    for(;keyIter.hasNext();) {
-		Object keyObject = keyIter.next();
-		String keyString = (String)keyObject;
-		//System.out.println(" key: " + keyString);
-		JButton tempButton = new JButton(keyString);
-		JLabel tempLabel = new JLabel("     ");
-		tempLabel.setOpaque(true);
-		tempLabel.setBackground((Color)theMap.get(keyString));
-		String tempTitle = edgeKey.getString() + " / " + keyString;
-		tempButton.addActionListener(new
-		    MapStringListener(ColorToDiscreteDialog.this,
-				      theMap,
-				      keyString,
-				      tempLabel,
-				      tempTitle));
-		intScrollPanel.add(tempButton);
-		intScrollPanel.add(tempLabel);
+	    else {
+		// this is so that we don't have to create mappings where none existed.
+		return;
+		
 	    }
+	    
+	    //valueMapColor.remove(key);
+	    //valueMapColor.put(key,c);
+	    
+	    JPanel popupPanel = new JPanel();
+	    
+	    GridBagLayout gridbag = new GridBagLayout(); 
+	    GridBagConstraints c = new GridBagConstraints();
+	    popupPanel.setLayout (gridbag);
+	    
+	    JScrollPane listScrollPane =
+		new JScrollPane(intScrollPanel,
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	    listScrollPane.setPreferredSize(new Dimension(150,150));
+	    extScrollPanel = new JPanel(new GridLayout(1,1));
+	    extScrollPanel.add(listScrollPane);
+	    MiscGB.set(c,0,0,2,1);
+	    MiscGB.insert(popupPanel,extScrollPanel,gridbag,c);
+	    
+	    JButton cancelButton = new JButton ("Cancel");
+	    cancelButton.addActionListener (new CancelAction ());
+	    MiscGB.set(c,0,3);
+	    MiscGB.insert(popupPanel,cancelButton,gridbag,c);
+	    
+	    JButton applyButton = new JButton ("Apply");
+	    applyButton.addActionListener (new ApplyAction ());
+	    MiscGB.set(c,1,3);
+	    MiscGB.insert(popupPanel,applyButton,gridbag,c);
+	    
+	    setContentPane(popupPanel);
+	    pack ();
+	    setLocationRelativeTo (EdgeTextPanel.this);
+	    setVisible (true);
+	    
 	}
-	else {
-	    // this is so that we don't have to create mappings where none existed.
-	    return;
-
-	}
-
-	//valueMapColor.remove(key);
-	//valueMapColor.put(key,c);
-
-	JPanel popupPanel = new JPanel();
+	public class EditAction extends AbstractAction {
+	    EditAction () { super (""); }
+	    public void actionPerformed (ActionEvent e) {
+		//
+		MutableColor mc =
+		    new MutableColor((Color)theMap.get(attribKey.getString()));
+		GeneralColorDialogListener gcdListener =
+		    new GeneralColorDialogListener (ColorToDiscreteDialog.this,
+						    mc,
+						    edgeKey.getString() +
+						    "/" +
+						    attribKey.getString());
+		gcdListener.popup();
+		theMap.remove(attribKey.getString());
+		theMap.put(attribKey.getString(),mc.getColor());
+		//ColorToDiscreteDialog.this.dispose ();
+	    }
+	} // EditAction
 	
-	GridBagLayout gridbag = new GridBagLayout(); 
-	GridBagConstraints c = new GridBagConstraints();
-	popupPanel.setLayout (gridbag);
-
-	JScrollPane listScrollPane =
-	    new JScrollPane(intScrollPanel,
-			    ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-			    ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	listScrollPane.setPreferredSize(new Dimension(150,150));
-	extScrollPanel = new JPanel(new GridLayout(1,1));
-	extScrollPanel.add(listScrollPane);
-	MiscGB.set(c,0,0,2,1);
-	MiscGB.insert(popupPanel,extScrollPanel,gridbag,c);
-
-	JButton cancelButton = new JButton ("Cancel");
-	cancelButton.addActionListener (new CancelAction ());
-	MiscGB.set(c,0,3);
-	MiscGB.insert(popupPanel,cancelButton,gridbag,c);
-
-	JButton applyButton = new JButton ("Apply");
-	applyButton.addActionListener (new ApplyAction ());
-	MiscGB.set(c,1,3);
-	MiscGB.insert(popupPanel,applyButton,gridbag,c);
+	public class CancelAction extends AbstractAction {
+	    CancelAction () { super (""); }
+	    public void actionPerformed (ActionEvent e) {
+		ColorToDiscreteDialog.this.dispose ();
+	    }
+	} // CancelAction
 	
-	setContentPane(popupPanel);
-	pack ();
-	setLocationRelativeTo (EdgeTextPanel.this);
-	setVisible (true);
-
-    }
-    public class EditAction extends AbstractAction {
-	EditAction () { super (""); }
-	public void actionPerformed (ActionEvent e) {
-	    //
-	    MutableColor mc =
-		new MutableColor((Color)theMap.get(attribKey.getString()));
-	    GeneralColorDialogListener gcdListener =
-		new GeneralColorDialogListener (ColorToDiscreteDialog.this,
-						mc,
-						edgeKey.getString() +
-						"/" +
-						attribKey.getString());
-	    gcdListener.popup();
-	    theMap.remove(attribKey.getString());
-	    theMap.put(attribKey.getString(),mc.getColor());
-	    //ColorToDiscreteDialog.this.dispose ();
-	}
-    } // EditAction
-
-
-    // note: this class never quite worked;
-    // it would have had to have access to the buttons and labels,
-    // perhaps through a HashMap.
-    public class RemoveAction extends AbstractAction {
-	RemoveAction () { super (""); }
-	public void actionPerformed (ActionEvent e) {
-	    //
-	    ColorToDiscreteDialog.this.dispose ();
-	}
-    } // RemoveAction
-
-    public void spawnNeedToSelectErrorMessage() {
-    }
-
-    public class CancelAction extends AbstractAction {
-	CancelAction () { super (""); }
-	public void actionPerformed (ActionEvent e) {
-	    ColorToDiscreteDialog.this.dispose ();
-	}
-    } // CancelAction
-
-    public class ApplyAction extends AbstractAction {
-	ApplyAction () { super (""); }
-	public void actionPerformed (ActionEvent e) {
-	    useThisMap=true;
-	    ColorToDiscreteDialog.this.dispose ();
-	}
-    } // CancelAction
-
-} // class ColorToDiscreteDialog
-
+	public class ApplyAction extends AbstractAction {
+	    ApplyAction () { super (""); }
+	    public void actionPerformed (ActionEvent e) {
+		useThisMap=true;
+		ColorToDiscreteDialog.this.dispose ();
+	    }
+	} // CancelAction
+	
+    } // class ColorToDiscreteDialog
+    
 } // class EdgeTextPanel
 
 
