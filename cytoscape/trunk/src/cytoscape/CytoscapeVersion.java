79947d1c25764b5ae41e7fea96d4c7af7aaa98f3
owen <owen@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2002-06-17 23:26:43 +0000

    updated version for 0.8 - first alpha release and adjusted all the other numbers appropriately (from 1.xx to 0.xx)
    git-svn-id: http://chianti.ucsd.edu/svn@4908 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/CytoscapeVersion.java
+++ cytoscape/trunk/src/cytoscape/CytoscapeVersion.java
@@ -7,24 +7,25 @@
 package cytoscape;
 //-----------------------------------------------------------------------------------
 public class CytoscapeVersion {
-  private String versionString = "1.20";
-  private String dateString = "2002/03/28";
+  private String versionString = "0.8";
+  private String dateString = "2002/06/17";
 
   private String [] briefHistory = {
-      "1.1  (2001/12/12) initial version",
-      "1.2  (2001/12/18) sped up node selection by name",
-      "1.3  (2001/12/20) node synonyms displayed in NodeProps dialog",
-      "1.4  (2001/12/21) edge attribute files supported",
-      "1.5  (2001/12/28) edge attributes now can control edge color",
-      "1.6  (2002/01/01) popup dialog 'relocation flash' now fixed",
-      "1.7  (2002/01/04) checkEnviroment centralized, now checks for java version",
-      "1.8  (2002/01/07) active paths dialog bounds checking fixed",
-      "1.9  (2002/01/07) IPBiodataServer.getGoTermName exception fixed",
-      "1.10 (2002/01/22) selected nodes make new window; active paths bug fixed",
-      "1.11 (2002/02/04) automatic running of active paths from command line\n" +
+      "0.1  (2001/12/12) initial version",
+      "0.2  (2001/12/18) sped up node selection by name",
+      "0.3  (2001/12/20) node synonyms displayed in NodeProps dialog",
+      "0.4  (2001/12/21) edge attribute files supported",
+      "0.5  (2001/12/28) edge attributes now can control edge color",
+      "0.6  (2002/01/01) popup dialog 'relocation flash' now fixed",
+      "0.7  (2002/01/04) checkEnviroment centralized, now checks for java version",
+      "0.8  (2002/01/07) active paths dialog bounds checking fixed",
+      "0.9  (2002/01/07) IPBiodataServer.getGoTermName exception fixed",
+      "0.10 (2002/01/22) selected nodes make new window; active paths bug fixed",
+      "0.11 (2002/02/04) automatic running of active paths from command line\n" +
        "                 data passed to ActivePathsFinder via arrays",
-      "1.12 (2002/02/19) reorganized directories; gene common names supported",
-      "1.20 (2002/03/28) now uses plugin architecture; redesign of VizMapping underway",
+      "0.12 (2002/02/19) reorganized directories; gene common names supported",
+      "0.20 (2002/03/28) now uses plugin architecture; redesign of VizMapping underway",
+      "0.8  (2002/06/17) first alpha release",
       };
 
 //-----------------------------------------------------------------------------------
