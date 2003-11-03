8249f15e59e1d23425cf483cd0eb9d0706a74425
lkamenko <lkamenko@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2003-11-03 20:10:57 +0000

    *** empty log message ***
    git-svn-id: http://chianti.ucsd.edu/svn@5496 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/BirdsEyeViewAction.java
+++ cytoscape/trunk/src/cytoscape/actions/BirdsEyeViewAction.java
@@ -2,7 +2,7 @@ package cytoscape.actions;
 
 import java.awt.event.ActionEvent;
 import javax.swing.*;
-
+import java.awt.Dimension;
 import cytoscape.view.NetworkView;
 import phoebe.PGraphView;
 import cytoscape.dialogs.GraphObjectSelection;
@@ -17,10 +17,16 @@ public class BirdsEyeViewAction extends AbstractAction {
   }
 
   public void actionPerformed (ActionEvent e) {
-    JDialog dialog = new JDialog( );
+	if (networkView.getView() == null) return;  
+    JDialog dialog = new JDialog();
     PGraphView pview = (PGraphView)networkView.getView();
+    
+    dialog.setTitle("Birds Eye View" );
     dialog.getContentPane().add( pview.getBirdsEyeView() );
+    
+    
     dialog.pack();
+    dialog.setSize(new Dimension ( 200, 200));
     dialog.setVisible( true );
   }
 
