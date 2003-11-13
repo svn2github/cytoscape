136dafed08441e4c426e554ffe96d583abb745c8
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2003-11-13 23:17:44 +0000

    Updated script files to use giny
    git-svn-id: http://chianti.ucsd.edu/svn@5511 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/bin/cytoscape.sh
+++ cytoscape/trunk/bin/cytoscape.sh
@@ -6,4 +6,4 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./plugins
 
-java -Xmx512M -jar cytoscape.jar -b GO/annotationAndSynonyms --JLD plugins $*
+java -Xmx512M -jar cytoscape.jar -y giny -b GO/annotationAndSynonyms --JLD plugins $*
