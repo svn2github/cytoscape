8a803d446b403f498cec41ff9a1905d39c69bab1
cworkman <cworkman@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2003-12-01 18:28:25 +0000

    replaces LoadGMLFileAction and LoadInteractionFileAction which were redundant
    git-svn-id: http://chianti.ucsd.edu/svn@5520 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/trunk/src/cytoscape/actions/LoadGraphFileAction.java
@@ -0,0 +1,88 @@
+//-------------------------------------------------------------------------
+// $Revision$
+// $Date$
+// $Author$
+//-------------------------------------------------------------------------
+package cytoscape.actions;
+//-------------------------------------------------------------------------
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import java.io.File;
+
+import cytoscape.CytoscapeObj;
+import cytoscape.data.CyNetwork;
+import cytoscape.data.CyNetworkFactory;
+import cytoscape.data.Semantics;
+import cytoscape.view.NetworkView;
+import cytoscape.util.CyFileFilter;
+//-------------------------------------------------------------------------
+public class LoadGraphFileAction extends AbstractAction {
+    NetworkView networkView;
+    
+    public LoadGraphFileAction(NetworkView networkView) {
+        super("Graph...");
+        this.networkView = networkView;
+    }
+    
+    public void actionPerformed(ActionEvent e)  {
+        CytoscapeObj cytoscapeObj = networkView.getCytoscapeObj();
+        File currentDirectory = networkView.getCytoscapeObj().getCurrentDirectory();
+        JFileChooser chooser = new JFileChooser(currentDirectory);
+        CyFileFilter intFilter = new CyFileFilter();
+        CyFileFilter gmlFilter = new CyFileFilter();
+	CyNetwork newNetwork;
+        gmlFilter.addExtension("gml");
+        gmlFilter.setDescription("GML files");
+        intFilter.addExtension("sif");
+        intFilter.setDescription("Interaction files");
+        chooser.addChoosableFileFilter(intFilter);
+        chooser.addChoosableFileFilter(gmlFilter);
+        chooser.setFileFilter(intFilter);
+        if (chooser.showOpenDialog(networkView.getMainFrame()) == chooser.APPROVE_OPTION) {
+            currentDirectory = chooser.getCurrentDirectory();
+	    //String fileType = chooser.getDescription(chooser.getSelectedFile());
+	    //System.out.println("FILETYPE: " + fileType);
+            networkView.getCytoscapeObj().setCurrentDirectory(currentDirectory);
+
+            String  name = chooser.getSelectedFile().toString();
+            boolean canonicalize = Semantics.getCanonicalize(cytoscapeObj);
+            String  species = Semantics.getDefaultSpecies( networkView.getNetwork(), cytoscapeObj );
+	    boolean isYFiles = networkView.getCytoscapeObj().getConfiguration().isYFiles();
+
+	    if( name.endsWith("gml") || name.endsWith("GML") )
+		newNetwork = CyNetworkFactory.createNetworkFromGMLFile( name, isYFiles );
+	    else
+		newNetwork =
+		    CyNetworkFactory.createNetworkFromInteractionsFile( name, 
+									canonicalize,
+									cytoscapeObj.getBioDataServer(), 
+									species, 
+									isYFiles );
+            if (newNetwork != null) {//valid read
+                //apply the semantics we usually expect
+                Semantics.applyNamingServices(newNetwork, networkView.getCytoscapeObj());
+                //set the new graph, don't erase old attributes
+		if ( isYFiles ) {
+		    networkView.getNetwork().setNewGraphFrom(newNetwork, false);
+		    networkView.setWindowTitle(name);
+		}
+		else {
+		    networkView.setNewNetwork(newNetwork);
+		    networkView.setWindowTitle(name);
+		}
+             } else {//give the user an error dialog
+                String lineSep = System.getProperty("line.separator");
+                StringBuffer sb = new StringBuffer();
+                sb.append("Could not read graph from file " + name + lineSep);
+                sb.append("This file may not be a valid GML or SIF file." + lineSep);
+                JOptionPane.showMessageDialog(networkView.getMainFrame(),
+                                              sb.toString(),
+                                              "Error loading graph",
+                                              JOptionPane.ERROR_MESSAGE);
+            }
+        } // if
+    } // actionPerformed
+}
+
