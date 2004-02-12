1f5852b51869cdfbe365164dd3613c9596f00cf7
cworkman <cworkman@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-02-12 18:11:36 +0000

    calls methods in cytoscape.data.readers.GMLTree
    git-svn-id: http://chianti.ucsd.edu/svn@5591 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SaveAsGMLAction.java
+++ cytoscape/trunk/src/cytoscape/actions/SaveAsGMLAction.java
@@ -10,10 +10,12 @@ import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import cytoscape.view.NetworkView;
 //import cytoscape.data.GraphProps;
-//import cytoscape.data.readers.GMLWriter;
+import cytoscape.data.readers.GMLTree;
 //-------------------------------------------------------------------------
 public class SaveAsGMLAction extends AbstractAction {
     NetworkView networkView;
@@ -27,28 +29,24 @@ public class SaveAsGMLAction extends AbstractAction {
         //save as GML isn't supported yet, so for now we put up an
         //error message. Eventually we'll adapt the code preserved
         //below to use the new GML parser.
-        String title = "Operation not supported";
-        String message = "The 'Save As GML' operation is not yet supported.";
-        JOptionPane.showMessageDialog(networkView.getMainFrame(), message,
-                                      title, JOptionPane.ERROR_MESSAGE);
-    }
-    
-    private void oldSavedCode() {
-        /*
-        File currentDirectory = networkView.getCytoscapeObj().getCurrentDirectory();
-        JFileChooser chooser = new JFileChooser(currentDirectory);
-        if (chooser.showSaveDialog(networkView.getMainFrame()) == chooser.APPROVE_OPTION) {
-            currentDirectory = chooser.getCurrentDirectory();
-            networkView.getCytoscapeObj().setCurrentDirectory(currentDirectory);
-            String name = chooser.getSelectedFile().toString();
-            if (!name.endsWith(".gml")) name = name + ".gml";
-            GraphProps props = new GraphProps(networkView.getNetwork().getGraph(),
-                                              networkView.getNetwork().getNodeAttributes(),
-                                              networkView.getNetwork().getEdgeAttributes());
-            GMLWriter writer = new GMLWriter(props);
-            writer.write(name);
-        } // if
-        */
+	File currentDirectory = networkView.getCytoscapeObj().getCurrentDirectory();
+      	JFileChooser chooser = new JFileChooser (currentDirectory);
+      	if (chooser.showSaveDialog (networkView.getMainFrame()) == chooser.APPROVE_OPTION) {
+          String name = chooser.getSelectedFile ().toString ();
+          currentDirectory = chooser.getCurrentDirectory();
+          networkView.getCytoscapeObj().setCurrentDirectory(currentDirectory);
+          if (!name.endsWith (".gml")) name = name + ".gml";
+          try {
+              FileWriter fileWriter = new FileWriter(name);
+	      GMLTree result = new GMLTree(networkView.getView());
+	      fileWriter.write(result.toString());
+	      fileWriter.close();
+	  }
+	  catch (IOException ioe) {
+              System.err.println("Error while writing " + name);
+              ioe.printStackTrace();
+          } 
+	}
     }
 } // SaveAsGMLAction
 
