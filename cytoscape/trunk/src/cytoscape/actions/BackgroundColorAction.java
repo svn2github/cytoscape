0a708f2148932367028b9522d504541e961f4d91
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-03-05 22:14:34 +0000

    Now synchronizes background color with current visual style
    git-svn-id: http://chianti.ucsd.edu/svn@5637 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/BackgroundColorAction.java
+++ cytoscape/trunk/src/cytoscape/actions/BackgroundColorAction.java
@@ -4,35 +4,70 @@
 // $Author$
 //-------------------------------------------------------------------------
 package cytoscape.actions;
+
 //-------------------------------------------------------------------------
-import java.awt.event.ActionEvent;
-import javax.swing.AbstractAction;
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.event.*;
 
 import cytoscape.view.NetworkView;
-//-------------------------------------------------------------------------
-public class BackgroundColorAction extends AbstractAction  {
+import cytoscape.visual.GlobalAppearanceCalculator;
+import cytoscape.visual.VisualMappingManager;
+import cytoscape.visual.VisualStyle;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+//-------------------------------------------------------------------------
+/**
+ * Prompts User for New Background Color.
+ */
+public class BackgroundColorAction extends AbstractAction {
+
+    /**
+     * Current Network View Object.
+     */
     NetworkView networkView;
-    
+
+    /**
+     * Constructor.
+     * @param networkView Network View Object.
+     */
     public BackgroundColorAction(NetworkView networkView) {
-        super ("Change Background Color");
+        super("Change Background Color");
         this.networkView = networkView;
     }
 
-    public void actionPerformed (ActionEvent ev) {
+    /**
+     * Captures User Menu Selection.
+     * @param ev Action Event.
+     */
+    public void actionPerformed(ActionEvent ev) {
         // Do this in the GUI Event Dispatch thread...
-        SwingUtilities.invokeLater( new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 JColorChooser color = new JColorChooser();
-                Paint newPaint = color.showDialog( networkView.getView().getComponent(),
-                                                   "Choose a background Color",
-                                                   (java.awt.Color)networkView.getView().getBackgroundPaint() );
-                networkView.getView().setBackgroundPaint(newPaint);
-            }
-        }); 
-    }//action performed
-}
+                Color newPaint = color.showDialog
+                        (networkView.getView().getComponent(),
+                                "Choose a Background Color",
+                                (java.awt.Color) networkView.getView().
+                                getBackgroundPaint());
 
+                //  Update the Current Background Color
+                //  and Synchronize with current Visual Style
+                networkView.getView().setBackgroundPaint(newPaint);
+                synchronizeVisualStyle(newPaint);
+            }
+        });
+    }//action performed
+
+    /**
+     * Synchronizes the New Background Color with the Current Visual Style.
+     * @param newColor New Color
+     */
+    private void synchronizeVisualStyle(Color newColor) {
+        VisualMappingManager vmm = networkView.getVizMapManager();
+        VisualStyle style = vmm.getVisualStyle();
+        GlobalAppearanceCalculator gCalc =
+                style.getGlobalAppearanceCalculator();
+        gCalc.setDefaultBackgroundColor(newColor);
+    }
+}
