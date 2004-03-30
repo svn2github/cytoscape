94e29edd00fddd07d06b62354ba1cd1237a6d0b8
bader <bader@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-03-30 23:21:57 +0000

    Fixed non floatable toolbar in WindowsXP - was related to a jgoodies look and feel.
    git-svn-id: http://chianti.ucsd.edu/svn@5738 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/CyMain.java
+++ cytoscape/trunk/src/cytoscape/CyMain.java
@@ -6,29 +6,29 @@
  ** under the terms of the GNU Lesser General Public License as published
  ** by the Free Software Foundation; either version 2.1 of the License, or
  ** any later version.
- ** 
+ **
  ** This library is distributed in the hope that it will be useful, but
  ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
  ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
  ** documentation provided hereunder is on an "as is" basis, and the
- ** Institute for Systems Biology and the Whitehead Institute 
+ ** Institute for Systems Biology and the Whitehead Institute
  ** have no obligations to provide maintenance, support,
  ** updates, enhancements or modifications.  In no event shall the
- ** Institute for Systems Biology and the Whitehead Institute 
+ ** Institute for Systems Biology and the Whitehead Institute
  ** be liable to any party for direct, indirect, special,
  ** incidental or consequential damages, including lost profits, arising
  ** out of the use of this software and its documentation, even if the
- ** Institute for Systems Biology and the Whitehead Institute 
+ ** Institute for Systems Biology and the Whitehead Institute
  ** have been advised of the possibility of such damage.  See
  ** the GNU Lesser General Public License for more details.
- ** 
+ **
  ** You should have received a copy of the GNU Lesser General Public License
  ** along with this library; if not, write to the Free Software Foundation,
  ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  **/
 
 // $Revision$
-// $Date$ 
+// $Date$
 // $Author$
 //-------------------------------------------------------------------------------------
 package cytoscape;
@@ -75,13 +75,13 @@ public CyMain (String [] args) throws Exception {
     //parse args and config files into config object
     CytoscapeConfig config = new CytoscapeConfig(args);
     splashScreen.advance(10);
-    
+
     //handle special cases of arguments
     if (config.helpRequested()) {
         System.out.println(version);
         System.out.println(config.getUsage());
         exit(0);
-    }    
+    }
     else if (config.inputsError()) {
         System.out.println(version);
         System.out.println("------------- Inputs Error");
@@ -98,7 +98,7 @@ public CyMain (String [] args) throws Exception {
     setupLogger(config);
     logger.info(config.toString());
     if (splashScreen != null) {splashScreen.advance(20);}
-    
+
     //try to create a bioDataServer
     String bioDataDirectory = config.getBioDataDirectory();
     BioDataServer bioDataServer = null;
@@ -118,10 +118,10 @@ public CyMain (String [] args) throws Exception {
     boolean canonicalize = Semantics.getCanonicalize(cytoscapeObj);
     String defaultSpecies = Semantics.getDefaultSpecies(null, cytoscapeObj);
     if (splashScreen != null) {splashScreen.advance(25);}
-    
+
     //window title
     String title = null;
-    
+
     //load a network if requested
     CyNetwork network = null;
     String geometryFilename = config.getGeometryFilename();
@@ -137,7 +137,7 @@ public CyMain (String [] args) throws Exception {
 	network = CyNetworkFactory.createNetworkFromGMLFile(geometryFilename);
 	logger.info("  done");
 	title = geometryFilename;
-    } 
+    }
     else if (interactionsFilename != null) {
         logger.info("reading " + interactionsFilename + "...");
         network =
@@ -156,7 +156,7 @@ public CyMain (String [] args) throws Exception {
     }
     //add the semantics we usually expect
     Semantics.applyNamingServices(network, cytoscapeObj);
-    
+
     //load any specified data attribute files
     logger.info("reading attribute files");
     CyNetworkFactory.loadAttributes(network,
@@ -164,7 +164,7 @@ public CyMain (String [] args) throws Exception {
                                     config.getEdgeAttributeFilenames(),
                                     canonicalize, bioDataServer, defaultSpecies);
     logger.info(" done");
-    
+
     //load expression data if specified
     String expDataFilename = config.getExpressionFilename();
     if (expDataFilename != null) {
@@ -181,9 +181,9 @@ public CyMain (String [] args) throws Exception {
             e.printStackTrace();
         }
         logger.info("  done");
-    }    
+    }
     if (splashScreen!=null) {splashScreen.advance(90);}
-    
+
     //create the window
     cyWindow = new CyWindow(cytoscapeObj, network, title);
 
@@ -193,7 +193,7 @@ public CyMain (String [] args) throws Exception {
     }
 
     cyWindow.showWindow();
-    
+
     if (splashScreen!=null) {splashScreen.advance(100);}
 } // ctor
 //------------------------------------------------------------------------------
@@ -201,14 +201,14 @@ public CyMain (String [] args) throws Exception {
  * configure logging:  cytoscape.props specifies what level of logging
  * messages are written to the console; by default, only SEVERE messages
  * are written.  in time, more control of logging (i.e., optional logging
- * to a file, disabling console logging, per-window or per-plugin logging) 
+ * to a file, disabling console logging, per-window or per-plugin logging)
  * can be provided
  */
 protected void setupLogger (CytoscapeConfig config) {
-    logger = Logger.getLogger("global"); 
+    logger = Logger.getLogger("global");
     Properties properties = config.getProperties();
     String level = properties.getProperty("logging", "SEVERE");
-    
+
     if (level.equalsIgnoreCase("severe")) {
         logger.setLevel(Level.SEVERE);
     } else if (level.equalsIgnoreCase("warning")) {
@@ -243,7 +243,7 @@ public void windowDeiconified (WindowEvent e) {}
 public void windowIconified   (WindowEvent e) {}
 
 //------------------------------------------------------------------------------
-public void windowOpened      (WindowEvent e) {  
+public void windowOpened      (WindowEvent e) {
     if(splashScreen != null) {
         splashScreen.advance(200);
         splashScreen.dispose();
@@ -252,15 +252,15 @@ public void windowOpened      (WindowEvent e) {
     windows.add (e.getWindow ());
 }
 //------------------------------------------------------------------------------
-public void windowClosed     (WindowEvent e) { 
+public void windowClosed     (WindowEvent e) {
     Window window = e.getWindow();
     if (windows.contains(window)) {windows.remove (window);}
-    
+
     if (windows.size () == 0) {
         logger.info("all windows closed, exiting...");
         exit(0);
     }
-}	
+}
 
 public CyWindow getMainWindow()
 {
@@ -276,30 +276,30 @@ public void exit(int exitCode) {
 }
 //------------------------------------------------------------------------------
 public static void main(String args []) throws Exception {
- 
+
   UIManager.put(Options.USE_SYSTEM_FONTS_APP_KEY, Boolean.TRUE);
   Options.setGlobalFontSizeHints(FontSizeHints.MIXED);
   Options.setDefaultIconSize(new Dimension(18, 18));
- 
+
   try {
      if ( LookUtils.isWindowsXP() ) {
        // use XP L&F
-       UIManager.setLookAndFeel( Options.getCrossPlatformLookAndFeelClassName() );
+       UIManager.setLookAndFeel( Options.getSystemLookAndFeelClassName() );
      } else if ( System.getProperty("os.name").startsWith( "Mac" ) ) {
        // do nothing, I like the OS X L&F
      } else {
        // this is for for *nix
        // I happen to like this color combo, there are others
        Plastic3DLookAndFeel laf = new Plastic3DLookAndFeel();
-       laf.setTabStyle( Plastic3DLookAndFeel.TAB_STYLE_METAL_VALUE ); 
-       laf.setHighContrastFocusColorsEnabled(true); 
+       laf.setTabStyle( Plastic3DLookAndFeel.TAB_STYLE_METAL_VALUE );
+       laf.setHighContrastFocusColorsEnabled(true);
        laf.setMyCurrentTheme( new com.jgoodies.plaf.plastic.theme.ExperienceBlue() );
        UIManager.setLookAndFeel( laf );
      }
    } catch (Exception e) {
      System.err.println("Can't set look & feel:" + e);
    }
-  
+
 
 
     CyMain app = new CyMain(args);
