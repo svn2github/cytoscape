6dfa9a7572cfd67a4905a052825e9c32b347c3b3
xmas <xmas@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-04-07 01:11:50 +0000

    Added FreeHEP printing support.
    git-svn-id: http://chianti.ucsd.edu/svn@5772 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/PrintAction.java
+++ cytoscape/trunk/src/cytoscape/actions/PrintAction.java
@@ -12,8 +12,19 @@ import java.awt.print.PageFormat;
 import java.awt.print.PrinterJob;
 
 import phoebe.*;
+import phoebe.util.*;
 
 import cytoscape.view.NetworkView;
+
+import org.freehep.graphics2d.VectorGraphics;
+import org.freehep.util.export.ExportDialog;
+
+import org.freehep.graphicsio.pdf.PDFExportFileType;
+import org.freehep.graphicsio.ps.EPSExportFileType;
+import org.freehep.graphicsio.ps.PSExportFileType;
+import org.freehep.graphicsio.svg.SVGExportFileType;
+import org.freehep.graphicsio.java.JAVAExportFileType;
+
 //-------------------------------------------------------------------------
 public class PrintAction extends AbstractAction  {
     NetworkView networkView;
@@ -24,8 +35,21 @@ public class PrintAction extends AbstractAction  {
     }
 
     public void actionPerformed(ActionEvent e) {
-        PGraphView ginyView = (PGraphView)networkView.getView();
-        ginyView.getCanvas().getLayer().print();
+
+   //    if ( System.getProperty("os.name").startsWith( "Windows" ) ) {
+//         cytoscape.util.PrintUtilities.printComponent( ( (PGraphView)networkView.getView() ).getCanvas() );
+//       } else {
+//         PGraphView ginyView = (PGraphView)networkView.getView();
+//         ginyView.getCanvas().getLayer().print();
+//       }
+
+      ( (PGraphView)networkView.getView() ).getCanvas().getCamera().addClientProperty( PrintingFixTextNode.PRINTING_CLIENT_PROPERTY_KEY, "true");
+
+      ExportDialog export = new ExportDialog();
+      export.showExportDialog( ( (PGraphView)networkView.getView() ).getCanvas(), "Export view as ...", ( (PGraphView)networkView.getView() ).getCanvas(), "export" );
+      
+      ( (PGraphView)networkView.getView() ).getCanvas().getCamera().addClientProperty( PrintingFixTextNode.PRINTING_CLIENT_PROPERTY_KEY, null);
+
     } // actionPerformed
 }
 
