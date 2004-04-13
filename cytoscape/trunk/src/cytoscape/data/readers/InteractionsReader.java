b3dd27bc8de2bbf7f4e9a5576f2db3d5a478fda6
bader <bader@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-04-13 05:38:05 +0000

    Node names were not getting stored for sif files - now fixed.
    git-svn-id: http://chianti.ucsd.edu/svn@5785 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/data/readers/InteractionsReader.java
+++ cytoscape/trunk/src/cytoscape/data/readers/InteractionsReader.java
@@ -6,30 +6,30 @@
  ** under the terms of the GNU Lesser General Public License as published
  ** by the Free Software Foundation; either version 2.1 of the License, or
  ** any later version.
- ** 
+ **
  ** This library is distributed in the hope that it will be useful, but
  ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
  ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
  ** documentation provided hereunder is on an "as is" basis, and the
- ** Institute for Systems Biology and the Whitehead Institute 
+ ** Institute for Systems Biology and the Whitehead Institute
  ** have no obligations to provide maintenance, support,
  ** updates, enhancements or modifications.  In no event shall the
- ** Institute for Systems Biology and the Whitehead Institute 
+ ** Institute for Systems Biology and the Whitehead Institute
  ** be liable to any party for direct, indirect, special,
  ** incidental or consequential damages, including lost profits, arising
  ** out of the use of this software and its documentation, even if the
- ** Institute for Systems Biology and the Whitehead Institute 
+ ** Institute for Systems Biology and the Whitehead Institute
  ** have been advised of the possibility of such damage.  See
  ** the GNU Lesser General Public License for more details.
- ** 
+ **
  ** You should have received a copy of the GNU Lesser General Public License
  ** along with this library; if not, write to the Free Software Foundation,
  ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  **/
 
 //------------------------------
-// $Revision$   
-// $Date$ 
+// $Revision$
+// $Date$
 // $Author$
 //------------------------------
 package cytoscape.data.readers;
@@ -48,7 +48,7 @@ import cytoscape.data.readers.*;
 /**
  * This is an Interaction Reader, it will create a network from
  * a given file.
- * 
+ *
  * The network can then be merged/added/replaced in Cytoscape
  */
 public class InteractionsReader implements GraphReader {
@@ -57,7 +57,7 @@ public class InteractionsReader implements GraphReader {
    * The File to be loaded
    */
   protected String filename;
-  
+
   /**
    * A Vector that holds all of the Interactions
    */
@@ -77,9 +77,9 @@ public class InteractionsReader implements GraphReader {
    * @param dataServer  a BioDataServer
    * @param species the species of the network being loaded
    * @param filename the file to load the network from
-   */ 
-  public InteractionsReader ( BioDataServer dataServer, 
-                              String species, 
+   */
+  public InteractionsReader ( BioDataServer dataServer,
+                              String species,
                               String filename ) {
     this.filename = filename;
     this.dataServer = dataServer;
@@ -89,7 +89,7 @@ public class InteractionsReader implements GraphReader {
 
   //----------------------------------------------------------------------------------------
   public void read ( boolean canonicalize ) {
-  
+
     String rawText;
     try {
       if (filename.trim().startsWith ("jar://")) {
@@ -113,17 +113,17 @@ public class InteractionsReader implements GraphReader {
     if (rawText.indexOf ("\t") >= 0)
       delimiter = "\t";
     StringTokenizer strtok = new StringTokenizer (rawText, "\n");
-  
+
     // commented out by iliana on 11.26.2002 :
     // Vector interactions = new Vector ();
-  
+
     while (strtok.hasMoreElements ()) {
       String newLine = (String) strtok.nextElement ();
       Interaction newInteraction = new Interaction (newLine, delimiter);
       allInteractions.addElement (newInteraction);
     }
     createRootGraphFromInteractionData (canonicalize);
-  
+
   }
   //-----------------------------------------------------------------------------------------
   /**
@@ -154,13 +154,13 @@ public class InteractionsReader implements GraphReader {
   //-------------------------------------------------------------------------------------------
   protected String canonicalizeName (String name)
   {
-    
+
     String canonicalName = name;
     if (dataServer != null) {
       canonicalName = dataServer.getCanonicalName (species, name);
       // added by iliana 11.14.2002
       // for some strange reason the server returned a null canonical name
-      if(canonicalName == null){canonicalName = name;} 
+      if(canonicalName == null){canonicalName = name;}
       //System.out.println (" -- canonicalizeName from server: " + canonicalName);
     }
     //System.out.println("the canonicalName for " + name + " is " + canonicalName);
@@ -181,7 +181,7 @@ public class InteractionsReader implements GraphReader {
       	String sourceName = interaction.getSource();
 	if(canonicalize) sourceName = canonicalizeName (sourceName);
         nodeNameSet.add(sourceName); //does nothing if already there
-        
+
         String [] targets = interaction.getTargets ();
 	for (int t=0; t < targets.length; t++) {
 	    String targetNodeName = targets[t];
@@ -190,7 +190,7 @@ public class InteractionsReader implements GraphReader {
             edgeCount++;
         }
     }
-    
+
     //now create the RootGraph, initialized with containers of appropriate size
     rootGraph = GinyFactory.createRootGraph(nodeNameSet.size(), edgeCount);
     //now create all of the nodes, storing a hash from name to node
@@ -198,6 +198,7 @@ public class InteractionsReader implements GraphReader {
     for (Iterator si = nodeNameSet.iterator(); si.hasNext(); ) {
         String nodeName = (String)si.next();
         Node node = rootGraph.getNode(rootGraph.createNode());
+	    node.setIdentifier(nodeName);
         nodes.put(nodeName, node);
         nodeAttributes.addNameMapping(nodeName, node);
     }
@@ -216,7 +217,7 @@ public class InteractionsReader implements GraphReader {
       Interaction interaction = interactions [i];
       String nodeName = interaction.getSource();
       if(canonicalize) nodeName = canonicalizeName (interaction.getSource ());
-    
+
       String interactionType = interaction.getType ();
       giny.model.Node sourceNode = (giny.model.Node) nodes.get (nodeName);
       String [] targets = interaction.getTargets ();
@@ -225,7 +226,7 @@ public class InteractionsReader implements GraphReader {
 	      targetNodeName = canonicalizeName (targets [t]);
 	  else
 	      targetNodeName = targets[t];
-      
+
 	  Node targetNode = (Node) nodes.get (targetNodeName);
 	  Edge edge = rootGraph.getEdge(rootGraph.createEdge (sourceNode, targetNode));
 	  String edgeName = nodeName + " (" + interactionType + ") " + targetNodeName;
