97b64bf1450b301b94a75b76937b2fc75868dbba
iavila <iavila@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-04-14 20:13:17 +0000

    Interface for creating metanodes. -iliana
    git-svn-id: http://chianti.ucsd.edu/svn@123 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/isb/iavila/metaNodeViewer/src/metaNodeViewer/MetaNodeFactory.java
@@ -0,0 +1,57 @@
+/**  Copyright (c) 2003 Institute for Systems Biology
+ **  This program is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 2 of the License, or
+ **  any later version.
+ **
+ **  This program is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ **  documentation provided hereunder is on an "as is" basis, and the
+ **  Institute for Systems Biology has no obligations to provide maintenance, 
+ **  support, updates, enhancements or modifications.  In no event shall the
+ **  Institute for Systems Biology be liable to any party for direct, 
+ **  indirect, special,incidental or consequential damages, including 
+ **  lost profits, arising out of the use of this software and its 
+ **  documentation, even if the Institute for Systems Biology 
+ **  has been advised of the possibility of such damage. See the
+ **  GNU General Public License for more details.
+ **   
+ **  You should have received a copy of the GNU General Public License
+ **  along with this program; if not, write to the Free Software
+ **  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **/
+/**
+ * @author Iliana Avila-Campillo iavila@systemsbiology.org
+ * @version %I%, %G%
+ * @since 2.0
+ */
+package metaNodeViewer;
+import cytoscape.data.CyNetwork;
+
+public interface MetaNodeFactory {
+  
+  /**
+   * Creates a MetaNode in the given CyNetwork with the given children
+   *
+   * @param cy_network the CyNetwork in which MetaNodes will be created
+   * @param children_node_indices the indices of the nodes that will be
+   * the children of the created meta-node
+   * @return the RootGraph index of the newly created meta-node, or zero if
+   * none created.
+   */
+  public int createMetaNode (CyNetwork cy_network,
+                              int [] children_node_indices);
+
+  /**
+   * Sets whether or not a default name for newly created meta-nodes should be given
+   * and added to the node attributes.
+   */
+  public void assignDefaultNames (boolean assign);
+
+  /**
+   * Whether or not default names are being assigned to newly created meta-nodes
+   */
+  public boolean getAssignDefaultNames ();
+    
+}//MetaNodeFactory
