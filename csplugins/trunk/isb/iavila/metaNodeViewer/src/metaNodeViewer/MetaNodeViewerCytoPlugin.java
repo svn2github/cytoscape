8f20dec31d198b7bcf185fcf9e47ea38deb42749
iavila <iavila@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-04-15 00:16:10 +0000

    Test -iliana
    git-svn-id: http://chianti.ucsd.edu/svn@128 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/isb/iavila/metaNodeViewer/src/metaNodeViewer/MetaNodeViewerCytoPlugin.java
+++ csplugins/trunk/isb/iavila/metaNodeViewer/src/metaNodeViewer/MetaNodeViewerCytoPlugin.java
@@ -1,13 +1,8 @@
 package metaNodeViewer;
 
-import metaNodeViewer.model.*;
+import metaNodeViewer.ui.MNcollapserDialog;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
-import java.util.*;
-import giny.model.*;
-import giny.view.*;
-import cytoscape.data.GraphObjAttributes;
-import cytoscape.data.CyNetwork;
 import cytoscape.view.CyWindow;
 import cytoscape.plugin.AbstractPlugin;
 import cytoscape.util.*;
@@ -15,127 +10,24 @@ import cytoscape.util.*;
 public class MetaNodeViewerCytoPlugin extends AbstractPlugin{
 
   protected CyWindow cyWindow;
-  protected AbstractMetaNodeModeler modeler;
-
+  
   /**
    * Constructor.
    */
-  // TODO: LEFT HERE, TEST NEW AbstractMetaNodeModeler
   public MetaNodeViewerCytoPlugin (CyWindow cy_window){
     this.cyWindow = cy_window;
-    // Add actions to the plugins menu
-    this.cyWindow.getCyMenus().getOperationsMenu().add(new ApplyModelForSelectedNodesAction());
-    //this.cyWindow.getCyMenus().getOperationsMenu().add(new UndoModel());
-    this.cyWindow.getCyMenus().getOperationsMenu().add(new UndoModelForSelectedNodes("Uncollapse",false));
-    this.cyWindow.getCyMenus().getOperationsMenu().add(new UndoModelForSelectedNodes("Uncollapse recursive",true));
-    
-    //( ( CytoscapeMenuBar )cyWindow.getCyMenus().getMenuBar() ).addAction( new GraphStateAction( cy_window ) );
-    
 
+    final MNcollapserDialog dialog = new MNcollapserDialog(this.cyWindow);
+    dialog.setOptions(false,true,false,false);
+    dialog.setResizable(false);
+    this.cyWindow.getCyMenus().getOperationsMenu().add(
+                                                       new AbstractAction ("Meta Node Collapser"){
+                                                         public void actionPerformed (ActionEvent e){
+                                                           dialog.pack();
+                                                           dialog.setVisible(true);
+                                                         }//actionPerformed
+                                                       }
+                                                       );
   }//constructor
-
-  // ---------- Testing Actions ----------- //
-  public class ApplyModelForSelectedNodesAction extends AbstractAction {
-    public ApplyModelForSelectedNodesAction () {
-      super("Collapse Selected Metanodes");
-    }// cons
-
-    public void actionPerformed (ActionEvent event){
-      System.err.println("------------------ In ApplyModelForSelectedNodesAction ---------------");
-      GraphView graphView = cyWindow.getView();
-      CyNetwork cyNetwork = cyWindow.getNetwork();
-      GraphPerspective mainGP = cyNetwork.getGraphPerspective();
-      int [] selectedNodeIndices = graphView.getSelectedNodeIndices();
-      if(modeler == null){
-        modeler = new AbstractMetaNodeModeler(mainGP.getRootGraph());
-      }
-      System.out.println("Got " + selectedNodeIndices.length + " selected nodes.");
-      if(selectedNodeIndices != null && selectedNodeIndices.length > 0){
-        HashSet uniqueParents = new HashSet();
-        for(int i = 0; i < selectedNodeIndices.length; i++){
-          System.err.println("Selected node = " + selectedNodeIndices[i]);
-          
-          // TODO: GraphView has a bug that returns node indices as selected
-          // even if this nodes are no longer in GraphPerspective, Rowan will fix this
-          // For now, check that selected nodes are in mainGP
-          
-          // For each selected node, get its parents
-          if(selectedNodeIndices[i] > mainGP.getNodeCount()){
-            System.err.println("Node index " + selectedNodeIndices[i] + " is > " +
-                               mainGP.getNodeCount() + " (node count), so skipping node");
-            continue;
-          }
-          int rootIndex = mainGP.getRootGraphNodeIndex(selectedNodeIndices[i]);
-          System.err.println("Selected node RootGraph  index = " + rootIndex);
-          int [] parents = 
-            mainGP.getRootGraph().getNodeMetaParentIndicesArray(rootIndex); 
-          if(parents != null){
-            System.err.println("Selected node " + rootIndex + " has " +
-                               parents.length + " parents");
-          }
-          for(int j = 0; j < parents.length; j++){
-            uniqueParents.add(new Integer(parents[j]));
-          }// for j
-        }// for i
-        Iterator it = uniqueParents.iterator();
-        while(it.hasNext()){
-          int parentRootIndex =  ((Integer)it.next()).intValue();
-          modeler.applyModel(mainGP, parentRootIndex);
-         
-        }// while it
-      }// if
-    }//actionEvent
-  }//ApplyModelForSelectedNodesAction
-  
-  
-  // public class ApplyModelAction extends AbstractAction {
-//     public ApplyModelAction () { super("Apply Top Level Model");}
-    
-//     public void actionPerformed (ActionEvent event){
-//       System.out.println("--------------In ApplyModelAction.actionPerformed()--------------");
-//       GraphView graphView = cyWindow.getView();
-//       CyNetwork cyNetwork = cyWindow.getNetwork();
-//       GraphPerspective mainGP = cyNetwork.getGraphPerspective();
-//       TopLevelMetaNodeModel.applyModel(mainGP);
-//     }//actionPerformed
-//   }//ApplyModelAction
-
-//   public class UndoModel extends AbstractAction {
-//     public UndoModel (){
-//       super("Undo Model");
-//     }//cons
-    
-//     public void actionPerformed (ActionEvent event){
-//       System.out.println("-------------In UndoModel.actionPerformed()----------------");
-//       GraphView graphView = cyWindow.getView();
-//       CyNetwork cyNetwork = cyWindow.getNetwork();
-//       GraphPerspective mainGP = cyNetwork.getGraphPerspective();
-//       TopLevelMetaNodeModel.undoModel(mainGP);
-//     }//actionPerformed
-//   }//UndoModel
-
-  public class UndoModelForSelectedNodes extends AbstractAction{
-    
-    boolean recursive;
-    
-    public UndoModelForSelectedNodes(String name, boolean recursive){
-      super(name);
-      this.recursive = recursive;
-    }//cons
-    
-    public void actionPerformed (ActionEvent event){
-      System.out.println("----------------------- In UndoModelForSelectedNodes() -----------------");
-      GraphView graphView = cyWindow.getView();
-      CyNetwork cyNetwork = cyWindow.getNetwork();
-      GraphPerspective mainGP = cyNetwork.getGraphPerspective();
-      int [] selectedNodeIndices = graphView.getSelectedNodeIndices();
-      if(selectedNodeIndices != null && selectedNodeIndices.length > 0){
-        for(int i = 0; i < selectedNodeIndices.length; i++){
-          modeler.undoModel(mainGP,selectedNodeIndices[i],this.recursive,false);
-        }
-      }
-    }//actionEvent
-    
-  }//UndoModelForSelectedNodes
 
 }//class MetaNodeViewerCytoPlugin
