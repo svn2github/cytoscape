0ec625afbcffe502fe4ae232498a0c9d753f2333
rmkelley <rmkelley@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-05-13 01:07:17 +0000

    Changed all kinds of stuff to allow for investigation of missing data
    git-svn-id: http://chianti.ucsd.edu/svn@150 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/rmkelley/GodFather/GodFather.java
+++ csplugins/trunk/ucsd/rmkelley/GodFather/GodFather.java
@@ -1,197 +1,112 @@
-package csplugins.ucsd.rmkelley.GodFather;
-import java.util.*;
-import java.awt.event.ActionEvent;
-import javax.swing.AbstractAction;
-import javax.swing.JOptionPane;
+// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.geocities.com/kpdus/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   GodFather.java
 
-import giny.model.RootGraph;
-import giny.model.GraphPerspective;
-import giny.model.Node;
-import giny.model.Edge;
-import giny.view.GraphView;
-import giny.view.NodeView;
-import giny.model.RootGraphChangeListener;
-import giny.model.RootGraphChangeEvent;
-
-import cytoscape.plugin.AbstractPlugin;
-import cytoscape.data.GraphObjAttributes;
+import csplugins.jActiveModules.ActivePathsFinder;
+import csplugins.jActiveModules.Component;
+import csplugins.jActiveModules.data.ActivePathFinderParameters;
 import cytoscape.data.CyNetwork;
-import cytoscape.view.CyWindow;
-import cytoscape.util.GinyFactory;
+import cytoscape.data.CyNetworkFactory;
+import giny.model.*;
+import java.util.*;
 
-/**
- * This is a sample Cytoscape plugin using Giny graph structures. For each
- * currently selected node in the graph view, the action method of this plugin
- * additionally selects the neighbors of that node if their canonical name ends
- * with the same letter. (For yeast genes, whose names are of the form 'YOR167C',
- * this selects genes that are on the same DNA strand). This operation was
- * chosen to be illustrative, not necessarily useful.
- *
- * Note that selection is a property of the view of the graph, while neighbors
- * are a property of the graph itself. Thus this plugin must access both the
- * graph and its view.
- */
-public class Temp extends AbstractPlugin{
-    
-  CyWindow cyWindow;
-  private static String TYPE_ATTRIBUTE = "Type";
-  private static String LABEL_ATTRIBUTE = "Label";
-  
-    
-  /**
-   * This constructor saves the cyWindow argument (the window to which this
-   * plugin is attached) and adds an item to the operations menu.
-   */
-  public Temp(CyWindow cyWindow) {
-    this.cyWindow = cyWindow;
-    cyWindow.getCyMenus().getOperationsMenu().add( new SamplePluginAction() );
-  }
-    
-  /**
-   * This class gets attached to the menu item.
-   */
-  public class SamplePluginAction extends AbstractAction {
-        
-    /**
-     * The constructor sets the text that should appear on the menu item.
-     */
-    public SamplePluginAction() {super("Sample action");}
-        
-    /**
-     * Gives a description of this plugin.
-     */
-    public String describe() {
-      StringBuffer sb = new StringBuffer();
-      sb.append("testing");
-      return sb.toString();
-    }
-        
-    /**
-     * This method is called when the user selects the menu item.
-     */
-    public void actionPerformed(ActionEvent ae) {
-                       
-      //inform listeners that we're doing an operation on the network
-      Thread t = new MyTask(cyWindow); 
-      t.start();
-    }
-  }
-}
+public class GodFather
+{
 
-public class MyTask extends Thread{
-  protected static String STATE_ATTRIBUTE = "expression";
-  CyWindow cyWindow;
-  Random rand;  
-  public MyTask(CyWindow cyWindow){
-    this.cyWindow = cyWindow;
-    this.rand = new Random();
-  }
-  public void run(){
-        
-    GraphGenerator myGenerator = new ModularGenerator(size,modularity);
-    //for loop wrapped around calls to runParameterCombination
-    
-    
+  public static void main(String bob[])
+  {
+    int size = 15;
+    double signal_ratio = 5D;
+    double percent = 1.0D;
+    String sif_file = "temp.sif";
+    String attr_names[] = {
+      "placeholder"
+    };
+    CyNetwork network = CyNetworkFactory.createNetworkFromInteractionsFile(sif_file);
+    ActivePathFinderParameters apfParams = new ActivePathFinderParameters();
+    apfParams.setRegionalBoolean(false);
+    apfParams.setMCboolean(false);
+    apfParams.setNumberOfPaths(1);
+    apfParams.setSearchDepth(2);
+    apfParams.setMaxDepth(3);
+				apfParams.setMaxThreads(2); 
+				double selectivity = 0.0D;
+    double sensitivity = 0.0D;
+    int iterations = 1;
+    for(int iteration = 0; iteration < iterations; iteration++)
+      {
+	System.err.println(""+iteration);
+	Set module = getModule(network.getRootGraph(), size);
+	HashMap expressionMap = createExpressionMap(network, module, signal_ratio, percent);
+	ActivePathsFinder apf = new ActivePathsFinder(expressionMap, attr_names, network, apfParams, null);
+	double result[] = compare(apf.findActivePaths()[0], module);
+	selectivity += result[0];
+	sensitivity += result[1];
+      }
+    System.out.println(""+selectivity/(double)iterations);
+    System.out.println(""+sensitivity/(double)iterations);
+
   }
 
-  protected void runParameterCombination(int moduleSize,double sn, double nsp, GraphGenerator myGenerator){
-    RootGraph randomGraph = myGenerator.getRandomGraph();
-    Set module = getModule(randomGraph,moduleSize);
-    CyNetwork network = new CyNetwork(randomGraph,new GraphObjAttributes(),new GraphObjAttributes());
-    setAndBlurAttribute(network,module,STATE_ATTRIBUTE,sn);
-    scrambleNetwork(network, nsp);
-    
-    Set foundModule = null;
-    /*Call jActiveModules to return what it thinks is the best module in the current network*/
-    //foundModule = jActive.getModule(network);
-    ///for rmk
-    compareAndTabulate(module,foundModule);
-   
+  protected static double[] compare(Component foundModule, Set definedModule)
+  {
+    int foundSize = foundModule.getNodes().size();
+    int definedSize = definedModule.size();
+    Iterator nodeIterator = definedModule.iterator();
+    int overlap = 0;
+    while(nodeIterator.hasNext()) 
+      if(foundModule.contains((Node)nodeIterator.next()))
+	overlap++;
+    double result[] = new double[2];
+    result[0] = (double)overlap / (double)foundSize;
+    result[1] = (double)overlap / (double)definedSize;
+    return result;
   }
 
-  protected void compareAndTabulate(Set module, Set foundModule){
-    //figure out the overlap between these two and
-    //update our global sensitivity and selectivity data structure
-    //for jfm
-  }
-
-  /**
-   * Returns a set random connected modules of size nodes
-   * from the graph
-   */
-  protected Set getModule(RootGraph graph, int size){
+  protected static Set getModule(RootGraph graph, int size)
+  {
     HashSet result = new HashSet();
-    //here we would randomly choose size selected
-    //nodes from graph and add them to result
-    //for whoever
-
+    List nodes = graph.nodesList();
+    Random rand = new Random();
+    Node seed = (Node)nodes.get(rand.nextInt(nodes.size()));
+    result.add(seed);
+    List neighbors = new LinkedList();
+    neighbors.addAll(graph.neighborsList(seed));
+    while(result.size() < size) 
+      {
+	Node nextNeighbor = (Node)neighbors.remove(rand.nextInt(neighbors.size()));
+	if(result.add(nextNeighbor))
+	  neighbors.addAll(graph.neighborsList(nextNeighbor));
+      }
     return result;
   }
 
-  /**
-   * Assign values to "attribute" in hte nodeAttributes for network
-   */
-  protected void setAndBlurAttribute(CyNetwork network,Set module,String attribute){
-    //for rmk    
-    //really also need signal to noise ratio here
-    double signalratio = 5.0;
-    HashMap expressionHash = new HashMap();
-    String condition = "condition";
-    
-    for (Iterator nodeIt = network.getGraphPerspective().nodesIterator();nodeIt.hasNext();) {
-      Node current = (Node)nodeIt.hasNext();
-      if (module.contains(current)) {
-	expressoinHash.put(current,new double[]{normalCDF(signalratio-rand.nextGaussian())};
-      } // end of if ()
-      else {
-	expressionHash.put(current,new double[]{rand.nextDouble()});
-      } // end of else
-    } // end of for ()
-    
-  }
+  protected static HashMap createExpressionMap(CyNetwork network, Set module, double signal_ratio, double percent)
+  {
+    Random rand = new Random();
+    HashMap expressionMap = new HashMap();
+    for(Iterator nodeIt = network.getGraphPerspective().nodesIterator(); nodeIt.hasNext();)
+      {
+	Node current = (Node)nodeIt.next();
+	if(rand.nextDouble() < percent)
+	  {
+	    if(module.contains(current))
+	      expressionMap.put(current, new double[] {
+		signal_ratio - rand.nextGaussian()
+	      });
+	    else
+	      expressionMap.put(current, new double[] {
+		rand.nextGaussian()
+	      });
+	  } else
+            {
+	      expressionMap.put(current, new double[] {
+		0.0D
+	      });
+            }
+      }
 
-  /**
-   * Add/remove edges randomly from this network according to some parameter
-   */
-  protected void scrambleNetwork(CyNetwork network, double parameter){
-    //for jfm
-  }
-      
-}
-
-
-
-abstract class GraphGenerator {
-  public abstract RootGraph getRandomGraph();
-  public abstract RootGraph getSize(int size);
-}
-
-class ModularGenerator extends GraphGenerator{
-  int size;
-  int modularity;
-  public ModularGenerator(int size, int modularity){
-    setSize(size);
-    setModularity(modularity);
-  }
-  
-  public void setSize(int size){
-    this.size = size;
-  }
-
-  public void getSize(int size){
-    return size;
-  }
-
-  public void setModularity(int modularity){
-    this.modularity = modularity;
-  }
-  
-  public RootGraph getRandomGraph(){
-    RootGraph result = GinyFactory.getRootGraph();
-    //here we would add ndoes and edges to result in a random
-    //to create a modular scale-free network
-
-
-    return result;
+    return expressionMap;
   }
 }
