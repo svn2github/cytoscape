b658b918ed3da936e2f90a09db924ec89b7d4dee
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-07-15 18:27:09 +0000

    Initial Commit
    git-svn-id: http://chianti.ucsd.edu/svn@4223 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/README.txt
@@ -0,0 +1,13 @@
+README.txt
+----------------
+
+This directory contains all files for cytoscape.org.
+
+I.  Deploying to cytoscape.org
+
+To deploy to cytoscape.org, type:
+
+ant deploy
+
+To do so, you will need a cytoscape.org password.  Update cyto.properties
+with the correct password.
---
+++ cyto_web/trunk/collab.php
@@ -0,0 +1,42 @@
+<div class="roundbox blue_outline">
+<br>
+	The <b> Cytoscape </b> project is an ongoing collaboration between: 
+<br>
+<table cellpadding="2" cellspacing="2" border="0" width="100%" align="center">
+
+<tbody>
+	<tr>
+		<td width="10">
+			<br>
+		</td>
+		<td valign="middle">
+			<img border="0" align="middle" alt="ISB" height="60" width="60" src="images/isb60.gif"> <a href="http://www.systemsbiology.org"> Institute for Systems Biology </a> 
+	</td>
+</tr>
+<tr>
+	<td width="10">
+		<br>
+	</td>
+	<td valign="middle">
+		<img border="0" align="middle" alt="UCSD" height="60" width="60" src="images/ucsd60.gif"> <a href="http://www.ucsd.edu"> University of California at San Diego </a> 
+</td>
+</tr>
+<tr>
+	<td width="10">
+		<br>
+	</td>
+	<td valign="middle">
+		<img border="0" align="middle" alt="MSKCC" height="60" width="60" src="images/mskcc60.gif"> <a href="http://www.mskcc.org"> Memorial Sloan-Kettering Cancer Center </a> 
+</td>
+</tr>
+<tr>
+	<td width="10">
+		&nbsp; 
+	</td>
+	<td valign="middle">
+		<a href="http://www.pasteur.fr"> <img src="images/ip60.gif" width="60" height="60" border="0" alt="IP" align="middle"> </a> <a href="http://www.pasteur.fr"> Institut Pasteur </a> 
+	</td>
+</tr>
+</tbody>
+</table>
+</div>
