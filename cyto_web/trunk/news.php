c630735f34ea07120394ca2e8373fc7d616c0a9c
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-07-22 15:55:03 +0000

    Moved community info out into its own component
    git-svn-id: http://chianti.ucsd.edu/svn@4243 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/news.php
+++ cyto_web/trunk/news.php
@@ -22,26 +22,3 @@ interactions retrieved from the cPath database.
 </TD>
 </TR>
 </TABLE>
-<P>
-<table width=100%>
-<TR><TD>
-<div class="roundbox peach">
-<B>Cytoscape Community:</B>
-
-<P>
-Cytoscape currently supports two mailing lists, both of which are hosted at 
-<A HREF="http://groups-beta.google.com/">Google Groups</A>:
-<UL>
-<LI>cytoscape-announce:  Low-volume mailing list used to accounce
-new developments in Cytoscape.
-<BR><A HREF="community.php">Subscribe</A> | <A HREF="http://groups-beta.google.com/group/cytoscape-announce">Browse Archives</A>
-<P>
-<LI>cytoscape-discuss:  Open forum for discussing Cytoscape, asking
-questions, and suggesting new features<BR
-<A HREF="community.php">Subscribe</A> | <A HREF="http://groups-beta.google.com/group/cytoscape-discuss">Browse Archives</A>
-</UL>
-<P>Please join one of our mailing lists and get involved!
-</div>
-</TD>
-</TR>
-</TABLE>
