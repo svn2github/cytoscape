ef52761d15fa25f4b55d66d79836b7d60090d6c6
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-07-22 22:18:57 +0000

    Added view all pubs link
    git-svn-id: http://chianti.ucsd.edu/svn@4266 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/index.php
+++ cyto_web/trunk/index.php
@@ -52,7 +52,8 @@
 	<div id="content">
 		<div id="indent">
 			<div class="item">
-				<h2> <I>New!</I>&nbsp;&nbsp;Announcing Cytoscape 2.0 </h2> 
+				<BR>
+				 <big><b><I>New!</I>&nbsp;&nbsp;Announcing Cytoscape 2.0 </b></big> 
 				<table width="100%">
 					<tr valign="top">
 						<td width="47%" valign="top" ">
@@ -91,7 +92,7 @@
 			</div>
 			<P>
 			<div class="item">
-				<h2> Genome Research article on Cytoscape 
+				 <big><b>Genome Research article on Cytoscape </b></big>
 					<br>
 				</h2> 
 				<p>
@@ -103,68 +104,60 @@
 			</div>
 			<P>
 			<div class="item">
-				<h2> Research using Cytoscape </h2> 
-				<br>
-				<p>
-					<b> Genome Research, June 2004 (cover article) </b> 
-					<br>
-						Baliga, NS, Bjork, SJ, Bonneau, R, Pan, M, Iloanusi, C, Kottemann, MCH, Hood, L, and DiRuggiero, J. 
-						<BR>Systems level insights into the stress ersponds to UV radiation in the halophilic archaeon Halobacterium NRC-1.
-						<BR>Genome Res. 2004 14: 1025-35. 
-					<br>
-						[<a href="http://www.genome.org/cgi/content/abstract/14/6/1025" class="Textlink" target="_blank">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/6/1025.pdf" class="Textlink" target="_blank">PDF</a>][<a href="http://www.genome.org/cgi/content/full/1993504/DC1" class="Textlink" target="_blank">Supplemental Research Data</a>] 
-				<p>
-				<br>
-				<p>
-					<b> Genome Research, March 2004 </b> 
-				<br>
-					Prinz S, Avila-Campillo I, Aldridge C, Srinivasan A, Dimitrov K, Siegel AF, and Galitski T.
-					<BR>Control of Yeast Filamentous-Form Growth by Modules in an Integrated Molecular Network.
-					<BR>Genome Res. 2004 14: 380-390. 
-				<br>
-					[<a href="http://www.genome.org/cgi/content/abstract/14/3/380" class="Textlink" target="_blank">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/3/380.pdf" class="Textlink" target="_blank">PDF</a>] [<a href="http://www.genome.org/cgi/content/full/14/3/380/DC1" class="Textlink" target="_blank">Supplemental Research Data</a>] 
-				</p>
-
-				<br>
-				<P>
-					<b>Seminars in Immunology, January 2004</b>
-					<BR>A systems approach to dissecting immunity and inflammation
-					<BR>Aderem A, Smith KD.
-					<BR>Semin Immunol. 2004 Feb;16(1):55-67.
-					<br>[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=pubmed&dopt=Abstract&list_uids=14751764">Pub Med Entry</A>]
-				[<a href="http://www.sciencedirect.com/science?_ob=ArticleURL&_udi=B6WX3-4BHSYWW-9&_coverDate=02%2F29%2F2004&_alid=188374479&_rdoc=1&_fmt=&_orig=search&_qd=1&_cdi=7147&_sort=d&view=c&_acct=C000016018&_version=1&_urlVersion=0&_userid=281587&md5=110a35c81c74edf330477297c5d59184">Full Text</A>]
-				<br>&nbsp;
-				<br>
-					<b>PNAS, Septemer 2003</b>
-				<br>
-				Kelley BP, Sharan R, Karp RM, Sittler T, Root DE, Stockwell BR, Ideker T.
-				<BR>Conserved pathways within bacteria and yeast as revealed by global protein network alignment.
-				<BR>Proc Natl Acad Sci U S A. 2003 Sep 30;100(20):11394-9.
-				<br>[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&db=pubmed&dopt=Abstract&list_uids=14504397">Pub Med Entry</A>]
-				[<a href="http://www.pubmedcentral.nih.gov/articlerender.fcgi?tool=pubmed&pubmedid=14504397">Full Text</A>]
-				<BR>&nbsp;
-				<BR>
-<p>
-<b> RECOMB 2003 </b> 
+ <big><b>Research using Cytoscape</b></big> 
+ &nbsp;&nbsp;
+[<a href="pubs.php">View All Publications</a>]
 <br>
-	Yeang C-H, Jaakkola T. Physical network models and multi-source data integration. <i>
-	<BR>The Seventh Annual International Conference on Research in Computational Molecular Biology</i>, 2003 
-	<a href="http://www.ai.mit.edu/people/tommi/papers/YeaJaa-recomb03.pdf"> [PDF] </a> 
-</p>
 <br>
 <p>
-<b> Molecular Cancer Research, 2002 </b> 
+	<b>Genome Biology, July 2004</b> 
+	<br>
+		Bonneau, Richard, Baliga Nitin S, Deutsch, Eric W, Shannon Paul and Hood, Leroy 
+	<br>
+		Comprehensive de novo structure prediction in a systems-biology context for the archaea Halobacterium sp. NRC-1 
+	<br>
+		Genome Biology 2004, 5:R52 
+	<br>
+		[<a href="http://genomebiology.com/2004/5/8/r52/abstract">Abstract</a>] [<a href="http://genomebiology.com/2004/5/8/r52">Full Text</a>] [<a href="http://genomebiology.com/content/pdf/gb-2004-5-8-r52.pdf">PDF</a>] 
 <br>
-	Begley TJ, Rosenbach AS, Ideker T, Samson LD.
-	<BR>Damage Recovery Pathways in Saccharomyces cerevisiae Revealed by Genomic Phenotyping and Interactome Mapping.
-	<BR>Mol Cancer Res. 2002 Dec;1(2):103-12. <a target="_blank" href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids=12496357&amp;dopt=Abstract"> [PubMed entry] </a> 
+&nbsp; 
+<p>
+<b> Genome Research, June 2004 (cover article) </b> 
+<br>
+	Baliga, NS, Bjork, SJ, Bonneau, R, Pan, M, Iloanusi, C, Kottemann, MCH, Hood, L, and DiRuggiero, J. 
+<br>
+	Systems level insights into the stress ersponds to UV radiation in the halophilic archaeon Halobacterium NRC-1. 
+<br>
+	Genome Res. 2004 14: 1025-35. 
+<br>
+	[<a href="http://www.genome.org/cgi/content/abstract/14/6/1025" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/6/1025.pdf" class="Textlink">PDF</a>][<a href="http://www.genome.org/cgi/content/full/1993504/DC1" class="Textlink">Supplemental Research Data</a>] 
+<p>
+<br>
+<p>
+	<b> Genome Research, March 2004 </b> 
+	<br>
+		Prinz S, Avila-Campillo I, Aldridge C, Srinivasan A, Dimitrov K, Siegel AF, and Galitski T. 
+	<br>
+		Control of Yeast Filamentous-Form Growth by Modules in an Integrated Molecular Network. 
+	<br>
+		Genome Res. 2004 14: 380-390. 
+	<br>
+		[<a href="http://www.genome.org/cgi/content/abstract/14/3/380" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/3/380.pdf" class="Textlink">PDF</a>] [<a href="http://www.genome.org/cgi/content/full/14/3/380/DC1" class="Textlink">Supplemental Research Data</a>] 
 </p>
+<p. 
 <br>
-<b> <a href="http://bioinformatics.oupjournals.org/cgi/content/abstract/18/suppl_1/S233"> ISMB 2002 </a> </b> 
+<p>
+<b>Science, Feb 2004</b> 
 <br>
-Ideker T, Ozier O, Schwikowski B, Siegel A.
-<BR>Discovering regulatory and signaling circuits in molecular interaction networks. <a href="http://bioinformatics.oupjournals.org/"> Bioinformatics </a> 18, S233 (2002). <a href="http://bioinformatics.oupjournals.org/cgi/content/abstract/18/suppl_1/S233"> [Abstract] </a> <a href="http://bioinformatics.oupjournals.org/cgi/reprint/18/suppl_1/S233.pdf"> [PDF] </a> <a target="_blank" href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids=12169552&amp;dopt=Abstract"> [PubMed entry] </a> <a href="ISMB2002/index.html"> [Supplementary Material] </a> 
+	Tong AH, et al., Global mapping of the yeast genetic interaction network. 
 <br>
+	Science. 2004 Feb 6;303(5659):808-13. 
+<br>
+	[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=pubmed&amp;dopt=Abstract&amp;list_uids=14764870">Pub Med Entry</a>] [<a href="http://www.sciencemag.org/cgi/content/full/303/5659/808">Full Text</a>] 
+<br>
+&nbsp; 
+<p><B>Note:</B>  If you have a publication which makes use of Cytoscape, please let us know by sending an email to the 
+<A HREF="http://groups-beta.google.com/group/cytoscape-announce">cytoscape-discuss</a> mailing list.
 </div>
 </td>
 </div>
