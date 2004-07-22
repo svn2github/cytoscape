85bb495a62a5c3ad211cbace3cda9ad9d73b547e
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-07-22 22:13:27 +0000

    Initial Commit
    git-svn-id: http://chianti.ucsd.edu/svn@4263 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/pubs.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+	<title> Research using Cytoscape </title> 
+	<link rel="stylesheet" type="text/css" media="screen" href="css/cytoscape.css">
+	<link rel="shortcut icon" href="images/cyto.ico">
+</head>
+<body bgcolor="#FFFFFF">
+<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
+	<tr>
+		<td width="10">
+			  
+		</td>
+		<td valign="center">
+			<h1> Research using Cytoscape </h1> 
+		</td>
+	</tr>
+</table>
+<? include "nav.php"; ?>
+<div id="indent">
+	<p>
+		<b>Genome Biology, July 2004</b> 
+		<br>
+		Bonneau, Richard, Baliga Nitin S, Deutsch, Eric W, Shannon Paul and Hood, Leroy 
+		<br>
+		Comprehensive de novo structure prediction in a systems-biology context for the archaea Halobacterium sp. NRC-1 
+		<br>
+		Genome Biology 2004, 5:R52 
+		<br>
+		[<a href="http://genomebiology.com/2004/5/8/r52/abstract">Abstract</a>] [<a href="http://genomebiology.com/2004/5/8/r52">Full Text</a>] [<a href="http://genomebiology.com/content/pdf/gb-2004-5-8-r52.pdf">PDF</a>]
+
+	<p>
+		<b> Genome Research, June 2004 (cover article) </b> 
+		<br>
+		Baliga, NS, Bjork, SJ, Bonneau, R, Pan, M, Iloanusi, C, Kottemann, MCH, Hood, L, and DiRuggiero, J. 
+		<br>
+		Systems level insights into the stress ersponds to UV radiation in the halophilic archaeon Halobacterium NRC-1. 
+		<br>
+		Genome Res. 2004 14: 1025-35. 
+		<br>
+		[<a href="http://www.genome.org/cgi/content/abstract/14/6/1025" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/6/1025.pdf" class="Textlink">PDF</a>][<a href="http://www.genome.org/cgi/content/full/1993504/DC1" class="Textlink">Supplemental Research Data</a>] 
+
+	<p>
+		<b> Genome Research, March 2004 </b> 
+		<br>
+		Prinz S, Avila-Campillo I, Aldridge C, Srinivasan A, Dimitrov K, Siegel AF, and Galitski T. 
+		<br>
+		Control of Yeast Filamentous-Form Growth by Modules in an Integrated Molecular Network. 
+		<br>
+		Genome Res. 2004 14: 380-390. 
+		<br>
+		[<a href="http://www.genome.org/cgi/content/abstract/14/3/380" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/3/380.pdf" class="Textlink">PDF</a>] [<a href="http://www.genome.org/cgi/content/full/14/3/380/DC1" class="Textlink">Supplemental Research Data</a>] 
+	</p>
+	<p>
+		<b>Science, Feb 2004</b> 
+		<br>
+		Tong AH, et al., Global mapping of the yeast genetic interaction network. 
+		<br>
+		Science. 2004 Feb 6;303(5659):808-13. 
+		<br>
+		[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=pubmed&amp;dopt=Abstract&amp;list_uids=14764870">Pub Med Entry</a>] [<a href="http://www.sciencemag.org/cgi/content/full/303/5659/808">Full Text</a>] 
+	<p>
+		<b>Seminars in Immunology, January 2004</b> 
+		<br>
+		A systems approach to dissecting immunity and inflammation. 
+		<br>
+		Aderem A, Smith KD. 
+		<br>
+		Semin Immunol. 2004 Feb;16(1):55-67. 
+		<br>
+		[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=pubmed&amp;dopt=Abstract&amp;list_uids=14751764">Pub Med Entry</a>] [<a href="http://www.sciencedirect.com/science?_ob=ArticleURL&amp;_udi=B6WX3-4BHSYWW-9&amp;_coverDate=02%2F29%2F2004&amp;_alid=188374479&amp;_rdoc=1&amp;_fmt=&amp;_orig=search&amp;_qd=1&amp;_cdi=7147&amp;_sort=d&amp;view=c&amp;_acct=C000016018&amp;_version=1&amp;_urlVersion=0&amp;_userid=281587&amp;md5=110a35c81c74edf330477297c5d59184">Full Text</a>] 
+		<br>
+		&nbsp; 
+		<br>
+		<b>PNAS, Septemer 2003</b> 
+		<br>
+		Kelley BP, Sharan R, Karp RM, Sittler T, Root DE, Stockwell BR, Ideker T. 
+		<br>
+		Conserved pathways within bacteria and yeast as revealed by global protein network alignment. 
+		<br>
+		Proc Natl Acad Sci U S A. 2003 Sep 30;100(20):11394-9. 
+		<br>
+		[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=pubmed&amp;dopt=Abstract&amp;list_uids=14504397">Pub Med Entry</a>] [<a href="http://www.pubmedcentral.nih.gov/articlerender.fcgi?tool=pubmed&amp;pubmedid=14504397">Full Text</a>] 
+		<br>
+		&nbsp; 
+		<br>
+</div>
+<? include "footer.php"; ?>
+</body>
+</html>
