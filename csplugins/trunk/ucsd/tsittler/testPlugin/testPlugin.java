fdf935a2e916f153c8bfd4cc69735a3895678a54
taylor <taylor@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-08-02 20:38:41 +0000

    Test Plugin for Error Chacking of the Abstract GenericPlugin
    git-svn-id: http://chianti.ucsd.edu/svn@212 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/tsittler/testPlugin/testPlugin.java
@@ -0,0 +1,35 @@
+package testPlugin;
+import java.util.*;
+import GenericPlugin.*;
+import java.io.File;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+public class testPlugin extends PluginUtil{
+    public testPlugin(){
+	initialize();
+    }
+
+    public HashMap getActions(){
+	HashMap actions=new HashMap(1);
+	HashMap args=new HashMap(5);
+	args.put("name","something");
+	args.put("intVal",new Integer(123));
+	args.put("fileVal",new File(""));
+	args.put("filesVal",new File[2]);
+	args.put("doubleVal",new Double(1234));
+	actions.put("doSomething",args);
+	return actions;
+    }
+
+    public void run(String action,HashMap args){
+	String outputStr="action: "+action;
+	Iterator it=args.keySet().iterator();
+	while (it.hasNext()){
+	    String nextOne=it.next().toString();
+	    outputStr=outputStr+nextOne+"="+args.get((Object)nextOne).toString();
+	}
+	JFrame frame=new JFrame();
+	JOptionPane.showMessageDialog(frame, outputStr);
+    }
+}
