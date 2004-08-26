7cc56ddfb0304694a314c8377ed85d1a8af43166
rmkelley <rmkelley@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-08-26 21:46:39 +0000

    Fixed selection code to use tagger instead of nodeview
    git-svn-id: http://chianti.ucsd.edu/svn@5969 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SelectFirstNeighborsAction.java
+++ cytoscape/trunk/src/cytoscape/actions/SelectFirstNeighborsAction.java
@@ -8,9 +8,14 @@ package cytoscape.actions;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import java.util.Vector;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
 
 import cytoscape.util.CytoscapeAction;
 import cytoscape.Cytoscape;
+import cytoscape.CyNetwork;
+import giny.model.Node;
 //-------------------------------------------------------------------------
 /**
  *  select every first neighbor (directly connected nodes) of the currently
@@ -24,7 +29,16 @@ public class SelectFirstNeighborsAction extends CytoscapeAction {
         setAcceleratorCombo( java.awt.event.KeyEvent.VK_F6,0 );
     }
     public void actionPerformed (ActionEvent e) {
-        GinyUtils.selectFirstNeighbors( Cytoscape.getCurrentNetworkView() );
+      CyNetwork currentNetwork = Cytoscape.getCurrentNetwork();
+      Set flaggedNodes = currentNetwork.getFlaggedNodes();
+      Set firstNeighbors = new HashSet();
+      for (Iterator nodeIt = flaggedNodes.iterator();nodeIt.hasNext();){
+	Node currentNode = (Node)nodeIt.next();
+	for(Iterator neighborIt = currentNetwork.neighborsList(currentNode).iterator();neighborIt.hasNext();){
+	  firstNeighbors.add(neighborIt.next());
+	}
+      }
+      currentNetwork.setFlaggedNodes(firstNeighbors,true);
     } // actionPerformed
 } // SelectFirstNeighborsAction
 
