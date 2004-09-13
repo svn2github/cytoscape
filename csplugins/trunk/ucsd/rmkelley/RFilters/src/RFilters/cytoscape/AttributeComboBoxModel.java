627f5ffedef589996ba1717b6e51e823ebf42d65
rmkelley <rmkelley@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-09-13 15:55:43 +0000

    Adding a combobox model for attributes
    git-svn-id: http://chianti.ucsd.edu/svn@307 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/rmkelley/RFilters/src/RFilters/cytoscape/AttributeComboBoxModel.java
@@ -0,0 +1,99 @@
+package filter.cytoscape;
+
+import java.util.*;
+import filter.view.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.beans.*;
+import cytoscape.Cytoscape;
+import cytoscape.data.GraphObjAttributes;
+
+public abstract class AttributeComboBoxModel implements ComboBoxModel, PropertyChangeListener{
+
+  protected static AttributeManager NODE_ATTRIBUTE_MANAGER;
+  protected static AttributeManager EDGE_ATTRIBUTE_MANAGER;
+  protected Object selectedObject;
+  protected Vector attributeList;
+ 
+  protected AttributeComboBoxModel () {
+    attributeList = new Vector();
+    Cytoscape.getSwingPropertyChangeSupport().addPropertyChangeListener(this);
+  }
+
+
+  public void notifyListeners(){
+    for(Iterator listenIt = listeners.iterator();listenIt.hasNext();){
+      ((ListDataListener)listenIt.next()).contentsChanged(new ListDataEvent(this,ListDataEvent.CONTENTS_CHANGED,0,attributeList.size()));
+    }
+  }
+  
+  //implements PropertyChange
+  public abstract void propertyChange(PropertyChangeEvent pce);
+
+
+  //implements ListModel
+  Vector listeners = new Vector();
+  public void addListDataListener(ListDataListener l){
+    listeners.add(l);
+  }
+    
+  public Object getElementAt(int index){
+    return attributeList.elementAt(index);
+  }
+   
+  public int getSize(){
+    return attributeList.size();
+  }
+  
+  public void removeListDataListener(ListDataListener l){
+    listeners.remove(l);
+  }
+
+  public void setSelectedItem(Object item){
+    selectedObject = item;
+  }
+
+  public Object getSelectedItem(){
+    return selectedObject;
+  }
+      
+
+}
+
+class NodeAttributeComboBoxModel extends AttributeComboBoxModel{
+  Class attributeClass;
+  GraphObjAttributes nodeAttributes;
+  public NodeAttributeComboBoxModel(Class attributeClass){
+    nodeAttributes = Cytoscape.getNodeNetworkData();
+    this.attributeClass = attributeClass;
+  }
+  public void propertyChange(PropertyChangeEvent pce){
+    String [] nodeAttributes = Cytoscape.getNodeAttributesList();
+    attributeList = new Vector();
+    for ( int idx = 0; idx < nodeAttributes.length; idx++) {
+      if (attributeClass.isAssignableFrom(this.nodeAttributes.getClass(nodeAttributes[idx]))) {
+	attributeList.add(nodeAttributes[idx]);
+      } // end of for ()
+      notifyListeners();
+    }
+  }
+}
+
+class EdgeAttributeComboBoxModel extends AttributeComboBoxModel{
+  Class attributeClass;
+  GraphObjAttributes edgeAttributes;
+  public EdgeAttributeComboBoxModel(Class attributeClass){
+    edgeAttributes = Cytoscape.getEdgeNetworkData();
+    this.attributeClass = attributeClass;
+  }
+  public void propertyChange(PropertyChangeEvent pce){
+    String [] edgeAttributes = Cytoscape.getEdgeAttributesList();
+    attributeList = new Vector();
+    for ( int idx = 0; idx < edgeAttributes.length; idx++) {
+      if (attributeClass.isAssignableFrom(this.edgeAttributes.getClass(edgeAttributes[idx]))) {
+	attributeList.add(edgeAttributes[idx]);
+      } // end of for ()
+      notifyListeners();
+    }
+  }
+}
