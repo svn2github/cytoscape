c529744b5a4b797a9041cb3dc17d5187ed4e2ed6
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-09-20 20:03:50 +0000

    Recording my thoughts before I forget them. The final location and/or usefulness of this class is not determined yet.
    git-svn-id: http://chianti.ucsd.edu/svn@6002 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/trunk/src/org/cytoscape/graph/layout/LayoutAlgorithm.java
@@ -0,0 +1,44 @@
+package org.cytoscape.graph.layout;
+
+/**
+ * This class encapsulates the logic needed to perform a layout on a graph.
+ * One of the goals of this class is to limit the set
+ * of dependent classes and packages to an absolute minimum.  This class
+ * should be so concise that it would be a simple effort to
+ * provide layout algorithms as web services using this layout framework.
+ */
+public abstract class LayoutAlgorithm implements Runnable
+{
+
+  protected final LayoutGraph graph;
+
+  protected LayoutAlgorithm(LayoutGraph graph)
+  {
+    if (graph == null) throw new NullPointerException("graph is null");
+    this.graph = graph;
+  }
+
+  /**
+   * <code>run()</code> is called externally, not by subclasses.
+   * Subclasses must implement this method - layout logic should be
+   * executed in this method, by the same thread that calls this method.
+   * The layout logic should act on
+   * the <code>LayoutGraph</code> stored in <code>this.graph</code>.
+   * <code>run()</code> shall only be called once for a given instance.
+   * If an asynchronous call to <code>destroy()</code> is made while
+   * <code>run()</code> is executing, <code>run()</code> should make an effort
+   * to abort its operations and exit as soon as possible.
+   */
+  public abstract void run();
+
+  /**
+   * <code>destroy()</code> is called externally, not by subclasses.
+   * Subclasses must implement this method.  <code>destroy()</code> should
+   * not block; it should return quickly.  If [an asynchronous] thread is
+   * executing <code>run()</code> when <code>destroy()</code> is called,
+   * a signal should be sent to the thread that is executing <code>run()</code>
+   * to abort and exit <code>run()</code>.
+   */
+  public abstract void destroy();
+
+}
