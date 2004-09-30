ef38569f44e0c68fe290adf78289a3cc56de0633
cmak <cmak@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-09-30 23:08:44 +0000

    forgot a file
    git-svn-id: http://chianti.ucsd.edu/svn@337 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/cmak/factor/src/FGNode.java
@@ -0,0 +1,21 @@
+public abstract class FGNode
+{
+    private NodeType type;
+
+    public FGNode(NodeType t)
+    {
+        type = t;
+    }
+
+    public boolean isType(NodeType t)
+    {
+        return t == type;
+    }
+    
+    public NodeType type()
+    {
+        return type;
+    }
+
+    
+}
