2b431fafb2eb1227e362481330ea9987817fb26b
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-08 16:28:34 +0000

    Adding Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@6079 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/graph/layout/algorithm/MutableGraphLayout.java
+++ cytoscape/trunk/src/cytoscape/graph/layout/algorithm/MutableGraphLayout.java
@@ -13,7 +13,14 @@ public interface MutableGraphLayout extends GraphLayout
    * This is a hook for layout algorithms to actually set locations of
    * nodes.  Layout algorithms should call this method.<p>
    * X, Y values set by this method shall be reflected in the return values
-   * of <code>getNodePosition()</code>.
+   * of <code>getNodePosition()</code>.<p>
+   * Layout algorithms are encouraged to set node positions such that
+   * their X and Y values use the full range of allowable values, including
+   * the boundary values <code>0</code>, <code>getMaxWidth()</code>, and
+   * <code>getMaxHeight()</code>.  Any notion of node thickness, graph
+   * border on perimeter, etc. should be predetermined by the application
+   * using a layout algorithm; <code>getMaxWidth()</code> and
+   * <code>getMaxHeight()</code> should be defined accordingly.
    *
    * @exception IllegalArgumentException if
    *   <nobr><code>xPos < 0.0</code></nobr>, if
