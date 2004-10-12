18f49af4f86df00d8b8b01bc187c02168c693dd5
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-12 20:33:21 +0000

    Now fully bypassing legacy code in cytoscape.layout.*; using the new cytoscape.graph.layout.impl.SpringEmbeddedLayouter2. Also, adding progress UI.  It's really much nicer now from the user perspective.
    git-svn-id: http://chianti.ucsd.edu/svn@6117 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutAction.java
+++ cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutAction.java
@@ -5,43 +5,183 @@
 //-------------------------------------------------------------------------
 package cytoscape.actions;
 //-------------------------------------------------------------------------
-import java.awt.event.ActionEvent;
-import javax.swing.AbstractAction;
-
-
-
 import cytoscape.Cytoscape;
+import cytoscape.graph.layout.algorithm.util.MutableGraphLayoutRepresentation;
+import cytoscape.graph.layout.impl.SpringEmbeddedLayouter2;
+import cytoscape.process.PercentCompletedCallback;
+import cytoscape.process.RunStoppable;
+import cytoscape.process.Stoppable;
+import cytoscape.process.Task;
+import cytoscape.process.ui.ProgressUI;
+import cytoscape.process.ui.ProgressUIControl;
 import cytoscape.util.CytoscapeAction;
-import cytoscape.util.SwingWorker;
-import cytoscape.util.IndeterminateProgressBar;
-import cytoscape.layout.*;
+import cytoscape.view.CyNetworkView;
+import giny.model.Edge;
+import giny.view.EdgeView;
+import giny.view.NodeView;
+import java.awt.event.ActionEvent;
+import java.util.Iterator;
+import java.util.Hashtable;
+import java.util.Vector;
 
-import cytoscape.*;
-import cytoscape.view.*;
-import java.util.*;
+public class SpringEmbeddedLayoutAction extends CytoscapeAction
+{
 
-
-
-public class SpringEmbeddedLayoutAction extends CytoscapeAction {
-    
-  public SpringEmbeddedLayoutAction () {
+  public SpringEmbeddedLayoutAction ()
+  {
     super("Apply Spring Embedded Layout");
-    setPreferredMenu( "Layout" );
+    setPreferredMenu("Layout");
   }
-    
-  public void actionPerformed ( ActionEvent e ) {
-   
-    
-    final SwingWorker worker = new SwingWorker(){
-        public Object construct(){
-          LayoutAlgorithm layout = new SpringEmbeddedLayouter( Cytoscape.getCurrentNetworkView() );
-          //LayoutAlgorithm layout = new ISOMLayout( Cytoscape.getCurrentNetworkView() );
-          Cytoscape.getCurrentNetworkView().applyLayout( layout );
-          return null;
-        }
-      };
-    worker.start();
 
+  public void actionPerformed (ActionEvent e)
+  {
+    // For the most part this is code copied and pasted from
+    // cytoscape.layout.SpringEmbeddedLayouter, which has to stick around
+    // because it's part of the "public API".
+    CyNetworkView graphView = Cytoscape.getCurrentNetworkView();
+    final int numNodesInTopology = graphView.getNodeViewCount();
+    final double maxLayoutDimension = 400.0d +
+      Math.sqrt(((double) (numNodesInTopology * numNodesInTopology)) * 100.0d);
+
+    // Definiition of nodeTranslation:
+    // nodeindexTranslation[i] defines, for node at index i in our
+    // GraphTopology object, the corresponding NodeView in Giny.  We just
+    // need this to be able to call NodeView.setOffset() at the end - this
+    // is what the legacy layout used to do.
+    final NodeView[] nodeTranslation = new NodeView[numNodesInTopology];
+
+    // Definiton of nodeIndexTranslation:
+    // Both keys and values of this hashtable are java.lang.Integer objects.
+    // There are exactly numNodesInTopology keys in this hashtable.
+    // Key-to-value mappings define index-of-node-in-Giny to
+    // index-of-node-in-GraphTopology mappings.  When I say
+    // "index-of-node-in-Giny", I mean giny.model.Node.getRootGraphIndex().
+    final Hashtable nodeIndexTranslation = new Hashtable();
+
+    Iterator nodeIterator = graphView.getNodeViewsIterator();
+    int nodeIndex = 0;
+    double minX = Double.MAX_VALUE;
+    double maxX = Double.MIN_VALUE;
+    double minY = Double.MAX_VALUE;
+    double maxY = Double.MIN_VALUE;
+
+    while (nodeIterator.hasNext())
+    {
+      NodeView currentNodeView = (NodeView) nodeIterator.next();
+      nodeTranslation[nodeIndex] = currentNodeView;
+      if (nodeIndexTranslation.put
+          (new Integer(currentNodeView.getNode().getRootGraphIndex()),
+           new Integer(nodeIndex++)) != null)
+        throw new IllegalStateException("Giny farted and someone lit a match");
+      minX = Math.min(minX, currentNodeView.getXPosition());
+      maxX = Math.max(maxX, currentNodeView.getXPosition());
+      minY = Math.min(minY, currentNodeView.getYPosition());
+      maxY = Math.max(maxY, currentNodeView.getYPosition());
+    }
+    if (nodeIndex != numNodesInTopology)
+      throw new IllegalStateException("something smells really bad here");
+    Vector directedEdgeVector = new Vector();
+    Vector undirectedEdgeVector = new Vector();
+    Iterator edgeIterator = graphView.getEdgeViewsIterator();
+    while (edgeIterator.hasNext())
+    {
+      Edge currentEdge = ((EdgeView) edgeIterator.next()).getEdge();
+      int ginySourceNodeIndex = currentEdge.getSource().getRootGraphIndex();
+      int ginyTargetNodeIndex = currentEdge.getTarget().getRootGraphIndex();
+      int nativeSourceNodeIndex =
+        ((Integer) nodeIndexTranslation.get
+         (new Integer(ginySourceNodeIndex))).intValue();
+      int nativeTargetNodeIndex =
+        ((Integer) nodeIndexTranslation.get
+         (new Integer(ginyTargetNodeIndex))).intValue();
+      Vector chosenEdgeVector = undirectedEdgeVector;
+      if (currentEdge.isDirected()) chosenEdgeVector = directedEdgeVector;
+      chosenEdgeVector.add(new int[] { nativeSourceNodeIndex,
+                                       nativeTargetNodeIndex });
+    }
+    final int[] directedEdgeSourceNodeIndices =
+      new int[directedEdgeVector.size()];
+    final int[] directedEdgeTargetNodeIndices =
+      new int[directedEdgeVector.size()];
+    for (int i = 0; i < directedEdgeVector.size(); i++) {
+      int[] edge = (int[]) directedEdgeVector.get(i);
+      directedEdgeSourceNodeIndices[i] = edge[0];
+      directedEdgeTargetNodeIndices[i] = edge[1]; }
+    final int[] undirectedEdgeSourceNodeIndices =
+      new int[undirectedEdgeVector.size()];
+    final int[] undirectedEdgeTargetNodeIndices =
+      new int[undirectedEdgeVector.size()];
+    for (int i = 0; i < undirectedEdgeVector.size(); i++) {
+      int[] edge = (int[]) undirectedEdgeVector.get(i);
+      undirectedEdgeSourceNodeIndices[i] = edge[0];
+      undirectedEdgeTargetNodeIndices[i] = edge[1]; }
+    final double[] nodeXPositions = new double[numNodesInTopology];
+    final double[] nodeYPositions = new double[numNodesInTopology];
+    final double xScaleFactor;
+    if (((float) (maxX - minX)) == 0.0) xScaleFactor = 1.0d;
+    else xScaleFactor = maxLayoutDimension / (maxX - minX);
+    final double yScaleFactor;
+    if (((float) (maxY - minY)) == 0.0) yScaleFactor = 1.0d;
+    else yScaleFactor = maxLayoutDimension / (maxY - minY);
+    for (int i = 0; i < numNodesInTopology; i++) {
+      nodeXPositions[i] =
+        Math.min(maxLayoutDimension,
+                 Math.max(0.0d, (nodeTranslation[i].getXPosition() - minX) *
+                          xScaleFactor));
+      nodeYPositions[i] =
+        Math.min(maxLayoutDimension,
+                 Math.max(0.0d, (nodeTranslation[i].getYPosition() - minY) *
+                          yScaleFactor)); }
+    final MutableGraphLayoutRepresentation nativeGraph =
+      new MutableGraphLayoutRepresentation(numNodesInTopology,
+                                           directedEdgeSourceNodeIndices,
+                                           directedEdgeTargetNodeIndices,
+                                           undirectedEdgeSourceNodeIndices,
+                                           undirectedEdgeTargetNodeIndices,
+                                           maxLayoutDimension,
+                                           maxLayoutDimension,
+                                           nodeXPositions,
+                                           nodeYPositions,
+                                           null);
+    SpringEmbeddedLayouter2 layoutAlg =
+      new SpringEmbeddedLayouter2(nativeGraph);
+
+    //////////////////////////////////////////////////////////
+    // BEGIN: The thread and process related code starts here.
+    //////////////////////////////////////////////////////////
+
+    final RunStoppable runStop = new RunStoppable((Task) layoutAlg);
+    final boolean[] stoppd = new boolean[] { false }; // Monitor "Stop" button.
+    final ProgressUIControl progCtrl = ProgressUI.startProgress
+      (Cytoscape.getDesktop(),
+       "Graph Layout",
+       "Laying out graph; please wait...",
+       new Stoppable() {
+         public void stop() {
+           stoppd[0] = true; // Use this to detect a pushed "Stop" button.
+           ((Stoppable) runStop).stop(); } });
+    layoutAlg.setPercentCompletedCallback((PercentCompletedCallback) progCtrl);
+    Runnable runAndDispose = new Runnable() {
+        public void run() {
+          runStop.run(); // It's important that we call run() on the
+                         // RunStoppable and NOT on the LayoutAlgorithm -
+                         // otherwise the RunStoppable won't block on stop().
+          // If our LayoutAlgorithm throws a RuntimeException in its run()
+          // method, the modal dialog will remain forever.  If we don't want
+          // this behavior, we should use 'try' block for run() and
+          // 'finally' block for dispose().
+          progCtrl.dispose(); } };
+    (new Thread(runAndDispose)).start();
+    progCtrl.show(); // This blocks until progCtrl.dispose() is called.
+    if (stoppd[0]) return; // Return without laying out graph if stopped.
+
+    //////////////////////////////////////////////////////
+    // END: The thread and process related code ends here.
+    //////////////////////////////////////////////////////
+
+    for (int i = 0; i < nodeTranslation.length; i++) {
+      nodeTranslation[i].setOffset(nativeGraph.getNodePosition(i).getX(),
+                                   nativeGraph.getNodePosition(i).getY()); }
   }
+
 }
-
