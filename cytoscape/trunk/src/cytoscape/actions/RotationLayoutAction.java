e4a227ac2de3f1f9f8e9e971292f401bd90f904c
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-22 19:34:26 +0000

    Refactoring code into GraphConverter.
    git-svn-id: http://chianti.ucsd.edu/svn@6143 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/RotationLayoutAction.java
+++ cytoscape/trunk/src/cytoscape/actions/RotationLayoutAction.java
@@ -1,19 +1,13 @@
 package cytoscape.actions;
 
 import cytoscape.Cytoscape;
-import cytoscape.graph.layout.algorithm.util.MutableGraphLayoutRepresentation;
+import cytoscape.foo.GraphConverter;
+import cytoscape.graph.layout.algorithm.MutableGraphLayout;
 import cytoscape.graph.layout.impl.RotationLayouter;
 import cytoscape.util.CytoscapeAction;
-import cytoscape.view.CyNetworkView;
-import giny.model.Edge;
-import giny.view.EdgeView;
-import giny.view.NodeView;
 import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Vector;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -34,104 +28,7 @@ public class RotationLayoutAction extends CytoscapeAction
 
   public void actionPerformed(ActionEvent e)
   {
-    CyNetworkView graphView = Cytoscape.getCurrentNetworkView();
-    final int numNodes = graphView.getNodeViewCount();
-
-    // Definition of nodeTranslation:
-    // nodeTranslation[i] defines, for node at index i in our
-    // GraphTopology object, the corresponding NodeView in Giny.
-    final NodeView[] nodeTranslation = new NodeView[numNodes];
-
-    // Definiton of nodeIndexTranslation:
-    // Both keys and values of this hashtable are java.lang.Integer objects.
-    // There are exactly numNodes keys in this hashtable.
-    // Key-to-value mappings define index-of-node-in-Giny to
-    // index-of-node-in-GraphTopology mappings.  When I say
-    // "index-of-node-in-Giny", I mean giny.model.Node.getRootGraphIndex().
-    final Hashtable nodeIndexTranslation = new Hashtable();
-
-    Iterator nodeIterator = graphView.getNodeViewsIterator();
-    int nodeIndex = 0;
-    double minX = Double.MAX_VALUE;
-    double maxX = Double.MIN_VALUE;
-    double minY = Double.MAX_VALUE;
-    double maxY = Double.MIN_VALUE;
-    boolean[] mobility = new boolean[numNodes];
-    final boolean noNodesSelected =
-      (graphView.getSelectedNodeIndices().length == 0);
-
-    while (nodeIterator.hasNext())
-    {
-      NodeView currentNodeView = (NodeView) nodeIterator.next();
-      nodeTranslation[nodeIndex] = currentNodeView;
-      if (nodeIndexTranslation.put
-          (new Integer(currentNodeView.getNode().getRootGraphIndex()),
-           new Integer(nodeIndex)) != null)
-        throw new IllegalStateException("Giny farted and someone lit a match");
-      minX = Math.min(minX, currentNodeView.getXPosition());
-      maxX = Math.max(maxX, currentNodeView.getXPosition());
-      minY = Math.min(minY, currentNodeView.getYPosition());
-      maxY = Math.max(maxY, currentNodeView.getYPosition());
-      if (noNodesSelected) mobility[nodeIndex] = true;
-      else mobility[nodeIndex] = currentNodeView.isSelected();
-      nodeIndex++;
-    }
-    if (nodeIndex != numNodes)
-      throw new IllegalStateException("something smells really bad here");
-    Vector directedEdgeVector = new Vector();
-    Vector undirectedEdgeVector = new Vector();
-    Iterator edgeIterator = graphView.getEdgeViewsIterator();
-    while (edgeIterator.hasNext())
-    {
-      Edge currentEdge = ((EdgeView) edgeIterator.next()).getEdge();
-      int ginySourceNodeIndex = currentEdge.getSource().getRootGraphIndex();
-      int ginyTargetNodeIndex = currentEdge.getTarget().getRootGraphIndex();
-      int nativeSourceNodeIndex =
-        ((Integer) nodeIndexTranslation.get
-         (new Integer(ginySourceNodeIndex))).intValue();
-      int nativeTargetNodeIndex =
-        ((Integer) nodeIndexTranslation.get
-         (new Integer(ginyTargetNodeIndex))).intValue();
-      Vector chosenEdgeVector = undirectedEdgeVector;
-      if (currentEdge.isDirected()) chosenEdgeVector = directedEdgeVector;
-      chosenEdgeVector.add(new int[] { nativeSourceNodeIndex,
-                                       nativeTargetNodeIndex });
-    }
-    final int[] directedEdgeSourceNodeIndices =
-      new int[directedEdgeVector.size()];
-    final int[] directedEdgeTargetNodeIndices =
-      new int[directedEdgeVector.size()];
-    for (int i = 0; i < directedEdgeVector.size(); i++) {
-      int[] edge = (int[]) directedEdgeVector.get(i);
-      directedEdgeSourceNodeIndices[i] = edge[0];
-      directedEdgeTargetNodeIndices[i] = edge[1]; }
-    final int[] undirectedEdgeSourceNodeIndices =
-      new int[undirectedEdgeVector.size()];
-    final int[] undirectedEdgeTargetNodeIndices =
-      new int[undirectedEdgeVector.size()];
-    for (int i = 0; i < undirectedEdgeVector.size(); i++) {
-      int[] edge = (int[]) undirectedEdgeVector.get(i);
-      undirectedEdgeSourceNodeIndices[i] = edge[0];
-      undirectedEdgeTargetNodeIndices[i] = edge[1]; }
-    final double[] nodeXPositions = new double[numNodes];
-    final double[] nodeYPositions = new double[numNodes];
-    for (int i = 0; i < numNodes; i++) {
-      nodeXPositions[i] =
-        nodeTranslation[i].getXPosition() - minX + ((maxX - minX) / 2);
-      nodeYPositions[i] =
-        nodeTranslation[i].getYPosition() - minY + ((maxY - minY) / 2); }
-    final MutableGraphLayoutRepresentation nativeGraph =
-      new MutableGraphLayoutRepresentation(numNodes,
-                                           directedEdgeSourceNodeIndices,
-                                           directedEdgeTargetNodeIndices,
-                                           undirectedEdgeSourceNodeIndices,
-                                           undirectedEdgeTargetNodeIndices,
-                                           (maxX - minX) * 2,
-                                           (maxY - minY) * 2,
-                                           nodeXPositions,
-                                           nodeYPositions,
-                                           mobility);
-
+    final MutableGraphLayout nativeGraph = GraphConverter.getGraphCopy(1.0d);
     Frame cyFrame = Cytoscape.getDesktop();
     JDialog dialog = new JDialog(cyFrame, "Rotate", true);
     dialog.setResizable(false);
@@ -141,7 +38,7 @@ public class RotationLayoutAction extends CytoscapeAction
     panel.add(new JLabel("Degrees of Rotation:"), BorderLayout.CENTER);
     final JSlider slider = new JSlider(0, 360, 0);
     slider.setMajorTickSpacing(90);
-    slider.setMinorTickSpacing(30);
+    slider.setMinorTickSpacing(15);
     slider.setPaintTicks(true);
     slider.setPaintLabels(true);
     slider.addChangeListener(new ChangeListener() {
@@ -160,10 +57,7 @@ public class RotationLayoutAction extends CytoscapeAction
                 cyFrame.location().y);
     dialog.show(); // This blocks until dialog is disposed of.
 
-    for (int i = 0; i < nodeTranslation.length; i++) {
-      nodeTranslation[i].setOffset
-        (nativeGraph.getNodePosition(i, true) + minX - ((maxX - minX) / 2),
-         nativeGraph.getNodePosition(i, false) + minY - ((maxY - minY) / 2)); }
+    GraphConverter.updateCytoscapeLayout(nativeGraph);
   }
 
 }
