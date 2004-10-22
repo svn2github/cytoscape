8c72fe0b4f3c0c95a25810db76fac4c359a06716
rmkelley <rmkelley@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-22 02:53:53 +0000

    Added a class to encapsulate options
    git-svn-id: http://chianti.ucsd.edu/svn@423 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/rmkelley/BetweenPathway/BetweenPathwayOptions.java
@@ -0,0 +1,29 @@
+package ucsd.rmkelley.BetweenPathway;
+import java.io.*;
+import java.util.*;
+import edu.umd.cs.piccolo.activities.*;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import giny.view.NodeView;
+import giny.model.*;
+import cytoscape.plugin.CytoscapePlugin;
+import cytoscape.Cytoscape;
+import cytoscape.CyNetwork;
+import cytoscape.view.CyNetworkView;
+import phoebe.PNodeView;
+import phoebe.PGraphView;
+import cytoscape.data.Semantics;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.*; 
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import cytoscape.layout.*;
+import java.awt.Dimension;
+
+public class BetweenPathwayOptions{
+  public boolean selectedSearch;
+  public CyNetwork physicalNetwork,geneticNetwork;
+  public File physicalScores,geneticScores;
+}
