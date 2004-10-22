135e7160e118300018c54bae4b69df36c0ef2085
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-22 19:41:57 +0000

    Refactoring code into GraphConverter.
    git-svn-id: http://chianti.ucsd.edu/svn@6144 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutAction.java
+++ cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutAction.java
@@ -1,12 +1,8 @@
-//-------------------------------------------------------------------------
-// $Revision$
-// $Date$
-// $Author$
-//-------------------------------------------------------------------------
 package cytoscape.actions;
-//-------------------------------------------------------------------------
+
 import cytoscape.Cytoscape;
-import cytoscape.graph.layout.algorithm.util.MutableGraphLayoutRepresentation;
+import cytoscape.foo.GraphConverter;
+import cytoscape.graph.layout.algorithm.MutableGraphLayout;
 import cytoscape.graph.layout.impl.SpringEmbeddedLayouter2;
 import cytoscape.process.PercentCompletedCallback;
 import cytoscape.process.RunStoppable;
@@ -15,126 +11,20 @@ import cytoscape.process.Task;
 import cytoscape.process.ui.ProgressUI;
 import cytoscape.process.ui.ProgressUIControl;
 import cytoscape.util.CytoscapeAction;
-import cytoscape.view.CyNetworkView;
-import giny.model.Edge;
-import giny.view.EdgeView;
-import giny.view.NodeView;
 import java.awt.event.ActionEvent;
-import java.util.Iterator;
-import java.util.Hashtable;
-import java.util.Vector;
 
 public class SpringEmbeddedLayoutAction extends CytoscapeAction
 {
 
-  public SpringEmbeddedLayoutAction ()
+  public SpringEmbeddedLayoutAction()
   {
     super("Apply Spring Embedded Layout");
     setPreferredMenu("Layout");
   }
 
-  public void actionPerformed (ActionEvent e)
+  public void actionPerformed(ActionEvent e)
   {
-    // For the most part this is code copied and pasted from
-    // cytoscape.layout.SpringEmbeddedLayouter, which has to stick around
-    // because it's part of the "public API".
-    CyNetworkView graphView = Cytoscape.getCurrentNetworkView();
-    final int numNodesInTopology = graphView.getNodeViewCount();
-
-    // Definiition of nodeTranslation:
-    // nodeindexTranslation[i] defines, for node at index i in our
-    // GraphTopology object, the corresponding NodeView in Giny.  We just
-    // need this to be able to call NodeView.setOffset() at the end - this
-    // is what the legacy layout used to do.
-    final NodeView[] nodeTranslation = new NodeView[numNodesInTopology];
-
-    // Definiton of nodeIndexTranslation:
-    // Both keys and values of this hashtable are java.lang.Integer objects.
-    // There are exactly numNodesInTopology keys in this hashtable.
-    // Key-to-value mappings define index-of-node-in-Giny to
-    // index-of-node-in-GraphTopology mappings.  When I say
-    // "index-of-node-in-Giny", I mean giny.model.Node.getRootGraphIndex().
-    final Hashtable nodeIndexTranslation = new Hashtable();
-
-    Iterator nodeIterator = graphView.getNodeViewsIterator();
-    int nodeIndex = 0;
-    double minX = Double.MAX_VALUE;
-    double maxX = Double.MIN_VALUE;
-    double minY = Double.MAX_VALUE;
-    double maxY = Double.MIN_VALUE;
-    boolean[] mobility = new boolean[numNodesInTopology];
-    final boolean noNodesSelected =
-      (graphView.getSelectedNodeIndices().length == 0);
-
-    while (nodeIterator.hasNext())
-    {
-      NodeView currentNodeView = (NodeView) nodeIterator.next();
-      nodeTranslation[nodeIndex] = currentNodeView;
-      if (nodeIndexTranslation.put
-          (new Integer(currentNodeView.getNode().getRootGraphIndex()),
-           new Integer(nodeIndex)) != null)
-        throw new IllegalStateException("Giny farted and someone lit a match");
-      minX = Math.min(minX, currentNodeView.getXPosition());
-      maxX = Math.max(maxX, currentNodeView.getXPosition());
-      minY = Math.min(minY, currentNodeView.getYPosition());
-      maxY = Math.max(maxY, currentNodeView.getYPosition());
-      if (noNodesSelected) mobility[nodeIndex] = true;
-      else mobility[nodeIndex] = currentNodeView.isSelected();
-      nodeIndex++;
-    }
-    if (nodeIndex != numNodesInTopology)
-      throw new IllegalStateException("something smells really bad here");
-    Vector directedEdgeVector = new Vector();
-    Vector undirectedEdgeVector = new Vector();
-    Iterator edgeIterator = graphView.getEdgeViewsIterator();
-    while (edgeIterator.hasNext())
-    {
-      Edge currentEdge = ((EdgeView) edgeIterator.next()).getEdge();
-      int ginySourceNodeIndex = currentEdge.getSource().getRootGraphIndex();
-      int ginyTargetNodeIndex = currentEdge.getTarget().getRootGraphIndex();
-      int nativeSourceNodeIndex =
-        ((Integer) nodeIndexTranslation.get
-         (new Integer(ginySourceNodeIndex))).intValue();
-      int nativeTargetNodeIndex =
-        ((Integer) nodeIndexTranslation.get
-         (new Integer(ginyTargetNodeIndex))).intValue();
-      Vector chosenEdgeVector = undirectedEdgeVector;
-      if (currentEdge.isDirected()) chosenEdgeVector = directedEdgeVector;
-      chosenEdgeVector.add(new int[] { nativeSourceNodeIndex,
-                                       nativeTargetNodeIndex });
-    }
-    final int[] directedEdgeSourceNodeIndices =
-      new int[directedEdgeVector.size()];
-    final int[] directedEdgeTargetNodeIndices =
-      new int[directedEdgeVector.size()];
-    for (int i = 0; i < directedEdgeVector.size(); i++) {
-      int[] edge = (int[]) directedEdgeVector.get(i);
-      directedEdgeSourceNodeIndices[i] = edge[0];
-      directedEdgeTargetNodeIndices[i] = edge[1]; }
-    final int[] undirectedEdgeSourceNodeIndices =
-      new int[undirectedEdgeVector.size()];
-    final int[] undirectedEdgeTargetNodeIndices =
-      new int[undirectedEdgeVector.size()];
-    for (int i = 0; i < undirectedEdgeVector.size(); i++) {
-      int[] edge = (int[]) undirectedEdgeVector.get(i);
-      undirectedEdgeSourceNodeIndices[i] = edge[0];
-      undirectedEdgeTargetNodeIndices[i] = edge[1]; }
-    final double[] nodeXPositions = new double[numNodesInTopology];
-    final double[] nodeYPositions = new double[numNodesInTopology];
-    for (int i = 0; i < numNodesInTopology; i++) {
-      nodeXPositions[i] = nodeTranslation[i].getXPosition() - minX;
-      nodeYPositions[i] = nodeTranslation[i].getYPosition() - minY; }
-    final MutableGraphLayoutRepresentation nativeGraph =
-      new MutableGraphLayoutRepresentation(numNodesInTopology,
-                                           directedEdgeSourceNodeIndices,
-                                           directedEdgeTargetNodeIndices,
-                                           undirectedEdgeSourceNodeIndices,
-                                           undirectedEdgeTargetNodeIndices,
-                                           maxX - minX,
-                                           maxY - minY,
-                                           nodeXPositions,
-                                           nodeYPositions,
-                                           mobility);
+    final MutableGraphLayout nativeGraph = GraphConverter.getGraphCopy(0.0d);
     SpringEmbeddedLayouter2 layoutAlg =
       new SpringEmbeddedLayouter2(nativeGraph);
 
@@ -171,10 +61,7 @@ public class SpringEmbeddedLayoutAction extends CytoscapeAction
     // END: The thread and process related code ends here.
     //////////////////////////////////////////////////////
 
-    for (int i = 0; i < nodeTranslation.length; i++)
-      nodeTranslation[i].setOffset
-        (nativeGraph.getNodePosition(i, true) + minX,
-         nativeGraph.getNodePosition(i, false) + minY);
+    GraphConverter.updateCytoscapeLayout(nativeGraph);
   }
 
 }
