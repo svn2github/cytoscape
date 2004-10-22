a47c38bd7bed37dea847717199ae4d26c967f08c
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-22 17:46:48 +0000

    Now providing a slider UI to rotate graph.  This is an intermittent step - I'm computing coordinates in MutableGraphLayoutRepresentation as we move the slider, but I'm not moving Giny nodes.  I will move Giny nodes in an animated fashion as the slider moves in my next commit.
    git-svn-id: http://chianti.ucsd.edu/svn@6140 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/RotationLayoutAction.java
+++ cytoscape/trunk/src/cytoscape/actions/RotationLayoutAction.java
@@ -8,17 +8,27 @@ import cytoscape.view.CyNetworkView;
 import giny.model.Edge;
 import giny.view.EdgeView;
 import giny.view.NodeView;
+import java.awt.BorderLayout;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Vector;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class RotationLayoutAction extends CytoscapeAction
 {
 
   public RotationLayoutAction()
   {
-    super("Rotate Graph... Just a Little (Hee Hee!)");
+    super("Rotate Graph");
     setPreferredMenu("Layout");
   }
 
@@ -121,7 +131,35 @@ public class RotationLayoutAction extends CytoscapeAction
                                            nodeXPositions,
                                            nodeYPositions,
                                            mobility);
-    RotationLayouter.rotateGraph(nativeGraph, 0.1d);
+
+    Frame cyFrame = Cytoscape.getDesktop();
+    JDialog dialog = new JDialog(cyFrame, "Rotate", true);
+    dialog.setResizable(false);
+    dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.setBorder(new EmptyBorder(20, 20, 20, 20));
+    panel.add(new JLabel("Degrees of Rotation:"), BorderLayout.CENTER);
+    final JSlider slider = new JSlider(0, 360, 0);
+    slider.setMajorTickSpacing(90);
+    slider.setMinorTickSpacing(30);
+    slider.setPaintTicks(true);
+    slider.setPaintLabels(true);
+    slider.addChangeListener(new ChangeListener() {
+        private int prevValue = slider.getValue();
+        public void stateChanged(ChangeEvent e) {
+          double radians = ((double) (slider.getValue() - prevValue)) *
+            2.0d * Math.PI / 360.0d;
+          RotationLayouter.rotateGraph(nativeGraph, radians);
+          prevValue = slider.getValue(); } });
+    panel.add(slider, BorderLayout.SOUTH);
+    dialog.getContentPane().add(panel, BorderLayout.CENTER);
+    dialog.pack();
+    dialog.move((cyFrame.size().width - dialog.size().width) / 2 +
+                cyFrame.location().x,
+                (cyFrame.size().height - dialog.size().height) / 5 +
+                cyFrame.location().y);
+    dialog.show(); // This blocks until dialog is disposed of.
+
     for (int i = 0; i < nodeTranslation.length; i++) {
       nodeTranslation[i].setOffset
         (nativeGraph.getNodePosition(i, true) + minX - ((maxX - minX) / 2),
