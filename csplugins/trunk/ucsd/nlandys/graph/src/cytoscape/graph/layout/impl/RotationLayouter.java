6269c51b9a82e5d3c4f559c4d10fd67ae50c2363
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-22 00:02:50 +0000

    RotationLayouter now compiles.  It has not been tested yet.
    git-svn-id: http://chianti.ucsd.edu/svn@419 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/impl/RotationLayouter.java
+++ csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/impl/RotationLayouter.java
@@ -14,8 +14,8 @@ public final class RotationLayouter
   // No constructor.
   private RotationLayouter() {}
 
-  public static rotateGraph(MutableGraphLayout graph,
-                            double radians)
+  public static void rotateGraph(MutableGraphLayout graph,
+                                 double radians)
   {
     double xMin = Double.MAX_VALUE; double xMax = Double.MIN_VALUE;
     double yMin = Double.MAX_VALUE; double yMax = Double.MIN_VALUE;
@@ -33,16 +33,16 @@ public final class RotationLayouter
     final Translation3D toOrig =
       new Translation3D(-xRectCenter, -yRectCenter, 0.0d);
     final AxisRotation3D rotation =
-      new AxisRotation3D(AxisRotation.Z_AXIS, radians);
+      new AxisRotation3D(AxisRotation3D.Z_AXIS, radians);
     final Translation3D fromOrig =
       new Translation3D(xRectCenter, yRectCenter, 0.0d);
     AffineTransform3D tentativeTransform =
       toOrig.concatenatePost(rotation.concatenatePost(fromOrig));
-    Point3D[] boudaryPoints = new Point3D[] {
-      new Point3D(xMin, yMin, 0.0d);
-      new Point3D(xMin, yMax, 0.0d);
-      new Point3D(xMax, yMax, 0.0d);
-      new Point3D(xMax, yMin, 0.0d); };
+    Point3D[] boundaryPoints = new Point3D[] {
+      new Point3D(xMin, yMin, 0.0d),
+      new Point3D(xMin, yMax, 0.0d),
+      new Point3D(xMax, yMax, 0.0d),
+      new Point3D(xMax, yMin, 0.0d) };
     xMin = Double.MAX_VALUE; xMax = Double.MIN_VALUE;
     yMin = Double.MAX_VALUE; yMax = Double.MIN_VALUE;
     for (int i = 0; i < boundaryPoints.length; i++) {
@@ -61,19 +61,19 @@ public final class RotationLayouter
                            (yRectCenter - graph.getMaxHeight()) /
                            (yRectCenter - yMax));
     final Scale3D scale = new Scale3D(scaleFactor, scaleFactor, 1.0d);
-    final AfiineTransform3D realTransform =
+    final AffineTransform3D realTransform =
       toOrig.concatenatePost(rotation.concatenatePost(scale.concatenatePost
                                                       (fromOrig)));
     double[] pointBuff = new double[3];
     for (int i = 0; i < graph.getNumNodes(); i++) {
-      if (!isMovableNode()) continue;
+      if (!graph.isMovableNode(i)) continue;
       Point2D nodePosition = graph.getNodePosition(i);
       pointBuff[0] = nodePosition.getX(); pointBuff[1] = nodePosition.getY();
       realTransform.transformArr(pointBuff);
       graph.setNodePosition
         (i,
          Math.max(0.0d, Math.min(graph.getMaxWidth(), pointBuff[0])),
-         Math.max(0,0d, Math.min(graph.getMaxHeight(), pointBuff[1]))); }
+         Math.max(0.0d, Math.min(graph.getMaxHeight(), pointBuff[1]))); }
   }
 
 }
