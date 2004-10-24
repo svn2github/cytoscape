d7995ee04922ebde73c23e4af0109dff8a1eed3c
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-24 02:12:50 +0000

    Fleshing out some Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@439 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/NodeNeighbors.java
+++ csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/NodeNeighbors.java
@@ -2,7 +2,13 @@ package cytoscape.graph;
 
 /**
  * This interface represents neighboring node information for a given
- * graph.
+ * graph.<p>
+ * Assuming that a binary relation on the set of nodes in a graph defining
+ * &quot;is a neighbor of&quot; is provided, it is possible to compute
+ * the information returned by this interface.  Nonetheless, a programmer
+ * may choose to access a legacy graph implementation's node adjacency list
+ * information instead of computing it inside of this &quot;wrapper&quot;
+ * graph framework - that's exactly what this interface is for.
  */
 public interface NodeNeighbors
 {
