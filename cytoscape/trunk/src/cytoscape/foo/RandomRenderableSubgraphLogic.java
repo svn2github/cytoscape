056be5d33a0fdd1ee293b9c8126900628260e0a1
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-27 22:51:28 +0000

    Adding some serious foo-zy logic code.
    git-svn-id: http://chianti.ucsd.edu/svn@6162 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/trunk/src/cytoscape/foo/RandomRenderableSubgraphLogic.java
@@ -0,0 +1,41 @@
+package cytoscape.foo;
+
+import cytoscape.CyNetwork;
+import cytoscape.Cytoscape;
+import cytoscape.CytoscapeInit;
+import cytoscape.util.CyNetworkNaming;
+import cytoscape.view.CyNetworkView;
+import java.util.Random;
+import java.util.Vector;
+
+public class RandomRenderableSubgraphLogic
+{
+
+  public static void justDoIt()
+  {
+    final int threshold = CytoscapeInit.getViewThreshold();
+    final CyNetwork currentNetwork = Cytoscape.getCurrentNetwork();
+    final int currentNodeCount = currentNetwork.getNodeCount();
+    if (currentNodeCount < threshold)
+      throw new IllegalStateException
+        ("misusage of this class - only use it if the number of nodes " +
+         "in the current network is equal to or exceeds the threshold");
+    Vector inx = new Vector();
+    for (int i = 0; i < currentNodeCount; i++)
+      inx.addElement(new Integer(i));
+    Random r = new Random();
+    while (inx.size() >= threshold)
+      inx.removeElementAt(r.nextInt(inx.size()));
+    final int[] nodeInx = new int[inx.size()];
+    for (int i = 0; i < nodeInx.length; i++)
+      nodeInx[i] = ((Integer) inx.elementAt(i)).intValue();
+    CyNetwork newNetwork = Cytoscape.createNetwork
+      (nodeInx, currentNetwork.getConnectingEdgeIndicesArray(nodeInx),
+       CyNetworkNaming.getSuggestedSubnetworkTitle(currentNetwork),
+       currentNetwork);
+    newNetwork.setExpressionData(currentNetwork.getExpressionData());
+    CyNetworkView newView =
+      Cytoscape.getNetworkView(newNetwork.getIdentifier());
+  }
+
+}
