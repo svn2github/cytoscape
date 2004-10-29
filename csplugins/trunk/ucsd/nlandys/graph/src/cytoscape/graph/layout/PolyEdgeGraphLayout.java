9c3c571ff125d1f0160cfc0ac71b8c6cb41e4d7d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-29 21:37:50 +0000

    Wee.  The Javadoc defines this object quite well now.
    git-svn-id: http://chianti.ucsd.edu/svn@466 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/PolyEdgeGraphLayout.java
+++ csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/PolyEdgeGraphLayout.java
@@ -4,22 +4,18 @@ import cytoscape.graph.IndexIterator;
 
 /**
  * This class extends <code>GraphLayout</code> to offer the possibility
- * of defining poly-line edges (as opposed to just straight-line edges).
-   * Defines a &quot;poly-line&quot; for edge at index
-   * <code>edgeIndex</code>.<p>
-   * The poly-line is defined in the following manner.
-   * If we define a sequence of points {P[0], P[1], ... P[N+1]} where<ul>
-   * <li>P[0] is the source node of edge at index
-   *     <code>edgeIndex</code>,</li>
-   * <li>P[N+1] is the target node of edge at index
-   *     <code>edgeIndex</code>, and</li>
-   * <li>for i not equal to 0 or N+1, P[i] is the edge anchor
-   *     point whose index is the i-th element in the return value of
-   *     <code>getAnchorIndices(edgeIndex)</code>,</li></ul>
-   * then the poly-line for edge at index <code>edgeIndex</code> is defined to
-   * be the union of line segments {S[0], S[1], ... S[N]} where
-   * each S[i] is the straight-line segment starting
-   * at P[i] and ending at P[i+1].
+ * of defining poly-line edges (as opposed to just straight-line edges).<p>
+ * For a given edge E in this graph, the definition of a poly-line
+ * for E is as follows.
+ * If we define a sequence of points {P[0], P[1], ... P[N+1]} where<ul>
+ * <li>P[0] is the source node of edge E,</li>
+ * <li>P[N+1] is the target node of edge E, and</li>
+ * <li>for i not equal to 0 or N+1, P[i] is edge E's anchor
+ *     point at index i-1,</li></ul>
+ * then the poly-line for edge E is defined to be the union of line segments
+ * {S[0], S[1], ... S[N]} where
+ * each S[i] is the straight-line segment starting
+ * at P[i] and ending at P[i+1].
  **/
 public interface PolyEdgeGraphLayout extends GraphLayout
 {
@@ -28,9 +24,9 @@ public interface PolyEdgeGraphLayout extends GraphLayout
    * Returns the number of anchor points belonging to edge at index
    * <code>edgeIndex</code>.  In other methods of this
    * interface an anchor point is referenced by the index of the edge to which
-   * it belongs along with the anchor point's index within that edge.
-   * Indices of anchor points within an edge at index <code>edgeIndex</code>
-   * start at <code>0</code> and end at
+   * the anchor point belongs along with the anchor point's index within that
+   * edge.  Indices of anchor points within an edge at index
+   * <code>edgeIndex</code> start at <code>0</code> and end at
    * <nobr><code>getNumAnchors(edgeIndex) - 1</code></nobr>, inclusive.
    *
    * @return the number of edge anchor points belonging to edge at index
@@ -45,10 +41,10 @@ public interface PolyEdgeGraphLayout extends GraphLayout
    *
    * @param edgeIndex the index of the edge to which the anchor point whose
    *   position we're seeking belongs.
-   * @param anchorIndex the index within an edge of the anchor point whose
-   *   position we're seeking.
-   * @param xPosition if <code>true</code>, return X position; if
-   *   <code>false</code>, return Y position.
+   * @param anchorIndex if edge E has index <code>edgeIndex</code>,
+   *   the index of anchor point, within E, whose position we're seeking.
+   * @param xPosition if <code>true</code>, return X position of anchor point;
+   *   if <code>false</code>, return Y position of anchor point.
    * @return the X or Y position of anchor point with index
    *   <code>anchorIndex</code> within edge at index <code>edgeIndex</code>.
    * @exception IndexOutOfBoundsException if <code>edgeIndex</code> is not in
