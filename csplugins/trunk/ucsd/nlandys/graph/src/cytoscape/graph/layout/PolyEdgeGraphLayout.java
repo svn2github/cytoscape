0156c5daefb5470618009ee4fd0db9091a59ec0f
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-29 21:14:53 +0000

    Experimenting with a better API.
    git-svn-id: http://chianti.ucsd.edu/svn@465 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/PolyEdgeGraphLayout.java
+++ csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/PolyEdgeGraphLayout.java
@@ -5,11 +5,6 @@ import cytoscape.graph.IndexIterator;
 /**
  * This class extends <code>GraphLayout</code> to offer the possibility
  * of defining poly-line edges (as opposed to just straight-line edges).
- **/
-public interface PolyEdgeGraphLayout extends GraphLayout
-{
-
-  /**
    * Defines a &quot;poly-line&quot; for edge at index
    * <code>edgeIndex</code>.<p>
    * The poly-line is defined in the following manner.
@@ -25,27 +20,45 @@ public interface PolyEdgeGraphLayout extends GraphLayout
    * be the union of line segments {S[0], S[1], ... S[N]} where
    * each S[i] is the straight-line segment starting
    * at P[i] and ending at P[i+1].
+ **/
+public interface PolyEdgeGraphLayout extends GraphLayout
+{
+
+  /**
+   * Returns the number of anchor points belonging to edge at index
+   * <code>edgeIndex</code>.  In other methods of this
+   * interface an anchor point is referenced by the index of the edge to which
+   * it belongs along with the anchor point's index within that edge.
+   * Indices of anchor points within an edge at index <code>edgeIndex</code>
+   * start at <code>0</code> and end at
+   * <nobr><code>getNumAnchors(edgeIndex) - 1</code></nobr>, inclusive.
    *
-   * @return a non-<code>null</code> iteration of indices of edge anchor points
-   *   belonging to edge at index <code>edgeIndex</code>; the order of the
-   *   iteration is significant - see above description.
+   * @return the number of edge anchor points belonging to edge at index
+   *   <code>edgeIndex</code>.
    * @exception IndexOutOfBoundsException if <code>edgeIndex</code> is not
    *   in the interval <nobr><code>[0, getNumEdges() - 1]</code></nobr>.
    **/
-  public IndexIterator getAnchorIndices(int edgeIndex);
+  public int getNumAnchors(int edgeIndex);
 
   /**
    * Returns the X or Y position of an edge anchor point.
    *
-   * @param anchorIndex the index of edge anchor point whose position we're
-   *   seeking.
+   * @param edgeIndex the index of the edge to which the anchor point whose
+   *   position we're seeking belongs.
+   * @param anchorIndex the index within an edge of the anchor point whose
+   *   position we're seeking.
    * @param xPosition if <code>true</code>, return X position; if
    *   <code>false</code>, return Y position.
-   * @return the X or Y position of edge anchor point at index
-   *   <code>anchorIndex</code>.
+   * @return the X or Y position of anchor point with index
+   *   <code>anchorIndex</code> within edge at index <code>edgeIndex</code>.
+   * @exception IndexOutOfBoundsException if <code>edgeIndex</code> is not in
+   *   the interval <nobr><code>[0, getNumEdges() - 1]</code></nobr>.
    * @exception IndexOutOfBoundsException if <code>anchorIndex</code> is not
-   *   an index of any edge anchor point on this graph.
+   *   in the interval
+   *   <nobr><code>[0, getNumAnchors(edgeIndex) - 1]</code></nobr>.
    **/
-  public double getAnchorPosition(int anchorIndex, boolean xPosition);
+  public double getAnchorPosition(int edgeIndex,
+                                  int anchorIndex,
+                                  boolean xPosition);
 
 }
