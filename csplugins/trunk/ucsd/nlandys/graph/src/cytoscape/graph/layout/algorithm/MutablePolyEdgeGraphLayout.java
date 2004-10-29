53b1f5f32a7d4e686dd3e8a943c2c8ae389bee41
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-29 23:09:16 +0000

    This class got a new API.
    git-svn-id: http://chianti.ucsd.edu/svn@468 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/algorithm/MutablePolyEdgeGraphLayout.java
+++ csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/algorithm/MutablePolyEdgeGraphLayout.java
@@ -11,79 +11,90 @@ public interface MutablePolyEdgeGraphLayout
 {
 
   /**
-   * Deletes an edge anchor point.
-   * If this anchor point belongs to edge E, then deletion of this anchor point
-   * will have no effect on the order of E's remaining anchor points.
+   * Deletes an edge anchor point.<p>
+   * The deletion of an anchor point is accomplished such that the ordering of
+   * remaining anchor points stays the same.  An anchor point [belonging
+   * to edge with index <code>edgeIndex</code>] with index greater than
+   * <code>anchorIndex</code> will be assigned a new index equal to its
+   * previous index minus one; an anchor point with index less than
+   * <code>anchorIndex</code> will keep its index.
    *
-   * @param anchorIndex the index of anchor point we're
-   *   trying to delete.
+   * @param edgeIndex the index of the edge to which the anchor point to be
+   *   deleted belongs.
+   * @param anchorIndex if edge E has index <code>edgeIndex</code>,
+   *   the index of anchor point, within E, which we're trying to delete.
+   * @exception IndexOutOfBoundsException if <code>edgeIndex</code> is not in
+   *   the interval <nobr><code>[0, getNumEdges() - 1]</code></nobr>.
    * @exception IndexOutOfBoundsException if <code>anchorIndex</code> is not
-   *   an index of any edge anchor point on this graph.
-   * @exception UnsupportedOperationException if the edge anchor point at index
-   *   <code>anchorIndex</code> belongs to an edge whose source and target
-   *   nodes are both non-movable.
+   *   in the interval
+   *   <nobr><code>[0, getNumAnchors(edgeIndex) - 1]</code></nobr>.
+   * @exception UnsupportedOperationException if edge at index
+   *   <code>edgeIndex</code> has source and target nodes that are both
+   *   non-movable.
    **/
-  public void deleteAnchor(int anchorIndex);
+  public void deleteAnchor(int edgeIndex, int anchorIndex);
 
   /**
-   * Creates a new anchor on edge with index <code>edgeIndex</code>.<p>
-   * <!-- If we define a sequence of points {P[0], P[1], ... P[N+1]} where:<ul>
-   * <li>P[0] is the source node of edge at index
-   *     <code>edgeIndex</code>,</li>
-   * <li>P[N+1] is the target node of edge at index
-   *     <code>edgeIndex</code>, and</li>
-   * <li>for i not equal to 0 or N+1, P[i] is the edge anchor
-   *     point whose index is the i-th element in the return value of
-   *     <code>getAnchorIndices(edgeIndex)</code>,</li></ul>
-   * then -->
-   * This method adds an anchor point to edge at index
-   * <code>edgeIndex</code> such that:<ul>
-   * <li>the order of existing edge anchor points is not changed and</li>
-   * <li>the new anchor point becomes such that its index will be the
-   *     <nobr><code>(position+1)</code>-th</nobr>
-   *     element in the return value of a subsequent call to
-   *     <code>getAnchorIndices(edgeIndex)</code>.</li></ul><p>
-   * Returns the index of the newly created anchor.
+   * Creates a new edge anchor point.<p>
+   * The creation of an anchor point is accomplished such that the ordering
+   * of existing anchor points stays the same.  An existing anchor point
+   * [belonging to edge with index <code>edgeIndex</code>] with index greater
+   * than or equal to <code>anchorIndex</code> will be assigned a new index
+   * equal to its previous index plus one; an existing anchor point with index
+   * less than <code>anchorIndex</code> will keep its index.<p>
+   * A new anchor point P's X,Y position is the midpoint along the segment
+   * whose end points are P's neighbors in the edge poly-line definition;
+   * X,Y positions of existing anchor points and nodes are unchanged.
    *
    * @param edgeIndex new anchor point will be created on edge with
    *   index <code>edgeIndex</code>.
-   * @param position defines the position, among other existing anchor
-   *   points on this edge, into which to insert this new anchor point; see
-   *   above definition of <code>position</code>.
-   * @return the anchor index of the newly created anchor point.
-   * @exception IndexOutOfBoundsException if <code>position</code>
-   *   is less than zero or if <code>position</code> is greater than
-   *   the number of existing anchor points on edge at index
-   *   <code>edgeIndex</code>.
+   * @param anchorIndex new anchor point will have index
+   *   <code>anchorIndex</code> within edge at index <code>edgeIndex</code>.
    * @exception IndexOutOfBoundsException if <code>edgeIndex</code> is not
    *   in the interval <nobr><code>[0, getNumEdges() - 1]</code></nobr>.
+   * @exception IndexOutOfBoundsException if <code>anchorIndex</code> is not
+   *   in the interval <nobr><code>[0, getNumAnchors(edgeIndex)]</code></nobr>.
    * @exception UnsupportedOperationException if edge at index
    *   <code>edgeIndex</code> has source and target
    *   nodes that are both non-movable.
    **/
-  public int createAnchor(int edgeIndex, int position);
+  public void createAnchor(int edgeIndex, int anchorIndex);
 
   /**
-   * Sets the X,Y position of an edge anchor point.
+   * Sets the X,Y position of an edge anchor point.<p>
+   * X, Y values set by this method shall be reflected in the return values
+   * of <code>getAnchorPosition()</code> - that is, if we call
+   * <blockquote><code>setAnchorPosition(eInx, aInx, x, y)</code></blockquote>
+   * then the subsequent expressions
+   * <blockquote>
+   * <nobr><code>x == getAnchorPosition(eInx, aInx, true)</code></nobr><br />
+   * <nobr><code>y == getAnchorPosition(eInx, aInx, false)</code></nobr>
+   * </blockquote>
+   * both evaluate to <code>true</code>.
    *
-   * @param anchorIndex the index of anchor point whose location we're
-   *   trying to specify.
-   * @param xPosition the desired X position of anchor point at index
-   *   <code>anchorIndex</code>.
-   * @param yPosition the desired Y position of anchor point at index
-   *   <code>anchorIndex</code>.
+   * @param edgeIndex the index of the edge to which the anchor point to be
+   *   positioned belongs.
+   * @param anchorIndex if edge E has index <code>edgeIndex</code>,
+   *   the index of anchor point, within E, which we're trying to position.
+   * @param xPosition the desired X position of specified edge anchor point.
+   * @param yPosition the desired Y position of specified edge anchor point.
+   * @exception IndexOutOfBoundsException if <code>edgeIndex</code> is not in
+   *   the interval <nobr><code>[0, getNumEdges() - 1]</code></nobr>.
    * @exception IndexOutOfBoundsException if <code>anchorIndex</code> is not
-   *   an index of any edge anchor point on this graph.
+   *   in the interval
+   *   <nobr><code>[0, getNumAnchors(edgeIndex) - 1]</code></nobr>.
    * @exception IllegalArgumentException if
    *   <nobr><code>xPosition < 0.0</code></nobr>, if
    *   <nobr><code>xPosition > getMaxWidth()</code></nobr>, if
    *   <nobr><code>yPosition < 0.0</code></nobr>, or if
    *   <nobr><code>yPosition > getMaxHeight()</code></nobr>.
-   * @exception UnsupportedOperationException if the edge anchor point at index
-   *   <code>anchorIndex</code> belongs to an edge whose source and target
-   *   nodes are both non-movable.
+   * @exception UnsupportedOperationException if edge at index
+   *   <code>edgeIndex</code> has source and target
+   *   nodes that are both non-movable.
    **/
-  public void setAnchorPosition(int anchorIndex, double xPosition,
+  public void setAnchorPosition(int edgeIndex,
+                                int anchorIndex,
+                                double xPosition,
                                 double yPosition);
 
 }
