1e876d2979afccd28abee7a65d57cbab8837f7bc
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-10-30 00:25:27 +0000

    Rotations now work with poly-edge graph drawings.
    git-svn-id: http://chianti.ucsd.edu/svn@470 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/impl/RotationLayouter.java
+++ csplugins/trunk/ucsd/nlandys/graph/src/cytoscape/graph/layout/impl/RotationLayouter.java
@@ -3,28 +3,50 @@ package cytoscape.graph.layout.impl;
 import com.nerius.math.xform.AffineTransform3D;
 import com.nerius.math.xform.AxisRotation3D;
 import com.nerius.math.xform.Translation3D;
-import cytoscape.graph.layout.algorithm.MutableGraphLayout;
+import cytoscape.graph.layout.algorithm.MutablePolyEdgeGraphLayout;
 
 public final class RotationLayouter
 {
 
-  private final MutableGraphLayout m_graph;
+  private final MutablePolyEdgeGraphLayout m_graph;
   private final Translation3D m_translationToOrig;
   private final Translation3D m_translationFromOrig;
 
   /**
+   * This operation does not affect edge anchor points which belong to edges
+   * containing at least one non-movable node.
+   *
    * @exception IllegalStateException
-   *   if the minimum bounding rectangle containing all movable nodes
+   *   if the minimum bounding rectangle containing all movable nodes and
+   *   respective edge anchor points
    *   is not fully free to rotate around its center while staying within
-   *   allowable node positions for specified
+   *   allowable area for specified
    *   <code>MutableGraphLayout</code>.
    **/
-  public RotationLayouter(MutableGraphLayout graph)
+  public RotationLayouter(MutablePolyEdgeGraphLayout graph)
   {
     m_graph = graph;
     double xMin = Double.MAX_VALUE; double xMax = Double.MIN_VALUE;
     double yMin = Double.MAX_VALUE; double yMax = Double.MIN_VALUE;
-    for (int i = 0; i < m_graph.getNumNodes(); i++)
+    final int numEdges = m_graph.getNumEdges();
+    for (int i = 0; i < numEdges; i++)
+    {
+      if (!(m_graph.isMovableNode(m_graph.getEdgeNodeIndex(i, true)) &&
+            m_graph.isMovableNode(m_graph.getEdgeNodeIndex(i, false))))
+        continue;
+      final int numAnchors = m_graph.getNumAnchors(i);
+      for (int j = 0; j < numAnchors; j++)
+      {
+        double anchXPosition = m_graph.getAnchorPosition(i, j, true);
+        double anchYPosition = m_graph.getAnchorPosition(i, j, false);
+        xMin = Math.min(xMin, anchXPosition);
+        xMax = Math.max(xMax, anchXPosition);
+        yMin = Math.min(yMin, anchYPosition);
+        yMax = Math.max(yMax, anchYPosition);
+      }
+    }
+    final int numNodes = m_graph.getNumNodes();
+    for (int i = 0; i < numNodes; i++)
     {
       if (!m_graph.isMovableNode(i)) continue;
       double nodeXPosition = m_graph.getNodePosition(i, true);
@@ -34,7 +56,7 @@ public final class RotationLayouter
       yMin = Math.min(yMin, nodeYPosition);
       yMax = Math.max(yMax, nodeYPosition);
     }
-    if (xMax < 0) // No nodes are movable.
+    if (xMax < 0) // Nothing is movable.
     {
       m_translationToOrig = null;
       m_translationFromOrig = null;
@@ -52,8 +74,8 @@ public final class RotationLayouter
           yRectCenter - hypotenuse < 0.0d ||
           yRectCenter + hypotenuse > m_graph.getMaxHeight())
         throw new IllegalStateException
-          ("minimum bounding rectangle of movable nodes not free to rotate " +
-           "within MutableGraphLayout boundaries");
+          ("minimum bounding rectangle of movable nodes and edge anchors " +
+           "not free to rotate within MutableGraphLayout boundaries");
       m_translationToOrig =
         new Translation3D(-xRectCenter, -yRectCenter, 0.0d);
       m_translationFromOrig =
@@ -69,13 +91,29 @@ public final class RotationLayouter
     final AffineTransform3D xform = m_translationToOrig.concatenatePost
       ((new AxisRotation3D(AxisRotation3D.Z_AXIS, radians)).concatenatePost
        (m_translationFromOrig));
-    for (int i = 0; i < m_graph.getNumNodes(); i++)
+    final int numNodes = m_graph.getNumNodes();
+    for (int i = 0; i < numNodes; i++)
     {
       if (!m_graph.isMovableNode(i)) continue;
       m_pointBuff[0] = m_graph.getNodePosition(i, true);
       m_pointBuff[1] = m_graph.getNodePosition(i, false);
       xform.transformArr(m_pointBuff);
       m_graph.setNodePosition(i, m_pointBuff[0], m_pointBuff[1]);
+    }
+    final int numEdges = m_graph.getNumEdges();
+    for (int i = 0; i < numEdges; i++)
+    {
+      if (!(m_graph.isMovableNode(m_graph.getEdgeNodeIndex(i, true)) &&
+            m_graph.isMovableNode(m_graph.getEdgeNodeIndex(i, false))))
+        continue;
+      final int numAnchors = m_graph.getNumAnchors(i);
+      for (int j = 0; j < numAnchors; j++)
+      {
+        m_pointBuff[0] = m_graph.getAnchorPosition(i, j, true);
+        m_pointBuff[1] = m_graph.getAnchorPosition(i, j, false);
+        xform.transformArr(m_pointBuff);
+        m_graph.setAnchorPosition(i, j, m_pointBuff[0], m_pointBuff[1]);
+      }
     }
   }
 
