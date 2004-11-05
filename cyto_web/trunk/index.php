37e2c42dd519501ce2262ea07adfb0366b557eac
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-05 14:57:58 +0000

    Validated HTML with BBEdit and fixed errors
    git-svn-id: http://chianti.ucsd.edu/svn@4316 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/index.php
+++ cyto_web/trunk/index.php
@@ -1,171 +1,237 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
 <? include "config.php"; ?>
-<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<title> Cytoscape: Analyzing and Visualizing Biological Network Data </title>
-	<link href="css/cytoscape.css" media="screen" type="text/css" rel="stylesheet">
-	<link href="images/cyto.ico" rel="shortcut icon">
-	<meta content="Cytoscape, visualization, interaction network,
+<html>
+	<head>
+		<title>
+			Cytoscape: Analyzing and Visualizing Biological Network Data 
+		</title>
+		<link href="css/cytoscape.css" media="screen" type="text/css" rel="stylesheet">
+		<link href="images/cyto.ico" rel="shortcut icon">
+		<meta content="Cytoscape, visualization, interaction network,
 		software, genetic, gene, expression, protein interaction, graph,
 		bioinformatics, computational biology,
 		Whitehead Institute, Institute for Systems Biology, microarray analysis,
 		clustering, pathways, integration, algorithm, simulated annealing,
 		gene regulation" name="keywords">
-</head>
-<body>
-<table summary="" cellspacing="0" cellpadding="0" border="0" id="feature">
-	<tbody>
-		<tr>
-			<td width="10">
-				&nbsp;
-			</td>
-			<td width="200" valign="top">
-				<div id="about">
-					<h1> Cytoscape </h1> Cytoscape is an open source bioinformatics software platform for <b> <i> visualizing </i> </b> molecular interaction networks and <b> <i> integrating </i> </b> these interactions with gene expression profiles and other state data.
-					<br>
-					<a href="features.php"> Read more &raquo; </a>
-			</div>
-		</td>
-		<td width="440" valign="bottom" align="right">
-			<div id="article">
-				<h2> <a href="download.php?file=cyto2"> Download Cytoscape 2.0! </a> </h2>
-				<p>
-					New!&nbsp; Cytoscape 2.0 is now available.
-					<br>
-					<a href="download.php?file=cyto2"> Download &raquo; </a>
-				<br>
-		</p>
-		<p>
-			<br>
-			<br>
-	</p>
-</div>
-</td>
-</tr>
-</tbody>
-</table>
+	</head>
+	<body>
+		<table summary="" cellspacing="0" cellpadding="0" border="0" id="feature">
+			<tbody>
+				<tr>
+					<td width="10">
+						&nbsp; 
+					</td>
+					<td width="200" valign="top">
+						<div id="about">
+							<h1>
+								Cytoscape 
+							</h1>
+							Cytoscape is an open source bioinformatics software platform for 
+							<b>
+								<i>
+									visualizing 
+								</i>
+							</b>
+							molecular interaction networks and 
+							<b>
+								<i>
+									integrating 
+								</i>
+							</b>
+							these interactions with gene expression profiles and other state data. 
+							<br>
+							<a href="features.php">
+								Read more &raquo; 
+							</a>
+						</div>
+					</td>
+					<td width="440" valign="bottom" align="right">
+						<div id="article">
+							<h2>
+								<a href="download.php?file=cyto2">
+									Download Cytoscape 2.0! 
+								</a>
+							</h2>
+							<p>
+								New!&nbsp; Cytoscape 2.0 is now available. 
+								<br>
+								<a href="download.php?file=cyto2">
+									Download &raquo; 
+								</a>
+								<br>
+							</p>
+							<p>
+								<br>
+								<br>
+							</p>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
 <? include "nav.php"; ?>
 <? include "detailed_nav.php"; ?>
-<TABLE WIDTH=100% BORDER=0 CELLPADDING=5 CELLSPACING=1>
-<TR VALIGN=TOP>
-<TD WIDTH=65%>
-<div id="contents">
-	<div id="content">
-		<div id="indent">
-			<div class="item">
-				<BR>
-				 <big><b><I>New!</I>&nbsp;&nbsp;Announcing Cytoscape 2.0 </b></big>
-				<table width="100%">
-					<tr valign="top">
-						<td width="47%" valign="top">
-							The Cytoscape Team is pleased to release Cytoscape 2.0.
-							<BR><BR>New Features include:
-							<ul>
-								<li>
-									100% Open Source Implementation.
-								</li>
-								<li>
-									Bird's Eye View for Viewing Large Networks.
-								</li>
-								<li>
-									Improved Performance for Viewing Large Networks.
-								</li>
-								<li>
-									Improved PlugIn Framework and PlugIn API.
-								</li>
-								<li>
-									Numerous Bug Fixes.
-								</li>
-							</ul>
-							Try out Cytoscape 2.0, and let us know what you think.
-							<ul>
-								<li>
-									<A HREF="download.php?file=cyto2">Download Cytoscape 2.0</A>.
-								</li>
-							</ul>
-							If you find a bug, please log it to the <a href="http://www.cbio.mskcc.org/cytoscape/bugs/main_page.php">Cytoscape Bug Tracker.</a>
-						</td>
-						<td valign="top">
-							<img src="images/screen_thumb1.gif" align="left" />
-						</td>
-					</tr>
-				</table>
-			</div>
-			<P>
-			<div class="item">
-				 <big><b>Genome Research article on Cytoscape </b></big>
-					<br>
-				</h2>
-				<p>
-					Genome Research has published a journal article on Cytoscape <a href="http://www.genome.org/cgi/content/full/13/11/2498"> [Abstract] </a> <a href="http://www.genome.org/cgi/reprint/13/11/2498"> [PDF] </a> <a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids=14597658&amp;dopt=Abstract"> [PubMed entry]</a>.
-				</p>
-				<p>
-					<br>
-				</p>
-			</div>
-			<P>
-			<div class="item">
- <big><b>Research using Cytoscape</b></big>
- &nbsp;&nbsp;
-[<a href="pubs.php">View All Publications</a>]
-<br>
-<br>
-<p>
-	<b>Genome Biology, July 2004</b>
-	<br>
-		Bonneau, Richard, Baliga Nitin S, Deutsch, Eric W, Shannon Paul and Hood, Leroy
-	<br>
-		Comprehensive de novo structure prediction in a systems-biology context for the archaea Halobacterium sp. NRC-1
-	<br>
-		Genome Biology 2004, 5:R52
-	<br>
-		[<a href="http://genomebiology.com/2004/5/8/r52/abstract">Abstract</a>] [<a href="http://genomebiology.com/2004/5/8/r52">Full Text</a>] [<a href="http://genomebiology.com/content/pdf/gb-2004-5-8-r52.pdf">PDF</a>]
-<br>
-&nbsp;
-<p>
-<b> Genome Research, June 2004 (cover article) </b>
-<br>
-	Baliga, NS, Bjork, SJ, Bonneau, R, Pan, M, Iloanusi, C, Kottemann, MCH, Hood, L, and DiRuggiero, J.
-<br>
-	Systems level insights into the stress ersponds to UV radiation in the halophilic archaeon Halobacterium NRC-1.
-<br>
-	Genome Res. 2004 14: 1025-35.
-<br>
-	[<a href="http://www.genome.org/cgi/content/abstract/14/6/1025" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/6/1025.pdf" class="Textlink">PDF</a>][<a href="http://www.genome.org/cgi/content/full/1993504/DC1" class="Textlink">Supplemental Research Data</a>]
-<p>
-<br>
-<p>
-	<b> Genome Research, March 2004 </b>
-	<br>
-		Prinz S, Avila-Campillo I, Aldridge C, Srinivasan A, Dimitrov K, Siegel AF, and Galitski T.
-	<br>
-		Control of Yeast Filamentous-Form Growth by Modules in an Integrated Molecular Network.
-	<br>
-		Genome Res. 2004 14: 380-390.
-	<br>
-		[<a href="http://www.genome.org/cgi/content/abstract/14/3/380" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/3/380.pdf" class="Textlink">PDF</a>] [<a href="http://www.genome.org/cgi/content/full/14/3/380/DC1" class="Textlink">Supplemental Research Data</a>]
-</p>
-<p>
-<br>
-<p>
-<b>Science, Feb 2004</b>
-<br>
-	Tong AH, et al., Global mapping of the yeast genetic interaction network.
-<br>
-	Science. 2004 Feb 6;303(5659):808-13.
-<br>
-	[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=pubmed&amp;dopt=Abstract&amp;list_uids=14764870">Pub Med Entry</a>] [<a href="http://www.sciencemag.org/cgi/content/full/303/5659/808">Full Text</a>]
-<br>
-&nbsp;
-<p><B>Note:</B>  If you have a publication which makes use of Cytoscape, please let us know by sending an email to the
-<A HREF="http://groups-beta.google.com/group/cytoscape-discuss">cytoscape-discuss</a> mailing list.
-</div>
-</td>
-</div>
-</div>
-</div>
-<td>
-<?php
+		<table width="100%" border="0" cellpadding="5" cellspacing="1">
+			<tr valign="top">
+				<td width="65%">
+					<div id="contents">
+						<div id="content">
+							<div id="indent">
+								<div class="item">
+									<br>
+									<big>
+										<b>
+											<i>
+												New!</i>&nbsp;&nbsp;Announcing Cytoscape 2.0 
+										</b>
+									</big>
+									<table width="100%">
+										<tr valign="top">
+											<td width="47%" valign="top">
+												The Cytoscape Team is pleased to release Cytoscape 2.0. 
+												<br>
+												<br>
+												New Features include: 
+												<ul>
+													<li>
+														100% Open Source Implementation. 
+													</li>
+													<li>
+														Bird's Eye View for Viewing Large Networks. 
+													</li>
+													<li>
+														Improved Performance for Viewing Large Networks. 
+													</li>
+													<li>
+														Improved PlugIn Framework and PlugIn API. 
+													</li>
+													<li>
+														Numerous Bug Fixes. 
+													</li>
+												</ul>
+												Try out Cytoscape 2.0, and let us know what you think. 
+												<ul>
+													<li>
+														<a href="download.php?file=cyto2">
+															Download Cytoscape 2.0</a>. 
+													</li>
+												</ul>
+												If you find a bug, please log it to the 
+												<a href="http://www.cbio.mskcc.org/cytoscape/bugs/main_page.php">
+													Cytoscape Bug Tracker.
+												</a>
+											</td>
+											<td valign="top">
+												<img src="images/screen_thumb1.gif" align="left" alt="Cytoscape Screenshot" />
+											</td>
+										</tr>
+									</table>
+								</div>
+								<p>
+								<div class="item">
+									<big>
+										<b>
+											Genome Research article on Cytoscape 
+										</b>
+									</big>
+									<br>
+									<p>
+										Genome Research has published a journal article on Cytoscape 
+										<a href="http://www.genome.org/cgi/content/full/13/11/2498">
+											[Abstract] 
+										</a>
+										<a href="http://www.genome.org/cgi/reprint/13/11/2498">
+											[PDF] 
+										</a>
+										<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids=14597658&amp;dopt=Abstract">
+											[PubMed entry]</a>. 
+									</p>
+									<p>
+										<br>
+									</p>
+								</div>
+								<p>
+								<div class="item">
+									<big>
+										<b>
+											Research using Cytoscape</b>
+									</big>
+									&nbsp;&nbsp; [<a href="pubs.php">View All Publications</a>] 
+									<p>
+									<br>
+									<br>
+									<p>
+										<b>
+											Genome Biology, July 2004
+										</b>
+										<br>
+										Bonneau, Richard, Baliga Nitin S, Deutsch, Eric W, Shannon Paul and Hood, Leroy 
+										<br>
+										Comprehensive de novo structure prediction in a systems-biology context for the archaea Halobacterium sp. NRC-1 
+										<br>
+										Genome Biology 2004, 5:R52 
+										<br>
+										[<a href="http://genomebiology.com/2004/5/8/r52/abstract">Abstract</a>] [<a href="http://genomebiology.com/2004/5/8/r52">Full Text</a>] [<a href="http://genomebiology.com/content/pdf/gb-2004-5-8-r52.pdf">PDF</a>] 
+										<br>
+										&nbsp; 
+									<p>
+										<b>
+											Genome Research, June 2004 (cover article) 
+										</b>
+										<br>
+										Baliga, NS, Bjork, SJ, Bonneau, R, Pan, M, Iloanusi, C, Kottemann, MCH, Hood, L, and DiRuggiero, J. 
+										<br>
+										Systems level insights into the stress ersponds to UV radiation in the halophilic archaeon Halobacterium NRC-1. 
+										<br>
+										Genome Res. 2004 14: 1025-35. 
+										<br>
+										[<a href="http://www.genome.org/cgi/content/abstract/14/6/1025" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/6/1025.pdf" class="Textlink">PDF</a>][<a href="http://www.genome.org/cgi/content/full/1993504/DC1" class="Textlink">Supplemental Research Data</a>] 
+									<p>
+										<br>
+									<p>
+										<b>
+											Genome Research, March 2004 
+										</b>
+										<br>
+										Prinz S, Avila-Campillo I, Aldridge C, Srinivasan A, Dimitrov K, Siegel AF, and Galitski T. 
+										<br>
+										Control of Yeast Filamentous-Form Growth by Modules in an Integrated Molecular Network. 
+										<br>
+										Genome Res. 2004 14: 380-390. 
+										<br>
+										[<a href="http://www.genome.org/cgi/content/abstract/14/3/380" class="Textlink">Abstract</a>][<a href="http://www.genome.org/cgi/reprint/14/3/380.pdf" class="Textlink">PDF</a>] [<a href="http://www.genome.org/cgi/content/full/14/3/380/DC1" class="Textlink">Supplemental Research Data</a>] 
+									</p>
+									<p>
+										<br>
+									<p>
+										<b>
+											Science, Feb 2004
+										</b>
+										<br>
+										Tong AH, et al., Global mapping of the yeast genetic interaction network. 
+										<br>
+										Science. 2004 Feb 6;303(5659):808-13. 
+										<br>
+										[<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=pubmed&amp;dopt=Abstract&amp;list_uids=14764870">Pub Med Entry</a>] [<a href="http://www.sciencemag.org/cgi/content/full/303/5659/808">Full Text</a>] 
+										<br>
+										&nbsp; 
+									<p>
+										<b>
+											Note:
+										</b>
+										If you have a publication which makes use of Cytoscape, please let us know by sending an email to the 
+										<a href="http://groups-beta.google.com/group/cytoscape-discuss">
+											cytoscape-discuss
+										</a>
+										mailing list. 
+								</div>
+							</div>
+						</div>
+					</div>
+				</td>
+				<td>
+<?
 	if ($news_option == "atom") {
 		include "feed.php";
 		include "community_box.php";
@@ -173,15 +239,12 @@
 		include "news.php";
 		include "community_box.php";
 	}
-	echo "<P>";
+	echo "<P>"; 
 	include "collab.php";
-?>
-</td>
-</tr>
-</table>
-
-</div>
+?> 
+				</td>
+			</tr>
+		</table>
 <? include "footer.php"; ?>
-</div>
-</body>
+	</body>
 </html>
