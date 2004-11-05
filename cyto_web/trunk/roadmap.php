f8232e84fedbd92ec5e9517ce4cea2191c972820
anderson <anderson@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-05 23:35:45 +0000

    Added page for roadmap and project plan.
    git-svn-id: http://chianti.ucsd.edu/svn@4318 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/roadmap.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+		<title>
+			Cytoscape Roadmap
+		</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="css/cytoscape.css" />
+		<link rel="shortcut icon" href="images/cyto.ico" />
+	</head>
+	<body bgcolor="#FFFFFF">
+		<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
+			<tr>
+				<td width="10">
+					&nbsp;
+				</td>
+				<td valign="center">
+					<h1>
+						Cytoscape Roadmap
+					</h1>
+				</td>
+			</tr>
+		</table>
+<? include "nav.php"; ?>
+<div id="contents">
+	<div id="content">
+		<div id="indent">
+			<H3>Planning</H2>
+			In September 2004, the Cytoscape annual technical
+			retreat and user meeting was held in NYC. At this
+			meeting, features and priorities
+			for Cytoscape development were identified and
+			discussed. From that meeting, planning and further
+			analysis of goals and priorities has been conducted and
+			a roadmap and project plan are now available.
+			<H3>Cytoscape Roadmap</H2>
+			The features discussed at the NYC retreat are being
+			developed and made available in two releases,
+			currently scheduled for early February 2005 and 
+			June 2005 availability.<P>
+			<IMG SRC="roadmap/roadmap.gif"></IMG>.
+			<P>The <B>Cytoscape 2.1</B> release, slated for early
+			February 2005 availability, is a performance and
+			scalability
+			focused release, improving performance, usability, and
+			the capability of the system to handle large networks.
+			Several important new features are also included in 2.1.
+			<P><B>Cytoscape 2.2</B> is a functionality focused
+			release, implementing a majority of the new features
+			identified at the retreat. Performance improvements
+			are also planned.  Cytoscape 2.2 is expected to be
+			released in June 2005.
+			<H3>Cytoscape Project Plan</H2>
+			The Cytoscape Project Plan is regularly updated, 
+			and is available in PDF and GIF formats.
+			<P>The current Project Plan, last updated 5 Nov 2004,
+			is available below:
+			<UL>
+			  <LI><A HREF="roadmap/ProjPlan.pdf">PDF</A>
+			  <LI><A HREF="roadmap/ProjPlan.gif">GIF</A>
+			</UL>
+		</div> 
+<? include "footer.php"; ?>
+	</body>
+</html>
