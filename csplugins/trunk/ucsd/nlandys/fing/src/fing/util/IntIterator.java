675070ef6f998a6d7b0281c7d209db17b9658941
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-21 19:02:47 +0000

    Adding Javadoc defining behavior.
    git-svn-id: http://chianti.ucsd.edu/svn@524 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/util/IntIterator.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/util/IntIterator.java
@@ -3,8 +3,16 @@ package fing.util;
 public interface IntIterator
 {
 
+  /**
+   * Returns the number of times that successive calls to nextInt() can
+   * be made successfully.
+   */
   public int numRemaining();
 
+  /**
+   * If numRemaining() returns a non-positive quantity before
+   * nextInt() is called, the behavior of nextInt() is undefined.
+   */
   public int nextInt();
 
 }
