12b17f19e42213c1ba23deab5024899edf1b6c75
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-22 00:06:51 +0000

    Starting to test fing.util.MinIntHeap.
    git-svn-id: http://chianti.ucsd.edu/svn@536 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/util/test/MinIntHeapTest.java
@@ -0,0 +1,13 @@
+package fing.util.test;
+
+import fing.util.MinIntHeap;
+
+public class MinIntHeapTest
+{
+
+  public static void main(String[] args)
+  {
+    MinIntHeap heap = new MinIntHeap();
+  }
+
+}
