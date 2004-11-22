8c29b292571bede8280797516902cebcecc48b11
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-22 00:19:03 +0000

    Implementing some of the test case.
    git-svn-id: http://chianti.ucsd.edu/svn@537 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/util/test/MinIntHeapTest.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/util/test/MinIntHeapTest.java
@@ -1,5 +1,6 @@
 package fing.util.test;
 
+import fing.util.IntIterator;
 import fing.util.MinIntHeap;
 
 public class MinIntHeapTest
@@ -8,6 +9,24 @@ public class MinIntHeapTest
   public static void main(String[] args)
   {
     MinIntHeap heap = new MinIntHeap();
+    System.out.println
+      ("Instantiated new MinIntHeap.");
+    final int[] arr = new int[] { 5, 8, 1, 3, 8, 3, 0, 9, 1, 2, 7, 3 };
+    System.out.println
+      ("Defined my int[] to be: { 5, 8, 1, 3, 8, 3, 0, 9, 1, 2, 7, 3 }.");
+    for (int i = 0; i < arr.length; i++) heap.toss(arr[i]);
+    System.out.println
+      ("Tossed all elements of array onto heap.");
+    IntIterator iter = heap.orderedElements(true);
+    System.out.println
+      ("Got an IntIterator by calling orderedElements(true) on heap.");
+    System.out.println
+      ("The iterator's numRemaining() method returns " +
+       iter.numRemaining() + ".");
+    System.out.print("The iterator looks like this: { ");
+    while (iter.numRemaining() > 1)
+      System.out.print(iter.nextInt() + ", ");
+    System.out.println(iter.nextInt() + " }.");
   }
 
 }
