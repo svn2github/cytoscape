f91932cec92c8053b0d51d31262f521dad5ef292
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-23 03:05:27 +0000

    Starting to implement arrayCopy() function.  Also changing the interpreted order of bits within each 32 bit int.  Also prettying up some Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@555 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/util/BitArray.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/util/BitArray.java
@@ -1,8 +1,8 @@
 package fing.util;
 
 /**
- * This class represents an array of boolean values, consuming a
- * factor of 32 less memory than an array of native Java boolean values.
+ * This class represents a fixed-size array of boolean values.  A BitArray
+ * of length N consumes 32 times less memory than a boolean[] of length N.
  */
 public final class BitArray
 {
@@ -31,28 +31,34 @@ public final class BitArray
   }
 
   /**
-   * Returns the value of this array at the specified index.
+   * Returns the value of this array at the specified index.<p>
    * If index is less than zero, results of this operation are undefined.
    * If index is greater than or equal to length(), results of this operation
    * are undefined.
+   * @see #length()
    */
   public final boolean get(int index)
   {
     int majorIndex = index / 32;
     int minorIndex = index % 32;
-    int mask = 0x00000001 << minorIndex;
+    int mask = 0x80000000 >>> minorIndex;
     return (m_arr[majorIndex] & mask) != 0;
   }
 
   /**
    * Manipulates the array such that a successive call to get(index)
-   * would return the boolean value specified.
+   * would return the boolean value specified.<p>
+   * If index is less than zero, results ofthis operation are undefined.
+   * If index is greater than or equal to length(), results of this
+   * operation are undefined.
+   * @see #get(int)
+   * @see #length()
    */
   public final void put(int index, boolean value)
   {
     int majorIndex = index / 32;
     int minorIndex = index % 32;
-    int mask = 0x00000001 << minorIndex;
+    int mask = 0x80000000 >>> minorIndex;
     if (value) m_arr[majorIndex] |= mask;
     else m_arr[majorIndex] &= ~mask;
   }
@@ -60,11 +66,59 @@ public final class BitArray
   /**
    * Returns the length of this array, as specified at
    * construction time.  Valid indices for elements in this array are
-   * between zero and length() minus one, inclusive.
+   * between zero and length()-1, inclusive.
    */
   public final int length()
   {
     return m_length;
+  }
+
+  /**
+   * Copies data from one array to another.<p>
+   * IMPORTANT NOTE: This method will perform extremely well when
+   * the difference between srcPos and destPos is a multiple of 32.
+   * Otherwise, this method performs only well.
+   * @param src the source array from which to copy information.
+   * @param srcPos a contiguous block of data is copied from the source array
+   *   starting at this index in the source array.
+   * @param dest the array into which to copy data.
+   * @param destPos the contiguous block of data is copied into the
+   *   destination array starting at this index in the destination array.
+   * @param length the number of contiguous elements to copy from the
+   *   source array into the destination array.
+   * @exception IndexOutOfBoundsException if copying would cause access of data
+   *   outside array bounds.
+   */
+  public static final void arrayCopy(BitArray src, int srcPos,
+                                     BitArray dest, int destPos, int length)
+  {
+    if (length < 0 || srcPos < 0 || destPos < 0 ||
+        srcPos + length > src.length() || destPos + length > dest.length())
+      throw new IndexOutOfBoundsException
+        ("combination of input parameters cause access outside array bounds");
+    if ((srcPos - destPos) % 32 != 0) // Even this case can be optimized
+      for (int i = 0; i < length; i++) // by using shifts on integers;
+        dest.put(destPos + i, src.get(srcPos + i)); // this is not optimized.
+    else { // We can directly copy arrays of integers.
+      int majorSrcBegin = srcPos / 32;
+      int minorSrcBegin = srcPos % 32;
+      int majorDestBegin = destPos / 32;
+      int minorDestBegin = destPos % 32;
+      int majorSrcEnd = (srcPos + length) / 32;
+      int minorSrcEnd = (srcPos + length) % 32;
+      int majorDestEnd = (destPos + length) / 32;
+      int minorDestEnd = (destPos + length) % 32;
+      if (minorSrcBegin != 0) {
+        int mask = ~(0xffffffff >>> minorSrcBegin);
+        if (majorSrcBegin == majorSrcEnd) mask |= (0xffffffff >>> minorSrcEnd);
+        int bitsToCopy = src.m_arr[majorSrcBegin] & ~mask;
+        dest.m_arr[majorDestBegin] &= mask;
+        dest.m_arr[majorDestBegin] |= bitsToCopy;
+        majorSrcBegin++;
+        minorSrcBegin = 0;
+        majorDestBegin++;
+        minorDestBegin = 0; }
+    }
   }
 
 }
