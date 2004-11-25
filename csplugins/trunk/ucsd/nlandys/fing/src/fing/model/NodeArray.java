7c01752ab528a6eb6cd0a18bc623e69291ed98c9
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-25 02:19:20 +0000

    Now handling cases where array length needs to be of length close to Integer.MAX_VALUE.  I dunno, maybe Trey will want graphs with 2^31 nodes on a supercomputer!
    git-svn-id: http://chianti.ucsd.edu/svn@605 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/NodeArray.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/NodeArray.java
@@ -1,5 +1,6 @@
 package fing.model;
 
+// Valid indices: [0, Integer.MAX_VALUE - 1].
 class NodeArray
 {
 
@@ -12,23 +13,35 @@ class NodeArray
     m_nodeArr = new FNode[INITIAL_CAPACITY];
   }
 
+  // Understand that this method will not increase the size of the underlying
+  // array, no matter what.
   FNode getNodeAtIndex(int index)
   {
     try { return m_nodeArr[index]; }
-    catch (ArrayIndexOutOfBoundsException e) { return null; }
+    catch (ArrayIndexOutOfBoundsException e) {
+      if (index < 0 || index == Integer.MAX_VALUE) { throw e; }
+      return null; }
   }
 
+  // Understand that this method will potentially increase the size of the
+  // underlying array, but only if two conditions hold:
+  //   1. node is not null and
+  //   2. index is greater than or equal to the length of the array.
   void setNodeAtIndex(FNode node, int index)
   {
     try { m_nodeArr[index] = node; }
     catch (ArrayIndexOutOfBoundsException e)
     {
-      if (index < 0) { throw e; }
+      if (index < 0 || index == Integer.MAX_VALUE) { throw e; }
+      else if (node == null) { return; }
       else {
-        final int newArrSize = Math.max(m_nodeArr.length * 2 + 1,
-                                        (index + 1) + INITIAL_CAPACITY);
-        FNode[] newArr = new FNode[(newArrSize > 0) ? newArrSize :
-                                   Integer.MAX_VALUE];
+        // We need to make sure that we at least double the array length
+        // in order to ensure amortized constant time hits for ths function.
+        final int newArrSize = (int)
+          Math.min((long) Integer.MAX_VALUE,
+                   Math.max(((long) m_nodeArr.length) * 2l + 1l,
+                            ((long) index) + 1l + (long) INITIAL_CAPACITY));
+        FNode[] newArr = new FNode[newArrSize];
         System.arraycopy(m_nodeArr, 0, newArr, 0, m_nodeArr.length);
         m_nodeArr = newArr;
         m_nodeArr[index] = node; }
