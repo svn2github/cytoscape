ccbcde1bded3f64c095314eaad599ac8448ade6d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-27 21:43:23 +0000

    These are my initial thoughts on the internal Edge/Node data structures.
    git-svn-id: http://chianti.ucsd.edu/svn@664 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/Edge.java
@@ -0,0 +1,14 @@
+package cytoscape.graph.dynamic.util;
+
+// Package visible.
+class Edge
+{
+
+  Edge m_nextAdjEdge;
+  boolean m_directed;
+  int m_sourceNode;
+  int m_targetNode;
+
+  Edge() { }
+
+}
---
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/Node.java
@@ -0,0 +1,10 @@
+package cytoscape.graph.dynamic.util;
+
+// Package visible.
+class Node
+{
+
+  Edge m_firstOutEdge;
+  Edge m_firstInEdge;
+
+}
