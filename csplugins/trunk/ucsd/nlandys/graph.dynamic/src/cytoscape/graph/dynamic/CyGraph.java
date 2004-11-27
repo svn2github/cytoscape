69fb7d2d4d628241e75fec759ca7a429a53bb4dc
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-27 18:39:08 +0000

    Adding CyGraph.
    git-svn-id: http://chianti.ucsd.edu/svn@652 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/CyGraph.java
@@ -0,0 +1,25 @@
+package cytoscape.graph.dynamic;
+
+import cytoscape.util.intr.IntEnumerator;
+
+// Edges and nodes are non-negative.
+interface CyGraph
+{
+  IntEnumerator nodes();
+  IntEnumerator edges();
+  boolean removeNode(int node);
+  int createNode();
+  boolean removeEdge(int edge);
+  // Returns -1 if nodes specified are invalid.
+  int createEdge(int sourceNode, int targetNode, boolean directed);
+  boolean containsNode(int node);
+  boolean containsEdge(int edge);
+  // Throws IllegalArgumentException.
+  IntEnumerator adjacentEdges(int node, boolean undirected,
+                              boolean incoming, boolean outgoing);
+  // Returns -1 if edge specified is invalid.
+  int sourceNode(int edge);
+  int targetNode(int edge);
+  // Throws IllegalArgumentException.
+  boolean isDirectedEdge(int edge);
+}
