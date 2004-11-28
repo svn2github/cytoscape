6b6f8b9173fc4c961caabef6c3bd6c8450cee25d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-28 02:05:39 +0000

    EdgeDepot is now a linked list instead of an array.
    git-svn-id: http://chianti.ucsd.edu/svn@677 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/EdgeDepot.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/EdgeDepot.java
@@ -3,42 +3,28 @@ package cytoscape.graph.dynamic.util;
 final class EdgeDepot
 {
 
-  private final static int INITIAL_CAPACITY = 11; // Must be non-negative.
-
-  private Edge[] m_edgeStack;
-  private int m_size;
+  private final Edge m_head;
 
   EdgeDepot()
   {
-    m_edgeStack = new Edge[INITIAL_CAPACITY];
-    m_size = 0;
+    m_head = new Edge();
   }
 
   // Gimme an edge, darnit!
   // Don't forget to initialize the edge's member variables!
   Edge getEdge()
   {
-    if (m_size == 0) { return new Edge(); }
-    else { return m_edgeStack[--m_size]; }
+    final Edge returnThis = m_head.nextOutEdge;
+    if (returnThis == null) { return new Edge(); }
+    m_head.nextOutEdge = returnThis.nextOutEdge;
+    return returnThis;
   }
 
   // Deinitialize the object's members yourself if you need or want to.
-  // Don't recycle null; this error condition is not checked for.
   void recycleEdge(Edge edge)
   {
-    try { m_edgeStack[m_size++] = edge; }
-    catch (ArrayIndexOutOfBoundsException e) {
-      m_size--;
-      final int newArrSize = (int)
-        Math.min((long) Integer.MAX_VALUE,
-                 ((long) m_edgeStack.length) * 2l + 1l);
-      if (newArrSize == m_edgeStack.length)
-        throw new IllegalStateException
-          ("unable to allocate large enough array");
-      Edge[] newArr = new Edge[newArrSize];
-      System.arraycopy(m_edgeStack, 0, newArr, 0, m_edgeStack.length);
-      m_edgeStack = newArr;
-      m_edgeStack[m_size++] = edge; }
+    edge.nextOutEdge = m_head.nextOutEdge;
+    m_head.nextOutEdge = edge;
   }
 
 }
