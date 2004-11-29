58a0bfb2ce8e28813e4e6c513390e3ccb5d2e737
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-29 23:03:23 +0000

    Chaning spelling in Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@690 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/IntHash.java
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/IntHash.java
@@ -10,7 +10,7 @@ package cytoscape.util.intr;
  * to elements being added (the underlying size of the hashtable is invisible
  * to the programmer).  In the underlying implementation, this hashtable never
  * decreases in size.  As a hashtable increases in size,
- * it takes at most 4 times as much memory as it would take
+ * it takes at most four times as much memory as it would take
  * to store the hashtable's elements in a perfectly-sized array.
  * Underlying size expansions are implemented such that the operation of
  * expanding in size is amortized over the contstant time complexity needed to
