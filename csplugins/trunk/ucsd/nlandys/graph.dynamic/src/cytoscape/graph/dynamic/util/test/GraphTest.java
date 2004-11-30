7bce6607c24924a6735a0d222873bc0e421a6dfa
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-11-30 17:41:27 +0000

    Starting a test package.
    git-svn-id: http://chianti.ucsd.edu/svn@703 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/GraphTest.java
@@ -0,0 +1,14 @@
+package cytoscape.graph.dynamic.util.test;
+
+import cytoscape.graph.dynamic.DynamicGraph;
+import cytoscape.graph.dynamic.util.DynamicGraphRepresentation;
+
+public class GraphTest
+{
+
+  public static void main(String[] args)
+  {
+    final DynamicGraph graph = new DynamicGraphRepresentation();
+  }
+
+}
