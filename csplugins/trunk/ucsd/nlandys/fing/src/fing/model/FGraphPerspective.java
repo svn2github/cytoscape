759cbc98eb7ae19a83b14512f6996bb5e92e7a3d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-12-02 17:02:13 +0000

    Adding add and remove listener methods.
    git-svn-id: http://chianti.ucsd.edu/svn@733 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/FGraphPerspective.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/FGraphPerspective.java
@@ -1,11 +1,32 @@
 package fing.model;
 
 import giny.model.GraphPerspective;
+import giny.model.GraphPerspectiveChangeListener;
 
 // Package visible class.
 class FGraphPerspective //implements GraphPerspective
 {
 
+  public void addGraphPerspectiveChangeListener
+    (GraphPerspectiveChangeListener listener)
+  {
+    lis = GraphPerspectiveChangeListenerChain.add(lis, listener);
+  }
 
+  public void removeGraphPerspectiveChangeListener
+    (GraphPerspectiveChangeListener listener)
+  {
+    lis = GraphPerspectiveChangeListenerChain.remove(lis, listener);
+  }
+
+  private GraphPerspectiveChangeListener lis;
+
+  // Package visible constructor.
+  FGraphPerspective()
+  {
+    lis = new GraphPerspectiveChangeListener() {
+        public void graphPerspectiveChanged(GraphPerspectiveChangeEvent evt)
+        { } }; // Dummy listener so that member variable lis never null.
+  }
 
 }
