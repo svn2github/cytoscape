a84b90b11f4034a25407c50a432e645b2dc6cd2a
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-12-09 22:02:31 +0000

    Starting a test package.
    git-svn-id: http://chianti.ucsd.edu/svn@857 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/GCTest.java
@@ -0,0 +1,15 @@
+package fing.model.test;
+
+import fing.model.FingRootGraphFactory;
+import giny.model.GraphPerspective;
+import giny.model.RootGraph;
+
+public class GCTest
+{
+
+  public static void main(String[] args)
+  {
+    final RootGraph root = FingRootGraphFactory.instantiateRootGraph();
+  }
+
+}
