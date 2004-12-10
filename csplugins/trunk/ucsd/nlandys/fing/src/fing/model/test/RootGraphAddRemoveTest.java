7ed4d0053c3b09b44f0e345370e39c46f64b2f81
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-12-10 20:34:31 +0000

    Adding a basic add/remove node/edge test for RootGraph implementations. Luna is failing miserably.
    git-svn-id: http://chianti.ucsd.edu/svn@881 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/RootGraphAddRemoveTest.java
@@ -0,0 +1,62 @@
+package fing.model.test;
+
+import fing.model.FingRootGraphFactory;
+import giny.model.Edge;
+import giny.model.GraphPerspective;
+import giny.model.Node;
+import giny.model.RootGraph;
+
+import java.util.Iterator;
+
+public final class RootGraphAddRemoveTest
+{
+
+  // No constructor.
+  private RootGraphAddRemoveTest() { }
+
+  public static final void main(String[] args)
+    throws ClassNotFoundException, InstantiationException,
+           IllegalAccessException
+  {
+    final RootGraph root = getRootGraph(args);
+
+    int[] nodeInx;
+    final int[] edgeInx = new int[10000];
+    final int[] nodeNums = new int[] { 2000, 1999, 2002 };
+    final int iterations = 10000;
+    for (int foo = 0; foo < iterations; foo++)
+    {
+      boolean print = true;
+      if (!(foo % 10 == 0)) print = false;
+      if (print) System.out.println("at add/remove iteration " + (foo + 1) + 
+                                    " of " + iterations);
+      final int numNodes = nodeNums[foo % nodeNums.length];
+      if (print) System.out.println("creating " + numNodes + " nodes");
+      nodeInx = root.createNodes(numNodes);
+      if (print) System.out.println("creating " + edgeInx.length + " edges");
+      for (int i = 0; i < edgeInx.length; i++)
+        edgeInx[i] = root.createEdge(nodeInx[i % nodeInx.length],
+                                     nodeInx[(i * 3) % nodeInx.length]);
+      if (print) System.out.println("in RootGraph: " + root.getNodeCount() +
+                                    " nodes and " + root.getEdgeCount() +
+                                    " edges");
+      if (print) System.out.println();
+      if (print) System.out.println("removing all edges from RootGraph");
+      root.removeEdges(edgeInx);
+      if (print) System.out.println("removing all nodes from RootGraph");
+      root.removeNodes(nodeInx);
+      if (print) System.out.println("in RootGraph: " + root.getNodeCount() +
+                                    " nodes and " + root.getEdgeCount() +
+                                    " edges");
+      if (print) System.out.println();
+    }
+  }
+
+  private static final RootGraph getRootGraph(String[] mainArgs)
+    throws ClassNotFoundException, InstantiationException,
+           IllegalAccessException {
+    if (mainArgs.length > 0 && mainArgs[0].equalsIgnoreCase("luna"))
+      return (RootGraph) Class.forName("luna.LunaRootGraph").newInstance();
+    else return FingRootGraphFactory.instantiateRootGraph(); }
+
+}
