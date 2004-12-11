889955af58a0841f00fe559fa1023a91539fc9a7
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-12-11 21:57:31 +0000

    Being more conservative with try/catch statements because they are a huge performance hit if exceptions are thrown.
    git-svn-id: http://chianti.ucsd.edu/svn@914 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/EdgeArray.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/EdgeArray.java
@@ -19,10 +19,10 @@ class EdgeArray
   // Integer.MAX_VALUE.
   FEdge getEdgeAtIndex(int index)
   {
-    try { return m_edgeArr[index]; }
-    catch (ArrayIndexOutOfBoundsException e) {
-      if (index < 0 || index == Integer.MAX_VALUE) { throw e; }
-      return null; }
+    // Do pre-checking because try/catch with thrown exception causes huge
+    // performance hit.
+    if (index >= m_edgeArr.length && index != Integer.MAX_VALUE) return null;
+    return m_edgeArr[index]; // Exception if Integer.MAX_VALUE or negative.
   }
 
   // Understand that this method will potentially increase the size of the
@@ -33,22 +33,22 @@ class EdgeArray
   // Integer.MAX_VALUE.
   void setEdgeAtIndex(FEdge edge, int index)
   {
+    // Do pre-checking because try/catch with thrown exception causes huge
+    // performance hit.
+    if (index >= m_edgeArr.length && edge == null &&
+        index != Integer.MAX_VALUE) return;
     try { m_edgeArr[index] = edge; }
     catch (ArrayIndexOutOfBoundsException e)
     {
       if (index < 0 || index == Integer.MAX_VALUE) { throw e; }
-      else if (edge == null) { return; }
-      else {
-        // We need to make sure that we at least double the array length
-        // in order to ensure amortized constant time hits for ths function.
-        final int newArrSize = (int)
-          Math.min((long) Integer.MAX_VALUE,
-                   Math.max(((long) m_edgeArr.length) * 2l + 1l,
-                            ((long) index) + 1l + (long) INITIAL_CAPACITY));
-        FEdge[] newArr = new FEdge[newArrSize];
-        System.arraycopy(m_edgeArr, 0, newArr, 0, m_edgeArr.length);
-        m_edgeArr = newArr;
-        m_edgeArr[index] = edge; }
+      final int newArrSize = (int)
+        Math.min((long) Integer.MAX_VALUE,
+                 Math.max(((long) m_edgeArr.length) * 2l + 1l,
+                          ((long) index) + 1l + (long) INITIAL_CAPACITY));
+      FEdge[] newArr = new FEdge[newArrSize];
+      System.arraycopy(m_edgeArr, 0, newArr, 0, m_edgeArr.length);
+      m_edgeArr = newArr;
+      m_edgeArr[index] = edge;
     }
   }
 
--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/NodeArray.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/NodeArray.java
@@ -19,10 +19,10 @@ class NodeArray
   // Integer.MAX_VALUE.
   FNode getNodeAtIndex(int index)
   {
-    try { return m_nodeArr[index]; }
-    catch (ArrayIndexOutOfBoundsException e) {
-      if (index < 0 || index == Integer.MAX_VALUE) { throw e; }
-      return null; }
+    // Do pre-checking because try/catch with thrown exception causes huge
+    // performance hit.
+    if (index >= m_nodeArr.length && index != Integer.MAX_VALUE) return null;
+    return m_nodeArr[index]; // Exception if Integer.MAX_VALUE or negative.
   }
 
   // Understand that this method will potentially increase the size of the
@@ -33,22 +33,22 @@ class NodeArray
   // Integer.MAX_VALUE.
   void setNodeAtIndex(FNode node, int index)
   {
+    // Do pre-checking because try/catch with thrown exception causes huge
+    // performance hit.
+    if (index >= m_nodeArr.length && node == null &&
+        index != Integer.MAX_VALUE) return;
     try { m_nodeArr[index] = node; }
     catch (ArrayIndexOutOfBoundsException e)
     {
       if (index < 0 || index == Integer.MAX_VALUE) { throw e; }
-      else if (node == null) { return; }
-      else {
-        // We need to make sure that we at least double the array length
-        // in order to ensure amortized constant time hits for ths function.
-        final int newArrSize = (int)
-          Math.min((long) Integer.MAX_VALUE,
-                   Math.max(((long) m_nodeArr.length) * 2l + 1l,
-                            ((long) index) + 1l + (long) INITIAL_CAPACITY));
-        FNode[] newArr = new FNode[newArrSize];
-        System.arraycopy(m_nodeArr, 0, newArr, 0, m_nodeArr.length);
-        m_nodeArr = newArr;
-        m_nodeArr[index] = node; }
+      final int newArrSize = (int)
+        Math.min((long) Integer.MAX_VALUE,
+                 Math.max(((long) m_nodeArr.length) * 2l + 1l,
+                          ((long) index) + 1l + (long) INITIAL_CAPACITY));
+      FNode[] newArr = new FNode[newArrSize];
+      System.arraycopy(m_nodeArr, 0, newArr, 0, m_nodeArr.length);
+      m_nodeArr = newArr;
+      m_nodeArr[index] = node;
     }
   }
 
