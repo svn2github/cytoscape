1cf5bd678e3acfb7fb1deb4f34855d7dd2f6f3d4
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-12-11 01:09:49 +0000

    Optimizing this for internal package use.
    git-svn-id: http://chianti.ucsd.edu/svn@897 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/RootGraphEdgesRemovedEvent.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/RootGraphEdgesRemovedEvent.java
@@ -7,21 +7,18 @@ final class RootGraphEdgesRemovedEvent extends RootGraphChangeEventAdapter
 {
 
   private final Edge[] m_removedEdges;
-  private final int[] m_removedEdgeInx;
 
   // Note that no copy of the array removedEdges is made - the exact
-  // array reference is kept.  However, copies are made in the return values
-  // of methods of this class.  Note that the Edge objects in the input array
+  // array reference is kept.  Methods on this class return this same
+  // array reference.  Note that the Edge objects in the input array
   // must contain valid RootGraph indices at the time this constructor is
   // called; further behavior of the Edge objects is not too important
-  // because the getRemovedEdges() method has been deprecated.
+  // because the getRemovedEdges() method has been deprecated in both
+  // GraphPerspective and RootGraph listener systems.
   RootGraphEdgesRemovedEvent(RootGraph rootGraph, Edge[] removedEdges)
   {
     super(rootGraph);
     m_removedEdges = removedEdges;
-    m_removedEdgeInx = new int[m_removedEdges.length];
-    for (int i = 0; i < m_removedEdgeInx.length; i++)
-      m_removedEdgeInx[i] = m_removedEdges[i].getRootGraphIndex();
   }
 
   public final int getType()
@@ -29,19 +26,20 @@ final class RootGraphEdgesRemovedEvent extends RootGraphChangeEventAdapter
     return EDGES_REMOVED_TYPE;
   }
 
+  // If this system of listeners and events is to be used publicly (outside
+  // of this package, that is), then we need to make a copy of this array
+  // and return that copy.
   public final Edge[] getRemovedEdges()
   {
-    final Edge[] returnThis = new Edge[m_removedEdges.length];
-    System.arraycopy(m_removedEdges, 0, returnThis, 0, m_removedEdges.length);
-    return returnThis;
+    return m_removedEdges;
   }
 
+  // This method throws an exception, which is fine, because this system of
+  // listeners and events is only used internally by this package.  Nothing
+  // in this package calls this method.
   public final int[] getRemovedEdgeIndices()
   {
-    final int[] returnThis = new int[m_removedEdgeInx.length];
-    System.arraycopy(m_removedEdgeInx, 0, returnThis, 0,
-                     m_removedEdgeInx.length);
-    return returnThis;
+    throw new UnsupportedOperationException("don't call this method!");
   }
 
 }
