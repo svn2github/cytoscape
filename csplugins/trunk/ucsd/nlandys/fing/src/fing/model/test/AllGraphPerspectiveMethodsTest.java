d4ce76ebab39bfe5af9c63019fef871507bc4197
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-12-22 04:20:33 +0000

    Starting a GraphPerspective test.
    git-svn-id: http://chianti.ucsd.edu/svn@1049 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/AllGraphPerspectiveMethodsTest.java
@@ -0,0 +1,52 @@
+package fing.model.test;
+
+import fing.model.FingRootGraphFactory;
+import giny.model.Edge;
+import giny.model.GraphPerspective;
+import giny.model.Node;
+import giny.model.RootGraph;
+
+import java.util.Iterator;
+import java.util.List;
+
+public final class AllGraphPerspectiveMethodsTest
+{
+
+  // No constructor.
+  private AllGraphPerspectiveMethodsTest() { }
+
+  public static final void main(String[] args)
+  {
+    final RootGraph root = FingRootGraphFactory.instantiateRootGraph();
+    final GraphPerspective persp =
+      root.createGraphPerspective((int[]) null, (int[]) null);
+    int[] nodeInx = new int[5];
+    for (int i = 0; i < nodeInx.length - 1; i++)
+      nodeInx[i] = root.createNode();
+    int[] edgeInx = new int[7];
+    edgeInx[0] = root.createEdge(nodeInx[0], nodeInx[1], true);
+    edgeInx[1] = root.createEdge(nodeInx[1], nodeInx[2], false);
+    edgeInx[2] = root.createEdge(nodeInx[2], nodeInx[0], true);
+    edgeInx[3] = root.createEdge(nodeInx[2], nodeInx[2], true);
+    edgeInx[4] = root.createEdge(nodeInx[1], nodeInx[1], false);
+    edgeInx[5] = root.createEdge(nodeInx[1], nodeInx[0], true);
+    edgeInx[6] = root.createEdge(nodeInx[3], nodeInx[2], true);
+    nodeInx[nodeInx.length - 1] = root.createNode
+      (null, new int[] { edgeInx[6], edgeInx[2] });
+    if (!(root.addNodeMetaChild(nodeInx[0], nodeInx[1]) &&
+          root.addNodeMetaChild(nodeInx[0], nodeInx[4]) &&
+          root.addNodeMetaChild(nodeInx[3], nodeInx[1]) &&
+          root.addNodeMetaChild(nodeInx[4], nodeInx[4]) &&
+          root.addEdgeMetaChild(nodeInx[3], edgeInx[6]) &&
+          root.addEdgeMetaChild(nodeInx[3], edgeInx[0]) &&
+          root.addEdgeMetaChild(nodeInx[0], edgeInx[4])))
+      throw new IllegalStateException("unable to create meta relationship");
+    for (int i = 0; i < nodeInx.length; i++)
+      if (persp.restoreNode(nodeInx[i]) != nodeInx[i])
+        throw new IllegalStateException("unable to restore node");
+    for (int i = 0; i < edgeInx.length; i++)
+      if (persp.restoreEdge(edgeInx[i]) != edgeInx[i])
+        throw new IllegalStateException("unable to restore edge");
+  }
+
+}
