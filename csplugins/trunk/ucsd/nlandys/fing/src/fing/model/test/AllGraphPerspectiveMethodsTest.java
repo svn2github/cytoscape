af753adcc56471e928af65b5846e58fa24f37225
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2004-12-22 04:33:22 +0000

    Expanding the test.
    git-svn-id: http://chianti.ucsd.edu/svn@1050 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/AllGraphPerspectiveMethodsTest.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/AllGraphPerspectiveMethodsTest.java
@@ -47,6 +47,40 @@ public final class AllGraphPerspectiveMethodsTest
     for (int i = 0; i < edgeInx.length; i++)
       if (persp.restoreEdge(edgeInx[i]) != edgeInx[i])
         throw new IllegalStateException("unable to restore edge");
+
+    // Not testing GraphPerspectiveChangeListener methods.
+
+    // clone().
+    final GraphPerspective persp2 = (GraphPerspective) persp.clone();
+    if (persp2.getNodeCount() != persp.getNodeCount() ||
+        persp2.getEdgeCount() != persp.getEdgeCount())
+      throw new IllegalStateException("clone has different topology");
+    int[] edgeInxArr = persp2.getEdgeIndicesArray();
+    for (int i = 0; i < edgeInxArr.length; i++)
+      if (persp2.hideEdge(edgeInxArr[i]) != edgeInxArr[i])
+        throw new IllegalStateException("cannot hide edge in clone");
+    if (persp2.getEdgeCount() != 0)
+      throw new IllegalStateException("some edges in clone remaining");
+    if (persp2.getNodeCount() != persp.getNodeCount())
+      throw new IllegalStateException("node counts should still be the same");
+    int[] nodeInxArr = persp2.getNodeIndicesArray();
+    for (int i = 0; i < nodeInxArr.length; i++)
+      if (persp2.hideNode(nodeInxArr[i]) != nodeInxArr[i])
+        throw new IllegalStateException("cannot hide node in clone");
+    if (persp2.getNodeCount() != 0 || persp2.getEdgeCount() != 0)
+      throw new IllegalStateException("nodes or edges remaining");
+
+    // getRootGraph().
+    if (persp.getRootGraph() != root || persp2.getRootGraph() != root)
+      throw new IllegalStateException("incorrect RootGraph");
+
+    // getNodeCount().
+    if (persp.getNodeCount() != 5)
+      throw new IllegalStateException("wrong number of nodes");
+
+    // getEdgeCount().
+    if (persp.getEdgeCount() != 7)
+      throw new IllegalStateException("wrong number of edges");
   }
 
 }
