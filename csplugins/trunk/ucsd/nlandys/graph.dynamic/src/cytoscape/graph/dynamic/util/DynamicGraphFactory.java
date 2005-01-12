a610bde3e81a213b8570596d1a1095d4277962ca
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-01-12 19:35:21 +0000

    Adding more time complexity documentation.
    git-svn-id: http://chianti.ucsd.edu/svn@1149 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/DynamicGraphFactory.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/DynamicGraphFactory.java
@@ -7,6 +7,15 @@ import cytoscape.graph.dynamic.DynamicGraph;
  * This DynamicGraph implementation requires a bare minimum of roughly 64
  * metabytes for a graph with one million edges and one hundred thousand nodes.
  * That is, the memory requirements are roughly 64 bytes per node and edge.<p>
+ * The returned DynamicGraph does not free up memory.  That is, if a
+ * DynamicGraph contains one million edges and one hundred thousand nodes,
+ * it consumes roughly 64 megabytes.  If all nodes and edges are then removed
+ * from the DynamicGraph, it will still consume roughly 64 megabytes of
+ * memory.  If one hundred thousand nodes and one million edges are
+ * then created, the DynamicGraph will still consume roughly 64 megabytes of
+ * memory.  If all nodes and edges are removed and then re-created, repeatedly
+ * over an infinite number of iterations, the overall memory consumption
+ * required by this DynamicGraph will remain constant.<p>
  * Below are time complexities of DynamicGraph methods:
  * <blockquote><table border=1 cellspacing=0 cellpadding=5>
  * <tr><th>DynamicGraph method</th><th>time complexity</th></tr>
@@ -27,13 +36,20 @@ import cytoscape.graph.dynamic.DynamicGraph;
  *     the DynamicGraph increases in its total node count.</td>
  * </tr><tr>
  * <td>nodeRemove(int)</td>
- * <td></td>
+ * <td>A node is removed in amortized O(E) time, where E is the number of
+ *     edges touching the node being removed.  Amortized because a data
+ *     structure is used to keep track of free node slots; doubling
+ *     the size of this data structure is necessary at certain points.</td>
  * </tr><tr>
  * <td>edgeCreate(int, int, boolean)</td>
- * <td></td>
+ * <td>An edge is created in amortized constant time.  Amortized because
+ *     doubling the size of an underlying array will be necessary as the
+ *     DynamicGraph increases in its total edge count.</td>
  * </tr><tr>
  * <td>edgeRemove(int)</td>
- * <td></td>
+ * <td>An edge is removed in amortized constant time.  Amortized because
+ *     a data structure is used to keep track of free edge slots; doubling
+ *     the size of this data structure is necessary at certain points.</td>
  * </tr><tr>
  * <td>nodeExists(int)</td>
  * <td></td>
