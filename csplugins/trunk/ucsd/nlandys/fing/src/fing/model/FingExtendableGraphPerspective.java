50818e880f53e8d6ffc4d78507ad4edad47a202b
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-01-13 22:08:19 +0000

    Starting an extendable version of FGraphPerspective.
    git-svn-id: http://chianti.ucsd.edu/svn@1183 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/FingExtendableGraphPerspective.java
@@ -0,0 +1,18 @@
+package fing.model;
+
+/**
+ * Please try to restrain from using this class.  This class was created so
+ * that certain legacy applications would have an easier time using this
+ * giny.model implementation.
+ * @deprecated Use FingRootGraphFactory instead.
+ * @see FingRootGraphFactory
+ **/
+public class FingExtendableGraphPerspective extends FGraphPerspective
+{
+
+  public FingExtendableGraphPerspective()
+  {
+    super(null, null, null);
+  }
+
+}
