31228fc0ddb1e870bc382ea8d2de0cde2a336fa0
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-01-13 22:20:00 +0000

    OK GraphPerspective is now extendable.
    git-svn-id: http://chianti.ucsd.edu/svn@1185 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/FingExtendableGraphPerspective.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/FingExtendableGraphPerspective.java
@@ -1,5 +1,7 @@
 package fing.model;
 
+import cytoscape.util.intr.IntIterator;
+
 /**
  * Please try to restrain from using this class.  This class was created so
  * that certain legacy applications would have an easier time using this
@@ -10,9 +12,19 @@ package fing.model;
 public class FingExtendableGraphPerspective extends FGraphPerspective
 {
 
-  public FingExtendableGraphPerspective()
+  /**
+   * rootGraphNodeInx need not contain all endpoint nodes corresponding to
+   * edges in rootGraphEdgeInx - this is calculated automatically by this
+   * constructor.  If any index does not correspond to an existing node or
+   * edge, an IllegalArgumentException is thrown.  The indices lists need not
+   * be non-repeating - the logic in this constructor handles duplicate
+   * filtering.
+   **/
+  public FingExtendableGraphPerspective(FingExtendableRootGraph root,
+                                        IntIterator rootGraphNodeInx,
+                                        IntIterator rootGraphEdgeInx)
   {
-    super(null, null, null);
+    super(root, rootGraphNodeInx, rootGraphEdgeInx);
   }
 
 }
