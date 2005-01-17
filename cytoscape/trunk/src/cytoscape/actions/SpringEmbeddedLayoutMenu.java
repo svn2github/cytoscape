2c9b0c82935601c8103a64143fc4acedec644018
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-01-17 20:54:16 +0000

    Temporarily commented out legacy task framework code;  will update with new code soon
    git-svn-id: http://chianti.ucsd.edu/svn@6280 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutMenu.java
+++ cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutMenu.java
@@ -4,12 +4,12 @@ import cytoscape.Cytoscape;
 import cytoscape.foo.GraphConverter;
 import cytoscape.graph.layout.algorithm.MutablePolyEdgeGraphLayout;
 import cytoscape.graph.layout.impl.SpringEmbeddedLayouter2;
-import cytoscape.task.RunStoppable;
-import cytoscape.task.Stoppable;
+//import cytoscape.task.RunStoppable;
+//import cytoscape.task.Stoppable;
 import cytoscape.task.Task;
 import cytoscape.task.TaskMonitor;
-import cytoscape.task.ui.ProgressUI;
-import cytoscape.task.ui.ProgressUIControl;
+//import cytoscape.task.ui.ProgressUI;
+//import cytoscape.task.ui.ProgressUIControl;
 import cytoscape.view.CyNetworkView;
 
 import java.awt.event.ActionEvent;
@@ -66,30 +66,30 @@ public class SpringEmbeddedLayoutMenu extends JMenu
       //////////////////////////////////////////////////////////
       // BEGIN: The thread and process related code starts here.
       //////////////////////////////////////////////////////////
-
-      final RunStoppable runStop = new RunStoppable((Task) layoutAlg);
-      final boolean[] stoppd = new boolean[] { false };
-      final ProgressUIControl progCtrl = ProgressUI.startProgress
-        (Cytoscape.getDesktop(),
-         layoutAlg.getTaskTitle(),
-         new Stoppable() {
-           public void stop() {
-             stoppd[0] = true; // Use this to detect a pushed "Stop" button.
-             ((Stoppable) runStop).stop(); } });
-      layoutAlg.setTaskMonitor((TaskMonitor) progCtrl);
-      Runnable runAndDispose = new Runnable() {
-          public void run() {
-            runStop.run(); // It's important that we call run() on the
-                           // RunStoppable and NOT on the LayoutAlgorithm -
-                           // otherwise the RunStoppable won't block on stop().
-            // If our LayoutAlgorithm throws a RuntimeException in its run()
-            // method, the modal dialog will remain forever.  If we don't want
-            // this behavior, we should use 'try' block for run() and
-            // 'finally' block for dispose().
-            progCtrl.dispose(); } };
-      (new Thread(runAndDispose)).start();
-      progCtrl.show(); // This blocks until progCtrl.dispose() is called.
-      if (stoppd[0]) return; // Return without laying out graph if stopped.
+//
+//      final RunStoppable runStop = new RunStoppable((Task) layoutAlg);
+//      final boolean[] stoppd = new boolean[] { false };
+//      final ProgressUIControl progCtrl = ProgressUI.startProgress
+//        (Cytoscape.getDesktop(),
+//         layoutAlg.getTaskTitle(),
+//         new Stoppable() {
+//           public void stop() {
+//             stoppd[0] = true; // Use this to detect a pushed "Stop" button.
+//             ((Stoppable) runStop).stop(); } });
+//      layoutAlg.setTaskMonitor((TaskMonitor) progCtrl);
+//      Runnable runAndDispose = new Runnable() {
+//          public void run() {
+//            runStop.run(); // It's important that we call run() on the
+//                           // RunStoppable and NOT on the LayoutAlgorithm -
+//                           // otherwise the RunStoppable won't block on stop().
+//            // If our LayoutAlgorithm throws a RuntimeException in its run()
+//            // method, the modal dialog will remain forever.  If we don't want
+//            // this behavior, we should use 'try' block for run() and
+//            // 'finally' block for dispose().
+//            progCtrl.dispose(); } };
+//      (new Thread(runAndDispose)).start();
+//      progCtrl.show(); // This blocks until progCtrl.dispose() is called.
+//      if (stoppd[0]) return; // Return without laying out graph if stopped.
 
       //////////////////////////////////////////////////////
       // END: The thread and process related code ends here.
