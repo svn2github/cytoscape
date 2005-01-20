823e62bc824105c5f1b0bb5170758067c19baad3
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-01-20 16:10:02 +0000

    Updated to use new task framework
    git-svn-id: http://chianti.ucsd.edu/svn@6297 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/LoadEdgeAttributesAction.java
+++ cytoscape/trunk/src/cytoscape/actions/LoadEdgeAttributesAction.java
@@ -1,56 +1,64 @@
 // $Revision$
 // $Date$
 // $Author$
-
 package cytoscape.actions;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import javax.swing.AbstractAction;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import java.io.File;
-
 import cytoscape.Cytoscape;
-import cytoscape.CytoscapeInit;
-import cytoscape.util.*;
-import cytoscape.data.Semantics;
+import cytoscape.task.ui.JTaskConfig;
+import cytoscape.task.util.TaskManager;
+import cytoscape.util.CyFileFilter;
+import cytoscape.util.CytoscapeAction;
+import cytoscape.util.FileUtil;
 
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 /* 
  * Added by T. Ideker April 16, 2003
  * to allow loading of edge / edge attributes from the GUI
  */
-public class LoadEdgeAttributesAction extends CytoscapeAction {
-  
-    
-  public LoadEdgeAttributesAction () {
-    super("Edge Attributes...");
-    setPreferredMenu( "File.Load" );
-  }
-    
-  public void actionPerformed(ActionEvent e)  {
-    
-    CyFileFilter nf = new CyFileFilter();
-    nf.addExtension("ea");
-    nf.setDescription("Edge Attribute files");
 
-    // get the file name
-    final String name;
-    try {
-      name = FileUtil.getFile( "Load Edge Attributes",
-                               FileUtil.LOAD,
-                               new CyFileFilter[] { nf } ).toString();
-    } catch ( Exception exp ) {
-      // this is because the selection was canceled
-      return;
-    } 
-             
-    Cytoscape.loadAttributes( new String[] {},
-                              new String[] { name },
-                              !CytoscapeInit.noCanonicalization(),
-                              Cytoscape.getBioDataServer(),
-                              CytoscapeInit.getDefaultSpeciesName() ) ;
-  } // actionPerformed
+public class LoadEdgeAttributesAction extends CytoscapeAction {
+
+    /**
+     * Constructor.
+     */
+    public LoadEdgeAttributesAction() {
+        super("Edge Attributes...");
+        setPreferredMenu("File.Load");
+    }
+
+    /**
+     * User Initiated Request.
+     *
+     * @param e Action Event.
+     */
+    public void actionPerformed(ActionEvent e) {
+
+        //  Use a Default CyFileFilter:  enables user to select any file type.
+        CyFileFilter nf = new CyFileFilter();
+
+        // get the file name
+        File file = FileUtil.getFile("Load Edge Attributes",
+                    FileUtil.LOAD, new CyFileFilter[]{nf});
+
+        if (file != null) {
+
+            //  Create Load Attributes Task
+            LoadAttributesTask task = new LoadAttributesTask
+                    (file, Cytoscape.getEdgeNetworkData(),
+                    LoadAttributesTask.NODE_ATTRIBUTES);
+
+            //  Configure JTask Dialog Pop-Up Box
+            JTaskConfig jTaskConfig = new JTaskConfig();
+            jTaskConfig.setOwner(Cytoscape.getDesktop());
+            jTaskConfig.displayCloseButton(true);
+            jTaskConfig.displayStatus(true);
+            jTaskConfig.setAutoDispose(false);
+
+            //  Execute Task in New Thread;  pop open JTask Dialog Box.
+            TaskManager.executeTask(task, jTaskConfig);
+        }
+    }
 }
 
