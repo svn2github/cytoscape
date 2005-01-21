276478098ad7d8e8d087d4f06ced7f4fd9f7cbe3
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-01-21 23:08:00 +0000

    Reverting to revision 1.2, and making necessary minimal changes to get this to work w/o the task manager.
    git-svn-id: http://chianti.ucsd.edu/svn@6315 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutMenu.java
+++ cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutMenu.java
@@ -4,147 +4,103 @@ import cytoscape.Cytoscape;
 import cytoscape.foo.GraphConverter;
 import cytoscape.graph.legacy.layout.algorithm.MutablePolyEdgeGraphLayout;
 import cytoscape.graph.legacy.layout.impl.SpringEmbeddedLayouter2;
-import cytoscape.task.Task;
-import cytoscape.task.TaskMonitor;
-import cytoscape.task.ui.JTaskConfig;
-import cytoscape.task.util.TaskManager;
+// import cytoscape.task.RunStoppable;
+// import cytoscape.task.Stoppable;
+// import cytoscape.task.Task;
+// import cytoscape.task.TaskMonitor;
+// import cytoscape.task.ui.ProgressUI;
+// import cytoscape.task.ui.ProgressUIControl;
 import cytoscape.view.CyNetworkView;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+// import javax.swing.event.MenuDragMouseEvent;
 
-/**
- * Menu Options for Running the SpringEmbedded Layout.
- */
-public class SpringEmbeddedLayoutMenu extends JMenu {
-    private final JMenuItem m_wholeGraph;
-    private final JMenuItem m_selectedNodesOnly;
+public class SpringEmbeddedLayoutMenu extends JMenu
+{
 
-    /**
-     * Constructor.
-     */
-    public SpringEmbeddedLayoutMenu() {
-        super("Apply Spring Embedded Layout");
-        m_wholeGraph = new JMenuItem(new SpringEmbeddedLayoutAction(false));
-        add(m_wholeGraph);
-        m_selectedNodesOnly = new JMenuItem
-                (new SpringEmbeddedLayoutAction(true));
-        add(m_selectedNodesOnly);
-        addMouseListener(new MouseAdapter() {
-            public void mouseEntered(MouseEvent e) {
-                CyNetworkView graphView = Cytoscape.getCurrentNetworkView();
-                if (graphView.getSelectedNodeIndices().length == 0)
-                    m_selectedNodesOnly.setEnabled(false);
-                else
-                    m_selectedNodesOnly.setEnabled(true);
-            }
-        });
+  private final JMenuItem m_wholeGraph;
+  private final JMenuItem m_selectedNodesOnly;
+
+  public SpringEmbeddedLayoutMenu()
+  {
+    super("Apply Spring Embedded Layout");
+    m_wholeGraph = new JMenuItem(new SpringEmbeddedLayoutAction(false));
+    add(m_wholeGraph);
+    m_selectedNodesOnly = new JMenuItem(new SpringEmbeddedLayoutAction(true));
+    add(m_selectedNodesOnly);
+    addMouseListener(new MouseAdapter() {
+        public void mouseEntered(MouseEvent e)
+        {
+          CyNetworkView graphView = Cytoscape.getCurrentNetworkView();
+          if (graphView.getSelectedNodeIndices().length == 0)
+            m_selectedNodesOnly.setEnabled(false);
+          else
+            m_selectedNodesOnly.setEnabled(true);
+        } });
+  }
+
+  private static class SpringEmbeddedLayoutAction extends AbstractAction
+  {
+
+    private final boolean m_selectedNodesOnly;
+
+    public SpringEmbeddedLayoutAction(boolean selectedNodesOnly)
+    {
+      super((selectedNodesOnly ? "Move Selected Nodes Only" :
+             "Move All Nodes"));
+      m_selectedNodesOnly = selectedNodesOnly;
     }
 
-    /**
-    * User has Requested that the Spring Embedded Layout Algorithm be executed
-    * on the current graph.
-    */
-    private static class SpringEmbeddedLayoutAction extends AbstractAction {
+    public void actionPerformed(ActionEvent e)
+    {
+      final MutablePolyEdgeGraphLayout nativeGraph =
+        GraphConverter.getGraphCopy(0.0d, false, m_selectedNodesOnly);
+      SpringEmbeddedLayouter2 layoutAlg =
+        new SpringEmbeddedLayouter2(nativeGraph);
 
-        private final boolean m_selectedNodesOnly;
+      //////////////////////////////////////////////////////////
+      // BEGIN: The thread and process related code starts here.
+      //////////////////////////////////////////////////////////
 
-        /**
-         * Constructor.
-         * @param selectedNodesOnly Flag for Selected Nodes Only.
-         */
-        public SpringEmbeddedLayoutAction(boolean selectedNodesOnly) {
-            super((selectedNodesOnly ? "Selected Nodes Only" :
-                    "All Nodes"));
-            m_selectedNodesOnly = selectedNodesOnly;
-        }
+//       final RunStoppable runStop = new RunStoppable((Task) layoutAlg);
+//       final boolean[] stoppd = new boolean[] { false };
+//       final ProgressUIControl progCtrl = ProgressUI.startProgress
+//         (Cytoscape.getDesktop(),
+//          layoutAlg.getTaskTitle(),
+//          new Stoppable() {
+//            public void stop() {
+//              stoppd[0] = true; // Use this to detect a pushed "Stop" button.
+//              ((Stoppable) runStop).stop(); } });
+//       layoutAlg.setTaskMonitor((TaskMonitor) progCtrl);
+//       Runnable runAndDispose = new Runnable() {
+//           public void run() {
+//             runStop.run(); // It's important that we call run() on the
+//                            // RunStoppable and NOT on the LayoutAlgorithm -
+//                            // otherwise the RunStoppable won't block on stop().
+//             // If our LayoutAlgorithm throws a RuntimeException in its run()
+//             // method, the modal dialog will remain forever.  If we don't want
+//             // this behavior, we should use 'try' block for run() and
+//             // 'finally' block for dispose().
+//             progCtrl.dispose(); } };
+//       (new Thread(runAndDispose)).start();
+//       progCtrl.show(); // This blocks until progCtrl.dispose() is called.
+//       if (stoppd[0]) return; // Return without laying out graph if stopped.
 
-        /**
-         * User-Initiated Action:  Do Layout.
-         *
-         * @param e ActionEvent Object.
-         */
-        public void actionPerformed(ActionEvent e) {
-            // Convert to NativeGraph Intermediate Data Structure
-            MutablePolyEdgeGraphLayout nativeGraph =
-                    GraphConverter.getGraphCopy
-                    (0.0d, false, m_selectedNodesOnly);
+      layoutAlg.setTaskMonitor(null);
+      layoutAlg.run();
 
-            //  Instantiate Layout Task
-            LayoutTaskWrapper task = new LayoutTaskWrapper(nativeGraph);
+      //////////////////////////////////////////////////////
+      // END: The thread and process related code ends here.
+      //////////////////////////////////////////////////////
 
-            //  Configure JTask Dialog Pop-Up Box
-            JTaskConfig jTaskConfig = new JTaskConfig();
-            jTaskConfig.setOwner(Cytoscape.getDesktop());
-            jTaskConfig.setMillisToDecideToPopup(200);
-            jTaskConfig.displayCancelButton(true);
-            jTaskConfig.displayStatus(true);
-            jTaskConfig.setAutoDispose(true);
-
-            //  Execute Task in New Thread;  pop open JTask Dialog Box.
-            TaskManager.executeTask(task, jTaskConfig);
-        }
-    }
-}
-
-/**
- * Wrapper for Running the Spring Embedded Layout Algorithm.
- */
-class LayoutTaskWrapper implements Task {
-    private TaskMonitor taskMonitor;
-    private SpringEmbeddedLayouter2 childTask;
-    private MutablePolyEdgeGraphLayout nativeGraph;
-
-    /**
-     * Constructor.
-     * @param nativeGraph MutablePolyEdgeGraphLayout Object.
-     */
-    LayoutTaskWrapper(MutablePolyEdgeGraphLayout nativeGraph) {
-        this.nativeGraph = nativeGraph;
-        childTask = new SpringEmbeddedLayouter2(nativeGraph);
+      GraphConverter.updateCytoscapeLayout(nativeGraph);
     }
 
-    /**
-     * Executes Layout.
-     * This is a two-part process:
-     * First, run layout off-screen using internal data structures;
-     * Second, update the screen.
-     */
-    public void run() {
-        //  Run Layout on Internal Data Structures
-        childTask.setTaskMonitor(taskMonitor);
-        childTask.run();
+  }
 
-        //  Then, update the screen
-        taskMonitor.setStatus("Displaying graph...");
-        taskMonitor.setPercentCompleted(-1);
-        GraphConverter.updateCytoscapeLayout(nativeGraph);
-        taskMonitor.setPercentCompleted(100);
-    }
-
-    /**
-     * Halts Task.
-     */
-    public void halt() {
-        childTask.halt();
-    }
-
-    /**
-     * Sets Task Monitor.
-     * @param taskMonitor TaskMonitor Object.
-     */
-    public void setTaskMonitor(TaskMonitor taskMonitor)
-            throws IllegalThreadStateException {
-        this.taskMonitor = taskMonitor;
-    }
-
-    /**
-     * Gets Task Title.
-     * @return Task Title.
-     */
-    public String getTitle() {
-        return childTask.getTitle();
-    }
 }
