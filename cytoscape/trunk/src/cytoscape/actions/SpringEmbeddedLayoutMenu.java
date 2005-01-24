8eb64256a9dc6745cea7774961b51260dfff6f28
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-01-24 21:04:58 +0000

    Updated code to use new TaskManager; based on discussions with Nerius
    git-svn-id: http://chianti.ucsd.edu/svn@6326 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutMenu.java
+++ cytoscape/trunk/src/cytoscape/actions/SpringEmbeddedLayoutMenu.java
@@ -5,6 +5,8 @@ import cytoscape.foo.GraphConverter;
 import cytoscape.graph.legacy.layout.algorithm.MutablePolyEdgeGraphLayout;
 import cytoscape.graph.legacy.layout.impl.SpringEmbeddedLayouter2;
 import cytoscape.task.Task;
+import cytoscape.task.ui.JTaskConfig;
+import cytoscape.task.util.TaskManager;
 import cytoscape.view.CyNetworkView;
 
 import java.awt.event.ActionEvent;
@@ -45,8 +47,8 @@ public class SpringEmbeddedLayoutMenu extends JMenu
 
     public SpringEmbeddedLayoutAction(boolean selectedNodesOnly)
     {
-      super((selectedNodesOnly ? "Move Selected Nodes Only" :
-             "Move All Nodes"));
+      super((selectedNodesOnly ? "Selected Nodes Only" :
+             "All Nodes"));
       m_selectedNodesOnly = selectedNodesOnly;
     }
 
@@ -60,20 +62,30 @@ public class SpringEmbeddedLayoutMenu extends JMenu
       // BEGIN: The thread and process related code starts here.
       //////////////////////////////////////////////////////////
 
-      {
-        task.setTaskMonitor(null);
-        task.run();
-      } // Whatever you do, make sure that task.run() is finished by the time
-        // we exit out of this code block.  This may require a synchronized
-        // block with Object.wait() if we call task.run() from another thread.
+      //  Configure UI Options.
+      JTaskConfig jTaskConfig = new JTaskConfig();
+      jTaskConfig.setAutoDispose(true);
+      jTaskConfig.setOwner(Cytoscape.getDesktop());
+      jTaskConfig.displayCloseButton(true);
+      jTaskConfig.displayCancelButton(true);
+      jTaskConfig.displayStatus(true);
+
+      //  Execute Task
+      //  This method will block until Pop-up Dialog Box is diposed/closed.
+      boolean success = TaskManager.executeTask(task, jTaskConfig);
+
+      // Whatever you do, make sure that task.run() is finished by the time
+      // we exit out of this code block.  This may require a synchronized
+      // block with Object.wait() if we call task.run() from another thread.
 
       //////////////////////////////////////////////////////
       // END: The thread and process related code ends here.
       //////////////////////////////////////////////////////
 
-      GraphConverter.updateCytoscapeLayout(nativeGraph);
+      //  Update the UI only if Task was Successful.
+      if (success) {
+          GraphConverter.updateCytoscapeLayout(nativeGraph);
+      }
     }
-
   }
-
 }
