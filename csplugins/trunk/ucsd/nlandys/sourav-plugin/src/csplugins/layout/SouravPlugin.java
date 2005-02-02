c6319a50da117e7788a90bf11925a69700f3c38a
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-02-02 20:27:31 +0000

    Adding some content to this source file.
    git-svn-id: http://chianti.ucsd.edu/svn@1306 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/sourav-plugin/src/csplugins/layout/SouravPlugin.java
@@ -0,0 +1,14 @@
+package csplugins.layout;
+
+import cytoscape.Cytoscape;
+import cytoscape.plugin.CytoscapePlugin;
+
+public class SouravPlugin extends CytoscapePlugin
+{
+
+  public SouravPlugin()
+  {
+    System.out.println("YOU HAVE REACHED SOURAV'S PLUGIN");
+  }
+
+}
