0b74c448764338ed4ca55e2ac16300e537a47e38
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-02-02 20:38:40 +0000

    Now adding a menu item.
    git-svn-id: http://chianti.ucsd.edu/svn@1307 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/sourav-plugin/src/csplugins/layout/SouravPlugin.java
+++ csplugins/trunk/ucsd/nlandys/sourav-plugin/src/csplugins/layout/SouravPlugin.java
@@ -1,14 +1,24 @@
 package csplugins.layout;
 
+import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
 import cytoscape.plugin.CytoscapePlugin;
+import cytoscape.view.CyNetworkView;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JMenuItem;
 
 public class SouravPlugin extends CytoscapePlugin
 {
 
   public SouravPlugin()
   {
-    System.out.println("YOU HAVE REACHED SOURAV'S PLUGIN");
+    JMenuItem sourav = new JMenuItem(new AbstractAction("Sourav's worries") {
+        public void actionPerformed(ActionEvent e) {
+        } });
+    Cytoscape.getDesktop().getCyMenus().getMenuBar().getMenu("Layout").add
+      (sourav);
   }
 
 }
