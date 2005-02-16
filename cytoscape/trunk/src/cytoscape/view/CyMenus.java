abe69f57474cb8dd62e30580e6848a01d377809d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-02-16 18:19:09 +0000

    Addressing bug 615: " Most menu actions don't check for presence of network"
    git-svn-id: http://chianti.ucsd.edu/svn@6413 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/view/CyMenus.java
+++ cytoscape/trunk/src/cytoscape/view/CyMenus.java
@@ -269,6 +269,22 @@ public class CyMenus  implements GraphViewChangeListener {
     editMenu    = menuBar.getMenu( "Edit" );
     dataMenu    = menuBar.getMenu( "Data" );
     selectMenu  = menuBar.getMenu( "Select" );
+    final JMenu f_selectMenu = selectMenu;
+    selectMenu.addMouseListener(new MouseAdapter() {
+        public void mouseEntered(MouseEvent e)
+        {
+          CyNetworkView graphView = Cytoscape.getCurrentNetworkView();
+          boolean inactive = false;
+          if (graphView == null || graphView.nodeCount() == 0) inactive = true;
+          MenuElement[] popup = f_selectMenu.getSubElements();
+          if (popup[0] instanceof JPopupMenu) {
+            MenuElement[] submenus =
+              ((JPopupMenu) popup[0]).getSubElements();
+            for (int i = 0; i < submenus.length; i++) {
+              if (submenus[i] instanceof JMenuItem) {
+                if (inactive) ((JMenuItem) submenus[i]).setEnabled(false);
+                else ((JMenuItem) submenus[i]).setEnabled(true); } } }
+        } });
     layoutMenu  = menuBar.getMenu( "Layout" );
     final JMenu f_layoutMenu = layoutMenu;
     layoutMenu.addMouseListener(new MouseAdapter() {
