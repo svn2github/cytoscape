9c5bdade0f7aa8f9514e45002eb62c0897d90506
bader <bader@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-02-17 17:36:38 +0000

    Added old methods that need to be there for compatibility of many plugins.  They are now deprecated.
    git-svn-id: http://chianti.ucsd.edu/svn@6422 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/CytoscapeObj.java
+++ cytoscape/trunk/src/cytoscape/CytoscapeObj.java
@@ -1,37 +1,50 @@
 package cytoscape;
 
+import cytoscape.data.servers.BioDataServer;
+
 import java.io.File;
 
-import cytoscape.data.servers.BioDataServer;
-import cytoscape.visual.CalculatorCatalog;
-import cytoscape.visual.CalculatorCatalogFactory;
-import cytoscape.visual.CalculatorIO;
-import cytoscape.plugin.*;
-
-import java.beans.*;
-
 public class CytoscapeObj {
- 
-  public CytoscapeObj () {
-  }
 
-  /**
-   * Returns the (possibly null) bioDataServer.
-   *
-   * @see BioDataServer
-   */
-  public BioDataServer getBioDataServer() {
-    return Cytoscape.getBioDataServer();
-  }
+    public CytoscapeObj() {
+    }
 
-  public CytoscapeConfig getConfiguration () {
-    return new CytoscapeConfig();
-  }
+    /**
+     * Returns the (possibly null) bioDataServer.
+     *
+     * @see BioDataServer
+     */
+    public BioDataServer getBioDataServer() {
+        return Cytoscape.getBioDataServer();
+    }
+
+    public CytoscapeConfig getConfiguration() {
+        return new CytoscapeConfig();
+    }
 
 
-  public int getViewThreshold () {
-    return 500;
-  }
+    public int getViewThreshold() {
+        return 500;
+    }
 
+    /**
+     * Get the current directory.
+     *
+     * @return The startup directory if a user has not chosen another via a load or save operation,
+     *         else returns the last user chosen directory
+     * @deprecated As of Cytoscape 2.1, replaced by {@link CytoscapeInit#getMRUD()};
+     */
+    public File getCurrentDirectory() {
+        return CytoscapeInit.getMRUD();
+    }
+
+    /**
+     * Set the current directory
+     *
+     * @deprecated As of Cytoscape 2.1, replaced by {@link CytoscapeInit#getMRUD()};
+     */
+    public void setCurrentDirectory(File mrud) {
+        CytoscapeInit.setMRUD(mrud);
+    }
 }
 
