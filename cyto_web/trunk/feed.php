944216372f94e3ebfc183bedfd0dbe324869ed22
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-02-22 21:52:14 +0000

    Now limits # of postings to 3 (requested by Gary);  also now includes a date stamp for each posting
    git-svn-id: http://chianti.ucsd.edu/svn@4364 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/feed.php
+++ cyto_web/trunk/feed.php
@@ -9,20 +9,36 @@ $rss = fetch_rss($url);
 
 echo "<A HREF='http://groups-beta.google.com/group/cytoscape-announce'>";
 echo "Cytoscape Announcements:</A></B><P>\n";
+#  Only show the three most recent items.
+$counter = 0;
 foreach ($rss->items as $item ) {
-	$name = $item["author_name"];
-	$summary = $item[summary];
-	$summary2 = str_replace("<BR>", "\n", $summary);
-	$date = $item[issued];
-	$title = $item[title];
-	$url   = $item[link];
-	echo "<a href=$url>$title</a></li><BR>\n";
-	echo " <UL><LI>$summary2 [cont.]</UL>";
+	if ($counter < 3) {
+		$name = $item["author_name"];
+		$summary = $item[summary];
+		$summary2 = str_replace("<br>", "\n", $summary);
+		$title = $item[title];
+		$url   = $item[link];
+
+		// Parse and format the date directly;
+		// strtotime() does not work
+		$date = ("$item[issued]");
+		list($usable_date, $extra) = split("T", $date);
+		list($year, $month, $day) = split ("-", $usable_date);
+		$time_stamp = strtotime("$month/$day/$year");
+		$date_formatted = date("F j, Y", $time_stamp);
+
+		echo "<a href=$url>$title</a></li>.&nbsp;&nbsp;$date_formatted<BR>\n";
+		echo " <UL><LI>$summary2 [cont.]</UL>";
 	
-	#print_r(array_keys($item));
-	echo "<BR>";
+		#print_r(array_keys($item));
+		echo "<BR>";
+	}
+	$counter++;
 }
+echo "<A HREF='http://groups-beta.google.com/group/cytoscape-announce'>";
+echo "View All Announcements</A></B><P>\n";
 ?>
+<P>
 	<P>
 	<form action="http://groups-beta.google.com/group/cytoscape-announce/boxsubscribe">
 		<div class="item">
