34d9659a71b10d28212c91751653a3c1f1151ea1
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-02-22 21:50:31 +0000

    Alphabetized list of plugins;  added column re:  works in 1.1
    git-svn-id: http://chianti.ucsd.edu/svn@4362 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/plugins1.php
+++ cyto_web/trunk/plugins1.php
@@ -26,13 +26,50 @@
 	<p>
 		Cytoscape includes a flexible PlugIn architecture that enables developers to add extra functionality beyond that provided in the core. PlugIns also provide a convenient place for testing out new Cytoscape features. </p> 
 	<p>
-		<b>Note that PlugIns on this page will only work with Cytoscape 1.1.1 or below</b>. We also maintain a list of <a href="plugins2.php">Cytoscape 2.0 PlugIns</a>. 
+		<b>Note that PlugIns on this page will only work with Cytoscape 1.1</b>. We also maintain a list of <a href="plugins2.php">Cytoscape 2.x PlugIns</a>. 
+	<p><big><b>PlugIn License Policy:</b></big>
+	<P>
+	Although the Cytoscape core
+application is distributed under a Library GNU Public License (LGPL),
+plugins are separate works which use Cytoscape as a Java code library.
+Plugins are therefore governed by independent software licenses
+distributed with and specific to each plugin.  The Cytoscape project
+has no intent to capture plugins under the license terms of the core
+Cytoscape LGPL.	
 	<p>
 		<big><b>Current Cytoscape 1.1 PlugIns:</b></big> 
 		<br>
 	</p>
-	<table style="margin-left: 30;  margin-right:30;border: dotted gray 1px;padding-left: 10px;font-size:small" width="95%" cellpadding="5" cellspacing="5">
+	<table style="margin-left: 30;  margin-right:30;border: dotted gray 1px;padding-left: 10px;font-size:small" width="95%" cellpadding="5" cellspacing="5" bgcolor="#ebebff">
 		<tbody>
+			<tr>
+				<td width="20%" valign="top">
+					<b>Biomodules</b> <font size="-1"> 
+						<br>
+						Version: 1.0 
+						<br>
+						Release Date: March 1, 2004 </font> 
+				</td>
+				<td width="40%" valign="top">
+					<p>
+						Biological modules are loose associations of preferred molecular interaction partners that perform a collective function. <i>Biomodules</i> is a Cytoscape plugin that identifies modules in molecular networks and analyzes their expression patterns and biological functions. This information is integrated, visualized, and analyzed in an interactive graphical representation. 
+					</p>
+					<p>
+						Released by: <a href="http://labs.systemsbiology.net/galitski">Galitski Lab</a> at the <a href="http://www.systemsbiology.org">Institute for Systems Biology</a> 
+					</p>
+				</td>
+				<td width="20%" valign="top">Verified to work in Cytoscape 1.1</td>				
+				<td width="20%" valign="top">
+					[<a href="http://labs.systemsbiology.net/galitski/projs/biomodules/index_biomodules.html">Biomodules Home Page</a>]<b> 
+					</td>
+				</b> 
+			</td>
+		</tr>
+			<tr>
+				<td colspan="4">
+					<hr>
+				</td>
+			</tr>		
 			<tr>
 				<td width="20%" valign="top">
 					<b>Data Services Plug-In</b> <font size="-1"> 
@@ -41,7 +78,7 @@
 						<br>
 						Release Date: September 5, 2003 </font> 
 				</td>
-				<td width="60%" valign="top">
+				<td width="40%" valign="top">
 					This PlugIn enables Cytoscape to import/export to multiple file formats. 
 					<p>
 						The current release includes support for: 
@@ -61,6 +98,7 @@
 						Released by: Sander Group, <a href="http://www.cbio.mskcc.org/">Computational Biology Center</a>, Memorial Sloan Kettering Cancer Center. 
 					</p>
 				</td>
+				<td width="20%" valign="top">Verified to work in Cytoscape 1.1.1</td>
 				<td width="20%" valign="top">
 					[<a href="http://www.cbio.mskcc.org/cytoscape/ds/README.txt">Release Notes</a>] 
 					<br>
@@ -70,10 +108,42 @@
 				</td>
 			</tr>
 			<tr>
-				<td colspan="3">
+				<td colspan="4">
 					<hr>
 				</td>
 			</tr>
+
+			<tr>
+				<td width="20%" valign="top">
+					<b>Expression Data Viewers</b> <font size="-1"> 
+						<br>
+						Version: 1.0 
+						<br>
+						Release Date: October 13, 2003 </font> 
+				</td>
+				<td width="40%" valign="top">
+					This package contains two plugins. The first enables the user to view the expression profiles of selected genes in a Cytoscape network across all the experimental conditions. The second enables the user to find genes with a high degree of correlation with a node or set of nodes of interest. 
+					<p>
+						This package includes the Visad library (http://www.ssec.wisc.edu/~billh/visad.html). 
+					</p>
+					<p>
+						Released by: <a href="http://www.systemsbiology.org">Institute for Systems Biology</a> 
+					</p>
+				</td>
+				<td width="20%" valign="top">Verified to work in Cytoscape 1.1</td>				
+				<td width="20%" valign="top">
+					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/expDataViewers/README.txt">Release Notes</a>] 
+					<br>
+					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/expDataViewers/license.txt">License</a>] 
+					<br>
+					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/expDataViewers/ExpDataViewers.jar">Download</a>] 
+				</td>
+			</tr>	
+			<tr>
+				<td colspan="4">
+					<hr>
+				</td>
+			</tr>			
 			<tr>
 				<td width="20%" valign="top">
 					<b>jActiveModules</b> <font size="-1"> 
@@ -82,7 +152,7 @@
 						<br>
 						Release Date: February 20, 2004 </font> 
 				</td>
-				<td width="60%" valign="top">
+				<td width="40%" valign="top">
 					This plugin enables Cytoscape to search for significant networks as described in <a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids=12169552&amp;dopt=Abstract">Bioinformatics. 2002 Jul;18 Suppl 1:S233-40</a>. 
 					<p>
 						This product was developed using tools from the Apache Software Foundation (<a href="http://www.apache.org/">http://www.apache.org</a>). 
@@ -91,6 +161,7 @@
 						Released by: <a href="http://www-bioeng.ucsd.edu/faculty/area/ideker_lab/">The Ideker Lab</a>, Department of Bioengineering, UCSD 
 					</p>
 				</td>
+				<td width="20%" valign="top">Verified to work in Cytoscape 1.1</td>
 				<td width="20%" valign="top">
 					[<a href="http://www-bioeng.ucsd.edu/faculty/area/ideker_lab/plugins/jActiveModules/README.txt">Release Notes</a>] 
 					<br>
@@ -100,7 +171,7 @@
 				</td>
 			</tr>
 			<tr>
-				<td colspan="3">
+				<td colspan="4">
 					<hr>
 				</td>
 			</tr>
@@ -112,12 +183,13 @@
 						<br>
 						Release Date: March 5, 2004 </font> 
 				</td>
-				<td width="60%" valign="top">
+				<td width="40%" valign="top">
 					This plugin to Cytoscape automates the process of aligning two protein-protein interaction networks and mining for evolutionarily conserved pathways, as described in <a href="http://www.pnas.org/cgi/content/abstract/100/20/11394">PNAS 2003 100: 11394-11399</a>. 
 					<p>
 						Released by: <a href="http://www-bioeng.ucsd.edu/faculty/area/ideker_lab/">The Ideker Lab</a>, Department of Bioengineering, UCSD 
 					</p>
 				</td>
+				<td width="20%" valign="top">Verified to work in Cytoscape 1.1</td>				
 				<td width="20%" valign="top">
 					[<a href="http://www-bioeng.ucsd.edu/faculty/area/ideker_lab/plugins/pathBlast/PathBlastManual.pdf">Release Notes</a>] 
 					<br>
@@ -129,7 +201,7 @@
 				</td>
 			</tr>
 			<tr>
-				<td colspan="3">
+				<td colspan="4">
 					<hr>
 				</td>
 			</tr>
@@ -141,75 +213,20 @@
 						<br>
 						Release Date: October 3, 2003 </font> 
 				</td>
-				<td width="60%" valign="top">
+				<td width="40%" valign="top">
 					This plugin allows you to load a level 1 <a href="http://sbml.org/"> SBML</a> file from Cytoscape's File menu, and to display the model as a Cytoscape network. You can also <a href="http://db.systemsbiology.net:8080/cytoscape/projects/static/sbmlReader/v1/cy.jnlp">run</a> Cytoscape with this plugin using Java Web Start, &nbsp; or view a <a href="http://db.systemsbiology.net:8080/cytoscape/plugins/sbmlReader/repressilator.jpg" target="aaa">snapshot</a> of a sample SBML model of the Repressilotor System (<a href="http://sbml.org/models/Models-published/Genetic-2000Elo/Genetic-2000Elo.html" target="aaa">Elowitz &amp; Leibler, 2000)</a>. 
 					<p>
 						Released by: <a href="http://www.systemsbiology.org/">Institute for Systems Biology</a> 
 					</p>
 				</td>
+				<td width="20%" valign="top">Verified to work in Cytoscape 1.1</td>				
 				<td width="20%" valign="top">
 					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/sbmlReader/releaseNotes.html">Release Notes</a>] 
 					<br>
 					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/sbmlReader/SBMLReader.jar">Download</a>] 
 				</td>
 			</tr>
-			<tr>
-				<td colspan="3">
-					<hr>
-				</td>
-			</tr>
-			<tr>
-				<td width="20%" valign="top">
-					<b>Expression Data Viewers</b> <font size="-1"> 
-						<br>
-						Version: 1.0 
-						<br>
-						Release Date: October 13, 2003 </font> 
-				</td>
-				<td width="60%" valign="top">
-					This package contains two plugins. The first enables the user to view the expression profiles of selected genes in a Cytoscape network across all the experimental conditions. The second enables the user to find genes with a high degree of correlation with a node or set of nodes of interest. 
-					<p>
-						This package includes the Visad library (http://www.ssec.wisc.edu/~billh/visad.html). 
-					</p>
-					<p>
-						Released by: <a href="http://www.systemsbiology.org">Institute for Systems Biology</a> 
-					</p>
-				</td>
-				<td width="20%" valign="top">
-					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/expDataViewers/README.txt">Release Notes</a>] 
-					<br>
-					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/expDataViewers/license.txt">License</a>] 
-					<br>
-					[<a href="http://db.systemsbiology.net:8080/cytoscape/plugins/expDataViewers/ExpDataViewers.jar">Download</a>] 
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3">
-					<hr>
-				</td>
-			</tr>
-			<tr>
-				<td width="20%" valign="top">
-					<b>Biomodules</b> <font size="-1"> 
-						<br>
-						Version: 1.0 
-						<br>
-						Release Date: March 1, 2004 </font> 
-				</td>
-				<td width="60%" valign="top">
-					<p>
-						Biological modules are loose associations of preferred molecular interaction partners that perform a collective function. <i>Biomodules</i> is a Cytoscape plugin that identifies modules in molecular networks and analyzes their expression patterns and biological functions. This information is integrated, visualized, and analyzed in an interactive graphical representation. 
-					</p>
-					<p>
-						Released by: <a href="http://labs.systemsbiology.net/galitski">Galitski Lab</a> at the <a href="http://www.systemsbiology.org">Institute for Systems Biology</a> 
-					</p>
-				</td>
-				<td width="20%" valign="top">
-					[<a href="http://labs.systemsbiology.net/galitski/projs/biomodules/index_biomodules.html">Biomodules Home Page</a>]<b> 
-					</td>
-				</b> 
-			</td>
-		</tr>
+
 	</tbody>
 </table>
 </div>
