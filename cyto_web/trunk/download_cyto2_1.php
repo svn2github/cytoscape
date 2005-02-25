f566b75d07b291c9acc47bd2dc390bb4cb7fa12c
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-02-25 22:14:06 +0000

    Removed Install Anywhere link temporarily
    git-svn-id: http://chianti.ucsd.edu/svn@4396 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/download_cyto2_1.php
+++ cyto_web/trunk/download_cyto2_1.php
@@ -7,6 +7,7 @@
 		You may now download Cytoscape for Unix, Windows and Mac OS X systems through any of the following 
 		four options:
 		</p>
+<!--		
 		<h4>
 		Option 1:  Windows / Linux:  One Step Installation
 		</h4>
@@ -17,18 +18,9 @@
 			Install Cytoscape 2.1 (One Click Install)</a> 
 		</UL>
 		</h2>
+-->
 		<p>
-		</p>
-		<h4>
-		Option 2:  Mac OS X Release
-		</h4>
-		<p>
-		<ul>
-			<li><a href='<?= $cyto2_1_mac_east ?>' >Mac OS X Release</A></li>
-		</ul>
-		</p>
-		<p>
-		<h4>Option 3:  All Platforms:  Download a .tar.gz or .zip distribution file
+		<h4>Option 1:  All Platforms:  Download a .tar.gz or .zip distribution file
 		</H4>
 		</p>
 		<p>
@@ -46,9 +38,21 @@
 			</li>		
 		</ul>
 		</p>
+
+		<p>
+		</p>
+		<h4>
+		Option 2:  Mac OS X Release
+		</h4>
+		<p>
+		<ul>
+			<li><a href='<?= $cyto2_1_mac_east ?>' >Mac OS X Release</A></li>
+		</ul>
+		</p>
+
 		<p>
 		<p>
-		<h4>Option 4:  All Platforms:  Download the Complete Source Code for Cytoscape
+		<h4>Option 3:  All Platforms:  Download the Complete Source Code for Cytoscape
 		</h4>
     		<UL>
 		    <li><a href='<?= $cyto2_1_source_east ?>'>Cytoscape source</a> - USA, East Coast (New York, NY).</li>  
