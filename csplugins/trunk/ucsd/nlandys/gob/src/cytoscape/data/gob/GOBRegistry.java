41de1f12f9e7e12b3cc82f88df756c7c2ffd66ab
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-03-22 20:05:08 +0000

    Making progress.
    git-svn-id: http://chianti.ucsd.edu/svn@1379 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/gob/src/cytoscape/data/gob/GOBRegistry.java
@@ -0,0 +1,53 @@
+package cytoscape.data.gob;
+
+import cytoscape.util.intr.IntEnumerator;
+
+public class GOBRegistry
+{
+
+  public final int ATTR_TYPE_BOOLEAN = 2;
+  public final int ATTR_TYPE_DOUBLE = 3;
+  public final int ATTR_TYPE_LONG = 5;
+  public final int ATTR_TYPE_STRING = 6;
+
+  /**
+   * @param type one of the ATTR_TYPE_* constants.
+   * @return an identifier for this newly created attribute or -1 if
+   *   the specified attribute type is not recognized as a valid type.
+   */
+  public int defineAttribute(int type)
+  {
+    return false;
+  }
+
+  /**
+   * @param attrID the attribute ID of the attribute span to remove.
+   * @return true if and only if the specified attribute exists at the time
+   *   this method is called.
+   */
+  public boolean removeAttribute(int attrID)
+  {
+    return false;
+  }
+
+  /**
+   * @return one of the ATTR_TYPE_* constants specifiying which kind of
+   *   attribute the specified attribute is, or -1 if no attribute with
+   *   specified attribute ID exists.
+   */
+  public int getAttributeType(int attrID)
+  {
+    return -1;
+  }
+
+  /**
+   * @return an enumeration of all attributes defined in this registry; each
+   *   entry in the returned enumeration is a unique attribute ID; this method
+   *   never returns null.
+   */
+  public IntEnumerator getAttributes()
+  {
+    return null;
+  }
+
+}
