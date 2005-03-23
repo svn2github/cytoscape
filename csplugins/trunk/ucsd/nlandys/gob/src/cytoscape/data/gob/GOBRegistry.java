3547e71ba1e1d16a71cd84279d29b5f33b0c916c
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-03-23 02:11:36 +0000

    OK the API is nearing completion.
    git-svn-id: http://chianti.ucsd.edu/svn@1387 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/gob/src/cytoscape/data/gob/GOBRegistry.java
+++ csplugins/trunk/ucsd/nlandys/gob/src/cytoscape/data/gob/GOBRegistry.java
@@ -12,49 +12,62 @@ public class GOBRegistry
 
   /**
    * @param type one of the ATTR_TYPE_* constants.
-   * @return an identifier for this newly created attribute definition or -1 if
-   *   the specified attribute type [input parameter] is not recognized as a
-   *   valid type; attribute definition IDs are always non-negative.
+   * @return an identifier for this newly created attribute definition;
+   *   attribute definition IDs are always non-negative.
+   * @exception IllegalArgumentException if the type specified is not one
+   *   of the ATTR_TYPE_* constants.
    */
   public int defineAttribute(int type)
   {
-    return -1;
+    throw new IllegalArgumentException("not implemented yet - pardon me");
   }
 
   /**
    * @param attrID an identifier of an existing attribute definition.
    * @return one of the ATTR_TYPE_* constants specifiying which kind of
-   *   attribute the specified attribute definition is, or -1 if no attribute
-   *   definition with specified ID exists.
+   *   attribute the specified attribute definition (attrID) is.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
    */
   public int attributeType(int attrID)
   {
-    return -1;
+    throw new IllegalArgumentException();
   }
 
   /**
-   * @return an enumeration of all attribute definitions in this registry; each
-   *   entry in the returned enumeration is a unique attribute definition ID;
-   *   this method never returns null.
+   * @param attrID a potential attribute definition ID.
+   * @return true if and only if the specified attribute definition (attrID)
+   *   is defined in this registry.
    */
-  public IntEnumerator attributes()
-  {
-    return null;
-  }
-
-  /**
-   * @param attrID the ID of the attribute definition to remove.
-   * @return true if and only if the specified attribute definition exists at
-   *   the time this method is called.
-   */
-  public boolean removeAttribute(int attrID)
+  public boolean attributeDefined(int attrID)
   {
     return false;
   }
 
   /**
+   * @return an enumeration of unique attribute definition identifiers in
+   *   this registry; this method never returns null.
+   */
+  public IntEnumerator attributes()
+  {
+    return new IntEnumerator() {
+        public int numRemaining() { return 0; }
+        public int nextInt() { return -1; } };
+  }
+
+  /**
+   * @param attrID the ID of the attribute definition to remove.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
+   */
+  public void removeAttribute(int attrID)
+  {
+    throw new IllegalArgumentException();
+  }
+
+  /**
    * NOTE: Instead of using this method, you are encouraged to use one of the
-   * assignXXXAttribute() methods that take a specific type of object as input.
+   * assignXXXAttribute() methods that take a specific type of value as input.
    * @param objID an object identifier; assigned attribute values belong
    *   to objects, and objects are abstracted as integers by this API.
    * @param attrID an identifier of an attribute definition that was created
@@ -64,16 +77,18 @@ public class GOBRegistry
    *   class of the Object must be java.lang.Boolean for ATTR_TYPE_BOOLEAN,
    *   java.lang.Double for ATTR_TYPE_DOUBLE, java.lang.Long for
    *   ATTR_TYPE_LONG, and java.lang.String for ATTR_TYPE_STRING.
-   * @return false if and only if no attribute definition with specified
-   *   ID exists.
    * @exception ClassCastException if the specified attribute definition
    *   (attrID) exists but the specified attribute value (attrValue) is
    *   not of the class implied by the type (ATTR_TYPE_*) of specified
    *   attribute definition.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
+   * @exception NullPointerException if the specified attribute value
+   *   (attrValue) is null.
    */
-  public boolean assignAttribute(int objID, int attrID, Object attrValue)
+  public void assignAttribute(int objID, int attrID, Object attrValue)
   {
-    return false;
+    throw new IllegalArgumentException();
   }
 
   /**
@@ -83,15 +98,14 @@ public class GOBRegistry
    *   using defineAttribute().
    * @param attrValue the attribute value that is to be assigned to the
    *   specified object, in the specified attribute definition domain.
-   * @return false if and only if no attribute definition with specified
-   *   ID exists.
    * @exception ClassCastException if the specified attribute definition
    *   (attrID) exists but is not of type ATTR_TYPE_BOOLEAN.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
    */
-  public boolean assignBooleanAttribute(int objID, int attrID,
-                                        boolean attrValue)
+  public void assignBooleanAttribute(int objID, int attrID, boolean attrValue)
   {
-    return false;
+    throw new IllegalArgumentException();
   }
 
   /**
@@ -101,14 +115,14 @@ public class GOBRegistry
    *   using defineAttribute().
    * @param attrValue the attribute value that is to be assigned to the
    *   specified object, in the specified attribute definition domain.
-   * @return false if and only if no attribute definition with specified
-   *   ID exists.
    * @exception ClassCastException if the specified attribute definition
    *   (attrID) exists but is not of type ATTR_TYPE_DOUBLE.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
    */
-  public boolean assignDoubleAttribute(int objID, int attrID, double attrValue)
+  public void assignDoubleAttribute(int objID, int attrID, double attrValue)
   {
-    return false;
+    throw new IllegalArgumentException();
   }
 
   /**
@@ -118,14 +132,14 @@ public class GOBRegistry
    *   using defineAttribute().
    * @param attrValue the attribute value that is to be assigned to the
    *   specified object, in the specified attribute definition domain.
-   * @return false if and only if no attribute definition with specified
-   *   ID (attrID) exists.
    * @exception ClassCastException if the specified attribute definition
    *   (attrID) exists but is not of type ATTR_TYPE_LONG.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
    */
-  public boolean assignLongAttribute(int objID, int attrID, long attrValue)
+  public void assignLongAttribute(int objID, int attrID, long attrValue)
   {
-    return false;
+    throw new IllegalArgumentException();
   }
   
   /**
@@ -134,24 +148,63 @@ public class GOBRegistry
    * @param attrID an identifier of an attribute definition that was created
    *   using defineAttribute().
    * @param attrValue the attribute value that is to be assigned to the
-   *   specified object, in the specified attribute definition domain.
-   * @return false if and only if no attribute definition with specified
-   *   ID (attrID) exists.
+   *   specified object, in the specified attribute definition domain; null
+   *   values are not allowed, but empty strings are allowed.
    * @exception ClassCastException if the specified attribute definition
    *   (attrID) exists but is not of type ATTR_TYPE_STRING.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
+   * @exception NullPointerException if the specified attribute value
+   *   (attrValue) is null.
    */
-  public boolean assignStringAttribute(int objID, int attrID, String attrValue)
+  public void assignStringAttribute(int objID, int attrID, String attrValue)
   {
-    return false;
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * @param objID an object identifier.
+   * @param attrID an identifier of an attribute definition that was
+   *   created using defineAttribute().
+   * @return true if and only if the specified object (objID) has an attribute
+   *   value from the specified attribute definition domain (attrID) assigned
+   *   to it; note that if the specified object is not recognized by this
+   *   registry, false is returned (as opposed to throwing an exception).
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
+   */
+  public boolean hasAttribute(int objID, int attrID)
+  {
+    throw new IllegalArgumentException();
   }
 
   /**
    * NOTE: Instead of using this method, you are encouraged to use one of the
-   * XXXAttributeValue() methods that return a specific type of object.
+   * XXXAttributeValue() methods that return a specific type.
+   * @param objID an object identifier that was previously used in
+   *   assigning an attribute value in the specified attribute definition
+   *   domain (attrID).
+   * @param attrID an identifier of an attribute definition that was
+   *   created using defineAttribute().
+   * @return the value that was previously assigned to specified object
+   *   in specified attribute definition domain; if this attribute definition
+   *   if of type ATTR_TYPE_BOOLEAN, the returned object is of type
+   *   java.lang.Boolean; if this attribute definition is of type
+   *   ATTR_TYPE_DOUBLE, the returned object is of type java.lang.Double;
+   *   if this attribute definition is of type ATTR_TYPE_LONG, the returned
+   *   object is of type java.lang.Long; if this attribute definition is of
+   *   type ATTR_TYPE_STRING, the returned object is of type java.lang.String;
+   *   null is never returned by this method.
+   * @exception NoSuchElementException if the specified attribute
+   *   definition (attrID) exists but there is no corresponding attribute
+   *   value assigned to specified object; note that Java try/catch blocks
+   *   are a huge performance bottleneck when exceptions are thrown.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
    */
   public Object attributeValue(int objID, int attrID)
   {
-    return null;
+    throw new IllegalArgumentException();
   }
 
   /**
@@ -175,7 +228,7 @@ public class GOBRegistry
    */
   public boolean booleanAttributeValue(int objID, int attrID)
   {
-    throw new RuntimeException();
+    throw new IllegalArgumentException();
   }
 
   /**
@@ -199,7 +252,7 @@ public class GOBRegistry
    */
   public double doubleAttributeValue(int objID, int attrID)
   {
-    throw new RuntimeException();
+    throw new IllegalArgumentException();
   }
 
   /**
@@ -223,7 +276,7 @@ public class GOBRegistry
    */
   public long longAttributeValue(int objID, int attrID)
   {
-    throw new RuntimeException();
+    throw new IllegalArgumentException();
   }
 
   /**
@@ -233,7 +286,8 @@ public class GOBRegistry
    * @param attrID an identifier of an attribute definition that was
    *   created using defineAttribute().
    * @return the value that was previously assigned to specified object
-   *   in specified attribute definition domain.
+   *   in specified attribute definition domain; null is never returned, but
+   *   the empty string may be returned.
    * @exception ClassCastException if the specified attribute definition
    *   (attrID) exists but is not of type ATTR_TYPE_STRING, regardless of
    *   whether or not the specified object (objID) is recognized by this
@@ -247,54 +301,87 @@ public class GOBRegistry
    */
   public String stringAttributeValue(int objID, int attrID)
   {
-    throw new RuntimeException();
+    throw new IllegalArgumentException();
   }
 
   /**
-   * @return an enumeration of attributes that currently have assigned
-   *   values to the specified objects; every entry in the returned enumeration
-   *   is an attribute identifier; this method null if and only if this
-   *   registry knows nothing of the specified object (for example if no
-   *   attributes were ever specified for this object or if all attributes
-   *   were deleted for this [once existing] object); note that an object
-   *   will cease to exist in this registry if it has no attributes assigned
-   *   to it.
+   * NOTE: This method is superfluous because we can implement it by calling
+   * assignedAttributes(objID) and checking for the empty enumeration.
+   * @param objID a potential object identifier.
+   * @return true if and only if the specified object (objID) has
+   *   at least one attribute value assigned.
    */
-  public IntEnumerator assignedAttributes(int objID)
-  {
-  }
-
-  /**
-   * @return an enumeration of all objects that have at least one
-   *   attribute value assigned; each element of the returned enumeration
-   *   is an object identifier; this method never returns null.
-   */
-  public IntEnumerator objects()
-  {
-  }
-
-  /**
-   * Forgets that any value for the specified attribute was ever assigned to
-   * the specified object.  Note that an object will cease to exist in this
-   * registry if it has no attributes assigned to it.
-   * @return true if and only if a value for the specified attribute is
-   *   assigned to the specified object at the time this method is called.
-   */
-  public boolean forgetAssignedAttribute(int objID, int attrID)
+  public boolean objectRecognized(int objID)
   {
     return false;
   }
 
   /**
+   * @return an enumeration of unique attribute definition identifiers such
+   *   that the specified object (objID) currently has an assigned attribute
+   *   value in each of the returned attribute definitions; this method never
+   *   returns null, and an empty enumeration is synonymous with
+   *   objectRecognized(objID) returning false.
+   */
+  public IntEnumerator assignedAttributes(int objID)
+  {
+    return new IntEnumerator() {
+        public int numRemaining() { return 0; }
+        public int nextInt() { return -1; } };
+  }
+
+  /**
+   * @return an enumeration of unique objects (object identifiers) that have
+   *   an attribute value in the specified attribute definition domain (attrID)
+   *   assigned; this method never returns null.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
+   */
+  public IntEnumerator objectsWithAttribute(int attrID)
+  {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * @return an enumeration of unique objects (object identifiers) that have
+   *   at least one attribute value assigned (from any existing attribute
+   *   definition domain); this method never returns null.
+   */
+  public IntEnumerator objects()
+  {
+    return new IntEnumerator() {
+        public int numRemaining() { return 0; }
+        public int nextInt() { return -1; } };
+  }
+
+  /**
+   * Forgets that any value for the specified attribute was ever assigned to
+   * the specified object.  Note that an object will cease to exist in this
+   * registry if it has no attributes assigned to it.  Also note that
+   * if no attribute value from the specified attribute definition domain
+   * (attrID) is assigned to the specified object (objID) at the time this
+   * method is called, nothing is changed, and no error condition is raised.
+   * @return true if and only if a value for the specified attribute is
+   *   assigned to the specified object at the time this method is called.
+   * @exception IllegalArgumentException if no attribute definition with
+   *   specified ID (attrID) exists.
+   */
+  public void forgetAssignedAttribute(int objID, int attrID)
+  {
+    throw new IllegalArgumentException();
+  }
+
+  /**
    * Forgets all attribute values that have been assigned to the specified
-   * object.<p>
+   * object (objID).  Note that if the specified object (objID) is not
+   * recognized by this registry at the time this method is called,
+   * nothing is changed, and no error condition is raised.<p>
    * NOTE: This method is superfluous because we can implement it by
    * getting all assigned attributes for specified object, then deleteing
    * those attributes one by one.
-   * @return true if and only if the specified object ID is currently
-   *   registered as having at least one attribute value assigned to it.
+   * @param objID an object identifier.
    */
-  public boolean forgetObject(int objID)
+  public void forgetObject(int objID)
   {
   }
 
