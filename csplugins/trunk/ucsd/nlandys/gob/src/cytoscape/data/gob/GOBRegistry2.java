b99ef01f316f5d0c1db7e44e30a0ec93bfa7b510
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-03-24 02:20:14 +0000

    Fleshing this out.
    git-svn-id: http://chianti.ucsd.edu/svn@1395 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/gob/src/cytoscape/data/gob/GOBRegistry2.java
+++ csplugins/trunk/ucsd/nlandys/gob/src/cytoscape/data/gob/GOBRegistry2.java
@@ -29,4 +29,59 @@ public class GOBRegistry2 extends GOBRegistry
     throw new IllegalArgumentException("not implemented yet - pardon me");
   }
 
+  /**
+   * Let us visualize a multi-attribute value as a point in a multidimensional
+   * space, whose axes are the primitive types that make up the multi-attribute
+   * definition.  In a given multi-attribute definition domain (such a
+   * domain is created with the defineMultiAttribute(byte[]) method), for a
+   * given [fixed] object, any number of unique multi-attribute values
+   * [points] can be assigned to that object.  By "unique values" I mean the
+   * following.  An object cannot have more than one
+   * multi-attribute value "point" assigned to it in the same location in the
+   * attribute definition domain space.  For example, if our multi-attribute
+   * definition domain defID1 is of two dimensions, ATTR_TYPE_LONG cross
+   * ATTR_TYPE_LONG, then a given object objID1 cannot be assigned two
+   * attributes attrID1 and attrID2 in space defID1 such that attrID1's value
+   * is (1,2) and attrID2's value is (1,2) - it is possible for attrID1's
+   * value to be (1,2) and for attrID2's value to be (1,3), for example.
+   * @return a multi-attribute identifier, referred to as "attrID" in other
+   *   methods; to uniquely identify an attribute value [multidimensional]
+   *   point for a given object in a given [multi] attribute definition domain,
+   *   what is needed is the triplet { objID, defID, attrID } (see
+   *   method multiAttributeValue()) - the attrID by itself
+   *   is not enough to uniquely identify a multi-attribute value point;
+   *   attribute IDs are nonnegative and are clustered close to zero.
+   */
+  public int assignMultiAttribute(int objID, int defID, Object[] attrValue)
+  {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * @return an enumeration of attribute IDs (attrID types) for attributes
+   *   assigned to the specified object (objID) in the specified attribute
+   *   definition domain (defID); the attribute definition domain must
+   *   be of type ATTR_TYPE_MULTI, otherwise an error condition is raised.
+   */
+  public IntEnumerator multiAttributeKeys(int objID, int defID)
+  {
+    throw new IllegalArgumentException();
+  }
+
+  public Object[] multiAttributeValue(int objID, int defID, int attrID)
+  {
+    throw new IllegalArgumentException();
+  }
+
+  /**
+   * NOTE: Calling the non-multi-attribute method
+   * forgetAssignedAttribute(objID, defID) is equivalent to calling this
+   * method with every attrID assigned to this object in this attribute
+   * definition domain.
+   */
+  public void forgetAssignedMultiAttribute(int objID, int defID, int attrID)
+  {
+    throw new IllegalArgumentException();
+  }
+
 }
