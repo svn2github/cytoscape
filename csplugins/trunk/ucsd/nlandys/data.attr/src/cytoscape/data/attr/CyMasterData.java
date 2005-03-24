7466109dd9eca2e3b47fcd5d8bd76688b553c18f
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-03-24 19:03:08 +0000

    Strarting a higher-level API.  Jotting down ideas.
    git-svn-id: http://chianti.ucsd.edu/svn@1399 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyMasterData.java
@@ -0,0 +1,12 @@
+package cytoscape.data.attr;
+
+/**
+ * This API one that is used internally by Cytoscape's implementation.
+ * Functionality here includes the ability to register a node's or edge's
+ * name.  It is Cytoscape's job to keep the state of this data object
+ * in sync with other data objects that Cytoscape uses - for example,
+ * the master RootGraph object.
+ */
+public interface CyMasterData
+{
+}
---
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyNodeData.java
@@ -0,0 +1,9 @@
+package cytoscape.data.attr;
+
+/**
+ * This API is one that plugin developers have visibility into - the ability
+ * to define, delete, or change a node's name is not included in this API.
+ */
+public interface CyNodeData
+{
+}
