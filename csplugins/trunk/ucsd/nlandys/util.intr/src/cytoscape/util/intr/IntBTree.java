c058ea05d5e09a95c04898222c1a23e336b682f0
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-03-25 22:09:14 +0000

    Fleshing out more details in Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@1415 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/IntBTree.java
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/IntBTree.java
@@ -16,24 +16,53 @@ public final class IntBTree
     m_root = new Node(MAX_BRANCHES, true);
   }
 
+  /**
+   * Inserts a new entry into this structure.  Duplicate entries are allowed.
+   * @param x the new entry to insert.
+   */
   public void insert(int x)
   {
   }
 
-  public void delete(int x)
+  /**
+   * Deletes exactly one instance of the integer x.  To delete all
+   * instances of the integer x, use deleteRange(x, 1).
+   * @return true if and only if an entry was deleted (at most one entry is
+   *   deleted by this method).
+   */
+  public boolean delete(int x)
   {
+    return false;
   }
 
-  public void deleteRange(int minX, int maxX)
+  /**
+   * Deletes all instances of entries in the range [xStart, xStart + spanSize)
+   * from this structure.
+   * @param spanSize specifies how many consecutive integers, starting
+   *   at xStart, to delete; spanSize cannot be negative (if spanSize is zero
+   *   no action is taken).
+   * @return the number of entries that were deleted from this structure.
+   * @exception IllegalArgumentException if spanSize is negative.
+   */
+  public int deleteRange(int xStart, int spanSize)
   {
+    return 0;
   }
 
+  /**
+   * @return the number of entries x currently in this structure.
+   */
   public int count(int x)
   {
     return 0;
   }
 
-  public IntEnumerator searchRange(int minX, int maxX)
+  /**
+   * Returns an enumeration of all entries in the range
+   * [xStart, xStart + spanSize) currently in this structure; the entries
+   * within the enumeration are returned in non-descending order.
+   */
+  public IntEnumerator searchRange(int xStart, int spanSize)
   {
     return null;
   }
