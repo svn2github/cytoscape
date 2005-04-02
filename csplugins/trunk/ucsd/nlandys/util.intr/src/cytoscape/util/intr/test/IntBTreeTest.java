e16ebdb433c020141e78a4a105e0adb1aef35e28
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-02 01:45:21 +0000

    Fleshing out the tests some more.
    git-svn-id: http://chianti.ucsd.edu/svn@1492 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/test/IntBTreeTest.java
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/test/IntBTreeTest.java
@@ -12,7 +12,7 @@ public class IntBTreeTest
     System.out.println
       ("Instantiated new IntBTree.");
     final int[] arr = new int[]
-      { 5, 8, 1, 3, 8, 3, 0, 9, 1, 7, 3, 3, 0, 4, 3, 1, 3, 3 };
+      { 8, 1, 3, 8, 3, 0, 9, 1, 7, 3, 3, 0, 4, 3, 1, 3, 3 };
     System.out.print
       ("My int[] is: { ");
     for (int i = 0; i < arr.length - 1; i++)
@@ -31,16 +31,16 @@ public class IntBTreeTest
       System.out.print(iter.nextInt() + " ");
     System.out.println(".");
     final int[] countThese =
-      new int[] { Integer.MIN_VALUE, -1, 0, 3, 4, 6, 7, 8, 9, 10,
+      new int[] { Integer.MIN_VALUE, -1, 0, 1, 3, 4, 6, 7, 8, 9, 10,
                   99, Integer.MAX_VALUE };
     for (int i = 0; i < countThese.length; i++)
       System.out.println
         ("The count of integer " + countThese[i] + " is " +
          tree.count(countThese[i]) + ".");
     final int[] xMins = new int[]
-      { Integer.MIN_VALUE, -23, 1, 3, 2, 3, 8, 4, 4 };
+      { Integer.MIN_VALUE, -23, 1, 3, 2, 3, 8, 4, 4, 5, 6, -1, 11 };
     final int[] xMaxs = new int[]
-      { 3, 99, 2, Integer.MAX_VALUE, 6, 4, 8, 4, 8 };
+      { 3, 99, 2, Integer.MAX_VALUE, 6, 4, 8, 4, 8, 6, 6, -1, 11 };
     for (int i = 0; i < xMins.length; i++) {
       System.out.print("In range [" + xMins[i] + ", " + xMaxs[i] + "]: ");
       iter = tree.searchRange(xMins[i], xMaxs[i]);
