734b0369579ede40437db62cc81f2cd08e6ad463
jangelo <jangelo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-05 21:53:54 +0000

    Fixed Bug# 599 (zoom to selection now works on a single selected node).
    git-svn-id: http://chianti.ucsd.edu/svn@6493 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/ZoomSelectedAction.java
+++ cytoscape/trunk/src/cytoscape/actions/ZoomSelectedAction.java
@@ -71,7 +71,12 @@ public class ZoomSelectedAction extends CytoscapeAction {
           }
         }
         
-        PBounds zoomToBounds = new PBounds( smallX  , smallY  , ( bigX - smallX + 100 ), ( bigY - smallY + 100 ) );
+        PBounds zoomToBounds;
+        if (selected_nodes.size() == 1) {
+          zoomToBounds = new PBounds( smallX - 100 , smallY - 100 , ( bigX - smallX + 200 ), ( bigY - smallY + 200 ) );
+        } else {
+          zoomToBounds = new PBounds( smallX  , smallY  , ( bigX - smallX + 100 ), ( bigY - smallY + 100 ) );
+        }
         PTransformActivity activity =  ( ( PhoebeNetworkView )view).getCanvas().getCamera().animateViewToCenterBounds( zoomToBounds, true, 500 );
     }
 }
