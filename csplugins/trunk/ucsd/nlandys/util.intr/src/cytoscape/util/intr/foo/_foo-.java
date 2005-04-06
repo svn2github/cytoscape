e2028076042a455b20fc20def581233d8ef74d0a
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-06 23:55:46 +0000

    OK.
    git-svn-id: http://chianti.ucsd.edu/svn@1520 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/foo/_foo-.java
@@ -0,0 +1,14 @@
+public class foo
+{
+
+  public final int deleteRange(final int xMin, final int xMax)
+  {
+  }
+
+  private final int deleteRange(final Node n,
+                                final int xMin, final int xMax,
+                                final int minBound, final int maxBound)
+  {
+  }
+
+}
