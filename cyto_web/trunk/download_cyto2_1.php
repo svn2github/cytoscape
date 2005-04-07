803b5b83ca551a6743ed24ce0d714d88dedb7d8e
bader <bader@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-07 21:01:55 +0000

    Added note about saving to disk for tar files - user complained that clicking on the tar file resulted in a corrupted file.
    git-svn-id: http://chianti.ucsd.edu/svn@4424 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/download_cyto2_1.php
+++ cyto_web/trunk/download_cyto2_1.php
@@ -1,13 +1,13 @@
 <div id="indent">
 	<blockquote>
 		<p>
-		You have successfully registered to download Cytoscape 2.1. 
+		You have successfully registered to download Cytoscape 2.1.
 		</p>
 		<p>
-		You may now download Cytoscape for Unix, Windows and Mac OS X systems through any of the following 
+		You may now download Cytoscape for Unix, Windows and Mac OS X systems through any of the following
 		four options:
 		</p>
-<!--		
+<!--
 		<h4>
 		Option 1:  Windows / Linux:  One Step Installation
 		</h4>
@@ -15,7 +15,7 @@
 		</p>
 		<UL>
 			<LI><a href='<?= $cyto2_1_install_anywhere ?>'>
-			Install Cytoscape 2.1 (One Click Install)</a> 
+			Install Cytoscape 2.1 (One Click Install)</a>
 		</UL>
 		</h2>
 -->
@@ -30,12 +30,12 @@
 				<a href='<?= $cyto2_1_gz_east ?>' >
 					Unix / Linux / Mac OS X
 				</a>
-				- 
+				-
 				<a href='<?= $cyto2_1_zip_east ?>'>
-					Windows 
+					Windows
 				</a>
-				- USA, East Coast (New York, NY). 
-			</li>		
+				- USA, East Coast (New York, NY).
+			</li>
 		</ul>
 		</p>
 
@@ -55,7 +55,8 @@
 		<h4>Option 3:  All Platforms:  Download the Complete Source Code for Cytoscape
 		</h4>
     		<UL>
-		    <li><a href='<?= $cyto2_1_source_east ?>'>Cytoscape source</a> - USA, East Coast (New York, NY).</li>  
+		    <li><a href='<?= $cyto2_1_source_east ?>'>Cytoscape source</a> - USA, East Coast (New York, NY).</li>
+		    <li>Note: Saving to disk via the right-click menu is recommended.</li>  
 		</UL>
   </p>
   <P>
