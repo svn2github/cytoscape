2d035a4c0c19bfce53d4428f08934dbdf158c934
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-07 00:58:17 +0000

    Progress.
    git-svn-id: http://chianti.ucsd.edu/svn@1522 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/foo/_foo-.java
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/foo/_foo-.java
@@ -3,6 +3,14 @@ public class foo
 
   public final int deleteRange(final int xMin, final int xMax)
   {
+    final int returnThis = deleteRange(m_root, xMin, xMax,
+                                       Integer.MIN_VALUE, Integer.MAX_VALUE);
+    if (m_root.sliceCount <= 1 && !isLeafNode(m_root)) {
+      if (m_root.sliceCount == 0) {
+        m_root = new Node(m_maxBranches, true); }
+      else { // One slice count.
+        m_root = m_root.data.children[0]; } }
+    return returnThis;
   }
 
   private final int deleteRange(final Node n,
