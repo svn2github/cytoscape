9258b0636456967683f698d582d695fdcf2b87a9
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-07 01:03:12 +0000

    Progress.
    git-svn-id: http://chianti.ucsd.edu/svn@1523 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/foo/_foo-.java
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/foo/_foo-.java
@@ -17,6 +17,14 @@ public class foo
                                 final int xMin, final int xMax,
                                 final int minBound, final int maxBound)
   {
+    // The input node is modified in such a way that its deep count and
+    // slice count correct once this method returns.  In addition, besides
+    // potentially suffering from underflow, everything else under this node
+    // will be correct, and not suffering from underflow.
+    // NOTE: At the expense of complexity, this recursion could be optimized
+    // for performance by not shifting array members more than once.  This
+    // implementation does two shifts -- once in this recursive step and once
+    // [potentially] by the caller of this method, if underflow is experienced.
   }
 
 }
