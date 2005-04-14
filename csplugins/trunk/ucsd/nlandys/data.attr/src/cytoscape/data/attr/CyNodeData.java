895df067b05ebc546fd9b1a49a71d9cd0079ca03
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-14 23:27:42 +0000

    Modifying Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@1563 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyNodeData.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyNodeData.java
@@ -6,11 +6,21 @@ public interface CyNodeData
 {
 
   /**
+   * @param nodeKey the node to which to bind a new attribute value.
+   * @param attrName the attribute definition in which to assign an
+   *   attribute value.
    * @param keyIntoValue an array of length N - 1 where N is the
    *   dimensionality of attribute attrName; this set of objects is
    *   a key that will uniquely define the attribute value for this node
-   *   in the specified attribute definition.
-   * @param attrValue the attribute value to set on specified nodeKey;
+   *   in the specified attribute definition; the entry at index i in this
+   *   array must correspond to the type of dimension i + 1 of the attribute
+   *   definition (for example if attribuute attrName has two or more
+   *   dimensions and if the first dimension of attribute attrName
+   *   is of type
+   *   CyNodeDataDef.DIM_TYPE_INTEGER, then the element at index zero in this
+   *   array must be of type java.lang.Long); keyIntoValue may be null if
+   *   the dimenstionality of attribute attrName is one.
+   * @param attrValue the attribute value to bind;
    *   the type of this object must be of the appropriate type based on
    *   the final dimension of the attribute definition.
    */
@@ -35,12 +45,12 @@ public interface CyNodeData
                                    Object[] prefix);
 
   /**
-   * Returns representatives, along specified prefix, of
-   * dimension prefix.length + 1.  The type of objects in the returned
-   * enumeration will all be of the type as specified by the
-   * dimension prefix.length + 1.
    * @param delete if true, all value entries with specified prefix are also
    *   deleted from specified nodeKey.
+   * @return representatives, along specified prefix, of
+   * dimension prefix.length + 1.  The type of objects in the returned
+   * enumeration will all be of the type as specified by the
+   * dimension prefix.length + 1 of attribute attrName.
    */
   public Enumeration getAttributeSpan(String nodeKey, String attrName,
                                       Object[] prefix, boolean delete);
