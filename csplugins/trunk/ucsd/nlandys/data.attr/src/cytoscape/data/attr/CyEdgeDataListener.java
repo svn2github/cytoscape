68c08540981f6432d7375ef8bdd7b0c9aa524d61
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-15 23:46:31 +0000

    Copying over the work that has been done in CyNodeDataListener to apply to edges.
    git-svn-id: http://chianti.ucsd.edu/svn@1583 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyEdgeDataListener.java
@@ -0,0 +1,29 @@
+package cytoscape.attr.data;
+
+public interface CyEdgeDataListener
+{
+
+  /**
+   * @param keyIntoValue don't modify this array!
+   */
+  public void edgeAttributeValueAssigned(String edgeKey,
+                                         String attributeName,
+                                         Object[] keuIntoValue,
+                                         Object attributeValue);
+
+  /**
+   * @param keyIntoValue don't modify this array!
+   */
+  public void edgeAttributeValueRemoved(String edgeKey,
+                                        String attributeName,
+                                        Object[] keyIntoValue,
+                                        Object attributeValue);
+
+  /**
+   * @param prefix don't modify this array!
+   */
+  public void edgeAttributeKeyspanRemoved(String edgeKey,
+                                          String attributeName,
+                                          Object[] keyPrefix);
+
+}
