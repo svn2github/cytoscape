e85b3bbbc68eccf10a7e409072ba471a7fa2ccc6
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-15 00:06:31 +0000

    Making changes as per Rowan's suggestions.
    git-svn-id: http://chianti.ucsd.edu/svn@1564 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyNodeDataDef.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyNodeDataDef.java
@@ -10,37 +10,59 @@ public interface CyNodeDataDef
   /**
    * This type corresponds to java.lang.Boolean.
    */
-  public final byte DIM_TYPE_BOOLEAN = 1;
+  public final byte TYPE_BOOLEAN = 1;
 
   /**
    * This type corresponds to java.lang.Double.
    */
-  public final byte DIM_TYPE_FLOATING_POINT = 2;
+  public final byte TYPE_FLOATING_POINT = 2;
 
   /**
    * This type corresponds to java.lang.Long.
    */
-  public final byte DIM_TYPE_INTEGER = 3;
+  public final byte TYPE_INTEGER = 3;
 
   /**
    * This type corresponds to java.lang.String.
    */
-  public final byte DIM_TYPE_STRING = 4;
+  public final byte TYPE_STRING = 4;
 
   /**
-   * @param attrName an identifier for this attribute definition; this value
-   *   must be unique from all existing node attribute definitions.
-   * @param dimTypes an array of length N, where N is the number of
-   *   dimensions in this attribute definition; each entry in this array is
-   *   one of the DIM_TYPE_* constants and the entry at index i in this
-   *   array defines the type of dimension i + 1.
-   * @param dimNames an array of length N, where N is the number of
-   *   dimensions in this attribute definition; the entry at index i in this
-   *   array defines the name of dimension i + 1; the dimension names must
-   *   be unique from each other withing a given attribute definition.
+   * Creates a node attribute definition.  The most common type of attribute
+   * (or at least the type most of us are familiar with) is one where the
+   * key space has zero dimensions.  For example, if I want to identify each
+   * node as having a color, I would create an attribute namespace which
+   * stores values of TYPE_STRING, and has no key sequence mapping color
+   * values.  By "no key sequence" I mean that the input parameters
+   * keyTypes and keyNames would be either null or the empty array for my
+   * color attribute definition.  The more interesting case is where the key
+   * space for an attribute has one or more dimensions.  For example, if I
+   * wanted to create an attribute that represents measured p-values for
+   * all nodes over a set of experiments ("Ideker experiment",
+   * "Salk experiment", ...) I would define a one-dimensional key space
+   * of TYPE_STRING (to represent the experiment names) and a value of
+   * TYPE_FLOATING_POINT (to represent p-values).
+   * @param attributeName an identifier for this attribute definition;
+   *   this value must be unique from all existing node attribute definitions;
+   *   ideally, the choice of name would describe values being stored by this
+   *   attribute definition.
+   * @param valueType one of the TYPE_* constants defining what type of
+   *   values are bound to nodes in this attribute definition.
+   * @param keyTypes defines the type of each dimension in the key space;
+   *   the entry at index i defines the type of key space dimension i + 1;
+   *   this parameter may either be null or the empty array if an attribute
+   *   definition does not use a key space (this is perhaps the most common
+   *   scenario).
+   * @param keyNames defines the name of each dimension in the key space;
+   *   the entry at index i defines the name of key space dimension i + 1;
+   *   this parameter may either be null or the empty array if an attribute
+   *   definition does not use a key space (this is perhaps the most common
+   *   scenario).
    */
-  public void defineNodeAttribute(String attrName,
-                                  byte[] dimTypes, String[] dimNames);
+  public void defineNodeAttribute(String attributeName,
+                                  byte valueType,
+                                  byte[] keyTypes,
+                                  String[] keyNames);
 
   /**
    * @return an enumeration of java.lang.String; each returned string
