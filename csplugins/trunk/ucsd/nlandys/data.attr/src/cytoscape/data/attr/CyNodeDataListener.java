5f1859fdd2d3b781b16826d8cb3f6616ea8faa38
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-15 22:56:15 +0000

    Changing some stuff.
    git-svn-id: http://chianti.ucsd.edu/svn@1577 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyNodeDataListener.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CyNodeDataListener.java
@@ -6,19 +6,24 @@ public interface CyNodeDataListener
   /**
    * @param keyIntoValue don't modify this array!
    */
-  public void attributeValueAssigned(String nodeKey, String attrName,
-                                     Object[] keuIntoValue, Object attrValue);
+  public void nodeAttributeValueAssigned(String nodeKey,
+                                         String attributeName,
+                                         Object[] keuIntoValue,
+                                         Object attributeValue);
 
   /**
    * @param keyIntoValue don't modify this array!
    */
-  public void attributeValueRemoved(String nodeKey, String attrName,
-                                    Object[] keyIntoValue, Object attrValue);
+  public void nodeAttributeValueRemoved(String nodeKey,
+                                        String attributeName,
+                                        Object[] keyIntoValue,
+                                        Object attributeValue);
 
   /**
    * @param prefix don't modify this array!
    */
-  public void attributeSpanRemoved(String nodeKey, String attrName,
-                                   Object[] prefix);
+  public void nodeAttributeKeyspanRemoved(String nodeKey,
+                                          String attributeName,
+                                          Object[] keyPrefix);
 
 }
