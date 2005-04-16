eae760c302ba0a002c25feaffa680ad16bcb8731
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-16 21:53:57 +0000

    Making some progress on the node attribute definitions.  I'm going to collapse the four HashMap members into one as my next commit.
    git-svn-id: http://chianti.ucsd.edu/svn@1590 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/CyDataModel.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/CyDataModel.java
@@ -10,20 +10,97 @@ import cytoscape.data.attr.CyNodeDataDefinitionListener;
 import cytoscape.data.attr.CyNodeDataListener;
 
 import java.util.Enumeration;
+import java.util.HashMap;
 
 final class CyDataModel
   implements CyNodeDataDefinition, CyNodeData, CyEdgeDataDefinition, CyEdgeData
 {
 
+  // Keys are attributeName, values are java.util.HashMap whose keys are
+  // nodeKey.
+  private final HashMap m_attrMap;
+
+  // Keys are attributeName, values are java.lang.Byte.
+  private final HashMap m_attrValueTypeMap;
+
+  // Keys are attributeName, values are byte[].
+  private final HashMap m_attrKeyTypesMap;
+
+  // Keys are attributeName, values are java.lang.String[].
+  private final HashMap m_attrKeyNamesMap;
+
   CyDataModel()
   {
+    m_attrMap = new HashMap();
+    m_attrValueTypeMap = new HashMap();
+    m_attrKeyTypesMap = new HashMap();
+    m_attrKeyNamesMap = new HashMap();
   }
 
-  public void defineNodeAttribute(String attributeName,
-                                  byte valueType,
-                                  byte[] keyTypes,
-                                  String[] keyNames)
+  public final void defineNodeAttribute(final String attributeName,
+                                        final byte valueType,
+                                        final byte[] keyTypes,
+                                        final String[] keyNames)
   {
+    // Error-check attributeName.  Unfortunately there are currently no
+    // constraints on the length or the contents of attributeName.
+    if (attributeName == null)
+      throw new NullPointerException("attributeName is null");
+    if (m_attrMap.containsKey(attributeName))
+      throw new IllegalStateException
+        ("attributeName '" + attributeName + "' already exists");
+
+    // Error-check valueType.
+    switch (valueType)
+    {
+      case CyNodeDataDefinition.TYPE_BOOLEAN:
+      case CyNodeDataDefinition.TYPE_FLOATING_POINT:
+      case CyNodeDataDefinition.TYPE_INTEGER:
+      case CyNodeDataDefinition.TYPE_STRING:
+        break;
+      default:
+        throw new IllegalArgumentException("valueType is unrecognized");
+    }
+
+    // Make sure keyTypes and keyNames are the same length.
+    final int keyTypesLength = (keyTypes == null ? 0 : keyTypes.length);
+    final int keyNamesLength = (keyNames == null ? 0 : keyNames.length);
+    if (keyTypesLength != keyNamesLength)
+      throw new IllegalArgumentException
+        ("lengths of keyTypes and keyNames arrays don't match");
+
+    // Make copies of keyTypes and keyNames.
+    final byte[] keyTypesCopy = new byte[keyTypesLength];
+    if (keyTypes != null)
+      System.arraycopy(keyTypes, 0, keyTypesCopy, 0, keyTypesLength);
+    final String[] keyNamesCopy = new String[keyNamesLength];
+    if (keyNames != null)
+      System.arraycopy(keyNames, 0, keyNamesCopy, 0, keyNamesLength);
+
+    // Error-check keyTypesCopy.
+    for (int i = 0; i < keyTypesCopy.length; i++)
+      switch (keyTypesCopy[i])
+      {
+        case CyNodeDataDefinition.TYPE_BOOLEAN:
+        case CyNodeDataDefinition.TYPE_FLOATING_POINT:
+        case CyNodeDataDefinition.TYPE_INTEGER:
+        case CyNodeDataDefinition.TYPE_STRING:
+          break;
+        default:
+          throw new IllegalArgumentException
+            ("keyTypes at index " + i + " is unrecognized");
+      }
+
+    // Error-check keyNamesCopy.  Make sure that all are names are distinct.
+    // Unfortunately there are currently no contraints on the length and
+    // content of the key names.
+    final HashMap tempHash = new HashMap();
+    for (int i = 0; i < keyNamesCopy.length; i++) {
+      if (keyNamesCopy[i] == null)
+        throw new NullPointerException("keyNames at index " + i + " is null");
+      if (tempHash.put(keyNamesCopy[i], keyNamesCopy[i]) != null)
+        throw new IllegalArgumentException
+          ("duplicate in keyNames at index " + i); }
   }
 
   public Enumeration getDefinedNodeAttributes()
