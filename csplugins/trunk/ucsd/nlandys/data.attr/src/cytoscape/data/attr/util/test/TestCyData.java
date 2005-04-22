f95c8aa5efc151353d307979f7ace460f8831bb7
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-22 00:28:33 +0000

    Changing var name 'enum' to something else thx to Rowan who pointed out that 'enum' is a Java 1.5 reserved keyword.
    git-svn-id: http://chianti.ucsd.edu/svn@1631 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/test/TestCyData.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/test/TestCyData.java
@@ -36,12 +36,12 @@ public final class TestCyData
     nodeData.setNodeAttributeValue
       (nodeTwoName, nodeAttrName, new Double(0.4),
        new Object[] { "Salk", new Long(0) });
-    Enumeration enum = nodeData.getNodeAttributeKeyspan
+    Enumeration foo = nodeData.getNodeAttributeKeyspan
       (nodeOneName, nodeAttrName, new Object[] { "Ideker" });
     for (int i = 0; i < 2; i++)
-      if (!(enum.nextElement() instanceof java.lang.Long))
+      if (!(foo.nextElement() instanceof java.lang.Long))
         throw new IllegalStateException("expected Long");
-    if (enum.hasMoreElements())
+    if (foo.hasMoreElements())
       throw new IllegalStateException("did not expect more elements");
     o = nodeData.getNodeAttributeValue
       (nodeOneName, nodeAttrName, new Object[] { "Ideker", new Long(1) });
