aaf85dd8672166184be709c1539455d5b9633e68
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-22 00:32:54 +0000

    Fixing compilation errors and re-adding this to build.
    git-svn-id: http://chianti.ucsd.edu/svn@1632 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/test/TestCyData.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/test/TestCyData.java
@@ -1,9 +1,7 @@
 package cytoscape.data.attr.util.test;
 
-import cytoscape.data.attr.CyEdgeData;
-import cytoscape.data.attr.CyEdgeDataDefinition;
-import cytoscape.data.attr.CyNodeData;
-import cytoscape.data.attr.CyNodeDataDefinition;
+import cytoscape.data.attr.CyData;
+import cytoscape.data.attr.CyDataDefinition;
 import cytoscape.data.attr.util.CyDataFactory;
 
 import java.util.Enumeration;
@@ -14,49 +12,47 @@ public final class TestCyData
   public final static void main(final String[] args)
   {
     Object o = CyDataFactory.instantiateDataModel();
-    final CyNodeDataDefinition nodeDef = (CyNodeDataDefinition) o;
-    final CyNodeData nodeData = (CyNodeData) o;
-    final CyEdgeDataDefinition edgeDef = (CyEdgeDataDefinition) o;
-    final CyEdgeData edgeData = (CyEdgeData) o;
-    final String nodeAttrName = "p-values";
-    final String nodeOneName = "node1";
-    final String nodeTwoName = "node2";
-    final String nodeThreeName = "node3";
-    nodeDef.defineNodeAttribute
-      (nodeAttrName, CyNodeDataDefinition.TYPE_FLOATING_POINT,
-       new byte[] { CyNodeDataDefinition.TYPE_STRING,
-                    CyNodeDataDefinition.TYPE_INTEGER },
+    final CyDataDefinition def = (CyDataDefinition) o;
+    final CyData data = (CyData) o;
+    final String attrName = "p-values";
+    final String oneName = "node1";
+    final String twoName = "node2";
+    final String threeName = "node3";
+    def.defineAttribute
+      (attrName, CyDataDefinition.TYPE_FLOATING_POINT,
+       new byte[] { CyDataDefinition.TYPE_STRING,
+                    CyDataDefinition.TYPE_INTEGER },
        new String[] { "experiment", "multi-value offset" });
-    nodeData.setNodeAttributeValue
-      (nodeOneName, nodeAttrName, new Double(0.5),
+    data.setAttributeValue
+      (oneName, attrName, new Double(0.5),
        new Object[] { "Ideker", new Long(0) });
-    nodeData.setNodeAttributeValue
-      (nodeOneName, nodeAttrName, new Double(0.6),
+    data.setAttributeValue
+      (oneName, attrName, new Double(0.6),
        new Object[] { "Ideker", new Long(1) });
-    nodeData.setNodeAttributeValue
-      (nodeTwoName, nodeAttrName, new Double(0.4),
+    data.setAttributeValue
+      (twoName, attrName, new Double(0.4),
        new Object[] { "Salk", new Long(0) });
-    Enumeration foo = nodeData.getNodeAttributeKeyspan
-      (nodeOneName, nodeAttrName, new Object[] { "Ideker" });
+    Enumeration foo = data.getAttributeKeyspan
+      (oneName, attrName, new Object[] { "Ideker" });
     for (int i = 0; i < 2; i++)
       if (!(foo.nextElement() instanceof java.lang.Long))
         throw new IllegalStateException("expected Long");
     if (foo.hasMoreElements())
       throw new IllegalStateException("did not expect more elements");
-    o = nodeData.getNodeAttributeValue
-      (nodeOneName, nodeAttrName, new Object[] { "Ideker", new Long(1) });
+    o = data.getAttributeValue
+      (oneName, attrName, new Object[] { "Ideker", new Long(1) });
     if (!(((Double) o).doubleValue() == 0.6d))
       throw new IllegalStateException("expected 0.6");
-    o = nodeData.getNodeAttributeValue
-      (nodeTwoName, nodeAttrName, new Object[] { "Salk", new Long(0) });
+    o = data.getAttributeValue
+      (twoName, attrName, new Object[] { "Salk", new Long(0) });
     if (!(((Double) o).doubleValue() == 0.4d))
       throw new IllegalStateException("expected 0.4");
-    o = nodeData.getNodeAttributeValue
-      ("noNode", nodeAttrName, new Object[] { "Howdy", new Long(0) });
+    o = data.getAttributeValue
+      ("noNode", attrName, new Object[] { "Howdy", new Long(0) });
     if (o != null)
       throw new IllegalStateException("expected null");
-    o = nodeData.getNodeAttributeValue
-      (nodeTwoName, nodeAttrName, new Object[] { "Salk", new Long(1) });
+    o = data.getAttributeValue
+      (twoName, attrName, new Object[] { "Salk", new Long(1) });
     if (o != null)
       throw new IllegalStateException("expected null");
   }
