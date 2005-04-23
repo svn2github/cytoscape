7653409e5f6a280031c4946f46bfc491ea4378a6
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-23 01:30:06 +0000

    Adding Javadoc, a method, and I don't know what else.
    git-svn-id: http://chianti.ucsd.edu/svn@1656 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/CyDataHelpers.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/util/CyDataHelpers.java
@@ -85,10 +85,56 @@ public final class CyDataHelpers
   }
 
   /**
+   * Convenience method for discovering attribute values along a specified
+   * key prefix.
+   * @param objectKey the object whose attribute values to return.
+   * @param attributeName the attribute definition in which to find attribute
+   *   values.
+   * @param keyPrefix an array of length less than or equal to the
+   *   dimensionality of key space of attributeName; entry at index i contains
+   *   a "representative" from dimension i + 1 of the key space of
+   *   attributeName; keyPrefix may be either null or the empty array, in
+   *   which case all attribute values bound to objectKey in attributeName will
+   *   be returned; if keyPrefix is not empty, all values having key
+   *   sequences whose beginning matches the specified prefix will be returned.
+   * @param cyData the data repository to use to dig for attribute values.
+   * @param cyDataDef the data definition registry to use to find out about
+   *   the dimensionality of attributeName.
+   * @return an enumeration of all bound values on objectKey in attributeName
+   *   along key space prefix keyPrefix, with duplicate values included; the
+   *   returned enumeration is never null; elements in the returned
+   *   enumeration are ordered arbitrarily.
+   * @exception IllegalStateException if attributeName is not an existing
+   *   attribute definition in cyData and cyDataDef.
+   * @exception NullPointerException if any one of the input parameters except
+   *   for keyPrefix is null, or if keyPrefix is of positive length and any one
+   *   of its entries is null.
+   * @exception ClassCastException if keyPrefix is of positive length and any
+   *   one of its entries does not match the type of object specified
+   *   by corresponding dimension type in attributeName's definition.
+   * @exception IllegalArgumentException if keyPrefix's length is
+   *   greater than the dimensionality of attributeName's key space.
+   */
+  public static CountedEnumeration getAllAttributeValuesAlongPrefix(
+                                              final String objectKey,
+                                              final String attributeName,
+                                              final Object[] keyPrefix,
+                                              final CyData cyData,
+                                              final CyDataDefinition cyDataDef)
+  {
+    throw new IllegalStateException("sorry not yet implemented");
+  }
+
+  /**
+   * Convenience method for deleting attribute values along a specified key
+   * prefix.<p>
+   * TIP: To find out exactly what is deleted by this method, add a
+   * CyDataListener to cyData.
    * @param objectKey the object whose attribute values to delete.
    * @param attributeName the attribute definition in which to delete
    *   attribute values.
-   * @param keyPrefix an array of length strictly less than the dimensionality
+   * @param keyPrefix an array of length less than or equal to the
+   *   dimensionality
    *   of key space of attributeName; entry at index i contains a
    *   "representative" from dimension i + 1 of the key space of attributeName;
    *   keyPrefix may be either null or the empty array, in which case all
@@ -101,8 +147,16 @@ public final class CyDataHelpers
    * @return true if and only if at least one attribute value has been deleted.
    * @exception IllegalStateException if attributeName is not an existing
    *   attribute definition in cyData and cyDataDef.
+   * @exception NullPointerException if any one of the input parameters except
+   *   for keyPrefix is null, or if keyPrefix is of positive length and any one
+   *   of its entries is null.
+   * @exception ClassCastException if keyPrefix is [not null and] of positive
+   *   length and any one of its entries does not match the type of object
+   *   specified by corresponding dimension type in attributeName's definition.
+   * @exception IllegalArgumentException if keyPrefix's length is 
+   *   greater than the dimensionality of attributeName's key space.
    */
-  public static boolean removeAttributeKeyspan(
+  public static boolean removeAllAttributeValuesAlongPrefix(
                                              final String objectKey,
                                              final String attributeName,
                                              final Object[] keyPrefix,
