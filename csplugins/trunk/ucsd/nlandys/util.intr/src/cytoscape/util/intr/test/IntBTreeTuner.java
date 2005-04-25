a913fa26ca657304c403d8e673984dbd61ef7cf9
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-25 21:40:29 +0000

    Starting a new test class which will be used to determine the best branching factor.
    git-svn-id: http://chianti.ucsd.edu/svn@1696 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/test/IntBTreeTuner.java
@@ -0,0 +1,38 @@
+package cytoscape.util.intr.test;
+
+import cytoscape.util.intr.IntBTree;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IntBTreeTuner
+{
+
+  public static void main(String[] args) throws IOException
+  {
+    int N = Integer.parseInt(args[0]);
+    int[] elements = new int[N];
+    InputStream in = System.in;
+    byte[] buff = new byte[4];
+    int inx = 0; int off = 0; int read;
+    while (inx < N && (read = in.read(buff, off, buff.length - off)) > 0) {
+      off += read;
+      if (off < buff.length) continue;
+      else off = 0;
+      elements[inx++] = Math.abs(assembleInt(buff)) % N; }
+    if (inx < N) throw new IOException("premature end of input");
+
+    for (int i = 0; i < elements.length; i++)
+      System.out.println(elements[i]);
+  }
+
+  private static final int assembleInt(byte[] fourConsecutiveBytes)
+  {
+    int firstByte = (((int) fourConsecutiveBytes[0]) & 0x000000ff) << 24;
+    int secondByte = (((int) fourConsecutiveBytes[1]) & 0x000000ff) << 16;
+    int thirdByte = (((int) fourConsecutiveBytes[2]) & 0x000000ff) << 8;
+    int fourthByte = (((int) fourConsecutiveBytes[3]) & 0x000000ff) << 0;
+    return firstByte | secondByte | thirdByte | fourthByte;
+  }
+
+}
