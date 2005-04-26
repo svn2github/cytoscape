798cd4cc10ccc637163ef39bebd219bb84ffd693
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-26 03:02:11 +0000

    Adding Javadocs.
    git-svn-id: http://chianti.ucsd.edu/svn@1703 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CountedEnumeration.java
+++ csplugins/trunk/ucsd/nlandys/data.attr/src/cytoscape/data/attr/CountedEnumeration.java
@@ -2,9 +2,17 @@ package cytoscape.data.attr;
 
 import java.util.Enumeration;
 
+/**
+ * A java.util.Enumeration with knowledge of how many elements are in the
+ * enumeration.
+ */
 public interface CountedEnumeration extends Enumeration
 {
 
+  /**
+   * Returns a non-negative integer I such that nextElement() will successfully
+   * return a value no more and no less than I times.
+   */
   public int numElementsRemaining();
 
 }
