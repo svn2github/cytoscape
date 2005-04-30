4a20688583c2b6cfe7796a224634b23546d5628a
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-04-30 02:29:06 +0000

    Adding an IntBTree test for query time.
    git-svn-id: http://chianti.ucsd.edu/svn@1775 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/util.intr/src/cytoscape/util/intr/test/IntBTreeQueryTuner.java
@@ -0,0 +1,50 @@
+package cytoscape.util.intr.test;
+
+import cytoscape.util.intr.IntBTree;
+import cytoscape.util.intr.IntEnumerator;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IntBTreeQueryTuner
+{
+
+  public static void main(String[] args) throws IOException
+  {
+    int branches = Integer.parseInt(args[0]);
+    int N = Integer.parseInt(args[1]);
+    int[] elements = new int[N];
+    InputStream in = System.in;
+    byte[] buff = new byte[4];
+    int inx = 0; int off = 0; int read;
+    while (inx < N && (read = in.read(buff, off, buff.length - off)) > 0) {
+      off += read;
+      if (off < buff.length) continue;
+      else off = 0;
+      elements[inx++] = Math.abs(assembleInt(buff)) % N; }
+    if (inx < N) throw new IOException("premature end of input");
+
+    IntBTree tree = new IntBTree(branches);
+    for (int i = 0; i < elements.length; i++) tree.insert(elements[i]);
+    int querySpan = N / 3;
+    int maxStart = querySpan * 2;
+    boolean reverseOrder = false;
+    long timeBegin = System.currentTimeMillis();
+    for (int i = 0; i < maxStart; i++) {
+      IntEnumerator iter = tree.searchRange(i, i + querySpan, reverseOrder);
+      while (iter.numRemaining() > 0) iter.nextInt();
+      reverseOrder = !reverseOrder; }
+    long timeEnd = System.currentTimeMillis();
+    System.out.println((timeEnd - timeBegin) + " milliseconds");
+  }
+
+  private static int assembleInt(byte[] fourConsecutiveBytes)
+  {
+    int firstByte = (((int) fourConsecutiveBytes[0]) & 0x000000ff) << 24;
+    int secondByte = (((int) fourConsecutiveBytes[1]) & 0x000000ff) << 16;
+    int thirdByte = (((int) fourConsecutiveBytes[2]) & 0x000000ff) << 8;
+    int fourthByte = (((int) fourConsecutiveBytes[3]) & 0x000000ff) << 0;
+    return firstByte | secondByte | thirdByte | fourthByte;
+  }
+
+}
