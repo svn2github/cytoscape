4a80a9949fc645a5c44feabe64bf9fa577252d1b
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-27 16:16:43 +0000

    Adding some more tests.
    git-svn-id: http://chianti.ucsd.edu/svn@1928 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
+++ csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
@@ -12,6 +12,15 @@ public class BasicQuietRTreeTest
     tree.insert(1, 2.0, 2.0, 3.0, 3.0);
     tree.insert(2, 0.5, 1.0, 1.5, 2.0);
     // Still before any split.
+    double[] extentsArr = new double[4];
+    for (int i = 0; i < 3; i++)
+      if (!tree.exists(i, extentsArr, 0))
+        throw new IllegalStateException("entry " + i + " does not exist");
+    if (tree.exists(3, extentsArr, 0))
+      throw new IllegalStateException("entry 3 exits");
+    if (extentsArr[0] != 0.5 || extentsArr[1] != 1.0 ||
+        extentsArr[2] != 1.5 || extentsArr[3] != 2.0)
+      throw new IllegalStateException("entry's extents don't match");
   }
 
 }
