a06680bd4868531d4c28b799b90cd83125809878
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-27 16:16:46 +0000

    Adding simple root split test.  There appears to be a bug.
    git-svn-id: http://chianti.ucsd.edu/svn@1931 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
+++ csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
@@ -74,6 +74,69 @@ public class BasicQuietRTreeTest
     } // END ROOT LEAF TEST.
 
     tree.insert(3, 2.5, 0.5, 3.5, 1.5);
+
+    { // BEGIN SIMPLE ROOT SPLIT TEST: Minimum # entries with a split.
+      double[] extentsArr = new double[4];
+      for (int i = 0; i < 4; i++)
+        if (!tree.exists(i, extentsArr, 0))
+          throw new IllegalStateException("entry " + i + " does not exist");
+      if (tree.exists(4, extentsArr, 0))
+        throw new IllegalStateException("entry 4 exists");
+      if (extentsArr[0] != 2.5 || extentsArr[1] != 0.5 ||
+          extentsArr[2] != 3.5 || extentsArr[3] != 1.5)
+        throw new IllegalStateException("entry's extents incorrect");
+      IntEnumerator iter = tree.queryOverlap
+        (Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY,
+         Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, extentsArr, 0);
+      if (iter.numRemaining() != 4)
+        throw new IllegalStateException("expected query to generate 4 hits");
+      IntBTree cache = new IntBTree();
+      for (int i = 0; i < 4; i++) cache.insert(i);
+      int foo = 0;
+      while (iter.numRemaining() > 0) { cache.delete(iter.nextInt()); foo++; }
+      if (foo != 4) throw new IllegalStateException
+                      ("iter claimed it had 3 elements but really didn't");
+      if (cache.size() != 0) throw new IllegalStateException
+                               ("iter returned wrong objKeys");
+      if (extentsArr[0] != 0.0 || extentsArr[1] != 0.0 ||
+          extentsArr[2] != 3.5 || extentsArr[3] != 3.0)
+        throw new IllegalStateException("extents from query wrong");
+      iter = tree.queryOverlap(2.0, 0.5, 2.2, 1.9, extentsArr, 0);
+      if (iter.numRemaining() != 0)
+        throw new IllegalStateException("expected query to generate 0 hits");
+      if (extentsArr[0] != Double.POSITIVE_INFINITY ||
+          extentsArr[1] != Double.POSITIVE_INFINITY ||
+          extentsArr[2] != Double.NEGATIVE_INFINITY ||
+          extentsArr[3] != Double.NEGATIVE_INFINITY)
+        throw new IllegalStateException
+          ("query extents - expected inverted infinite");
+      iter = tree.queryOverlap
+        (Double.NEGATIVE_INFINITY, 1.1, Double.POSITIVE_INFINITY, 1.2,
+         extentsArr, 0);
+      if (iter.numRemaining() != 2)
+        throw new IllegalStateException("expected query to generate 2 hits");
+      cache.insert(2); cache.insert(3); foo = 0;
+      while (iter.numRemaining() > 0) { cache.delete(iter.nextInt()); foo++; }
+      if (foo != 2) throw new IllegalStateException
+                      ("iter claimed it had 2 elements but really didn't");
+      if (cache.size() != 0) throw new IllegalStateException
+                               ("query returned wrong objKeys");
+      if (extentsArr[0] != 0.5 || extentsArr[1] != 3.5 ||
+          extentsArr[2] != 0.0 || extentsArr[3] != 3.0)
+        throw new IllegalStateException("extents from query wrong");
+      iter = tree.queryOverlap(1.0, 1.0, 1.0, 1.0, extentsArr, 0);
+      if (iter.numRemaining() != 2)
+        throw new IllegalStateException("expected query to generate 2 hits");
+      cache.insert(0); cache.insert(2); foo = 0;
+      while (iter.numRemaining() > 0) { cache.delete(iter.nextInt()); foo++; }
+      if (foo != 2) throw new IllegalStateException
+                      ("iter claimed it had 2 elements but really didn't");
+      if (cache.size() != 0) throw new IllegalStateException
+                               ("query returned wrong objKeys");
+      if (extentsArr[0] != 0.0 || extentsArr[1] != 0.0 ||
+          extentsArr[2] != 1.5 || extentsArr[3] != 2.0)
+        throw new IllegalStateException("extents from query wrong");
+    } // END SIMPLE ROOT SPLIT TEST.
   }
 
 }
