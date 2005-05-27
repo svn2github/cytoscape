f025928172aad3a01da45630daf696a62650d530
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-27 16:16:42 +0000

    Now successfully inserting three entries in my test.  I had to fix a bug - m_entryMap was not instantiated.
    git-svn-id: http://chianti.ucsd.edu/svn@1927 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
+++ csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
@@ -8,6 +8,10 @@ public class BasicQuietRTreeTest
   public static void main(String[] args)
   {
     RTree tree = new RTree(3);
+    tree.insert(0, 0.0, 0.0, 1.0, 1.0);
+    tree.insert(1, 2.0, 2.0, 3.0, 3.0);
+    tree.insert(2, 0.5, 1.0, 1.5, 2.0);
+    // Still before any split.
   }
 
 }
