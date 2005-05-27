3438bed170edc15df057b9ec398963aac077c150
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-27 16:16:41 +0000

    Now instantiating an RTree in my test.
    git-svn-id: http://chianti.ucsd.edu/svn@1926 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
+++ csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
@@ -1,10 +1,13 @@
 package cytoscape.geom.rtree.test;
 
+import cytoscape.geom.rtree.RTree;
+
 public class BasicQuietRTreeTest
 {
 
   public static void main(String[] args)
   {
+    RTree tree = new RTree(3);
   }
 
 }
