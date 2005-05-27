f135bf134452d0d1947f2d84e53f5dbc7f4b1f12
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-27 16:16:44 +0000

    Implemented the next series of tests.  No bugs found.
    git-svn-id: http://chianti.ucsd.edu/svn@1929 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
+++ csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/BasicQuietRTreeTest.java
@@ -1,6 +1,8 @@
 package cytoscape.geom.rtree.test;
 
 import cytoscape.geom.rtree.RTree;
+import cytoscape.util.intr.IntBTree;
+import cytoscape.util.intr.IntEnumerator;
 
 public class BasicQuietRTreeTest
 {
@@ -8,19 +10,69 @@ public class BasicQuietRTreeTest
   public static void main(String[] args)
   {
     RTree tree = new RTree(3);
+
+    { // BEGIN EMPTY TREE TESTS: We run our first tests when this tree empty.
+      double[] extentsArr = new double[4];
+      IntEnumerator iter = tree.queryOverlap
+        (Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY,
+         Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, extentsArr, 0);
+      if (iter.numRemaining() != 0)
+        throw new IllegalStateException
+          ("did not expect query to generate results");
+      if (extentsArr[0] != Double.POSITIVE_INFINITY ||
+          extentsArr[1] != Double.POSITIVE_INFINITY ||
+          extentsArr[2] != Double.NEGATIVE_INFINITY ||
+          extentsArr[3] != Double.NEGATIVE_INFINITY)
+        throw new IllegalStateException
+          ("expected query to return inverted infinite extents");
+      if (tree.exists(0, extentsArr, 0))
+        throw new IllegalStateException("did not expect there to be an entry");
+    } // END EMPTY TREE TESTS.
+
     tree.insert(0, 0.0, 0.0, 1.0, 1.0);
     tree.insert(1, 2.0, 2.0, 3.0, 3.0);
     tree.insert(2, 0.5, 1.0, 1.5, 2.0);
-    // Still before any split.
-    double[] extentsArr = new double[4];
-    for (int i = 0; i < 3; i++)
-      if (!tree.exists(i, extentsArr, 0))
-        throw new IllegalStateException("entry " + i + " does not exist");
-    if (tree.exists(3, extentsArr, 0))
-      throw new IllegalStateException("entry 3 exits");
-    if (extentsArr[0] != 0.5 || extentsArr[1] != 1.0 ||
-        extentsArr[2] != 1.5 || extentsArr[3] != 2.0)
-      throw new IllegalStateException("entry's extents don't match");
+
+    { // BEGIN ROOT LEAF TEST: Still before any split.
+      double[] extentsArr = new double[5];
+      for (int i = 0; i < 3; i++)
+        if (!tree.exists(i, extentsArr, 0))
+          throw new IllegalStateException("entry " + i + " does not exist");
+      if (tree.exists(3, extentsArr, 0))
+        throw new IllegalStateException("entry 3 exits");
+      if (extentsArr[0] != 0.5 || extentsArr[1] != 1.0 ||
+          extentsArr[2] != 1.5 || extentsArr[3] != 2.0)
+        throw new IllegalStateException("entry's extents don't match");
+      IntEnumerator iter = tree.queryOverlap
+        (Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY,
+         Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, extentsArr, 0);
+      if (iter.numRemaining() != 3)
+        throw new IllegalStateException("expected query to generate 3 hits");
+      IntBTree cache = new IntBTree();
+      for (int i = 0; i < 3; i++) cache.insert(i);
+      int foo = 0;
+      while (iter.numRemaining() > 0) { cache.delete(iter.nextInt()); foo++; }
+      if (foo != 3) throw new IllegalStateException
+                      ("iter claimed it had 3 elements but really didn't");
+      if (cache.size() != 0) throw new IllegalStateException
+                               ("iter returned wrong objKeys");
+      if (extentsArr[0] != 0.0 || extentsArr[1] != 0.0 ||
+          extentsArr[2] != 3.0 || extentsArr[3] != 3.0)
+        throw new IllegalStateException("extents from query wrong");
+      iter = tree.queryOverlap(1.25, 2.0, 2.1, 3.3, extentsArr, 1);
+      if (iter.numRemaining() != 2)
+        throw new IllegalStateException("exptected query to return 2 hits");
+      cache.insert(1); cache.insert(2); foo = 0;
+      while (iter.numRemaining() > 0) { cache.delete(iter.nextInt()); foo++; }
+      if (foo != 2) throw new IllegalStateException
+                      ("iter claimed it had 2 elements but really didn't");
+      if (cache.size() != 0) throw new IllegalStateException
+                               ("iter returned wrong objKeys");
+      if (extentsArr[1] != 0.5 || extentsArr[2] != 1.0 ||
+          extentsArr[3] != 3.0 || extentsArr[4] != 3.0)
+        throw new IllegalStateException("extents from query wrong");
+    } // END ROOT LEAF TEST.
+                 
   }
 
 }
