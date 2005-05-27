a05df922cc3c11e2c2563ed0b8d9874f0c4e22ac
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-27 16:17:50 +0000

    Starting to implement this test.
    git-svn-id: http://chianti.ucsd.edu/svn@1968 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/RTreeQueryPerformance.java
+++ csplugins/trunk/ucsd/nlandys/rtree/src/cytoscape/geom/rtree/test/RTreeQueryPerformance.java
@@ -1,12 +1,56 @@
 package cytoscape.geom.rtree.test;
 
 import cytoscape.geom.rtree.RTree;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RTreeQueryPerformance
 {
 
-  public static void main(String[] args)
+  /**
+   * For given N, creates N rectangles whose centers are in the space
+   * [0,1] X [0,1].  Each rectangle has width and height less than
+   * approximately 1/sqrt(N).  The location of the centers of the rectangles
+   * and the choice of width and height for each rectangle is determined
+   * be the input stream, which in most cases will be a randomly generated
+   * stream of bytes.  Please see the actual code for an explanation of
+   * how the input stream of bytes is converted into the rectangle
+   * information.
+   */
+  public static void main(String[] args) throws Exception
   {
+    int N = Integer.parseInt(args[0]);
+    int[] randomData = new int[N * 4];
+    InputStream in = System.in;
+    byte[] buff = new byte[4];
+    int inx = 0;
+    int off = 0;
+    int read;
+    while (inx < randomData.length &&
+           (read = in.read(buff, off, buff.length - off)) > 0) {
+      off += read;
+      if (off < buff.length) continue;
+      else off = 0;
+      randomData[inx++] = assembleInt(buff); }
+    if (inx < N) throw new IOException("premature end of input");
+
+    for (int i = 0; i < randomData.length; i++)
+      System.out.println(randomData[i]);
+
+//     double maxDim = 1.0d / Math.sqrt((double) N);
+//     final double[] xMins = new int[N];
+//     final double[] yMins = new int[N];
+//     final double[] xMaxs = new int[N];
+//     final double[] yMaxs = new int[N];
+  }
+
+  private static final int assembleInt(byte[] fourConsecutiveBytes)
+  {
+    int firstByte = (((int) fourConsecutiveBytes[0]) & 0x000000ff) << 24;
+    int secondByte = (((int) fourConsecutiveBytes[1]) & 0x000000ff) << 16;
+    int thirdByte = (((int) fourConsecutiveBytes[2]) & 0x000000ff) << 8;
+    int fourthByte = (((int) fourConsecutiveBytes[3]) & 0x000000ff) << 0;
+    return firstByte | secondByte | thirdByte | fourthByte;
   }
 
 }
