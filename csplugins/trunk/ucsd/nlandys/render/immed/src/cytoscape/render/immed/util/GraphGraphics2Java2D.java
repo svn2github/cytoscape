8a5da9df95d9d6ad8df26847c675226d46972c08
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-31 23:00:13 +0000

    Adding some substance so that build would work.
    git-svn-id: http://chianti.ucsd.edu/svn@2031 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/render/immed/src/cytoscape/render/immed/util/GraphGraphics2Java2D.java
@@ -0,0 +1,20 @@
+package cytoscape.render.immed.util;
+
+import cytoscape.render.immed.GraphGraphics;
+
+final class GraphGraphics2Java2D implements GraphGraphics
+{
+
+  public final void extents(double[] extentsArr) {}
+
+  public final void clear() {}
+
+  public final void hiDrawNode(byte shape, double width, double height,
+                               double xPos, double yPos, int fillColorRGB,
+                               byte borderType, double borderWidth,
+                               int borderColorRGB) {}
+
+  public final void loDrawNode(double width, double height, double xPos,
+                               double yPos, int fillColorRGB) {}
+
+}
