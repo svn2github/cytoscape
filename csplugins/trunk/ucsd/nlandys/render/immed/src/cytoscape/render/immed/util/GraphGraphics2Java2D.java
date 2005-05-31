586f533b6127746ca809a2a480750fda889f696e
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-05-31 23:36:45 +0000

    One last commit before continuing this from home.
    git-svn-id: http://chianti.ucsd.edu/svn@2032 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/immed/src/cytoscape/render/immed/util/GraphGraphics2Java2D.java
+++ csplugins/trunk/ucsd/nlandys/render/immed/src/cytoscape/render/immed/util/GraphGraphics2Java2D.java
@@ -1,9 +1,14 @@
 package cytoscape.render.immed.util;
 
 import cytoscape.render.immed.GraphGraphics;
+import java.awt.Graphics2D;
 
 final class GraphGraphics2Java2D implements GraphGraphics
 {
+
+  GraphGraphics2Java2D(Graphics2D graphics)
+  {
+  }
 
   public final void extents(double[] extentsArr) {}
 
