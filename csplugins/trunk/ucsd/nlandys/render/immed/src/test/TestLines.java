8dc191ced5a3d970d913d2e32f964bb05078d90b
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-06-01 03:25:54 +0000

    Now rendering lines, revealing flaw in line drawing algorithm.
    git-svn-id: http://chianti.ucsd.edu/svn@2034 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/immed/src/test/TestLines.java
+++ csplugins/trunk/ucsd/nlandys/render/immed/src/test/TestLines.java
@@ -1,9 +1,11 @@
 package test;
 
+import java.awt.Color;
 import java.awt.Event;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
 
 public final class TestLines
 {
@@ -12,7 +14,17 @@ public final class TestLines
   {
     final Frame f = new Frame() {
         public final void paint(Graphics g) {
-          g.fillOval(50, 100, 70, 50); }
+          Graphics2D g2d = (Graphics2D) g;
+          final double x1 = 22.388;
+          final double y1 = 147.847;
+          final double x2 = 162.904;
+          final double y2 = 72.491;
+          g2d.setPaint(Color.red);
+          g2d.draw(new Line2D.Double(x1, y1, x2, y2));
+          g2d.setPaint(Color.blue);
+          g2d.draw(new Line2D.Double(x1 + (x2 - x1) * 0.33333,
+                                     y1 + (y2 - y1) * 0.33333,
+                                     x2, y2)); }
         public boolean handleEvent(Event evt) {
           if (evt.id == Event.WINDOW_DESTROY) System.exit(0);
           return super.handleEvent(evt); } };
