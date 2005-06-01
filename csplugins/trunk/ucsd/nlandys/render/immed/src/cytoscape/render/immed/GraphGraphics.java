9dd50f64aff36c3cc9410daddd98224a85b9a12c
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-06-01 21:58:13 +0000

    Cleaning things up.  Ripping it up.
    git-svn-id: http://chianti.ucsd.edu/svn@2043 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/immed/src/cytoscape/render/immed/GraphGraphics.java
+++ csplugins/trunk/ucsd/nlandys/render/immed/src/cytoscape/render/immed/GraphGraphics.java
@@ -1,5 +1,6 @@
 package cytoscape.render.immed;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -25,72 +26,55 @@ public final class GraphGraphics
   public static final byte BORDER_SOLID = 2;
 
   private static final boolean s_threadDebug = true;
+  private static final Color s_transparent = new Color(0, 0, 0, 0);
 
   /**
    * The image that was passed into the constructor.
    */
   public final Image image;
 
-  private Graphics2D g2d = null;
+  private Graphics2D g2d;
 
   /**
-   * This constructor does not necessarily need to be called from the
-   * AWT event handling thread.
+   * This constructor needs to be called from the AWT event handling thread.
    * @param img an off-screen image (an image gotten via the call
    *   java.awt.Component.createImage(int, int)).
-   */
-  public GraphGraphics(final Image image)
-  {
-    if (img == null) throw new NullPointerException("image is null");
-    this.image = image;
-  }
-
-  /**
-   * This must be called to let the renderer know when drawing operations
-   * are about to be performed.  The sequence of calls will look like this:
-   * <blockquote>
-   * startFrame()<br />
-   * [rendering operation 1]<br />
-   * [rendering operation 2]<br />
-   * ...<br />
-   * finishFrame()<br />
-   * </blockquote>
-   * Thereafter, startFrame() may be called again to start another frame.<p>
-   * This method must be called from the AWT event handling thread.
    * @exception IllegalThreadStateException if the calling thread isn't the
    *   AWT event handling thread.
    */
-  public void startFrame()
+  public GraphGraphics(final Image image)
   {
-    if (s_threadDebug && !EventQueue.isDispatchThread())
-      throw new IllegalThreadStateException
-        ("calling thread is not AWT event dispatcher");
-    if (g2d != null) throw new IllegalStateException
-                       ("previous frame was not finished");
-    g2d = (Graphics2D) image.getGraphics();
+    this.image = image;
+    this.clear();
   }
 
   /**
-   * Tells the renderer when rendering operations for one frame are done.
-   * The renderer then disposes of resources used during the rendering
-   * process.<p>
-   * This method must be called from the AWT event handling thread.
-   */
-  public void finishFrame();
-
-  /**
    * Clears image area and makes it transparent.  This is a rendering
-   * operation.<p>
+   * operation.  It is healthy to call this method right before starting
+   * to render a new picture.  Don't try to be clever in not calling this
+   * method.<p>
    * This method must be called from the AWT event handling thread.
    */
-  public void clear();
+  public void clear()
+  {
+    if (s_threadDebug && !EventQueue.isDispatchThread())
+      throw new IllegalStateException
+        ("calling thread is not AWT event dispatcher");
+    g2d = (Graphics2D) image.getGraphics();
+    g2d.setBackground(s_transparent);
+    g2d.clearRect(0, 0, image.getWidth(null), image.getHeight(null));
+  }
 
-  public void hiDrawNode(byte shape, double width, double height,
-                         double xPos, double yPos, int fillColorRGB,
-                         byte borderType, double borderWidth,
-                         int borderColorRGB);
+  public void drawNodeFull(byte shape, double width, double height,
+                           double xPos, double yPos, int fillColorRGB,
+                           byte borderType, double borderWidth,
+                           int borderColorRGB)
+  {
+  }
 
-  public void loDrawNode(double width, double height, double xPos, double yPos,
-                         int fillColorRGB);
+  public void drawNodeLow(double width, double height, double xPos,
+                          double yPos, int fillColorRGB)
+  {
+  }
 
 }
