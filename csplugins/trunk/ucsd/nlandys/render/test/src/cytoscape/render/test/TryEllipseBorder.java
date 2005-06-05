728ca53d18d40546e671b75c559e678f9bd7b870
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-06-05 04:13:12 +0000

    The test is now complete.
    git-svn-id: http://chianti.ucsd.edu/svn@2113 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryEllipseBorder.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryEllipseBorder.java
@@ -19,17 +19,21 @@ public final class TryEllipseBorder
         public final void paint(Graphics g) {
           Graphics2D g2d = (Graphics2D) g;
           AffineTransform xform = new AffineTransform();
-          xform.setToScale(1000.0d, 1000.0d);
+          final double scaleFactor = 1000.0d; // If you set this to 100.0, the
+                                              // ellipse will become smooth.
+          xform.setToScale(scaleFactor, scaleFactor);
           g2d.transform(xform);
           g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                                RenderingHints.VALUE_ANTIALIAS_ON);
-          g2d.setStroke(new BasicStroke(0.002f));
+          g2d.setStroke(new BasicStroke((float) (2.0d / scaleFactor)));
           g2d.setPaint(Color.red);
-          g2d.draw(new Ellipse2D.Double(0.0145d, 0.0384d, 0.1901d, 0.0597d)); }
+          g2d.draw(new Ellipse2D.Double
+                   (14.5d / scaleFactor, 38.4d / scaleFactor,
+                    190.1d / scaleFactor, 59.7d / scaleFactor)); }
         public boolean handleEvent(Event evt) {
           if (evt.id == Event.WINDOW_DESTROY) System.exit(0);
           return super.handleEvent(evt); } };
-    f.resize(300, 200);
+    f.resize(230, 120);
     f.show();
   }
 
