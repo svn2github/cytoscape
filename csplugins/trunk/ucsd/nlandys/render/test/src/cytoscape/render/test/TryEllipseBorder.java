a64c69d67e3ec13caa4f2e5e5e20b10260b67c0b
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-06-05 04:03:04 +0000

    Now using an AffineTransform.  I will shortly reveal the bug.
    git-svn-id: http://chianti.ucsd.edu/svn@2110 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryEllipseBorder.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryEllipseBorder.java
@@ -7,6 +7,7 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 
 public final class TryEllipseBorder
@@ -17,11 +18,14 @@ public final class TryEllipseBorder
     final Frame f = new Frame() {
         public final void paint(Graphics g) {
           Graphics2D g2d = (Graphics2D) g;
+          AffineTransform xform = new AffineTransform();
+          xform.setToScale(10.0d, 10.0d);
+          g2d.transform(xform);
           g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                                RenderingHints.VALUE_ANTIALIAS_ON);
-          g2d.setStroke(new BasicStroke(0.5f));
+          g2d.setStroke(new BasicStroke(0.2f));
           g2d.setPaint(Color.red);
-          g2d.draw(new Ellipse2D.Double(14.5, 38.4, 190.1, 139.7)); }
+          g2d.draw(new Ellipse2D.Double(1.45, 3.84, 19.01, 13.97)); }
         public boolean handleEvent(Event evt) {
           if (evt.id == Event.WINDOW_DESTROY) System.exit(0);
           return super.handleEvent(evt); } };
