89bb88db5b55bec9e94debd52da7c6cab38188e6
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-06-06 18:35:30 +0000

    Changing public API to use float for specifying extents.  Removing cruft.
    git-svn-id: http://chianti.ucsd.edu/svn@2130 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/immed/src/cytoscape/render/immed/GraphGraphics.java
+++ csplugins/trunk/ucsd/nlandys/render/immed/src/cytoscape/render/immed/GraphGraphics.java
@@ -1,6 +1,5 @@
 package cytoscape.render.immed;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics2D;
@@ -28,9 +27,6 @@ public final class GraphGraphics
   public static final byte SHAPE_RECTANGLE = 5;
   public static final byte SHAPE_TRIANGLE = 6;
 
-  public static final byte BORDER_DASHED = 0;
-  public static final byte BORDER_SOLID = 1;
-
   /**
    * The image that was passed into the constructor.
    */
@@ -39,11 +35,10 @@ public final class GraphGraphics
   private final Color m_bgColor;
   private final boolean m_antialias;
   private final boolean m_debug;
-  private final Rectangle2D.Double m_rect2d;
-  private final Ellipse2D.Double m_ellp2d;
+  private final Rectangle2D.Float m_rect2d;
+  private final Ellipse2D.Float m_ellp2d;
   private final GeneralPath m_poly2d;
   private Graphics2D m_g2d;
-  private float m_currStrokeWidth;
   private AffineTransform m_currXform; // Not sure that we will need this.
 
   /**
@@ -63,8 +58,8 @@ public final class GraphGraphics
     m_bgColor = bgColor;
     m_antialias = antialias;
     m_debug = debug;
-    m_rect2d = new Rectangle2D.Double();
-    m_ellp2d = new Ellipse2D.Double();
+    m_rect2d = new Rectangle2D.Float();
+    m_ellp2d = new Ellipse2D.Float();
     m_poly2d = new GeneralPath();
     clear(0.0d, 0.0d, 1.0d);
   }
@@ -111,12 +106,6 @@ public final class GraphGraphics
       m_g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                              RenderingHints.VALUE_ANTIALIAS_ON);
 
-    // Set stroke.
-    m_currStrokeWidth = 1.0f;
-    final BasicStroke stroke = new BasicStroke
-      (m_currStrokeWidth, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER);
-    m_g2d.setStroke(stroke);
-
     // Set transform.  This is an infrequently used method so don't optimize.
     final AffineTransform translationPreScale = new AffineTransform();
     translationPreScale.setToTranslation(-xCenter, -yCenter);
@@ -136,11 +125,9 @@ public final class GraphGraphics
 
   /**
    * The xMin, yMin, xMax, and yMax parameters specify the extents of the
-   * node shape (in the node coordinate system), but not including the border
-   * width.  If a node's border is nonzero, then that node's extents will
-   * be slightly larger than specified.  The ``fill'' part of the node,
-   * however, will remain exactly as specified.
-   * @param borderWidth the border width, in node coordinate system; if
+   * node shape (in the node coordinate system), including the border
+   * width.
+   * @param borderWidth the border width, in node coordinate space; if
    *   this value is zero, the rendering engine skips over the process of
    *   rendering the border, which gives a significant performance boost.
    * @exception IllegalThreadStateException if the calling thread isn't the
@@ -152,8 +139,8 @@ public final class GraphGraphics
    *   constants.
    */
   public final void drawNodeFull(final byte shapeType,
-                                 final double xMin, final double yMin,
-                                 final double xMax, final double yMax,
+                                 final float xMin, final float yMin,
+                                 final float xMax, final float yMax,
                                  final Color fillColor, final byte borderType,
                                  final float borderWidth,
                                  final Color borderColor)
@@ -178,9 +165,9 @@ public final class GraphGraphics
       break;
     case SHAPE_TRIANGLE:
       m_poly2d.reset();
-      m_poly2d.moveTo((float) xMin, (float) yMax);
-      m_poly2d.lineTo((float) ((xMin + xMax) / 2.0d), (float) yMin);
-      m_poly2d.lineTo((float) xMax, (float) yMax);
+      m_poly2d.moveTo(xMin, yMax);
+      m_poly2d.lineTo((xMin + xMax) / 2.0f, yMin);
+      m_poly2d.lineTo(xMax, yMax);
       m_poly2d.closePath();
       shape = m_poly2d;
       break;
@@ -188,19 +175,10 @@ public final class GraphGraphics
       throw new IllegalArgumentException("shapeType is not recognized"); }
     m_g2d.setColor(fillColor);
     m_g2d.fill(shape);
-    if (borderWidth != 0.0f) {
-      if (borderWidth != m_currStrokeWidth) {
-        m_currStrokeWidth = borderWidth;
-        final BasicStroke stroke = new BasicStroke
-          (m_currStrokeWidth, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER);
-        m_g2d.setStroke(stroke); }
-      m_g2d.setColor(borderColor);
-      m_g2d.draw(shape); }
   }
 
   /**
-   * This is the method that will render a node very quickly.  For maximum
-   * performance, use this method and render all nodes with the same color.
+   * This is the method that will render a node very quickly.
    * The node shape used by this method is SHAPE_RECTANGLE.<p>
    * xMin, yMin, xMax, and yMax specify the extents of the node in the
    * node coordinate space, not the image coordinate space.  Thus, these
@@ -211,8 +189,8 @@ public final class GraphGraphics
    * @exception IllegalArgumentException if xMin is greater than xMax or if
    *   yMin is greater than yMax.
    */
-  public final void drawNodeLow(final double xMin, final double yMin,
-                                final double xMax, final double yMax,
+  public final void drawNodeLow(final float xMin, final float yMin,
+                                final float xMax, final float yMax,
                                 final Color fillColor)
   {
     if (m_debug) {
