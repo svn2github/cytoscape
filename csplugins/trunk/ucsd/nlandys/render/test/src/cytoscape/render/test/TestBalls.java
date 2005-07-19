1b35479b5413708c8687abfc22508d193da937a7
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-07-19 21:00:16 +0000

    Starting a new test that renders a static image.  This code will test ellipse node and edge intersections with positive offsets (disk arrows on edges).
    git-svn-id: http://chianti.ucsd.edu/svn@2271 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TestBalls.java
@@ -0,0 +1,57 @@
+package cytoscape.render.test;
+
+import cytoscape.render.immed.GraphGraphics;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public final class TestBalls extends Frame
+{
+
+  public final static void main(String[] args) throws Exception
+  {
+    EventQueue.invokeAndWait(new Runnable() {
+        public void run() {
+          Frame f = new TestBalls();
+          f.show();
+          f.addWindowListener(new WindowAdapter() {
+              public void windowClosing(WindowEvent e) {
+                System.exit(0); } }); } });
+  }
+
+  private final int m_imgWidth = 600;
+  private final int m_imgHeight = 480;
+  private final Image m_img;
+  private final GraphGraphics m_grafx;
+
+  public TestBalls()
+  {
+    super();
+    addNotify();
+    m_img = createImage(m_imgWidth, m_imgHeight);
+    m_grafx = new GraphGraphics(m_img, Color.white, true);
+    generateImage();
+  }
+
+  public void paint(final Graphics g)
+  {
+    final Insets insets = insets();
+    g.drawImage(m_img, insets.left, insets.top, null);
+    resize(m_imgWidth + insets.left + insets.right,
+           m_imgHeight + insets.top + insets.bottom);
+  }
+
+  private final void generateImage()
+  {
+    m_grafx.clear(0.0d, 0.0d, 1.0d);
+  }
+
+  public boolean isResizable() { return false; }
+
+}
