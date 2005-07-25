c869032d499462345b15eb9eb38dca9de6fd354e
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-07-25 15:21:17 +0000

    Removed --delete option, so that we don't delete existing files on server by accident
    git-svn-id: http://chianti.ucsd.edu/svn@4440 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/deploy.sh
+++ cyto_web/trunk/deploy.sh
@@ -5,4 +5,4 @@
 ant config_prod
 
 #  Issue rsync command
-rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links --delete --exclude "CVS/" --exclude "deploy.sh" --exclude "build.xml" . treyideker@cytoscape.org:/usr/local/www/virtual3/66/175/24/126/html/  
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --times --perms --links ---exclude "CVS/" --exclude "deploy.sh" --exclude "build.xml" . treyideker@cytoscape.org:/usr/local/www/virtual3/66/175/24/126/html/  
