16060dc21e8496553edc71bdbd948f023fcd86c3
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-01 16:50:40 +0000

    Making this test better.
    git-svn-id: http://chianti.ucsd.edu/svn@2380 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierDisk.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierDisk.java
@@ -5,6 +5,7 @@ import java.awt.Event;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.GeneralPath;
 
 public final class TryBezierDisk
@@ -15,6 +16,16 @@ public final class TryBezierDisk
     final Frame f = new Frame() {
         public final void paint(Graphics g) {
           Graphics2D g2d = (Graphics2D) g;
+          g2d.setBackground(Color.white);
+          g2d.clearRect(0, 0, 400, 400);
+          g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                               RenderingHints.VALUE_ANTIALIAS_ON);
+          g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                               RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+          g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
+                               RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+          g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
+                               RenderingHints.VALUE_STROKE_PURE);
           GeneralPath path = new GeneralPath();
           path.moveTo(200, 100);
           path.quadTo(100, 100, 100, 200);
