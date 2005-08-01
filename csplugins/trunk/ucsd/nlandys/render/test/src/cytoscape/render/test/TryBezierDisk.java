ec0e02a29fe98cf822a017fb7562fbbe2bd138b5
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-01 17:26:28 +0000

    Demonstrating the use of cubic Bezier curves.  Works like a charm.
    git-svn-id: http://chianti.ucsd.edu/svn@2382 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierDisk.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierDisk.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.GeneralPath;
+import java.awt.geom.Ellipse2D;
 
 public final class TryBezierDisk
 {
@@ -27,13 +28,21 @@ public final class TryBezierDisk
           g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
                                RenderingHints.VALUE_STROKE_PURE);
           GeneralPath path = new GeneralPath();
+          double a = 4.0d * (Math.sqrt(2.0d) - 1.0d) / 3.0d;
           path.moveTo(200, 100);
-          path.quadTo(100, 100, 100, 200);
-          path.quadTo(100, 300, 200, 300);
+          path.curveTo((float) (200.0d - 100.0d * a), 100,
+                       100, (float) (200.0d - 100.0d * a),
+                       100, 200);
+          path.curveTo(100, (float) (200.0d + 100.0d * a),
+                       (float) (200.0d - 100.0d * a), 300,
+                       200, 300);
           path.quadTo(300, 300, 300, 200);
           path.quadTo(300, 100, 200, 100);
           g2d.setColor(Color.black);
-          g2d.fill(path); }
+          g2d.fill(path);
+          Ellipse2D rect = new Ellipse2D.Float(100, 100, 200, 200);
+          g2d.setColor(Color.red);
+          g2d.fill(rect); }
         public boolean handleEvent(Event evt) {
           if (evt.id == Event.WINDOW_DESTROY) System.exit(0);
           return super.handleEvent(evt); } };
