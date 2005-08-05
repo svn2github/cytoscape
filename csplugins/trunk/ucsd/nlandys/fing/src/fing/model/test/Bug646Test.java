9d36c22f2243ab4c91f48962e858a289a4cb55a9
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-05 21:52:47 +0000

    Making this code look exactly like Mike Creech's test code.
    git-svn-id: http://chianti.ucsd.edu/svn@2471 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/Bug646Test.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/Bug646Test.java
@@ -84,35 +84,23 @@ public class Bug646Test
   {
     final RootGraph root =
       new FingExtensibleRootGraph(new MyNodeDepot(), new MyEdgeDepot());
-    final int node1 = root.createNode();
-    final int node2 = root.createNode();
-    final int node3 = root.createNode();
-    final int node4 = root.createNode();
-    final int edge1 = root.createEdge(node1, node2, true);
-    final int edge2 = root.createEdge(node1, node3, false);
-    final int edge3 = root.createEdge(node3, node4, true);
-    final int edge4 = root.createEdge(node2, node3, false);
-    final int edge5 = root.createEdge(node4, node1, true);
-    final int edge6 = root.createEdge(node4, node2, false);
-    final int[] nodes = new int[] { node1, node2, node3, node4 };
-    final int[] edges = new int[] { edge1, edge2, edge3, edge4, edge5, edge6 };
-    final GraphPerspective persp = root.createGraphPerspective(nodes, edges);
-    root.removeEdge(edge2);
-    root.removeEdge(edge3);
-    root.removeEdge(edge4);
-    root.removeEdge(edge5);
-    final int edge7 = root.createEdge(node4, node3, true);
-    final int edge8 = root.createEdge(node3, node2, false);
-    final Edge edge7Obj = root.getEdge(edge7);
-    final Edge edge8Obj = root.getEdge(edge8);
-    if (edge7 != edge7Obj.getRootGraphIndex() ||
-        edge8 != edge8Obj.getRootGraphIndex()) {
-      throw new IllegalStateException("the bug is here"); }
-    if (edge7Obj.getSource() == null ||
-        edge7Obj.getTarget() == null ||
-        edge8Obj.getSource() == null ||
-        edge8Obj.getTarget() == null) {
-      throw new IllegalStateException("the bug is here"); }
+    final Node n1 = root.getNode(root.createNode());
+    final Node n2 = root.getNode(root.createNode());
+    final Node n3 = root.getNode(root.createNode());
+    final Node n4 = root.getNode(root.createNode());
+    root.createEdge(n1, n2, false);
+    int edge_idx = root.createEdge(n1, n3, false);
+    final Edge del1 = root.getEdge(edge_idx);
+    edge_idx = root.createEdge(n2, n3, false);
+    final Edge del2 = root.getEdge(edge_idx);
+    root.removeEdge(del1);
+    root.removeEdge(del2);
+    edge_idx = root.createEdge(n1, n4, false);
+    final Edge subedge = root.getEdge(edge_idx);
+    final Node src = subedge.getSource();
+    final Node target = subedge.getTarget();
+    if (src == null || target == null)
+      throw new IllegalStateException("the bug is here");
   }
 
 }
