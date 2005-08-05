f48abd76845b82862f5778423a31aa532bb31ff5
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-05 21:42:17 +0000

    Making this test look more like production code.  This test still passes.
    git-svn-id: http://chianti.ucsd.edu/svn@2470 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/Bug646Test.java
+++ csplugins/trunk/ucsd/nlandys/fing/src/fing/model/test/Bug646Test.java
@@ -1,16 +1,89 @@
 package fing.model.test;
 
-import fing.model.FingRootGraphFactory;
+import fing.model.FingEdgeDepot;
+import fing.model.FingExtensibleRootGraph;
+import fing.model.FingNodeDepot;
 import giny.model.Edge;
 import giny.model.GraphPerspective;
+import giny.model.Node;
 import giny.model.RootGraph;
 
 public class Bug646Test
 {
 
+  private static class MyNodeDepot implements FingNodeDepot
+  {
+    public Node getNode(RootGraph root, int index, String id) {
+      return new MyNode(root, index, id); }
+    public void recycleNode(Node node) { }
+  }
+
+  private static class MyNode implements Node
+  {
+    final RootGraph m_rootGraph;
+    final int m_rootGraphIndex;
+    String m_identifier = null;
+    MyNode(RootGraph root, int index, String id) {
+      m_rootGraph = root;
+      m_rootGraphIndex = index;
+      m_identifier = id; }
+    public GraphPerspective getGraphPerspective() {
+      return m_rootGraph.createGraphPerspective
+        (m_rootGraph.getNodeMetaChildIndicesArray(m_rootGraphIndex),
+         m_rootGraph.getEdgeMetaChildIndicesArray(m_rootGraphIndex)); }
+    public boolean setGraphPerspective(GraphPerspective gp) {
+      if (gp.getRootGraph() != m_rootGraph) return false;
+      final int[] nodeInx = gp.getNodeIndicesArray();
+      final int[] edgeInx = gp.getEdgeIndicesArray();
+      for (int i = 0; i < nodeInx.length; i++)
+        m_rootGraph.addNodeMetaChild(m_rootGraphIndex, nodeInx[i]);
+      for (int i = 0; i < edgeInx.length; i++)
+        m_rootGraph.addEdgeMetaChild(m_rootGraphIndex, edgeInx[i]);
+      return true; }
+    public RootGraph getRootGraph() { return m_rootGraph; }
+    public int getRootGraphIndex() { return m_rootGraphIndex; }
+    public String getIdentifier() { return m_identifier; }
+    public boolean setIdentifier(String new_id) {
+      m_identifier = new_id;
+      return true; }
+  }
+
+  private static class MyEdgeDepot implements FingEdgeDepot
+  {
+    public Edge getEdge(RootGraph root, int index, String id) {
+      return new MyEdge(root, index, id); }
+    public void recycleEdge(Edge edge) { }
+  }
+
+  private static class MyEdge implements Edge
+  {
+    final RootGraph m_rootGraph;
+    final int m_rootGraphIndex;
+    String m_identifier = null;
+    MyEdge(RootGraph root, int index, String id) {
+      m_rootGraph = root;
+      m_rootGraphIndex = index;
+      m_identifier = id; }
+    public Node getSource() {
+      return m_rootGraph.getNode
+        (m_rootGraph.getEdgeSourceIndex(m_rootGraphIndex)); }
+    public Node getTarget() {
+      return m_rootGraph.getNode
+        (m_rootGraph.getEdgeTargetIndex(m_rootGraphIndex)); }
+    public boolean isDirected() {
+      return m_rootGraph.isEdgeDirected(m_rootGraphIndex); }
+    public RootGraph getRootGraph() { return m_rootGraph; }
+    public int getRootGraphIndex() { return m_rootGraphIndex; }
+    public String getIdentifier() { return m_identifier; }
+    public boolean setIdentifier(String new_id) {
+      m_identifier = new_id;
+      return true; }
+  }
+
   public static void main(String[] args)
   {
-    final RootGraph root = FingRootGraphFactory.instantiateRootGraph();
+    final RootGraph root =
+      new FingExtensibleRootGraph(new MyNodeDepot(), new MyEdgeDepot());
     final int node1 = root.createNode();
     final int node2 = root.createNode();
     final int node3 = root.createNode();
