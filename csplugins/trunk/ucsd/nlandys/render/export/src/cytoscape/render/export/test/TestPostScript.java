2e0a04cc7c4a6ecdd21e09f181bf51b908ee9d26
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-09 00:30:52 +0000

    Now testing text.
    git-svn-id: http://chianti.ucsd.edu/svn@2506 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestPostScript.java
+++ csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestPostScript.java
@@ -4,6 +4,7 @@ import cytoscape.render.export.ImageImposter;
 import cytoscape.render.immed.GraphGraphics;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Image;
 import org.freehep.graphicsio.ps.PSGraphics2D;
 
@@ -21,8 +22,10 @@ public class TestPostScript
     psGrafx.startExport();
     grafx.clear(0.0d, 0.0d, 1.0d);
     grafx.drawNodeFull(GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
-                       -87.3f, -20.9f, 23.34f, 67.81f, Color.red, 1.4f,
+                       -87.3f, -20.9f, 23.34f, 67.81f, Color.cyan, 1.4f,
                        Color.black);
+    final Font font = new Font("Serif", Font.PLAIN, 14);
+    grafx.drawText(font, "Tequst", -31.98f, 23.455f, Color.black);
     grafx.drawEdgeFull(GraphGraphics.ARROW_DELTA, 10.0f, Color.magenta,
                        GraphGraphics.ARROW_TEE, 10.0f, Color.blue,
                        71.4f, 20.83f, -10.1f, -81.3f,
