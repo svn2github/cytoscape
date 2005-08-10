93000fb6ca0b0e480b8c50b122156476069515c5
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-10 20:28:13 +0000

    API changes regarding FLAG_TEXT_AS_SHAPE.
    git-svn-id: http://chianti.ucsd.edu/svn@2553 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestPostScript.java
+++ csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestPostScript.java
@@ -18,16 +18,15 @@ public class TestPostScript
     final PSGraphics2D psGrafx =
       new PSGraphics2D(System.out, new Dimension(width, height));
     final Image img = new ImageImposter(psGrafx, width, height);
-    final GraphGraphics grafx = new GraphGraphics
-      (img, Color.white,
-       (args.length > 0) ? GraphGraphics.FLAG_TEXT_AS_SHAPE : 0, false);
+    final GraphGraphics grafx = new GraphGraphics(img, Color.white, false);
     psGrafx.startExport();
     grafx.clear(0.0d, 0.0d, 1.0d);
     grafx.drawNodeFull(GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
                        -87.3f, -20.9f, 23.34f, 67.81f, Color.cyan, 1.4f,
                        Color.black);
     final Font font = new Font("Serif", Font.PLAIN, 14);
-    grafx.drawText(font, 1.0d, "Tequst", -31.98f, 23.455f, Color.black);
+    grafx.drawTextFull(font, 1.0d, "Tequst", -31.98f, 23.455f, Color.black,
+                       args.length > 0);
     grafx.drawEdgeFull(GraphGraphics.ARROW_DELTA, 10.0f, Color.magenta,
                        GraphGraphics.ARROW_TEE, 10.0f, Color.blue,
                        71.4f, 20.83f, -10.1f, -81.3f,
