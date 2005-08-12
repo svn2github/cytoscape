03611df7efd8ec4c29337e125250b2443a121645
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-12 22:51:26 +0000

    More progress.
    git-svn-id: http://chianti.ucsd.edu/svn@2598 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestNativePrint.java
+++ csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestNativePrint.java
@@ -4,14 +4,17 @@ import cytoscape.render.export.ImageImposter;
 import cytoscape.render.immed.GraphGraphics;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class TestNativePrint extends Frame
+public class TestNativePrint extends Frame implements MouseListener
 {
 
   public static void main(String[] args) throws Exception
@@ -36,6 +39,7 @@ public class TestNativePrint extends Frame
     addNotify();
     m_img = createImage(m_imgWidth, m_imgHeight);
     drawGraph(new GraphGraphics(m_img, m_bgColor, true));
+    addMouseListener(this);
   }
 
   public void paint(Graphics g)
@@ -48,7 +52,7 @@ public class TestNativePrint extends Frame
 
   private void drawGraph(GraphGraphics gg)
   {
-    gg.clear(-80.0d, -80.0d, 1.8d);
+    gg.clear(-80.0d, -70.0d, 1.6d);
     gg.drawNodeFull(GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
                     -200.0f, -200.0f, -100.0f, -50.0f,
                     new Color(255, 0, 0, 128), 3.0f,
@@ -61,8 +65,19 @@ public class TestNativePrint extends Frame
                     -130.0f, -150.0f, -15.0f, 10.0f,
                     new Color(0, 255, 0, 128), 3.0f,
                     new Color(0, 0, 0, 128));
+    gg.drawTextFull(new Font("Dialog", Font.PLAIN, 12),
+                    2.0d, "Click here to print.",
+                    -20.0f, 35.0f, Color.black, true);
   }
 
   public boolean isResizable() { return false; }
+
+  public void mouseClicked(MouseEvent e) {
+    System.out.println("Great!"); }
+  public void mouseEntered(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {}
+  public void mousePressed(MouseEvent e) {}
+  public void mouseReleased(MouseEvent e) {}
+
 
 }
