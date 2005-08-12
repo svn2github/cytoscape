4f52a12f4848dc8156c3f9d996bf0d55ec38ed25
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-12 22:18:45 +0000

    Making progress on this test.
    git-svn-id: http://chianti.ucsd.edu/svn@2595 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestNativePrint.java
+++ csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestNativePrint.java
@@ -1,10 +1,32 @@
 package cytoscape.render.export.test;
 
-public class TestNativePrint
+import cytoscape.render.immed.GraphGraphics;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TestNativePrint extends Frame
 {
 
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
+    EventQueue.invokeAndWait(new Runnable() {
+        public void run() {
+          Frame f = new TestNativePrint();
+          f.show();
+          f.addWindowListener(new WindowAdapter() {
+              public void windowClosing(WindowEvent e) {
+                System.exit(0); } }); } });
   }
+
+  public TestNativePrint()
+  {
+    super();
+  }
+
+  public boolean isResizable() { return false; }
 
 }
