d85a6494d7068faf85aa1916eca88941276b0f6d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-13 06:18:15 +0000

    Getting the FreeHEP and epsgraphics code to look similar.
    git-svn-id: http://chianti.ucsd.edu/svn@2605 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestPostScript.java
+++ csplugins/trunk/ucsd/nlandys/render/export/src/cytoscape/render/export/test/TestPostScript.java
@@ -13,24 +13,27 @@ public class TestPostScript
 
   public static void main(String[] args)
   {
-    final int width = 200;
-    final int height = 200;
+    final int width = 640;
+    final int height = 480;
     final PSGraphics2D psGrafx =
       new PSGraphics2D(System.out, new Dimension(width, height));
     final Image img = new ImageImposter(psGrafx, width, height);
-    final GraphGraphics grafx = new GraphGraphics(img, Color.white, false);
+    final GraphGraphics gg = new GraphGraphics(img, Color.white, false);
     psGrafx.startExport();
-    grafx.clear(0.0d, 0.0d, 1.0d);
-    grafx.drawNodeFull(GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
-                       -87.3f, -20.9f, 23.34f, 67.81f, Color.cyan, 1.4f,
-                       Color.black);
-    final Font font = new Font("Serif", Font.PLAIN, 14);
-    grafx.drawTextFull(font, 1.0d, "Tequst", -31.98f, 23.455f, Color.black,
-                       args.length > 0);
-    grafx.drawEdgeFull(GraphGraphics.ARROW_DELTA, 10.0f, Color.magenta,
-                       GraphGraphics.ARROW_TEE, 10.0f, Color.blue,
-                       71.4f, 20.83f, -10.1f, -81.3f,
-                       3.0f, Color.orange, 9.0f);
+    gg.clear(-80.0d, -70.0d, 1.6d);
+    final int alpha = 128;
+    gg.drawNodeFull(GraphGraphics.SHAPE_RECTANGLE,
+                    -200.0f, -200.0f, -100.0f, -50.0f,
+                    new Color(255, 0, 0, alpha), 3.0f,
+                    new Color(0, 0, 0, alpha));
+    gg.drawNodeFull(GraphGraphics.SHAPE_RECTANGLE,
+                    -220.0f, -100.0f, -30.0f, 0.0f,
+                    new Color(0, 0, 255, alpha), 3.0f,
+                    new Color(0, 0, 0, alpha));
+    gg.drawNodeFull(GraphGraphics.SHAPE_RECTANGLE,
+                    -130.0f, -150.0f, -15.0f, 10.0f,
+                    new Color(0, 255, 0, alpha), 3.0f,
+                    new Color(0, 0, 0, alpha));
     psGrafx.endExport();
   }
 
