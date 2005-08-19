afa48c78e5c27c7b107bef55d0301747ef594b44
grossb <grossb@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-19 17:15:10 +0000

    The CytoPanelListener inteface.
    git-svn-id: http://chianti.ucsd.edu/svn@2684 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/mskcc/grossb/cytopanels/src/cytoscape/cytopanel/ICytoPanelListener.java
@@ -0,0 +1,26 @@
+package cytoscape.cytopanel;
+
+import java.util.EventListener;
+
+/**
+ * This listener interface provides the
+ * mechanism to respond to CytoPanel Events.
+ *
+ * @author Ben Gross
+ */
+public interface ICytoPanelListener extensd EventListener {
+
+    /**
+     * Notifies the listener on a change in the CytoPanel state.
+     *
+     * @param newState The new CytoPanel state - see CytoPanelConstants class.
+     */
+    public void onStateChange(int newState);
+
+    /**
+     * Notifies the listener when a new component on the CytoPanel is selected.
+     *
+	 * @param componentIndex The index of the component selected.
+     */
+    public void onComponentSelected(int componentIndex);
+}
