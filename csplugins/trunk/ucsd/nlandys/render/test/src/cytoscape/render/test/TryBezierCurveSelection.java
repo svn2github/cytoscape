b698b9fdb23a1e3bb1633f0be71cdda3b92a2c29
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-19 18:19:05 +0000

    Making progress on this test.
    git-svn-id: http://chianti.ucsd.edu/svn@2690 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierCurveSelection.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierCurveSelection.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.RenderingHints;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -24,9 +26,23 @@ public final class TryBezierCurveSelection extends Frame
                 System.exit(0); } }); } });
   }
 
+  private final int m_imgWidth = 640;
+  private final int m_imgHeight = 480;
+  private final Image m_img;
+
   public TryBezierCurveSelection()
   {
     super();
+    addNotify();
+    m_img = createImage(m_imgWidth, m_imgHeight);
+  }
+
+  public void paint(final Graphics g)
+  {
+    final Insets insets = insets();
+    g.drawImage(m_img, insets.left, insets.top, null);
+    resize(m_imgWidth + insets.left + insets.right,
+           m_imgHeight + insets.top + insets.bottom);
   }
 
   public boolean isResizable() { return false; }
