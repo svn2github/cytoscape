893497edd911f142129f58dfe4a365c6e4bd97ef
grossb <grossb@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-19 19:52:46 +0000

    added more methods to interface (add components, tabs, gets, indexes, etc)
    git-svn-id: http://chianti.ucsd.edu/svn@2694 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/mskcc/grossb/cytopanels/src/cytoscape/cytopanel/ICytoPanel.java
+++ csplugins/trunk/mskcc/grossb/cytopanels/src/cytoscape/cytopanel/ICytoPanel.java
@@ -6,43 +6,129 @@ import javax.swing.Icon;
 /**
  * Interface to a CytoPanel.
  *
- * @author Ben Gross
+ * @author Ben Gross.
  */
 public interface ICytoPanel {
 
     /**
-     * Adds a new Component to the CytoPanel
+     * Adds a component to the CytoPanel.
      *
-     * @param title     Copmone title.
-	 * @param icon      Icon
+     * @param component  Component reference.
+	 * @return component Component reference.
+     */
+    public Component add(Component component);
+
+    /**
+     * Adds a component to the CytoPanel at specified index.
+     *
      * @param component Component reference.
-     * @param tip       Tool tip text.
+     * @param index     Tab index.
+	 * @return component Component reference.
+     */
+    public Component add(Component component, int index);
+
+    /**
+     * Adds a component to the CytoPanel with a specified title.
+     *
+     * @param title     Component title.
+     * @param component Component reference.
+	 * @return component Component reference.
+     */
+    public Component add(String title, Component component);
+
+    /**
+     * Adds a component to the CytoPanel with specified title and icon.
+     *
+     * @param title     Component title (can be null).
+	 * @param icon      Component icon (can be null).
+     * @param component Component reference.
+     */
+    public void add(String title, Icon icon, Component component);
+
+    /**
+     * Adds a component to the CytoPanel with specified title, icon, and tool tip.
+     *
+     * @param title     Component title (can be null).
+	 * @param icon      Component icon (can be null).
+     * @param component Component reference.
+     * @param tip       Component Tool tip text.
      */
     public void add(String title, Icon icon, Component component, String tip);
 
+	/**
+	 * Returns the currently selected component.
+	 *
+	 * @return component Currently selected Component reference.
+	 */
+	public Component getSelectedComponent();
+
+	/**
+	 * Returns the currently selected index.
+	 *
+	 * @return index Currently selected index.
+	 */
+	public int getSelectedIndex();
+
+	/**
+	 * Returns the index of the tab for the specified component.
+	 *
+     * @param component Component reference.
+	 * @return int      Tab Index of the Component or -1 if not found.
+	 */
+	public int indexOfComponent(Component component);
+
+	/**
+	 * Returns the first tab index with given title.
+	 *
+     * @param title Tab title.
+	 * @return int  Tab index with given title or -1 if not found.
+	 */
+	public int indexOfTab(String title);
+
+	/**
+	 * Remove specified component from the CytoPanel.
+	 *
+	 * @param component Component reference.
+	 */
+	public void remove(Component component);
+
+	/**
+	 * Removes the tab and component from the CytoPanel at the specified index.
+	 *
+     * @param index Tab index.
+	 */
+	public void remove(int index);
+
+	/**
+	 * Removes all the tabs and corresponding components from the CytoPanel.
+	 */
+	public void removeAll();
+
     /**
-     * Sets the state of the CytoPanel.
+     * Set the state of the CytoPanel.
      *
-     * @param cytoPanelState A CytoPanelConstants state.
+     * @param An integer representing the CytoPanel state - see CytoPanelConstants class.
      */
     public void setState(int cytoPanelState);
 
     /**
-     * Gets the state of the CytoPanel.
+     * Get the state of the CytoPanel.
      *
-	 * @return cytoPanelState A CytoPanel Constants state
+	 * @return An integer representing the CytoPanel state - CytoPanelConstants class.
      */
     public int getState();
 
 	/**
 	 * Add a CytoPanel listener.
-	 * @param cytoPanelListener Reference to a ICytoPanelListener
+	 *
+	 * @param cytoPanelListener Reference to a ICytoPanelListener.
 	 */
 	public void addCytoPanelListener(ICytoPanelListener cytoPanelListener);
 
 	/**
 	 * Remove a CytoPanel listener.
-	 * @param cytoPanelListener Reference to a ICytoPanelListener
+	 *
+	 * @param cytoPanelListener Reference to a ICytoPanelListener.
 	 */
 	public void removeCytoPanelListener(ICytoPanelListener cytoPanelListener);
 
