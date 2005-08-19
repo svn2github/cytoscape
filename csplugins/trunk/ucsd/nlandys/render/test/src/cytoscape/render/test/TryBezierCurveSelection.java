6fb4183569dd423b4ea75815e82241da94c0f655
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-19 20:10:38 +0000

    Now drawing a curve.
    git-svn-id: http://chianti.ucsd.edu/svn@2698 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierCurveSelection.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TryBezierCurveSelection.java
@@ -29,12 +29,33 @@ public final class TryBezierCurveSelection extends Frame
   private final int m_imgWidth = 640;
   private final int m_imgHeight = 480;
   private final Image m_img;
+  private final GeneralPath m_curve1;
+//   private final GeneralPath m_curve2;
+//   private final GeneralPath m_curve3;
 
   public TryBezierCurveSelection()
   {
     super();
     addNotify();
     m_img = createImage(m_imgWidth, m_imgHeight);
+    m_curve1 = new GeneralPath();
+    m_curve1.moveTo(10.0f, 10.0f);
+    m_curve1.curveTo(300.0f, 10.0f, 310.0f, 20.0f, 310.0f, 320.0f);
+    m_curve1.curveTo(310.0f, 620.0f, 100.0f, 300.0f, 590.0f, 150.0f);
+    updateImage();
+  }
+
+  private final void updateImage()
+  {
+    final Graphics2D g2d = (Graphics2D) m_img.getGraphics();
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                         RenderingHints.VALUE_ANTIALIAS_ON);
+    g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
+                         RenderingHints.VALUE_STROKE_PURE);
+    g2d.setBackground(Color.white);
+    g2d.clearRect(0, 0, m_imgWidth, m_imgHeight);
+    g2d.setColor(Color.black);
+    g2d.draw(m_curve1);
   }
 
   public void paint(final Graphics g)
