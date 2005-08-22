e5c00e299b2f11a33432dfe757f51a01ccc71a15
iavila <iavila@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-22 11:44:48 +0000

    *** empty log message ***
    git-svn-id: http://chianti.ucsd.edu/svn@2716 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/isb/iavila/xmlRpc/src/org/isb/xmlrpc/handler/annotations/AnnotationsDataSource.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.isb.xmlrpc.handler.annotations;
+
+import java.lang.*;
+import java.util.*;
+import org.isb.xmlrpc.handler.DataSource;
+
+/**
+ * @author jpark
+ *
+ */
+public interface AnnotationsDataSource extends DataSource{
+
+}
