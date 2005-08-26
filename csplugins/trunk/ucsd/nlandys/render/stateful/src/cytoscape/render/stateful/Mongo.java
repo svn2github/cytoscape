8af19ff2861bc9336ef4af1e9553d66b33b45b9c
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-26 21:31:24 +0000

    Making progress with the build system.
    git-svn-id: http://chianti.ucsd.edu/svn@2839 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/Mongo.java
@@ -0,0 +1,16 @@
+package cytoscape.render.stateful;
+
+import cytoscape.graph.fixed.FixedGraph;
+import cytoscape.util.intr.IntEnumerator;
+
+public class Mongo
+{
+
+  private IntEnumerator foo;
+
+  public final FixedGraph getGraphTopology()
+  {
+    return null;
+  }
+
+}
