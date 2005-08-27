a4870193bc0b4de79ef78c26a5f6348f4de2af61
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-27 00:44:00 +0000

    Re-thinking the design of this module.
    git-svn-id: http://chianti.ucsd.edu/svn@2847 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/Mongo.java
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/Mongo.java
@@ -8,7 +8,9 @@ import cytoscape.render.immed.GraphGraphics;
 import cytoscape.util.intr.IntEnumerator;
 import cytoscape.util.intr.IntIterator;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 
 /**
  * This module aggregates several smaller modules into a very powerful
@@ -16,62 +18,39 @@ import java.awt.Graphics2D;
  * framework, it helps to understand the public API of the package
  * cytoscape.render.immed.
  */
-public final class Mongo implements FixedGraph
+public final class Mongo
 {
 
-  private final ImageSource m_imgSource;
+  private final boolean m_debug;
   private final DynamicGraph m_graph;
   private final RTree m_tree;
   private GraphGraphics m_grafx;
 
-  public Mongo(final int width, final int height,
-               final ImageSource imgSource)
+  public Mongo(final Image img, final boolean debug)
   {
-    m_imgSource = imgSource;
+    m_debug = debug;
     m_graph = DynamicGraphFactory.instantiateDynamicGraph();
     m_tree = new RTree();
-    m_grafx = new GraphGraphics
-      (m_imgSource.createImageBuffer(width, height), true);
+    m_grafx = new GraphGraphics(img, m_debug);
   }
 
-  public final IntEnumerator nodes() {
-    return m_graph.nodes(); }
-
-  public final IntEnumerator edges() {
-    return m_graph.edges(); }
-
-  public final boolean nodeExists(final int node) {
-    return m_graph.nodeExists(node); }
-
-  public final byte edgeType(final int edge) {
-    return m_graph.edgeType(edge); }
-
-  public final int edgeSource(final int edge) {
-    return m_graph.edgeSource(edge); }
-
-  public final int edgeTarget(final int edge) {
-    return m_graph.edgeTarget(edge); }
-
-  public final IntEnumerator edgesAdjacent(final int node,
-                                           final boolean outgoing,
-                                           final boolean incoming,
-                                           final boolean undirected) {
-    return m_graph.edgesAdjacent(node, outgoing, incoming, undirected); }
-
-  public final IntIterator edgesConnecting(final int node0, final int node1,
-                                           final boolean outgoing,
-                                           final boolean incoming,
-                                           final boolean und) {
-    return m_graph.edgesConnecting(node0, node1, outgoing, incoming, und); }
-
-  public final void print(Graphics2D graphics, boolean textAsShapes,
-                          GraphLOD lod)
+  public final void print(final Graphics2D graphics,
+                          final Color bgColor,
+                          final GraphLOD lod,
+                          final NodeDetails nodeDetails,
+                          final EdgeDetails edgeDetails)
   {
   }
 
-  public final void resize(final int newWidth, final int newHeight)
+  public final void render(final Color bgColor,
+                           final GraphLOD lod,
+                           final NodeDetails nodeDetails,
+                           final EdgeDetails edgeDetails)
   {
-    // Create new GraphGraphics with Image gotten from ImageSource.
+  }
+
+  public final void resize(final Image img)
+  {
   }
 
 }
