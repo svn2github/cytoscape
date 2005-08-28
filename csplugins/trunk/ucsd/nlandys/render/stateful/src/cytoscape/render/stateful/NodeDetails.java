e0e082a8a4cdee4867ba568a7bd32a62c77b4809
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-08-28 19:25:34 +0000

    Making this into an "abstract" class.
    git-svn-id: http://chianti.ucsd.edu/svn@2852 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/NodeDetails.java
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/NodeDetails.java
@@ -1,21 +1,45 @@
 package cytoscape.render.stateful;
 
+import cytoscape.render.immed.GraphGraphics;
 import java.awt.Color;
 
 /**
  * Defines visual properties of a node modulo the node size and location.
- * This interface only makes sense in the context of a Mongo.
+ * Even though this class is not declared abstract, in most situations it
+ * makes sense to override at least some of its methods in order to gain
+ * control over graph visual properties.
  */
-public interface NodeDetails
+public class NodeDetails
 {
 
-  public byte shape(int node);
+  /**
+   * Returns a SHAPE_* constant (or a custom node shape) that an instance of
+   * cytoscape.render.immed.GraphGraphics understands.
+   * By default this method returns SHAPE_RECTANGLE.
+   */
+  public byte shape(final int node) {
+    return GraphGraphics.SHAPE_RECTANGLE; }
 
-  public Color fillColor(int node);
+  /**
+   * Returns the color of the interior of the node shape.  By default this
+   * method returns Color.red.
+   */
+  public Color fillColor(final int node) {
+    return Color.red; }
 
-  public float borderWidth(int node);
+  /**
+   * Returns the border width of the node shape.  By default this method
+   * returns 0.0f.
+   */
+  public float borderWidth(final int node) {
+    return 0.0f; }
 
-  public Color borderColor(int node);
+  /**
+   * Returns the color of the border of the node shape.  By default this method
+   * returns null.
+   */
+  public Color borderColor(final int node) {
+    return null; }
 
   // What about node label?
 
