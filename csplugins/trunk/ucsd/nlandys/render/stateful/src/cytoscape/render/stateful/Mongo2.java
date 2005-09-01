467dfcc9282bcd7011830b12518ae10442f9f1c9
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-01 22:39:26 +0000

    Making progress; filling in Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@2926 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/Mongo2.java
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/Mongo2.java
@@ -13,36 +13,60 @@ public final class Mongo2
   private Mongo2() { }
 
   /**
-   * @return the edges that were rendered.
+   * Renders a graph.
+   * @param graph the graph topology; nodes in this graph must correspond to
+   *   objKeys in nodePositions (the SpacialIndex2D parameter) and vice versa.
+   * @param nodePositions defines the positions and extents of nodes in graph;
+   *   each entry (objKey) in this structure must correspond to a node in graph
+   *   (the FixedGraph parameter) and vice versa.
+   * @param lod defines the different levels of detail; an appropriate level
+   *   of detail is chosen based on the results of method calls on this
+   *   object.
+   * @param nodeDetails defines details of nodes such as colors, node border
+   *   thickness, and shape; the node arguments passed to methods on this
+   *   object will be nodes in the graph parameter.
+   * @param edgeDetails defines details of edges such as colors, thickness,
+   *   and arrow type; the edge arguments passed to methods on this
+   *   object will be edges in the graph parameter.
+   * @param nodeBuff this is a computational helper that is required in the
+   *   implementation of this method; this method starts by emptying this
+   *   hashtable, and when this method returns, the hashtable will contain
+   *   exactly the nodes that were rendered.
+   * @param grafx the graphics context that is to render this graph.
+   * @param bgColor the background color to use when calling grafx.clear().
+   * @param xCenter the xCenter parameter to use when calling grafx.clear().
+   * @param yCenter the yCenter parameter to use when calling grafx.clear().
+   * @param scaleFactor the scaleFactor parameter to use when calling
+   *   grafx.clear().
    */
-  public final static IntHash renderGraph(final GraphGraphics grafx,
-                                          final FixedGraph graph,
-                                          final SpacialIndex2D nodePositions,
-                                          final GraphLOD lod,
-                                          final NodeDetails nodeDetails,
-                                          final EdgeDetails edgeDetails,
-                                          final Color bgColor,
-                                          final double xCenter,
-                                          final double yCenter,
-                                          final double scaleFactor)
+  public final static void renderGraph(final FixedGraph graph,
+                                       final SpacialIndex2D nodePositions,
+                                       final GraphLOD lod,
+                                       final NodeDetails nodeDetails,
+                                       final EdgeDetails edgeDetails,
+                                       final IntHash nodeBuff,
+                                       final GraphGraphics grafx,
+                                       final Color bgColor,
+                                       final double xCenter,
+                                       final double yCenter,
+                                       final double scaleFactor)
   {
-    return null;
   }
 
-  public final static boolean queryEdgeIntersect(
-                                            final GraphGraphics grafx,
-                                            final FixedGraph graph,
-                                            final SpacialIndex2D nodePositions,
-                                            final GraphLOD lod,
-                                            final NodeDetails nodeDetails,
-                                            final EdgeDetails edgeDetails,
-                                            final int edge,
-                                            final float xMinQuery,
-                                            final float yMinQuery,
-                                            final float xMaxQuery,
-                                            final float yMaxQuery)
-  {
-    return false;
-  }
+//   public final static boolean queryEdgeIntersect(
+//                                             final GraphGraphics grafx,
+//                                             final FixedGraph graph,
+//                                             final SpacialIndex2D nodePositions,
+//                                             final GraphLOD lod,
+//                                             final NodeDetails nodeDetails,
+//                                             final EdgeDetails edgeDetails,
+//                                             final int edge,
+//                                             final float xMinQuery,
+//                                             final float yMinQuery,
+//                                             final float xMaxQuery,
+//                                             final float yMaxQuery)
+//   {
+//     return false;
+//   }
 
 }
