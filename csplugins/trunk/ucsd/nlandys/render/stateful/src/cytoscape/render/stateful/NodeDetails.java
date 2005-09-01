b87ee1119260cfa26f396830337ce65cda4a3ad4
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-01 23:16:57 +0000

    Adding node label related methods.
    git-svn-id: http://chianti.ucsd.edu/svn@2929 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/NodeDetails.java
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/NodeDetails.java
@@ -2,6 +2,7 @@ package cytoscape.render.stateful;
 
 import cytoscape.render.immed.GraphGraphics;
 import java.awt.Color;
+import java.awt.Font;
 
 /**
  * Defines visual properties of a node modulo the node size and location.
@@ -13,9 +14,9 @@ public class NodeDetails
 {
 
   /**
-   * Returns a SHAPE_* constant (or a custom node shape) that an instance of
-   * cytoscape.render.immed.GraphGraphics understands.
-   * By default this method returns SHAPE_RECTANGLE.
+   * Returns a GraphGraphics.SHAPE_* constant (or a custom node shape) that an
+   * instance of GraphGraphics understands.
+   * By default this method returns GraphGraphics.SHAPE_RECTANGLE.
    */
   public byte shape(final int node) {
     return GraphGraphics.SHAPE_RECTANGLE; }
@@ -29,18 +30,56 @@ public class NodeDetails
 
   /**
    * Returns the border width of the node shape.  By default this method
-   * returns 0.0f.
+   * returns zero.
    */
   public float borderWidth(final int node) {
     return 0.0f; }
 
   /**
    * Returns the color of the border of the node shape.  By default this method
-   * returns null.
+   * returns null.  This return value is ignored if borderWidth(node)
+   * returns zero; it is an error to return null if borderWidth(node) returns
+   * a value greater than zero.
    */
   public Color borderColor(final int node) {
     return null; }
 
-  // What about node label?
+  /**
+   * Returns the text label this node has.  By default this method returns
+   * null; returning null is the optimal way to specify that this
+   * node has no text label.
+   */
+  public String label(final int node) {
+    return null; }
+
+  /**
+   * Returns the font to use when rendering a text label on this node.
+   * By default this method returns null.
+   * This return value is ignored if label(node) returns either null or the
+   * empty string; it is an error to return null if label(node) returns a
+   * non-empty string.
+   */
+  public Font font(final int node) {
+    return null; }
+
+  /**
+   * Returns an additional scaling factor that is to be applied to the font
+   * used to render text labels; this scaling factor, applied to the point
+   * size of the font returned by font(node), yields a new virtual font that
+   * is used to actually render text labels.  By default this method returns
+   * 1.0.  This return value is ignored if label(node) returns either null or
+   * the empty string.
+   */
+  public double fontScaleFactor(final int node) {
+    return 1.0d; }
+
+  /**
+   * Returns the color of the text label on this node.  By default this method
+   * returns null.  This return value is ignored if label(node) returns
+   * either null or the empty string; it is an error to return null if
+   * label(node) returns a non-empty string.
+   */
+  public Color labelColor(final int node) {
+    return null; }
 
 }
