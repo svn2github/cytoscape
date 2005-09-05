6c747b47248ce54f8736424dfdb20fc018a66a55
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-05 21:23:20 +0000

    Making some progress.
    git-svn-id: http://chianti.ucsd.edu/svn@2983 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TestGraphRenderingSimple.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/TestGraphRenderingSimple.java
@@ -2,6 +2,9 @@ package cytoscape.render.test;
 
 import java.awt.EventQueue;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
@@ -23,11 +26,25 @@ public class TestGraphRenderingSimple
                 System.exit(0); } }); } });
   }
 
+  private final int m_imgWidth = 800;
+  private final int m_imgHeight = 600;
+  private final Image m_img;
+
   public TestGraphRenderingSimple()
   {
     super();
+    addNotify();
+    m_img = createImage(m_imgWidth, m_imgHeight);
     addMouseListener(this);
     addMouseMotionListener(this);
+  }
+
+  public void paint(Graphics g)
+  {
+    final Insets insets = insets();
+    g.drawImage(m_img, insets.left, insets.top, null);
+    resize(m_imgWidth + insets.left + insets.right,
+           m_imgHeight + insets.top + insets.bottom);
   }
 
   public void mouseClicked(MouseEvent e) {}
