a1a0b3510275fe2d06c18980033d326e951ac0b6
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-06 20:37:51 +0000

    Adding renderAllEdges().
    git-svn-id: http://chianti.ucsd.edu/svn@3006 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/GraphLOD.java
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/GraphLOD.java
@@ -12,9 +12,25 @@ public class GraphLOD
 {
 
   /**
+   * Determines whether or not to render all edges in a graph.  By default
+   * this method returns false, which leads the rendering engine to render
+   * only those edges that touch at least one visible node.  Note that
+   * rendering all edges leads to a dramatic performance decrease when
+   * rendering large graphs.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param totalEdgeCount the total number of edges in the graph that is
+   *   being rendered.
+   */
+  public boolean renderAllEdges(final int renderNodeCount,
+                                final int totalEdgeCount)
+  {
+    return false;
+  }
+
+  /**
    * Determines whether or not to render a graph at full detail.
-   * By default this method returns true if and only if the sum of visible
-   * nodes and visible edges is less than 1200.<p>
+   * By default this method returns true if and only if the sum of rendered
+   * nodes and rendered edges is less than 1200.<p>
    * The following table describes the difference between full and low
    * rendering detail in terms of what methods on an instance of
    * GraphGraphics get called:
@@ -31,49 +47,60 @@ public class GraphLOD
    *   <tr>  <th>node labels</th>
    *         <td>drawTextFull()</td>
    *         <td>not rendered</td>                                        </tr>
-   * </table></blockquote>
-   * @param visibleNodeCount the number of nodes that are about to be rendered.
-   * @param visibleEdgeCount the number of edges that are about to be rendered.
+   * </table></blockquote><p>
+   * This method gets called after renderAllEdges() by the rendering
+   * engine.  The renderEdgeCount parameter will be the total edge count in
+   * the graph if renderAllEdges() returned true.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param renderEdgeCount the number of edges that are about to be rendered.
    * @return true for full detail, false for low detail.
+   * @see #renderAllEdges(int, int)
    */
-  public boolean detail(final int visibleNodeCount,
-                        final int visibleEdgeCount)
+  public boolean detail(final int renderNodeCount, final int renderEdgeCount)
   {
-    return visibleNodeCount + visibleEdgeCount < 1200;
+    return renderNodeCount + renderEdgeCount < 1200;
   }
 
   /**
    * Determines whether or not to render node borders.  By default this
-   * method returns true if and only if the sum of visible nodes and visible
+   * method returns true if and only if the sum of rendered nodes and rendered
    * edges is less than 500.<p>
-   * Note that it is only possible to draw node borders at the full detail
-   * level.  If low detail is chosen, the output of this method is ignored.
-   * @param visibleNodeCount the number of nodes that are about to be rendered.
-   * @param visibleEdgeCount the number of edges that are about to be rendered.
+   * It is only possible to draw node borders at the full detail
+   * level.  If low detail is chosen, the output of this method is ignored.<p>
+   * This method gets called after renderAllEdges() by the rendering
+   * engine.  The renderEdgeCount parameter will be the total edge count in
+   * the graph if renderAllEdges() returned true.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param renderEdgeCount the number of edges that are about to be rendered.
    * @return true if and only if node borders are to be rendered.
    * @see #detail(int, int)
+   * @see #renderAllEdges(int, int)
    */
-  public boolean nodeBorders(final int visibleNodeCount,
-                             final int visibleEdgeCount)
+  public boolean nodeBorders(final int renderNodeCount,
+                             final int renderEdgeCount)
   {
-    return visibleNodeCount + visibleEdgeCount < 500;
+    return renderNodeCount + renderEdgeCount < 500;
   }
 
   /**
    * Determines whether or not to render node labels.  By default this method
-   * returns true if and only if the number of visible nodes is less than
+   * returns true if and only if the number of rendered nodes is less than
    * 80.<p>
    * Node labels are only rendered at the full detail level.  If low detail is
-   * chosen, the output of this method is ignored.
-   * @param visibleNodeCount the number of nodes that are about to be rendered.
-   * @param visibleEdgeCount the number of edges that are about to be rendered.
+   * chosen, the output of this method is ignored.<p>
+   * This method gets called after renderAllEdges() by the rendering
+   * engine.  The renderEdgeCount parameter will be the total edge count in
+   * the graph if renderAllEdges() returned true.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param renderEdgeCount the number of edges that are about to be rendered.
    * @return true if and only if node labels are to be rendered.
    * @see #detail(int, int)
+   * @see #renderAllEdges(int, int)
    */
-  public boolean nodeLabels(final int visibleNodeCount,
-                            final int visibleEdgeCount)
+  public boolean nodeLabels(final int renderNodeCount,
+                            final int renderEdgeCount)
   {
-    return visibleNodeCount < 80;
+    return renderNodeCount < 80;
   }
 
   /**
@@ -81,52 +108,64 @@ public class GraphLOD
    * labels.  By default this method always returns false.<p>
    * This method affects the boolean parameter drawTextAsShape in the method
    * call GraphGraphics.drawTextFull().  If node labels are not rendered
-   * altogether, the output of this method is ignored.
-   * @param visibleNodeCount the number of nodes that are about to be rendered.
-   * @param visibleEdgeCount the number of edges that are about to be rendered.
+   * altogether, the output of this method is ignored.<p>
+   * This method gets called after renderAllEdges() by the rendering
+   * engine.  The renderEdgeCount parameter will be the total edge count in
+   * the graph if renderAllEdges() returned true.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param renderEdgeCount the number of edges that are about to be rendered.
    * @return true if and only if rendered node label text should be drawn as
    *   primitive shapes.
    * @see #nodeLabels(int, int)
+   * @see #renderAllEdges(int, int)
    */
-  public boolean textAsShape(final int visibleNodeCount,
-                             final int visibleEdgeCount)
+  public boolean textAsShape(final int renderNodeCount,
+                             final int renderEdgeCount)
   {
     return false;
   }
 
   /**
    * Determines whether or not to render edge arrows.  By default this
-   * method returns true if and only if the sum of visible nodes and visible
+   * method returns true if and only if the sum of rendered nodes and rendered
    * edges is less than 500.<p>
-   * Note that it is only possible to draw edge arrows at the full detail
-   * level.  If low detail is chosen, the output of this method is ignored.
-   * @param visibleNodeCount the number of nodes that are about to be rendered.
-   * @param visibleEdgeCount the number of edges that are about to be rendered.
+   * It is only possible to draw edge arrows at the full detail
+   * level.  If low detail is chosen, the output of this method is ignored.<p>
+   * This method gets called after renderAllEdges() by the rendering
+   * engine.  The renderEdgeCount parameter will be the total edge count in
+   * the graph if renderAllEdges() returned true.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param renderEdgeCount the number of edges that are about to be rendered.
    * @return true if and only if edge arrows are to be rendered.
    * @see #detail(int, int)
+   * @see #renderAllEdges(int, int)
    */
-  public boolean edgeArrows(final int visibleNodeCount,
-                            final int visibleEdgeCount)
+  public boolean edgeArrows(final int renderNodeCount,
+                            final int renderEdgeCount)
   {
-    return visibleNodeCount + visibleEdgeCount < 500;
+    return renderNodeCount + renderEdgeCount < 500;
   }
 
   /**
    * Determines whether or not to honor dashed edges.  By default this
    * method always returns true.  If false is returned, edges that
    * claim to be dashed will be rendered as solid.<p>
-   * Note that it is only possible to draw dashed edges at the full detail
+   * It is only possible to draw dashed edges at the full detail
    * level.  If low detail is chosen, the output of this method is ignored.
-   * Note also that drawing dashed edges is computationally expensive;
+   * Note that drawing dashed edges is computationally expensive;
    * the default implementation of this method does not make a very
-   * performance-minded decision if a lot of edges happen to be dashed.
-   * @param visibleNodeCount the number of nodes that are about to be rendered.
-   * @param visibleEdgeCount the number of edges that are about to be rendered.
+   * performance-minded decision if a lot of edges happen to be dashed.<p>
+   * This method gets called after renderAllEdges() by the rendering
+   * engine.  The renderEdgeCount parameter will be the total edge count in
+   * the graph if renderAllEdges() returned true.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param renderEdgeCount the number of edges that are about to be rendered.
    * @return true if and only if dashed edges are to be honored.
    * @see #detail(int, int)
+   * @see #renderAllEdges(int, int)
    */
-  public boolean dashedEdges(final int visibleNodeCount,
-                             final int visibleEdgeCount)
+  public boolean dashedEdges(final int renderNodeCount,
+                             final int renderEdgeCount)
   {
     return true;
   }
@@ -136,15 +175,19 @@ public class GraphLOD
    * method always returns true.  If false is returned, edges that
    * claim to have edge anchors will be rendered as simple straight
    * edges.<p>
-   * Note that it is only possible to draw poly-edges at the full detail
-   * level.  If low detail is chosen, the output of this method is ignored.
-   * @param visibleNodeCount the number of nodes that are about to be rendered.
-   * @param visibleEdgeCount the number of edges that are about to be rendered.
+   * It is only possible to draw poly-edges at the full detail
+   * level.  If low detail is chosen, the output of this method is ignored.<p>
+   * This method gets called after renderAllEdges() by the rendering
+   * engine.  The renderEdgeCount parameter will be the total edge count in
+   * the graph if renderAllEdges() returned true.
+   * @param renderNodeCount the number of nodes that are about to be rendered.
+   * @param renderEdgeCount the number of edges that are about to be rendered.
    * @return true if and only if edge anchors are to be honored.
    * @see #detail(int, int)
+   * @see #renderAllEdges(int, int)
    */
-  public boolean edgeAnchors(final int visibleNodeCount,
-                             final int visibleEdgeCount)
+  public boolean edgeAnchors(final int renderNodeCount,
+                             final int renderEdgeCount)
   {
     return true;
   }
