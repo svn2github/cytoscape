94878a5456f8102a72866d3e04b7cdc4c337207a
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-07 21:40:54 +0000

    Changing strategy a little bit.
    git-svn-id: http://chianti.ucsd.edu/svn@3025 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/obo/src/cytoscape/cruft/obo/CellularComponentAnnotationReader.java
@@ -0,0 +1,13 @@
+package cytoscape.cruft.obo;
+
+import java.io.Reader;
+
+public final class CellularComponentAnnotationReader
+{
+
+  public CellularComponentAnnotationReader(final String speciesName,
+                                           final Reader geneAssociationFile)
+  {
+  }
+
+}
