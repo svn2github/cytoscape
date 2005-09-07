7ed2d0b1eaedc9c4366d07cc4e8d3c0471635521
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-07 22:03:43 +0000

    Keeping files in sync with one another.
    git-svn-id: http://chianti.ucsd.edu/svn@3029 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/obo/src/cytoscape/cruft/obo/CellularComponentAnnotationReader.java
+++ csplugins/trunk/ucsd/nlandys/obo/src/cytoscape/cruft/obo/CellularComponentAnnotationReader.java
@@ -1,13 +1,54 @@
 package cytoscape.cruft.obo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 
+/**
+ * The purpose of this class is to convert a 'gene_association.*' file
+ * (for example 'gene_association.sgd') into a crufty old 'cellcomp.anno' file.
+ * The parameter passed to the constructor is the 'gene_association.*' file
+ * and the content read from this reader is the crufty old 'cellcomp.anno'
+ * file.<p>
+ * Please note that this code was not written with performance in mind.
+ * Therefore, evaluating this code will not give a good indication of the
+ * author's programming ability.
+ */
 public final class CellularComponentAnnotationReader
 {
+
+  private final String NL = System.getProperty("line.separator");
+  private BufferedReader m_file;
+  private String m_readString;
+  private int m_readInx; // Index into a m_readString character.
 
   public CellularComponentAnnotationReader(final String speciesName,
                                            final Reader geneAssociationFile)
   {
+    if (geneAssociationFile == null)
+      throw new NullPointerException("geneAssociationFile is null");
+    m_file = new BufferedReader(geneAssociationFile);
+    m_readString = "(species=" + speciesName +
+      ") (type=Cellular Component) (curator=??)" + NL;
+    m_readInx = 0;
+  }
+
+  public final int read(final char[] cbuf,
+                        final int off,
+                        final int len) throws IOException
+  {
+    return -1;
+  }
+
+  /**
+   * Closes the underlying gene_association file stream as well.
+   */
+  public final void close() throws IOException
+  {
+    try {
+      m_file.close(); }
+    finally {
+      m_file = null; }
   }
 
 }
