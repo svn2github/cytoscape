9362b033983c663772e9d1ab377a3bdd0ed9d2a9
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-07 21:19:12 +0000

    Adding this new parser.
    git-svn-id: http://chianti.ucsd.edu/svn@3024 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/obo/src/cytoscape/cruft/obo/GeneAssociationReader.java
@@ -0,0 +1,12 @@
+package cytoscape.cruft.obo;
+
+import java.io.Reader;
+
+public final class GeneAssociationReader
+{
+
+  public GeneAssociationReader(final Reader geneAssociationFile)
+  {
+  }
+
+}
