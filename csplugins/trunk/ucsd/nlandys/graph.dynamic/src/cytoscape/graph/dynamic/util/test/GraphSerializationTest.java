64c2018e4a8f10d77a29c4c18c2c17929c16eb07
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-08 20:34:26 +0000

    Adding tests.
    git-svn-id: http://chianti.ucsd.edu/svn@3047 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/GraphSerializationTest.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/GraphSerializationTest.java
@@ -2,7 +2,9 @@ package cytoscape.graph.dynamic.util.test;
 
 import cytoscape.graph.dynamic.DynamicGraph;
 import cytoscape.graph.dynamic.util.DynamicGraphFactory;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class GraphSerializationTest
@@ -13,7 +15,16 @@ public class GraphSerializationTest
     DynamicGraph graph = DynamicGraphFactory.instantiateDynamicGraph();
     ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
     ObjectOutputStream objOut = new ObjectOutputStream(byteOut);
-    objOut.writeObject(graph);
+    objOut.writeObject(graph); objOut.flush(); objOut.close();
+    System.out.println("An empty graph takes " + byteOut.size() +
+                       " bytes in serialized form.");
+    ByteArrayInputStream byteIn =
+      new ByteArrayInputStream(byteOut.toByteArray());
+    ObjectInputStream objIn = new ObjectInputStream(byteIn);
+    graph = (DynamicGraph) objIn.readObject(); objIn.close();
+    if (graph.nodes().numRemaining() != 0 ||
+        graph.edges().numRemaining() != 0)
+      throw new IllegalStateException("expected restored graph to be empty");
   }
 
 }
