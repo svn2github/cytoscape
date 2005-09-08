94ed2e1a091c921305c746ffce2ecf6737e25c20
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-08 18:03:27 +0000

    This is where the test fails.
    git-svn-id: http://chianti.ucsd.edu/svn@3045 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/GraphSerializationTest.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/GraphSerializationTest.java
@@ -2,7 +2,18 @@ package cytoscape.graph.dynamic.util.test;
 
 import cytoscape.graph.dynamic.DynamicGraph;
 import cytoscape.graph.dynamic.util.DynamicGraphFactory;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
 
 public class GraphSerializationTest
 {
+
+  public static void main(String[] args) throws Exception
+  {
+    DynamicGraph graph = DynamicGraphFactory.instantiateDynamicGraph();
+    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+    ObjectOutputStream objOut = new ObjectOutputStream(byteOut);
+    objOut.writeObject(graph);
+  }
+
 }
