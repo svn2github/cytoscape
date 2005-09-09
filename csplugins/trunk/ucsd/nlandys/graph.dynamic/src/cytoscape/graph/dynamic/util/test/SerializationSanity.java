aeab398e82d4fb83eb1190cfe394eeeefaee3957
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-09 17:44:19 +0000

    Making the list doubly linked.
    git-svn-id: http://chianti.ucsd.edu/svn@3072 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/SerializationSanity.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/SerializationSanity.java
@@ -11,7 +11,7 @@ public class SerializationSanity implements Serializable
 
   public static void main(String[] args) throws Exception
   {
-    final int loopSize = 3;
+    final int loopSize = Integer.parseInt(args[0]);
     final SerializationSanity ss = new SerializationSanity();
     {
       final Node nodeLoop = new Node();
@@ -20,6 +20,11 @@ public class SerializationSanity implements Serializable
         currNode.m_next = new Node();
         currNode = currNode.m_next; }
       currNode.m_next = nodeLoop;
+      currNode = nodeLoop;
+      while (true) {
+        currNode.m_next.m_prev = currNode;
+        if (currNode.m_next == nodeLoop) break;
+        currNode = currNode.m_next; }
       ss.m_node = nodeLoop;
     }
 
@@ -33,6 +38,7 @@ public class SerializationSanity implements Serializable
 
   private static class Node implements Serializable {
     private Node m_next;
+    private Node m_prev;
     private Node() {} }
 
 }
