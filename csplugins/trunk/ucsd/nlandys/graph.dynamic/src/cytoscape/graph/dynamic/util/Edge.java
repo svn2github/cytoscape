3670d64150c5b455db26070ce87c299fd180c009
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-09 22:04:44 +0000

    Making Node and Edge serializable modulo pointers.
    git-svn-id: http://chianti.ucsd.edu/svn@3080 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/Edge.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/Edge.java
@@ -1,8 +1,21 @@
 package cytoscape.graph.dynamic.util;
 
 // Package visible.
-final class Edge implements java.io.Serializable
+final class Edge implements java.io.Externalizable
 {
+
+  public final void writeExternal(final java.io.ObjectOutput out)
+    throws java.io.IOException {
+    out.writeInt(edgeId);
+    out.writeBoolean(directed);
+    out.writeInt(sourceNode);
+    out.writeInt(targetNode); }
+  public final void readExternal(final java.io.ObjectInput in)
+    throws java.io.IOException {
+    edgeId = in.readInt();
+    directed = in.readBoolean();
+    sourceNode = in.readInt();
+    targetNode = in.readInt(); }
 
   int edgeId;
   Edge nextOutEdge;
