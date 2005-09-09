1e5669708065485bb2da39ea2c01bc9810dbfa28
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-09 17:28:08 +0000

    Implementing this test.
    git-svn-id: http://chianti.ucsd.edu/svn@3071 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/SerializationSanity.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/test/SerializationSanity.java
@@ -1,10 +1,38 @@
 package cytoscape.graph.dynamic.util.test;
 
-public class SerializationSanity
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializationSanity implements Serializable
 {
 
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
+    final int loopSize = 3;
+    final SerializationSanity ss = new SerializationSanity();
+    {
+      final Node nodeLoop = new Node();
+      Node currNode = nodeLoop;
+      for (int i = 1; i < loopSize; i++) {
+        currNode.m_next = new Node();
+        currNode = currNode.m_next; }
+      currNode.m_next = nodeLoop;
+      ss.m_node = nodeLoop;
+    }
+
+    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+    ObjectOutputStream objOut = new ObjectOutputStream(byteOut);
+    objOut.writeObject(ss); objOut.flush(); objOut.close();
   }
+
+  private Node m_node;
+  private SerializationSanity() {}
+
+  private static class Node implements Serializable {
+    private Node m_next;
+    private Node() {} }
 
 }
