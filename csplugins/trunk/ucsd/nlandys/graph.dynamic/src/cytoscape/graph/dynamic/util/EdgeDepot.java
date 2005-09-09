2c8d89867c503195a4aa52f6b601cc164ca03595
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-09 23:23:36 +0000

    Reverting to revision 1.5 modulo a couple of comments.
    git-svn-id: http://chianti.ucsd.edu/svn@3086 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/EdgeDepot.java
+++ csplugins/trunk/ucsd/nlandys/graph.dynamic/src/cytoscape/graph/dynamic/util/EdgeDepot.java
@@ -1,25 +1,8 @@
 package cytoscape.graph.dynamic.util;
 
 // Package visible.
-final class EdgeDepot implements java.io.Externalizable
+final class EdgeDepot
 {
-
-  // Externalizable.  Docs say instantiated using public no-arg constructor.
-  // No other references to the edges in this depot exist.
-  public final void writeExternal(final java.io.ObjectOutput out)
-    throws java.io.IOException {
-    for (Edge currEdge = m_head.nextOutEdge; currEdge != null;
-         currEdge = currEdge.nextOutEdge) out.writeInt(currEdge.edgeId);
-    out.writeInt(-1); }
-  public final void readExternal(final java.io.ObjectInput in)
-    throws java.io.IOException {
-    Edge currEdge = m_head;
-    while (true) {
-      final int id = in.readInt();
-      if (id < 0) break;
-      currEdge.nextOutEdge = new Edge();
-      currEdge = currEdge.nextOutEdge;
-      currEdge.edgeId = id; } }
 
   private final Edge m_head;
 
