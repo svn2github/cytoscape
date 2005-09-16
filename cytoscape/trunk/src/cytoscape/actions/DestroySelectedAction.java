85888ec4800b9e672abae7f928e01599b03bc863
akuchinsky <akuchinsky@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-16 15:12:06 +0000

    fire a network modified event
    git-svn-id: http://chianti.ucsd.edu/svn@6637 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/DestroySelectedAction.java
+++ cytoscape/trunk/src/cytoscape/actions/DestroySelectedAction.java
@@ -1,8 +1,10 @@
-//-------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 package cytoscape.actions;
+
 //-------------------------------------------------------------------------
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import javax.swing.undo.AbstractUndoableEdit;
 
 import giny.model.*;
 import giny.view.*;
@@ -11,41 +13,48 @@ import java.util.*;
 import cytoscape.Cytoscape;
 import cytoscape.CyNetwork;
 import cytoscape.util.CytoscapeAction;
+
 //-------------------------------------------------------------------------
-public class DestroySelectedAction extends CytoscapeAction  {
+public class DestroySelectedAction extends CytoscapeAction {
 
-    public DestroySelectedAction () {
-        super( "Destroy Selected Nodes/Edges");
-        setPreferredMenu( "Edit" );
-    }
-    
-    public DestroySelectedAction ( boolean label) {
-        super(  );
-    }
-    
-    public void actionPerformed (ActionEvent e) {
-	CyNetwork gp = Cytoscape.getCurrentNetwork();
-	Set flaggedNodes = gp.getFlaggedNodes();
-	Set flaggedEdges = gp.getFlaggedEdges();
-	int [] hiddenNodeIndices = new int [flaggedNodes.size()];
-	int [] hiddenEdgeIndices = new int [flaggedEdges.size()];
-	
-	int j=0;
-	for (Iterator i = flaggedNodes.iterator(); i.hasNext(); ) {
-	    hiddenNodeIndices[j++] = gp.getIndex((Node) i.next());
-	}
-	j=0;
-	for (Iterator i = flaggedEdges.iterator(); i.hasNext(); ) {
-	    hiddenEdgeIndices[j++] = gp.getIndex((Edge) i.next());
+	public DestroySelectedAction() {
+		super("Destroy Selected Nodes/Edges");
+		setPreferredMenu("Edit");
 	}
 
-	// unflag then hide nodes from graph perspective
-	gp.unFlagAllNodes();
-	gp.unFlagAllEdges();
-	gp.hideEdges(hiddenEdgeIndices);
-	gp.hideNodes(hiddenNodeIndices);
-	    
-    }//action performed
+	public DestroySelectedAction(boolean label) {
+		super();
+	}
 
+	public void actionPerformed(ActionEvent e) {
+		CyNetwork gp = Cytoscape.getCurrentNetwork();
+		Set flaggedNodes = gp.getFlaggedNodes();
+		Set flaggedEdges = gp.getFlaggedEdges();
+		int[] hiddenNodeIndices = new int[flaggedNodes.size()];
+		int[] hiddenEdgeIndices = new int[flaggedEdges.size()];
+
+		int j = 0;
+		for (Iterator i = flaggedNodes.iterator(); i.hasNext();) {
+			hiddenNodeIndices[j++] = gp.getIndex((Node) i.next());
+		}
+		j = 0;
+		for (Iterator i = flaggedEdges.iterator(); i.hasNext();) {
+			hiddenEdgeIndices[j++] = gp.getIndex((Edge) i.next());
+		}
+
+		// unflag then hide nodes from graph perspective
+		gp.unFlagAllNodes();
+		gp.unFlagAllEdges();
+		gp.hideEdges(hiddenEdgeIndices);
+		gp.hideNodes(hiddenNodeIndices);
+
+		// AJK: 09/14/05 BEGIN
+		//      fire a NETWORK_MODIFIED event
+
+		Cytoscape.firePropertyChange(Cytoscape.NETWORK_MODIFIED, null, gp);
+
+
+		// AJK: 09/14/05 END
+	}//action performed
 }
 
