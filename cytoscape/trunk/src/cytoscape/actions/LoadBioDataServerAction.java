7cb73b7ce87ee179af1fb5fe4a9310121199a4e3
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-09-19 10:34:27 +0000

    New biodataserver loader with wizard.
    git-svn-id: http://chianti.ucsd.edu/svn@6650 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/LoadBioDataServerAction.java
+++ cytoscape/trunk/src/cytoscape/actions/LoadBioDataServerAction.java
@@ -5,45 +5,44 @@
 package cytoscape.actions;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import javax.swing.AbstractAction;
-import javax.swing.JFileChooser;
 import java.io.File;
 
 import cytoscape.Cytoscape;
-import cytoscape.util.*;
-import cytoscape.data.Semantics;
-import cytoscape.data.servers.BioDataServer;
-import cytoscape.data.Semantics;
+import cytoscape.util.CyFileFilter;
+import cytoscape.util.CytoscapeAction;
+import cytoscape.util.FileUtil;
+import cytoscape.util.swing.BioDataServerWizard;
 
 /**
  * Action allows the loading of a BioDataServer from the gui.
- *
+ * 
  * added by dramage 2002-08-20
+ * 
+ * Mod by kono 2005-09-16
+ * Added new biodataserver wizard
  */
 public class LoadBioDataServerAction extends CytoscapeAction {
-  
-    
-  public LoadBioDataServerAction () {
-    super("Bio Data Server...");
-    setPreferredMenu( "File.Load" );
-  }
 
-  public void actionPerformed(ActionEvent e) {
-   
-    // get the file name
-    final String name;
-    try {
-      name = FileUtil.getFile( "Load BioDataServer ",
-                               FileUtil.LOAD,
-                               new CyFileFilter[] {} ).toString();
-    } catch ( Exception exp ) {
-      // this is because the selection was canceled
-      return;
-    } 
-    Cytoscape.loadBioDataServer( name );
-    
-  }
+	BioDataServerWizard wiz;
+	
+	public LoadBioDataServerAction() {
+		super("Bio Data Server...");
+		setPreferredMenu("File.Load");
+	}
+
+	/*
+	 *  (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+
+		// Get the file name
+		final String name;
+		int wizardResult;
+
+		// Create the wizard to choose biodataserver
+		wiz = new BioDataServerWizard();
+		wizardResult = wiz.show();
+		System.out.println( "Wizard returned: " + wizardResult );
+	}
 }
-
-
