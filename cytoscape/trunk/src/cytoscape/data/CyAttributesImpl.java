b4ace0f923574e84893648492860b05d3366c41d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-10-11 17:06:33 +0000

    Beginning implementation of CyAttributes.
    git-svn-id: http://chianti.ucsd.edu/svn@6729 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/trunk/src/cytoscape/data/CyAttributesImpl.java
@@ -0,0 +1,101 @@
+package cytoscape.data;
+
+import cytoscape.data.attr.MultiHashMap;
+import cytoscape.data.attr.MultiHashMapDefinition;
+
+import java.util.List;
+import java.util.Map;
+
+public class CyAttributesImpl implements CyAttributes
+{
+
+  public String[] getAttributeNames()
+  {
+    return null;
+  }
+
+  public boolean hasAttribute(String id, String attributeName)
+  {
+    return false;
+  }
+
+  public void setAttribute(String id, String attributeName, Boolean value)
+  {
+  }
+
+  public void setAttribute(String id, String attributeName, Integer value)
+  {
+  }
+
+  public void setAttribute(String id, String attributeName, Double value)
+  {
+  }
+
+  public void setAttribute(String id, String attributeName, String value)
+  {
+  }
+
+  public Boolean getBooleanAttribute(String id, String attributeName)
+  {
+    return null;
+  }
+
+  public Integer getIntegerAttribute(String id, String attributeName)
+  {
+    return null;
+  }
+
+  public Double getDoubleAttribute(String id, String attributeName)
+  {
+    return null;
+  }
+
+  public String getStringAttribute(String id, String attributeName)
+  {
+    return null;
+  }
+
+  public byte getType(String attributeName)
+  {
+    return 0;
+  }
+
+  public boolean deleteAttribute(String id, String attributeName)
+  {
+    return false;
+  }
+
+  public boolean deleteAttribute(String attributeName)
+  {
+    return false;
+  }
+
+  public void setAttributeList(String id, String attributeName, List list)
+  {
+  }
+
+  public List getAttributeList(String id, String attributeName)
+  {
+    return null;
+  }
+
+  public void setAttributeMap(String id, String attributeName, Map map)
+  {
+  }
+
+  public Map getAttributeMap(String id, String attributeName)
+  {
+    return null;
+  }
+
+  public MultiHashMap getMultiHashMap()
+  {
+    return null;
+  }
+
+  public MultiHashMapDefinition getMultiHashMapDefinition()
+  {
+    return null;
+  }
+
+}
