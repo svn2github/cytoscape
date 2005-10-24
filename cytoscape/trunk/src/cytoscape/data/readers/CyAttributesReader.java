f5e3188c359d4069affc02119f225c0ba272a003
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-10-24 21:09:36 +0000

    Making progress with the reader.
    git-svn-id: http://chianti.ucsd.edu/svn@6838 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/data/readers/CyAttributesReader.java
+++ cytoscape/trunk/src/cytoscape/data/readers/CyAttributesReader.java
@@ -1,8 +1,11 @@
 package cytoscape.data.readers;
 
 import cytoscape.data.CyAttributes;
+import cytoscape.data.attr.MultiHashMapDefinition;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.StringTokenizer;
 
 public class CyAttributesReader
 {
@@ -10,6 +13,33 @@ public class CyAttributesReader
   public static void loadAttributes(CyAttributes cyAttrs,
                                     Reader fileIn) throws IOException
   {
+    final BufferedReader reader;
+    if (fileIn instanceof BufferedReader) { reader = (BufferedReader) fileIn; }
+    else { reader = new BufferedReader(fileIn); }
+    final String attributeName;
+    byte type = -1;
+    {
+      final String firstLine = reader.readLine();
+      if (firstLine == null) { return; }
+      final StringTokenizer tokens = new StringTokenizer(firstLine);
+      attributeName = tokens.nextToken();
+      if (tokens.hasMoreTokens()) {
+        final String foo = tokens.nextToken();
+        final String searchStr = "class=";
+        final int inx = foo.indexOf(searchStr);
+        if (inx >= 0) {
+          String className = foo.substring(inx + searchStr.length()).trim();
+          if (className.endsWith(")")) {
+            className = className.substring(0, className.length() - 1); }
+          if (className.equalsIgnoreCase("java.lang.String")) {
+            type = MultiHashMapDefinition.TYPE_STRING; }
+          else if (className.equalsIgnoreCase("java.lang.Boolean")) {
+            type = MultiHashMapDefinition.TYPE_BOOLEAN; }
+          else if (className.equalsIgnoreCase("java.lang.Integer")) {
+            type = MultiHashMapDefinition.TYPE_INTEGER; }
+          else if (className.equalsIgnoreCase("java.lang.Double")) {
+            type = MultiHashMapDefinition.TYPE_FLOATING_POINT; } } }
+    }
   }
 
 }
