4966032bef5c7a665497c4cc391d1fcd60f77bfe
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-11-09 19:17:05 +0000

    Adding method stubs.
    git-svn-id: http://chianti.ucsd.edu/svn@3244 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/TextRenderingUtils.java
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/TextRenderingUtils.java
@@ -1,6 +1,8 @@
 package cytoscape.render.stateful;
 
+import cytoscape.render.immed.GraphGraphics;
 import java.awt.Font;
+import java.awt.Paint;
 
 final class TextRenderingUtils
 {
@@ -17,6 +19,18 @@ final class TextRenderingUtils
                                                  final float[] rtrnVal2x)
   {
     
+  }
+
+  public final static void renderHorizontalText(final GraphGraphics grafx,
+                                                final String text,
+                                                final Font font,
+                                                final double fontScaleFactor,
+                                                final float textXCenter,
+                                                final float textYCenter,
+                                                final byte textJustify,
+                                                final Paint paint,
+                                                final boolean textAsShape)
+  {
   }
 
 }
