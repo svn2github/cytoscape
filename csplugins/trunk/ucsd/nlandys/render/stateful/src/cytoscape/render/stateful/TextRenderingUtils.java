a2f992d80de7372ccc8a5a08bdf6fd4974be739f
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-11-09 19:11:13 +0000

    Starting a new package-visible class.
    git-svn-id: http://chianti.ucsd.edu/svn@3243 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/render/stateful/src/cytoscape/render/stateful/TextRenderingUtils.java
@@ -0,0 +1,22 @@
+package cytoscape.render.stateful;
+
+import java.awt.Font;
+
+final class TextRenderingUtils
+{
+
+  // No constructor.
+  private TextRenderingUtils() { }
+
+  /**
+   * @param text potentially multi-line text.
+   */
+  public final static void computeTextDimensions(final String text,
+                                                 final Font font,
+                                                 final double fontScaleFactor,
+                                                 final float[] rtrnVal2x)
+  {
+    
+  }
+
+}
