ca3953f95e2271cbe0176e607cb0ad3c5518a76a
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-11-22 22:29:50 +0000

    *** empty log message ***
    git-svn-id: http://chianti.ucsd.edu/svn@4463 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/cyto_2_2_features.php
+++ cyto_web/trunk/cyto_2_2_features.php
@@ -42,13 +42,13 @@
 		</p>
 	<ul>
   		<li>
-			CytoPanel 1 (appears on the left)
+			CytoPanel 1 (appears on the left).  By default, it has Network Tree Browser panel.
 		</li>
   		<li>
-  			CytoPanel 2 (appears on the bottom)
+  			CytoPanel 2 (appears on the bottom).  Node/Edge attribute browser will be shown here.
 		</li>
 		<li>
-			CytoPanel 3 (appears on the right)
+			CytoPanel 3 (appears on the right).  Mainly for advanced features.
 		</li>
 	</ul>
 	</td>
@@ -59,18 +59,8 @@
 <tr>
 	<td>
 		<big><b>Improved node/edge attribute browsing:</b></big>
-		<p>Improved Attribute Browser is available for 2.2.  It is on CytoPanel 2 </p>
-	<ul>
-  		<li>
-  			Large graphs are now supported (&gt;100K
-			nodes, edges, depending on system)
-		</li>
-  		<li>
-  			The 2.2 release features significant performance 
-  			improvements for graph loading, creation, and miscellaneous
-			graph operations (&gt; 2 - 10x improvements for some operations)
-		</li>
-	</ul>
+		<p>Improved Attribute Browser is on CytoPanel 2.  Users can browse, edit, 
+		and export attributes in the table.</p>
 	</td>
 </tr>
 </table>
@@ -79,18 +69,7 @@
 <tr>
 	<td>
 		<big><b>Cytoscape Graph Editor v1.0</b></big>
-		<p>Performance Optimization of Graph Model:</p>
-	<ul>
-  		<li>
-  			Large graphs are now supported (&gt;100K
-			nodes, edges, depending on system)
-		</li>
-  		<li>
-  			The 2.2 release features significant performance 
-  			improvements for graph loading, creation, and miscellaneous
-			graph operations (&gt; 2 - 10x improvements for some operations)
-		</li>
-	</ul>
+		<p>New network files can be created visually by Cytoscape Editor.</p>
 	</td>
 </tr>
 </table>
@@ -100,18 +79,20 @@
 <tr>
 	<td>
 		<big><b>Support for Gene Ontology OBO and gene annotation files</b></big>
-		<p>Performance Optimization of Graph Model:</p>
+		<p>Now Cytoscape supports standard ontology and annotation file formats:</p>
 	<ul>
   		<li>
-  			Large graphs are now supported (&gt;100K
-			nodes, edges, depending on system)
+  			<a href="http://www.geneontology.org/GO.downloads.shtml#ont" target="_blank" >
+				OBO Ontology File
+			</a>
 		</li>
   		<li>
-  			The 2.2 release features significant performance 
-  			improvements for graph loading, creation, and miscellaneous
-			graph operations (&gt; 2 - 10x improvements for some operations)
+  			<a href="http://www.geneontology.org/GO.current.annotations.shtml" target="_blank">
+  				Gene Association (annotation) File
+  			</a>
 		</li>
 	</ul>
+	Through the new Gene Ontology Server Wizard, users can select those files from GUI. 
 	</td>
 </tr>
 </table>
@@ -122,18 +103,7 @@
 <tr>
 	<td>
 		<big><b>Independent internal network windows for easy comparison</b></big>
-		<p>Performance Optimization of Graph Model:</p>
-	<ul>
-  		<li>
-  			Large graphs are now supported (&gt;100K
-			nodes, edges, depending on system)
-		</li>
-  		<li>
-  			The 2.2 release features significant performance 
-  			improvements for graph loading, creation, and miscellaneous
-			graph operations (&gt; 2 - 10x improvements for some operations)
-		</li>
-	</ul>
+		<p>Ecah network is displayed in a independent window.</p>
 	</td>
 </tr>
 </table>
@@ -143,18 +113,10 @@
 <tr>
 	<td>
 		<big><b>New GML visual style to manage visual attributes from GML files</b></big>
-		<p>Performance Optimization of Graph Model:</p>
-	<ul>
-  		<li>
-  			Large graphs are now supported (&gt;100K
-			nodes, edges, depending on system)
-		</li>
-  		<li>
-  			The 2.2 release features significant performance 
-  			improvements for graph loading, creation, and miscellaneous
-			graph operations (&gt; 2 - 10x improvements for some operations)
-		</li>
-	</ul>
+		<p>
+			When a gml file is loaded into Cytoscape, its visual properties will be registered
+			as a new visual style. 
+		</p>
 	</td>
 </tr>
 </table>
@@ -163,51 +125,84 @@
 
 
 <table style="margin-left: 30;margin-right:30;border: dotted gray 1px;padding-left: 10px;font-size:small" cellpadding="5" cellspacing="5" width="90%">
-<tr>
-<td>
-<big><b>Core Plug-Ins:</b></big>
-<p>Five "core" plug-ins are bundled and distributed with Cytoscape 2.1. These
-plug-ins offer fundamental operations of value to many users,
-and are included in the basic distribution.
-<BR>
-<center>
-<table style="margin-left: 30;margin-right:30;padding-left: 10px;font-size:small" cellpadding="5" cellspacing="5" width="90%">
-    <td>
-    <p><b>Plug-In Name</b></p>
-    </td>
-    <td width="172">
-    <p><b>JAR file</b></p>
-    </td>
-    <td style="width: 281px;">
-    <p><b>Description</b></p>
-    </td>
-  </tr>
-  <tr valign="top">
-      <td width="132">
-      <p>Filter</p>
-      </td>
-      <td width="172">
-      <p>filter.jar</p>
-      </td>
-      <td width="281">
-      <p>Provides
-filtering functionality; adds filter icon, filters menu and filtering
-dialog box.</p>
-      </td>
-    </tr>
-    <tr valign="top">
-      <td style="width: 132px;">
-      <p>yFiles Layouts</p>
-      </td>
-      <td width="172">
-      <p>yLayouts.jar</p>
-      </td>
-      <td style="width: 281px;">
-      <p>Provides yFiles layouts functionality; adds
-yFiles submenu to layout menu.</p>
-Layouts provided:<br>
-&nbsp; - Circular<br>
-&nbsp; - Organic<br>
+	<tr>
+		<td>
+			<big><b>Core Plug-Ins:</b></big>
+			<p>
+				Five "core" plug-ins are bundled and distributed with Cytoscape 2.2. These
+				plug-ins offer fundamental operations of value to many users,
+				and are included in the basic distribution.
+			</p>
+			<center>
+			<table style="margin-left: 30;margin-right:30;padding-left: 10px;font-size:small" cellpadding="5" cellspacing="5" width="90%">
+    		<tr>
+    			<td>
+    				<p><b>Plug-In Name</b></p>
+    			</td>
+    			<td width="172">
+    				<p><b>JAR file</b></p>
+    			</td>
+    			<td style="width: 281px;">
+    				<p><b>Description</b></p>
+    			</td>
+  			</tr>
+  			<tr valign="top">
+      			<td width="132">
+      				<p>Filter</p>
+      			</td>
+      			<td width="172">
+      				<p>filter.jar</p>
+      			</td>
+      			<td width="281">
+      				<p>
+      					Provides filtering functionality; adds filter icon, filters 
+      					menu and filtering dialog box.
+      				</p>
+      			</td>
+    		</tr>
+    		<tr valign="top">
+      			<td width="132">
+      				<p>Attribute Browser</p>
+      			</td>
+      			<td width="172">
+      				<p>browser.jar</p>
+      			</td>
+      			<td width="281">
+      				<p>
+      					Provides attribute browser functionality; adds browser menu item under Data.
+      				</p>
+      			</td>
+    		</tr>
+    		<tr valign="top">
+      			<td width="132">
+      				<p>Cytoscape Editor</p>
+      			</td>
+      			<td width="172">
+      				<p>CytoscapeEditor.jar</p>
+      			</td>
+      			<td width="281">
+      				<p>
+      					Provides network editor functionality; adds setEditor menu item under File.
+      				</p>
+      			</td>
+    		</tr>
+    		
+    		
+    		<tr valign="top">
+      			<td style="width: 132px;">
+      				<p>yFiles Layouts</p>
+      			</td>
+      			<td width="172">
+      				<p>yLayouts.jar</p>
+      			</td>
+      			<td style="width: 281px;">
+      				<p>
+      					Provides yFiles layouts functionality; adds
+						yFiles submenu to layout menu.
+					</p>
+					Layouts provided:<br>
+					&nbsp; - Circular<br>
+					&nbsp; - Organic<br>
 &nbsp; - Hierarchic<br>
 &nbsp; - Random<br>
 &nbsp; - MirrorX<br>
