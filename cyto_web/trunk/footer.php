24dfe4438e9bee2f7adc81a1780534cf454c8702
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-12-01 07:18:58 +0000

    NSF added to the footer.
    git-svn-id: http://chianti.ucsd.edu/svn@4476 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/footer.php
+++ cyto_web/trunk/footer.php
@@ -1,7 +1,8 @@
 <div id="footer">
 <b>Funding for Cytoscape is provided by a federal grant from the U.S.
 <a href="http://www.nigms.nih.gov">National Institute of General Medical Sciences (NIGMS)</a> of the 
-<a href="http://www.nih.gov">National Institutes of Health (NIH)</a> under award number GM070743-01.
+<a href="http://www.nih.gov">National Institutes of Health (NIH)</a> under award number GM070743-01 and 
+the U.S. <a href="http://www.nsf.gov">National Science Foundation (NSF) </a>.
 Corporate funding is provided through a contract from
 <a href="http://www.unilever.com">Unilever PLC</a>. 
 </b>
