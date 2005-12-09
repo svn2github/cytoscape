6faf7c25ebded5e4404c6ac6542c26ddc62453e1
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-12-09 17:30:41 +0000

    Making progress on this test.  Adding placeholders mostly.
    git-svn-id: http://chianti.ucsd.edu/svn@3295 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/SampleNetwork1.java
+++ csplugins/trunk/ucsd/nlandys/render/test/src/cytoscape/render/test/SampleNetwork1.java
@@ -1,8 +1,14 @@
 package cytoscape.render.test;
 
+import cytoscape.geom.rtree.RTree;
+import cytoscape.graph.dynamic.DynamicGraph;
 import cytoscape.graph.dynamic.util.DynamicGraphFactory;
 import java.awt.EventQueue;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
@@ -26,11 +32,82 @@ public class SampleNetwork1
 
   private final int m_imgWidth = 800;
   private final int m_imgHeight = 600;
+  private final DynamicGraph m_graph;
+  private final RTree m_rtree;
+  private final Image m_img;
 
   public SampleNetwork1()
   {
     super();
     m_graph = DynamicGraphFactory.instantiateDynamicGraph();
+    m_rtree = new RTree();
+    addNotify();
+    m_img = createImage(m_imgWidth, m_imgHeight);
   }
+
+  public void paint(Graphics g)
+  {
+    final Insets insets = insets();
+    g.drawImage(m_img, insets.left, insets.top, null);
+    resize(m_imgWidth + insets.left + insets.right,
+           m_imgHeight + insets.top + insets.bottom);
+  }
+
+  public void update(Graphics g)
+  {
+    final Insets insets = insets();
+    updateImage();
+    g.drawImage(m_img, insets.left, insets.top, null);
+  }
+
+  private void updateImage()
+  {
+  }
+
+  public void mouseClicked(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {}
+
+  public void mousePressed(MouseEvent e)
+  {
+//     if (e.getButton() == MouseEvent.BUTTON3) {
+//       m_currMouseButton = 3;
+//       m_lastXMousePos = e.getX();
+//       m_lastYMousePos = e.getY(); }
+//     else if (e.getButton() == MouseEvent.BUTTON2) {
+//       m_currMouseButton = 2;
+//       m_lastXMousePos = e.getX();
+//       m_lastYMousePos = e.getY(); }
+  }
+
+  public void mouseReleased(MouseEvent e)
+  {
+//     if (e.getButton() == MouseEvent.BUTTON3) {
+//       if (m_currMouseButton == 3) m_currMouseButton = 0; }
+//     else if (e.getButton() == MouseEvent.BUTTON2) {
+//       if (m_currMouseButton == 2) m_currMouseButton = 0; }
+  }
+
+  public void mouseDragged(MouseEvent e)
+  {
+//     if (m_currMouseButton == 3) {
+//       double deltaX = e.getX() - m_lastXMousePos;
+//       double deltaY = e.getY() - m_lastYMousePos;
+//       m_lastXMousePos = e.getX();
+//       m_lastYMousePos = e.getY();
+//       m_currXCenter -= deltaX / m_currScale;
+//       m_currYCenter += deltaY / m_currScale; // y orientations are opposite.
+//       repaint(); }
+//     else if (m_currMouseButton == 2) {
+//       double deltaY = e.getY() - m_lastYMousePos;
+//       m_lastXMousePos = e.getX();
+//       m_lastYMousePos = e.getY();
+//       m_currScale *= Math.pow(2, -deltaY / 300.0d);
+//       repaint(); }
+  }
+
+  public void mouseMoved(MouseEvent e) {}
+
+  public boolean isResizable() { return false; }
 
 }
