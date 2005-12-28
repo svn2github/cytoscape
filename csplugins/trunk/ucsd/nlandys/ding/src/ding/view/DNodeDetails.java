3852eb651f6d17f9f82848e38b18df0049a870e3
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2005-12-28 21:19:13 +0000

    Adding implementation.
    git-svn-id: http://chianti.ucsd.edu/svn@3337 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/ding/src/ding/view/DNodeDetails.java
+++ csplugins/trunk/ucsd/nlandys/ding/src/ding/view/DNodeDetails.java
@@ -1,7 +1,33 @@
 package ding.view;
 
 import cytoscape.render.stateful.NodeDetails;
+import java.util.HashMap;
 
+/*
+ * Access to the methods of this class should be synchronized externally if
+ * there is a threat of multiple threads.
+ */
 class DNodeDetails extends NodeDetails
 {
+
+  // The values are Byte objects; the bytes are shapes defined in
+  // cytoscape.render.immed.GraphGraphics.
+  final HashMap m_shapes = new HashMap();
+
+  public byte shape(int node)
+  {
+    final Object o = m_shapes.get(new Integer(node));
+    if (o == null) { return super.shape(node); }
+    return ((Byte) o).byteValue();
+  }
+
+  /*
+   * The shape argument must be pre-checked for correctness.
+   */
+  void overrideShape(int node, byte shape)
+  {
+    if (super.shape(node) == shape) { m_shapes.remove(new Integer(node)); }
+    m_shapes.put(new Integer(node), new Byte(shape));
+  }
+
 }
