ce5d3429168fff0538c6520570a40f8fe0b0e0ed
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-01-26 00:50:13 +0000

    Jotting down some revised ideas.
    git-svn-id: http://chianti.ucsd.edu/svn@3486 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/TopologyChangeListener.java
+++ csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/TopologyChangeListener.java
@@ -3,6 +3,9 @@ package cytoscape.fung;
 public interface TopologyChangeListener
 {
 
-  public void topologyChanged(TopologyChangeEvent evt);
+  public void nodeCreated(int node);
+  public void nodeRemoved(int node);
+  public void edgeCreated(int edge);
+  public void edgeRemoved(int edge);
 
 }
