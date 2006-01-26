a6b4db784f7003bb599847d1e72d5663212f479d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-01-26 20:43:38 +0000

    Adding the edge and node hide events.
    git-svn-id: http://chianti.ucsd.edu/svn@3495 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/ding/src/ding/view/GraphViewEdgesHiddenEvent.java
@@ -0,0 +1,40 @@
+package ding.view;
+
+import giny.model.Edge;
+import giny.view.GraphView;
+
+final class GraphViewEdgesHiddenEvent extends GraphViewChangeEventAdapter
+{
+
+  private final GraphView m_view;
+  private final int[] m_hiddenEdgeInx;
+
+  GraphViewEdgesHiddenEvent(GraphView view, int[] hiddenEdgeInx)
+  {
+    super(view);
+    m_view = view;
+    m_hiddenEdgeInx = hiddenEdgeInx;
+  }
+
+  public final int getType()
+  {
+    return EDGES_HIDDEN_TYPE;
+  }
+
+  public final Edge[] getHiddenEdges()
+  {
+    final Edge[] returnThis = new Edge[m_hiddenEdgeInx.length];
+    for (int i = 0; i < returnThis.length; i++) {
+      returnThis[i] = m_view.getRootGraph().getEdge(m_hiddenEdgeInx[i]); }
+    return returnThis;
+  }
+
+  public final int[] getHiddenEdgeIndices()
+  {
+    final int[] returnThis = new int[m_hiddenEdgeInx.length];
+    for (int i = 0; i < returnThis.length; i++) {
+      returnThis[i] = m_hiddenEdgeInx[i]; }
+    return returnThis;
+  }
+
+}
