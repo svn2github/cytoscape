8f6a0b875622bc043842b7b95d91162a328581cd
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-01-27 23:18:49 +0000

    Adding API encapsulating extents of node.
    git-svn-id: http://chianti.ucsd.edu/svn@3511 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/NodeView.java
+++ csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/NodeView.java
@@ -1,5 +1,37 @@
 package cytoscape.fung;
 
+import java.awt.geom.Dimension2D;
+import java.awt.geom.Point2D;
+
 public final class NodeView
 {
+
+  final int m_node;
+
+  NodeView(final int node)
+  {
+    m_node = node;
+  }
+
+  public final int getNode()
+  {
+    return m_node;
+  }
+
+  public final void getLocation(final Point2D p)
+  {
+  }
+
+  public final void setLocation(final Point2D p)
+  {
+  }
+
+  public final void getSize(final Dimension2D d)
+  {
+  }
+
+  public final void setSize(final Dimension2D d)
+  {
+  }
+
 }
