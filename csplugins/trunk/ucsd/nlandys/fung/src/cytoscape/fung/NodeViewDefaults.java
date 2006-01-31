0fa637c5f8b40b457fe1e606fe31fff3c353ab54
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-01-31 02:47:44 +0000

    Starting a new way of specifying defaults.
    git-svn-id: http://chianti.ucsd.edu/svn@3529 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/NodeViewDefaults.java
@@ -0,0 +1,72 @@
+package cytoscape.fung;
+
+import java.awt.Color;
+import java.awt.Paint;
+
+public final class NodeViewDefaults
+{
+
+  public final static double DEFAULT_WIDTH = 10.0d;
+  public final static double DEFAULT_HEIGHT = 10.0d;
+  public final static Color DEFAULT_COLOR_LOW_DETAIL = Color.red;
+  public final static byte DEFAULT_SHAPE = NodeView.SHAPE_ELLIPSE;
+  public final static Paint DEFAULT_FILL_PAINT = Color.red;
+  public final static double DEFAULT_BORDER_WIDTH = 1.0d;
+  public final static Paint DEFAULT_BORDER_PAINT = Color.black;
+
+  final float m_widthDiv2;
+  final float m_heightDiv2;
+  final Color m_colorLowDetail;
+  final byte m_shape;
+  final Paint m_fillPaint;
+  final double m_borderWidth;
+  final Paint m_borderPaint;
+
+  public NodeViewDefaults()
+  {
+    this(DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_COLOR_LOW_DETAIL,
+         DEFAULT_SHAPE, DEFAULT_FILL_PAINT, DEFAULT_BORDER_WIDTH,
+         DEFAULT_BORDER_PAINT);
+  }
+
+  public NodeViewDefaults(final double width,
+                          final double height,
+                          final Color colorLowDetail,
+                          final byte shape,
+                          final Paint fillPaint,
+                          final double borderWidth,
+                          final Paint borderPaint)
+  {
+    m_widthDiv2 = (float) (width / 2.0d);
+    if (!(m_widthDiv2 > 0.0f)) {
+      throw new IllegalArgumentException("width is too small"); }
+    m_heightDiv2 = (float) (height / 2.0d);
+    if (!(m_heightDiv2 > 0.0f)) {
+      throw new IllegalArgumentException("height is too small"); }
+    m_colorLowDetail = colorLowDetail;
+    if (m_colorLowDetail == null) {
+      throw new NullPointerException("colorLowDetail is null"); }
+    m_shape = shape;
+    switch (m_shape) {
+    case NodeView.SHAPE_RECTANGLE:
+    case NodeView.SHAPE_DIAMOND:
+    case NodeView.SHAPE_ELLIPSE:
+    case NodeView.SHAPE_HEXAGON:
+    case NodeView.SHAPE_OCTAGON:
+    case NodeView.SHAPE_PARALLELOGRAM:
+    case NodeView.SHAPE_ROUNDED_RECTANGLE:
+    case NodeView.SHAPE_TRIANGLE:
+      break;
+    default:
+      throw new IllegalArgumentException("shape is unrecognized"); }
+    m_fillPaint = fillPaint;
+    if (m_fillPaint == null) {
+      throw new NullPointerException("fillPaint is null"); }
+    m_borderWidth = borderWidth;
+    // TODO: Error check border width against size and shape combinations.
+    m_borderPaint = borderPaint;
+    if (m_borderPaint == null) {
+      throw new NullPointerException("borderPaint is null"); }
+  }
+
+}
