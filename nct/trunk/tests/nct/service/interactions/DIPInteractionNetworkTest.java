416f9031b9dc98276c9eb971786434cc594b3d1c
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-01-31 21:10:55 +0000

    DIPInteractionNetworkTest.java
    git-svn-id: http://chianti.ucsd.edu/svn@3993 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ nct/trunk/tests/nct/service/interactions/DIPInteractionNetworkTest.java
@@ -0,0 +1,51 @@
+
+//============================================================================
+// 
+//  file: DIPInteractionNetworkTest.java 
+// 
+//  Copyright (c) 2006, University of California, San Diego
+//  All rights reverved.
+// 
+//============================================================================
+
+package nct.service.interactions;
+
+import junit.framework.*;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.io.FileReader;
+
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import nct.graph.*;
+import nct.graph.basic.*;
+
+// A JUnit test class for DIPInteractionNetworkTest.java
+public class DIPInteractionNetworkTest extends TestCase {
+	Graph<String,Double> chicken;
+	DIPInteractionNetwork chickenNet;
+	protected void setUp() {
+
+		chicken = new BasicGraph<String,Double>();
+		chickenNet = new DIPInteractionNetwork("Gallus gallus");
+		chickenNet.updateGraph(chicken);
+
+		try {
+		XMLReader xr = XMLReaderFactory.createXMLReader();
+		xr.setContentHandler(chickenNet);
+		xr.parse(new InputSource(new FileReader("examples/Gallus_gallus.xin")));
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+
+	public void testParse() {
+		assertTrue("expect 8 nodes, got: " + chicken.numberOfNodes(),chicken.numberOfNodes() == 8);
+		assertTrue("expect 1 edge, got: " + chicken.numberOfEdges(),chicken.numberOfEdges() == 1);
+	}
+   
+	public static Test suite() {
+		return new TestSuite(DIPInteractionNetworkTest.class);
+	}
+}
