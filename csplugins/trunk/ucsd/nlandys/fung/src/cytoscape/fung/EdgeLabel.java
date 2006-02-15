a0ae3c9bd7c3fd53353a03b9e7963b2f65e7b613
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-02-15 19:27:32 +0000

    Adding EdgeLabel.
    git-svn-id: http://chianti.ucsd.edu/svn@3567 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/EdgeLabel.java
@@ -0,0 +1,39 @@
+package cytoscape.fung;
+
+import cytoscape.render.stateful.EdgeDetails;
+import java.awt.Font;
+import java.awt.Paint;
+import java.awt.geom.Point2D;
+
+public final class EdgeLabel extends Label
+{
+
+  public static final byte EDGE_ANCHOR_MIDPOINT =
+    EdgeDetails.EDGE_ANCHOR_MIDPOINT;
+  public static final byte EDGE_ANCHOR_SOURCE =
+    EdgeDetails.EDGE_ANCHOR_SOURCE;
+  public static final byte EDGE_ANCHOR_TARGET =
+    EdgeDetails.EDGE_ANCHOR_TARGET;
+
+  final byte m_edgeAnchor;
+
+  public EdgeLabel(final String text,
+                   final Font font,
+                   final double scaleFactor,
+                   final Paint paint,
+                   final byte textAnchor,
+                   final byte edgeAnchor,
+                   final Point2D offsetVector,
+                   final byte justify)
+  {
+    super(text, font, scaleFactor, paint, textAnchor,
+          offsetVector.getX(), offsetVector.getY(), justify);
+    m_edgeAnchor = edgeAnchor;
+  }
+
+  public final byte getEdgeAnchor()
+  {
+    return m_edgeAnchor;
+  }
+
+}
