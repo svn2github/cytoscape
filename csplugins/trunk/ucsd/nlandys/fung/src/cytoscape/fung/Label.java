a81110814ce0cc41ac0f74124f30cc31bda624c0
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-02-15 18:56:08 +0000

    Finalizing the API for NodeLabel.  Need to add error checking to both Label and NodeLabel.
    git-svn-id: http://chianti.ucsd.edu/svn@3566 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/Label.java
+++ csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/Label.java
@@ -1,5 +1,6 @@
 package cytoscape.fung;
 
+import cytoscape.render.stateful.NodeDetails;
 import java.awt.Font;
 import java.awt.Paint;
 import java.awt.geom.Point2D;
@@ -7,11 +8,45 @@ import java.awt.geom.Point2D;
 public abstract class Label
 {
 
-  final String m_text = null;
-  final Font m_font = null;
-  final double m_scaleFactor = 1.0d;
-  final Paint m_paint = null;
-  final byte m_textAnchor = 0;
+  public static final byte ANCHOR_CENTER = NodeDetails.ANCHOR_CENTER;
+  public static final byte ANCHOR_NORTH = NodeDetails.ANCHOR_NORTH;
+  public static final byte ANCHOR_NORTHEAST = NodeDetails.ANCHOR_NORTHEAST;
+  public static final byte ANCHOR_EAST = NodeDetails.ANCHOR_EAST;
+  public static final byte ANCHOR_SOUTHEAST = NodeDetails.ANCHOR_SOUTHEAST;
+  public static final byte ANCHOR_SOUTH = NodeDetails.ANCHOR_SOUTH;
+  public static final byte ANCHOR_SOUTHWEST = NodeDetails.ANCHOR_SOUTHWEST;
+  public static final byte ANCHOR_WEST = NodeDetails.ANCHOR_WEST;
+  public static final byte ANCHOR_NORTHWEST = NodeDetails.ANCHOR_NORTHWEST;
+
+  public static final byte JUSTIFY_CENTER =
+    NodeDetails.LABEL_WRAP_JUSTIFY_CENTER;
+  public static final byte JUSTIFY_LEFT =
+    NodeDetails.LABEL_WRAP_JUSTIFY_LEFT;
+  public static final byte JUSTIFY_RIGHT =
+    NodeDetails.LABEL_WRAP_JUSTIFY_RIGHT;
+
+  final String m_text;
+  final Font m_font;
+  final double m_scaleFactor;
+  final Paint m_paint;
+  final byte m_textAnchor;
+  final double m_offsetVectorX;
+  final double m_offsetVectorY;
+  final byte m_justify;
+
+  Label(String text, Font font, double scaleFactor, Paint paint,
+        byte textAnchor, double offsetVectorX, double offsetVectorY,
+        byte justify)
+  {
+    m_text = text;
+    m_font = font;
+    m_scaleFactor = scaleFactor;
+    m_paint = paint;
+    m_textAnchor = textAnchor;
+    m_offsetVectorX = offsetVectorX;
+    m_offsetVectorY = offsetVectorY;
+    m_justify = justify;
+  }
 
   public final String getText()
   {
@@ -40,12 +75,12 @@ public abstract class Label
 
   public final Point2D getOffsetVector()
   {
-    return null;
+    return new Point2D.Double(m_offsetVectorX, m_offsetVectorY);
   }
 
   public final byte getJustify()
   {
-    return 0;
+    return m_justify;
   }
 
 }
--- csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/NodeLabel.java
+++ csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/NodeLabel.java
@@ -1,6 +1,31 @@
 package cytoscape.fung;
 
+import java.awt.Font;
+import java.awt.Paint;
+import java.awt.geom.Point2D;
+
 public final class NodeLabel extends Label
 {
+
+  final byte m_nodeAnchor;
+
+  public NodeLabel(final String text,
+                   final Font font,
+                   final double scaleFactor,
+                   final Paint paint,
+                   final byte textAnchor,
+                   final byte nodeAnchor,
+                   final Point2D offsetVector,
+                   final byte justify)
+  {
+    super(text, font, scaleFactor, paint, textAnchor,
+          offsetVector.getX(), offsetVector.getY(), justify);
+    m_nodeAnchor = nodeAnchor;
+  }
+
+  public final byte getNodeAnchor()
+  {
+    return m_nodeAnchor;
+  }
 
 }
