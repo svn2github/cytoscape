ff0cc436f471bf564f0daeaf954e26ef15e6c212
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-02-25 00:35:55 +0000

    Making m_graphModel a FungDynamicGraph instead of DynamicGraph and making it package visible instead of private.
    git-svn-id: http://chianti.ucsd.edu/svn@3624 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/Fung.java
+++ csplugins/trunk/ucsd/nlandys/fung/src/cytoscape/fung/Fung.java
@@ -16,7 +16,7 @@ public final class Fung
   final float[] m_extentsBuff = new float[4];
   final double[] m_doubleBuff = new double[2];
   private final Canvas m_canvas = null;
-  private final DynamicGraph m_graphModel = new FungDynamicGraph();
+  final FungDynamicGraph m_graphModel = new FungDynamicGraph();
   final RTree m_rtree = new RTree();
   final ObjArray m_nodeViewStorage = new ObjArray();
   final ObjArray m_edgeViewStorage = new ObjArray();
