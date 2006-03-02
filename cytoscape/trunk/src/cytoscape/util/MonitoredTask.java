b36f5073045bbcae7bf912208553e579a5d5f76c
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-03-02 19:01:48 +0000

    updated copyright license headers
    git-svn-id: http://chianti.ucsd.edu/svn@7149 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/util/MonitoredTask.java
+++ cytoscape/trunk/src/cytoscape/util/MonitoredTask.java
@@ -1,26 +1,41 @@
-/**  Copyright (c) 2003 Institute for Systems Biology
- **  This program is free software; you can redistribute it and/or modify
- **  it under the terms of the GNU General Public License as published by
- **  the Free Software Foundation; either version 2 of the License, or
- **  any later version.
- **
- **  This program is distributed in the hope that it will be useful,
- **  but WITHOUT ANY WARRANTY; without even the implied warranty of
- **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  The software and
- **  documentation provided hereunder is on an "as is" basis, and the
- **  Institute for Systems Biology has no obligations to provide maintenance, 
- **  support, updates, enhancements or modifications.  In no event shall the
- **  Institute for Systems Biology be liable to any party for direct, 
- **  indirect, special,incidental or consequential damages, including 
- **  lost profits, arising out of the use of this software and its 
- **  documentation, even if the Institute for Systems Biology 
- **  has been advised of the possibility of such damage. See the
- **  GNU General Public License for more details.
- **   
- **  You should have received a copy of the GNU General Public License
- **  along with this program; if not, write to the Free Software
- **  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- **/
+
+/*
+  File: MonitoredTask.java 
+  
+  Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+  
+  The Cytoscape Consortium is: 
+  - Institute of Systems Biology
+  - University of California San Diego
+  - Memorial Sloan-Kettering Cancer Center
+  - Pasteur Institute
+  - Agilent Technologies
+  
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+  
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute 
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute 
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute 
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
 // MonitoredTask. An interface for classes for which progress will be monitored 
 // (for example, using a progress bar UI)
 //----------------------------------------------------------------------------------------
