fe2f76877a0801f82a97cf78826d8bf8e7544fed
(no author) <(no author)@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-03-07 05:00:03 +0000

    This commit was manufactured by cvs2svn to create tag 'v2_2'.
    git-svn-id: http://chianti.ucsd.edu/svn@3669 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/tags/v2_2/ucsd/rmkelley/jActiveModules2.2/src/csplugins/jActiveModules/data/ActivePathFinderParameters.java
@@ -0,0 +1,302 @@
+// ActivePathFinderParameters.java
+//---------------------------------------------------------------------------------------
+// $Revision$   
+// $Date$ 
+// $Author$
+//-----------------------------------------------------------------------------------
+package csplugins.jActiveModules.data;
+//---------------------------------------------------------------------------------------
+public class ActivePathFinderParameters {
+
+	double initialTemperature = 1.0;
+	double finalTemperature = 0.01;
+	double hubAdjustment = 0;
+	int totalIterations = 2500;
+	int numberOfPaths = 5;
+	int displayInterval = 500;
+	int pseudoRandomSeed = Math.abs((int) System.currentTimeMillis());
+	int minHubSize = 0;
+	int randomSeed = pseudoRandomSeed;
+	int searchDepth = 1;
+	int maxDepth = 2;
+	boolean enableMaxDepth = true;
+	double overlapThreshold = 0.8;
+	boolean toQuench = true;
+	boolean edgesNotNodes = false;
+	boolean toUseMCFile = false;
+	boolean mcBoolean = true;
+	String mcFileName = "";
+	boolean isDefault = true;
+	boolean regionalBoolean = true;
+	boolean searchFromNodes = false;
+	boolean exit = false;
+	int maxThreads = 1;
+	boolean greedySearch = true;
+	boolean enableFiltering = true;
+	boolean run = false;
+	//boolean enableSpokePenalty = false;
+	
+	// ---------------------------------------------------------------------------------------
+	public ActivePathFinderParameters() {
+
+	} // default ctor
+	// ---------------------------------------------------------------------------------------
+	/*
+	 * public ActivePathFinderParameters (double initialTemperature, double
+	 * finalTemperature, double hubAdjustment, int totalIterations, int
+	 * numberOfPaths, int displayInterval, int minHubSize, int randomSeed, int
+	 * searchDepth, int maxDepth, boolean toQuench, boolean edgesNotNodes,
+	 * boolean toUseMCFile, boolean mcBoolean, String mcFileName, boolean
+	 * regionalBoolean, boolean searchFromNodes, int maxThreads, boolean exit,
+	 * boolean greedySearch)
+	 *  { this.initialTemperature = initialTemperature; this.finalTemperature =
+	 * finalTemperature; this.hubAdjustment = hubAdjustment;
+	 * this.totalIterations = totalIterations; this.numberOfPaths =
+	 * numberOfPaths; this.displayInterval = displayInterval; this.minHubSize =
+	 * minHubSize; this.randomSeed = randomSeed; this.searchDepth = searchDepth;
+	 * this.maxDepth = maxDepth; this.toQuench = toQuench; this.edgesNotNodes =
+	 * edgesNotNodes; this.toUseMCFile = toUseMCFile; this.mcBoolean =
+	 * mcBoolean; this.mcFileName = mcFileName; this.regionalBoolean =
+	 * regionalBoolean; this.searchFromNodes = searchFromNodes; this.isDefault =
+	 * false; this.maxThreads = maxThreads; this.exit = exit; this.greedySearch =
+	 * greedySearch; } // full ctor
+	 */
+	public ActivePathFinderParameters(ActivePathFinderParameters oldAPFP) {
+		setParams(oldAPFP);
+	} // copy ctor
+
+	public void setParams(ActivePathFinderParameters oldAPFP) {
+		this.initialTemperature = oldAPFP.getInitialTemperature();
+		this.finalTemperature = oldAPFP.getFinalTemperature();
+		this.hubAdjustment = oldAPFP.getHubAdjustment();
+		this.totalIterations = oldAPFP.getTotalIterations();
+		this.numberOfPaths = oldAPFP.getNumberOfPaths();
+		this.displayInterval = oldAPFP.getDisplayInterval();
+		this.minHubSize = oldAPFP.getMinHubSize();
+		this.randomSeed = oldAPFP.getRandomSeed();
+		this.searchDepth = oldAPFP.getSearchDepth();
+		this.maxDepth = oldAPFP.getMaxDepth();
+		this.toQuench = oldAPFP.getToQuench();
+		this.edgesNotNodes = oldAPFP.getEdgesNotNodes();
+		this.toUseMCFile = oldAPFP.getToUseMCFile();
+		this.mcBoolean = oldAPFP.getMCboolean();
+		this.mcFileName = oldAPFP.getMcFileName();
+		this.regionalBoolean = oldAPFP.getRegionalBoolean();
+		this.searchFromNodes = oldAPFP.searchFromNodes;
+		this.isDefault = false;
+		this.maxThreads = oldAPFP.getMaxThreads();
+		this.exit = oldAPFP.getExit();
+		this.greedySearch = oldAPFP.getGreedySearch();
+		this.overlapThreshold = oldAPFP.getOverlapThreshold();
+		this.enableFiltering = oldAPFP.getEnableFiltering();
+		this.enableMaxDepth = oldAPFP.getEnableMaxDepth();
+		this.run = oldAPFP.getRun();
+		
+	} // copy ctor
+
+	public boolean getRun(){
+		return this.run;
+	}
+	
+	public void setRun(boolean newValue){
+		this.run = newValue;
+	}
+	
+	public boolean getEnableMaxDepth(){
+		return this.enableMaxDepth;
+	}
+	
+	public void setEnableMaxDepth(boolean newValue){
+		this.enableMaxDepth = newValue;
+	}
+	public boolean getEnableFiltering(){
+		return this.enableFiltering;
+	}
+	
+	public void setEnableFiltering(boolean newValue){
+		this.enableFiltering = newValue;
+	}
+	
+	public void setExit(boolean flag) {
+		exit = flag;
+	}
+
+	public boolean getExit() {
+		return exit;
+	}
+	public boolean getSearchFromNodes() {
+		return searchFromNodes;
+	}
+	public void setSearchFromNodes(boolean newValue) {
+		searchFromNodes = newValue;
+		this.isDefault = false;
+	}
+
+	public double getOverlapThreshold() {
+		return overlapThreshold;
+	}
+
+	public void setOverlapThreshold(double newValue) {
+		if (newValue < 0 || newValue > 1) {
+			throw new IllegalArgumentException("Invalid overlap value: "
+					+ newValue);
+		}
+		this.overlapThreshold = newValue;
+	}
+
+	public boolean getRegionalBoolean() {
+		return regionalBoolean;
+	}
+	public void setRegionalBoolean(boolean newValue) {
+		regionalBoolean = newValue;
+		this.isDefault = false;
+	}
+	public boolean getToUseMCFile() {
+		return toUseMCFile;
+	}
+	public void setToUseMCFile(boolean newValue) {
+		toUseMCFile = newValue;
+		this.isDefault = false;
+	}
+	public boolean getMCboolean() {
+		return mcBoolean;
+	}
+	public void setMCboolean(boolean newValue) {
+		mcBoolean = newValue;
+		this.isDefault = false;
+	}
+	public String getMcFileName() {
+		return mcFileName;
+	}
+	public void setMcFileName(String newValue) {
+		mcFileName = newValue;
+		this.isDefault = false;
+	}
+	public double getInitialTemperature() {
+		return initialTemperature;
+	}
+	public void setInitialTemperature(double newValue) {
+		initialTemperature = newValue;
+		this.isDefault = false;
+	}
+	public double getFinalTemperature() {
+		return finalTemperature;
+	}
+	public void setFinalTemperature(double newValue) {
+		finalTemperature = newValue;
+		this.isDefault = false;
+	}
+	public int getTotalIterations() {
+		return totalIterations;
+	}
+	public void setTotalIterations(int newValue) {
+		totalIterations = newValue;
+		this.isDefault = false;
+	}
+	public int getNumberOfPaths() {
+		return numberOfPaths;
+	}
+	public void setNumberOfPaths(int newValue) {
+		numberOfPaths = newValue;
+		this.isDefault = false;
+	}
+	public int getDisplayInterval() {
+		return displayInterval;
+	}
+	public void setDisplayInterval(int newValue) {
+		displayInterval = newValue;
+		this.isDefault = false;
+	}
+	public int getRandomSeed() {
+		return randomSeed;
+	}
+	public void setRandomSeed(int newValue) {
+		randomSeed = newValue;
+		this.isDefault = false;
+	}
+	public int getSearchDepth() {
+		return searchDepth;
+	}
+	public void setSearchDepth(int newValue) {
+		if (newValue < 0) {
+			throw new IllegalArgumentException("Search depth must be > 0");
+		}
+		searchDepth = newValue;
+		this.isDefault = false;
+	}
+	public int getMaxDepth() {
+		return maxDepth;
+	}
+	public void setMaxDepth(int newValue) {
+		maxDepth = newValue;
+		this.isDefault = false;
+	}
+	public int getMinHubSize() {
+		return minHubSize;
+	}
+	public void setMinHubSize(int newValue) {
+		minHubSize = newValue;
+		this.isDefault = false;
+	}
+	public double getHubAdjustment() {
+		return hubAdjustment;
+	}
+	public void setHubAdjustment(double newValue) {
+		hubAdjustment = newValue;
+		this.isDefault = false;
+	}
+	public boolean getToQuench() {
+		return toQuench;
+	}
+	public void setToQuench(boolean newValue) {
+		toQuench = newValue;
+		this.isDefault = false;
+	}
+	public boolean getEdgesNotNodes() {
+		return edgesNotNodes;
+	}
+	
+
+	public boolean getGreedySearch() {
+		return greedySearch;
+	}
+	
+	public void setGreedySearch(boolean newValue){
+		this.greedySearch = newValue;
+	}
+
+	public int getMaxThreads() {
+		return maxThreads;
+	}
+
+	public void setMaxThreads(int maxThreads) {
+		this.maxThreads = maxThreads;
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("      initial temperature: " + initialTemperature + "\n");
+		sb.append("        final temperature: " + finalTemperature + "\n");
+		sb.append("         total iterations: " + totalIterations + "\n");
+		sb.append("          number of paths: " + numberOfPaths + "\n");
+		sb.append("         display interval: " + displayInterval + "\n");
+		sb.append("         minimum hub size: " + minHubSize + "\n");
+		sb.append("       hub penalty factor: " + hubAdjustment + "\n");
+		sb.append("              random seed: " + randomSeed + "\n");
+		sb.append("                   quench: " + toQuench + "\n");
+		sb.append("          edges not nodes: " + edgesNotNodes + "\n");
+		sb.append("            use MC at all: " + mcBoolean + "\n");
+		sb.append("                 MC file?: " + toUseMCFile + "\n");
+		sb.append("              MC filename: " + mcFileName + "\n");
+		sb.append("         regional scoring: " + regionalBoolean + "\n");
+		sb.append("             search depth: " + searchDepth + "\n");
+		sb.append("                max depth: " + maxDepth + "\n");
+		sb.append("        search from nodes: " + searchFromNodes + "\n");
+		sb.append("              max threads: " + maxThreads + "\n");
+		return sb.toString();
+
+	} // toString
+
+} // class ActivePathFinderParameters
+
---
+++ csplugins/tags/v2_2/ucsd/rmkelley/jActiveModules2.2/src/csplugins/jActiveModules/dialogs/ConditionsVsPathwaysTable.java
@@ -0,0 +1,270 @@
+// ConditionsVsPathwaysTable
+//------------------------------------------------------------------------------------------
+// $Revision$
+// $Date$
+// $Author$
+//---------------------------------------------------------------------------------------
+package csplugins.jActiveModules.dialogs;
+//---------------------------------------------------------------------------------------
+import giny.model.Node;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+
+import csplugins.jActiveModules.ActivePathViewer;
+import csplugins.jActiveModules.Component;
+import cytoscape.CyNetwork;
+
+//---------------------------------------------------------------------------------------
+public class ConditionsVsPathwaysTable extends JDialog {
+	JTable table;
+	//JPanel topTable, bottomTable;
+	ActivePathViewer pathViewer;
+	csplugins.jActiveModules.Component[] activePaths;
+	String[] conditionNames;
+	CyNetwork cyNetwork;
+
+	// -----------------------------------------------------------------------------------------
+	public ConditionsVsPathwaysTable(Frame parentFrame, CyNetwork cyNetwork,
+			String[] conditionNames,
+			csplugins.jActiveModules.Component[] activePaths,
+			ActivePathViewer pathViewer) {
+		super(parentFrame, false);
+		this.cyNetwork = cyNetwork;
+		this.activePaths = activePaths;
+		this.conditionNames = conditionNames;
+		this.pathViewer = pathViewer;
+		init(parentFrame, pathViewer);
+	} // ConditionsVsPathwaysTable ctor
+
+	private void init(Frame parentFrame, ActivePathViewer pathViewer) {
+
+		setTitle("Conditions vs. Pathways");
+
+		JPanel mainPanel = new JPanel();
+		 table = new JTable(new ActivePathsTableModel(activePaths,
+				conditionNames));
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.setDefaultRenderer(Boolean.class, new ColorRenderer());
+		JScrollPane scrollpane = new JScrollPane(table);
+		//mainPanel.add(scrollpane);
+
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent me) {
+				//System.err.println("Mouse pressed");
+				ConditionsVsPathwaysTable.this.pathViewer.displayPath(
+						activePaths[table.getSelectedRow()], "");
+			}
+		});
+		mainPanel.setLayout(new BorderLayout());
+//		JPanel tablePanel = new JPanel();
+//		tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
+//
+//		topTable = new ConditionsVsPathwaysTopTable(activePaths);
+//		tablePanel.add(topTable);
+//		bottomTable = new ConditionsVsPathwaysBottomTable(conditionNames,
+//				activePaths, pathViewer);
+//		tablePanel.add(bottomTable);
+//		JScrollPane scrollPane = new JScrollPane(tablePanel);
+		mainPanel.add(scrollpane, BorderLayout.CENTER);
+		JPanel buttonPanel = new JPanel();
+		JButton saveButton = new JButton("Save");
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				ConditionsVsPathwaysTable.this.saveState();
+			}
+		});
+		JButton dismissButton = new JButton("Dismiss");
+		dismissButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				ConditionsVsPathwaysTable.this.hide();
+			}
+		});
+		buttonPanel.add(saveButton, BorderLayout.CENTER);
+		buttonPanel.add(dismissButton, BorderLayout.CENTER);
+		mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+		setContentPane(mainPanel);
+		//mainPanel.setPreferredSize(new Dimension(500, 500));
+		//setSize(800, 600);
+
+	}
+
+	public csplugins.jActiveModules.Component[] getActivePaths() {
+		return activePaths;
+	}
+
+	public void saveState(String filename) {
+		try {
+			FileWriter fileWriter = new FileWriter(filename);
+			for (int i = 0; i < activePaths.length; i++) {
+				StringBuffer sb = new StringBuffer();
+				sb.append("#Subnetwork " + i + "\n");
+				csplugins.jActiveModules.Component ap = activePaths[i];
+				List nodeNames = ap.getDisplayNodes();
+				String[] condNames = ap.getConditions();
+				double score = ap.getScore();
+				sb.append("#Score\n");
+				sb.append(score + "\n");
+				sb.append("#Nodes\n");
+				for (int j = 0; j < nodeNames.size(); j++)
+					sb.append(((Node) nodeNames.get(j)).getIdentifier() + "\n");
+				sb.append("#Conditions\n");
+				for (int j = 0; j < condNames.length; j++)
+					sb.append(condNames[j] + "\n");
+				sb.append("\n");
+				fileWriter.write(sb.toString());
+			}
+			fileWriter.close();
+		} catch (IOException ioe) {
+			System.err.println("Error while writing " + filename);
+			ioe.printStackTrace();
+		} // catch
+	} // saveState
+
+	// --------------------------------------------------------------------------------
+
+	public void saveState() {
+		JFileChooser chooser = new JFileChooser();
+		if (chooser.showSaveDialog(null) == chooser.APPROVE_OPTION) {
+			String name = chooser.getSelectedFile().toString();
+			saveState(name);
+		}
+	}
+
+} // class ConditionsVsPathwaysTable
+
+class ActivePathsTableModel extends AbstractTableModel {
+	Component[] activePaths;
+	String[] columnNames;
+	Boolean[][] data;
+	protected static int HEADER_COLUMNS = 3;
+	public ActivePathsTableModel(Component[] activePaths, String[] conditions) {
+		this.activePaths = activePaths;
+		columnNames = new String[HEADER_COLUMNS + conditions.length];
+		columnNames[0] = "Network";
+		columnNames[1] = "Size";
+		columnNames[2] = "Score";
+		for (int idx = 0; idx < conditions.length; idx++) {
+			columnNames[HEADER_COLUMNS + idx] = conditions[idx];
+		}
+
+		data = new Boolean[activePaths.length][conditions.length];
+
+		for (int column = 0; column < conditions.length; column++) {
+			for (int row = 0; row < activePaths.length; row++) {
+				csplugins.jActiveModules.Component path = activePaths[row];
+				String[] conditionsForThisPath = path.getConditions();
+				boolean matchedCondition = false;
+				for (int cond = 0; cond < conditionsForThisPath.length; cond++) {
+					String condition = conditionsForThisPath[cond];
+					if (conditions[column].equalsIgnoreCase(condition)) {
+						matchedCondition = true;
+						break;
+					}
+				}
+				if (matchedCondition)
+					data[row][column] = new Boolean(true);
+				else
+					data[row][column] = new Boolean(false);
+			} // for column
+		}
+	}
+
+	public Class getColumnClass(int column) {
+		switch (column) {
+			case 0: return Integer.class; 
+			case 1: return Integer.class; 
+			case 2: return Double.class; 
+			default: return Boolean.class;
+		}
+	}
+
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	public String getColumnName(int column) {
+		return columnNames[column];
+	}
+	public int getRowCount() {
+		return activePaths.length;
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		switch (columnIndex) {
+			case 0 :
+				return new Integer(rowIndex + 1);
+			case 1 :
+				return new Integer(activePaths[rowIndex].getDisplayNodes()
+						.size());
+			case 2 :
+				return new Double(activePaths[rowIndex].getScore());
+			default :
+				return data[rowIndex][columnIndex - HEADER_COLUMNS];
+		// return new
+		// Double(activePaths[rowIndex].getDisplayScores()[columnIndex-HEADER_COLUMNS]);
+		}
+	}
+
+}
+
+class ColorRenderer extends JLabel implements TableCellRenderer {
+	public ColorRenderer() {
+		setOpaque(true);
+	}
+
+	// public java.awt.Component getTableCellRendererComponent (JTable table,
+	// Object value,
+	// boolean isSelected, boolean hasFocus,
+	// int row, int column) {
+	// float cell_value = ((Double)value).floatValue();
+	// float MAX = 1;
+	// float MID = .5f;
+	// float MIN = 0;
+	// float green_fraction =
+	// Math.min(1f,Math.max(0f,(cell_value-MID)/(MAX-MID)));
+	// float red_fraction =
+	// Math.min(1f,Math.max(0f,(MID-cell_value)/(MID-MIN)));
+	// setBackground(new Color(red_fraction,green_fraction,0f));
+	// setText(String.valueOf(cell_value));
+	// setForeground(Color.WHITE);
+	// return this;
+	// } // getTableCellRendererComponent
+	public java.awt.Component getTableCellRendererComponent(JTable table,
+			Object value, boolean isSelected, boolean hasFocus, int row,
+			int column) {
+		boolean cell_value = ((Boolean) value).booleanValue();
+		if (cell_value) {
+			setBackground(Color.RED);
+		} else {
+			setBackground(Color.WHITE);
+		}
+
+		setForeground(Color.WHITE);
+		return this;
+	} // getTableCellRendererComponent
+}
