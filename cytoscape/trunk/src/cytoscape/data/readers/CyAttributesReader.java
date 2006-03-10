07f3617e4b8774ae60b2d5f232c6dfafc87fc7e1
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-03-10 17:41:47 +0000

    Now allowing spaces in attributeName.
    git-svn-id: http://chianti.ucsd.edu/svn@7160 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/data/readers/CyAttributesReader.java
+++ cytoscape/trunk/src/cytoscape/data/readers/CyAttributesReader.java
@@ -60,24 +60,26 @@ public class CyAttributesReader
     {
       final String firstLine = reader.readLine();
       if (firstLine == null) { return; }
-      final StringTokenizer tokens = new StringTokenizer(firstLine);
-      attributeName = tokens.nextToken();
-      if (tokens.hasMoreTokens()) {
-        final String foo = tokens.nextToken();
-        final String searchStr = "class=";
-        final int inx = foo.indexOf(searchStr);
-        if (inx >= 0) {
-          String className = foo.substring(inx + searchStr.length()).trim();
-          if (className.endsWith(")")) {
-            className = className.substring(0, className.length() - 1); }
-          if (className.equalsIgnoreCase("java.lang.String")) {
-            type = MultiHashMapDefinition.TYPE_STRING; }
-          else if (className.equalsIgnoreCase("java.lang.Boolean")) {
-            type = MultiHashMapDefinition.TYPE_BOOLEAN; }
-          else if (className.equalsIgnoreCase("java.lang.Integer")) {
-            type = MultiHashMapDefinition.TYPE_INTEGER; }
-          else if (className.equalsIgnoreCase("java.lang.Double")) {
-            type = MultiHashMapDefinition.TYPE_FLOATING_POINT; } } }
+      final String searchStr = "class=";
+      final int inx = firstLine.indexOf(searchStr);
+      if (inx < 0) {
+        attributeName = firstLine.trim(); }
+      else {
+        attributeName = firstLine.substring(0, inx).trim();
+        String foo = firstLine.substring(inx);
+        final StringTokenizer tokens = new StringTokenizer(foo);
+        foo = tokens.nextToken();
+        String className = foo.substring(searchStr.length()).trim();
+        if (className.endsWith(")")) {
+          className = className.substring(0, className.length() - 1); }
+        if (className.equalsIgnoreCase("java.lang.String")) {
+          type = MultiHashMapDefinition.TYPE_STRING; }
+        else if (className.equalsIgnoreCase("java.lang.Boolean")) {
+          type = MultiHashMapDefinition.TYPE_BOOLEAN; }
+        else if (className.equalsIgnoreCase("java.lang.Integer")) {
+          type = MultiHashMapDefinition.TYPE_INTEGER; }
+        else if (className.equalsIgnoreCase("java.lang.Double")) {
+          type = MultiHashMapDefinition.TYPE_FLOATING_POINT; } }
     }
     boolean firstLine = true;
     boolean list = false;
