2b82b64ddf8d03b62a09d5a7ae29501544b882f6
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-03-11 00:27:21 +0000

    *** empty log message ***
    git-svn-id: http://chianti.ucsd.edu/svn@4130 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ coreplugins/trunk/AutomaticLayout/src/csplugins/layout/LayoutPlugin.java
@@ -0,0 +1,234 @@
+package csplugins.layout;
+
+import cytoscape.*;
+import cytoscape.plugin.*;
+import cytoscape.util.*;
+import cytoscape.layout.*;
+import cytoscape.view.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import phoebe.util.GraphPartition;
+
+import csplugins.layout.algorithms.*;
+import csplugins.layout.algorithms.springEmbedded.SpringEmbeddedLayoutAction;
+import csplugins.layout.algorithms.hierarchicalLayout.HierarchicalLayoutListener;
+//import csplugins.layout.algorithms.radialHierarchicalLayout.RadialHierarchicalLayoutAlgorithm;
+import csplugins.layout.algorithms.graphPartition.AttributeCircleLayoutMenu;
+//import csplugins.layout.algorithms.graphPartition.ISOMLayout;
+import csplugins.layout.algorithms.graphPartition.DegreeSortedCircleLayout;
+
+import giny.view.NodeView;
+
+public class LayoutPlugin extends CytoscapePlugin
+{
+  public LayoutPlugin()
+  {
+    initialize();
+  }
+
+  protected void initialize()
+  {
+    // NOTE:
+    // The CircleGraphLayoutAlgorithm does not implement a decent layout,
+    // therefore it has been commented out.
+
+    /*
+    JMenuItem circle = new JMenuItem(new AbstractAction("Circle")
+    {
+      public void actionPerformed (ActionEvent e )
+      {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+          public void run()
+          {
+            final SwingWorker worker = new SwingWorker()
+            {
+              public Object construct()
+              {
+                LayoutAlgorithm layout = new
+                  CircleGraphLayoutAlgorithm(Cytoscape.getCurrentNetworkView());
+                Cytoscape.getCurrentNetworkView().applyLayout(layout);
+                return null;
+              } // end construct()
+
+            }; // end SwingWorker
+
+            worker.start();
+          } // end run()
+
+        }); // end new Runnable()
+
+      } // end actionPerformed()
+
+    }); // end new AbstractAction
+    */
+
+    // NOTE: The RadialTreeLayoutAlgorithm is broken, thus it has been
+    // commented out.
+
+    /*
+    JMenuItem radial = new JMenuItem(new AbstractAction("Radial")
+    {
+      public void actionPerformed (ActionEvent e )
+      {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+          public void run()
+          {
+            final SwingWorker worker = new SwingWorker()
+            {
+              public Object construct()
+              {
+                LayoutAlgorithm layout = new
+                  RadialTreeLayoutAlgorithm(Cytoscape.getCurrentNetworkView());
+                Cytoscape.getCurrentNetworkView().applyLayout(layout);
+                return null;
+              } // end construct()
+
+            }; // end SwingWorker
+
+            worker.start();
+          } // end run()
+
+        }); // end new Runnable()
+
+      } // end actionPerformed()
+
+    }); // end new AbstractAction
+    */
+
+    // NOTE: The SpringEmbeddedLayoutAlgorithm is broken. Therefore
+    // it has been commented out.
+
+    /*
+    JMenuItem spring = new JMenuItem(new AbstractAction("Spring")
+    {
+      public void actionPerformed (ActionEvent e )
+      {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+          public void run()
+          {
+            final SwingWorker worker = new SwingWorker()
+            {
+              public Object construct()
+              {
+                LayoutAlgorithm layout = new
+                  SpringEmbeddedLayoutAlgorithm( Cytoscape.getCurrentNetworkView() );
+                Cytoscape.getCurrentNetworkView().applyLayout(layout);
+                return null;
+              } // end construct()
+
+            }; // end SwingWorker
+
+            worker.start();
+          } // end run()
+
+        }); // end new Runnable()
+
+      } // end actionPerformed()
+
+    }); // end new AbstractAction
+    */
+
+    // NOTE: The ISOMLayoutAlgorithm is broken. Therefore, it has been
+    // commented out.
+
+    /*
+    JMenuItem isom = new JMenuItem(new AbstractAction("ISOM")
+    {
+      public void actionPerformed (ActionEvent e )
+      {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+          public void run()
+          {
+            final SwingWorker worker = new SwingWorker()
+            {
+              public Object construct()
+              {
+                LayoutAlgorithm layout = new
+                  ISOMLayoutAlgorithm(Cytoscape.getCurrentNetworkView());
+                Cytoscape.getCurrentNetworkView().applyLayout(layout);
+                return null;
+              }
+            };
+            worker.start();
+          }
+        });
+      }
+    });
+    */
+
+    JMenuItem hierarchical = new JMenuItem("Hierarchical");
+    {
+      HierarchicalLayoutListener hierarchicalListener = new HierarchicalLayoutListener();
+      hierarchical.addActionListener(hierarchicalListener);
+    }
+    
+    JMenuItem springEmbAll = new JMenuItem(new SpringEmbeddedLayoutAction(true));
+    JMenuItem springEmbSome = new JMenuItem(new SpringEmbeddedLayoutAction(false));
+    JMenu springEmbMenu = new JMenu("Spring Embedded");
+    springEmbMenu.add(springEmbAll);
+    springEmbMenu.add(springEmbSome);
+
+    // NOTE: ISOMLayout is broken. Therefore it has been commented out.
+    
+    /*
+    JMenuItem isomLay = new JMenuItem(new AbstractAction("ISOM Layout")
+    {
+      public void actionPerformed (ActionEvent e )
+      {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+          public void run()
+          {
+            ISOMLayout layout = new ISOMLayout(Cytoscape.getCurrentNetwork());
+            layout.layout();
+          } // end run()
+
+        }); // end new Runnable()
+
+      } // end actionPerformed()
+
+    }); // end new AbstractAction
+    */
+
+    JMenuItem degSortCircle = new JMenuItem(new AbstractAction("Degree Sorted Circle Layout")
+    {
+      public void actionPerformed (ActionEvent e )
+      {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+          public void run()
+          {
+            DegreeSortedCircleLayout layout = new DegreeSortedCircleLayout(Cytoscape.getCurrentNetwork());
+            layout.layout();
+          } // end run()
+
+        }); // end new Runnable()
+
+      } // end actionPerformed()
+
+    }); // end new AbstractAction
+
+    JMenu menu = new JMenu("Cytoscape Layouts");
+    //menu.add(circle);
+    //menu.add(radial);
+    //menu.add(spring);
+    //menu.add(isom);
+    menu.add(hierarchical);
+    //menu.add(radHier);
+    menu.add(springEmbMenu);
+    menu.add(new AttributeCircleLayoutMenu());
+    //menu.add(isomLay);
+    menu.add(degSortCircle);
+
+    JMenu layoutMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar()
+                                .getMenu("Layout");
+    layoutMenu.add(menu);
+    layoutMenu.add(new JGraphLayoutMenu());
+  }
+}
+
---
+++ coreplugins/trunk/AutomaticLayout/src/csplugins/layout/algorithms/graphPartition/ISOMLayout.java
@@ -0,0 +1,228 @@
+/*
+ * This is based on the ISOMLayout from the JUNG project.
+ */
+
+package csplugins.layout.algorithms.graphPartition;
+
+import cern.colt.list.IntArrayList;
+import cern.colt.matrix.impl.DenseDoubleMatrix1D;
+import cern.colt.matrix.DoubleMatrix1D;
+import cern.colt.map.OpenIntObjectHashMap;
+import cern.colt.map.PrimeFinder;
+
+import cytoscape.CyNetwork;
+import giny.model.*;
+
+
+public class ISOMLayout extends AbstractLayout {
+
+  
+	private int maxEpoch;
+	private int epoch;
+
+	private int radiusConstantTime;
+	private int radius;
+	private int minRadius;
+
+	private double adaption;
+	private double initialAdaption;
+	private double minAdaption;
+
+	private double factor;
+	private double coolingFactor;
+
+  private boolean trace;
+	private boolean done;
+
+  //Queue, First In First Out, use add() and get(0)/remove(0)
+  private IntArrayList q;
+	private String status = null;
+
+  OpenIntObjectHashMap nodeIndexToDataMap;
+  double globalX, globalY;
+  GraphPerspective net;
+  int[] nodes;
+  double squared_size;
+  
+  public ISOMLayout ( CyNetwork network ) {
+    super( network );
+  
+    q = new IntArrayList();
+		trace = false;
+ 
+  }
+
+  public void layoutPartion ( GraphPerspective net) {
+    
+    this.net = net;
+
+    nodeIndexToDataMap = new OpenIntObjectHashMap( PrimeFinder.nextPrime( net.getNodeCount() ) );
+    nodes = net.getNodeIndicesArray();
+    squared_size = nodes.length*50;
+   
+		epoch = 1;
+    maxEpoch = 5000;
+   
+		radiusConstantTime = 100;
+		radius = 5;
+		minRadius = 1;
+
+		initialAdaption = 90.0D / 100.0D;
+		adaption = initialAdaption;
+		minAdaption = 0;
+
+    coolingFactor = 2;
+
+    System.out.println ( "Epoch: "+epoch+" maxEpoch: "+maxEpoch );
+    while ( epoch < maxEpoch ) {
+      adjust();
+			updateParameters();
+    }
+   
+  }
+
+  /**
+   * @return the closest NodeView to these coords.
+   */
+  public int getClosestPosition ( double x, double y ) {
+    double minDistance = Double.MAX_VALUE;
+    int closest = 0;
+    for ( int i = 0; i < nodes.length; i++ ) {
+
+      double dx = layout.getX(  nodes[i] );
+      double dy = layout.getY(  nodes[i] );
+      double dist = dx * dx + dy * dy;
+      if ( dist < minDistance ) {
+        minDistance = dist;
+        closest = nodes[i];
+      }
+    }
+    return closest;
+  }
+
+
+
+  public  void adjust() {
+		//Generate random position in graph space
+		ISOMVertexData tempISOM = new ISOMVertexData();
+		
+
+		// creates a new XY data location
+		globalX = 10 + Math.random() * squared_size;
+    globalY = 10 + Math.random() * squared_size;
+
+    //Get closest vertex to random position
+    int winner = getClosestPosition( globalX, globalY );
+    
+    for ( int i = 0; i < nodes.length; i++ ) {
+      ISOMVertexData ivd = getISOMVertexData(nodes[i]);
+			ivd.distance = 0;
+			ivd.visited = false;
+		}
+		adjustVertex(winner);
+	}
+  
+  public  void updateParameters() {
+		epoch++;
+		double factor = Math.exp(-1 * coolingFactor * (1.0 * epoch / maxEpoch));
+		adaption = Math.max(minAdaption, factor * initialAdaption);
+    if ((radius > minRadius) && (epoch % radiusConstantTime == 0)) {
+			radius--;
+		}
+	}
+
+  public void adjustVertex( int v ) {
+		q.clear();
+		ISOMVertexData ivd = getISOMVertexData(v);
+		ivd.distance = 0;
+		ivd.visited = true;
+		q.add(v);
+		int current;
+
+    while ( !q.isEmpty() ) {
+			current =  q.get(0);
+      q.remove(0);
+
+			ISOMVertexData currData = getISOMVertexData(current);
+			
+      double current_x = layout.getX( current );
+      double current_y = layout.getY( current );
+      
+			double dx = globalX - current_x;
+			double dy = globalY - current_y;
+      
+      // possible mod
+			double factor = adaption / Math.pow(2, currData.distance) ;
+      
+
+      layout.setX( current, current_x + factor * dx );
+      layout.setY( current, current_y + factor * dy );
+
+      
+			if (currData.distance < radius) {
+				int[] neighbors = net.neighborsArray( current );
+
+        for ( int neighbor_index = 0; neighbor_index < neighbors.length; ++neighbor_index ) {
+          
+          ISOMVertexData childData = getISOMVertexData(neighbors[neighbor_index]);
+					if (!childData.visited) {
+						childData.visited = true;
+						childData.distance = currData.distance + 1;
+            q.add(neighbors[neighbor_index]);
+					}
+				}
+			}
+		}
+	}
+
+  public ISOMVertexData getISOMVertexData (  int v ) {
+    ISOMVertexData vd = ( ISOMVertexData )nodeIndexToDataMap.get( v );
+    if ( vd == null ) {
+      vd = new ISOMVertexData();
+      nodeIndexToDataMap.put( v, vd );
+    }
+    return vd;
+  }                                                
+
+
+  public static class ISOMVertexData {
+		public DoubleMatrix1D disp;
+
+		int distance;
+		boolean visited;
+
+		public ISOMVertexData() {
+			initialize();
+		}
+
+		public void initialize() {
+			disp = new DenseDoubleMatrix1D(2);
+
+			distance = 0;
+			visited = false;
+		}
+
+		public double getXDisp() {
+			return disp.get(0);
+		}
+
+		public double getYDisp() {
+			return disp.get(1);
+		}
+
+		public void setDisp(double x, double y) {
+			disp.set(0, x);
+			disp.set(1, y);
+		}
+
+		public void incrementDisp(double x, double y) {
+			disp.set(0, disp.get(0) + x);
+			disp.set(1, disp.get(1) + y);
+		}
+
+		public void decrementDisp(double x, double y) {
+			disp.set(0, disp.get(0) - x);
+			disp.set(1, disp.get(1) - y);
+		}
+	}
+}
