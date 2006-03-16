3da5bce13978039f152fddd4996cda03844ad86d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-03-16 17:08:17 +0000

    Changing DEFAULT_ARROW_SIZE from 3 to 5. Note that there is no way to change the arrow size using this API. So Cytoscape looks better with size 5.  Lame hack.
    git-svn-id: http://chianti.ucsd.edu/svn@3704 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/ding/src/ding/view/DEdgeView.java
+++ csplugins/trunk/ucsd/nlandys/ding/src/ding/view/DEdgeView.java
@@ -20,7 +20,7 @@ import java.util.List;
 class DEdgeView implements EdgeView, Label, Bend, EdgeAnchors
 {
 
-  static final float DEFAULT_ARROW_SIZE = 3.0f;
+  static final float DEFAULT_ARROW_SIZE = 5.0f;
   static final Paint DEFAULT_ARROW_PAINT = Color.black;
   static final float DEFAULT_EDGE_THICKNESS = 1.0f;
   static final String DEFAULT_LABEL_TEXT = "";
