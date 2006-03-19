ff280e9e12352d8497050b1a92c67e06cdef0b76
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-03-19 19:24:38 +0000

    Adding Javadoc.
    git-svn-id: http://chianti.ucsd.edu/svn@3715 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/ding/src/ding/view/ContentChangeListener.java
+++ csplugins/trunk/ucsd/nlandys/ding/src/ding/view/ContentChangeListener.java
@@ -3,6 +3,14 @@ package ding.view;
 public interface ContentChangeListener
 {
 
+  /**
+   * This gets fired upon graph redraw when at least one of the following
+   * things change: node unselected, edge unselected, background paint
+   * change, node view added, edge view added, node view removed,
+   * edge view removed, node view hidden, edge view hidden, node view
+   * restored, edge view restored, graph lod changed, node visual property
+   * changed, edge visual property changed.
+   */
   public void contentChanged();
 
 }
--- csplugins/trunk/ucsd/nlandys/ding/src/ding/view/ViewportChangeListener.java
+++ csplugins/trunk/ucsd/nlandys/ding/src/ding/view/ViewportChangeListener.java
@@ -3,6 +3,10 @@ package ding.view;
 public interface ViewportChangeListener
 {
 
+  /**
+   * This gets fired upon graph redraw when zoom is changed or the graph
+   * center is changed.
+   */
   public void viewportChanged(double newXCenter, double newYCenter,
                               double newScaleFactor);
 
