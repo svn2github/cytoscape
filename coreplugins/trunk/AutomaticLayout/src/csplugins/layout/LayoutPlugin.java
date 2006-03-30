4377c93f74dccfcfab3371e878387db0d612b069
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-03-30 22:03:21 +0000

    updated csplugins/layout/LayoutPlugin.java and removed sections of code that were commented out.
    git-svn-id: http://chianti.ucsd.edu/svn@4147 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/AutomaticLayout/src/csplugins/layout/LayoutPlugin.java
+++ coreplugins/trunk/AutomaticLayout/src/csplugins/layout/LayoutPlugin.java
@@ -13,9 +13,7 @@ import phoebe.util.GraphPartition;
 import csplugins.layout.algorithms.*;
 import csplugins.layout.algorithms.springEmbedded.SpringEmbeddedLayoutAction;
 import csplugins.layout.algorithms.hierarchicalLayout.HierarchicalLayoutListener;
-//import csplugins.layout.algorithms.radialHierarchicalLayout.RadialHierarchicalLayoutAlgorithm;
 import csplugins.layout.algorithms.graphPartition.AttributeCircleLayoutMenu;
-//import csplugins.layout.algorithms.graphPartition.ISOMLayout;
 import csplugins.layout.algorithms.graphPartition.DegreeSortedCircleLayout;
 
 import giny.view.NodeView;
@@ -29,138 +27,6 @@ public class LayoutPlugin extends CytoscapePlugin
 
   protected void initialize()
   {
-    // NOTE:
-    // The CircleGraphLayoutAlgorithm does not implement a decent layout,
-    // therefore it has been commented out.
-
-    /*
-    JMenuItem circle = new JMenuItem(new AbstractAction("Circle")
-    {
-      public void actionPerformed (ActionEvent e )
-      {
-        SwingUtilities.invokeLater(new Runnable()
-        {
-          public void run()
-          {
-            final SwingWorker worker = new SwingWorker()
-            {
-              public Object construct()
-              {
-                LayoutAlgorithm layout = new
-                  CircleGraphLayoutAlgorithm(Cytoscape.getCurrentNetworkView());
-                Cytoscape.getCurrentNetworkView().applyLayout(layout);
-                return null;
-              } // end construct()
-
-            }; // end SwingWorker
-
-            worker.start();
-          } // end run()
-
-        }); // end new Runnable()
-
-      } // end actionPerformed()
-
-    }); // end new AbstractAction
-    */
-
-    // NOTE: The RadialTreeLayoutAlgorithm is broken, thus it has been
-    // commented out.
-
-    /*
-    JMenuItem radial = new JMenuItem(new AbstractAction("Radial")
-    {
-      public void actionPerformed (ActionEvent e )
-      {
-        SwingUtilities.invokeLater(new Runnable()
-        {
-          public void run()
-          {
-            final SwingWorker worker = new SwingWorker()
-            {
-              public Object construct()
-              {
-                LayoutAlgorithm layout = new
-                  RadialTreeLayoutAlgorithm(Cytoscape.getCurrentNetworkView());
-                Cytoscape.getCurrentNetworkView().applyLayout(layout);
-                return null;
-              } // end construct()
-
-            }; // end SwingWorker
-
-            worker.start();
-          } // end run()
-
-        }); // end new Runnable()
-
-      } // end actionPerformed()
-
-    }); // end new AbstractAction
-    */
-
-    // NOTE: The SpringEmbeddedLayoutAlgorithm is broken. Therefore
-    // it has been commented out.
-
-    /*
-    JMenuItem spring = new JMenuItem(new AbstractAction("Spring")
-    {
-      public void actionPerformed (ActionEvent e )
-      {
-        SwingUtilities.invokeLater(new Runnable()
-        {
-          public void run()
-          {
-            final SwingWorker worker = new SwingWorker()
-            {
-              public Object construct()
-              {
-                LayoutAlgorithm layout = new
-                  SpringEmbeddedLayoutAlgorithm( Cytoscape.getCurrentNetworkView() );
-                Cytoscape.getCurrentNetworkView().applyLayout(layout);
-                return null;
-              } // end construct()
-
-            }; // end SwingWorker
-
-            worker.start();
-          } // end run()
-
-        }); // end new Runnable()
-
-      } // end actionPerformed()
-
-    }); // end new AbstractAction
-    */
-
-    // NOTE: The ISOMLayoutAlgorithm is broken. Therefore, it has been
-    // commented out.
-
-    /*
-    JMenuItem isom = new JMenuItem(new AbstractAction("ISOM")
-    {
-      public void actionPerformed (ActionEvent e )
-      {
-        SwingUtilities.invokeLater(new Runnable()
-        {
-          public void run()
-          {
-            final SwingWorker worker = new SwingWorker()
-            {
-              public Object construct()
-              {
-                LayoutAlgorithm layout = new
-                  ISOMLayoutAlgorithm(Cytoscape.getCurrentNetworkView());
-                Cytoscape.getCurrentNetworkView().applyLayout(layout);
-                return null;
-              }
-            };
-            worker.start();
-          }
-        });
-      }
-    });
-    */
-
     JMenuItem hierarchical = new JMenuItem("Hierarchical");
     {
       HierarchicalLayoutListener hierarchicalListener = new HierarchicalLayoutListener();
@@ -172,28 +38,6 @@ public class LayoutPlugin extends CytoscapePlugin
     JMenu springEmbMenu = new JMenu("Spring Embedded");
     springEmbMenu.add(springEmbAll);
     springEmbMenu.add(springEmbSome);
-
-    // NOTE: ISOMLayout is broken. Therefore it has been commented out.
-    
-    /*
-    JMenuItem isomLay = new JMenuItem(new AbstractAction("ISOM Layout")
-    {
-      public void actionPerformed (ActionEvent e )
-      {
-        SwingUtilities.invokeLater(new Runnable()
-        {
-          public void run()
-          {
-            ISOMLayout layout = new ISOMLayout(Cytoscape.getCurrentNetwork());
-            layout.layout();
-          } // end run()
-
-        }); // end new Runnable()
-
-      } // end actionPerformed()
-
-    }); // end new AbstractAction
-    */
 
     JMenuItem degSortCircle = new JMenuItem(new AbstractAction("Degree Sorted Circle Layout")
     {
@@ -214,15 +58,9 @@ public class LayoutPlugin extends CytoscapePlugin
     }); // end new AbstractAction
 
     JMenu menu = new JMenu("Cytoscape Layouts");
-    //menu.add(circle);
-    //menu.add(radial);
-    //menu.add(spring);
-    //menu.add(isom);
     menu.add(hierarchical);
-    //menu.add(radHier);
     menu.add(springEmbMenu);
     menu.add(new AttributeCircleLayoutMenu());
-    //menu.add(isomLay);
     menu.add(degSortCircle);
 
     JMenu layoutMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar()
