3d6d71456eeb317fe6318b1ca01587704b470ffc
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-04-12 16:39:11 +0000

    fixed a windoze compatibility bug for jar file separators
    git-svn-id: http://chianti.ucsd.edu/svn@7307 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/CytoscapeInit.java
+++ cytoscape/trunk/src/cytoscape/CytoscapeInit.java
@@ -548,8 +548,6 @@ public class CytoscapeInit { //implements PropertyChangeListener {
 		// the creation of the class loader automatically loads the plugins
 		classLoader = new URLClassLoader(urls, Cytoscape.class.getClassLoader());
 
-		String fileSep = System.getProperty("file.separator");
-
 		// iterate through the given jar files and find classes that are
 		// assignable
 		// from CytoscapePlugin
@@ -587,7 +585,15 @@ public class CytoscapeInit { //implements PropertyChangeListener {
 					if (entry.endsWith("class")) {
 						// convert the entry to an assignable class name
 						entry = entry.replaceAll("\\.class$", "");
-						entry = entry.replaceAll(fileSep, ".");
+						// Ideally we would use 
+						// System.getProperty("file.separator") or 
+						// File.separatorChar,  however that doesn't work 
+						// on windoze.  The problem appears to be that
+						// because the jar file was created on a unix platform,
+						// the separator remains in unix style.  If the jar 
+						// ever gets created on a windows machine,
+						// we may have to revisit this.
+						entry = entry.replaceAll("/", ".");
 
 						// System.out.println(" CLASS: " + entry);
 						if (!(isClassPlugin(entry))) {
