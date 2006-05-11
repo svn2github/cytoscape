c1af0ab5d79017ac14902327a61e2fc755bda05d
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-05-11 22:46:25 +0000

    Fixing a spelling error.
    git-svn-id: http://chianti.ucsd.edu/svn@3774 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/ding/src/ding/view/ViewportChangeListener.java
+++ csplugins/trunk/ucsd/nlandys/ding/src/ding/view/ViewportChangeListener.java
@@ -7,7 +7,7 @@ public interface ViewportChangeListener
    * This gets fired upon graph redraw when zoom is changed or the graph
    * center is changed or the view component is resized.
    */
-  public void viewportChanged(int viewportWidth, int viewportHeigh,
+  public void viewportChanged(int viewportWidth, int viewportHeight,
                               double newXCenter, double newYCenter,
                               double newScaleFactor);
 
