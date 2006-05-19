521ba8cc2dc0958f960cd505d584830d93dbce55
nlandys <nlandys@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-05-19 21:32:14 +0000

    Making a slight adjustment for this rounded rectangle bug.
    git-svn-id: http://chianti.ucsd.edu/svn@3796 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/nlandys/ding/src/ding/view/DNodeView.java
+++ csplugins/trunk/ucsd/nlandys/ding/src/ding/view/DNodeView.java
@@ -204,7 +204,7 @@ class DNodeView implements NodeView, Label
       final double w = ((double) xMax) - xMin;
       final double h = ((double) m_view.m_extentsBuff[3]) -
         m_view.m_extentsBuff[1];
-      if (Math.max(w, h) < 2.0d * Math.min(w, h) &&
+      if (!(Math.max(w, h) < 2.01d * Math.min(w, h)) &&
           getShape() == NodeView.ROUNDED_RECTANGLE) {
         setShape(NodeView.RECTANGLE); }
       m_view.m_contentChanged = true; 
@@ -237,7 +237,7 @@ class DNodeView implements NodeView, Label
       final double w = ((double) m_view.m_extentsBuff[2]) -
         m_view.m_extentsBuff[0];
       final double h = ((double) yMax) - yMin;
-      if (Math.max(w, h) < 2.0d * Math.min(w, h) &&
+      if (!(Math.max(w, h) < 2.01d * Math.min(w, h)) &&
           getShape() == NodeView.ROUNDED_RECTANGLE) {
         setShape(NodeView.RECTANGLE); }
       m_view.m_contentChanged = true;
@@ -454,7 +454,7 @@ class DNodeView implements NodeView, Label
       case NodeView.ROUNDED_RECTANGLE:
         final double width = getWidth();
         final double height = getHeight();
-        if (Math.max(width, height) < 2.0d * Math.min(width, height)) {
+        if (!(Math.max(width, height) < 2.01d * Math.min(width, height))) {
           nativeShape = GraphGraphics.SHAPE_RECTANGLE; }
         else {
           nativeShape = GraphGraphics.SHAPE_ROUNDED_RECTANGLE; }
