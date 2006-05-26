74c938e05fa3e54b7fe86fb5c826231509d1cb02
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-05-26 00:09:12 +0000

    changed cvs server names
    git-svn-id: http://chianti.ucsd.edu/svn@4624 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/download_list.php
+++ cyto_web/trunk/download_list.php
@@ -59,9 +59,9 @@
 			from our CVS server are provided below.
 			When prompted for a password for anonymous, simply press the
 			Enter key.
-			<P>cvs -d :pserver:anonymous@bordeaux.ucsd.edu:/cvsdir5 login
-			<BR>cvs -d :pserver:anonymous@bordeaux.ucsd.edu:/cvsdir5 co cytoscape
-			<BR>cvs -d :pserver:anonymous@bordeaux.ucsd.edu:/cvsdir5 logout
+			<P>cvs -d :pserver:anonymous@cabernet.ucsd.edu:/cvsdir5 login
+			<BR>cvs -d :pserver:anonymous@cabernet.ucsd.edu:/cvsdir5 co cytoscape
+			<BR>cvs -d :pserver:anonymous@cabernet.ucsd.edu:/cvsdir5 logout
 			</P>
 </div>
 <? include "footer.php"; ?>
