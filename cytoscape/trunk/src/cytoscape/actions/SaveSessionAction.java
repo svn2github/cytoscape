b0eee3bd996faa4d173c2ebaecd671595aa81ca8
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-06-01 22:40:32 +0000

    Rwqrote commnts and some minor modifications.
    git-svn-id: http://chianti.ucsd.edu/svn@7610 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/SaveSessionAction.java
+++ cytoscape/trunk/src/cytoscape/actions/SaveSessionAction.java
@@ -1,48 +1,44 @@
-
 /*
-  File: SaveSessionAction.java 
-  
-  Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
-  
-  The Cytoscape Consortium is: 
-  - Institute for Systems Biology
-  - University of California San Diego
-  - Memorial Sloan-Kettering Cancer Center
-  - Pasteur Institute
-  - Agilent Technologies
-  
-  This library is free software; you can redistribute it and/or modify it
-  under the terms of the GNU Lesser General Public License as published
-  by the Free Software Foundation; either version 2.1 of the License, or
-  any later version.
-  
-  This library is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
-  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
-  documentation provided hereunder is on an "as is" basis, and the
-  Institute for Systems Biology and the Whitehead Institute 
-  have no obligations to provide maintenance, support,
-  updates, enhancements or modifications.  In no event shall the
-  Institute for Systems Biology and the Whitehead Institute 
-  be liable to any party for direct, indirect, special,
-  incidental or consequential damages, including lost profits, arising
-  out of the use of this software and its documentation, even if the
-  Institute for Systems Biology and the Whitehead Institute 
-  have been advised of the possibility of such damage.  See
-  the GNU Lesser General Public License for more details.
-  
-  You should have received a copy of the GNU Lesser General Public License
-  along with this library; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
-*/
+ File: SaveSessionAction.java 
+ 
+ Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+ 
+ The Cytoscape Consortium is: 
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Pasteur Institute
+ - Agilent Technologies
+ 
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+ 
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute 
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute 
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute 
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
 
 package cytoscape.actions;
 
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.io.IOException;
-
-import javax.xml.bind.JAXBException;
 
 import cytoscape.Cytoscape;
 import cytoscape.data.writers.CytoscapeSessionWriter;
@@ -54,22 +50,26 @@ import cytoscape.util.CyFileFilter;
 import cytoscape.util.CytoscapeAction;
 import cytoscape.util.FileUtil;
 
-//
-//	"Save Project" Action
-//		1. Extract current status
-//		2. Generate file based on the extracted information
-//		3. Write as XML.
-//		4.
-//
-//
+/**
+ * Save Session action.<br>
+ * Write current states into a CYS file.<br>
+ * 
+ * @version 1.0
+ * @since 2.3
+ * @see cytoscape.data.writers.CytoscapeSessionWriter
+ * @author kono
+ * 
+ */
 public class SaveSessionAction extends CytoscapeAction {
 
 	// Extension for the new cytoscape session file
-	public static String SESSION_EXT = ".cys";
+	public final static String SESSION_EXT = ".cys";
 
 	/**
 	 * Constructor.
 	 * 
+	 * @param label
+	 *            Label for the menu item.
 	 */
 	public SaveSessionAction(String label) {
 		super(label);
@@ -77,28 +77,33 @@ public class SaveSessionAction extends CytoscapeAction {
 		setAcceleratorCombo(java.awt.event.KeyEvent.VK_S, ActionEvent.CTRL_MASK);
 	}
 
-
-        public SaveSessionAction() {
-               super();
+	/**
+	 * Constructor.<br>
+	 * This is mainly for the toolbar icon button.<br>
+	 * 
+	 */
+	public SaveSessionAction() {
+		super();
 	}
 
-
-	// If no current session file exists, open dialog box to save new session,
-	// and if it exists, overwrite the file.
+	/**
+	 * If no current session file exists, open dialog box to ask user a new
+	 * session file name, otherwise, overwrite the file.
+	 */
 	public void actionPerformed(ActionEvent e) {
 
 		// Call file chooser only when the currentFileName is null.
 		String name = Cytoscape.getCurrentSessionFileName();
 
 		if (name == null) {
-			// Open Dialog to ask user the file name.
-			try {
-				name = FileUtil.getFile("Save Current Session as CYS File",
-						FileUtil.SAVE, new CyFileFilter[] {}).toString();
-			} catch (Exception exp) {
-				// this is because the selection was canceled
+
+			File file = FileUtil.getFile("Save Current Session as CYS File",
+					FileUtil.SAVE, new CyFileFilter[] {});
+			if (file == null) {
 				return;
 			}
+
+			name = file.getAbsolutePath();
 
 			if (!name.endsWith(SESSION_EXT))
 				name = name + SESSION_EXT;
@@ -110,8 +115,8 @@ public class SaveSessionAction extends CytoscapeAction {
 
 		// Configure JTask Dialog Pop-Up Box
 		JTaskConfig jTaskConfig = new JTaskConfig();
-		
-		jTaskConfig.displayCancelButton(true);
+
+		jTaskConfig.displayCancelButton(false);
 		jTaskConfig.setOwner(Cytoscape.getDesktop());
 		jTaskConfig.displayCloseButton(true);
 		jTaskConfig.displayStatus(true);
@@ -120,65 +125,63 @@ public class SaveSessionAction extends CytoscapeAction {
 		// Execute Task in New Thread; pop open JTask Dialog Box.
 		TaskManager.executeTask(task, jTaskConfig);
 	}
-} // SaveAsGMLAction
+} //End of SaveSessionAction
 
 /**
- * Task to save current session as a project file.
+ * Save Session Task.<br>
+ * Call the Session Writer to save the following:<br>
+ * <ul>
+ * <li>Networks with metadata</li>
+ * <li>All attributes (for nodes, edges, and network)</li>
+ * <li>Visual Styles</li>
+ * <li>Cytoscape Properties</li>
+ * </ul>
  * 
- * To save the current status (call this 'session'), we need to create the
- * following structure: 0. Networks (includes the following) 1. Nodes with
- * attributes 2. Edges with attributes 3. Tree structure of the networks 4.
- * Vizmap 5. GraphML style properties (tentative) 6. Properties 7. Hidden node
- * info.
+ * @author kono
  * 
- * everything will be stored in XML. Schema will be available soon.
  */
 class SaveSessionTask implements Task {
 
 	private String fileName;
 	private TaskMonitor taskMonitor;
-	
+
 	CytoscapeSessionWriter sw;
 
-	// private CytoscapeSessionWriter sw;
-
 	/**
-	 * Constructor.
+	 * Constructor.<br>
 	 * 
-	 * @param network
-	 *            Network Object.
-	 * @param view
-	 *            Network View Object.
-	 * @throws JAXBException
+	 * @param fileName
+	 *            Absolute path to the Session file.
 	 */
 	SaveSessionTask(String fileName) {
 		this.fileName = fileName;
-
 		// Create session writer object
 		sw = new CytoscapeSessionWriter(fileName);
-
 	}
 
 	/**
-	 * Executes Task
+	 * Execute task.<br>
 	 */
 	public void run() {
-		taskMonitor.setStatus("Saving Session.\n\nIf Session file is large, it takes a while.\nPlease be patient...");
+		taskMonitor
+				.setStatus("Saving Cytoscape Session.\n\nIt may take a while.  Please wait...");
 		taskMonitor.setPercentCompleted(-1);
 
 		try {
 			sw.writeSessionToDisk();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			taskMonitor.setException(e, "Could not write session to the file: "
+					+ fileName);
 		}
 
 		taskMonitor.setPercentCompleted(100);
 		taskMonitor.setStatus("Session successfully saved to:  " + fileName);
-		
+
 		// Show the session Name as the window title.
 		File shortName = new File(fileName);
-		Cytoscape.getDesktop().setTitle("Cytoscape Desktop (Session: " + shortName.getName() + ")" );
+		Cytoscape.setCurrentSessionFileName(fileName);
+		Cytoscape.getDesktop().setTitle(
+				"Cytoscape Desktop (Session: " + shortName.getName() + ")");
 	}
 
 	/**
@@ -205,7 +208,7 @@ class SaveSessionTask implements Task {
 	 * @return Task Title.
 	 */
 	public String getTitle() {
-		return new String("Saving Project");
+		return new String("Saving Cytoscape Session");
 	}
 
-} // End of SaveSessionAction
+} // End of SaveSessionTask
