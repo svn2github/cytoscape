7a68cbcb4ad9c8580441ad1eaec8d16316fd472a
iavila <iavila@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-06-28 19:07:06 +0000

    *** empty log message ***
    git-svn-id: http://chianti.ucsd.edu/svn@3920 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/isb/iavila/metaNodePlugin/src/metanodePlugin/view/VisualStyleFactory.java
+++ csplugins/trunk/isb/iavila/metaNodePlugin/src/metanodePlugin/view/VisualStyleFactory.java
@@ -49,106 +49,110 @@ public class VisualStyleFactory {
    */
   public static final String NODE_TYPE_ATT = "nodeType";
   
+  public static boolean sizeProportionalToNumChildren = true;
+  
+  
   /**
-   * Creates and returns a visual style for meta-nodes as modeled by 
-   * <code>metaNodeViewer.model.AbstractMetaNodeModeler</code>
+   * Creates and returns a visual style for meta-nodes, sizing of nodes proportional to children number is optional
    * 
-   * @param network the CyNetwork with meta-nodes
+   * @param network the CyNetwork with metanodes
+   * @return the VisualStyle
    */
   public static VisualStyle createAbstractMetaNodeVisualStyle (CyNetwork network){
-    
-    VisualMappingManager vmManager = Cytoscape.getVisualMappingManager();
-    NodeAppearanceCalculator nodeAppCalc = new NodeAppearanceCalculator();
-    EdgeAppearanceCalculator edgeAppCalc = new EdgeAppearanceCalculator();
-    CalculatorCatalog calculatorCatalog = vmManager.getCalculatorCatalog();
-    // ------------------------------ Set the label ------------------------------//
-    String cName = MetaNodeModelerFactory.getCytoscapeAbstractMetaNodeModeler().getNetworkAttributesHandler(network).getNodeLabelAttribute();
-    NodeLabelCalculator nlc = calculatorCatalog.getNodeLabelCalculator(cName);
-    if (nlc == null) {
-      PassThroughMapping m =
-        new PassThroughMapping(new String(), cName);
-      nlc = new GenericNodeLabelCalculator(cName, m);
-    }
-    nodeAppCalc.setNodeLabelCalculator(nlc);
-    
-    // ------------------------------ Set node shapes ---------------------------//
-    DiscreteMapping disMapping = new DiscreteMapping(new Byte(ShapeNodeRealizer.RECT),
-                                                     ObjectMapping.NODE_MAPPING);
-    disMapping.setControllingAttributeName(NODE_TYPE_ATT,
-                                           network,
-                                           false);
-    disMapping.putMapValue("metaNode", new Byte(ShapeNodeRealizer.ELLIPSE));
-    GenericNodeShapeCalculator shapeCalculator = 
-      new GenericNodeShapeCalculator("Abstract MetaNode", disMapping);
-    nodeAppCalc.setNodeShapeCalculator(shapeCalculator);
-    
-    //---------------------- Set the thickness of the border -------------------//
-    DiscreteMapping borderMapping = new DiscreteMapping(LineType.LINE_1,
-                                                        ObjectMapping.NODE_MAPPING);
-    borderMapping.setControllingAttributeName(NODE_TYPE_ATT,
-                                              network,
-                                              false);
-    borderMapping.putMapValue("metaNode", LineType.LINE_5);
-    GenericNodeLineTypeCalculator lineCalculator = 
-      new GenericNodeLineTypeCalculator("Abstract MetaNode",
-                                        borderMapping);
-    nodeAppCalc.setNodeLineTypeCalculator(lineCalculator);
-    
-    //--------------------- Set the size of the nodes --------------------------//
-    Double defaultWidth = new Double(70);
-    DiscreteMapping wMapping = new DiscreteMapping(defaultWidth,
-                                                   ObjectMapping.NODE_MAPPING);
-    wMapping.setControllingAttributeName(NODE_TYPE_ATT,
-                                         network,
-                                         true);
-    SpecificNodeSizeCalculator nodeSizeCalculator = 
-      new SpecificNodeSizeCalculator("Abstract MetaNode Width",
-                                     wMapping);
-    
-    nodeSizeCalculator.setSpecialAttrName(NodeAppearanceCalculator.nodeWidthBypass);
-    nodeAppCalc.setNodeWidthCalculator(nodeSizeCalculator);
-    
-    Double defaultHeight = new Double(50);
-    DiscreteMapping hMapping = new DiscreteMapping(defaultHeight,
-                                                   ObjectMapping.NODE_MAPPING);
-    hMapping.setControllingAttributeName(NODE_TYPE_ATT,
-                                         network,
-                                         true);
-    SpecificNodeSizeCalculator nodeSizeCalculator2 = 
-      new SpecificNodeSizeCalculator ("Abstract MetaNode Height",
-                                      hMapping);
-    
-    nodeSizeCalculator2.setSpecialAttrName(NodeAppearanceCalculator.nodeHeightBypass);
-    nodeAppCalc.setNodeHeightCalculator(nodeSizeCalculator2);
-    
-    // ------------------------------ Font sizes of labels -----------------------------//
-    DiscreteMapping fontSizeMapping = new DiscreteMapping (new Integer(12),
-                                                           ObjectMapping.NODE_MAPPING);
-    fontSizeMapping.setControllingAttributeName(NODE_TYPE_ATT,
-                                                network,
-                                                false);
-    fontSizeMapping.putMapValue("metaNode",new Integer(48));
-    
-    GenericNodeFontSizeCalculator fontSizeCalculator = 
-      new GenericNodeFontSizeCalculator("Abstract MetaNode",
-                                        fontSizeMapping);
-    nodeAppCalc.setNodeFontSizeCalculator(fontSizeCalculator);
-    
-    //------------------------- Create a visual style -------------------------------//
-    GlobalAppearanceCalculator gac = 
-      vmManager.getVisualStyle().getGlobalAppearanceCalculator();
-    VisualStyle vs = calculatorCatalog.getVisualStyle(ABSTRACT_METANODE_VS);
-    if(vs == null){
-    		vs = new VisualStyle(ABSTRACT_METANODE_VS,
-                                              nodeAppCalc,
-                                              edgeAppCalc,gac);
-    		calculatorCatalog.addVisualStyle(vs);
-    }else{
-    		vs.setNodeAppearanceCalculator(nodeAppCalc);
-    		vs.setEdgeAppearanceCalculator(edgeAppCalc);
-    		vs.setGlobalAppearanceCalculator(gac);
-    }
-    return vs;
-  }//createAbstractMetaNodeVisualStyle
-
+	  VisualMappingManager vmManager = Cytoscape.getVisualMappingManager();
+	    NodeAppearanceCalculator nodeAppCalc = new NodeAppearanceCalculator();
+	    EdgeAppearanceCalculator edgeAppCalc = new EdgeAppearanceCalculator();
+	    CalculatorCatalog calculatorCatalog = vmManager.getCalculatorCatalog();
+	    // ------------------------------ Set the label ------------------------------//
+	    String cName = MetaNodeModelerFactory.getCytoscapeAbstractMetaNodeModeler().getNetworkAttributesHandler(network).getNodeLabelAttribute();
+	    NodeLabelCalculator nlc = calculatorCatalog.getNodeLabelCalculator(cName);
+	    if (nlc == null) {
+	      PassThroughMapping m =
+	        new PassThroughMapping(new String(), cName);
+	      nlc = new GenericNodeLabelCalculator(cName, m);
+	    }
+	    nodeAppCalc.setNodeLabelCalculator(nlc);
+	    
+	    // ------------------------------ Set node shapes ---------------------------//
+	    DiscreteMapping disMapping = new DiscreteMapping(new Byte(ShapeNodeRealizer.RECT),
+	                                                     ObjectMapping.NODE_MAPPING);
+	    disMapping.setControllingAttributeName(NODE_TYPE_ATT,
+	                                           network,
+	                                           false);
+	    disMapping.putMapValue("metaNode", new Byte(ShapeNodeRealizer.ELLIPSE));
+	    GenericNodeShapeCalculator shapeCalculator = 
+	      new GenericNodeShapeCalculator("Abstract MetaNode", disMapping);
+	    nodeAppCalc.setNodeShapeCalculator(shapeCalculator);
+	    
+	    //---------------------- Set the thickness of the border -------------------//
+	    DiscreteMapping borderMapping = new DiscreteMapping(LineType.LINE_1,
+	                                                        ObjectMapping.NODE_MAPPING);
+	    borderMapping.setControllingAttributeName(NODE_TYPE_ATT,
+	                                              network,
+	                                              false);
+	    borderMapping.putMapValue("metaNode", LineType.LINE_5);
+	    GenericNodeLineTypeCalculator lineCalculator = 
+	      new GenericNodeLineTypeCalculator("Abstract MetaNode",
+	                                        borderMapping);
+	    nodeAppCalc.setNodeLineTypeCalculator(lineCalculator);
+	    
+	    //--------------------- Set the size of the nodes --------------------------//
+	    if(sizeProportionalToNumChildren){
+		    Double defaultWidth = new Double(70);
+		    DiscreteMapping wMapping = new DiscreteMapping(defaultWidth,
+		                                                   ObjectMapping.NODE_MAPPING);
+		    wMapping.setControllingAttributeName(NODE_TYPE_ATT,
+		                                         network,
+		                                         true);
+		    SpecificNodeSizeCalculator nodeSizeCalculator = 
+		      new SpecificNodeSizeCalculator("Abstract MetaNode Width",
+		                                     wMapping);
+		    
+		    nodeSizeCalculator.setSpecialAttrName(NodeAppearanceCalculator.nodeWidthBypass);
+		    nodeAppCalc.setNodeWidthCalculator(nodeSizeCalculator);
+		    
+		    Double defaultHeight = new Double(50);
+		    DiscreteMapping hMapping = new DiscreteMapping(defaultHeight,
+		                                                   ObjectMapping.NODE_MAPPING);
+		    hMapping.setControllingAttributeName(NODE_TYPE_ATT,
+		                                         network,
+		                                         true);
+		    SpecificNodeSizeCalculator nodeSizeCalculator2 = 
+		      new SpecificNodeSizeCalculator ("Abstract MetaNode Height",
+		                                      hMapping);
+		    
+		    nodeSizeCalculator2.setSpecialAttrName(NodeAppearanceCalculator.nodeHeightBypass);
+		    nodeAppCalc.setNodeHeightCalculator(nodeSizeCalculator2);
+	    }
+	    
+	    // ------------------------------ Font sizes of labels -----------------------------//
+	    if(sizeProportionalToNumChildren){
+		    DiscreteMapping fontSizeMapping = new DiscreteMapping (new Integer(12),
+		                                                           ObjectMapping.NODE_MAPPING);
+		    fontSizeMapping.setControllingAttributeName(NODE_TYPE_ATT,
+		                                                network,
+		                                                false);
+		    fontSizeMapping.putMapValue("metaNode",new Integer(48));
+		    
+		    GenericNodeFontSizeCalculator fontSizeCalculator = 
+		      new GenericNodeFontSizeCalculator("Abstract MetaNode",
+		                                        fontSizeMapping);
+		    nodeAppCalc.setNodeFontSizeCalculator(fontSizeCalculator);
+	    }
+	    //------------------------- Create a visual style -------------------------------//
+	    GlobalAppearanceCalculator gac = 
+	      vmManager.getVisualStyle().getGlobalAppearanceCalculator();
+	    VisualStyle vs = calculatorCatalog.getVisualStyle(ABSTRACT_METANODE_VS);
+	    if(vs == null){
+	    		vs = new VisualStyle(ABSTRACT_METANODE_VS,
+	                                              nodeAppCalc,
+	                                              edgeAppCalc,gac);
+	    		calculatorCatalog.addVisualStyle(vs);
+	    }else{
+	    		vs.setNodeAppearanceCalculator(nodeAppCalc);
+	    		vs.setEdgeAppearanceCalculator(edgeAppCalc);
+	    		vs.setGlobalAppearanceCalculator(gac);
+	    }
+	    return vs; 
+  }
 }//VisualStyleFactory
