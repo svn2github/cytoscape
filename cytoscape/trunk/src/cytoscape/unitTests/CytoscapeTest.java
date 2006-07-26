53a05fc51c5777eaed93a3dcabe33122cc84866d
bradkohl <bradkohl@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-07-26 23:35:50 +0000

    some nullNetwork tests
    git-svn-id: http://chianti.ucsd.edu/svn@7800 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/unitTests/CytoscapeTest.java
+++ cytoscape/trunk/src/cytoscape/unitTests/CytoscapeTest.java
@@ -1,8 +1,17 @@
-package cytoscape.unitTests;
+package src.cytoscape.unitTests;
+
 
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import cytoscape.data.ImportHandler;
+import cytoscape.Cytoscape;
+import cytoscape.CyNetwork;
 
 public class CytoscapeTest extends TestCase {
+	CyNetwork cytoNetwork;
+	String title;
+	int nodeCount;
+	int edgeCount;
 
 	public void setUp() throws Exception {
 	}
@@ -11,16 +20,39 @@ public class CytoscapeTest extends TestCase {
 	}
 
 	public void testGetImportHandler() throws Exception {
-		//ImportHandler importHandler = Cytoscape.getImportHandler();
-		//assertEquals(importHandler.getClass(), ImportHandler.class);
+		ImportHandler importHandler = Cytoscape.getImportHandler();
+		assertEquals(importHandler.getClass(), ImportHandler.class);
+	}
+	
+	public void testNullNetwork() throws Exception {	
+		cytoNetwork = Cytoscape.getNullNetwork();
+		
+		title = cytoNetwork.getTitle();
+		assertEquals("0", title);
+		
+		nodeCount = cytoNetwork.getNodeCount();
+		assertEquals(0, nodeCount);
+		
+		edgeCount = cytoNetwork.getEdgeCount();
+		assertEquals(0, edgeCount);	
 	}
 	
 	//public void test
 	//try getting network attributes
 	
-	//try gettting the null network
-	
 	//try creating a network
+	public void testCreateNetwork() throws Exception {
+		cytoNetwork = Cytoscape.createNetworkFromFile("testNetwork");
+		
+		title = cytoNetwork.getTitle();
+		assertEquals("20", title);
+		
+		nodeCount = cytoNetwork.getNodeCount();
+		assertEquals(0, nodeCount);
+		
+		edgeCount = cytoNetwork.getEdgeCount();
+		assertEquals(0, edgeCount);
+	}
 	
 	
 }
