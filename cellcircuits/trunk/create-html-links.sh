0124faa5da2e231073be7c8d55e01121498436b1
cmak <cmak@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-08-21 23:16:14 +0000

    Added hotbackup script
    git-svn-id: http://chianti.ucsd.edu/svn@7897 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cellcircuits/trunk/create-html-links.sh
+++ cellcircuits/trunk/create-html-links.sh
@@ -1,5 +1,7 @@
 
 ln -s /var/www/html/search/data /var/www/html/search/v1.1/
+ln -s /var/www/html/idekerlab /var/www/html/search/v1.1/
+
 ln -s /var/www/html/Kelley2004 /var/www/html/search/v1.1/
 ln -s /var/www/html/Yeang2004 /var/www/html/search/v1.1/
 ln -s /var/www/html/Suthram2006 /var/www/html/search/v1.1/
