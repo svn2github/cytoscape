2cc5cc592cdc952434c767a38d149b25798562d8
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-08-22 18:50:58 +0000

    experimental branch for visual refactoring
    git-svn-id: http://chianti.ucsd.edu/svn@7908 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/branches/try_visual_refactoring/src/cytoscape/data/unitTests/CyAttributesEquivalenceTest.java
@@ -0,0 +1,350 @@
+
+/*
+  File: CyAttributesEquivalenceTest.java 
+  
+  Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+  
+  The Cytoscape Consortium is: 
+  - Institute for Systems Biology
+  - University of California San Diego
+  - Memorial Sloan-Kettering Cancer Center
+  - Institut Pasteur
+  - Agilent Technologies
+  
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+  
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute 
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute 
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute 
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+package cytoscape.data.unitTests;
+
+import junit.framework.TestCase;
+import cytoscape.data.CyAttributes;
+import cytoscape.data.CyAttributesImpl;
+import cytoscape.data.GraphObjAttributes;
+import cytoscape.data.Semantics;
+import cytoscape.Cytoscape;
+import cytoscape.CyNode;
+import cytoscape.CyEdge;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Tests that attributes set via CyAttributes are accessible via
+ * GraphObjAttributes, and vice versa.  This is an important test
+ * of backward compatibility.
+ */
+public class CyAttributesEquivalenceTest extends TestCase {
+    private CyAttributes cyAttributes;
+    private GraphObjAttributes graphAttributes;
+    private static final String DUMMY_ID_1 = "id_1";
+    private static final String DUMMY_ID_2 = "id_2";
+    private static final String DUMMY_BOOLEAN_ATTRIBUTE = "attribute1";
+    private static final String DUMMY_STRING_ATTRIBUTE = "attribute2";
+    private static final String DUMMY_LIST_ATTRIBUTE = "attribute3";
+    private static final String DUMMY_BUTTON_ATTRIBUTE = "attribute5";
+    private static final String DUMMY_STACK_ATTRIBUTE = "attribute6";
+
+    /**
+     * Set things up.
+     * @throws Exception All Exceptions.
+     */
+    protected void setUp() throws Exception {
+        cyAttributes = new CyAttributesImpl();
+        graphAttributes = new GraphObjAttributes (cyAttributes);
+    }
+
+    /**
+     * Tests Boolean Values.
+     */
+    public void testBooleanValue() {
+        //  Verify that attribute does not (yet) exist in either
+        //  CyAttributes or GraphObjAttributes.
+        boolean exists = cyAttributes.hasAttribute(DUMMY_ID_1,
+                DUMMY_BOOLEAN_ATTRIBUTE);
+        assertEquals (false, exists);
+
+        exists = graphAttributes.hasAttribute(DUMMY_BOOLEAN_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals (false, exists);
+
+        //  Since the attribute does not yet exist, this should be null in
+        //  both CyAttribues and GraphObjAttribues.
+        Boolean value = cyAttributes.getBooleanAttribute
+                (DUMMY_ID_1, DUMMY_BOOLEAN_ATTRIBUTE);
+        assertEquals (null, value);
+        Object o = graphAttributes.get(DUMMY_BOOLEAN_ATTRIBUTE, DUMMY_ID_1);
+        assertEquals (null, o);
+
+        //  Set a Boolean Attribute Value;  should work
+        cyAttributes.setAttribute(DUMMY_ID_1, DUMMY_BOOLEAN_ATTRIBUTE,
+                new Boolean (true));
+
+        //  Verify that attribute now exists in CyAttributes.
+        exists = cyAttributes.hasAttribute(DUMMY_ID_1, DUMMY_BOOLEAN_ATTRIBUTE);
+        assertEquals (true, exists);
+
+        //  Verify that attribute now exists in GraphObjAttributes
+        exists = graphAttributes.hasAttribute(DUMMY_BOOLEAN_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals (true, exists);
+
+        //  Verify # of Attributes in GraphObjAttributes
+        int num = graphAttributes.numberOfAttributes();
+        assertEquals (1, num);
+
+        //  Verify value stored in CyAttributes
+        value = cyAttributes.getBooleanAttribute(DUMMY_ID_1,
+                DUMMY_BOOLEAN_ATTRIBUTE);
+        assertEquals (true, value.booleanValue());
+
+        //  Verify value stored in GraphObjAttributes
+        value = (Boolean) graphAttributes.get(DUMMY_BOOLEAN_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals (true, value.booleanValue());
+
+        //  Check out the attribute names in CyAttributes
+        String attribs[] = cyAttributes.getAttributeNames();
+        assertEquals (1, attribs.length);
+        assertEquals (DUMMY_BOOLEAN_ATTRIBUTE, attribs[0]);
+
+        //  Check out the attribute names in GraphObjAttributes
+        attribs = graphAttributes.getAttributeNames();
+        assertEquals (1, attribs.length);
+        assertEquals (DUMMY_BOOLEAN_ATTRIBUTE, attribs[0]);
+
+        //  Now delete the attribute
+        boolean success = cyAttributes.deleteAttribute(DUMMY_ID_1,
+                DUMMY_BOOLEAN_ATTRIBUTE);
+        assertEquals (true, success);
+
+        //  Verify that attribute no longer exists in CyAttributes
+        exists = cyAttributes.hasAttribute(DUMMY_ID_1, DUMMY_BOOLEAN_ATTRIBUTE);
+        assertEquals (false, exists);
+
+        //  Verify that attribute no longer exists in GraphObjAttributes
+        exists = graphAttributes.hasAttribute(DUMMY_BOOLEAN_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals (false, exists);
+
+        //  Try setting an Integer value;  this should fail
+        try {
+            graphAttributes.set(DUMMY_BOOLEAN_ATTRIBUTE, DUMMY_ID_2, new
+                Integer (5));
+            fail ("Illegal Argument Exception should have been thrown.");
+        } catch (IllegalArgumentException e) {
+            assertTrue (e != null);
+        }
+    }
+
+    /**
+     * Tests Boolean Values, Take 2.
+     */
+    public void testBooleanValues2() {
+        //  Try setting a value via GraphObjAttributes
+        graphAttributes.set(DUMMY_BOOLEAN_ATTRIBUTE, DUMMY_ID_2,
+                new Boolean(false));
+
+        //  Verify value stored in CyAttributes
+        Boolean value = cyAttributes.getBooleanAttribute(DUMMY_ID_2,
+                DUMMY_BOOLEAN_ATTRIBUTE);
+        assertEquals (false, value.booleanValue());
+
+        //  Verify value stored in GraphObjAttributes
+        value = (Boolean) graphAttributes.get(DUMMY_BOOLEAN_ATTRIBUTE,
+                DUMMY_ID_2);
+        assertEquals (false, value.booleanValue());
+    }
+
+    /**
+     * Tests String Values.
+     */
+    public void testStringValue() {
+        //  Set a String Attribute Value via CyAttributes
+        cyAttributes.setAttribute(DUMMY_ID_1, DUMMY_STRING_ATTRIBUTE,
+                new String ("Cytoscape"));
+
+        //  Verify that attribute now exists in CyAttributes
+        boolean exists = cyAttributes.hasAttribute(DUMMY_ID_1,
+                DUMMY_STRING_ATTRIBUTE);
+        assertEquals (true, exists);
+
+        //  Verify that attribute now exists in GraphObjAttributes
+        exists = graphAttributes.hasAttribute(DUMMY_STRING_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals (true, exists);
+
+        //  Verify value stored in CyAttributes
+        String value = cyAttributes.getStringAttribute(DUMMY_ID_1,
+                DUMMY_STRING_ATTRIBUTE);
+        assertEquals ("Cytoscape", value);
+
+        //  Verify value stored in GraphObjAttributes
+        value = (String) graphAttributes.get(DUMMY_STRING_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals ("Cytoscape", value);
+        value = graphAttributes.getStringValue(DUMMY_STRING_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals ("Cytoscape", value);
+
+        //  Try getting as a Double;  should result in a null;
+        Double d = graphAttributes.getDoubleValue(DUMMY_STRING_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertTrue (d==null);
+    }
+
+    /**
+     * Tests Simple Lists, Take 1
+     */
+    public void testSimpleLists1() {
+        //  Set a list via CyAttributes
+        List list = new ArrayList();
+        list.add(new Integer (5));
+        list.add(new Integer (6));
+        cyAttributes.setAttributeList(DUMMY_ID_1, DUMMY_LIST_ATTRIBUTE, list);
+
+        //  Verify value exists in CyAttributes
+        boolean exists = cyAttributes.hasAttribute(DUMMY_ID_1,
+                DUMMY_LIST_ATTRIBUTE);
+        assertEquals (true, exists);
+
+        //  Verify value exists in GraphObjAttributes
+        exists = graphAttributes.hasAttribute(DUMMY_LIST_ATTRIBUTE, DUMMY_ID_1);
+        assertEquals (true, exists);
+
+        //  Get the list back from CyAttributes, and verify its contents
+        List storedList = cyAttributes.getAttributeList(DUMMY_ID_1,
+                DUMMY_LIST_ATTRIBUTE);
+        assertEquals (2, storedList.size());
+        Integer int0 = (Integer) storedList.get(0);
+        Integer int1 = (Integer) storedList.get(1);
+        assertEquals (5, int0.intValue());
+        assertEquals (6, int1.intValue());
+
+        //  Get the list back from GraphObjAttributes, and verify its contents
+        storedList = graphAttributes.getList(DUMMY_LIST_ATTRIBUTE, DUMMY_ID_1);
+        assertEquals (2, storedList.size());
+        int0 = (Integer) storedList.get(0);
+        int1 = (Integer) storedList.get(1);
+        assertEquals (5, int0.intValue());
+        assertEquals (6, int1.intValue());
+    }
+
+    /**
+     * Tests Simple Lists, Take 2
+     */
+    public void testSimpleLists2() {
+        //  Set a list via GraphObjAttributes
+        graphAttributes.append(DUMMY_LIST_ATTRIBUTE, DUMMY_ID_1,
+                new Integer(5));
+        graphAttributes.append(DUMMY_LIST_ATTRIBUTE, DUMMY_ID_1,
+                new Integer(6));
+
+        //  Verify value exists in CyAttributes
+        boolean exists = cyAttributes.hasAttribute(DUMMY_ID_1,
+                DUMMY_LIST_ATTRIBUTE);
+        assertEquals (true, exists);
+
+        //  Verify value exists in GraphObjAttributes
+        exists = graphAttributes.hasAttribute(DUMMY_LIST_ATTRIBUTE, DUMMY_ID_1);
+        assertEquals (true, exists);
+
+        //  Get the list back from CyAttributes, and verify its contents
+        List storedList = cyAttributes.getAttributeList(DUMMY_ID_1,
+                DUMMY_LIST_ATTRIBUTE);
+        assertEquals (2, storedList.size());
+        Integer int0 = (Integer) storedList.get(0);
+        Integer int1 = (Integer) storedList.get(1);
+        assertEquals (5, int0.intValue());
+        assertEquals (6, int1.intValue());
+
+        //  Get the list back from GraphObjAttributes, and verify its contents
+        storedList = graphAttributes.getList(DUMMY_LIST_ATTRIBUTE, DUMMY_ID_1);
+        assertEquals (2, storedList.size());
+        int0 = (Integer) storedList.get(0);
+        int1 = (Integer) storedList.get(1);
+        assertEquals (5, int0.intValue());
+        assertEquals (6, int1.intValue());
+    }
+
+    /**
+     * Tests that we can still store arbitary objects in GraphObjAttributes.
+     */
+    public void testArbitraryObjects() {
+        //  Create an arbitary object
+        JButton button = new JButton ("Hello World!");
+
+        //  Store it in GraphObjAttributes
+        graphAttributes.set(DUMMY_BUTTON_ATTRIBUTE, DUMMY_ID_1, button);
+
+        //  Get it back
+        button = (JButton) graphAttributes.get(DUMMY_BUTTON_ATTRIBUTE,
+                DUMMY_ID_1);
+        assertEquals ("Hello World!", button.getText());
+
+        //  Test another arbitrary Object
+        Stack stack = new Stack();
+        stack.push(new String ("Apple"));
+        graphAttributes.set(DUMMY_BUTTON_ATTRIBUTE, DUMMY_ID_2, stack);
+
+        //  Get it back
+        stack = (Stack) graphAttributes.get(DUMMY_BUTTON_ATTRIBUTE, DUMMY_ID_2);
+        String value = (String) stack.pop();
+        assertEquals ("Apple", value);
+
+        //  Verify that you can store arbitary objects via Cytoscape.java
+        //  First test node attributes
+        stack = new Stack();
+        stack.push(new String ("Apple"));
+        CyNode node = Cytoscape.getCyNode(DUMMY_ID_1, true);
+        Cytoscape.setNodeAttributeValue(node, DUMMY_STACK_ATTRIBUTE,
+                stack);
+        stack = (Stack) Cytoscape.getNodeAttributeValue
+                (node, DUMMY_STACK_ATTRIBUTE);
+        value = (String) stack.pop();
+        assertEquals ("Apple", value);
+
+        //  Then test edge attributes
+        stack = new Stack();
+        stack.push(new String ("Apple"));
+        CyNode node1 = Cytoscape.getCyNode("1", true);
+        CyNode node2 = Cytoscape.getCyNode("2", true);
+        CyEdge edge = Cytoscape.getCyEdge(node1, node2, Semantics.INTERACTION,
+                "pp", true);
+        Cytoscape.setEdgeAttributeValue(edge, DUMMY_STACK_ATTRIBUTE, stack);
+        stack = (Stack) Cytoscape.getEdgeAttributeValue
+                (edge, DUMMY_STACK_ATTRIBUTE);
+        value = (String) stack.pop();
+        assertEquals ("Apple", value);
+    }
+
+    /**
+     * Runs just this one unit test.
+     */
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(CyAttributesEquivalenceTest.class);
+    }
+}
---
+++ cytoscape/branches/try_visual_refactoring/src/cytoscape/unitTests/CytoscapeTest.java
@@ -0,0 +1,170 @@
+package cytoscape.unitTests;
+
+
+import junit.framework.TestCase;
+import cytoscape.CyNetwork;
+import cytoscape.Cytoscape;
+import cytoscape.CyEdge;
+import cytoscape.data.ImportHandler;
+import cytoscape.data.Semantics;
+import giny.model.Edge;
+import giny.model.Node;
+import java.util.*; 
+
+public class CytoscapeTest extends TestCase {
+	CyNetwork cytoNetwork;
+	String title;
+	int nodeCount;
+	int edgeCount;
+
+	public void setUp() throws Exception {
+	}
+	
+	public void tearDown() throws Exception {
+	}
+
+	public void testGetImportHandler() throws Exception {
+		ImportHandler importHandler = Cytoscape.getImportHandler();
+		assertEquals(importHandler.getClass(), ImportHandler.class);
+	}
+	
+	public void testNullNetwork() throws Exception {	
+		cytoNetwork = Cytoscape.getNullNetwork();
+		
+		title = cytoNetwork.getTitle();
+		assertEquals("0", title);
+		
+		nodeCount = cytoNetwork.getNodeCount();
+		assertEquals(0, nodeCount);
+		
+		edgeCount = cytoNetwork.getEdgeCount();
+		assertEquals(0, edgeCount);	
+	}
+	
+	//public void test
+	//try getting network attributes
+	
+	//try creating a network
+	public void testCreateNetwork() throws Exception {
+		try {
+		cytoNetwork = Cytoscape.createNetworkFromFile("testNetwork");
+		
+		/*
+		 * Network title is unpredictable! 
+		 */
+//		title = cytoNetwork.getTitle();
+//		assertEquals("20", title);
+		
+		nodeCount = cytoNetwork.getNodeCount();
+		assertEquals(0, nodeCount);
+		
+		edgeCount = cytoNetwork.getEdgeCount();
+		assertEquals(0, edgeCount);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	public void testBug839() {
+
+		Set<String> nodes = new HashSet<String>();
+		nodes.add("n1");
+		nodes.add("n2");
+		nodes.add("n3");
+		nodes.add("n4");
+		nodes.add("n5");
+
+		cytoNetwork = Cytoscape.createNetworkFromFile("testData/bug_839.sif");
+
+		// check that all nodes exist
+		Iterator it = cytoNetwork.nodesIterator();
+		while ( it.hasNext() ) {
+			Node n = (Node)it.next();
+			assertTrue("checking node " + n.getIdentifier(), 
+			           nodes.contains(n.getIdentifier()));
+		}
+
+		Set<String> edges = new HashSet<String>();
+		edges.add(CyEdge.createIdentifier("n1","activates","n2"));
+		edges.add(CyEdge.createIdentifier("n1","activates","n4"));
+		edges.add(CyEdge.createIdentifier("n1","activates","n5"));
+		edges.add(CyEdge.createIdentifier("n2","activates","n1"));
+		edges.add(CyEdge.createIdentifier("n2","activates","n5"));
+		edges.add(CyEdge.createIdentifier("n3","inhibits","n3"));
+		edges.add(CyEdge.createIdentifier("n3","inhibits","n4"));
+		edges.add(CyEdge.createIdentifier("n3","inhibits","n5"));
+		edges.add(CyEdge.createIdentifier("n4","activates","n1"));
+		edges.add(CyEdge.createIdentifier("n4","activates","n2"));
+		edges.add(CyEdge.createIdentifier("n4","activates","n4"));
+		edges.add(CyEdge.createIdentifier("n5","activates","n1"));
+		edges.add(CyEdge.createIdentifier("n5","activates","n4"));
+		edges.add(CyEdge.createIdentifier("n5","activates","n5"));
+
+		it = cytoNetwork.edgesIterator();
+		while ( it.hasNext() ) {
+			Edge e = (Edge)it.next();
+			assertTrue("checking edge " + e.getIdentifier(), edges.contains(e.getIdentifier()));
+		}
+	}
+
+	public void testgetCyEdgeWithStrings() {
+		
+		cytoNetwork = Cytoscape.createNetworkFromFile("testData/directedGraph.sif");
+
+		assertEquals(2,cytoNetwork.getNodeCount());
+		assertEquals(4,cytoNetwork.getEdgeCount());
+
+		String en1 = CyEdge.createIdentifier("a","pp","b");
+
+		// edge should exist in network already
+		CyEdge ce1 = Cytoscape.getCyEdge("a",en1,"b","pp");
+		assertNotNull(ce1);
+
+		CyEdge ce1_again = Cytoscape.getCyEdge("a",en1,"b","pp");
+		assertTrue(ce1 == ce1_again);
+
+		// edge should be created
+		String en2 = CyEdge.createIdentifier("a","xx","b");
+		CyEdge ce2 = Cytoscape.getCyEdge("a",en2,"b","pp");
+		assertNotNull(ce2);
+
+		// should create a different edge because of directedness
+		String en3 = CyEdge.createIdentifier("b","pp","a");
+		CyEdge ce3 = Cytoscape.getCyEdge("b",en3,"a","pp");
+		assertTrue(ce1 != ce3);
+
+	}
+
+	public void testgetCyEdgeWithNodes() {
+		cytoNetwork = Cytoscape.createNetworkFromFile("testData/directedGraph.sif");
+		Node a = Cytoscape.getCyNode("a");
+		Node b = Cytoscape.getCyNode("b");
+		Node c = Cytoscape.getCyNode("c",true); 
+		String attr = Semantics.INTERACTION;
+
+		// test directed edges
+		assertNotNull(Cytoscape.getCyEdge(a,b,attr,"pp",false,true));
+		assertNotNull(Cytoscape.getCyEdge(b,a,attr,"pp",false,true));
+		assertNotNull(Cytoscape.getCyEdge(a,a,attr,"pp",false,true));
+		assertNotNull(Cytoscape.getCyEdge(a,a,attr,"pp",false,true));
+		assertNotNull(Cytoscape.getCyEdge(a,b,attr,"pd",false,true));
+		assertNull(Cytoscape.getCyEdge(b,a,attr,"pd",false,true));
+
+		// test undirectedness
+		assertNotNull(Cytoscape.getCyEdge(b,a,attr,"pd",false,false));
+
+		// test non-existent edge
+		assertNull(Cytoscape.getCyEdge(a,c,attr,"pp",false,true));
+
+		// test bad attr_value
+		assertNull(Cytoscape.getCyEdge(a,b,attr,"xx",false,true));
+
+		// test create node
+		assertNotNull(Cytoscape.getCyEdge(a,c,attr,"pd",true,true));
+
+		// make sure we got the node we created
+		assertNotNull(Cytoscape.getCyEdge(a,c,attr,"pd",false,true));
+	}
+
+}
