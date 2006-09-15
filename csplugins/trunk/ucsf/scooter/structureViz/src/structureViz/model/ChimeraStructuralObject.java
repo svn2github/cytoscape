4fd38d86b5113211e72af55893c5b7a10525150b
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-09-15 19:32:27 +0000

    More refactoring
    git-svn-id: http://chianti.ucsd.edu/svn@8206 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsf/scooter/structureViz/src/structureViz/model/ChimeraStructuralObject.java
@@ -0,0 +1,82 @@
+/* vim: set ts=2: */
+/**
+ * Copyright (c) 2006 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions, and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions, and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *   3. Redistributions must acknowledge that this software was
+ *      originally developed by the UCSF Computer Graphics Laboratory
+ *      under support by the NIH National Center for Research Resources,
+ *      grant P41-RR01081.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package structureViz.model;
+
+import java.lang.String;
+import structureViz.model.ChimeraModel;
+
+/**
+ * This interface provides a common set of methods that are implemented
+ * by the ChimeraModel, ChimeraChain, and ChimeraResidue classes.
+ * 
+ * @author scooter
+ *
+ */
+
+public interface ChimeraStructuralObject {
+		
+	/**
+	 * Return the Chimera selection specification for this object
+	 *
+	 * @return a String representing a Chimera atom-spec
+	 */
+	public String toSpec();
+
+	/**
+	 * Return a String representation for this object
+	 *
+	 * @return a String representing the object name
+	 */
+	public String toString();
+
+	/**
+	 * Return the userData for this object
+	 *
+	 * @return an Object representing the userData (usually TreePath)
+	 */
+	public Object getUserData();
+
+	/**
+	 * Set the userData for this object
+	 *
+	 * @param userData the Object representing the userData (usually TreePath)
+	 */
+	public void setUserData(Object userData);
+
+	/**
+	 * Return the ChimeraModel for this object
+	 *
+	 * @return the ChimeraModel this object is part of
+	 */
+	public ChimeraModel getChimeraModel();
+}
---
+++ csplugins/trunk/ucsf/scooter/structureViz/src/structureViz/ui/PopupMenuListener.java
@@ -0,0 +1,146 @@
+/* vim: set ts=2: */
+/**
+ * Copyright (c) 2006 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions, and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions, and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *   3. Redistributions must acknowledge that this software was
+ *      originally developed by the UCSF Computer Graphics Laboratory
+ *      under support by the NIH National Center for Research Resources,
+ *      grant P41-RR01081.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package structureViz.ui;
+
+// System imports
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.*;
+import javax.swing.tree.*;
+import javax.swing.event.*;
+
+import java.awt.event.*;
+
+// Cytoscape imports
+
+// StructureViz imports
+import structureViz.model.ChimeraModel;
+import structureViz.model.ChimeraResidue;
+import structureViz.model.ChimeraChain;
+import structureViz.model.ChimeraTreeModel;
+import structureViz.ui.ActionPopupMenu;
+
+import structureViz.Chimera;
+
+class PopupMenuListener implements MouseListener {
+	JTree navigationTree;
+	Chimera chimeraObject;
+
+	public PopupMenuListener(Chimera object, JTree tree) {
+		this.navigationTree = tree;
+		this.chimeraObject = object;
+	}
+
+	public void mouseEntered(MouseEvent ev) {return;}
+	public void mouseClicked(MouseEvent ev) {return;}
+	public void mouseExited(MouseEvent ev) {return;}
+
+	public void mousePressed(MouseEvent ev) {
+		if (ev.isPopupTrigger())
+			createPopupMenu(ev);
+	}
+
+	public void mouseReleased(MouseEvent ev) {
+		if (ev.isPopupTrigger())
+			createPopupMenu(ev);
+	}
+
+	private void createPopupMenu(MouseEvent ev) {
+		// Create our popup menu -- depends on what's selected.  If we
+		// have things selected at multiple levels of the hierarchy, see
+		// if the menu was over a specific type
+		TreePath overPath = navigationTree.getPathForLocation(ev.getX(), ev.getY());
+		TreePath[] paths = navigationTree.getSelectionPaths();
+		Object userObject = null;
+
+		if (paths == null && overPath == null) {
+			System.out.println("No context");
+		} else if (paths == null) {
+			paths = new TreePath[1];
+			paths[0] = overPath;
+		}
+
+		if (overPath != null) {
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode) overPath.getLastPathComponent();
+			userObject = node.getUserObject();
+		}
+
+		// Create lists of types
+		ArrayList modelList = new ArrayList();
+		ArrayList chainList = new ArrayList();
+		ArrayList residueList = new ArrayList();
+		int context = 0;
+
+		for (int i = 0; i < paths.length; i++) {
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode) paths[i].getLastPathComponent();
+			Object nodeInfo = node.getUserObject();
+			if (nodeInfo.getClass() == ChimeraModel.class) {
+				modelList.add(nodeInfo);
+				if (modelList.contains(userObject)) {
+					if (context == 0)
+						context = ActionPopupMenu.MODEL_CONTEXT;
+					else
+						context = ActionPopupMenu.GENERIC_CONTEXT;
+				}
+			} else if (nodeInfo.getClass() == ChimeraChain.class) {
+				chainList.add(nodeInfo);
+				if (chainList.contains(userObject)) {
+					if (context == 0)
+						context = ActionPopupMenu.CHAIN_CONTEXT;
+					else
+						context = ActionPopupMenu.GENERIC_CONTEXT;
+				}
+			} else if (nodeInfo.getClass() == ChimeraResidue.class) {
+				residueList.add(nodeInfo);
+				if (residueList.contains(userObject)) {
+					if (context == 0)
+						context = ActionPopupMenu.RESIDUE_CONTEXT;
+					else
+						context = ActionPopupMenu.GENERIC_CONTEXT;
+				}
+			}
+		}
+
+		if (userObject != null) {
+			// Get the component
+			System.out.println("Clicked over "+userObject.toString());
+		}
+
+		JPopupMenu menu = new ActionPopupMenu(chimeraObject,navigationTree,
+																					modelList,chainList,residueList,context);
+		menu.setVisible(true);
+		menu.show(navigationTree, ev.getX(), ev.getY());
+
+	}
+}
