f9016c432d4637e1523a03d54836c0dfe723415a
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-09-15 20:13:43 +0000

    ManualLayout controls are moved to Cytopanel_3
    git-svn-id: http://chianti.ucsd.edu/svn@8210 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/ManualLayout/src/ManualLayout/ManualLayoutPlugin.java
+++ coreplugins/trunk/ManualLayout/src/ManualLayout/ManualLayoutPlugin.java
@@ -1,29 +1,53 @@
 package ManualLayout;
 
+import java.awt.Dimension;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.AbstractAction;
-import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
 
 import cytoscape.plugin.CytoscapePlugin;
 import cytoscape.Cytoscape;
 
-import ManualLayout.rotate.RotateAction;
-import ManualLayout.scale.ScaleAction;
-import ManualLayout.control.ControlAction;
+import ManualLayout.rotate.RotatePanel;
+import ManualLayout.scale.ScalePanel;
+import ManualLayout.control.ControlPanel;
 
 public class ManualLayoutPlugin extends CytoscapePlugin
 {
   public ManualLayoutPlugin()
   {
+	init();
+	
     JMenu layoutMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar()
                                 .getMenu("Layout");
-    JMenuItem rotateMenuItem = new JMenuItem(new RotateAction());
-    JMenuItem scaleMenuItem = new JMenuItem(new ScaleAction());
-    JMenuItem controlMenuItem = new JMenuItem(new ControlAction());
+    JMenuItem rotateMenuItem = new JMenuItem(new ManualLayoutAction("Rotate"));
+    JMenuItem scaleMenuItem = new JMenuItem(new ManualLayoutAction("Scale"));
+    JMenuItem controlMenuItem = new JMenuItem(new ManualLayoutAction("Align and Distribute"));
 
     layoutMenu.add(rotateMenuItem, 0);
     layoutMenu.add(scaleMenuItem, 1);
     layoutMenu.add(controlMenuItem, 2);
+  }
+  
+  private void init()
+  {
+	  //JTabbedPane manualLayoutPanel = new JTabbedPane();
+	  
+	  RotatePanel rotatePanel = new RotatePanel();
+	  //rotatePanel.setPreferredSize(new Dimension(100,50));
+	  //manualLayoutPanel.add("Rotation", rotatePanel);
+	  
+	  ScalePanel scalePanel = new ScalePanel();
+	  //scalePanel.setPreferredSize(new Dimension(100,50));
+	  //manualLayoutPanel.add("Scale", ScalePanel);
+	  
+	  ControlPanel controlPanel = new ControlPanel();
+	  //controlPanel.setPreferredSize(new Dimension(100,50));
+	  //manualLayoutPanel.add("Control", controlPanel);
+	  
+	  Cytoscape.getDesktop().getCytoPanel(SwingConstants.EAST).add("Rotate",rotatePanel);
+	  Cytoscape.getDesktop().getCytoPanel(SwingConstants.EAST).add("Scale",scalePanel);
+	  Cytoscape.getDesktop().getCytoPanel(SwingConstants.EAST).add("Align and Distribute",controlPanel);
   }
 }
