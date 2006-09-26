57942dbe70cf7006a574fdfbb91638967c5a9dfb
cmak <cmak@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-09-26 18:20:11 +0000

    Docs now show object inheritance structure
    git-svn-id: http://chianti.ucsd.edu/svn@8336 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- pathfinder/trunk/perldoc.pl
+++ pathfinder/trunk/perldoc.pl
@@ -1,11 +1,15 @@
 #!/usr/bin/perl -w
+#
+# Generate JavaDoc-inspired HTML documentation for a group
+# of Perl packages (objects).
+# 
 
 use PerlFile;
 use File::Spec;
 
 if(scalar(@ARGV) != 2)
 {
-    die "?0: <directory> <outdir>\n";
+    die "?0: <directory of .p[ml] files> <output directory>\n";
 }
 
 my ($inDir, $outDir) = @ARGV;
@@ -14,7 +18,8 @@ my $pm = File::Spec->catfile( $inDir, "*.pm" );
 my $pl = File::Spec->catfile( $inDir, "*.pl" );
 my @perlFiles = glob "$pm $pl";
 
-my @outputFiles;
+my %outputFiles;  # map each packageName to an HTML file
+my %tree;         # map packageName to the packages that inherit from it
 
 foreach $file (@perlFiles)
 {
@@ -22,21 +27,99 @@ foreach $file (@perlFiles)
 
     my $pf = PerlFile->new($file);
     
-    push @outputFiles, $pf->writeHTML($outDir);
+    my $filename = $pf->writeHTML($outDir);
+
+    if($pf->packageName() ne "")
+    {
+	$outputFiles{$pf->packageName()} = $filename;
+	
+	if(exists($pf->arrays()->{"ISA"}))
+	{
+	    foreach my $isa (@{$pf->arrays()->{"ISA"}})
+	    {
+		push @{$tree{$isa}}, $pf->packageName();
+	    }
+	}
+	else
+	{
+	    if(!exists($tree{$pf->packageName()}))
+	    {
+		$tree{$pf->packageName()} = [];
+	    }
+	}
+    }
+    else
+    {
+	# Uncomment to include .pl files in documentation
+	#$outputFiles{$file} = $filename;
+	#$tree{$file} = [];
+    }
+
 }
 
-makeIndex($outDir, \@outputFiles);
+makeIndex($outDir, \%outputFiles, \%tree);
 
 sub makeLink
 {
     my ($name, $file) = @_;
 
-    return "<a href=\"$file\" target=\"fileFrame\">$name</a>";
+    return "<a href=\"$file\" target=\"fileFrame\">$name</a><br>\n";
 }
 
+#
+# Return an array of the root nodes of the package tree
+#
+sub getRootPackages
+{
+    my ($tree) = @_;
+
+    my %childNodes;
+    my @roots;
+    foreach my $valArray (values %{$tree})
+    {
+	foreach my $val (@{$valArray})
+	{
+	    $childNodes{$val}++;
+	}
+    }
+
+    ## Root nodes are not children 
+    foreach my $key (keys %{$tree})
+    {
+	if(! exists($childNodes{$key}))
+	{
+	    push @roots, $key;
+	}
+    }
+
+    return @roots;
+}
+
+#
+# Recurse the package tree and generate HTML links to each file,
+# indenting each link according to its depth in the tree
+#
+sub recursePackageTree
+{
+    my ($HTML, $tree, $outputFiles, $root, $depth) = @_;
+
+    my $prefix = "";
+    
+    $prefix = "&nbsp;&nbsp;&nbsp;&nbsp;" x $depth;
+
+    $$HTML .= $prefix . makeLink($root, $outputFiles->{$root});
+    foreach my $child (@{$tree->{$root}})
+    {
+	recursePackageTree($HTML, $tree, $outputFiles, $child, $depth + 1);
+    }
+}
+
+#
+# Make index.html (entry point file) and overview-frame.html (navigation)
+#
 sub makeIndex
 {
-    my ($outDir, $outputFiles) = @_;
+    my ($outDir, $outputFiles, $packageTree) = @_;
     
     my $indexText = 
 	qq(<html>
@@ -51,9 +134,11 @@ sub makeIndex
 
     my $fileHTML = "";
 
-    foreach my $f (@{$outputFiles})
+    my @rootPackages = getRootPackages($packageTree);
+
+    foreach my $root (@rootPackages)
     {
-	$fileHTML .= makeLink(PerlFile::basename($f), $f) . "<br>\n";
+	recursePackageTree(\$fileHTML, $packageTree, $outputFiles, $root, 0);
     }
     
     my $now = localtime(time());
@@ -62,7 +147,7 @@ sub makeIndex
 	qq(<html>
 	   <head><title>All packages</title></head>
 	   <body>
-	   <b>All packages</b>
+	   <b>All packages</b><br>
 	   $fileHTML
 	   <br>
 	   <font size="-1">
@@ -86,4 +171,24 @@ sub makeIndex
     open(OUT, ">$overviewFile") || die "Can't open $overviewFile\n";
     print OUT $overviewText;
     close OUT;
+}
+
+#
+# Utility method for printing the tree
+#
+sub printTree
+{
+    my ($tree) = @_;
+    
+    foreach my $key (keys %{$tree})
+    {
+	my @vals = @{$tree->{$key}};
+
+	print "$key: ";
+	foreach my $v (@vals)
+	{
+	    print "$v, ";
+	}
+	print "\n";
+    }
 }
