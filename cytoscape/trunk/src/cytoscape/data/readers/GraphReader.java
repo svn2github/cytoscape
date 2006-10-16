f79c5142dc59b2a10ae4de1f4c1c7aaa518fda25
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-10-16 20:02:45 +0000

    Added a new doPostProcessing() method;  moved deprecated methods to bottom of class;  added additional javadocs
    git-svn-id: http://chianti.ucsd.edu/svn@8449 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/data/readers/GraphReader.java
+++ cytoscape/trunk/src/cytoscape/data/readers/GraphReader.java
@@ -1,36 +1,35 @@
-
 /*
-  File: GraphReader.java 
-  
+  File: GraphReader.java
+
   Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
-  
-  The Cytoscape Consortium is: 
+
+  The Cytoscape Consortium is:
   - Institute for Systems Biology
   - University of California San Diego
   - Memorial Sloan-Kettering Cancer Center
   - Institut Pasteur
   - Agilent Technologies
-  
+
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published
   by the Free Software Foundation; either version 2.1 of the License, or
   any later version.
-  
+
   This library is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
   documentation provided hereunder is on an "as is" basis, and the
-  Institute for Systems Biology and the Whitehead Institute 
+  Institute for Systems Biology and the Whitehead Institute
   have no obligations to provide maintenance, support,
   updates, enhancements or modifications.  In no event shall the
-  Institute for Systems Biology and the Whitehead Institute 
+  Institute for Systems Biology and the Whitehead Institute
   be liable to any party for direct, indirect, special,
   incidental or consequential damages, including lost profits, arising
   out of the use of this software and its documentation, even if the
-  Institute for Systems Biology and the Whitehead Institute 
+  Institute for Systems Biology and the Whitehead Institute
   have been advised of the possibility of such damage.  See
   the GNU Lesser General Public License for more details.
-  
+
   You should have received a copy of the GNU Lesser General Public License
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
@@ -38,42 +37,76 @@
 
 package cytoscape.data.readers;
 
+import cytoscape.data.CyAttributes;
+import cytoscape.CyNetwork;
 import giny.model.RootGraph;
 import giny.view.GraphView;
 
 import java.io.IOException;
 
-import cytoscape.data.CyAttributes;
-
+/**
+ * Interface for Reading in Cytoscape Graphs.
+ *
+ * @author Cytoscape Development Group.
+ */
 public interface GraphReader {
 
-	public void read() throws IOException;
+    /**
+     * Reads/imports the graph.
+     *
+     * @throws IOException IO Errors.
+     */
+    public void read() throws IOException;
 
-        /**
-         * @deprecated Use read() instead.  Will be removed Dec 2006.
-         */
-	public void read(boolean canonicalizeNodeNames) throws IOException;
+    /**
+     * Lays out the graph.
+     *
+     * @param myView
+     */
+    public void layout(GraphView myView);
 
-        /**
-         * @deprecated Use Cytoscape.getRootGraph() instead. Will be removed Dec 2006.
-         */
-	public RootGraph getRootGraph();
+    /**
+     * Gets an array of node indices that participate in the newly created graph.
+     *
+     * @return array of node indices from the root graph.
+     */
+    public int[] getNodeIndicesArray();
 
-        /**
-         * @deprecated Use Cytoscape.getNodeAttributes() instead. Will be removed Dec 2006.
-         */
-	public CyAttributes getNodeAttributes();
+    /**
+     * Gets an array of edges indices that participate in the newly created graph.
+     *
+     * @return array of edges indices from the root graph.
+     */
+    public int[] getEdgeIndicesArray();
 
-        /**
-         * @deprecated Use Cytoscape.getEdgeAttributes() instead. Will be removed Dec 2006.
-         */
-	public CyAttributes getEdgeAttributes();
+    /**
+     * Execute whatever post-processing is required.
+     */
+    public void doPostProcessing(CyNetwork network);
 
-	public void layout(GraphView myView);
+    /**
+     * @deprecated Use read() instead.  Will be removed Dec 2006.
+     */
+    public void read(boolean canonicalizeNodeNames) throws IOException;
 
-	public int[] getNodeIndicesArray();
+    /**
+     * @deprecated Use Cytoscape.getRootGraph() instead. Will be removed Dec 2006.
+     */
+    public RootGraph getRootGraph();
 
-	public int[] getEdgeIndicesArray();
+    /**
+     * @deprecated Use Cytoscape.getNodeAttributes() instead. Will be removed Dec 2006.
+     */
+    public CyAttributes getNodeAttributes();
 
-	public String getNetworkName();
+    /**
+     * @deprecated Use Cytoscape.getEdgeAttributes() instead. Will be removed Dec 2006.
+     */
+    public CyAttributes getEdgeAttributes();
+
+    /**
+     * Gets the name of the network.
+     * @return network name.
+     */
+    public String getNetworkName();
 }
