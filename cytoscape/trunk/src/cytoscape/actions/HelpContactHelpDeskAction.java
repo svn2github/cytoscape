a456542312db346c6f3e533edcb66447a18356f2
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-10-19 22:02:55 +0000

    Original creation
    git-svn-id: http://chianti.ucsd.edu/svn@8518 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/trunk/src/cytoscape/actions/HelpContactHelpDeskAction.java
@@ -0,0 +1,104 @@
+/*
+ File: HelpContactHelpDeskAction.java 
+ 
+ Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+ 
+ The Cytoscape Consortium is: 
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+ 
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+ 
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute 
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute 
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute 
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+package cytoscape.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.lang.reflect.Method;
+import javax.swing.JOptionPane;
+
+
+public class HelpContactHelpDeskAction extends AbstractAction {
+
+	private String helpDeskURL = "http://groups.google.com/group/cytoscape-helpdesk";
+	
+	public HelpContactHelpDeskAction() {
+		super("Contact Help Desk");
+	}
+
+	public void actionPerformed(ActionEvent e) {		
+		openURL(helpDeskURL);
+	}
+
+	
+	/////////////////////////////////////////////////////////
+	//  Bare Bones Browser Launch                          //
+	//  Version 1.5                                        //
+	//  December 10, 2005                                  //
+	//  Supports: Mac OS X, GNU/Linux, Unix, Windows XP    //
+	//  Example Usage:                                     //
+	//     String url = "http://www.centerkey.com/";       //
+	//     BareBonesBrowserLaunch.openURL(url);            //
+	//  Public Domain Software -- Free to Use as You Like  //
+	/////////////////////////////////////////////////////////
+
+
+   //public class BareBonesBrowserLaunch {
+
+   private static final String errMsg = "Error attempting to launch web browser";
+
+   public static void openURL(String url) {
+      String osName = System.getProperty("os.name");
+      try {
+         if (osName.startsWith("Mac OS")) {
+            Class fileMgr = Class.forName("com.apple.eio.FileManager");
+            Method openURL = fileMgr.getDeclaredMethod("openURL",
+               new Class[] {String.class});
+            openURL.invoke(null, new Object[] {url});
+            }
+         else if (osName.startsWith("Windows"))
+            Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+         else { //assume Unix or Linux
+            String[] browsers = {
+               "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+            String browser = null;
+            for (int count = 0; count < browsers.length && browser == null; count++)
+               if (Runtime.getRuntime().exec(
+                     new String[] {"which", browsers[count]}).waitFor() == 0)
+                  browser = browsers[count];
+            if (browser == null)
+               throw new Exception("Could not find web browser");
+            else
+               Runtime.getRuntime().exec(new String[] {browser, url});
+            }
+         }
+      catch (Exception e) {
+         JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
+         }
+    }//End of openURL()
+}
