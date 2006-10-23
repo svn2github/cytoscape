d5535697bec7fe8b6e50dc312db34d9d5ca9be04
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-10-23 20:45:52 +0000

    Plugin now registers PSI-MI plugin with central import handler
    git-svn-id: http://chianti.ucsd.edu/svn@8551 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/PSI-MI/src/org/cytoscape/coreplugin/psi_mi/plugin/PsiMiPlugIn.java
+++ coreplugins/trunk/PSI-MI/src/org/cytoscape/coreplugin/psi_mi/plugin/PsiMiPlugIn.java
@@ -3,6 +3,7 @@ package org.cytoscape.coreplugin.psi_mi.plugin;
 import cytoscape.plugin.CytoscapePlugin;
 import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
+import cytoscape.data.ImportHandler;
 
 import java.util.ArrayList;
 
@@ -15,46 +16,13 @@ import org.cytoscape.coreplugin.psi_mi.cyto_mapper.MapToCytoscape;
 
 public class PsiMiPlugIn extends CytoscapePlugin {
 
-    public PsiMiPlugIn() throws Exception {
-        loadPsi1();
-        loadPsi2();
-    }
+    /**
+     * PSI-MI Plugin
+     */
+    public PsiMiPlugIn() {
 
-    private void loadPsi1() throws Exception {
-        //  First, get some interactions from sample data file.
-        ArrayList interactions = new ArrayList();
-        ContentReader reader = new ContentReader();
-        String xml = reader.retrieveContent("testData/psi_sample1.xml");
-
-        //  Map from PSI One to DataService Interaction Objects.
-        MapPsiOneToInteractions mapper1 = new MapPsiOneToInteractions
-                (xml, interactions);
-        mapper1.doMapping();
-
-        //  Now Map to Cytocape Network Objects.
-        CyNetwork network = Cytoscape.createNetwork("psi_1");
-        MapToCytoscape mapper2 = new MapToCytoscape
-                (interactions, network, MapToCytoscape.MATRIX_VIEW);
-        mapper2.doMapping();
-    }
-
-    private void loadPsi2() throws DataServiceException, MapperException {
-        //  First, get some interactions from sample data file.
-        ArrayList interactions = new ArrayList();
-        ContentReader reader = new ContentReader();
-        String xml = reader.retrieveContent("testData/psi_sample_2_5_2.xml");
-
-        //  Map from PSI to DataService Interaction Objects.
-        MapPsiTwoFiveToInteractions mapper1 = new MapPsiTwoFiveToInteractions
-                (xml, interactions);
-        mapper1.doMapping();
-
-        //  Create CyNetwork
-        CyNetwork network = Cytoscape.createNetwork("psi_2_5");
-
-        //  Now map interactions to cyNetwork.
-        MapToCytoscape mapper2 = new MapToCytoscape
-                (interactions, network, MapToCytoscape.SPOKE_VIEW);
-        mapper2.doMapping();
+        //  Register PsiMiFilter
+        ImportHandler importHandler = new ImportHandler();
+        importHandler.addFilter(new PsiMiFilter());
     }
 }
