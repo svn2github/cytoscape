a991c2ac4cf24673bf30bb1f4ddc16a78a5b3c0d
cmak <cmak@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-10-27 18:35:02 +0000

    Changed to use cache directory for file locations
    git-svn-id: http://chianti.ucsd.edu/svn@8589 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cellcircuits/trunk/meta-analysis/YeastHumanGeneMapper.pm
+++ cellcircuits/trunk/meta-analysis/YeastHumanGeneMapper.pm
@@ -4,40 +4,15 @@ use GOGeneMap;
 
 sub new
 {
-#    my ($caller, $orgs) = @_;
-    my ($caller) = @_;
+    my ($caller, $cacheDir) = @_;
     my $class = ref($caller) || $caller;
     my $self = bless({}, $class);
     
-    #foreach my $org (@{$orgs})
-    #{
-    #	my $key = makeKey($org);
-    #	$self->{$key} = GOGeneMap->new("../db/dump/$key-in-GO-DB.txt");
-    #}
 
-    $self->{human} = GOGeneMap->new("../db/dump/human-genes-in-GO-DB.txt");
-    $self->{yeast} = GOGeneMap->new("../db/dump/yeast-genes-in-GO-DB.txt");
+    $self->{human} = GOGeneMap->new($cacheDir . "/human-genes-in-GO-DB.txt");
+    $self->{yeast} = GOGeneMap->new($cacheDir . "/yeast-genes-in-GO-DB.txt");
 
     return $self;
-}
-
-my %keyCache;
-
-sub makeKey
-{
-    my ($org) = @_;
-    
-    if(exists($keyCache{$org}))
-    {
-	return $keyCache{$org};
-    }
-    else
-    {
-	$org =~ s/\s+/-/g;
-	my $k = lc($org);
-	$keyCache{$org} = $k;
-	return $k;
-    }
 }
 
 sub getHumanMap
@@ -54,7 +29,7 @@ sub getYeastMap
 }
 
 #
-# map human uniprot ids to GO db internal id
+# map human symbol to GO db internal id
 # map yeast ORF to GO db internal id
 #
 sub mapName
@@ -64,13 +39,13 @@ sub mapName
     if($org =~ /homo sapiens/i)
     {
 	my $map = $self->{human};
-	if($map->existsXref($gene))
+	if($map->existsSymbol($gene))
 	{
-	    return $map->xref2go($gene);
+	    return $map->symbol2go($gene);
 	}
-	elsif($gene =~ /(.*)-\d+/ && $map->existsXref($1))
+	elsif($gene =~ /(.*)-\d+/ && $map->existsSymbol($1))
 	{
-	    return $map->xref2go($1);
+	    return $map->symbol2go($1);
 	}
     }
     elsif($org =~ /saccharomyces cerevisiae/i)
