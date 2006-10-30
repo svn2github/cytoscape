75ae040dfefea43de990e980d48a6921c584b227
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-10-30 18:48:30 +0000

    Test cases for text/Excel readers.
    git-svn-id: http://chianti.ucsd.edu/svn@8619 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/tests/cytoscape/data/readers/DefaultAttributeTableReaderTest.java
+++ cytoscape/trunk/tests/cytoscape/data/readers/DefaultAttributeTableReaderTest.java
@@ -1,21 +1,25 @@
 package cytoscape.data.readers;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.TestCase;
 import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
+import cytoscape.data.CyAttributes;
+import static cytoscape.data.readers.TextFileDelimiters.*;
 
 public class DefaultAttributeTableReaderTest extends TestCase {
 
 	private DefaultAttributeTableReader tableReader;
 
-	private static final String DATA_FILE = "testData/goslim_annotation.txt";
-	private static final String NETWORK_FILE = "testData/go_ccNetwork.sif";
-	
+	/*
+	 * Toy example created from galFiltered.sif and its attribute files.
+	 */
+	private static final String DATA_FILE = "testData/annotation/galSubnetworkAnnotation2.txt";
+	private static final String NETWORK_FILE = "testData/galSubnetwork.sif";
+
 	private static final String DATA_FILE2 = "testData/annotation/annotationSampleForYeast.txt";
 	private static final String NETWORK_FILE2 = "testData/galFiltered.sif";
 
@@ -26,57 +30,92 @@ public class DefaultAttributeTableReaderTest extends TestCase {
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
-		
+
 	}
 
-	public void testReadTable() throws IOException {
+	public void testReadTable() throws Exception {
 		File network = new File(NETWORK_FILE);
 		File galNetwork = new File(NETWORK_FILE2);
-
-		CyNetwork net = Cytoscape.createNetworkFromFile(network.getAbsolutePath());
+		//
+		CyNetwork net = Cytoscape.createNetworkFromFile(network
+				.getAbsolutePath());
 		File source = new File(DATA_FILE);
 
-		CyNetwork galNet = Cytoscape.createNetworkFromFile(galNetwork.getAbsolutePath());
-		
-		
-		
+		CyNetwork galNet = Cytoscape.createNetworkFromFile(galNetwork
+				.getAbsolutePath());
+
 		File galSource = new File(DATA_FILE2);
-		
+
+		/*
+		 * Test1
+		 */
 		List<String> delimiters = new ArrayList<String>();
 		delimiters.add(TextFileDelimiters.TAB.toString());
-		tableReader = new DefaultAttributeTableReader(source.toURL(),
-				TextTableReader.ObjectType.NODE, delimiters);
-		tableReader.readTable();
-		
-		List<Integer> aliases = new ArrayList<Integer>();
-		aliases.add(2);
-		String[] cols = {"Object Name in SGD", "key", "ali", "Taxon ID"};
-		
-		tableReader = new DefaultAttributeTableReader(galSource.toURL(),
-				TextTableReader.ObjectType.NODE, delimiters, "\\|", 1, "ID", aliases, cols, null, null);
+
+		List<Integer> aliasList = new ArrayList<Integer>();
+		aliasList.add(2);
+
+		String[] galAttrName = { "ID", "ID in SGD", "Synonyms",
+				"Description of Genes", "Date", "Sample Boolean Attr2",
+				"gal1RGexp", "gal1RGsig", "String List"};
+		byte[] galAttrTypes = { CyAttributes.TYPE_STRING,
+				CyAttributes.TYPE_STRING, CyAttributes.TYPE_STRING,
+				CyAttributes.TYPE_STRING, CyAttributes.TYPE_INTEGER,
+				CyAttributes.TYPE_BOOLEAN, CyAttributes.TYPE_FLOATING,
+				CyAttributes.TYPE_FLOATING, CyAttributes.TYPE_SIMPLE_LIST };
+
+		for (int i = 0; i < galAttrTypes.length; i++) {
+			System.out.println("GAL Data Type " + i + " = " + galAttrTypes[i]);
+		}
+
+		AttributeMappingParameters mapping = new AttributeMappingParameters(
+				TextTableReader.ObjectType.NODE, null, COMMA.toString(), 0, "ID", aliasList,
+				galAttrName, galAttrTypes, null);
+		tableReader = new DefaultAttributeTableReader(source.toURL(), mapping);
 		tableReader.readTable();
 
-		assertEquals("plasma membrane", Cytoscape.getNodeAttributes()
-				.getStringAttribute("GO:0005886", "Ontology Term Name"));
-		assertEquals("protein complex", Cytoscape.getNodeAttributes()
-				.getStringAttribute("GO:0043234", "Ontology Term Name"));
-		assertEquals("goslim_yeast", Cytoscape.getNodeAttributes()
-				.getStringAttribute("GO:0005815", "Subset"));
-		
-		System.out.println("* YOL123W Object Name in SGD = " + Cytoscape.getNodeAttributes()
-				.getStringAttribute("YOL123W", "Object Name in SGD"));
+		assertEquals("ribosomal protein S28A (S33A) (YS27)", Cytoscape
+				.getNodeAttributes().getStringAttribute("YOR167C",
+						"Description of Genes"));
+		assertEquals(new Integer(20010118), Cytoscape.getNodeAttributes()
+				.getIntegerAttribute("YHR141C", "Date"));
+		assertEquals(4, Cytoscape.getNodeAttributes()
+				.getAttributeList("YER112W", "alias").size());
+		assertEquals(7, Cytoscape.getNodeAttributes()
+				.getAttributeList("YDR277C", "String List").size());
+
+		assertEquals("List", Cytoscape.getNodeAttributes()
+				.getAttributeList("YDR277C", "String List").get(5));
+
+		/*
+		 * Test2
+		 */
+
+		List<Integer> aliases = new ArrayList<Integer>();
+		aliases.add(2);
+		String[] cols = { "Object Name in SGD", "key", "ali", "Taxon ID" };
+
+		tableReader = new DefaultAttributeTableReader(galSource.toURL(),
+				TextTableReader.ObjectType.NODE, delimiters, PIPE.toString(), 1, "ID",
+				aliases, cols, null, null);
+		tableReader.readTable();
+
+		System.out.println("* YOL123W Object Name in SGD = "
+				+ Cytoscape.getNodeAttributes().getStringAttribute("YOL123W",
+						"Object Name in SGD"));
 		assertEquals("S000005483", Cytoscape.getNodeAttributes()
 				.getStringAttribute("YOL123W", "Object Name in SGD"));
 		assertEquals("S000006010", Cytoscape.getNodeAttributes()
 				.getStringAttribute("YPL089C", "Object Name in SGD"));
 		assertEquals("taxon:4932", Cytoscape.getNodeAttributes()
 				.getStringAttribute("YDR009W", "Taxon ID"));
-		assertTrue(Cytoscape.getNodeAttributes().getAttributeList("YOR315W", "alias").contains("SFG1"));
-		
-		assertTrue(Cytoscape.getOntologyServer().getNodeAliases().getAliases("YLR319C").contains("AIP3"));
-		
+		assertTrue(Cytoscape.getNodeAttributes().getAttributeList("YOR315W",
+				"alias").contains("SFG1"));
+
+		assertTrue(Cytoscape.getOntologyServer().getNodeAliases().getAliases(
+				"YLR319C").contains("AIP3"));
+
 		Cytoscape.destroyNetwork(galNet);
 		Cytoscape.destroyNetwork(net);
 	}
-
 }
