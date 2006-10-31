7cf07dbdc4caf868f81ec2e2831a027637c0f768
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-10-31 02:14:10 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@8635 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/data/servers/ui/enums/ImportDialogFontTheme.java
+++ cytoscape/trunk/src/cytoscape/data/servers/ui/enums/ImportDialogFontTheme.java
@@ -5,7 +5,8 @@ import java.awt.Font;
 public enum ImportDialogFontTheme {
 	SELECTED_COL_FONT(new Font("Sans-serif", Font.BOLD, 14)),
 	SELECTED_FONT(new Font("Sans-serif", Font.BOLD, 14)),
-	UNSELECTED_FONT(new Font("Sans-serif",Font.PLAIN, 14));
+	UNSELECTED_FONT(new Font("Sans-serif",Font.PLAIN, 14)),
+	KEY_FONT(new Font("Sans-Serif", Font.BOLD, 14));
 	
 	private Font font;
 	
