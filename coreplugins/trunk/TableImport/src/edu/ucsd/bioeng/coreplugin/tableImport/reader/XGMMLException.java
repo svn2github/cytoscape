5a2995ecc982928f915a46ea6c1986eb89096d60
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-11-17 19:29:51 +0000

    Source dir for Table Import core plugin.
    git-svn-id: http://chianti.ucsd.edu/svn@8834 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ coreplugins/trunk/TableImport/src/edu/ucsd/bioeng/coreplugin/tableImport/reader/XGMMLException.java
@@ -0,0 +1,12 @@
+package edu.ucsd.bioeng.coreplugin.tableImport.reader;
+
+/**
+ * 
+ * Exception for XGMML Reader.
+ *
+ */
+public class XGMMLException extends RuntimeException {
+	public XGMMLException(String message) {
+		super(message);
+	}
+}
