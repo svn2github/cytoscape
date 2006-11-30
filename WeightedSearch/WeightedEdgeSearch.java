fac6b8166cd3052f7913e25da8830d5b38d11955
rmkelley <rmkelley@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-11-30 19:20:24 +0000

    Updated randomization procedure to be automatic
    git-svn-id: http://chianti.ucsd.edu/svn@9017 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- WeightedSearch/WeightedEdgeSearch.java
+++ WeightedSearch/WeightedEdgeSearch.java
@@ -17,73 +17,188 @@ public class WeightedEdgeSearch {
 	 */
 
 	protected static double[][] edgeScores;
+
 	protected static Vector<String> idx2Name = new Vector<String>();
+
 	protected static double SIZE_FACTOR = Math.log(0.6);
+
 	protected static final int MAX_SIZE = 15;
+
 	protected static final int MIN_SIZE = 10;
-	protected static final int TRIALS = 100;
-	//protected static final double DEFAULT_FILL = -10;
-	
+
+	protected static final int RANDOM_TRIALS = 100;
+
+
+	// protected static final double DEFAULT_FILL = -10;
+
 	Map<Integer, Double> idx2Expression;
 
 	public static void main(String[] args) {
-		boolean random = true;
-		System.err.println("Version 0.39");
-		System.err.println("Building graph");
-		readData(args[0]);
-		if (random) {
-			initializeOutput();
-			shuffleExpression();
-			createGraph();
-			for(int idx = 0;idx < TRIALS; idx += 1){
-				System.err.println("Trial "+idx);
-				SearchResult bestResult = search();
-				outputScore(bestResult.score);
-				shuffleExpression();
-				createGraph();
-			}
-			
-		} else {
-			createGraph();
-			System.err.println("Finished building graph");
-			System.err.println("Starting search");
 
-			int trials = 20;
-			for (int idx = 0; idx < trials; idx += 1) {
-				System.err.println("Starting iteration " + idx);
-				SearchResult bestResult = search();
-				outputResult(idx, bestResult);
-
-				System.err.println("Resetting edge scores");
-				resetScores(bestResult);
-				System.err.println("Finished resetting edge scores");
+		System.err.println("Version 0.41");
+		Vector<SearchResult> searchResults = new Vector<SearchResult>();
+		readEdgeScores();
+		readNodeScores(args[0]);
+		setupGraph();
+		scoreGraph();
+		double lastScore = 9999999;
+		double cutoff = lastScore - 1;
+		int searchIteration = 0;
+		while (lastScore > cutoff) {
+			SearchResult bestResult = search();
+			searchResults.add(bestResult);
+			outputResult(searchIteration, bestResult);
+			lastScore = bestResult.score;
+			if (bestResult.score < cutoff) {
+				DoubleArrayList scores = new DoubleArrayList(RANDOM_TRIALS);
+				for (int idx = 0; idx < RANDOM_TRIALS; idx += 1) {
+					/*
+					 * read in the node scores
+					 */
+					readNodeScores(args[0]);
+					/*
+					 * eliminate the scores correspond to the previous best
+					 * reslts
+					 */
+					for (int idy = 0; idy < searchResults.size() - 1; idy += 1) {
+						removeScores(searchResults.get(idy));
+					}
+					/*
+					 * randomize the expression
+					 */
+					shuffleExpression();
+					
+					/*
+					 * regenerat the scoring matrix
+					 */
+					scoreGraph();
+					/*
+					 * find the best result and record the score
+					 */
+					SearchResult randomResult = search();
+					scores.add(randomResult.score);
+				}
+				/*
+				 * Figure out hte cutoff value
+				 */
+				scores.sort();
+				outputRandomTrials(searchIteration, scores);
+				cutoff = scores.get((int)(scores.size()*0.95));
+				/*
+				 * read hte node scores again and remove hte last n-1 node
+				 * scores
+				 */
+				readNodeScores(args[0]);
+				for (int idy = 0; idy < searchResults.size() - 1; idy += 1) {
+					removeScores(searchResults.get(idy));
+				}
 			}
+			/*
+			 * remove the node scores for the previous result and regenerate hte
+			 * edge score matrix
+			 */
+			removeScores(searchResults.get(searchResults.size() - 1));
+			scoreGraph();
+			searchIteration += 1;
 		}
-		
-		
+
+//		for (int idx = 0; idx < trials; idx += 1) {
+//			System.err.println("Starting iteration " + idx);
+//			SearchResult bestResult = search();
+//			outputResult(idx, bestResult);
+//
+//			System.err.println("Resetting edge scores");
+//			resetScores(bestResult);
+//			System.err.println("Finished resetting edge scores");
+//		}
+//		if (random) {
+//			initializeOutput();
+//			shuffleExpression();
+//			createGraph();
+//			for (int idx = 0; idx < RANDOM_TRIALS; idx += 1) {
+//				System.err.println("Trial " + idx);
+//				SearchResult bestResult = search();
+//				outputScore(bestResult.score);
+//				shuffleExpression();
+//				createGraph();
+//			}
+//
+//		} else {
+//
+//		}
+
 	}
-	
-	protected static void shuffleExpression(){
+
+	protected static void shuffleExpression() {
 		(new DoubleArrayList(idx2NodeLLR)).shuffle();
 	}
-	
-	protected static double harmonicMean(double x1, double x2){
-		return 2*x1*x2/(x1+x2);
-		//return (x1+x2)/2;
-	}
-	
-	static Map<String, Integer> name2Idx = new HashMap<String, Integer>();
-	static Vector<String> sources = new Vector<String>();
-	static Vector<String> targets = new Vector<String>();
-	static Vector<Double> scores = new Vector<Double>();
-	static double minEdgeScore = Double.POSITIVE_INFINITY;
-	static double [] idx2NodeLLR;
 
-	
-	protected static void readData(String nodeAttributeFile) {
-		idx2Name = new Vector<String>();
+	protected static double harmonicMean(double x1, double x2) {
+		return 2 * x1 * x2 / (x1 + x2);
+		// return (x1+x2)/2;
+	}
+
+	static Map<String, Integer> name2Idx = new HashMap<String, Integer>();
+
+	static Vector<String> sources = new Vector<String>();
+
+	static Vector<String> targets = new Vector<String>();
+
+	static Vector<Double> scores = new Vector<Double>();
+
+	static double minEdgeScore = Double.POSITIVE_INFINITY;
+
+	static double[] idx2NodeLLR;
+
+	protected static void readNodeScores(String nodeAttributeFile) {
+		/*
+		 * Read in the llr scores for the nodes
+		 */
+		idx2NodeLLR = new double[name2Idx.size()];
+		System.err.println("Reading in node scores");
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(
+					nodeAttributeFile));
+			String line = reader.readLine();
+			Vector<String> names = new Vector<String>();
+			Vector<Double> LLRs = new Vector<Double>();
+			double LLRsum = 0;
+			while (line != null) {
+				String[] splat = line.split("\t");
+				names.add(splat[0]);
+				Double LLR = new Double(splat[1]);
+				LLRs.add(LLR);
+				LLRsum += LLR.doubleValue();
+				line = reader.readLine();
+			}
+			reader.close();
+			double LLRaverage = LLRsum / names.size();
+			/*
+			 * For those nodes in the graph that we don't have expression data
+			 * for, just use the expected value
+			 */
+			Arrays.fill(idx2NodeLLR, LLRaverage);
+
+			for (int idx = 0; idx < names.size(); idx += 1) {
+				String name = names.get(idx);
+				Double LLR = LLRs.get(idx);
+				if (name2Idx.containsKey(name)) {
+					int nodeIndex = name2Idx.get(name);
+					idx2NodeLLR[nodeIndex] = LLR.doubleValue();
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Error when reading in node scores");
+			System.err.println(e);
+			System.exit(-1);
+		}
+		System.err.println("Finished reading in node scores");
+	}
+
+	protected static void readEdgeScores() {
+	idx2Name = new Vector<String>();
 		name2Idx = new HashMap<String, Integer>();
-		sources = new Vector<String>();
+sources = new Vector<String>();
 		targets = new Vector<String>();
 		scores = new Vector<Double>();
 
@@ -129,51 +244,10 @@ public class WeightedEdgeSearch {
 			e.printStackTrace();
 			System.exit(-1);
 		}
+	}
 
+public static void setupGraph(){
 		System.err.println("Min edge score is " + minEdgeScore);
-		/*
-		 * Read in the llr scores for the nodes
-		 */
-		idx2NodeLLR = new double[name2Idx.size()];
-		System.err.println("Reading in node scores");
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(
-					nodeAttributeFile));
-			String line = reader.readLine();
-			Vector<String> names = new Vector<String>();
-			Vector<Double> LLRs = new Vector<Double>();
-			double LLRsum = 0;
-			while (line != null) {
-				String[] splat = line.split("\t");
-				names.add(splat[0]);
-				Double LLR = new Double(splat[1]);
-				LLRs.add(LLR);
-				LLRsum += LLR.doubleValue();
-				line = reader.readLine();
-			}
-			reader.close();
-			double LLRaverage = LLRsum / names.size();
-			/*
-			 * For those nodes in the graph that we don't have expression data
-			 * for, just use the expected value
-			 */
-			Arrays.fill(idx2NodeLLR, LLRaverage);
-
-			for (int idx = 0; idx < names.size(); idx += 1) {
-				String name = names.get(idx);
-				Double LLR = LLRs.get(idx);
-				if (name2Idx.containsKey(name)) {
-					int nodeIndex = name2Idx.get(name);
-					idx2NodeLLR[nodeIndex] = LLR.doubleValue();
-				}
-			}
-		} catch (Exception e) {
-			System.err.println("Error when reading in node scores");
-			System.err.println(e);
-			System.exit(-1);
-		}
-		System.err.println("Finished reading in node scores");
-
 		/*
 		 * Build the edge scores data structure
 		 */
@@ -191,7 +265,8 @@ public class WeightedEdgeSearch {
 		System.err.println("Finished building edge array data structure");
 	}
 
-	protected static void createGraph() {
+
+	protected static void scoreGraph() {
 		System.err.println("Writing scores into data structure");
 		System.err.println("Filling background scores");
 		try {
@@ -233,49 +308,50 @@ public class WeightedEdgeSearch {
 
 	}
 
-	protected static void resetScores(SearchResult result){
+	protected static void removeScores(SearchResult result) {
 		Vector<Integer> members = new Vector<Integer>(result.members);
-		for(int idx = 0;idx < members.size() - 1; idx +=1 ){
+		for (int idx = 0; idx < members.size(); idx += 1) {
+			int member = members.get(idx);
+			idx2NodeLLR[member] = Double.NEGATIVE_INFINITY;
+		}
+	}
+
+	protected static void resetScores(SearchResult result) {
+		Vector<Integer> members = new Vector<Integer>(result.members);
+		for (int idx = 0; idx < members.size() - 1; idx += 1) {
 			int source = members.get(idx);
-			for(int idy = idx + 1; idy < members.size() ; idy += 1){
+			for (int idy = idx + 1; idy < members.size(); idy += 1) {
 				int target = members.get(idy);
-				if(source > target){
+				if (source > target) {
 					edgeScores[source][target] = Double.NEGATIVE_INFINITY;
-				}
-				else{
+				} else {
 					edgeScores[target][source] = Double.NEGATIVE_INFINITY;
 				}
 			}
 		}
 	}
-	
-	protected static void initializeOutput(){
+
+
+	protected static void outputRandomTrials(int id, DoubleArrayList scores) {
 		try {
-			FileWriter fw = new FileWriter("random.scores");
-			fw.close();
-		}catch(Exception e){
-			e.printStackTrace();
-			System.exit(-1);
-		}
-	}
-	protected static void outputScore(double score){
-		try {
-			FileWriter fw = new FileWriter("random.scores",true);
-			fw.write(""+score+"\n");
+			FileWriter fw = new FileWriter(""+id+".random");
+			for(int idx = 0;idx < scores.size(); idx += 1){
+				fw.write(""+scores.get(idx)+"\n");
+			}
 			fw.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		
+
 	}
-	
+
 	protected static void outputResult(int id, SearchResult result) {
 		try {
 			FileWriter fw = new FileWriter("" + id + ".out");
-			fw.write("Score:\t"+result.score+"\n");
-			fw.write("Size Factor:\t"+SIZE_FACTOR+"\n");
-			fw.write("Size:\t"+result.members.size()+"\n");                                                          
+			fw.write("Score:\t" + result.score + "\n");
+			fw.write("Size Factor:\t" + SIZE_FACTOR + "\n");
+			fw.write("Size:\t" + result.members.size() + "\n");
 			for (Integer member : result.members) {
 				fw.write(idx2Name.get(member) + "\n");
 			}
@@ -285,18 +361,19 @@ public class WeightedEdgeSearch {
 			System.exit(-1);
 		}
 	}
-	
-	protected static SearchResult search(){
+
+	protected static SearchResult search() {
 		SearchResult bestResult = null;
-		for(int seed = 0;seed < edgeScores.length; seed += 1){
-			//System.err.println("Searching from seed "+seed+", best so far: size ="+bestResult.members.size());
+		for (int seed = 0; seed < edgeScores.length; seed += 1) {
+			// System.err.println("Searching from seed "+seed+", best so far:
+			// size ="+bestResult.members.size());
 			SearchResult currentResult = new SearchResult();
 			currentResult.score = 0;
 			currentResult.sum = 0;
-			double [] neighbors = new double[edgeScores.length];
+			double[] neighbors = new double[edgeScores.length];
 			int bestNeighbor = seed;
 			double newScore = 0;
-			do{
+			do {
 				currentResult.score = newScore;
 				currentResult.sum += neighbors[bestNeighbor];
 				/*
@@ -304,41 +381,44 @@ public class WeightedEdgeSearch {
 				 */
 				currentResult.members.add(bestNeighbor);
 				neighbors[bestNeighbor] = Double.NEGATIVE_INFINITY;
-				
+
 				/*
 				 * Update the neighbors array
 				 */
-				for(int neighbor=0;neighbor < neighbors.length;neighbor += 1){
-					if(neighbor == bestNeighbor){
+				for (int neighbor = 0; neighbor < neighbors.length; neighbor += 1) {
+					if (neighbor == bestNeighbor) {
 						continue;
 					}
-					if(neighbor > bestNeighbor){
+					if (neighbor > bestNeighbor) {
 						neighbors[neighbor] += edgeScores[neighbor][bestNeighbor];
-					}
-					else{
+					} else {
 						neighbors[neighbor] += edgeScores[bestNeighbor][neighbor];
 					}
 				}
-				
+
 				bestNeighbor = maxEntry(neighbors);
-				//int size = currentResult.members.size()+1;
-				//newScore = (currentResult.sum + neighbors[bestNeighbor])/Math.pow(size*(size-1)/2,0.75);
-				//newScore = currentResult.sum + neighbors[bestNeighbor];
+				// int size = currentResult.members.size()+1;
+				// newScore = (currentResult.sum +
+				// neighbors[bestNeighbor])/Math.pow(size*(size-1)/2,0.75);
+				// newScore = currentResult.sum + neighbors[bestNeighbor];
 				newScore = (currentResult.sum + neighbors[bestNeighbor]);
-		//	}while(currentResult.members.size() < MIN_SIZE || ( currentResult.members.size() < MAX_SIZE && newScore > currentResult.score));
-			}while( currentResult.members.size() < MAX_SIZE && newScore > currentResult.score);
-					
-			if(bestResult == null || currentResult.score > bestResult.score){
+				// }while(currentResult.members.size() < MIN_SIZE || (
+				// currentResult.members.size() < MAX_SIZE && newScore >
+				// currentResult.score));
+			} while (currentResult.members.size() < MAX_SIZE
+					&& newScore > currentResult.score);
+
+			if (bestResult == null || currentResult.score > bestResult.score) {
 				bestResult = currentResult;
 			}
 		}
 		return bestResult;
 	}
 
-	protected static int maxEntry(double [] array){
+	protected static int maxEntry(double[] array) {
 		int result = 0;
-		for(int idx = 0 ;idx < array.length ; idx += 1){
-			if(array[idx] > array[result]){
+		for (int idx = 0; idx < array.length; idx += 1) {
+			if (array[idx] > array[result]) {
 				result = idx;
 			}
 		}
@@ -346,16 +426,17 @@ public class WeightedEdgeSearch {
 	}
 }
 
-
-class SearchResult{
-	public SearchResult(){
+class SearchResult {
+	public SearchResult() {
 		score = 0;
 		sum = 0;
-		
+
 		members = new HashSet<Integer>();
 	}
 
 	public double score;
+
 	public double sum;
+
 	public Set<Integer> members;
 }
