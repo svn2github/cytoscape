5fd152a1e6eea48a2424d9ede137c37561de481a
mcreech <mcreech@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-12-15 18:23:48 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@9179 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/CytoscapeEditor2/src/cytoscape/editor/editors/SimpleBioPAXEditor.java
+++
@@ -1,6 +0,0 @@
-package cytoscape.editor.editors;
-
-// AJK: 12/09/06 placeholder for driving BioPAX editing via its visual style
-public class SimpleBioPAXEditor extends DefaultCytoscapeEditor {
-
-}
