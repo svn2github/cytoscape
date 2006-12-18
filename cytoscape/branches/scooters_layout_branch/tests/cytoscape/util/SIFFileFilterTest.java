f4f0d945f6eb25a88e9b3e02977ad5984a0f63b3
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-12-18 19:06:42 +0000

    Layout branch
    git-svn-id: http://chianti.ucsd.edu/svn@9199 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/branches/scooters_layout_branch/tests/cytoscape/util/SIFFileFilterTest.java
@@ -0,0 +1,34 @@
+package cytoscape.util;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+import cytoscape.data.readers.GraphReader;
+import cytoscape.data.readers.InteractionsReader;
+import cytoscape.util.SIFFileFilter;
+
+/**
+ * Tests Implementation of CyFileFilter.
+ * 
+ * TODO: Add to DataSuite
+ */
+public class SIFFileFilterTest extends TestCase {
+	SIFFileFilter test;
+	File DUMMY_SIF_FILE;
+	GraphReader reader;
+
+	public void setUp() throws Exception {
+		test = new SIFFileFilter();
+		DUMMY_SIF_FILE = File.createTempFile("inputSifTest", ".sif");
+	}
+
+	public void tearDown() throws Exception {
+		test = null;
+		DUMMY_SIF_FILE.delete();
+	}
+
+	public void testGetReader() throws Exception {
+		reader = test.getReader(DUMMY_SIF_FILE.toString());
+		assertEquals(reader.getClass(), InteractionsReader.class);
+	}
+}
