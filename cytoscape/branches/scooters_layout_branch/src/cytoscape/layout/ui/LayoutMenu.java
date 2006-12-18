902280346e14302518fe49dcd90e01f34b65afe1
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-12-18 23:04:24 +0000

    Landing layout changes
    git-svn-id: http://chianti.ucsd.edu/svn@9211 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/branches/scooters_layout_branch/src/cytoscape/layout/ui/LayoutMenu.java
@@ -0,0 +1,108 @@
+/*
+  File: LayoutAlgorithm.java 
+  
+  Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+  
+  The Cytoscape Consortium is: 
+  - Institute for Systems Biology
+  - University of California San Diego
+  - Memorial Sloan-Kettering Cancer Center
+  - Institut Pasteur
+  - Agilent Technologies
+  
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+  
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute 
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute 
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute 
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+package cytoscape.layout.ui;
+
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.event.MenuListener;
+import javax.swing.event.MenuEvent;
+import javax.swing.JMenu;
+
+import cytoscape.CyNetwork;
+import cytoscape.Cytoscape;
+import cytoscape.data.CyAttributes;
+import cytoscape.layout.LayoutAlgorithm;
+
+/**
+ * 
+ * A DynamicLayoutMenu is a more complicated layout menu that constructs layout menu
+ * items on-the-fly based on the capabilities of the layout algorithm and environment
+ * factors such as whether or not nodes are selected, the presence of node or edge
+ * attributes, etc.
+ */
+public class LayoutMenu extends JMenu implements MenuListener {
+	ArrayList<LayoutAlgorithm> subMenuList;
+
+	public LayoutMenu(String menuName) {
+		super(menuName);
+		addMenuListener(this);
+		subMenuList = new ArrayList();
+	}
+
+	public void add(LayoutAlgorithm layout) {
+		subMenuList.add(layout);
+	}
+
+	public void remove(LayoutAlgorithm layout) {
+		subMenuList.remove(layout);
+	}
+
+	public int getItemCount() {
+		return subMenuList.size();
+	}
+
+	public void menuCanceled (MenuEvent e) {};
+	public void menuDeselected (MenuEvent e) {};
+	public void menuSelected (MenuEvent e)
+	{
+		// Clear any previous entries
+		this.removeAll();
+
+		// Figure out if we have anything selected
+		CyNetwork network = Cytoscape.getCurrentNetwork();
+		Set selectedNodes = network.getSelectedNodes();
+
+		// Now, add each layout, as appropriate
+		Iterator iter = subMenuList.iterator();
+		while (iter.hasNext()) {
+			LayoutAlgorithm layout = (LayoutAlgorithm)iter.next();
+			// Make sure we don't have any lingering locked nodes
+			layout.unlockAllNodes();
+			if (layout.supportsNodeAttributes() != null ||
+					layout.supportsEdgeAttributes() != null ) {
+				super.add(new DynamicLayoutMenu(layout));
+			} else if (layout.supportsSelectedOnly() &&
+					selectedNodes.size() > 0) {
+				super.add(new DynamicLayoutMenu(layout));
+			} else {
+				super.add(new StaticLayoutMenu(layout));
+			}
+		}
+	}
+}
