ce920abcb0f4b4646392751e53c07bb03a30fd74
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2006-12-21 20:37:21 +0000

    Bug fix: [Mantis:  Cytoscape 2.x Bugs 0000870]: Problem selecting filter when creating new Node Topology Filter
    git-svn-id: http://chianti.ucsd.edu/svn@9252 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/RFilters/src/RFilters/cytoscape/NodeTopologyFilterEditor.java
+++ coreplugins/trunk/RFilters/src/RFilters/cytoscape/NodeTopologyFilterEditor.java
@@ -167,6 +167,11 @@ public class NodeTopologyFilterEditor
 
   }
 
+  public void resetFilterBoxModel()
+  {
+	    filterBox.setModel(FilterManager.defaultManager().getComboBoxModel());	  
+  }
+  
   //----------------------------------------//
   // Implements Filter Editor
   //----------------------------------------//
