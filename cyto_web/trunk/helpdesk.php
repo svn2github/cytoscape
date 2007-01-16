d0f6082c5e75849d236a79b0f6757883cdc97baf
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-01-16 19:34:02 +0000

    Original creation
    git-svn-id: http://chianti.ucsd.edu/svn@9397 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/helpdesk.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+		<title>
+			 Cytoscape Help Desk
+		</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="css/cytoscape.css" />
+		<link rel="shortcut icon" href="images/cyto.ico" />
+	</head>
+	<body bgcolor="#FFFFFF">
+		<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
+			<tr>
+				<td width="10">
+					&nbsp;
+				</td>
+				<td valign="center">
+					<h1>
+						Cytoscape Help Desk
+					</h1>
+				</td>
+			</tr>
+		</table>
+<? include "nav.php"; ?>
+<div  id="indent">
+			<P>
+
+			<UL>
+		    <LI>
+					<A HREF="faq.php">Frequent asked questions</A>
+		    <LI>
+		    <LI>
+					<A HREF="http://groups.google.com/group/cytoscape-helpdesk">Ask a question at Cytoscape help desk Google group</A>
+		    <LI>
+			</UL>
+
+			</div>
+<? include "footer.php"; ?>
+	</body>
+</html>
