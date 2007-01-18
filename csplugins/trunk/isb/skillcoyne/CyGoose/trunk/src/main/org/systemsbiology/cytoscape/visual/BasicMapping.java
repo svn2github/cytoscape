c1586473a29e61a9dc65d5853c9d997053481001
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-01-18 19:53:43 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@9431 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/isb/skillcoyne/CyGoose/trunk/src/main/org/systemsbiology/cytoscape/visual/BasicMapping.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.systemsbiology.cytoscape.visual;
+
+import cytoscape.visual.mappings.*;
+
+/**
+ * @author skillcoy
+ * Class to create a seed of node mappings specifically for movies (handleHashMap()) so something can be
+ * shown and the user can customize later.
+ */
+public class BasicMapping
+	{
+	
+	public static ObjectMapping getNewMap(String AttributeName)
+		{
+		
+		return null;
+		}
+
+	}
