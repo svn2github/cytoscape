077e9d8573a5e30983141efcd63588ceb6c22b6a
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-01-18 19:52:39 +0000

    Removing dead geese handled, changed naming scheme
    git-svn-id: http://chianti.ucsd.edu/svn@9430 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/isb/skillcoyne/CyGoose/trunk/src/main/org/systemsbiology/cytoscape/GagglePlugin.java
+++ csplugins/trunk/isb/skillcoyne/CyGoose/trunk/src/main/org/systemsbiology/cytoscape/GagglePlugin.java
@@ -5,22 +5,29 @@ package org.systemsbiology.cytoscape;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.rmi.Naming;
 import java.rmi.server.UnicastRemoteObject;
 
-import org.systemsbiology.cytoscape.*;
+import org.systemsbiology.cytoscape.CyGoose;
+import org.systemsbiology.cytoscape.dialog.GooseDialog;
 import org.systemsbiology.gaggle.boss.Boss;
 import org.systemsbiology.gaggle.geese.Goose;
 import org.systemsbiology.gaggle.util.MiscUtil;
 
 import javax.swing.JOptionPane;
-import java.util.*;
+
+import java.util.Set;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.ArrayList;
 
 import cytoscape.Cytoscape;
 import cytoscape.CyNetwork;
-import cytoscape.CyNetworkEvent;
-import cytoscape.CyNetworkListener;
 import cytoscape.plugin.CytoscapePlugin;
+import cytoscape.CytoscapeVersion;
 
 /**
  * @author skillcoy
@@ -31,24 +38,23 @@ public class GagglePlugin extends CytoscapePlugin
 	private static Boss GaggleBoss;
 	private static GooseDialog Dialog;
 	private static boolean Initialized = false;
-	protected static String myGaggleName = "Cytoscape2.4";
-
-	// keeps track of all the network ids 
-	// key = network id  value = goose name
-	private static HashMap<String, String> NetworkGeese;
-
+	protected static String myGaggleName = "Cytoscape";
+	// keeps track of all the network ids key = network id  value = goose 
+	private static HashMap<String, Goose> NetworkGeese;
+	
 	private static void print(String S) { System.out.println(S); }
 
 	public GagglePlugin()
 		{
 		// constructor gets called at load time and every time the toolbar is used
 		if (Initialized) { return; }
-		print("**GagglePlugin constructor");
 		
-		NetworkGeese = new HashMap<String, String>();
+		myGaggleName += " v." + CytoscapeVersion.version;
+		
+		NetworkGeese = new HashMap<String, Goose>();
 		Dialog = new GooseDialog();
-		// initial connection - this may not be the desired behaivior as this "goose" may not do anything
 		GaggleBoss = this.rmiConnect();
+		this.updateNetworkGeese();
 		if (GaggleBoss == null) 
 			{ 
 			JOptionPane.showMessageDialog(Cytoscape.getDesktop(), "Failed to connect to the Boss", "Error", JOptionPane.ERROR_MESSAGE); 
@@ -61,8 +67,9 @@ public class GagglePlugin extends CytoscapePlugin
 			this.updateAction();
 			// this gives an initial goose that is cytoscape with a null network
 			CyNetwork CurrentNet = Cytoscape.getNullNetwork();
-			this.createNewGoose(CurrentNet);
-			NetworkGeese.put(CurrentNet.getIdentifier(), this.createGooseName(CurrentNet));
+			CurrentNet.setTitle(myGaggleName);
+			Goose NewGoose = this.createNewGoose(CurrentNet);
+			NetworkGeese.put(CurrentNet.getIdentifier(), NewGoose);
 			}
 		Initialized = true;
 		}
@@ -72,10 +79,8 @@ public class GagglePlugin extends CytoscapePlugin
 	 */
 	private void registerGoose(Goose G)
 		{
-		try
-			{
-			UnicastRemoteObject.exportObject(G, 0);
-			}
+		String RegisteredName = null;
+		try { UnicastRemoteObject.exportObject(G, 0); }
 		catch (Exception e)
 			{
 			e.printStackTrace();
@@ -85,7 +90,8 @@ public class GagglePlugin extends CytoscapePlugin
 		
 		try
 			{
-			GaggleBoss.register(G);
+			RegisteredName = GaggleBoss.register(G);
+			G.setName(RegisteredName);
 			Dialog.registerButton.setEnabled(false); 
 			}
 		catch (Exception E)
@@ -106,14 +112,8 @@ public class GagglePlugin extends CytoscapePlugin
 		String hostname = "localhost";
 		String uri = "rmi://" + hostname + "/" + serviceName;
 		Boss GB = null;
-		try 
-			{ 
-			GB = (Boss) Naming.lookup(uri); 
-			}
-		catch (Exception E)
-			{
-			E.printStackTrace();			
-			}
+		try { GB = (Boss) Naming.lookup(uri); }
+		catch (Exception E) { E.printStackTrace(); }
 		return GB;
 		}
 	
@@ -141,52 +141,52 @@ public class GagglePlugin extends CytoscapePlugin
 	/*
 	 * Creates a new goose for the given network
 	 */
-	private void createNewGoose(CyNetwork Network)
+	private Goose createNewGoose(CyNetwork Network)
 		{
 		CyGoose Goose = new CyGoose(Dialog, GaggleBoss);
 		Goose.setNetworkId(Network.getIdentifier());
 		Goose.setName( this.createGooseName(Network) );
 		this.registerGoose(Goose);
-		//Dialog.gooseChooser.addItem(Goose.getName());
-		MiscUtil.updateGooseChooser(GaggleBoss, Dialog.gooseChooser, Goose.getName(), null);
+		return Goose;
 		}
 
 	/*
 	 * Creates a standard goose name for any network
 	 */
 	private String createGooseName(String Id, String Title)
-		{ return (myGaggleName + ": " + Title + "(" + Id +")"); }
-	
+		{ return (Title + "(" + Id +")"); }
 	private String createGooseName(CyNetwork Network)
 		{ return createGooseName(Network.getIdentifier(), Network.getTitle()); }
 	
-	
-	// TODO: remove closed networks from the boss
+	/*
+	 * Updates the list of network geese in the boss by adding new networks as gees
+	 * and removing destroyed networks.
+	 * TODO: If 2 sessions of cytoscape were open and one gets closed it's geese do not go away WHY?
+	 */
 	private void updateNetworkGeese()
 		{
 		Iterator<CyNetwork> NetIter = Cytoscape.getNetworkSet().iterator();
 		Set<String> CurrentNetIds = new HashSet<String>();
-		// this SHOULD be adding each network as a potential "goose" in the list
+
+		// this should be adding each network as a potential "goose" in the list
 		while (NetIter.hasNext())
 			{
 			CyNetwork CurrentNet = NetIter.next();
 			CurrentNetIds.add(CurrentNet.getIdentifier());
-			print(CurrentNet.getIdentifier());
 
 			// if the network is already in the hash it should be in the list of geese
 			if (NetworkGeese.containsKey(CurrentNet.getIdentifier())) continue;
 			else
 				{
-				NetworkGeese.put(CurrentNet.getIdentifier(), this.createGooseName(CurrentNet));
-				this.createNewGoose(CurrentNet);
+				Goose NewGoose = this.createNewGoose(CurrentNet);
+				NetworkGeese.put(CurrentNet.getIdentifier(), NewGoose);
 				}
 			}
 		this.removeOldNetworks(CurrentNetIds);
+		MiscUtil.updateGooseChooser(GaggleBoss, Dialog.gooseChooser, null, null);
 		}
 
-	
-	
-	// this needs to remove networks from the boss that have been destroyed
+	// TODO Correct networks are slated for removal, but it isn't occurringx
 	private void removeOldNetworks(Set<String> AllCurrentNetIds)
 		{
 		print("removeOldNetworks");
@@ -196,7 +196,8 @@ public class GagglePlugin extends CytoscapePlugin
 		while (NetGeeseIter.hasNext())
 			{
 			String Id = NetGeeseIter.next();
-			if (AllCurrentNetIds.contains(Id)) continue; 
+			// null network = 0, should never try to remove it
+			if ( AllCurrentNetIds.contains(Id) || Id.equals("0")) continue; 
 			else DeadIds.add(Id); 
 			}
 
@@ -204,20 +205,20 @@ public class GagglePlugin extends CytoscapePlugin
 		while(deadIter.hasNext())
 			{ 
 			String Id = deadIter.next();
-			print("Removeing "+Id+" from the boss");
-			NetworkGeese.remove(Id); 
-			try { GaggleBoss.remove( NetworkGeese.get(Id)); }
+			try 
+				{ 
+				print("Removeing " + NetworkGeese.get(Id).getName() + " from the boss");
+				GaggleBoss.remove( NetworkGeese.get(Id).getName() ); 
+				UnicastRemoteObject.unexportObject(NetworkGeese.get(Id), true);
+				Dialog.gooseChooser.removeItem( NetworkGeese.get(Id).getName() );
+				}
 			catch (Exception E) { E.printStackTrace(); }
-			Dialog.gooseChooser.removeItem( NetworkGeese.get(Id) );
-			MiscUtil.updateGooseChooser(GaggleBoss, Dialog.gooseChooser, null, null);
+			NetworkGeese.remove(Id); 
 			}
 		}
 
 
 	public static void showDialogBox(String message, String title, int msgType)
-		{
-		JOptionPane.showMessageDialog(Cytoscape.getDesktop(), message, title,
-				msgType);
-		}
+		{ JOptionPane.showMessageDialog(Cytoscape.getDesktop(), message, title, msgType); }
 
 	}
