ecf4752eca96333bb1f30d1bfecc70e0681e1aac
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-02-06 01:16:42 +0000

    Added new field and method.
    git-svn-id: http://chianti.ucsd.edu/svn@9505 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/visual/calculators/CalculatorTypes.java
+++ cytoscape/trunk/src/cytoscape/visual/calculators/CalculatorTypes.java
@@ -10,41 +10,71 @@ package cytoscape.visual.calculators;
  * 
  */
 public enum CalculatorTypes {
-	NODE_COLOR("Node Color"), NODE_BORDER_COLOR("Node Border Color"), NODE_LINETYPE(
-			"Node Line Type"), NODE_SHAPE("Node Shape"), NODE_WIDTH(
-			"Node Width"), NODE_HEIGHT("Node Height"), NODE_LABEL("Node Label"), NODE_LABEL_FONT_FACE(
-			"Node Label Font Face"), NODE_LABEL_FONT_SIZE(
-			"Node Label Font Size"), NODE_LABEL_COLOR("Node Label Color"), NODE_TOOLTIP(
-			"Node Tooltip"), NODE_LABEL_POSITION("Node Label Position"),
+	NODE_COLOR("Node Color", "nodeColorCalculator"), NODE_BORDER_COLOR(
+			"Node Border Color", "nodeBorderColorCalculator"), NODE_LINETYPE(
+			"Node Line Type", "nodeLineTypeCalculator"), NODE_SHAPE(
+			"Node Shape", "nodeShapeCalculator"), NODE_WIDTH("Node Width",
+			"nodeWidthCalculator"), NODE_HEIGHT("Node Height",
+			"nodeHeightCalculator"), NODE_LABEL("Node Label",
+			"nodeLabelCalculator"), NODE_FONT_FACE("Node Font Face",
+			"nodeFontFaceCalculator"), NODE_FONT_SIZE("Node Font Size",
+			"nodeFontSizeCalculator"), NODE_LABEL_COLOR("Node Label Color",
+			"nodeLabelColor"), NODE_TOOLTIP("Node Tooltip",
+			"nodeTooltipCalculator"), NODE_LABEL_POSITION(
+			"Node Label Position", "nodeLabelPositionCalculator"),
 
-	EDGE_COLOR("Edge Color"), EDGE_LINETYPE("Edge Line Type"), EDGE_SRCARROW(
-			"Edge Source Arrow"), EDGE_TGTARROW("Edge Target Arrow"), EDGE_LABEL(
-			"Edge Label"), EDGE_LABEL_FONT_FACE("Edge Label Font Face"), EDGE_LABEL_FONT_SIZE(
-			"Edge Label Font Size"), EDGE_LABEL_COLOR("Edge Label Color"), EDGE_TOOLTIP(
-			"Edge Tooltip"), EDGE_LABEL_POSITION("Edge Label Position");
+	EDGE_COLOR("Edge Color", "edgeColorCalculator"), EDGE_LINETYPE(
+			"Edge Line Type", "edgeLineTypeCalculator"), EDGE_SRCARROW(
+			"Edge Source Arrow", "edgeSourceArrowCalculator"), EDGE_TGTARROW(
+			"Edge Target Arrow", "edgeTargetArrowCalculator"), EDGE_LABEL(
+			"Edge Label", "edgeLabelCalculator"), EDGE_FONT_FACE(
+			"Edge Font Face", "edgeFontFaceCalculator"), EDGE_FONT_SIZE(
+			"Edge Font Size", "edgeFontSizeCalculator"), EDGE_LABEL_COLOR(
+			"Edge Label Color", "edgeLabelColorCalculator"), EDGE_TOOLTIP(
+			"Edge Tooltip", "edgeTooltipCalculator"), EDGE_LABEL_POSITION(
+			"Edge Label Position", "edgeLabelPositionCalculator");
 
-	
 	/*
 	 * String returned by toString() method.
 	 */
 	private final String calcName;
 
 	/*
+	 * Property label in prop file.
+	 */
+	private String propertyLabel;
+
+	/*
 	 * private constructor to put name into this enum.
 	 */
-	private CalculatorTypes(final String typeName) {
-		this.calcName = typeName;
+	private CalculatorTypes(final String calcName, final String propertyLabel) {
+		this.calcName = calcName;
+		this.propertyLabel = propertyLabel;
 	}
 
+	/**
+	 * Returns name of calculator.
+	 */
 	public String toString() {
 		return calcName;
 	}
+	
+	/**
+	 * Returns string used as property label in VS prop file.
+	 * @return
+	 */
+	public String getPropertyLabel() {
+		return propertyLabel;
+	}
 
+	/**
+	 * Returns number of available calculators
+	 * @return
+	 */
 	public int totalCalululatorsCount() {
 		return values().length;
 	}
 
-	
 	/**
 	 * Return position in thie enum as byte.<br>
 	 * Will be used as type.
