e387ee3370fe36b26adc2e48152420d6e94ed85f
apico <apico@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-02-08 01:21:13 +0000

    quick fix to QuickFind conflict
    git-svn-id: http://chianti.ucsd.edu/svn@9525 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/agilent/kuchinsky/BubbleRouter/src/csplugins/brquickfind/util/QuickFindFactory.java
@@ -0,0 +1,25 @@
+package csplugins.brquickfind.util;
+
+import cytoscape.Cytoscape;
+
+
+/**
+ * Factory for Creating QuickFind Classes.
+ *
+ * @author Ethan Cerami.
+ */
+public class QuickFindFactory {
+    private static QuickFind quickFind;
+
+    /**
+     * Gets instance of Global QuickFind Singleton.
+     *
+     * @return Global QuickFind Class.
+     */
+    public static QuickFind getGlobalQuickFindInstance() {
+        if (quickFind == null) {
+            quickFind = new QuickFindImpl(Cytoscape.getNodeAttributes());
+        }
+        return quickFind;
+    }
+}
