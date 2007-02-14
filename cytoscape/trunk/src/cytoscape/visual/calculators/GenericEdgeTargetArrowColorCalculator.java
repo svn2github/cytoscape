dd8cedbb2c30bb8a4050715169a8001baf3b8f41
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-02-14 16:47:29 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@9581 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/visual/calculators/GenericEdgeTargetArrowColorCalculator.java
+++ cytoscape/trunk/src/cytoscape/visual/calculators/GenericEdgeTargetArrowColorCalculator.java
@@ -1,6 +1,6 @@
 package cytoscape.visual.calculators;
 
-import static cytoscape.visual.VisualPropertyType.EDGE_TGTARROW_COLOR;
+//import static cytoscape.visual.VisualPropertyType.EDGE_TGTARROW_COLOR;
 import giny.model.Edge;
 
 import java.util.Properties;
@@ -11,32 +11,32 @@ import cytoscape.visual.EdgeAppearance;
 import cytoscape.visual.mappings.ObjectMapping;
 import cytoscape.visual.parsers.ArrowParser;
 
-public class GenericEdgeTargetArrowColorCalculator extends EdgeCalculator {
+public class GenericEdgeTargetArrowColorCalculator {
 
-	public GenericEdgeTargetArrowColorCalculator(String name, ObjectMapping m) {
-		super(name, m, Arrow.class, EDGE_TGTARROW_COLOR);
-	}
-
-	public GenericEdgeTargetArrowColorCalculator(String name, Properties props,
-			String baseKey) {
-		super(name, props, baseKey, new ArrowParser(), Arrow.NONE,
-				EDGE_TGTARROW_COLOR);
-	}
-
-	public void apply(EdgeAppearance appr, Edge edge, CyNetwork network) {
-		Arrow a = (Arrow) getRangeValue(edge);
-
-		// default has already been set - no need to do anything
-		if (a == null)
-			return;
-
-		appr.setTargetArrow(a);
-	}
-
-	public Arrow calculateEdgeArrow(Edge e, CyNetwork n) {
-		final EdgeAppearance ea = new EdgeAppearance();
-		apply(ea, e, n);
-
-		return ea.getTargetArrow();
-	}
+//	public GenericEdgeTargetArrowColorCalculator(String name, ObjectMapping m) {
+//		super(name, m, Arrow.class, EDGE_TGTARROW_COLOR);
+//	}
+//
+//	public GenericEdgeTargetArrowColorCalculator(String name, Properties props,
+//			String baseKey) {
+//		super(name, props, baseKey, new ArrowParser(), Arrow.NONE,
+//				EDGE_TGTARROW_COLOR);
+//	}
+//
+//	public void apply(EdgeAppearance appr, Edge edge, CyNetwork network) {
+//		Arrow a = (Arrow) getRangeValue(edge);
+//
+//		// default has already been set - no need to do anything
+//		if (a == null)
+//			return;
+//
+//		appr.setTargetArrow(a);
+//	}
+//
+//	public Arrow calculateEdgeArrow(Edge e, CyNetwork n) {
+//		final EdgeAppearance ea = new EdgeAppearance();
+//		apply(ea, e, n);
+//
+//		return ea.getTargetArrow();
+//	}
 }
