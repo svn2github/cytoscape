9e8ffa077456b3840fe23b34a58f6a6b52e92d5a
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-02-14 16:53:29 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@9583 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/visual/calculators/GenericEdgeTargetArrowShapeCalculator.java
+++ cytoscape/trunk/src/cytoscape/visual/calculators/GenericEdgeTargetArrowShapeCalculator.java
@@ -2,11 +2,11 @@ package cytoscape.visual.calculators;
 
 import cytoscape.visual.mappings.ObjectMapping;
 
-public class GenericEdgeTargetArrowShapeCalculator extends EdgeCalculator {
+public class GenericEdgeTargetArrowShapeCalculator {
 
-	public GenericEdgeTargetArrowShapeCalculator(String name, ObjectMapping m, Class c) {
-		super(name, m, c);
-		// TODO Auto-generated constructor stub
-	}
+//	public GenericEdgeTargetArrowShapeCalculator(String name, ObjectMapping m, Class c) {
+//		super(name, m, c);
+//		// TODO Auto-generated constructor stub
+//	}
 
 }
