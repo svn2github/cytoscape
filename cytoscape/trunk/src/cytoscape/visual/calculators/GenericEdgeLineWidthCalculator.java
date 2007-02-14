b0317eaa1bd512099dc295aa4d5e01fa92361577
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-02-14 16:48:04 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@9582 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/visual/calculators/GenericEdgeLineWidthCalculator.java
+++ cytoscape/trunk/src/cytoscape/visual/calculators/GenericEdgeLineWidthCalculator.java
@@ -1,6 +1,6 @@
 package cytoscape.visual.calculators;
 
-import static cytoscape.visual.VisualPropertyType.EDGE_LINE_WIDTH;
+//import static cytoscape.visual.VisualPropertyType.EDGE_LINE_WIDTH;
 import giny.model.Edge;
 
 import java.util.Properties;
@@ -12,31 +12,31 @@ import cytoscape.visual.LineTypeDef;
 import cytoscape.visual.mappings.ObjectMapping;
 import cytoscape.visual.parsers.LineTypeParser;
 
-public class GenericEdgeLineWidthCalculator extends EdgeCalculator {
+public class GenericEdgeLineWidthCalculator  {
 
-	public GenericEdgeLineWidthCalculator(String name, ObjectMapping m, Class c) {
-		super(name, m, c, EDGE_LINE_WIDTH);
-	}
-
-	public GenericEdgeLineWidthCalculator(String name, Properties props,
-			String baseKey) {
-		super(name, props, baseKey, new LineTypeParser(), new Line(LineTypeDef.SOLID, 1.0f),
-				EDGE_LINE_WIDTH);
-	}
-	
-	public void apply(EdgeAppearance appr, Edge edge, CyNetwork network) {
-		final Line line = (Line) getRangeValue(edge);
-
-		// default has already been set - no need to do anything
-		if (line == null)
-			return;
-
-		appr.setLine(line);
-	}
-
-	public Line calculateEdgeLineWidth(Edge e, CyNetwork n) {
-		final EdgeAppearance ea = new EdgeAppearance();
-		apply(ea, e, n);
-		return ea.getLine();
-	}
+//	public GenericEdgeLineWidthCalculator(String name, ObjectMapping m, Class c) {
+//		super(name, m, c, EDGE_LINE_WIDTH);
+//	}
+//
+//	public GenericEdgeLineWidthCalculator(String name, Properties props,
+//			String baseKey) {
+//		super(name, props, baseKey, new LineTypeParser(), new Line(LineTypeDef.SOLID, 1.0f),
+//				EDGE_LINE_WIDTH);
+//	}
+//	
+//	public void apply(EdgeAppearance appr, Edge edge, CyNetwork network) {
+//		final Line line = (Line) getRangeValue(edge);
+//
+//		// default has already been set - no need to do anything
+//		if (line == null)
+//			return;
+//
+//		appr.setLine(line);
+//	}
+//
+//	public Line calculateEdgeLineWidth(Edge e, CyNetwork n) {
+//		final EdgeAppearance ea = new EdgeAppearance();
+//		apply(ea, e, n);
+//		return ea.getLine();
+//	}
 }
