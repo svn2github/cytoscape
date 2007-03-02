c063affaa40e9220b026ae455cb70c9d3f14aa5a
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-02 22:24:33 +0000

    added an index file for the dir
    git-svn-id: http://chianti.ucsd.edu/svn@9641 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/nature.protocols/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+		<title>
+			Cytoscape Nature Protocol
+		</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="../css/cytoscape.css" />
+		<link rel="shortcut icon" href="../images/cyto.ico" />
+	</head>
+	<body bgcolor="#FFFFFF">
+		<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
+			<tr>
+				<td width="10">
+					&nbsp;
+				</td>
+				<td valign="center">
+					<h1>
+						Nature Protocols
+					</h1>
+				</td>
+			</tr>
+		</table>
+<? include "../nav.php"; ?>
+<div id="contents">
+	<div id="content">
+			<P>This page contains a link to the webstart that contains all of the plugins
+			necessary to properly run the protocol described in this paper: (ref here).</p>
+
+			<p>
+			Click <A HREF="runCytoscape.jnlp">here</A> for the WebStart.
+			</p>
+			<p>
+			For more information on Java Webstart, click <A HREF="http://java.sun.com/products/javawebstart/">here</A>.	
+			</p>
+	</div>
+</div>
+
+<? include "../footer.php"; ?>
+	</body>
+</html>
