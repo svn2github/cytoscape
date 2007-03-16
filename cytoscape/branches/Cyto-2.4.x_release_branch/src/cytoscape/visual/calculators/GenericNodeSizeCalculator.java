731abc49bcfef299a60a30d1e80b59b908c69286
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-16 19:08:24 +0000

    fix for properly saving node border colors, edge source arrows, node width, and node height visual styles
    git-svn-id: http://chianti.ucsd.edu/svn@9713 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/branches/Cyto-2.4.x_release_branch/src/cytoscape/visual/calculators/GenericNodeSizeCalculator.java
+++ cytoscape/branches/Cyto-2.4.x_release_branch/src/cytoscape/visual/calculators/GenericNodeSizeCalculator.java
@@ -1,36 +1,35 @@
-
 /*
-  File: GenericNodeSizeCalculator.java 
-  
+  File: GenericNodeSizeCalculator.java
+
   Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
-  
-  The Cytoscape Consortium is: 
+
+  The Cytoscape Consortium is:
   - Institute for Systems Biology
   - University of California San Diego
   - Memorial Sloan-Kettering Cancer Center
   - Institut Pasteur
   - Agilent Technologies
-  
+
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published
   by the Free Software Foundation; either version 2.1 of the License, or
   any later version.
-  
+
   This library is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
   MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
   documentation provided hereunder is on an "as is" basis, and the
-  Institute for Systems Biology and the Whitehead Institute 
+  Institute for Systems Biology and the Whitehead Institute
   have no obligations to provide maintenance, support,
   updates, enhancements or modifications.  In no event shall the
-  Institute for Systems Biology and the Whitehead Institute 
+  Institute for Systems Biology and the Whitehead Institute
   be liable to any party for direct, indirect, special,
   incidental or consequential damages, including lost profits, arising
   out of the use of this software and its documentation, even if the
-  Institute for Systems Biology and the Whitehead Institute 
+  Institute for Systems Biology and the Whitehead Institute
   have been advised of the possibility of such damage.  See
   the GNU Lesser General Public License for more details.
-  
+
   You should have received a copy of the GNU Lesser General Public License
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
@@ -42,87 +41,139 @@
 // $Author$
 //----------------------------------------------------------------------------
 package cytoscape.visual.calculators;
-//----------------------------------------------------------------------------
-import java.util.Map;
-import java.util.Properties;
-import javax.swing.JPanel;
+
+import cytoscape.CyNetwork;
+
+import cytoscape.visual.NodeAppearance;
+
+import cytoscape.visual.mappings.ObjectMapping;
+
+import cytoscape.visual.parsers.DoubleParser;
+
+import cytoscape.visual.ui.VizMapUI;
 
 import giny.model.Node;
 
-import cytoscape.CyNetwork;
-import cytoscape.visual.mappings.ObjectMapping;
-import cytoscape.visual.parsers.DoubleParser;
+//----------------------------------------------------------------------------
+import java.util.Map;
+import java.util.Properties;
 
-import cytoscape.visual.NodeAppearance;
-import cytoscape.visual.ui.VizMapUI;
+import javax.swing.JPanel;
 
-/** @deprecated Use NodeWidth,NodeHeight, or NodeUniformSize instead. 
+
+/** @deprecated Use NodeWidth,NodeHeight, or NodeUniformSize instead.
     will be removed 10/2007 */
-public class GenericNodeSizeCalculator extends AbstractNodeSizeCalculator 
+public class GenericNodeSizeCalculator extends AbstractNodeSizeCalculator
     implements NodeSizeCalculator {
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public byte getType() {
+		return sizeType;
+	}
 
-    public byte getType() {
-	return sizeType; 
-    } 
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public String getPropertyLabel() {
+		return propertyLabel;
+	}
 
-    public String getPropertyLabel() {
-        return propertyLabel; 
-    }
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public String getTypeName() {
+		return typename;
+	}
 
-    public String getTypeName() {
-        return typename; 
-    }
+	protected String getClassName() {
+		if (sizeType == VizMapUI.NODE_SIZE)
+			return "cytoscape.visual.calculators.GenericNodeUniformSizeCalculator";
 
-    protected String getClassName() {
-    	if ( sizeType == VizMapUI.NODE_SIZE )
-		return "cytoscape.visual.calculators.GenericNodeUniformSizeCalculator";
-    	if ( sizeType == VizMapUI.NODE_WIDTH )
-		return "cytoscape.visual.calculators.GenericNodeWidthCalculator";
-    	if ( sizeType == VizMapUI.NODE_HEIGHT )
-		return "cytoscape.visual.calculators.GenericNodeHeightCalculator";
-	return getClass().getName();
-    }
-    
-    GenericNodeSizeCalculator() {
-	super();
-	// do this as a better default than 0,null,null - still not good though
-	set(VizMapUI.NODE_SIZE,"nodeUniformSizeCalculator","Node Size");
-    }
-   
-    public GenericNodeSizeCalculator(String name, ObjectMapping m) {
-	super(name, m);
-	// do this as a better default than 0,null,null - still not good though
-	set(VizMapUI.NODE_SIZE,"nodeUniformSizeCalculator","Node Size");
-    }
-   
-    public GenericNodeSizeCalculator(String name, Properties props, String baseKey) {
-        super(name, props, baseKey);
-	// do this as a better default than 0,null,null - still not good though
-	set(VizMapUI.NODE_SIZE,"nodeUniformSizeCalculator","Node Size");
-    }
-    
-    public void apply(NodeAppearance appr, Node node, CyNetwork network) {
-    	if ( sizeType == VizMapUI.NODE_WIDTH )
-		apply(appr,node,network,WIDTH);
-	else if  ( sizeType == VizMapUI.NODE_HEIGHT )
-		apply(appr,node,network,HEIGHT);
-	else if  ( sizeType == VizMapUI.NODE_SIZE )
-		apply(appr,node,network,SIZE);
-	else
-		System.err.println("don't know what kind of calculator this is!");
-    }
+		if (sizeType == VizMapUI.NODE_WIDTH)
+			return "cytoscape.visual.calculators.GenericNodeWidthCalculator";
 
-    public double calculateNodeSize(Node e, CyNetwork n) {
-        NodeAppearance ea = new NodeAppearance();
-        apply(ea,e,n);
-    	if ( sizeType == VizMapUI.NODE_WIDTH )
-		return ea.getWidth();	
-	else if  ( sizeType == VizMapUI.NODE_HEIGHT )
-		return ea.getHeight();	
-	else if  ( sizeType == VizMapUI.NODE_SIZE )
-		return ea.getSize();	
-	else
-		return -1;	
-    }
+		if (sizeType == VizMapUI.NODE_HEIGHT)
+			return "cytoscape.visual.calculators.GenericNodeHeightCalculator";
+
+		return getClass().getName();
+	}
+
+	GenericNodeSizeCalculator() {
+		super();
+		// do this as a better default than 0,null,null - still not good though
+		set(VizMapUI.NODE_SIZE, "nodeUniformSizeCalculator", "Node Size");
+	}
+
+	/**
+	 * Creates a new GenericNodeSizeCalculator object.
+	 *
+	 * @param name  DOCUMENT ME!
+	 * @param m  DOCUMENT ME!
+	 */
+	public GenericNodeSizeCalculator(String name, ObjectMapping m) {
+		super(name, m);
+		// do this as a better default than 0,null,null - still not good though
+		set(VizMapUI.NODE_SIZE, "nodeUniformSizeCalculator", "Node Size");
+	}
+
+	/**
+	 * Creates a new GenericNodeSizeCalculator object.
+	 *
+	 * @param name  DOCUMENT ME!
+	 * @param props  DOCUMENT ME!
+	 * @param baseKey  DOCUMENT ME!
+	 */
+	public GenericNodeSizeCalculator(String name, Properties props, String baseKey) {
+		super(name, props, baseKey);
+		// do this as a better default than 0,null,null - still not good though
+		set(VizMapUI.NODE_SIZE, "nodeUniformSizeCalculator", "Node Size");
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param appr DOCUMENT ME!
+	 * @param node DOCUMENT ME!
+	 * @param network DOCUMENT ME!
+	 */
+	public void apply(NodeAppearance appr, Node node, CyNetwork network) {
+		if (sizeType == VizMapUI.NODE_WIDTH)
+			apply(appr, node, network, WIDTH);
+		else if (sizeType == VizMapUI.NODE_HEIGHT)
+			apply(appr, node, network, HEIGHT);
+		else if (sizeType == VizMapUI.NODE_SIZE)
+			apply(appr, node, network, SIZE);
+		else
+			System.err.println("don't know what kind of calculator this is!");
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param e DOCUMENT ME!
+	 * @param n DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public double calculateNodeSize(Node e, CyNetwork n) {
+		NodeAppearance ea = new NodeAppearance();
+		apply(ea, e, n);
+
+		if (sizeType == VizMapUI.NODE_WIDTH)
+			return ea.getWidth();
+		else if (sizeType == VizMapUI.NODE_HEIGHT)
+			return ea.getHeight();
+		else if (sizeType == VizMapUI.NODE_SIZE)
+			return ea.getSize();
+		else
+
+			return -1;
+	}
 }
-
