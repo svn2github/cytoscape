5dccc69e6fd45e266d676681be848661dfc26ce4
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-19 23:07:32 +0000

    Starts from the list dialog now.
    git-svn-id: http://chianti.ucsd.edu/svn@9732 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/PluginManagerAction.java
+++ cytoscape/trunk/src/cytoscape/actions/PluginManagerAction.java
@@ -7,13 +7,16 @@ import cytoscape.plugin.PluginManager;
 import cytoscape.plugin.PluginInfo;
 import cytoscape.util.CytoscapeAction;
 import cytoscape.Cytoscape;
+import cytoscape.CytoscapeInit;
 import cytoscape.dialogs.PluginInstallDialog;
+import cytoscape.dialogs.PluginListDialog;
 
 import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Collection;
 
 import javax.swing.JOptionPane;
 
@@ -46,33 +49,13 @@ public class PluginManagerAction extends CytoscapeAction
 		 * have a button to "install plugins" poping up the PluginInstallDialog
 		 */
 		
-		PluginManager Mgr = new PluginManager();
-		System.out.println("Default URL: " + Mgr.getDefaultUrl());
-		Map<String, List<PluginInfo>> Plugins = Mgr.getPluginsByCategory();
-		Iterator<String> catI = Plugins.keySet().iterator();
-		System.out.println("Plugin categories: " + Plugins.size());
-
-		if (Plugins.size() > 0)
-			{
-			// obviously this is not all that will be done
-			PluginInstallDialog pid = new PluginInstallDialog();
-			int index = 0;
-			while(catI.hasNext())
-				{
-				String CategoryName = catI.next();
-				System.out.println("Categor: " + CategoryName);
-				pid.addCategory(CategoryName, Plugins.get(CategoryName), index);
-				if (index == 0) index++; //something about how trees work index needs to be only 0 or 1??
-				}
+		PluginManager Mgr = CytoscapeInit.getPluginManager();
+		PluginInfo[] Info = Mgr.getInstalledPlugins();
 		
-			pid.pack();
-			pid.setVisible(true);
-			}
-		else 
-			{ // again, this is not how it will stay implemented
-			String Msg = "No plugins were found at " + Mgr.getDefaultUrl() + " for Cytoscape version " + cytoscape.CytoscapeVersion.version;
-			JOptionPane.showMessageDialog(Cytoscape.getDesktop(), Msg, "Warning", JOptionPane.WARNING_MESSAGE);
-			}
+		PluginListDialog dialog = new PluginListDialog();
+		dialog.createTable(Info);
+		dialog.pack();
+		dialog.setVisible(true);
 		}
 
 	}
