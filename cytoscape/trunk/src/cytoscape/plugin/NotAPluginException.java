6bd801416a17fed5a96fd712849585e1e54ceab8
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-20 22:14:38 +0000

    Changed reference to AbstractPlugin
    git-svn-id: http://chianti.ucsd.edu/svn@9745 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/plugin/NotAPluginException.java
+++ cytoscape/trunk/src/cytoscape/plugin/NotAPluginException.java
@@ -39,7 +39,7 @@ package cytoscape.plugin;
 
 /**
  * Thrown to indicate that a class cannot be used as a plugin
- * becuase it does not extend AbstractPlugin.
+ * becuase it does not extend CytoscapePlugin.
  */
 public class NotAPluginException extends Exception {
 	/**
