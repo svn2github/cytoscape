e100c291a86b1c121738fcb4ee03fba3546b5fa9
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-22 20:41:44 +0000

    General usability improvments.  Networks are added/removed from the dialog box when created/deleted in Cytoscape.  Added error messages for broadcasts that do not have required elements to broadcast.
    git-svn-id: http://chianti.ucsd.edu/svn@9760 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/isb/skillcoyne/CyGoose/tags/CyGoose_2.4.2/src/main/org/systemsbiology/cytoscape/CyBroadcast.java
@@ -0,0 +1,461 @@
+/**
+ * 
+ */
+package org.systemsbiology.cytoscape;
+
+import org.systemsbiology.cytoscape.dialog.CyAttrDialog;
+import org.systemsbiology.cytoscape.dialog.GooseDialog;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.util.*;
+
+import javax.swing.JOptionPane;
+
+import org.systemsbiology.gaggle.boss.Boss;
+import org.systemsbiology.gaggle.experiment.datamatrix.DataMatrix;
+import org.systemsbiology.gaggle.network.*;
+import org.systemsbiology.gaggle.geese.Goose;
+
+import cytoscape.*;
+import cytoscape.data.CyAttributes;
+import cytoscape.data.Semantics;
+
+/**
+ * @author skillcoy
+ * 
+ */
+public class CyBroadcast
+	{
+	private GooseDialog GDialog;
+	private Boss GaggleBoss;
+	private Goose CyGoose;
+
+	private String broadcastID = "ID";
+
+ 	// text strings for popup dialog boxes
+  private String broadcastStr = "The following checked attributes will be captured for broadcast.\n"
+      + "Click \"OK\" to proceed or \"Cancel\" to cancel transaction.";
+
+	
+	private static void print(String S)
+		{ System.out.println(S); }
+	
+	public CyBroadcast(GooseDialog Dialog, Boss boss, Goose goose)
+		{
+		this.GDialog = Dialog;
+		this.CyGoose = goose;
+		this.GaggleBoss = boss;
+
+		this.addButtonActions();
+		}
+
+
+	private String getTargetGoose()
+		{
+    int targetGooseIndex = this.GDialog.getGooseBox().getSelectedIndex();
+		String targetGooseName = (String)this.GDialog.getGooseBox().getSelectedItem();
+		print("Target index: "+targetGooseIndex+"  Target item: "+targetGooseName);
+		return targetGooseName;
+		}
+
+
+	// very basically for the moment we will only broadcast by ID	
+	public void broadcastNameList()
+		{
+		print("broadcastNameList");
+		if (Cytoscape.getCurrentNetwork().getSelectedNodes().size() <= 0)
+			{
+			GagglePlugin.showDialogBox("No nodes were selected for list broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+			return;
+			}
+
+
+    Set<CyNode> SelectedNodes = Cytoscape.getCurrentNetwork().getSelectedNodes();
+    Iterator<CyNode> NodesIter = SelectedNodes.iterator();
+
+    // warning: no nodes are selected for broadcast
+    if (SelectedNodes.size() == 0)
+      {
+      GagglePlugin.showDialogBox("No nodes selected for broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+      return;
+      }
+
+    String[] NodeIds = new String[SelectedNodes.size()];
+		// TODO: get species
+		String Species = null;
+
+    //CyNode Node;
+    for (int i = 0; i < SelectedNodes.size(); i++)
+      {
+      CyNode Node = NodesIter.next();
+			NodeIds[i] = Node.getIdentifier();
+			}
+
+		try 
+			{ GaggleBoss.broadcast(this.CyGoose.getName(), this.getTargetGoose(), Species, NodeIds); }
+		catch(Exception E) 
+			{ 
+			GagglePlugin.showDialogBox("Failed to broadcast list of names to " + this.getTargetGoose(), "Error", JOptionPane.ERROR_MESSAGE);
+			E.printStackTrace(); 
+			}
+		}
+
+	// broadcasts hash of selected attributes	
+	public void broadcastHashMap()
+		{
+		print("broadcastHashMap");
+
+    Set<CyNode> SelectedNodes = Cytoscape.getCurrentNetwork().getSelectedNodes();
+
+    // warning: no nodes are selected for broadcast
+    if (SelectedNodes.size() == 0)
+      {
+      GagglePlugin.showDialogBox("No nodes selected for broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+      return;
+      }
+
+
+    // pass string of attribute names
+    String[] AllAttrNames = Cytoscape.getNodeAttributes().getAttributeNames();
+    
+    // confirmAttrSelection(AllAttrNames, "HashMap");
+    AttrSelectAction okAction = new AttrSelectAction()
+      {
+        public void takeAction(String[] selectAttr)
+          {
+          broadcastHashMap(selectAttr);
+          }
+      };
+
+    CyAttrDialog dialog = new CyAttrDialog(AllAttrNames, okAction, CyAttrDialog.MULTIPLE_SELECT);
+    dialog.setDialogText(broadcastStr);
+    dialog.preSelectCheckBox(AllAttrNames);
+    dialog.buildDialogWin();
+		}
+
+
+  private void broadcastHashMap(String[] attrNames)
+    {
+		if (Cytoscape.getCurrentNetwork().getSelectedNodes().size() <= 0)
+			{
+			GagglePlugin.showDialogBox("No nodes were selected for map broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+			return;
+			}
+
+    Set selectedNodes = Cytoscape.getCurrentNetwork().getSelectedNodes();
+    Iterator nodeIter = selectedNodes.iterator();
+
+    // create a string of node names
+    String[] nodeArr = new String[selectedNodes.size()];
+    //CyNode node;
+    for (int i = 0; (i < selectedNodes.size()) && nodeIter.hasNext(); i++)
+      {
+      CyNode CurrentNode = (CyNode) nodeIter.next();
+      nodeArr[i] = CurrentNode.getIdentifier();
+      }
+
+    HashMap map = new HashMap();
+    for (int i = 0; i < attrNames.length; i++)
+      {
+      String attr = attrNames[i];
+      ArrayList namesAndValues = new ArrayList();
+      switch (Cytoscape.getNodeAttributes().getType(attr))
+        {
+        case CyAttributes.TYPE_INTEGER:
+        int[] intValues = new int[nodeArr.length];
+        for (int j = 0; j < nodeArr.length; j++)
+          intValues[j] = Cytoscape.getNodeAttributes().getIntegerAttribute(
+              nodeArr[j], attr).intValue();
+        // create HashMap
+        namesAndValues.add(nodeArr);
+        namesAndValues.add(intValues);
+        break;
+        case CyAttributes.TYPE_FLOATING:
+        double[] dbValues = new double[nodeArr.length];
+        for (int j = 0; j < nodeArr.length; j++)
+          dbValues[j] = Cytoscape.getNodeAttributes().getDoubleAttribute(
+              nodeArr[j], attr).doubleValue();
+        // create HashMap
+        namesAndValues.add(nodeArr);
+        namesAndValues.add(dbValues);
+        break;
+        case CyAttributes.TYPE_BOOLEAN:
+        boolean[] boolValues = new boolean[nodeArr.length];
+        for (int j = 0; j < nodeArr.length; j++)
+          boolValues[j] = Cytoscape.getNodeAttributes().getBooleanAttribute(
+              nodeArr[j], attr).booleanValue();
+        // create HashMap
+        namesAndValues.add(nodeArr);
+        namesAndValues.add(boolValues);
+        break;
+        case CyAttributes.TYPE_STRING:
+        String[] strValues = new String[nodeArr.length];
+        for (int j = 0; j < nodeArr.length; j++)
+          strValues[j] = Cytoscape.getNodeAttributes().getStringAttribute(
+              nodeArr[j], attr);
+        // create HashMap
+        namesAndValues.add(nodeArr);
+        namesAndValues.add(strValues);
+        break;
+        }
+      map.put(attr, namesAndValues);
+      }
+
+    // String species = CytoscapeInit.getDefaultSpeciesName();
+    String species = getSpecies(nodeArr[0]);
+    String dataTitle = "";
+
+
+		try
+	    { this.GaggleBoss.broadcast(this.CyGoose.getName(), this.getTargetGoose(), species, dataTitle, map); }
+		catch (Exception E) 
+			{ 
+			GagglePlugin.showDialogBox("Failed to broadcast map to " + this.getTargetGoose(), "Error", JOptionPane.ERROR_MESSAGE);
+			E.printStackTrace(); 
+			}
+		}
+
+	public void broadcastDataMatrix()
+		{
+		print("broadcastDataMatrix"); 
+
+		if (Cytoscape.getCurrentNetwork().getSelectedNodes().size() <= 0)
+			{
+			GagglePlugin.showDialogBox("No nodes were selected for Data Matrix broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+			return;
+			}
+
+    Set SelectedNodes = Cytoscape.getCurrentNetwork().getSelectedNodes();
+
+    // warning: no nodes are selected for broadcast
+    if (SelectedNodes.size() == 0)
+      {
+      GagglePlugin.showDialogBox("No nodes selected for broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+      return;
+      }
+
+    //create an array of experiment conditions (columnTitles in DataMatrix)
+    ArrayList<String> condNamesArrList = new ArrayList<String>();
+    String[] attributeNames = Cytoscape.getNodeAttributes().getAttributeNames();
+
+    for(String CurrentAttr: attributeNames)
+      {
+      // assume all DOUBLE type attributes are expression data
+      if (Cytoscape.getNodeAttributes().getType(CurrentAttr) == CyAttributes.TYPE_FLOATING)
+        condNamesArrList.add(CurrentAttr);
+      }
+
+    // move everything from ArrayList to a String array
+    String[] condNames = new String[condNamesArrList.size()];
+    condNamesArrList.toArray(condNames);
+
+    // dialog for user to select attributes for broadcast
+    AttrSelectAction okAction = new AttrSelectAction()
+      {
+        public void takeAction(String[] selectAttr)
+          {
+          broadcastDataMatrix(selectAttr);
+          }
+      };
+
+    if (condNames.length > 0)
+    	{
+	    CyAttrDialog dialog = new CyAttrDialog(condNames, okAction, CyAttrDialog.MULTIPLE_SELECT);
+	    dialog.setDialogText(broadcastStr);
+	    dialog.preSelectCheckBox(condNames);
+	    dialog.buildDialogWin();
+    	}
+    else
+    	{
+      GagglePlugin.showDialogBox("The selected nodes do not have numerical attributes for a matrix", "Warning", JOptionPane.WARNING_MESSAGE);
+    	}
+		}
+	
+  private void broadcastDataMatrix(String[] condNames)
+    {
+		if (Cytoscape.getCurrentNetwork().getSelectedNodes().size() <= 0)
+			{
+			GagglePlugin.showDialogBox("No nodes were selected for Data Matrix broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+			return;
+			}
+
+    Set selectedNodes = Cytoscape.getCurrentNetwork().getSelectedNodes();
+    Iterator selectedNodesIter = selectedNodes.iterator();
+
+    DataMatrix matrix = new DataMatrix();
+    // initialize DataMatrix
+    matrix.setColumnTitles(condNames);
+    // matrix.setSpecies(getSpecies());
+
+    
+    //loop through all flagged nodes and construct a DataMatrix with
+    // row=columnNames & column=condNames
+    while (selectedNodesIter.hasNext())
+      {
+      double[] condVals = new double[condNames.length];
+      CyNode CurrentSelectedNode = (CyNode) selectedNodesIter.next();
+      String NodeId = CurrentSelectedNode.getIdentifier();
+      for (int i=0; i < condNames.length; i++)
+        {
+        try
+          {
+          Double val = Cytoscape.getNodeAttributes().getDoubleAttribute(NodeId, condNames[i]);
+          if (val != null) condVals[i] = val.doubleValue();
+          }
+        catch (Exception ex)
+          {
+          System.out.println("broadcastDataMatrix() error: incompatible data type for " + condNames[i]);
+          }
+        }
+      // use other attribute to identify node if selected by user
+
+      if (!broadcastID.equals(CyAttrDialog.DEFAULT_NODE_ID)) // again..."ID" == "ID" WHY??
+        {
+        NodeId = Cytoscape.getNodeAttributes().getStringAttribute(NodeId, broadcastID);
+
+        // skip any node that is null // shouldn't this happen before?
+        if (NodeId == null) continue;
+        }
+      // add new row to DataMatrix
+      matrix.addRow(NodeId, condVals);
+      matrix.setSpecies(getSpecies(NodeId)); // er...does this set the species every time 'round?  what if the species is different from one node to another?
+      }
+
+		try 
+			{ this.GaggleBoss.broadcast(this.CyGoose.getName(), this.getTargetGoose(), matrix); }
+		catch (Exception E) 
+			{ 
+			GagglePlugin.showDialogBox("Failed to broadcast matrix to " + this.getTargetGoose(), "Error", JOptionPane.ERROR_MESSAGE);
+			E.printStackTrace(); 
+			}
+		}
+
+	public void broadcastNetwork()
+		{
+		print("broadcastNetwork");
+
+		if (Cytoscape.getCurrentNetwork().getSelectedEdges().size() <= 0)
+			{
+			GagglePlugin.showDialogBox("Please make a selection that includes edges for a network broadcast.", "Warning", JOptionPane.WARNING_MESSAGE);
+			return;
+			}
+		
+    Iterator<CyNode> NodesIter = Cytoscape.getCurrentNetwork().getSelectedNodes().iterator();
+  	Iterator<CyEdge> EdgesIter = Cytoscape.getCurrentNetwork().getSelectedEdges().iterator();
+		CyAttributes EdgeAtts = Cytoscape.getEdgeAttributes();
+
+    Network GaggleNetwork = new Network();
+    String Species = "";
+    while (EdgesIter.hasNext())
+      {
+      CyEdge CurrentSelectedEdge = EdgesIter.next();
+      String SourceNodeId = CurrentSelectedEdge.getSource().getIdentifier();
+      String TargetNodeId = CurrentSelectedEdge.getTarget().getIdentifier();
+
+      String InteractionType = EdgeAtts.getStringAttribute(CurrentSelectedEdge.getIdentifier(), Semantics.INTERACTION);
+
+      // create a new GaggleInteraction for broadcast
+      org.systemsbiology.gaggle.network.Interaction GaggleInteraction = 
+				new Interaction(SourceNodeId, TargetNodeId, InteractionType, CurrentSelectedEdge.isDirected());
+      GaggleNetwork.add(GaggleInteraction);
+
+      // again if there's more than one species we'll only get the last one!!!
+      Species = this.getSpecies(SourceNodeId);
+      }
+			try 
+				{ this.GaggleBoss.broadcast(this.CyGoose.getName(), this.getTargetGoose(), Species, GaggleNetwork); }
+			catch (Exception E) { E.printStackTrace(); }
+		}
+
+	private String getSpecies(String NodeId)
+		{
+    // try the "species" attribute first; if not found, use DefaultSpeciesName
+    String Species = "";
+
+//    try
+      {
+      Species = Cytoscape.getNodeAttributes().getStringAttribute(NodeId, Semantics.SPECIES);
+
+      if (Species == null) Species = CytoscapeInit.getProperties().getProperty("defaultSpeciesName");
+      }
+/*
+    catch (Exception ex)
+      {
+      System.out.println("Exception encountered while fetching species name for node: " + nodeID);
+      }
+*/
+    return Species;
+
+		}	
+	
+
+	private void addButtonActions()
+		{
+    /* broadcast name list to other goose (geese) */
+    GDialog.getListButton().addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent event)
+          {
+          try
+            {
+            broadcastNameList();
+            }
+          catch (Exception ex)
+            {
+            ex.printStackTrace();
+            }
+          }
+      });
+
+    /* broadcast a network to other goose (geese) */
+    GDialog.getNetButton().addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent event)
+          {
+          try
+            {
+            broadcastNetwork();
+            }
+          catch (Exception ex)
+            {
+            ex.printStackTrace();
+            }
+          }
+      });
+
+    /* broadcast data matrix to other goose (geese) */
+    GDialog.getMatrixButton().addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent event)
+          {
+          try
+            {
+            broadcastDataMatrix();
+            }
+          catch (Exception ex)
+            {
+            ex.printStackTrace();
+            }
+          }
+      });
+
+    /* broadcast HashMap to other goose (geese) */
+    GDialog.getMapButton().addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent event)
+          {
+          try
+            {
+            broadcastHashMap();
+            }
+          catch (Exception ex)
+            {
+            ex.printStackTrace();
+            }
+					}
+			});
+		}
+
+	}
---
+++ csplugins/trunk/isb/skillcoyne/CyGoose/tags/CyGoose_2.4.2/src/main/org/systemsbiology/cytoscape/GagglePlugin.java
@@ -0,0 +1,335 @@
+/**
+ * 
+ */
+package org.systemsbiology.cytoscape;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.rmi.Naming;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+import java.net.MalformedURLException;
+
+import org.systemsbiology.cytoscape.CyGoose;
+import org.systemsbiology.cytoscape.dialog.GooseDialog;
+import org.systemsbiology.gaggle.boss.Boss;
+import org.systemsbiology.gaggle.geese.Goose;
+import org.systemsbiology.gaggle.util.MiscUtil;
+
+import javax.swing.JOptionPane;
+
+//import java.util.Set;
+import java.util.HashMap;
+//import java.util.Iterator;
+//import java.util.HashSet;
+//import java.util.Collection;
+//import java.util.ArrayList;
+
+import cytoscape.Cytoscape;
+import cytoscape.view.cytopanels.CytoPanel;
+//import cytoscape.view.cytopanels.CytoPanelState;
+
+import cytoscape.CyNetwork;
+import cytoscape.plugin.CytoscapePlugin;
+import cytoscape.CytoscapeVersion;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+/**
+ * @author skillcoy
+ * 
+ */
+public class GagglePlugin extends CytoscapePlugin implements PropertyChangeListener
+	{
+	private static Boss GaggleBoss;
+	private static GooseDialog Dialog;
+	private static boolean Initialized = false;
+	protected static String myGaggleName = "Cytoscape";
+	// keeps track of all the network ids key = network id  value = goose 
+	private static HashMap<String, Goose> NetworkGeese;
+	
+	private static void print(String S) { System.out.println(S); }
+
+	public GagglePlugin()
+		{
+		// constructor gets called at load time and every time the toolbar is used
+		if (Initialized) { return; }
+
+		Cytoscape.getPropertyChangeSupport().addPropertyChangeListener( this );
+		
+		myGaggleName += " v." + CytoscapeVersion.version;
+		
+		NetworkGeese = new HashMap<String, Goose>();
+		Dialog = new GooseDialog();
+		
+		CytoPanel GoosePanel = Cytoscape.getDesktop().getCytoPanel(javax.swing.SwingConstants.WEST);
+		GoosePanel.add("CyGoose Plugin", null, Dialog, "Gaggle Goose");
+		GoosePanel.setSelectedIndex( GoosePanel.indexOfComponent(Dialog) );
+		
+		this.registerAction();
+	
+		try 
+			{ 
+			GaggleBoss = this.rmiConnect(); 
+			// this gives an initial goose that is cytoscape with a null network
+			this.createDefaultGoose();
+			this.updateAction();
+			}
+		catch (Exception E)
+			{
+			GagglePlugin.showDialogBox("Failed to connect to the Boss", "Error", JOptionPane.ERROR_MESSAGE);
+			E.printStackTrace();
+			}
+		
+		Initialized = true;
+		}
+	
+	private void createDefaultGoose() throws RemoteException
+		{
+		// this gives an initial goose that is cytoscape with a null network
+		CyNetwork CurrentNet = Cytoscape.getNullNetwork();
+		CurrentNet.setTitle(myGaggleName);
+		Goose NewGoose = this.createNewGoose(CurrentNet);
+		NetworkGeese.put(CurrentNet.getIdentifier(), NewGoose);
+    MiscUtil.updateGooseChooser(GaggleBoss, Dialog.getGooseBox(), null, null);
+
+    try { Cytoscape.getDesktop().setTitle("Goose: " + NewGoose.getName()); }
+		catch (RemoteException E) { E.printStackTrace(); }
+		}
+	
+	
+	public void propertyChange(PropertyChangeEvent Event)
+		{
+		// register a goose
+		if (Event.getPropertyName() == Cytoscape.NETWORK_CREATED)
+			{
+      String netId = Event.getNewValue().toString();
+      CyNetwork net = Cytoscape.getNetwork(netId);
+
+      try
+      	{
+	      Goose NewGoose = this.createNewGoose(net);
+				NetworkGeese.put(net.getIdentifier(), NewGoose);
+	      MiscUtil.updateGooseChooser(GaggleBoss, Dialog.getGooseBox(), null, null);
+      	}
+      catch (RemoteException E)
+      	{
+      	GagglePlugin.showDialogBox("Failed to create a new Goose for network " + net.getTitle(), "Error", JOptionPane.ERROR_MESSAGE);
+      	E.printStackTrace();
+      	}
+      
+      
+			}
+		// remove a goose
+		if (Event.getPropertyName() == Cytoscape.NETWORK_DESTROYED)
+			{
+      String netId = Event.getNewValue().toString();
+      CyNetwork net = Cytoscape.getNetwork(netId);
+
+      String Name = "";
+      try
+      	{
+        Goose OldGoose = (Goose) NetworkGeese.get(net.getIdentifier());
+      	Name = OldGoose.getName();
+				GaggleBoss.remove( OldGoose.getName() ); 
+				UnicastRemoteObject.unexportObject(OldGoose, true);
+				Dialog.getGooseBox().removeItem( OldGoose.getName() );
+      	}
+      catch (RemoteException E)
+      	{
+      	GagglePlugin.showDialogBox("Failed to remove goose '" + Name + "' from Boss", "Warning", JOptionPane.WARNING_MESSAGE);
+      	E.printStackTrace();
+      	}
+			}
+		
+		}
+
+	/*
+	 * Exports and registers the goose with the Boss
+	 */
+	private void registerGoose(Goose G) throws RemoteException
+		{
+		String RegisteredName = null;
+		
+		try { UnicastRemoteObject.exportObject(G, 0); }
+		catch (RemoteException e)
+			{
+			e.printStackTrace();
+			String ErrorMsg = "Cytoscape failed to export remote object.";
+			GagglePlugin.showDialogBox(ErrorMsg, "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		print("BOSS: " + GaggleBoss.toString() );
+
+		String FilePath = null;
+		java.util.Iterator pI = cytoscape.CytoscapeInit.getPluginURLs().iterator();
+		while (pI.hasNext())
+			{
+			java.net.URL url = (java.net.URL)pI.next();
+			if (url.getPath().contains("CyGoose.jar"))
+				{
+				FilePath = url.getPath();
+				FilePath = FilePath.replace("/", System.getProperty("file.separator"));
+				FilePath = FilePath.replaceFirst("file:", "");
+				}
+			}
+//		System.setProperty("java.rmi.server.codebase", FilePath);
+//		System.out.println( "CLASSPATH: " + System.getProperty("java.class.path"));
+//		System.out.println("RMI CODEBASE: " + System.getProperty("java.rmi.server.codebase"));
+		RegisteredName = GaggleBoss.register(G);
+		G.setName(RegisteredName);
+		Dialog.getRegisterButton().setEnabled(false); 
+		}
+	
+	/*
+	 * Creates the boss via the rmi connection
+	 */
+	private Boss rmiConnect() throws RemoteException, java.rmi.NotBoundException, MalformedURLException 
+		{
+		String serviceName = "gaggle";
+		String hostname = "localhost";
+		
+		String osName = System.getProperty("os.name");
+		print("OS name: " + osName);
+
+		String uri = "rmi://" + hostname + "/" + serviceName;
+		print("Rmi uri: " + uri);
+		
+		return ( (Boss) Naming.lookup(uri) ); 
+		}
+	
+	private void registerAction()
+		{
+		Dialog.getRegisterButton().addActionListener( new ActionListener()
+			{
+			public void actionPerformed(ActionEvent event)
+				{
+				try
+					{
+					GaggleBoss = rmiConnect();
+					createDefaultGoose();
+					// todo want to be able to update all the networks/geese here since user 
+					// may have opened some prior to connection
+					}
+				catch (Exception E)
+					{ 
+					GagglePlugin.showDialogBox("Failed to connect to the Boss", "Error", JOptionPane.ERROR_MESSAGE);
+					E.printStackTrace(); 
+					}
+				}
+			}	);
+		}
+	
+	/*
+	 * action button
+	 */
+	private void updateAction()
+		{ 
+		Dialog.getUpdateButton().addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent event)
+					{
+					try
+						{
+						MiscUtil.updateGooseChooser(GaggleBoss, Dialog.getGooseBox(), null, null);
+						}
+					catch (Exception ex)
+						{
+						ex.printStackTrace();
+						}
+					}
+			});
+		}
+
+	/*
+	 * Creates a new goose for the given network
+	 */
+	private Goose createNewGoose(CyNetwork Network) throws RemoteException
+		{
+		CyGoose Goose = new CyGoose(Dialog, GaggleBoss);
+		Goose.setNetworkId(Network.getIdentifier());
+		Goose.setName( this.createGooseName(Network) );
+		this.registerGoose(Goose);
+		return Goose;
+		}
+
+	/*
+	 * Creates a standard goose name for any network
+	 * TODO: 
+	 * Burak suggested that renaming the network to the gaggle name would be useful
+	 * for some reason Network.setTitle("title") isn't working for me though
+	 * Ideally geese could be assigned ids rather than id'd by their name, this would make it easier 
+	 * but that's up to gaggle
+	 */
+	private String createGooseName(String Id, String Title)
+		{ return (Title + "(" + Id +")"); }
+	private String createGooseName(CyNetwork Network)
+		{ return createGooseName(Network.getIdentifier(), Network.getTitle()); }
+	
+	/*
+	 * Updates the list of network geese in the boss by adding new networks as gees
+	 * and removing destroyed networks.
+	 * TODO: If 2 sessions of cytoscape were open and one gets closed it's geese do not go away WHY?
+	 */
+//	private void updateNetworkGeese()
+//		{
+//		Iterator<CyNetwork> NetIter = Cytoscape.getNetworkSet().iterator();
+//		Set<String> CurrentNetIds = new HashSet<String>();
+//
+//		// this should be adding each network as a potential "goose" in the list
+//		while (NetIter.hasNext())
+//			{
+//			CyNetwork CurrentNet = NetIter.next();
+//			CurrentNetIds.add(CurrentNet.getIdentifier());
+//
+//			// if the network is already in the hash it should be in the list of geese
+//			if (NetworkGeese.containsKey(CurrentNet.getIdentifier())) continue;
+//			else
+//				{
+//				Goose NewGoose = this.createNewGoose(CurrentNet);
+//				NetworkGeese.put(CurrentNet.getIdentifier(), NewGoose);
+//				}
+//			}
+//		this.removeOldNetworks(CurrentNetIds);
+//		MiscUtil.updateGooseChooser(GaggleBoss, Dialog.getGooseBox(), null, null);
+//		}
+//
+//	/* 
+//	 * Remove dead geese from the boss
+//	 * This will only remove networks that have been completely deleted, a network with no view is still a goose
+//	 */
+//	private void removeOldNetworks(Set<String> AllCurrentNetIds)
+//		{
+//		Iterator<String> NetGeeseIter = NetworkGeese.keySet().iterator();
+//		
+//		Collection<String> DeadIds = new ArrayList<String>();
+//		while (NetGeeseIter.hasNext())
+//			{
+//			String Id = NetGeeseIter.next();
+//			// null network = 0, should never try to remove it
+//			if ( AllCurrentNetIds.contains(Id) || Id.equals("0")) continue; 
+//			else DeadIds.add(Id); 
+//			}
+//
+//		Iterator<String> deadIter = DeadIds.iterator();
+//		while(deadIter.hasNext())
+//			{ 
+//			String Id = deadIter.next();
+//			try 
+//				{ 
+//				print("Removing " + NetworkGeese.get(Id).getName() + " from the boss");
+//				GaggleBoss.remove( NetworkGeese.get(Id).getName() ); 
+//				UnicastRemoteObject.unexportObject(NetworkGeese.get(Id), true);
+//				Dialog.getGooseBox().removeItem( NetworkGeese.get(Id).getName() );
+//				}
+//			catch (Exception E) { E.printStackTrace(); }
+//			NetworkGeese.remove(Id); 
+//			}
+//		}
+
+
+	public static void showDialogBox(String message, String title, int msgType)
+		{ JOptionPane.showMessageDialog(Cytoscape.getDesktop(), message, title, msgType); }
+
+	}
