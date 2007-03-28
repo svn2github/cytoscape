44f66274fe41755e6bc0fb42ad3b1010220bae50
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-28 19:18:37 +0000

    Change the attributes in author table
    git-svn-id: http://chianti.ucsd.edu/svn@9813 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/resources/plugin_website/CyPluginDB_schema.sql
+++ cytoscape/trunk/resources/plugin_website/CyPluginDB_schema.sql
@@ -35,8 +35,8 @@ create table plugin_list (
 create table plugin_version (
 	version_auto_id	int not null primary key auto_increment,
     plugin_id       int,
-	version			varchar(20),
-	os				set('windows','unix','mac','other'),
+    plugin_file_id	int,
+	version			varchar(20) default 'unknown',
 	release_date	Date,
 	release_note	text,
 	release_note_url varchar(100),	
@@ -57,16 +57,10 @@ create table plugin_files (
 
 create table authors (
 	author_auto_id	int not null primary key auto_increment,
-    last_name      	varchar(50),
-    first_name      varchar(50),
-    middle_name		varchar(50),
-    lab				varchar(100),
-    lab_url		varchar(100),
-    dept			varchar(100),
-    dept_url		varchar(100),    
-	institution		varchar(200),
-    institution_url	varchar(100),
-    email      		varchar(50)	
+    names      		varchar(150),
+    email      		varchar(90),
+    affiliation		varchar(150),
+    affiliationURL	varchar(200)
 );
 
 create table plugin_author (
@@ -83,7 +77,7 @@ create table usagelog (
 );
 
 grant select, insert on cyplugindb.* to cytouser identified by 'cytouser';
-grant all on cyplugindb.* to cytoadmin identified by 'cytoadmin';
+grant all on cyplugindb.* to cytoastaff identified by 'cytostaff';
 
 insert into categories (category_id, name, description) 
             values (1, 'Analysis Plugins', 'Used for analyzing existing networks');
@@ -95,34 +89,3 @@ insert into categories (category_id,name, description)
             values (4, 'Functional Enrichment Plugins', 'Used for functional enrichment of networks');
 insert into categories (category_id,name, description) 
             values (5, 'Communication/Scripting Plugins', 'Used for communicating with or scripting Cytoscape');
-
--- plugin_list
-insert into plugin_list (name, description, project_url, category_id) 
-            values ('Cerebral Plugin', 'Cerebral (Cell Region-Based Rendering And Layout) is an open-source Java plugin for the Cytoscape biomolecular interaction viewer. Given an interaction network and subcellular localization annotation, Cerebral automatically generates a view of the network in the style of traditional pathway diagrams, providing an intuitive interface for the exploration of a biological pathway or system. The molecules are separated into layers according to their subcellular localization. Potential products or outcomes of the pathway can be shown at the bottom of the view, clustered according to any molecular attribute data - protein function - for example. Cerebral scales well to networks containing thousands of nodes.',
-            'http://www.pathogenomics.ca/cerebral/', 1);
-insert into plugin_list (name, description, project_url, category_id, license_brief) 
-            values ('Hyperbolic Focus', 'Hyperbolic Focus Layout plugin. This plug-in was created by Robert Ikeda, who was supported (partially) by the PRIME Program funded by NSF (OISE 0407508) and Calit2',
-            null, 1,'Licensed under GNU Public License');
-
--- plugin_version          
-insert into plugin_version (plugin_id, version, os, release_date,release_note, release_note_url,comment,source_url,jar_url, cy_version)
-            values (1,'1.0', 'windows,mac', '2007-02-20',null,'http://www.pathogenomics.ca/cerebral/','not tested on 2.0','http://www.source.com','http://jar_url.edu/','2.3,2.4');
-insert into plugin_version (plugin_id, version, os, release_date,release_note, release_note_url,comment,source_url, cy_version)
-            values (2,'1.0', null, null,null,null,null,null,'2.4');
-          
--- authors  
-insert into authors (last_name, first_name, middle_name, lab, lab_url, dept, dept_url, institution, institution_url)
-            values ('Gardy','Jennifer', null, null,null, null, null,'University of British Columbia', 'http://ubc.eu');            
-insert into authors (last_name, first_name, middle_name, lab, lab_url, dept, dept_url, institution, institution_url)
-            values ('Barsky','Aaron', null, null,null, null, null,'University of British Columbia', 'http://ubc.eu');
-insert into authors (last_name, first_name, middle_name, lab, lab_url, dept, dept_url, institution, institution_url)
-            values (null,null, null, 'Ideker Lab','http://chianti.ucsd.edu/idekerlab/', null, null,'UCSD', null);
-            
--- plugin_author  
-insert into plugin_author (plugin_version_id, author_id, authorship_seq)
-            values (1,1,1);
-insert into plugin_author (plugin_version_id, author_id, authorship_seq)
-            values (1,2,2);
-insert into plugin_author (plugin_version_id, author_id, authorship_seq)
-            values (2,3,0);
-            
