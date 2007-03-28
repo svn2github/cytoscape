9fc72c5f00d039f49429d8b3590ef2810bb30a9e
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-28 21:23:38 +0000

    updated nature.protocols
    git-svn-id: http://chianti.ucsd.edu/svn@9814 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/nature.protocols/index.php
+++ cyto_web/trunk/nature.protocols/index.php
@@ -24,18 +24,33 @@
 <? include "../nav.php"; ?>
 <div id="contents">
 	<div id="content">
-			<P>This page contains a link to the webstart that contains all of the plugins
-			necessary to properly run the protocol described in this paper: (ref here).</p>
+            <P>This page contains a link to the webstart that contains all of the plugins
+            necessary to properly run the protocol described in this paper: (ref here).</p>
 
-			<p>
-			Click <A HREF="runCytoscape.jnlp">here</A> for the WebStart.
-			</p>
-			<p>
-			For more information on Java Webstart, click <A HREF="http://java.sun.com/products/javawebstart/">here</A>.	
-			</p>
-			
-			<p>To get the required data and configuation files for the Agilent Literature Search software
-			click <A HREF="../../download_agilent_literature_search_v2.4_data.php">here</A>
+            <p>
+            Click <A HREF="runCytoscape.jnlp">here</A> for the WebStart.
+            </p>
+            <p>
+            For more information on Java Webstart, click <A HREF="http://java.sun.com/products/javawebstart/">here</A>.
+            </p>
+
+            <p>To get the required data and configuation files for the Agilent Literature Search software
+            click <A HREF="../../download_agilent_literature_search_v2.4_data.php">here</A>
+
+            <p>Two supplemental data files are provided for the convenience of any readers who have no
+            data available, or do not wish to execute the complete protocol:
+            <ol>
+            <li> <A HREF="galExpData.pvals">galExpData.pvals</A> is a sample expression data file
+            <li> <A HREF="gal1rg.top50.cpath.xgmml">gal1rg.top50.cpath.xgmml</A> contains a network obtained by
+            searching cPath with the top 50 genes, sorted by p-value, from the gal1RG experiment in the expression data file.
+            Users can import this network into Cytoscape instead of searching cPath.
+            <p>
+            Note that the number of interactions in cPath increases over time.  Users who attempt to re-generate this
+            network at a later date might not get exactly the same network, but a larger one.
+            </ol>
+            All the figures shown in the paper were generated with these data files.
+            <p>
+
 	</div>
 </div>
 
