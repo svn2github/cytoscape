aa95dc391144478720824c6e108c61730a27efd6
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-29 05:10:09 +0000

    Update to use newest version of PluginManager
    git-svn-id: http://chianti.ucsd.edu/svn@9815 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/PluginManagerAction.java
+++ cytoscape/trunk/src/cytoscape/actions/PluginManagerAction.java
@@ -6,23 +6,23 @@ package cytoscape.actions;
 import cytoscape.Cytoscape;
 
 import cytoscape.dialogs.PluginManageDialog;
-import cytoscape.dialogs.PluginManageDialog.PluginStatus;
 
-import cytoscape.plugin.PluginInfo;
-import cytoscape.plugin.PluginManager;
-import cytoscape.plugin.ManagerError;
+import cytoscape.dialogs.PluginManageDialog.PluginInstallStatus;
+
+import cytoscape.plugin.*;
 
 import cytoscape.util.CytoscapeAction;
 
+import java.awt.event.ActionEvent;
+
 import java.util.List;
 import java.util.Map;
-import java.awt.event.ActionEvent;
 
 import javax.swing.JOptionPane;
 
+
 /**
  * @author skillcoy
- *
  */
 public class PluginManagerAction extends CytoscapeAction {
 	/**
@@ -34,9 +34,10 @@ public class PluginManagerAction extends CytoscapeAction {
 	}
 
 	/**
-	 *  DOCUMENT ME!
+	 * DOCUMENT ME!
 	 *
-	 * @param e DOCUMENT ME!
+	 * @param e
+	 *          DOCUMENT ME!
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -47,24 +48,28 @@ public class PluginManagerAction extends CytoscapeAction {
 		PluginManageDialog dialog = new PluginManageDialog(Cytoscape.getDesktop());
 		PluginManager Mgr = PluginManager.getPluginManager();
 
-		Map<String, List<PluginInfo>> InstalledInfo = 
-			Mgr.getPluginsByCategory(Mgr.getInstalledPlugins());
+		List<PluginInfo> Current = Mgr.getPlugins(PluginTracker.PluginStatus.CURRENT);
+		Map<String, List<PluginInfo>> InstalledInfo = ManagerUtil.sortByCategory(Current);
+
 		try {
-			Map<String, List<PluginInfo>> DownloadInfo = Mgr.getPluginsByCategory(Mgr.inquire());
-	
+			Map<String, List<PluginInfo>> DownloadInfo = ManagerUtil.sortByCategory(Mgr.inquire());
+
 			for (String Category : DownloadInfo.keySet()) {
-				dialog.addCategory(Category, DownloadInfo.get(Category), PluginStatus.AVAILABLE);
+				dialog.addCategory(Category, DownloadInfo.get(Category),
+				                   PluginInstallStatus.AVAILABLE);
 			}
+
 			dialog.setSiteName("Cytoscape");
 		} catch (ManagerError E) {
-				JOptionPane.showMessageDialog(Cytoscape.getDesktop(), 
-						E.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-				E.printStackTrace();
-		} 
-		
-		for (String Category : InstalledInfo.keySet()) {
-			dialog.addCategory(Category, InstalledInfo.get(Category), PluginStatus.INSTALLED);
+			JOptionPane.showMessageDialog(Cytoscape.getDesktop(), E.getMessage(), "Error",
+			                              JOptionPane.ERROR_MESSAGE);
+			E.printStackTrace();
 		}
+
+		for (String Category : InstalledInfo.keySet()) {
+			dialog.addCategory(Category, InstalledInfo.get(Category), PluginInstallStatus.INSTALLED);
+		}
+
 		dialog.setVisible(true);
 	}
 }
