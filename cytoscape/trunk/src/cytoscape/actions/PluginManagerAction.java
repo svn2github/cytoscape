7d8bc143538b9225bf3ba2386bb105fc72af5e8d
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-03-29 23:27:11 +0000

    Updates made for using the new PluginManager
    git-svn-id: http://chianti.ucsd.edu/svn@9828 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/actions/PluginManagerAction.java
+++ cytoscape/trunk/src/cytoscape/actions/PluginManagerAction.java
@@ -1,15 +1,59 @@
-/**
- *
- */
+/*
+  File: PluginManagerAction.java
+
+  Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+
+  The Cytoscape Consortium is:
+  - Institute for Systems Biology
+  - University of California San Diego
+  - Memorial Sloan-Kettering Cancer Center
+  - Institut Pasteur
+  - Agilent Technologies
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+// EdgeControlDialog
+
+//---------------------------------------------------------------------------------------
+// $Revision: 9565 $
+// $Date: 2007-02-13 11:36:50 -0800 (Tue, 13 Feb 2007) $
+// $Author: mes $
+//---------------------------------------------------------------------------------------
 package cytoscape.actions;
 
 import cytoscape.Cytoscape;
 
 import cytoscape.dialogs.PluginManageDialog;
-
 import cytoscape.dialogs.PluginManageDialog.PluginInstallStatus;
 
-import cytoscape.plugin.*;
+import cytoscape.plugin.PluginManager;
+import cytoscape.plugin.PluginInfo;
+import cytoscape.plugin.ManagerUtil;
+import cytoscape.plugin.ManagerError;
+import cytoscape.plugin.PluginTracker.PluginStatus;
 
 import cytoscape.util.CytoscapeAction;
 
@@ -21,23 +65,17 @@ import java.util.Map;
 import javax.swing.JOptionPane;
 
 
-/**
- * @author skillcoy
- */
 public class PluginManagerAction extends CytoscapeAction {
-	/**
-	 *
-	 */
 	public PluginManagerAction() {
 		super("Manage Plugins");
 		setPreferredMenu("Plugins");
 	}
 
 	/**
-	 * DOCUMENT ME!
+	 * Gets the lists of plugins and creates the ManagerDialog for users.
 	 *
 	 * @param e
-	 *          DOCUMENT ME!
+	 *   
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -48,7 +86,7 @@ public class PluginManagerAction extends CytoscapeAction {
 		PluginManageDialog dialog = new PluginManageDialog(Cytoscape.getDesktop());
 		PluginManager Mgr = PluginManager.getPluginManager();
 
-		List<PluginInfo> Current = Mgr.getPlugins(PluginTracker.PluginStatus.CURRENT);
+		List<PluginInfo> Current = Mgr.getPlugins(PluginStatus.CURRENT);
 		Map<String, List<PluginInfo>> InstalledInfo = ManagerUtil.sortByCategory(Current);
 
 		try {
---
+++ cytoscape/trunk/src/cytoscape/actions/PluginUpdateAction.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package cytoscape.actions;
+
+import java.awt.event.ActionEvent;
+
+import cytoscape.Cytoscape;
+
+import cytoscape.util.CytoscapeAction;
+
+import cytoscape.dialogs.PluginUpdateDialog;
+
+import cytoscape.plugin.PluginInfo;
+import cytoscape.plugin.PluginManager;
+import cytoscape.plugin.ManagerError;
+// import cytoscape.plugin.ManagerUtil;
+import cytoscape.plugin.PluginTracker.PluginStatus;
+
+// import java.util.Map;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class PluginUpdateAction extends CytoscapeAction {
+	public PluginUpdateAction() {
+		super("Update Plugins");
+		setPreferredMenu("Plugins");
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		PluginUpdateDialog Dialog = new PluginUpdateDialog(Cytoscape
+				.getDesktop());
+
+		try {
+			boolean updateFound = false;
+			PluginManager Mgr = PluginManager.getPluginManager();
+			// Find updates
+			for (PluginInfo Current : Mgr.getPlugins(PluginStatus.CURRENT)) {
+				List<PluginInfo> Updates = Mgr.findUpdates(Current);
+				if (Updates.size() > 0) {
+					Dialog.addCategory(Current.getCategory(), Current, Updates);
+					updateFound = true;
+				}
+			}
+			if (updateFound) {
+				Dialog.setVisible(true);
+			} else {
+				JOptionPane.showMessageDialog(Cytoscape.getDesktop(),
+						"No updates avaialbe for currently installed plugins.",
+						"Plugin Updates", JOptionPane.INFORMATION_MESSAGE);
+			}
+		} catch (ManagerError E) {
+			JOptionPane.showMessageDialog(Cytoscape.getDesktop(), E
+					.getMessage(), "Plugin Update Error",
+					JOptionPane.ERROR_MESSAGE);
+			E.printStackTrace();
+		}
+	}
+
+}
