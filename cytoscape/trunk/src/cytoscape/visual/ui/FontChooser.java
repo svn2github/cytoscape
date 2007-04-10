5eeb177cb19f11ea37b23960e00f22dcc1d0e106
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-04-10 22:36:38 +0000

    Title added for the font chooser dialog.
    git-svn-id: http://chianti.ucsd.edu/svn@9930 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/visual/ui/FontChooser.java
+++ cytoscape/trunk/src/cytoscape/visual/ui/FontChooser.java
@@ -44,12 +44,14 @@ package cytoscape.visual.ui;
 
 
 //--------------------------------------------------------------------------
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.font.*;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-import javax.swing.*;
-import javax.swing.event.*;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
 
 
 //--------------------------------------------------------------------------
@@ -58,99 +60,98 @@ import javax.swing.event.*;
  * components to display font face selection.
  */
 public class FontChooser extends JPanel {
-	private Font selectedFont;
-	protected DefaultComboBoxModel fontFaceModel;
-	protected JComboBox face;
-	protected static float displaySize = 12F;
-	protected static Font[] scaledFonts;
-	protected static Font defFont = new Font("dialog", Font.PLAIN, 1);
+    private Font selectedFont;
+    protected DefaultComboBoxModel fontFaceModel;
+    protected JComboBox face;
+    protected static final float DEF_SIZE = 12F;
+    protected static Font[] scaledFonts;
+    protected static final Font DEF_FONT = new Font("SansSerif", Font.PLAIN, 1);
 
-	static {
-		scaledFonts = scaleFonts(GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts(),
-		                         displaySize);
-	}
+    static {
+        scaledFonts = scaleFonts(
+                GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts(),
+                DEF_SIZE);
+    }
 
-	/**
-	 * Create a FontChooser to choose between all fonts available on the system.
-	 */
-	public FontChooser() {
-		this(null);
-	}
+    /**
+     * Create a FontChooser to choose between all fonts available on the system.
+     */
+    public FontChooser() {
+        this(null);
+    }
 
-	/**
-	 * Creates a new FontChooser object.
-	 *
-	 * @param def  DOCUMENT ME!
-	 */
-	public FontChooser(Font def) {
-		this(scaledFonts, def);
-	}
+    /**
+     * Creates a new FontChooser object.
+     *
+     * @param def  DOCUMENT ME!
+     */
+    public FontChooser(Font def) {
+        this(scaledFonts, def);
+    }
 
-	/**
-	 * Create a FontChooser to choose between the given array of fonts.
-	 */
-	public FontChooser(Font[] srcFonts, Font def) {
-		Font[] displayFonts = scaledFonts;
+    /**
+     * Create a FontChooser to choose between the given array of fonts.
+     */
+    public FontChooser(Font[] srcFonts, Font def) {
+        Font[] displayFonts = scaledFonts;
 
-		if (srcFonts != scaledFonts) {
-			System.out.println("scaling fonts");
-			displayFonts = scaleFonts(srcFonts, displaySize);
-		}
+        if (srcFonts != scaledFonts)
+            displayFonts = scaleFonts(srcFonts, DEF_SIZE);
 
-		this.fontFaceModel = new DefaultComboBoxModel(displayFonts);
+        this.fontFaceModel = new DefaultComboBoxModel(displayFonts);
 
-		this.face = new JComboBox(fontFaceModel);
-		face.setRenderer(new FontRenderer());
+        this.face = new JComboBox(fontFaceModel);
+        face.setRenderer(new FontRenderer());
 
-		// set the prototype display for the combo box
-		//face.setPrototypeDisplayValue("Really really long font name Bold Italic");
-		face.addItemListener(new FontFaceSelectionListener());
+        // set the prototype display for the combo box
+        face.addItemListener(new FontFaceSelectionListener());
 
-		// set the currently selected face, default if null
-		if (def == null)
-			this.selectedFont = defFont;
-		else
-			this.selectedFont = def.deriveFont(1F);
+        // set the currently selected face, default if null
+        if (def == null)
+            this.selectedFont = DEF_FONT;
+        else
+            this.selectedFont = def.deriveFont(1F);
 
-		face.setSelectedItem(this.selectedFont);
+        face.setSelectedItem(this.selectedFont);
 
-		add(face);
-	}
+        add(face);
+    }
 
-	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @return  DOCUMENT ME!
-	 */
-	public JComboBox getFaceComboBox() {
-		return face;
-	}
+    /**
+     *  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public JComboBox getFaceComboBox() {
+        return face;
+    }
 
-	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @return  DOCUMENT ME!
-	 */
-	public Font getSelectedFont() {
-		return selectedFont;
-	}
+    /**
+     *  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public Font getSelectedFont() {
+        return selectedFont;
+    }
 
-	private class FontFaceSelectionListener implements ItemListener {
-		public void itemStateChanged(ItemEvent e) {
-			if (e.getStateChange() == ItemEvent.SELECTED) {
-				JComboBox source = (JComboBox) e.getItemSelectable();
-				selectedFont = (Font) source.getSelectedItem();
-			}
-		}
-	}
+    private class FontFaceSelectionListener
+        implements ItemListener {
+        public void itemStateChanged(ItemEvent e) {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                JComboBox source = (JComboBox) e.getItemSelectable();
+                selectedFont = (Font) source.getSelectedItem();
+            }
+        }
+    }
 
-	private static Font[] scaleFonts(Font[] inFonts, float size) {
-		Font[] outFonts = new Font[inFonts.length];
-		int i = 0;
+    private static Font[] scaleFonts(Font[] inFonts, float size) {
+        Font[] outFonts = new Font[inFonts.length];
+        int i = 0;
 
-		for (Font f : inFonts)
-			outFonts[i++] = f.deriveFont(size);
+        for (Font f : inFonts)
+            outFonts[i++] = f.deriveFont(size);
 
-		return outFonts;
-	}
+        return outFonts;
+    }
 }
--- cytoscape/trunk/src/cytoscape/visual/ui/PopupFontChooser.java
+++ cytoscape/trunk/src/cytoscape/visual/ui/PopupFontChooser.java
@@ -36,128 +36,114 @@
  */
 package cytoscape.visual.ui;
 
-import cytoscape.visual.Arrow;
-import cytoscape.visual.LabelPosition;
-import cytoscape.visual.LineType;
-import cytoscape.visual.ShapeNodeRealizer;
-
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dialog;
-import java.awt.Dialog;
 import java.awt.Font;
 import java.awt.Frame;
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-
-import java.text.DecimalFormat;
-
-import java.util.HashMap;
 
 import javax.swing.AbstractAction;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JDialog;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.plaf.metal.MetalButtonUI;
 
 
 /**
  *
  */
 public class PopupFontChooser extends JDialog {
-	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @param f DOCUMENT ME!
-	 * @param begin DOCUMENT ME!
-	 *
-	 * @return  DOCUMENT ME!
-	 */
-	public static Font showDialog(Frame f, Font begin) {
-		PopupFontChooser fpc = new PopupFontChooser(f, begin);
+    /**
+     * DOCUMENT ME!
+     *
+     * @param f
+     *            DOCUMENT ME!
+     * @param begin
+     *            DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public static Font showDialog(Frame f, Font begin) {
+        final PopupFontChooser fpc = new PopupFontChooser(f, begin);
 
-		return fpc.getThisFont();
-	}
+        return fpc.getThisFont();
+    }
 
-	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @param f DOCUMENT ME!
-	 * @param begin DOCUMENT ME!
-	 *
-	 * @return  DOCUMENT ME!
-	 */
-	public static Font showDialog(Dialog f, Font begin) {
-		PopupFontChooser fpc = new PopupFontChooser(f, begin);
+    /**
+     * DOCUMENT ME!
+     *
+     * @param f
+     *            DOCUMENT ME!
+     * @param begin
+     *            DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public static Font showDialog(Dialog f, Font begin) {
+        PopupFontChooser fpc = new PopupFontChooser(f, begin);
 
-		return fpc.getThisFont();
-	}
+        return fpc.getThisFont();
+    }
 
-	private Font font;
-	private FontChooser chooser;
-	private Component parent;
+    private Font font;
+    private FontChooser chooser;
 
-	private PopupFontChooser(Frame parent, Font begin) {
-		super(parent, true);
-		init(parent, begin);
-	}
+    private PopupFontChooser(Frame parent, Font begin) {
+        super(parent, true);
+        init(parent, begin);
+    }
 
-	private PopupFontChooser(Dialog parent, Font begin) {
-		super(parent, true);
-		init(parent, begin);
-	}
+    private PopupFontChooser(Dialog parent, Font begin) {
+        super(parent, true);
+        init(parent, begin);
+    }
 
-	private void init(Component parent, Font begin) {
-		this.parent = parent;
-		font = begin;
+    private void init(Component parent, Font begin) {
+        this.setTitle("Please select a font...");
+        font = begin;
 
-		if (font == null)
-			chooser = new FontChooser();
-		else
-			chooser = new FontChooser(begin.deriveFont(1F));
+        if (font == null)
+            chooser = new FontChooser();
+        else
+            chooser = new FontChooser(begin.deriveFont(1F));
 
-		JComboBox face = chooser.getFaceComboBox();
+        // JComboBox face = chooser.getFaceComboBox();
+        JPanel butPanel = new JPanel(false);
 
-		JPanel butPanel = new JPanel(false);
+        // buttons - OK/Cancel
+        JButton okBut = new JButton("OK");
+        okBut.addActionListener(
+            new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    font = chooser.getSelectedFont()
+                                  .deriveFont(12F);
+                    dispose();
+                }
+            });
 
-		// buttons - OK/Cancel
-		JButton okBut = new JButton("OK");
-		okBut.addActionListener(new AbstractAction() {
-				public void actionPerformed(ActionEvent e) {
-					font = chooser.getSelectedFont().deriveFont(12F);
-					dispose();
-				}
-			});
+        JButton cancelBut = new JButton("Cancel");
+        cancelBut.addActionListener(
+            new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    dispose();
+                }
+            });
 
-		JButton cancelBut = new JButton("Cancel");
-		cancelBut.addActionListener(new AbstractAction() {
-				public void actionPerformed(ActionEvent e) {
-					dispose();
-				}
-			});
+        butPanel.add(okBut);
+        butPanel.add(cancelBut);
 
-		butPanel.add(okBut);
-		butPanel.add(cancelBut);
+        Container content = getContentPane();
+        content.setLayout(new BorderLayout());
 
-		Container content = getContentPane();
-		content.setLayout(new BorderLayout());
+        content.add(chooser, BorderLayout.CENTER);
+        content.add(butPanel, BorderLayout.SOUTH);
+        pack();
+        setLocationRelativeTo(parent);
+        setVisible(true);
+    }
 
-		content.add(chooser, BorderLayout.CENTER);
-		content.add(butPanel, BorderLayout.SOUTH);
-		pack();
-		setLocationRelativeTo(parent);
-		setVisible(true);
-	}
-
-	private Font getThisFont() {
-		return font;
-	}
+    private Font getThisFont() {
+        return font;
+    }
 }
