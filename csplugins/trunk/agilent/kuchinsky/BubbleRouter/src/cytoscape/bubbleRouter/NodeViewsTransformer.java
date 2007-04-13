a1961efebadc4d125fcc8a2c162c34e4aa0bafd5
akuchinsky <akuchinsky@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-04-13 11:59:07 +0000

    convert to use generics, update javadoc
    git-svn-id: http://chianti.ucsd.edu/svn@9949 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/agilent/kuchinsky/BubbleRouter/src/cytoscape/bubbleRouter/NodeViewsTransformer.java
+++ csplugins/trunk/agilent/kuchinsky/BubbleRouter/src/cytoscape/bubbleRouter/NodeViewsTransformer.java
@@ -19,16 +19,19 @@ import ding.view.DGraphView;
  */
 public class NodeViewsTransformer {
 
-	public static void transform(List nodeViews, Rectangle2D to) {
+	/**
+	 * takes a list of nodeviews and, for each NodeView, transforms its coordinates
+     * into those specifying the corresponding position in the *to* rectangle.
+	 * @param nodeViews
+	 * @param to
+	 */
+	public static void transform(List<NodeView> nodeViews, Rectangle2D to) {
 		double scaleX = 1.0d;
 		double scaleY = 1.0d;
 		double minX = Double.POSITIVE_INFINITY;
 		double minY = Double.POSITIVE_INFINITY;
 		double maxY = Double.NEGATIVE_INFINITY;
 		double maxX = Double.NEGATIVE_INFINITY;
-
-		double offsetX;
-		double offsetY;
 
 		double[] topLeft = new double[2];
 		topLeft[0] = to.getMinX();
@@ -51,11 +54,10 @@ public class NodeViewsTransformer {
 
 		double currentX;
 		double currentY;
-		// first calculate the min/max x and y for the list of *relevant*
-		// nodeviews
-		Iterator it = nodeViews.iterator();
+		// first calculate the min/max x and y for the list of *relevant* nodeviews
+		Iterator<NodeView> it = nodeViews.iterator();
 		while (it.hasNext()) {
-			NodeView nv = (NodeView) it.next();
+			NodeView nv = it.next();
 			currentX = nv.getXPosition();
 			currentY = nv.getYPosition();
 			if ((currentX - (nv.getWidth() * 0.5)) < minX) {
@@ -73,8 +75,6 @@ public class NodeViewsTransformer {
 
 		}
 
-		// scaleX = (maxX - minX) / to.getWidth();
-		// scaleY = (maxY - minY) / to.getHeight();
 		scaleX = (maxX - minX) / toWidth;
 		if (scaleX == 0.0) {
 			scaleX = 1.0d;
@@ -83,44 +83,27 @@ public class NodeViewsTransformer {
 		if (scaleY == 0.0) {
 			scaleY = 1.0d;
 		}
-		// System.out.println("NodeViewsTransformer: scale factor = " + scaleX
-		// + "," + scaleY);
-		// System.out.println("NodeViewsTransformer: min/max x/y = " + minX +
-		// ","
-		// + minY + " " + maxX + "," + maxY);
-		// System.out.println("For " + nodeViews.size() + " nodes.");
 
-		// now iterate through the NodeViews and move/scale their coordinates
-		Iterator it2 = nodeViews.iterator();
-		int kount = 0;
+		Iterator<NodeView> it2 = nodeViews.iterator();
 		while (it2.hasNext()) {
-			NodeView nv = (NodeView) it2.next();
+			NodeView nv =  it2.next();
 			currentX = nv.getXPosition();
 			currentY = nv.getYPosition();
-			kount++;
-			// if ((kount % 20) == 0)
-			{
-				// System.out.println("moving node from position: " + currentX
-				// + "," + currentY);
-			}
-			// nv.setXPosition(to.getMinX() + ((currentX - minX) * scaleX));
-			// nv.setYPosition(to.getMinY() + ((currentY - minY) * scaleY));
 			nv.setXPosition(toMinX + ((currentX - minX) / scaleX));
 			nv.setYPosition(toMinY + ((currentY - minY) / scaleY));
-			// if ((kount % 20) == 0)
-			{
-				// System.out.println("to position: "
-				// + nv.getXPosition() + "," + nv.getYPosition());
-			}
 		}
 	}
 	
 	
 	// from Region: AP 1.2.07
-	// Method for selecting NodeViews within boundary of current region (for
-	// moving and resizing)
-	public static List bounded(List nodeViews, Rectangle2D from) {
-		List boundedNodeViews = new ArrayList();
+	/**
+	 * 
+	 * @param nodeViews
+	 * @param from
+	 * @return NodeViews within boundary of current region (for moving and resizing)
+	 */ 
+	public static List bounded(List<NodeView> nodeViews, Rectangle2D from) {
+		List<NodeView> boundedNodeViews = new ArrayList<NodeView>();
 		double[] topLeft2 = new double[2];
 		double fromMinX = 0; // no buffer here
 		double fromMinY = 0;
@@ -142,17 +125,13 @@ public class NodeViewsTransformer {
 			fromMaxX = bottomRight2[0]; // no buffer here
 			fromMaxY = bottomRight2[1];
 
-			// double fromWidth = fromMaxX - fromMinX;
-			// double fromHeight = fromMaxY - fromMinY;
 		}
-		// current
 		double currentX;
 		double currentY;
-		// first calculate the min/max x and y for the list of *relevant*
-		// nodeviews
-		Iterator it = nodeViews.iterator();
+		// first calculate the min/max x and y for the list of *relevant* nodeviews
+		Iterator<NodeView> it = nodeViews.iterator();
 		while (it.hasNext()) {
-			NodeView nv = (NodeView) it.next();
+			NodeView nv = it.next();
 			currentX = nv.getXPosition();
 			currentY = nv.getYPosition();
 			if ((from == null)
