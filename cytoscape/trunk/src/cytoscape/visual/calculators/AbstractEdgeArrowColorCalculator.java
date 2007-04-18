2891dd240551c8c077e35d0c6034d5272526e3c3
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-04-18 01:55:07 +0000

    New calculators for line and arrow.
    git-svn-id: http://chianti.ucsd.edu/svn@9997 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/trunk/src/cytoscape/visual/calculators/AbstractEdgeArrowColorCalculator.java
@@ -0,0 +1,69 @@
+package cytoscape.visual.calculators;
+
+import cytoscape.CyNetwork;
+
+import cytoscape.visual.EdgeAppearance;
+import cytoscape.visual.VisualPropertyType;
+
+import cytoscape.visual.mappings.ObjectMapping;
+
+import cytoscape.visual.parsers.ColorParser;
+
+import giny.model.Edge;
+
+import java.awt.Color;
+
+import java.util.Properties;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+  */
+public abstract class AbstractEdgeArrowColorCalculator extends EdgeCalculator {
+    /**
+     * Creates a new AbstractEdgeArrowColorCalculator object.
+     *
+     * @param name DOCUMENT ME!
+     * @param m DOCUMENT ME!
+     * @param type DOCUMENT ME!
+     */
+    public AbstractEdgeArrowColorCalculator(String name, ObjectMapping m,
+        VisualPropertyType type) {
+        super(name, m, Color.class, type);
+    }
+
+    /**
+     * Creates a new AbstractEdgeArrowColorCalculator object.
+     *
+     * @param name DOCUMENT ME!
+     * @param props DOCUMENT ME!
+     * @param baseKey DOCUMENT ME!
+     * @param type DOCUMENT ME!
+     */
+    public AbstractEdgeArrowColorCalculator(String name, Properties props,
+        String baseKey, VisualPropertyType type) {
+        super(name, props, baseKey, new ColorParser(), Color.black, type);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param appr DOCUMENT ME!
+     * @param edge DOCUMENT ME!
+     * @param network DOCUMENT ME!
+     */
+    public void apply(EdgeAppearance appr, Edge edge, CyNetwork network) {
+        Color c = (Color) getRangeValue(edge);
+
+        // default has already been set - no need to do anything
+        if (c == null)
+            return;
+
+        if (type == VisualPropertyType.EDGE_SRCARROW_COLOR)
+            appr.setSourceArrowColor(c);
+        else if (type == VisualPropertyType.EDGE_TGTARROW_COLOR)
+            appr.setTargetArrowColor(c);
+    }
+}
---
+++ cytoscape/trunk/src/cytoscape/visual/calculators/AbstractEdgeLineCalculator.java
@@ -0,0 +1,66 @@
+package cytoscape.visual.calculators;
+
+import cytoscape.CyNetwork;
+
+import cytoscape.visual.EdgeAppearance;
+import cytoscape.visual.Line;
+import cytoscape.visual.VisualPropertyType;
+
+import cytoscape.visual.mappings.ObjectMapping;
+
+import cytoscape.visual.parsers.LineParser;
+
+import giny.model.Edge;
+
+import java.util.Properties;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+  */
+public abstract class AbstractEdgeLineCalculator extends EdgeCalculator {
+    /**
+     * Creates a new AbstractEdgeLineCalculator object.
+     *
+     * @param name DOCUMENT ME!
+     * @param m DOCUMENT ME!
+     * @param c DOCUMENT ME!
+     * @param type DOCUMENT ME!
+     */
+    public AbstractEdgeLineCalculator(String name, ObjectMapping m, Class c,
+        VisualPropertyType type) {
+        super(name, m, c, type);
+    }
+
+    /**
+     * Creates a new AbstractEdgeLineCalculator object.
+     *
+     * @param name DOCUMENT ME!
+     * @param props DOCUMENT ME!
+     * @param baseKey DOCUMENT ME!
+     * @param type DOCUMENT ME!
+     */
+    public AbstractEdgeLineCalculator(String name, Properties props,
+        String baseKey, VisualPropertyType type) {
+        super(name, props, baseKey, new LineParser(), Line.DEFAULT_LINE, type);
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param appr DOCUMENT ME!
+     * @param edge DOCUMENT ME!
+     * @param network DOCUMENT ME!
+     */
+    public void apply(EdgeAppearance appr, Edge edge, CyNetwork network) {
+        final Line line = (Line) getRangeValue(edge);
+
+        // default has already been set - no need to do anything
+        if (line == null)
+            return;
+
+        appr.setLine(line);
+    }
+}
