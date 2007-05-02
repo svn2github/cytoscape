f68fbf7d35cad2dbfefe0d01e7494c5876409c22
grossb <grossb@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-05-02 12:34:51 +0000

    Tagging the 2.4.1 release of the 'BioPAX' project.
    git-svn-id: http://chianti.ucsd.edu/svn@10156 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ coreplugins/tags/BioPAX/2.4.1/BioPAX/src/legacy/layout/GraphLayout.java
@@ -0,0 +1,76 @@
+
+/*
+ Copyright (c) 2006, 2007, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+package legacy.layout;
+
+import legacy.GraphTopology;
+
+
+/**
+ * This class represents not just a graph's topology but also a layout of its
+ * nodes in 2D space (a &quot;straight-line graph drawing&quot;).
+ */
+public interface GraphLayout extends GraphTopology {
+	/**
+	 * Returns the maximum allowable value of X positions of nodes.
+	 * All X positions of nodes in this graph will lie in the interval
+	 * <nobr><code>[0.0, getMaxWidth()]</code></nobr>.
+	 *
+	 * @see #getNodePosition(int, boolean)
+	 */
+	public double getMaxWidth();
+
+	/**
+	 * Returns the maximum allowable value of Y positions of nodes.
+	 * All Y positions of nodes in this graph will lie in the interval
+	 * <nobr><code>[0.0, getMaxHeight()]</code></nobr>.
+	 *
+	 * @see #getNodePosition(int, boolean)
+	 */
+	public double getMaxHeight();
+
+	/**
+	 * Returns the X or Y position of a node.
+	 *
+	 * @param nodeIndex the index of node whose position we're seeking.
+	 * @param xPosition if <code>true</code>, return X position; if
+	 *                  <code>false</code>, return Y position.
+	 * @return the X or Y position of node at index <code>nodeIndex</code>.
+	 * @throws IndexOutOfBoundsException if <code>nodeIndex</code> is not
+	 *                                   in the interval <nobr><code>[0, getNumNodes() - 1]</code></nobr>.
+	 */
+	public double getNodePosition(int nodeIndex, boolean xPosition);
+}
