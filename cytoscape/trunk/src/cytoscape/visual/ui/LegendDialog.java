f9a68dc949cb20e03cd319626cc0c653f85ce685
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-05-12 01:08:30 +0000

    Made generateLegendPanel() public and static
    git-svn-id: http://chianti.ucsd.edu/svn@10249 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/visual/ui/LegendDialog.java
+++ cytoscape/trunk/src/cytoscape/visual/ui/LegendDialog.java
@@ -1,163 +1,158 @@
 /*
  * LegendDialog.java
  */
+
 package cytoscape.visual.ui;
-
-import cytoscape.visual.EdgeAppearanceCalculator;
-import cytoscape.visual.NodeAppearanceCalculator;
-import static cytoscape.visual.VisualPropertyType.NODE_HEIGHT;
-import static cytoscape.visual.VisualPropertyType.NODE_SIZE;
-import static cytoscape.visual.VisualPropertyType.NODE_WIDTH;
-
-import cytoscape.visual.VisualStyle;
-
-import cytoscape.visual.calculators.Calculator;
-
-import cytoscape.visual.mappings.ObjectMapping;
-import cytoscape.visual.mappings.PassThroughMapping;
-
-import org.freehep.util.export.ExportDialog;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dialog;
+import java.awt.Font;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.ActionEvent;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.List;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import java.awt.Dialog;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.table.TableCellRenderer;
 
+import org.freehep.util.export.ExportDialog;
+import org.jdesktop.layout.GroupLayout; 
 
-/**
- * DOCUMENT ME!
- *
- * @author $author$
-  */
+import cytoscape.visual.Arrow;
+import cytoscape.visual.LineType;
+import cytoscape.visual.VisualStyle;
+import cytoscape.visual.NodeAppearanceCalculator;
+import cytoscape.visual.EdgeAppearanceCalculator;
+import cytoscape.visual.calculators.Calculator;
+import cytoscape.visual.mappings.ObjectMapping;
+import cytoscape.visual.mappings.PassThroughMapping;
+import cytoscape.visual.mappings.DiscreteMapping;
+import cytoscape.visual.mappings.ContinuousMapping;
+
+import cytoscape.Cytoscape;
+
 public class LegendDialog extends JDialog {
-    private VisualStyle visualStyle;
-    private JPanel jPanel1;
-    private JButton jButton1;
-    private JButton jButton2;
-    private JScrollPane jScrollPane1;
-    private Component parent;
 
-    /**
-     * Creates a new LegendDialog object.
-     *
-     * @param parent DOCUMENT ME!
-     * @param vs DOCUMENT ME!
-     */
-    public LegendDialog(Dialog parent, VisualStyle vs) {
-        super(parent, true);
-        visualStyle = vs;
-        this.parent = parent;
-        initComponents();
-    }
 
-    private JPanel generateLegendPanel() {
-        JPanel legend = new JPanel();
-        legend.setLayout(new BoxLayout(legend, BoxLayout.Y_AXIS));
-        legend.setBackground(Color.white);
+	private Map legendMap;
+	private VisualStyle visualStyle;
+	private JPanel jPanel1;
+	private JButton jButton1;
+	private JButton jButton2;
+	private JScrollPane jScrollPane1;
+	private Component parent;
 
-        NodeAppearanceCalculator nac = visualStyle.getNodeAppearanceCalculator();
-        List<Calculator> calcs = nac.getCalculators();
+	public LegendDialog(Dialog parent, VisualStyle vs) {
+		super(parent,true);
+		visualStyle = vs;
+		this.parent = parent;
+		initComponents();
+	}
 
-        for (Calculator calc : calcs) {
-            // AAARGH
-            if (nac.getNodeSizeLocked()) {
-                if (calc.getType() == NODE_WIDTH.getType())
-                    continue;
-                else if (calc.getType() == NODE_HEIGHT.getType())
-                    continue;
-            } else {
-                if (calc.getType() == NODE_SIZE.getType())
-                    continue;
-            }
+	public static JPanel generateLegendPanel(VisualStyle visualStyle) {
 
-            ObjectMapping om = calc.getMapping(0);
-            JPanel mleg = om.getLegend(
-                    calc.getTypeName(),
-                    calc.getType());
+		JPanel legend = new JPanel();
+		legend.setLayout(new BoxLayout(legend, BoxLayout.Y_AXIS));
+		legend.setBackground(Color.white);
 
-            // Add passthrough mappings to the top since they don't
-            // display anything besides the title.
-            if (om instanceof PassThroughMapping)
-                legend.add(mleg, 0);
-            else
-                legend.add(mleg);
-        }
+		NodeAppearanceCalculator nac = visualStyle.getNodeAppearanceCalculator();
+		List<Calculator> calcs = nac.getCalculators();
+		for ( Calculator calc : calcs ) {
 
-        EdgeAppearanceCalculator eac = visualStyle.getEdgeAppearanceCalculator();
-        calcs = eac.getCalculators();
+			// AAARGH
+			if ( nac.getNodeSizeLocked() ) {
+				if ( calc.getType() == VizMapUI.NODE_WIDTH ) 
+					continue;
+				else if ( calc.getType() == VizMapUI.NODE_HEIGHT ) 
+					continue;
+			} else {
+				if ( calc.getType() == VizMapUI.NODE_SIZE ) 
+					continue;
+			}
 
-        int top = legend.getComponentCount();
 
-        for (Calculator calc : calcs) {
-            ObjectMapping om = calc.getMapping(0);
-            JPanel mleg = om.getLegend(
-                    calc.getTypeName(),
-                    calc.getType());
+			ObjectMapping om = calc.getMapping(0);
+			JPanel mleg = om.getLegend(calc.getTypeName(),calc.getType()); 
+			// Add passthrough mappings to the top since they don't
+			// display anything besides the title.
+			if ( om instanceof PassThroughMapping )
+				legend.add( mleg, 0 ); 
+			else
+				legend.add( mleg ); 
+		}
 
-            // Add passthrough mappings to the top since they don't
-            // display anything besides the title.
-            if (om instanceof PassThroughMapping)
-                legend.add(mleg, top);
-            else
-                legend.add(mleg);
-        }
+		EdgeAppearanceCalculator eac = visualStyle.getEdgeAppearanceCalculator();
+		calcs = eac.getCalculators();
+		int top = legend.getComponentCount(); 
+		for ( Calculator calc : calcs ) {
+			ObjectMapping om = calc.getMapping(0);
+			JPanel mleg = om.getLegend(calc.getTypeName(),calc.getType()); 
+			// Add passthrough mappings to the top since they don't
+			// display anything besides the title.
+			if ( om instanceof PassThroughMapping )
+				legend.add( mleg, top ); 
+			else
+				legend.add( mleg ); 
+		}
 
-        return legend;
-    }
+		return legend;
+	}
 
-    private void initComponents() {
-        jPanel1 = generateLegendPanel();
+	private void initComponents() {
 
-        jScrollPane1 = new JScrollPane();
-        jScrollPane1.setViewportView(jPanel1);
+		jPanel1 = generateLegendPanel(visualStyle);
 
-        jButton1 = new JButton();
-        jButton1.setText("Export");
-        jButton1.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent evt) {
-                    export();
-                }
-            });
+		jScrollPane1 = new JScrollPane();
+		jScrollPane1.setViewportView(jPanel1);
 
-        jButton2 = new JButton();
-        jButton2.setText("Cancel");
-        jButton2.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent evt) {
-                    dispose();
-                }
-            });
+		jButton1 = new JButton();
+		jButton1.setText("Export");
+		jButton1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				export();
+			}
+		});
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add(jButton1);
-        buttonPanel.add(jButton2);
+		jButton2 = new JButton();
+		jButton2.setText("Cancel");
+		jButton2.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent evt) {
+				dispose();	
+			}
+		});
 
-        JPanel containerPanel = new JPanel();
-        containerPanel.setLayout(
-            new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
-        containerPanel.add(jScrollPane1);
-        containerPanel.add(buttonPanel);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(jButton1);
+		buttonPanel.add(jButton2);
+	
+		JPanel containerPanel = new JPanel();
+		containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
+		containerPanel.add(jScrollPane1);
+		containerPanel.add(buttonPanel);
+		
+		setContentPane(containerPanel);
+		setPreferredSize(new Dimension(400,400));
 
-        setContentPane(containerPanel);
-        setPreferredSize(new Dimension(400, 400));
+		pack();
+	}
 
-        pack();
-    }
-
-    private void export() {
-        ExportDialog export = new ExportDialog();
-        export.showExportDialog(parent, "Export legend as ...", jPanel1,
-            "export");
-    }
+	private void export() {
+		ExportDialog export = new ExportDialog();
+		export.showExportDialog(parent, "Export legend as ...",
+			jPanel1, "export");
+	}
 }
