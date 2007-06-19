6075f49b0141a259b06faf126ca423a9d768a0ce
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-06-19 17:44:51 +0000

    Fixed bug where the entire legend would not be drawn.
    git-svn-id: http://chianti.ucsd.edu/svn@10519 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/slotia/SessionForWebPlugin/src/SessionForWebPlugin/VisualStyleToImage.java
+++ csplugins/trunk/ucsd/slotia/SessionForWebPlugin/src/SessionForWebPlugin/VisualStyleToImage.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 import javax.swing.JDialog;
+import javax.swing.JScrollPane;
 
 import cytoscape.visual.VisualStyle;
 import cytoscape.visual.ui.LegendDialog;
@@ -59,16 +60,25 @@ public class VisualStyleToImage
 				legend.add( mleg ); 
 		}
 
+		//
 		// What the hell is this?
 		//
-		// For some reason, legend's dimensions remain 0 by 0.
+		// A JPanel and its components remain at size (0,0)
+		// if it is not placed within a JFrame or JDialog.
 		// Inserting it into a JDialog is the only way
 		// that it will be properly sized. If someone knows
 		// a better way to do this, go ahead and fix it.
+		// Moreover, the legend panel is placed in a
+		// JScrollPane to ensure the entire legend
+		// is converted.
 		//
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(legend);
 		JDialog dialog = new JDialog();
-		dialog.setContentPane(legend);
+		dialog.setContentPane(scrollPane);
 		dialog.setPreferredSize(new Dimension(400,400));
+		System.out.println(dialog.getSize());
+		System.out.println(dialog.getMaximumSize());
 		dialog.pack();
 
 		BufferedImage image = new BufferedImage(legend.getWidth(),
