384a873fdb109c96d3854aa372d2c3350853d293
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-06-20 22:20:37 +0000

    Schema changes, added cytoscape_version table and updated plugin_version table to use it.
    git-svn-id: http://chianti.ucsd.edu/svn@10536 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/resources/plugin_website/CyPluginDB_schema.sql
+++ cytoscape/trunk/resources/plugin_website/CyPluginDB_schema.sql
@@ -2,18 +2,10 @@
     Database schema for CyPluginDB    
 */
 
--- drop database cyplugindb;
--- create database cyplugindb;
+drop database if exists cyplugindb;
+create database cyplugindb;
 
 use cyplugindb; 
-
-drop table if exists categories;
-drop table if exists plugin_list;
-drop table if exists plugin_version;
-drop table if exists authors;
-drop table if exists plugin_files;
-drop table if exists plugin_author;
-drop table if exists usagelog;
 
 create table categories (
 	category_id	int not null primary key,
@@ -23,7 +15,7 @@ create table categories (
 
 create table plugin_list (
 	plugin_auto_id	int not null primary key auto_increment,
-    name     		varchar(100) not null,
+  name     		varchar(100) not null,
 	description		text,
 	license			text,
 	license_required	varchar(3),	
@@ -36,24 +28,29 @@ create table plugin_version (
 	version_auto_id	int not null primary key auto_increment,
     plugin_id       int,
     plugin_file_id	int,
-	version			varchar(20) default 'unknown',
-	release_date	Date,
-	release_note	text,
-	release_note_url varchar(100),	
-	comment			text,
-	jar_url			varchar(100),
-	source_url		varchar(100),	
-	cy_version		set('2.0','2.1','2.2','2.3','2.4','2.5'),
-	status			varchar(20),
-	reference		text,
+		cytoscape_version_id	int not null,	
+		version			double default 1.0,
+		release_date	Date,
+		release_note	text,
+		release_note_url varchar(100),	
+		comment			text,
+		jar_url			varchar(100),
+		source_url		varchar(100),
+		status			varchar(20),
+		reference		text,
     sysdat          Date
+);
+
+create table cytoscape_version (
+	cytoscape_version_auto_id int primary key auto_increment,
+	cytoscape_version 	varchar(50)
 );
 
 create table plugin_files (
 	plugin_file_auto_id	int not null primary key auto_increment,
 	file_data		mediumblob, -- up to 16M
-	file_type		varchar(50),
-	file_name 		varchar(50)
+	file_type		enum('jar', 'zip'),
+	file_name 	varchar(100),
 );
 
 create table authors (
@@ -80,18 +77,15 @@ create table usagelog (
 grant select, insert, update on cyplugindb.* to cytouser identified by 'cytouser';
 grant select, insert, update, delete on cyplugindb.* to cytostaff identified by 'cytostaff';
 
+insert into cytoscape_version (cytoscape_version)
+	values('2.0'), ('2.1'), ('2.2'), ('2.3'), ('2.4'), ('2.5');
+
 insert into categories (category_id, name, description) 
-            values (1, 'Core Plugins', 'Cytoscape core');
-insert into categories (category_id, name, description) 
-            values (2, 'Analysis Plugins', 'Used for analyzing existing networks');
-insert into categories (category_id,name, description)
-            values (3, 'Network and Attribute I/O Plugins', 'Used for importing networks and attributes in different file formats');
-insert into categories (category_id,name, description) 
-            values (4, 'Network Inference Plugins', 'Used for inferring new networks');
-insert into categories (category_id,name, description) 
-            values (5, 'Functional Enrichment Plugins', 'Used for functional enrichment of networks');
-insert into categories (category_id,name, description) 
-            values (6, 'Communication/Scripting Plugins', 'Used for communicating with or scripting Cytoscape');
-insert into categories (category_id,name, description) 
-            values (7, 'Other Plugins', 'None of the above');
+	values (1, 'Core Plugins', 'Cytoscape core'),
+         (2, 'Analysis Plugins', 'Used for analyzing existing networks'),
+         (3, 'Network and Attribute I/O Plugins', 'Used for importing networks and attributes in different file formats'),
+         (4, 'Network Inference Plugins', 'Used for inferring new networks'),
+         (5, 'Functional Enrichment Plugins', 'Used for functional enrichment of networks'),
+         (6, 'Communication/Scripting Plugins', 'Used for communicating with or scripting Cytoscape'),
+         (7, 'Other Plugins', 'None of the above');
             
