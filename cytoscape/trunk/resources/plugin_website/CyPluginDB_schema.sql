3a303815647f2b662101428f023d7afcffeba29f
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-06-21 15:35:19 +0000

    switched back to original way of dealing with cytoscape versions per Peng
    git-svn-id: http://chianti.ucsd.edu/svn@10548 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/resources/plugin_website/CyPluginDB_schema.sql
+++ cytoscape/trunk/resources/plugin_website/CyPluginDB_schema.sql
@@ -1,91 +1,89 @@
-/*
-    Database schema for CyPluginDB    
-*/
+DROP DATABASE IF EXISTS cyplugindb;
+CREATE DATABASE cyplugindb;
 
-drop database if exists cyplugindb;
-create database cyplugindb;
+USE cyplugindb;
 
-use cyplugindb; 
-
-create table categories (
-	category_id	int not null primary key,
-    name      		varchar(50) not null,
-    description     varchar(100)    
+CREATE TABLE authors (
+  author_auto_id int(11) NOT NULL auto_increment,
+  names varchar(150) default NULL,
+  email varchar(90) default NULL,
+  affiliation varchar(150) default NULL,
+  affiliationURL varchar(200) default NULL,
+  PRIMARY KEY  (author_auto_id)
 );
 
-create table plugin_list (
-	plugin_auto_id	int not null primary key auto_increment,
-  name     		varchar(100) not null,
-	description		text,
-	license			text,
-	license_required	varchar(3),	
-	project_url		varchar(100),
-	category_id		int,
-    sysdat          Date
+
+
+CREATE TABLE categories (
+  category_id int(11) NOT NULL,
+  name varchar(50) NOT NULL,
+  description varchar(100) default NULL,
+  PRIMARY KEY  (category_id)
 );
 
-create table plugin_version (
-	version_auto_id	int not null primary key auto_increment,
-    plugin_id       int,
-    plugin_file_id	int,
-		cytoscape_version_id	int not null,	
-		version			double default 1.0,
-		release_date	Date,
-		release_note	text,
-		release_note_url varchar(100),	
-		comment			text,
-		jar_url			varchar(100),
-		source_url		varchar(100),
-		status			varchar(20),
-		reference		text,
-    sysdat          Date
+INSERT INTO categories VALUES 
+(1,'Core Plugins','Cytoscape core'),
+(2,'Analysis Plugins','Used for analyzing existing networks'),
+(3,'Network and Attribute I/O Plugins','Used for importing networks and attributes in different file formats'),
+(4,'Network Inference Plugins','Used for inferring new networks'),
+(5,'Functional Enrichment Plugins','Used for functional enrichment of networks'),
+(6,'Communication/Scripting Plugins','Used for communicating with or scripting Cytoscape'),
+(7,'Other Plugins','None of the above');
+
+
+CREATE TABLE plugin_author (
+  plugin_version_id int(11) default NULL,
+  author_id int(11) default NULL,
+  authorship_seq int(11) default NULL
 );
 
-create table cytoscape_version (
-	cytoscape_version_auto_id int primary key auto_increment,
-	cytoscape_version 	varchar(50)
+
+CREATE TABLE plugin_files (
+  plugin_file_auto_id int(11) NOT NULL auto_increment,
+  file_data mediumblob,
+  file_type enum('jar','zip') default NULL,
+  file_name varchar(100) default NULL,
+  PRIMARY KEY  (plugin_file_auto_id)
 );
 
-create table plugin_files (
-	plugin_file_auto_id	int not null primary key auto_increment,
-	file_data		mediumblob, -- up to 16M
-	file_type		enum('jar', 'zip'),
-	file_name 	varchar(100),
+
+CREATE TABLE plugin_list (
+  plugin_auto_id int(11) NOT NULL auto_increment,
+  name varchar(100) NOT NULL,
+  description text,
+  license text,
+  license_required varchar(3) default NULL,
+  project_url varchar(100) default NULL,
+  category_id int(11) default NULL,
+  sysdat date default NULL,
+  PRIMARY KEY  (plugin_auto_id)
 );
 
-create table authors (
-	author_auto_id	int not null primary key auto_increment,
-    names      		varchar(150),
-    email      		varchar(90),
-    affiliation		varchar(150),
-    affiliationURL	varchar(200)
+
+CREATE TABLE plugin_version (
+  version_auto_id int(11) NOT NULL auto_increment,
+  plugin_id int(11) default NULL,
+  plugin_file_id int(11) default NULL,
+  cy_version set('2.0','2.1','2.2','2.3','2.4','2.5') default NULL,
+  version double default '0.1',
+  release_date date default NULL,
+  release_note text,
+  release_note_url varchar(100) default NULL,
+  comment text,
+  jar_url varchar(100) default NULL,
+  source_url varchar(100) default NULL,
+  status varchar(20) default NULL,
+  reference text,
+  sysdat date default NULL,
+  PRIMARY KEY  (version_auto_id)
 );
 
-create table plugin_author (
-	plugin_version_id	int,
-	author_id		int,
-	authorship_seq	int
-);
 
-create table usagelog (
-	plugin_version_id	int not null primary key,
-	remote_host 	varchar(60),
-	ip_address		varchar(20),
-    sysdat          Date
-);
+CREATE TABLE usagelog (
+  plugin_version_id int(11) NOT NULL,
+  remote_host varchar(60) default NULL,
+  ip_address varchar(20) default NULL,
+  sysdat date default NULL,
+  PRIMARY KEY  (plugin_version_id)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-grant select, insert, update on cyplugindb.* to cytouser identified by 'cytouser';
-grant select, insert, update, delete on cyplugindb.* to cytostaff identified by 'cytostaff';
-
-insert into cytoscape_version (cytoscape_version)
-	values('2.0'), ('2.1'), ('2.2'), ('2.3'), ('2.4'), ('2.5');
-
-insert into categories (category_id, name, description) 
-	values (1, 'Core Plugins', 'Cytoscape core'),
-         (2, 'Analysis Plugins', 'Used for analyzing existing networks'),
-         (3, 'Network and Attribute I/O Plugins', 'Used for importing networks and attributes in different file formats'),
-         (4, 'Network Inference Plugins', 'Used for inferring new networks'),
-         (5, 'Functional Enrichment Plugins', 'Used for functional enrichment of networks'),
-         (6, 'Communication/Scripting Plugins', 'Used for communicating with or scripting Cytoscape'),
-         (7, 'Other Plugins', 'None of the above');
-            
