128a1f9258992900dc627ce3fa11f9fef5de1133
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-06-22 17:44:06 +0000

    reverted the previous change that was made by mistake
    git-svn-id: http://chianti.ucsd.edu/svn@10593 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/QuickFind/src/csplugins/test/quickfind/test/TaskMonitorBase.java
+++ coreplugins/trunk/QuickFind/src/csplugins/test/quickfind/test/TaskMonitorBase.java
@@ -34,7 +34,7 @@
  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
 
-package cytoscape.quickfind.test.quickfind.test;
+package csplugins.test.quickfind.test;
 
 import cytoscape.task.TaskMonitor;
 
