63f4862e8e2f137114c17a8a9ce47a289dd10218
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-05 10:48:23 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@10827 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/kono/rucy/testScripts/cytoscape_scripting_test1.rb
+++ csplugins/trunk/ucsd/kono/rucy/testScripts/cytoscape_scripting_test1.rb
@@ -1,5 +1,4 @@
 require 'java'
-puts "============= Running Ruby Script for Cytoscape ================" 
 
 include_class 'java.util.HashMap'
 include_class 'cytoscape.Cytoscape'
@@ -9,29 +8,29 @@ include_class 'cytoscape.CytoscapeInit'
 include_class 'java.awt.Color'
 include_class 'cytoscape.visual.VisualStyle'
 include_class 'cytoscape.visual.VisualPropertyType'
-
+include_class 'java.awt.Frame'
 
 props = CytoscapeInit.getProperties
 props.setProperty("layout.default", "force-directed")
 puts "Apply Layout Algorithm: " + props.getProperty("layout.default");
 
+colorProp = VisualPropertyType.values[0]
+edgeColorProp = colorProp = VisualPropertyType.values[13]
+edgeWidthProp = colorProp = VisualPropertyType.values[23]
+
 defVS = Cytoscape.getVisualMappingManager.getVisualStyle
-defVS.getNodeAppearanceCalculator.getDefaultAppearance.setFillColor(Color.white)
+defVS.getNodeAppearanceCalculator.getDefaultAppearance.set(colorProp, Color.red)
+defVS.getEdgeAppearanceCalculator.getDefaultAppearance.set(edgeColorProp, Color.white)
+defVS.getEdgeAppearanceCalculator.getDefaultAppearance.set(edgeWidthProp, 3)
+
 defVS.getGlobalAppearanceCalculator.setDefaultBackgroundColor(Color.black)
 
 Dir::foreach("../../sample1") {|f|
-  puts("File Names = " + f)
-  
   if /^\./ =~ f
-  	puts("This is dir" + f)
+  	puts("Ignore non-network file: " + f)
   else
   	fileName = "../../sample1/" + f
   	Cytoscape.createNetworkFromFile fileName
   	Cytoscape.getCurrentNetworkView.redrawGraph(false, true)
   end
-} 
-
-
-
-
-puts "================ Done! ================"
+}
