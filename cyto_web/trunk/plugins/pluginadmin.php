22a7bf43180b16fe98458202c3ad935111cefdba
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-11 22:43:06 +0000

    userName and password are now requred to view the admin page
    git-svn-id: http://chianti.ucsd.edu/svn@10900 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/plugins/pluginadmin.php
+++ cyto_web/trunk/plugins/pluginadmin.php
@@ -1,10 +1,11 @@
+<?php include "logininfo.inc"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 	<title>Cytoscape Plugin Administration</title>
-	<link rel="stylesheet" type="text/css" media="screen" href="/css/cytoscape.css">
-	<link rel="shortcut icon" href="images/cyto.ico">
+	<link rel="stylesheet" type="text/css" media="screen" href="/cyto_web/css/cytoscape.css">
+	<link rel="shortcut icon" href="/cyto_web/images/cyto.ico">
 </head>
 <body bgcolor="#ffffff">
 <table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
