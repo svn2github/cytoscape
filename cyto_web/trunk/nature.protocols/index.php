025fcc875f103850122932fb0012f4f47851a2c7
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-15 00:18:40 +0000

    made the nature protocols webstart public
    git-svn-id: http://chianti.ucsd.edu/svn@10963 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/nature.protocols/index.php
+++ cyto_web/trunk/nature.protocols/index.php
@@ -28,7 +28,7 @@
             necessary to properly run the protocol described in this paper: (ref here).</p>
 
             <p>
-            At this time, we are resolving some minor issues with webstart, and advise you to run a locally-installed version of Cytoscape instead.  We apologize for the inconvenience.
+			<h3><a href="http://cytoscape.org/nature.protocols/webstart/cy1.jnlp">Run Cytoscape Webstart</a></h3>
             </p>
             <p>
             For more information on Java Webstart, click <A HREF="http://java.sun.com/products/javawebstart/">
