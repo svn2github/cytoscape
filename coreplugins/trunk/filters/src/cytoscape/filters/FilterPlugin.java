77a46c5d939a424ce993a59faf8f74a410feeed4
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-16 23:10:45 +0000

    Catch Throwable in RestoreSessionState()
    git-svn-id: http://chianti.ucsd.edu/svn@10975 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/filters/src/cytoscape/filters/FilterPlugin.java
+++ coreplugins/trunk/filters/src/cytoscape/filters/FilterPlugin.java
@@ -32,48 +32,53 @@
  along with this library; if not, write to the Free Software Foundation,
  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
-
 package cytoscape.filters;
 
 import cytoscape.*;
 
 import cytoscape.data.*;
+
 import cytoscape.filters.util.FilterUtil;
+
 import cytoscape.filters.view.FilterMainPanel;
+
+import cytoscape.plugin.CytoscapePlugin;
+import cytoscape.plugin.PluginInfo;
 
 import cytoscape.util.*;
 
 import cytoscape.view.*;
+
 import cytoscape.view.cytopanels.CytoPanelImp;
 import cytoscape.view.cytopanels.CytoPanelState;
 
 import java.awt.event.*;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
 import javax.swing.*;
 
-import cytoscape.plugin.CytoscapePlugin;
-import cytoscape.plugin.PluginInfo;
-
 
 /**
  *
  */
 public class FilterPlugin extends CytoscapePlugin {
-	private CytoPanelImp cytoPanelWest = (CytoPanelImp) Cytoscape.getDesktop().getCytoPanel(SwingConstants.WEST);
-
+	private CytoPanelImp cytoPanelWest = (CytoPanelImp) Cytoscape.getDesktop()
+	                                                             .getCytoPanel(SwingConstants.WEST);
 	private FilterMainPanel filterMainPanel = new FilterMainPanel();
 	private Vector<CompositeFilter> allFilterVect = null;
-	
+
 	/**
 	 * Creates a new FilterPlugin object.
 	 *
@@ -81,9 +86,8 @@ public class FilterPlugin extends CytoscapePlugin {
 	 * @param csfilter  DOCUMENT ME!
 	 */
 	public FilterPlugin() {
-		
 		init();
-				
+
 		// for debug only
 		//System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
 		//System.out.println("New filters plugin");
@@ -91,84 +95,88 @@ public class FilterPlugin extends CytoscapePlugin {
 		//int indexInCytoPanel = cytoPanelWest.indexOfComponent("Filters");
 		//cytoPanelWest.setSelectedIndex(indexInCytoPanel);						
 	}
-	
+
 	private void init() {
-		
-		ImageIcon icon =  new ImageIcon(getClass().getResource("/cytoscape/images/ximian/stock_filter-data-by-criteria.png"));
-		ImageIcon icon2 = new ImageIcon(getClass().getResource("/cytoscape/images/ximian/stock_filter-data-by-criteria-16.png"));
+		ImageIcon icon = new ImageIcon(getClass()
+		                                   .getResource("/cytoscape/images/ximian/stock_filter-data-by-criteria.png"));
+		ImageIcon icon2 = new ImageIcon(getClass()
+		                                    .getResource("/cytoscape/images/ximian/stock_filter-data-by-criteria-16.png"));
 
 		// Add a menuItem on "select" menu
 		FilterMenuItemAction menuAction = new FilterMenuItemAction(icon2, this);
 		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction) menuAction);
-		
+
 		// Add an icon on toolbar
-		FilterPluginToolBarAction toolbarAction = new FilterPluginToolBarAction(icon, this);		
-		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction( ( CytoscapeAction )toolbarAction );
-		
+		FilterPluginToolBarAction toolbarAction = new FilterPluginToolBarAction(icon, this);
+		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction) toolbarAction);
+
 		//initialize the filterMainPanel and add it to the CytoPanelWEST, i.e. the control (management) panel
 		File global_filter_file = CytoscapeInit.getConfigFile("filters.props");
 		allFilterVect = getFilterVectFromPropFile(global_filter_file);
 
-		System.out.println("FilterPlugin.init(): load "+allFilterVect.size() + " filters from filters.prop");
+		System.out.println("FilterPlugin.init(): load " + allFilterVect.size()
+		                   + " filters from filters.prop");
 
 		filterMainPanel.setAllFilterVect(allFilterVect);
 		cytoPanelWest.add("Filters", filterMainPanel);
 	}
-	
+
 	// Read the filter property file and construct the filter objects
 	//based on the string representation of each filter
 	private Vector<CompositeFilter> getFilterVectFromPropFile(File pPropFile) {
-
 		Vector<CompositeFilter> retVect = new Vector<CompositeFilter>();
 
 		Vector<String> tmpVect = new Vector<String>();
-	
+
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(pPropFile));
-			
+
 			String oneLine = in.readLine();
 
-			while (oneLine != null) {				
+			while (oneLine != null) {
 				// ignore comment or empty line
-				if (oneLine.startsWith("#")||oneLine.trim().equals("")){
+				if (oneLine.startsWith("#") || oneLine.trim().equals("")) {
 					oneLine = in.readLine();
+
 					continue;
 				}
-				
-				if (oneLine.trim().startsWith("Filter_name")){
+
+				if (oneLine.trim().startsWith("Filter_name")) {
 					if (tmpVect.size() == 0) { // start of another Filter string
-						tmpVect.add(oneLine);	
-					}
-					else {
+						tmpVect.add(oneLine);
+					} else {
 						CompositeFilter theFilter = FilterUtil.createFilterFromString(tmpVect);
 						retVect.add(theFilter);
-						
+
 						tmpVect = new Vector<String>();
 						tmpVect.add(oneLine);
 					}
-				}
-				else {
+				} else {
 					tmpVect.add(oneLine);
 				}
+
 				oneLine = in.readLine();
-			}// while loop
+			} // while loop
+
 			in.close();
-			
 		} catch (Exception ex) {
 			System.out.println("Filter Read error");
 			ex.printStackTrace();
 		}
-		
+
 		// The last filter string
-		if (tmpVect.size()>0) {
+		if (tmpVect.size() > 0) {
 			CompositeFilter theFilter = FilterUtil.createFilterFromString(tmpVect);
 			retVect.add(theFilter);
 		}
-	
+
 		return retVect;
 	}
 
-	public void onCytoscapeExit(){
+	/**
+	 *  DOCUMENT ME!
+	 */
+	public void onCytoscapeExit() {
 		//System.out.println("onCytoscapeExit() ...");
 		// Save global filter to "filters.prop"
 		File filter_file = CytoscapeInit.getConfigFile("filters.props");
@@ -176,22 +184,24 @@ public class FilterPlugin extends CytoscapePlugin {
 
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(filter_file));
-			for (int i=0; i<allFilterVect.size(); i++ ) {
-				CompositeFilter theFilter = (CompositeFilter)allFilterVect.elementAt(i);
+
+			for (int i = 0; i < allFilterVect.size(); i++) {
+				CompositeFilter theFilter = (CompositeFilter) allFilterVect.elementAt(i);
 				AdvancedSetting advSetting = theFilter.getAdvancedSetting();
+
 				if (advSetting.isGlobalChecked()) {
 					writer.write(theFilter.toString());
 					writer.newLine();
 				}
 			}
+
 			writer.close();
 		} catch (Exception ex) {
 			System.out.println("Global filter Write error");
 			ex.printStackTrace();
-		}		
+		}
 	}
 
-	
 	// override the following two methods to save state.
 	/**
 	 * DOCUMENT ME!
@@ -200,42 +210,53 @@ public class FilterPlugin extends CytoscapePlugin {
 	 *            DOCUMENT ME!
 	 */
 	public void restoreSessionState(List<File> pStateFileList) {
-		System.out.println("FilterPlugin.restoreSessionState()");		
-		
-		if (pStateFileList == null || pStateFileList.size() == 0) {
+		System.out.println("FilterPlugin.restoreSessionState()");
+
+		if ((pStateFileList == null) || (pStateFileList.size() == 0)) {
 			System.out.println("\tNo previous filter state to restore.");
+
 			return;
 		}
-		
-		File session_filter_file = pStateFileList.get(0);
-		
-		Vector<CompositeFilter> sessionFilterVect = getFilterVectFromPropFile(session_filter_file);
 
-		System.out.println("\tLoad " + sessionFilterVect.size() + " session filters");
-		allFilterVect = filterMainPanel.getAllFilterVect();
+		try {
+			File session_filter_file = pStateFileList.get(0);
 
-		int currentFilterCount =allFilterVect.size(); 
-		System.out.println("\tcurrentFilterCount=" + currentFilterCount);
-		
-		for (int i =0; i<sessionFilterVect.size(); i++ ){
-			// Exclude duplicated filter
-			boolean isDuplicated = false;
-			for (int j=0; j<currentFilterCount; j++ ) {
-				if (sessionFilterVect.elementAt(i).toString().equalsIgnoreCase(allFilterVect.elementAt(j).toString())) {
-					isDuplicated= true;
-					break;
-				}				
+			Vector<CompositeFilter> sessionFilterVect = getFilterVectFromPropFile(session_filter_file);
+
+			System.out.println("\tLoad " + sessionFilterVect.size() + " session filters");
+			allFilterVect = filterMainPanel.getAllFilterVect();
+
+			int currentFilterCount = allFilterVect.size();
+			System.out.println("\tcurrentFilterCount=" + currentFilterCount);
+
+			for (int i = 0; i < sessionFilterVect.size(); i++) {
+				// Exclude duplicated filter
+				boolean isDuplicated = false;
+
+				for (int j = 0; j < currentFilterCount; j++) {
+					if (sessionFilterVect.elementAt(i).toString()
+					                     .equalsIgnoreCase(allFilterVect.elementAt(j).toString())) {
+						isDuplicated = true;
+
+						break;
+					}
+				}
+
+				if (isDuplicated) {
+					continue;
+				}
+
+				allFilterVect.add(sessionFilterVect.elementAt(i));
 			}
-			if (isDuplicated) {
-				continue;
-			}
-			allFilterVect.add(sessionFilterVect.elementAt(i));
+
+			System.out.println("\t"
+			                   + ((currentFilterCount + sessionFilterVect.size())
+			                     - allFilterVect.size()) + " duplicated filters are not added");
+		} catch (Throwable ee) {
+			System.out.println("Failed to restore Filters from session!");
 		}
-		
-		System.out.println("\t" + (currentFilterCount+ sessionFilterVect.size() -allFilterVect.size())+ " duplicated filters are not added");
 	}
 
-	
 	/**
 	 * DOCUMENT ME!
 	 *
@@ -244,30 +265,33 @@ public class FilterPlugin extends CytoscapePlugin {
 	 */
 	public void saveSessionStateFiles(List<File> pFileList) {
 		System.out.println("saveSessionStateFiles() ...");
-		
+
 		//Create an empty file on system temp directory
 		String tmpDir = System.getProperty("java.io.tmpdir");
-	    System.out.println("java.io.tmpdir: [" + tmpDir + "]");
-	    
+		System.out.println("java.io.tmpdir: [" + tmpDir + "]");
+
 		File session_filter_file = new File(tmpDir, "session_filters.props");
 		Vector<CompositeFilter> allFilterVect = filterMainPanel.getAllFilterVect();
 
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(session_filter_file));
-			for (int i=0; i<allFilterVect.size(); i++ ) {
-				CompositeFilter theFilter = (CompositeFilter)allFilterVect.elementAt(i);
+
+			for (int i = 0; i < allFilterVect.size(); i++) {
+				CompositeFilter theFilter = (CompositeFilter) allFilterVect.elementAt(i);
 				AdvancedSetting advSetting = theFilter.getAdvancedSetting();
+
 				if (advSetting.isSessionChecked()) {
 					writer.write(theFilter.toString());
 					writer.newLine();
 				}
 			}
+
 			writer.close();
 		} catch (Exception ex) {
 			System.out.println("Session filter Write error");
 			ex.printStackTrace();
-		}		
-		
+		}
+
 		pFileList.add(session_filter_file);
 	}
 }
