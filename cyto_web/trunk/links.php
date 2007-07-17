f8f258a92b2565cbf5acb5783480e6b6f4efe203
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-17 21:58:50 +0000

    updated links.php with link to BiologicalNetworks
    git-svn-id: http://chianti.ucsd.edu/svn@10985 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/links.php
+++ cyto_web/trunk/links.php
@@ -40,6 +40,8 @@ and provides a powerful framework for user interaction and data display.
 	<li><a href="http://nemo-cyclone.sourceforge.net">Cyclone</a>
 	<li><a href="http://medcd.iab.keio.ac.jp/expanda/">eXpanda (Network visualization/analysis Perl library supporting sif/gml file I/O)</a>
 	<li><a href="http://pynetconv.sourceforge.net/">pyNetConv - Interconverts network file formats e.g. .net, .gml, .sif</a>
+	<li><a href="http://biologicalnetworks.net">BiologicalNetworks</a>
+
 </ul>
 
 <p><big><b>Network Databases</b></big></p>
