b69df2ee67cbfa78e20abd1ddb544d1422ac84ac
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-23 07:00:18 +0000

    New page for past news.
    git-svn-id: http://chianti.ucsd.edu/svn@11048 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/past_news.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<! Updated for 2.5 by kono >
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<title>Cytoscape Screen Shots</title>
+<link rel="stylesheet" type="text/css" media="screen" href="css/cytoscape.css" />
+<link rel="shortcut icon" href="images/cyto.ico" />
+</head>
+<body bgcolor="#FFFFFF">
+<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
+  <tr>
+    <td width="10">&nbsp;</td>
+    <td valign="center"><h1>Past News</h1></td>
+  </tr>
+</table>
+<? include "nav.php"; ?>
+Past news will be saved here.
+<? include "footer.php"; ?>
+</body>
+</html>
--- cyto_web/trunk/screenshots.php
+++ cyto_web/trunk/screenshots.php
@@ -2,68 +2,44 @@
 <! Updated for 2.4 by kono >
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
-	<title>Cytoscape Screen Shots</title> 
-	<link rel="stylesheet" type="text/css" media="screen" href="css/cytoscape.css" />
-	<link rel="shortcut icon" href="images/cyto.ico" />
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<title>Cytoscape Screen Shots</title>
+<link rel="stylesheet" type="text/css" media="screen" href="css/cytoscape.css" />
+<link rel="shortcut icon" href="images/cyto.ico" />
 </head>
 <body bgcolor="#FFFFFF">
 <table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
-	<tr>
-		<td width="10">
-			&nbsp; 
-		</td>
-		<td valign="center">
-			<h1>Cytoscape Screen Shots</h1> 
-		</td>
-	</tr>
+  <tr>
+    <td width="10">&nbsp;</td>
+    <td valign="center"><h1>Cytoscape Screen Shots</h1></td>
+  </tr>
 </table>
 <? include "nav.php"; ?>
 <div id="indent">
-	<p>
-		(All images below are created by Cytoscape version 2.4)
-	</p>
-	<p>
-		Sample session file <i>galFiletred.cys</i> is opened.  Cytoscape 2.4 uses new icon set for cleaner look. 
-	</p>
-	<p>
-		<img src="screenshots/2_4_ss1.png" alt="Main cytoscape window"> 
-	</p>
-	<hr width="100%" size="2">
-	<p>
-		From version 2.4, Cytoscape can load network files from local file system and remote data sources.  
-		<br>
-		<br>
-		<img src="screenshots/2_4_ss2.png" alt="Network Import">
-	</p>
-	<hr width="100%" size="2">
-	<p>
-		Cytoscape supports standard file formats (SBML, XGMML, GML, PSI-MI and BioPAX) by default.  In addition, users can import free-format tables in text or Microsoft Excel format.    
-		<br>
-		<p><img src="screenshots/2_4_ss3.png" alt="Table Import 1"></p>
-		<p><img src="screenshots/2_4_ss4.png" alt="Table Import 2"></p>
-	</p>
-	<hr width="100%" size="2">
-	<p>
-		Node label position can be controled by new GUI in VizMapper.    
-		<br>
-		<br>
-		<img src="screenshots/2_4_ss5.png" alt="Label Position">
-	</p>
-	<hr width="100%" size="2">
-	<p>
-		From 2.4, Cytoscape manage Ontology DAGs as regular network.  User can visualize DAGs using Cytoscape's function.    
-		<br>
-		<br>
-		<img src="screenshots/2_4_ss6.png" alt="GO DAG">
-	</p>
-	<hr width="100%" size="2">
-	<p>
-		Cytoscape can export network as an image in variety of formats, including eps, PDF, SVG, jpeg, png.  
-		(The image below is an image export of sample file yeastHighQuality.sif)
-	</p>
-		<img src="screenshots/yeastHighQuality.png" alt="Image Export"> 
-</div>
+  <p> (All images below are created by Cytoscape version 2.4) </p>
+  <p> Sample session file <i>galFiletred.cys</i> is opened.  Cytoscape 2.4 uses new icon set for cleaner look. </p>
+  <p> <img src="screenshots/2_4_ss1.png" alt="Main cytoscape window"> </p>
+  <hr width="100%" size="2">
+  <p> From version 2.4, Cytoscape can load network files from local file system and remote data sources. <br>
+    <br>
+    <img src="screenshots/2_4_ss2.png" alt="Network Import"> </p>
+  <hr width="100%" size="2">
+  <p> Cytoscape supports standard file formats (SBML, XGMML, GML, PSI-MI and BioPAX) by default.  In addition, users can import free-format tables in text or Microsoft Excel format. <br>
+  <p><img src="screenshots/2_4_ss3.png" alt="Table Import 1"></p>
+  <p><img src="screenshots/2_4_ss4.png" alt="Table Import 2"></p>
+  </p>
+  <hr width="100%" size="2">
+  <p> Node label position can be controled by new GUI in VizMapper. <br>
+    <br>
+    <img src="screenshots/2_4_ss5.png" alt="Label Position"> </p>
+  <hr width="100%" size="2">
+  <p> From 2.4, Cytoscape manage Ontology DAGs as regular network.  User can visualize DAGs using Cytoscape's function. <br>
+    <br>
+    <img src="screenshots/2_4_ss6.png" alt="GO DAG"> </p>
+  <hr width="100%" size="2">
+  <p> Cytoscape can export network as an image in variety of formats, including eps, PDF, SVG, jpeg, png.  
+    (The image below is an image export of sample file yeastHighQuality.sif) </p>
+  <img src="screenshots/yeastHighQuality.png" alt="Image Export"> </div>
 </div>
 <? include "footer.php"; ?>
 </body>
