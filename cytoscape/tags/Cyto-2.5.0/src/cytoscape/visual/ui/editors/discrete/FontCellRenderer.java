62251c9d4931c56fd2be3306362923f5b268849b
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-23 19:32:22 +0000

    Tagging release 2.5.0
    git-svn-id: http://chianti.ucsd.edu/svn@11055 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/tags/Cyto-2.5.0/src/cytoscape/visual/ui/editors/discrete/FontCellRenderer.java
@@ -0,0 +1,59 @@
+package cytoscape.visual.ui.editors.discrete;
+
+import com.l2fprod.common.swing.renderer.DefaultCellRenderer;
+
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.JTable;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author kono
+ */
+public class FontCellRenderer extends DefaultCellRenderer {
+    /**
+     * DOCUMENT ME!
+     *
+     * @param table
+     *            DOCUMENT ME!
+     * @param value
+     *            DOCUMENT ME!
+     * @param isSelected
+     *            DOCUMENT ME!
+     * @param hasFocus
+     *            DOCUMENT ME!
+     * @param row
+     *            DOCUMENT ME!
+     * @param column
+     *            DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public Component getTableCellRendererComponent(JTable table, Object value,
+        boolean isSelected, boolean hasFocus, int row, int column) {
+        if (isSelected) {
+            setBackground(table.getSelectionBackground());
+            setForeground(table.getSelectionForeground());
+        } else {
+            setBackground(table.getBackground());
+            setForeground(table.getForeground());
+        }
+
+        if ((value != null) && value instanceof Font) {
+            final Font font = (Font) value;
+            final Font modFont = new Font(
+                    font.getFontName(),
+                    font.getStyle(),
+                    12);
+
+            this.setFont(modFont);
+            this.setText(modFont.getFontName());
+        } else
+            this.setValue(null);
+
+        return this;
+    }
+}
