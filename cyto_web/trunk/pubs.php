5c00d3364c3add2d94ebba9411e4f550c1ff192b
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-24 18:39:44 +0000

    Bug on the title fixed.
    git-svn-id: http://chianti.ucsd.edu/svn@11111 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/pubs.php
+++ cyto_web/trunk/pubs.php
@@ -7,16 +7,11 @@
 	<link rel="shortcut icon" href="images/cyto.ico">
 </head>
 <body bgcolor="#FFFFFF">
-<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
-	<tr>
-		<td width="10">
-			�
-		</td>
-		<td valign="center">
-			<h1> Research using Cytoscape </h1>
-		</td>
-	</tr>
-</table>
+
+<div id="topbar">
+	<div class="title">Research Using Cytoscape</div>
+</div>
+
 <? include "nav.php"; ?>
 <div id="indent">
 
