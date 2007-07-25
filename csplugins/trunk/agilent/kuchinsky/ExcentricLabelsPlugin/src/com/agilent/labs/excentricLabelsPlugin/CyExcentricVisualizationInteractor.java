3bf9a2d088efd877399c891a8f9423737886f353
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-25 20:45:39 +0000

    Reformatted code;  added more comments
    git-svn-id: http://chianti.ucsd.edu/svn@11166 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/agilent/kuchinsky/ExcentricLabelsPlugin/src/com/agilent/labs/excentricLabelsPlugin/CyExcentricVisualizationInteractor.java
+++ csplugins/trunk/agilent/kuchinsky/ExcentricLabelsPlugin/src/com/agilent/labs/excentricLabelsPlugin/CyExcentricVisualizationInteractor.java
@@ -3,166 +3,168 @@ package com.agilent.labs.excentricLabelsPlugin;
 import infovis.visualization.inter.BasicVisualizationInteractor;
 import infovis.visualization.magicLens.ExcentricLabels;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JComponent;
-import javax.swing.Timer;
-
+/**
+ * Handles user interaction with Excentric labels.
+ *
+ * @author Allan Kuchinsky, Jean-Daniel Fekete
+ * @version $Revision: 0.1 $
+*/
 public class CyExcentricVisualizationInteractor extends
-		BasicVisualizationInteractor {
-	protected ExcentricLabels excentric;
+        BasicVisualizationInteractor {
+    protected ExcentricLabels excentric;
 
-	protected Timer insideTimer;
+    protected Timer insideTimer;
 
-	protected int threshold = 20;
+    protected int threshold = 20;
 
-	protected boolean entered;
-	
-	private CyExcentricLabelsWrapper wrapper;
+    protected boolean entered;
 
-	public CyExcentricVisualizationInteractor(CyExcentricLabelsWrapper wrapper) {
-		super();
-		this.excentric = wrapper.getExcentric();
-		this.wrapper = wrapper;
-		insideTimer = new Timer(50000, new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+    private CyExcentricLabelsWrapper wrapper;
+
+    public CyExcentricVisualizationInteractor (CyExcentricLabelsWrapper wrapper) {
+        super();
+        this.excentric = wrapper.getExcentric();
+        this.wrapper = wrapper;
+        insideTimer = new Timer(50000, new ActionListener() {
+            public void actionPerformed (ActionEvent e) {
                 System.out.println("Ping!");
                 excentric.setVisible(true);
-		
-			}
-		});
-		insideTimer.setRepeats(false);
-		setVisualization (wrapper);
-	}
 
-	public void setVisualization(CyExcentricLabelsWrapper wrapper)
-	{
-		if (this.wrapper == wrapper)
-			return;
-		if (wrapper != null && wrapper.getParent() != null) {
-			uninstall(wrapper);
+            }
+        });
+        insideTimer.setRepeats(false);
+        setVisualization(wrapper);
+    }
 
-		}
-		this.wrapper = wrapper;
-		if (wrapper != null) {
-			if (wrapper.parent != null) {
-				install(wrapper);
-			}
-		}
-	}
+    public void setVisualization (CyExcentricLabelsWrapper wrapper) {
+        if (this.wrapper == wrapper) {
+            return;
+        }
+        if (wrapper != null && wrapper.getParent() != null) {
+            uninstall(wrapper);
+        }
+        this.wrapper = wrapper;
+        if (wrapper != null) {
+            if (wrapper.parent != null) {
+                install(wrapper);
+            }
+        }
+    }
 
-	public void install(JComponent comp) {
-		if (comp != null) {
-//			System.out.println ("installing mouse listener on: " + comp);
-			comp.addMouseListener(this);
-			comp.addMouseMotionListener(this);
-				restart();
-		}
-	}
+    public void install (JComponent comp) {
+        if (comp != null) {
+            comp.addMouseListener(this);
+            comp.addMouseMotionListener(this);
+            restart();
+        }
+    }
 
-	public void uninstall(JComponent comp) {
-		super.uninstall(comp);
-		if (comp != null) {
-			setVisible(false);
-			stop();
-			comp.removeMouseListener(this);
-			comp.removeMouseMotionListener(this);
-		}
-	}
+    public void uninstall (JComponent comp) {
+        super.uninstall(comp);
+        if (comp != null) {
+            setVisible(false);
+            stop();
+            comp.removeMouseListener(this);
+            comp.removeMouseMotionListener(this);
+        }
+    }
 
-	public void restart() {
-		if (insideTimer != null)
-			insideTimer.restart();
-	}
+    public void restart () {
+        if (insideTimer != null) {
+            insideTimer.restart();
+        }
+    }
 
-	public void stop() {
-		if (insideTimer != null)
-			insideTimer.stop();
-	}
+    public void stop () {
+        if (insideTimer != null) {
+            insideTimer.stop();
+        }
+    }
 
-	public void setVisible(boolean v) {
-		excentric.setVisible(v);
-	}
+    public void setVisible (boolean v) {
+        excentric.setVisible(v);
+    }
 
-	public boolean isVisible() {
-		return excentric.isVisible();
-	}
+    public boolean isVisible () {
+        return excentric.isVisible();
+    }
 
-	public static float dist2(float dx, float dy) {
-		return dx * dx + dy * dy;
-	}
+    public static float dist2 (float dx, float dy) {
+        return dx * dx + dy * dy;
+    }
 
-	/**
-	 * @see java.awt.event.MouseAdapter#mouseEntered(MouseEvent)
-	 */
-	public void mouseEntered(MouseEvent e) {
-		entered = true;
-		restart();
-	}
+    /**
+     * @see java.awt.event.MouseAdapter#mouseEntered(MouseEvent)
+     */
+    public void mouseEntered (MouseEvent e) {
+        entered = true;
+        restart();
+    }
 
-	/**
-	 * @see java.awt.event.MouseAdapter#mouseExited(MouseEvent)
-	 */
-	public void mouseExited(MouseEvent e) {
-		if (e.getModifiers() != 0) {
-			return;
-		}
-		entered = false;
-		stop();
-		setVisible(false);
-	}
+    /**
+     * @see java.awt.event.MouseAdapter#mouseExited(MouseEvent)
+     */
+    public void mouseExited (MouseEvent e) {
+        if (e.getModifiers() != 0) {
+            return;
+        }
+        entered = false;
+        stop();
+        setVisible(false);
+    }
 
-	/**
-	 * @see java.awt.event.MouseAdapter#mousePressed(MouseEvent)
-	 */
-	public void mousePressed(MouseEvent e) {
-		setVisible(false);
-	}
+    /**
+     * @see java.awt.event.MouseAdapter#mousePressed(MouseEvent)
+     */
+    public void mousePressed (MouseEvent e) {
+        setVisible(false);
+    }
 
-	/**
-	 * @see java.awt.event.MouseMotionListener#mouseMoved(MouseEvent)
-	 */
-	public void mouseMoved(MouseEvent e) {
+    /**
+     * @see java.awt.event.MouseMotionListener#mouseMoved(MouseEvent)
+     */
+    public void mouseMoved (MouseEvent e) {
 //		System.out.println("mouse event at: " + e.getWhen());
-		if (isVisible()) {
+        if (isVisible()) {
 //			System.out.println("Visible mouse event: " + e);
-			if (e.getModifiers() != 0)
-				return;
-			if (dist2(excentric.getLensX() - e.getX(), excentric.getLensY()
-					- e.getY()) > threshold * threshold) {
-				setVisible(false);
-				insideTimer.restart();
-			}
-		}
+            if (e.getModifiers() != 0) {
+                return;
+            }
+            if (dist2(excentric.getLensX() - e.getX(), excentric.getLensY()
+                    - e.getY()) > threshold * threshold) {
+                setVisible(false);
+                insideTimer.restart();
+            }
+        }
+        excentric.setLens(e.getX(), e.getY());
+    }
 
-		excentric.setLens(e.getX(), e.getY());
-	}
+    /**
+     * Returns the threshold.
+     * <p/>
+     * When the mouse moves a distance larger than this threshold since the last
+     * event, excentric labels are disabled.
+     *
+     * @return int
+     */
+    public int getThreshold () {
+        return threshold;
+    }
 
-	/**
-	 * Returns the threshold.
-	 * 
-	 * When the mouse moves a distance larger than this threshold since the last
-	 * event, excentric labels are disabled.
-	 * 
-	 * @return int
-	 */
-	public int getThreshold() {
-		return threshold;
-	}
-
-	/**
-	 * Sets the threshold.
-	 * 
-	 * When the mouse moves a distance larger than the specified threshold since
-	 * the last event, excentric labels are disabled.
-	 * 
-	 * @param threshold
-	 *            The threshold to set
-	 */
-	public void setThreshold(int threshold) {
-		this.threshold = threshold;
-	}
-
+    /**
+     * Sets the threshold.
+     * <p/>
+     * When the mouse moves a distance larger than the specified threshold since
+     * the last event, excentric labels are disabled.
+     *
+     * @param threshold The threshold to set
+     */
+    public void setThreshold (int threshold) {
+        this.threshold = threshold;
+    }
 }
