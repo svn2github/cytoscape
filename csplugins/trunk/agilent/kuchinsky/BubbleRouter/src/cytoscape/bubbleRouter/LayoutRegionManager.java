d7cb2368fbe4fbb1ff65c263213b2c77a6557b83
apico <apico@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-07-30 00:05:49 +0000

    major refactoring code clean
    git-svn-id: http://chianti.ucsd.edu/svn@11223 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/agilent/kuchinsky/BubbleRouter/src/cytoscape/bubbleRouter/LayoutRegionManager.java
+++ csplugins/trunk/agilent/kuchinsky/BubbleRouter/src/cytoscape/bubbleRouter/LayoutRegionManager.java
@@ -12,15 +12,17 @@ import ding.view.DingCanvas;
 import ding.view.DGraphView.Canvas;
 
 /**
- * @author apico
- *
+ * This class maintains a hash map of all regions added to the view, in addition
+ * to a updated unique name list and counter.
  */
 public class LayoutRegionManager {
 
-	public static HashMap<CyNetworkView, List<LayoutRegion>> regionViewMap = new HashMap();
-	
+	public static HashMap<CyNetworkView, List<LayoutRegion>> regionViewMap = new HashMap<CyNetworkView, List<LayoutRegion>>();
+
+	public static List<String> regionNameList = new ArrayList<String>();
+
 	public static int regionCount = 0;
-	
+
 	/**
 	 * canvas to add regions to -- currently BACKGROUND_CANVAS
 	 */
@@ -30,14 +32,18 @@ public class LayoutRegionManager {
 	 * 
 	 * @param view
 	 * @return list of regions for this NetworkView
-	 */public static List getRegionListForView(CyNetworkView view) {
-		return (List) regionViewMap.get(view);
+	 */
+	public static List<LayoutRegion> getRegionListForView(CyNetworkView view) {
+		return (List<LayoutRegion>) regionViewMap.get(view);
 	}
-	
+
 	/**
 	 * add a region/view association in the regionViewMap data structure
-	 * @param view the CyNetworkView to add to
-	 * @param region the region you are adding
+	 * 
+	 * @param view
+	 *            the CyNetworkView to add to
+	 * @param region
+	 *            the region you are adding
 	 */
 	public static void addRegionForView(CyNetworkView view, LayoutRegion region) {
 		List<LayoutRegion> regionList = regionViewMap.get(view);
@@ -50,55 +56,71 @@ public class LayoutRegionManager {
 
 	/**
 	 * remove the region and index from the view
+	 * 
 	 * @param view
 	 * @param index
 	 */
-	// APico 9.16.06
-	// TODO: use this to delete regions
 	public static void removeRegionFromView(CyNetworkView view, Integer index) {
-		List<LayoutRegion> regionList =  regionViewMap.get(view);
+		List<LayoutRegion> regionList = regionViewMap.get(view);
 		if (regionList == null) {
 			regionList = new ArrayList<LayoutRegion>();
 		}
 		regionList.remove(index);
 		regionViewMap.put(view, regionList);
 	}
-	
+
+	/**
+	 * add a region name to the regionNameList
+	 * 
+	 * @param region
+	 *            the region you are adding
+	 */
+	public static void addRegionNameToList(LayoutRegion region) {
+		regionNameList.add(region.getRegionAttributeValue().toString());
+	}
+
+	/**
+	 * remove the region name from the list
+	 * 
+	 * @param index
+	 */
+	public static void removeRegionNameFromList(LayoutRegion region) {
+		regionNameList.remove(region.getRegionAttributeValue().toString());
+	}
+
+	public static List getRegionNameList() {
+		return regionNameList;
+	}
 
 	/**
 	 * 
 	 * @param view
 	 * @return number of regions for this CyNetworkView
 	 */
-	public static int getNumRegionsForView (CyNetworkView view)
-	{
+	public static int getNumRegionsForView(CyNetworkView view) {
 		List<LayoutRegion> regionList = regionViewMap.get(view);
 		if (regionList == null) {
 			return 0;
-		}
-		else 
-		{
+		} else {
 			return regionList.size();
 		}
 	}
-	
+
 	/**
 	 * remove all the regions associated with this CyNetworkView
+	 * 
 	 * @param view
 	 */
-	public static void removeAllRegionsForView (CyNetworkView view)
-	{
+	public static void removeAllRegionsForView(CyNetworkView view) {
 		List<LayoutRegion> regionList = regionViewMap.get(view);
 		if (regionList == null) {
 			return;
 		}
-		while (regionList.size() > 0)
-		{
-			removeRegion (view, (LayoutRegion) regionList.get(0));
+		while (regionList.size() > 0) {
+			removeRegion(view, (LayoutRegion) regionList.get(0));
 		}
 		regionViewMap.put(view, null);
 	}
-	
 
 	/**
 	 * 
@@ -115,12 +137,10 @@ public class LayoutRegionManager {
 		// correct for region that has already been removed
 		if (regionList.contains(region)) {
 			regionList.remove(region);
-			if (regionList.size() > 0)
-			{
+			if (regionList.size() > 0) {
 				regionViewMap.put(view, regionList);
-			}
-			else 
-				// remove entry from regionViewMap if number of regions goes to zero
+			} else
+			// remove entry from regionViewMap if number of regions goes to zero
 			{
 				regionViewMap.remove(view);
 			}
@@ -128,8 +148,8 @@ public class LayoutRegionManager {
 	}
 
 	/**
-	 * Keeps count of all created regions (even ones that have been deleted/removed)
-	 * to allow for unique region counter 
+	 * Keeps count of all created regions (even ones that have been
+	 * deleted/removed) to allow for unique region counter
 	 * 
 	 * Useful in generating a unique name for each region Group
 	 * 
@@ -141,55 +161,84 @@ public class LayoutRegionManager {
 
 	/**
 	 * higher-level routine for adding a region to a view
+	 * 
 	 * @param view
 	 * @param region
 	 */
-	public static void addRegion(CyNetworkView view, LayoutRegion region) {
+	public static void addRegionFromFile(CyNetworkView view, LayoutRegion region) {
 		addRegionForView(view, region);
-
+		addRegionNameToList(region);
 		// Add to counter
 		regionCount++;
-		
+
 		// Grab ArbitraryGraphicsCanvas (a prefab canvas) and add the
 		// layout region
 		DGraphView dview = (DGraphView) view;
 		DingCanvas backgroundLayer = dview.getCanvas(REGION_CANVAS);
 		backgroundLayer.add(region);
-	
-		// AJK: 01/07/2007 BEGIN
-		//    oy what a hack: do an infinitesimal change of zoom factor so that it
-		//    forces a viewport changed event, 
-		//    which enables us to get original viewport centerpoint and scale factor
+
+		// oy what a hack: do an infinitesimal change of zoom factor so that it
+		// forces a viewport changed event,
+		// which enables us to get original viewport centerpoint and scale
+		// factor
 		dview.setZoom(dview.getZoom() * 0.99999999999999999d);
-		
+
+		// Do not call BubbleRouterPlugin.newGroup(region)
+	}
+
+	/**
+	 * higher-level routine for adding a region to a view
+	 * 
+	 * @param view
+	 * @param region
+	 */
+	public static void addRegion(CyNetworkView view, LayoutRegion region) {
+		addRegionForView(view, region);
+		addRegionNameToList(region);
+		// Add to counter
+		regionCount++;
+
+		// Grab ArbitraryGraphicsCanvas (a prefab canvas) and add the
+		// layout region
+		DGraphView dview = (DGraphView) view;
+		DingCanvas backgroundLayer = dview.getCanvas(REGION_CANVAS);
+		backgroundLayer.add(region);
+
+		// oy what a hack: do an infinitesimal change of zoom factor so that it
+		// forces a viewport changed event,
+		// which enables us to get original viewport centerpoint and scale
+		// factor
+		dview.setZoom(dview.getZoom() * 0.99999999999999999d);
+
 		BubbleRouterPlugin.newGroup(region);
 	}
-	
-	
+
 	/**
 	 * higher-level routine for removing a region from a view
+	 * 
 	 * @param view
 	 * @param region
 	 */
 	public static void removeRegion(CyNetworkView view, LayoutRegion region) {
 		removeRegionFromView(view, region);
+		removeRegionNameFromList(region);
 		DGraphView dview = (DGraphView) view;
 		DingCanvas backgroundLayer = dview.getCanvas(REGION_CANVAS);
 		backgroundLayer.remove(region);
 		backgroundLayer.repaint();
-		
+
 		BubbleRouterPlugin.groupWillBeRemoved(region);
 	}
-	
 
 	/**
 	 * return the LayoutRegion that has been mouse-clicked upon, if any
+	 * 
 	 * @param pt
 	 * @return
 	 */
 	public static LayoutRegion getPickedLayoutRegion(Point pt)
 	{
-		// first see if click was on a node or edge.  If so, return
+		// first see if click was on a node or edge. If so, return
 		if ((((DGraphView) 
 				Cytoscape.getCurrentNetworkView()).getPickedNodeView(pt) != null) ||
 				(((DGraphView) 
@@ -200,8 +249,7 @@ public class LayoutRegionManager {
 		}
 		
 		// next go down list of regions and see if there is a hit
-		List<LayoutRegion> regionList = getRegionListForView (Cytoscape.getCurrentNetworkView());
-		
+/		
 		DingCanvas myCanvas = 
 		((DGraphView) Cytoscape.getCurrentNetworkView()).getCanvas(REGION_CANVAS);
 		
@@ -219,18 +267,18 @@ public class LayoutRegionManager {
 		// if we get to this point then we haven't a region
 		return null;
 	}
-	
-	
+
 	/**
 	 * is the input point located within the bounds of the Layout region?
-	 * @param pt the input point, in screen coordinates
-	 * @param region the region, whose bounds are also in screen coordinates
+	 * 
+	 * @param pt
+	 *            the input point, in screen coordinates
+	 * @param region
+	 *            the region, whose bounds are also in screen coordinates
 	 * @return
 	 */
-	private static boolean isPointOnRegion (Point pt, LayoutRegion region)
-	{
+	private static boolean isPointOnRegion(Point pt, LayoutRegion region) {
 		return region.getBounds().contains(pt);
 	}
-
 
 }
