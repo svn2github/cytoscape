9ad0705e799e162becff96ca502628a5d20a48db
Maitali Ashkenazi <Maitali Ashkenazi@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-08-07 16:33:54 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@11327 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/maital/EnhancedSearch/src/csplugins/enhanced/search/EnhancedSearchPlugin.java
+++ csplugins/trunk/soc/maital/EnhancedSearch/src/csplugins/enhanced/search/EnhancedSearchPlugin.java
@@ -36,38 +36,11 @@
 package csplugins.enhanced.search;
 
 import cytoscape.plugin.CytoscapePlugin;
-import cytoscape.plugin.PluginInfo;
-import cytoscape.plugin.Category;
 
-/**
- * Enhanced Search plugin
- * 
- * @author Maital Ashkenazi
- * @version 1.0
- * 
- */
 public class EnhancedSearchPlugin extends CytoscapePlugin {
-
-	private static final double VERSION = 1.00;
 
 	// Creates a new EnhancedSearchPlugin object.
 	public EnhancedSearchPlugin() {
 		EnhancedSearch plugin = new EnhancedSearch();
-	}
-
-	// Information needed for Cytoscape version 2.5 plugin management
-	public PluginInfo getPluginInfoObject() {
-
-		PluginInfo info = new PluginInfo();
-
-		info.setName("EnhancedSearch");
-		info.setDescription("Perform search on multiple attribute fields.");
-		info.setCategory(Category.NONE);
-		info.setPluginVersion(VERSION);
-		info.setCytoscapeVersion("2.5");
-		info.setProjectUrl("http://conklinwolf.ucsf.edu/genmappwiki/Google_Summer_of_Code_2007/Maital");
-		info.addAuthor("Maital Ashkenazi", "HUJI");
-
-		return info;
 	}
 }
