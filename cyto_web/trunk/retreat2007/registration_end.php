9215c2f0030b02e6b248bbf07109a170f15e0ded
piet <piet@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-08-09 14:27:16 +0000

    Added registration_end.php to retreat part
    git-svn-id: http://chianti.ucsd.edu/svn@11358 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/retreat2007/registration_end.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+  <title>5th Annual Cytoscape Retreat and Symposium - Venue</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="css/cytoretreat.css">
+  <link rel="../shortcut icon" href="../images/cyto.ico">
+  <!--[if lt IE 7]>
+  <script defer type="text/javascript" src="js/pngfix.js"></script>
+  <![endif]-->
+</head>
+
+<body style="background-color: rgb(255, 255, 255);">
+
+<!-- The header with changing pictures -->
+
+<? include "header.php"; ?>
+
+<!-- Retreat content table-->
+
+<table>
+ <tr valign="top">
+  <!-- Left retreat navigation bar -->
+  <td width="10%">
+   <? include "nav_retreat.php"; ?>
+  </td>
+  <!-- This page content -->
+  <td width="90%">
+  <div id="contents">
+  <div id="content">
+  <div class="item">
+  <div class="top">
+   <h2>Registration </h2>
+   <br>
+  </div>
+  Registration is now complete; thank you for registering and see you at the retreat!  
+  </div>
+  </div>
+  </div>
+
+  </td>  <!--End Main page content -->
+ </tr>
+</table>
+<? include "footer.php"; ?>
+</body>
+</html>
