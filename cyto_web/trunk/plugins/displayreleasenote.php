b3011e8a711ee2623571ae9cba8deebe069ec887
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-09-05 21:17:14 +0000

    Adjust the header due to change of css
    git-svn-id: http://chianti.ucsd.edu/svn@11527 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/plugins/displayreleasenote.php
+++ cyto_web/trunk/plugins/displayreleasenote.php
@@ -7,17 +7,11 @@
 	<link rel="shortcut icon" href="/cyto_web/images/cyto.ico">
 </head>
 <body bgcolor="#ffffff">
-<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
-	<tbody>
-		<tr>
-			<td width="10">&nbsp;
-			</td>
-			<td valign="bottom">
-				<h1>Cytoscape 2.x Plugins Release Note</h1>
-			</td>
-		</tr>
-	</tbody>
-</table>
+
+<div id="topbar">
+        <div class="title">Cytoscape 2.x Plugins Release Note</div>
+</div>
+
 <?php include "../nav.php"; ?>
 
 <?php
