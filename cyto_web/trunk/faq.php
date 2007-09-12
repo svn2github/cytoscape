efa3c8a5a56493a34a2869692af499d867792755
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-09-12 16:38:26 +0000

    correct path to css and image
    git-svn-id: http://chianti.ucsd.edu/svn@11565 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/faq.php
+++ cyto_web/trunk/faq.php
@@ -2,22 +2,16 @@
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
-<title>Submit Plugin to Cytoscape</title>
-	<link rel="stylesheet" type="text/css" media="screen" href="/css/cytoscape.css">
-	<link rel="shortcut icon" href="images/cyto.ico">
+<title>Frequently Asked Questions</title>
+	<link rel="stylesheet" type="text/css" media="screen" href="/cyto_web/css/cytoscape.css">
+	<link rel="shortcut icon" href="/cyto_web/images/cyto.ico">
 </head>
 <body bgcolor="#ffffff">
-<table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
-	<tbody>
-		<tr>
-			<td width="10">&nbsp;
-			</td>
-			<td valign="bottom">
-				<h1>Frequently Asked Questions</h1>
-			</td>
-		</tr>
-	</tbody>
-</table>
+
+<body bgcolor="#ffffff">
+<div id="topbar">
+	<div class="title">Frequently Asked Questions</div>
+</div>
 
 <?php include "nav.php"; ?>
 
@@ -28,7 +22,7 @@
   </tr>
   <tr>
     <td><strong>A</strong>: </td>
-    <td>The message “all plugins have been successfully loaded” means all of the plugins found in the plugins dir are actually loaded.  The "plugins" menu is somewhat misleading as there is no requirement that a plugin actually present itself there.  For example, the biopax plugin is accessed simply by importing a biopax formatted file.  Other plugins are found throughout the application, like the cPath plugin that is accessed through the "File -> New -> Network -> Construct network using cPath" menu.</td>
+    <td>The message ï¿½all plugins have been successfully loadedï¿½ means all of the plugins found in the plugins dir are actually loaded.  The "plugins" menu is somewhat misleading as there is no requirement that a plugin actually present itself there.  For example, the biopax plugin is accessed simply by importing a biopax formatted file.  Other plugins are found throughout the application, like the cPath plugin that is accessed through the "File -> New -> Network -> Construct network using cPath" menu.</td>
   </tr>
   <tr>
     <td>&nbsp;</td>
