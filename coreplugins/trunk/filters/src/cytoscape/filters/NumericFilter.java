d1c50872ce724942b4e4c2f2519b34d32831cc27
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-01 18:02:37 +0000

    Redefined for dynamic filter
    git-svn-id: http://chianti.ucsd.edu/svn@11617 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/filters/src/cytoscape/filters/NumericFilter.java
+++ coreplugins/trunk/filters/src/cytoscape/filters/NumericFilter.java
@@ -36,83 +36,130 @@
 
 package cytoscape.filters;
 
-
+import giny.model.Edge;
+import giny.model.Node;
 
 import java.util.*;
+
+import csplugins.quickfind.util.QuickFind;
+import csplugins.widgets.autocomplete.index.TextIndex;
+import cytoscape.CyNetwork;
+import cytoscape.Cytoscape;
+import csplugins.widgets.autocomplete.index.Hit;
+import csplugins.widgets.autocomplete.index.NumberIndex;
 
 
 /**
  * This is a Cytoscape specific filter that will pass nodes if
  * a selected attribute matches a specific value.
  */
-public class NumericFilter extends AtomicFilter {
-	
-	//---------------------------------------//
-	// Constructor
-	//----------------------------------------//
+
+public class NumericFilter<T extends Number> extends AtomicFilter {
+
+	private T lowBound, highBound;
+	private NumberIndex numberIndex = null;
 
 	/**
-	 * Creates a new NumericFilter
+	 * Creates a new NumericFilter, parameterized with interger/float/double
 	 */
-	public NumericFilter(String pAttributeName) {
-		super(pAttributeName, null);
-	}
-
-	public NumericFilter(String pAttributeName, double pLowBound, double pUpBound) {
-		super(pAttributeName, null);
-		searchValues = new String[2];
-		searchValues[0] = Double.toString(pLowBound);
-		searchValues[1] = Double.toString(pUpBound);
-	}
-
-	public NumericFilter(String attributeName, String[] searchValues) {
-		super(attributeName, searchValues);
+	public NumericFilter(String pName, String pControllingAttribute, T pLowBound, T pUpBound) {
+		super(pName,pControllingAttribute);
+		//controllingAttribute = pControllingAttribute;
+		lowBound = pLowBound;
+		highBound = pUpBound;
 	}
 	
-	public Double getLowValue(){
-		return new Double(searchValues[0]);
+	public boolean passesFilter(Object obj) {
+		return false;
+	}
+	public T getLowBound(){
+		return lowBound;
 	}
 
-	public Double getHighValue(){
-		return new Double(searchValues[1]);
+	public T getHighBound(){
+		return highBound;
 	}
 
-	public void setLowValue(Double pLowValue){
-		if (searchValues == null){
-			searchValues = new String[2];
-		}
-		searchValues[0] = pLowValue.toString();
+	public void setLowBound(T pLowBound){
+		lowBound = pLowBound;
 	}
 
-	public void setHighValue(Double pHighValue){
-		if (searchValues == null){
-			searchValues = new String[2];
-		}
-		searchValues[1] = pHighValue.toString();
-	}
-	public void setLowValue(String pLowValue){
-		if (searchValues == null){
-			searchValues = new String[2];
-		}
-		searchValues[0] = pLowValue;
+	public void setUpBound(T pUpBound){
+		highBound = pUpBound;
 	}
 
-	public void setHighValue(String pHighValue){
-		if (searchValues == null){
-			searchValues = new String[2];
-		}
-		searchValues[1] = pHighValue;
+	public void setRange(T pLowBound, T pUpBound){
+		lowBound = pLowBound;
+		highBound = pUpBound;
+	}
+
+	public NumberIndex getNumberIndex() {
+		return numberIndex;
 	}
 	
+	public void setNumberIndex(NumberIndex pNumberIndex) {
+		numberIndex = pNumberIndex;
+	}
+
+	
+	public void apply() {
+		
+		if (network == null) {
+			network = Cytoscape.getCurrentNetwork();					
+		}
+		
+		List<Node> nodes_list = null;
+		List<Edge> edges_list=null;
+
+		if (index_type == QuickFind.INDEX_NODES) {
+			nodes_list = network.nodesList();
+			objectCount = nodes_list.size();
+		}
+		else if (index_type == QuickFind.INDEX_EDGES) {
+			edges_list = network.edgesList();
+			objectCount = edges_list.size();
+		}
+		else {
+			System.out.println("StringFilter: Index_type is undefined.");
+			return;
+		}
+
+		bits = new BitSet(objectCount); // all the bits are false initially
+
+		//System.out.println(" NumberFilter.apply(): objectCount = " + objectCount);
+		List list = numberIndex.getRange(lowBound, highBound);
+
+		if (list.size() == 0) {
+			return;
+		}
+
+		int index;		
+		if (index_type == QuickFind.INDEX_NODES) {
+			for (Object obj : list) {
+				index = nodes_list.lastIndexOf((Node) obj);
+				bits.set(index, true);
+			}
+		} else if (index_type == QuickFind.INDEX_EDGES) {
+			for (Object obj : list) {
+				index = edges_list.lastIndexOf((Edge) obj);
+				bits.set(index, true);
+			}
+		}		
+		
+		if (negation) {
+			bits.flip(0, objectCount);
+		}		
+	}
+
 	/**
 	 * 
 	 */
 	public String toString() {
-		return attributeName + ","+searchValues[0]+"," + searchValues[1];
+		return "NumericFilter="+name+":"+controllingAttribute + ":" + negation+ ":"+lowBound+":" + highBound+ ":"+index_type;
 	}
 	
-	public NumericFilter clone() {
-		return new NumericFilter(attributeName, searchValues);
-	}
+	//public NumericFilter clone() {
+	//	return new NumericFilter(attributeName, searchValues);
+	//}
 
 }
