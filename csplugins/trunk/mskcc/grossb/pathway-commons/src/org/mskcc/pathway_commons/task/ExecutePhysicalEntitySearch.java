1fac3d47f8f7787b2c659f1115dcfebcbaf09d1a
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-04 20:44:49 +0000

    Getting first draft set-up
    git-svn-id: http://chianti.ucsd.edu/svn@11674 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/mskcc/grossb/pathway-commons/src/org/mskcc/pathway_commons/task/ExecutePhysicalEntitySearch.java
@@ -0,0 +1,78 @@
+package org.mskcc.pathway_commons.task;
+
+import org.mskcc.pathway_commons.web_service.PathwayCommonsWebApi;
+import cytoscape.task.Task;
+import cytoscape.task.TaskMonitor;
+
+/**
+ * Controller for Executing a Physical Entity Search.
+ *
+ * @author Ethan Cerami.
+ */
+public class ExecutePhysicalEntitySearch implements Task {
+    private PathwayCommonsWebApi webApi;
+    private String keyword;
+    private int ncbiTaxonomyId;
+    private int startIndex;
+    private TaskMonitor taskMonitor;
+
+    /**
+     * Constructor.
+     * @param webApi            Pathway Commons Web Api.
+     * @param keyword           Keyword
+     * @param ncbiTaxonomyId    NCBI Taxonomy ID.
+     * @param startIndex        Start Index.
+     */
+    public ExecutePhysicalEntitySearch (PathwayCommonsWebApi webApi, String keyword,
+            int ncbiTaxonomyId, int startIndex) {
+        this.webApi = webApi;
+        this.keyword = keyword;
+        this.ncbiTaxonomyId = ncbiTaxonomyId;
+        this.startIndex = startIndex;
+    }
+
+    /**
+     * Our implementation of Task.halt()
+     */
+    public void halt() {
+        // Task can not currently be halted.
+    }
+
+    /**
+     * Our implementation of Task.setTaskMonitor().
+     *
+     * @param taskMonitor TaskMonitor
+     */
+    public void setTaskMonitor(TaskMonitor taskMonitor) throws IllegalThreadStateException {
+        this.taskMonitor = taskMonitor;
+    }
+
+    /**
+     * Our implementation of Task.getTitle.
+     *
+     * @return Task Title.
+     */
+    public String getTitle() {
+        return "Searching Pathway Commons";
+    }
+
+    /**
+     * Our implementation of Task.run().
+     */
+    public void run() {
+        try {
+            // read the network from pathway commons
+            taskMonitor.setPercentCompleted(-1);
+            taskMonitor.setStatus("Searching Pathway Commons...");
+
+            //  Execute the Search
+            webApi.searchPhysicalEntities(keyword, ncbiTaxonomyId, startIndex);
+            
+            // update the task monitor
+            taskMonitor.setStatus("Done");
+            taskMonitor.setPercentCompleted(100);
+        } catch (Exception e) {
+            taskMonitor.setException(e, "Error executing search.");
+        }
+    }
+}
