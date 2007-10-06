4ac177e5464350912ace87a87a0744afeeee453f
akuchinsky <akuchinsky@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-06 00:05:35 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@11711 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsf/apico/SubgeneViewer/src/org/genmapp/subgeneviewer/readers/ParseSplice.java
@@ -0,0 +1,12 @@
+package org.genmapp.subgeneviewer.readers;
+
+import org.genmapp.subgeneviewer.splice.view.SpliceNetworkView;
+
+public class ParseSplice {
+	
+	public ParseSplice (SpliceNetworkView view, String nodeId)
+	{
+		
+	}
+
+}
