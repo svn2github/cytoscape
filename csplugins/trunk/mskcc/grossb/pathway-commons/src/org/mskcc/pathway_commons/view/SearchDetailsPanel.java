6911981e610ea72020bd0a83157411b3c00b2119
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-11 21:03:25 +0000

    Modified layout, so that it fits nicely into the SOUTH CytoPanel;  also added download of pathways
    git-svn-id: http://chianti.ucsd.edu/svn@11760 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/mskcc/grossb/pathway-commons/src/org/mskcc/pathway_commons/view/SearchDetailsPanel.java
+++ csplugins/trunk/mskcc/grossb/pathway-commons/src/org/mskcc/pathway_commons/view/SearchDetailsPanel.java
@@ -1,18 +1,30 @@
 package org.mskcc.pathway_commons.view;
 
+import cytoscape.actions.LoadNetworkTask;
+
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.URL;
+import java.net.MalformedURLException;
+
+import org.mskcc.pathway_commons.web_service.PathwayCommonsWebApiListener;
+import org.mskcc.pathway_commons.web_service.PathwayCommonsWebApi;
+import org.mskcc.pathway_commons.web_service.CPathProtocol;
+import org.mskcc.pathway_commons.schemas.search_response.SearchResponseType;
+import org.mskcc.pathway_commons.view.model.InteractionTableModel;
+import org.mskcc.pathway_commons.view.model.PathwayTableModel;
+import org.mskcc.pathway_commons.util.NetworkUtil;
 
 /**
  * Search Details Panel.
  *
  * @author Ethan Cerami.
  */
-class SearchDetailsPanel extends JPanel {
+public class SearchDetailsPanel extends JPanel {
 
     /**
      * Constructor.
@@ -20,14 +32,14 @@ class SearchDetailsPanel extends JPanel {
      * @param interactionTableModel InteractionTableModel Object.
      * @param pathwayTableModel     PathwayTableModel Object.
      */
-    public SearchDetailsPanel(DefaultTableModel interactionTableModel,
-            DefaultTableModel pathwayTableModel) {
-        GridLayout gridLayout = new GridLayout(2, 0);
+    public SearchDetailsPanel(InteractionTableModel interactionTableModel,
+            PathwayTableModel pathwayTableModel) {
+        GridLayout gridLayout = new GridLayout(1, 0);
         setLayout(gridLayout);
         JPanel interactionPane = createInteractionBundleTable(interactionTableModel);
         JScrollPane pathwayPane = createPathwayTable(pathwayTableModel);
         add(pathwayPane);
-        add(interactionPane);
+        //add(interactionPane);
     }
 
     /**
@@ -35,7 +47,7 @@ class SearchDetailsPanel extends JPanel {
      *
      * @return JScrollPane Object.
      */
-    private JPanel createInteractionBundleTable(DefaultTableModel interactionTableModel) {
+    private JPanel createInteractionBundleTable(InteractionTableModel interactionTableModel) {
         JPanel panel = new JPanel();
         panel.setBorder(new TitledBorder("Interactions"));
         panel.setLayout(new BorderLayout());
@@ -67,7 +79,7 @@ class SearchDetailsPanel extends JPanel {
      *
      * @return JScrollPane Object.
      */
-    private JScrollPane createPathwayTable(DefaultTableModel pathwayTableModel) {
+    private JScrollPane createPathwayTable(final PathwayTableModel pathwayTableModel) {
         final JTable pathwayTable = new JTable(pathwayTableModel);
         pathwayTable.setAutoCreateColumnsFromModel(true);
         pathwayTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -76,12 +88,31 @@ class SearchDetailsPanel extends JPanel {
             public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 2) {
                     int rows[] = pathwayTable.getSelectedRows();
-                    JOptionPane.showMessageDialog(new JFrame(), "Downloading Pathway:  " + rows[0]);
+                    if (rows.length > 0) {
+                        downloadPathway(rows, pathwayTableModel);
+                    }
                 }
             }
         });
         JScrollPane scrollPane = new JScrollPane(pathwayTable);
         scrollPane.setBorder(new TitledBorder("Pathways (double-click to download)"));
         return scrollPane;
+    }
+
+    /**
+     * Downloads a single pathway in a new thread.
+     * @param rows                  Selected row.
+     * @param pathwayTableModel     Pathway Table Model.
+     */
+    private void downloadPathway(int[] rows, PathwayTableModel pathwayTableModel) {
+        long internalId = pathwayTableModel.getInternalId(rows[0]);
+        CPathProtocol protocol = new CPathProtocol();
+        protocol.setCommand(CPathProtocol.COMMAND_GET_RECORD_BY_CPATH_ID);
+        protocol.setQuery(Long.toString(internalId));
+        protocol.setFormat(CPathProtocol.FORMAT_BIOPAX);
+        String uri = protocol.getURI();
+        System.out.println("Connecting to:  " + uri);
+        NetworkUtil networkUtil = new NetworkUtil(uri, null, false, null);
+        networkUtil.start();
     }
 }
