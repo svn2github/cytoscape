05fa9448d63e104a731469e674a5e3d3bf691077
akuchinsky <akuchinsky@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-14 06:41:41 +0000

    made SubgeneNodeView a subclass of JComponent, so that it can paint itself
    git-svn-id: http://chianti.ucsd.edu/svn@11778 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsf/apico/SubgeneViewer/src/org/genmapp/subgeneviewer/view/SubgeneNodeView.java
+++ csplugins/trunk/ucsf/apico/SubgeneViewer/src/org/genmapp/subgeneviewer/view/SubgeneNodeView.java
@@ -2,37 +2,21 @@ package org.genmapp.subgeneviewer.view;
 
 import java.awt.Rectangle;
 
-import cytoscape.CyNode;
+import javax.swing.JComponent;
 
 
 
-public class SubgeneNodeView {
+public class SubgeneNodeView extends JComponent {
 
 	private String _id;
 
-	private Rectangle _bounds = null;
-	
-
-	public Rectangle getBounds() {
-		return _bounds;
-	}
-
-	public void setBounds(Rectangle bounds) {
-		_bounds = bounds;
-	}
-	
-	public void setBounds(int x, int y, int width, int height)
-	{
-		_bounds = new Rectangle(x, y, width, height);
-	}
-	
 	/**
 	 * computeBounds	should be overwritten in subclasses, e.g. block, region, feature
 	 * @return
 	 */
 	public Rectangle computeBounds()
 	{
-		return _bounds;
+		return this.getBounds();
 	}
 
 	public String getId() {
