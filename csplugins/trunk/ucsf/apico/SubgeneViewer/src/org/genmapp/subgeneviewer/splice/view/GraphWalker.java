4e5a57097596e363bc0bc80c1d6de969069ad353
akuchinsky <akuchinsky@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-18 01:19:36 +0000

    renders splice events and start sites
    git-svn-id: http://chianti.ucsd.edu/svn@11823 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsf/apico/SubgeneViewer/src/org/genmapp/subgeneviewer/splice/view/GraphWalker.java
+++ csplugins/trunk/ucsf/apico/SubgeneViewer/src/org/genmapp/subgeneviewer/splice/view/GraphWalker.java
@@ -1,8 +1,8 @@
 package org.genmapp.subgeneviewer.splice.view;
 
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.util.Iterator;
 
@@ -51,7 +51,10 @@ public class GraphWalker {
 	private static final int START_SITE_HEIGHT = NODE_HEIGHT;
 	private static final int SPLICE_EVENT_HEIGHT = NODE_HEIGHT;
 	private static final int LABEL_TRACK_HEIGHT = NODE_HEIGHT;
+
 	private static final int TITLE_LEGEND_HEIGHT = NODE_HEIGHT;
+	private static final int FLAG_WIDTH = 10;  // width of bounding box for triangle of flag on StartSite
+	private static final int FLAG_HEIGHT = 10;  // height of bounding box for triangle of flag on StartSite
 
 	/**
 	 * perform a depth-first search of a SubgeneNetworkView and set bounds for all Blocks, Regions, Features
@@ -97,43 +100,49 @@ public class GraphWalker {
 				}
 				
 				// regions sit within and subdivide blocks
-//				System.out.println("setting bounds for region: " + region);
-//				System.out.println("of block type: " + type);
-				if (type.trim().equals("I"))
-				{
-					region.setBounds(regionXOffset, blockBoundsY + (NODE_HEIGHT / 2) - 1, xOffset - regionXOffset, 
-							2);  // 2-pixel line for introns
+				// System.out.println("setting bounds for region: " + region);
+				// System.out.println("of block type: " + type);
+				if (type.trim().equals("I")) {
+					region
+							.setBounds(regionXOffset, blockBoundsY
+									+ (NODE_HEIGHT / 2) - 1, xOffset
+									- regionXOffset, 2); // 2-pixel line for
+															// introns
+				} else if (type.trim().equals("e")) {
+					region.setBounds(regionXOffset, blockBoundsY
+							+ (NODE_HEIGHT / 4), xOffset - regionXOffset,
+							NODE_HEIGHT / 2); // block/region height is half
+												// of a feature's height
 				}
-				else if (type.trim().equals("e"))
-				{
-					region.setBounds(regionXOffset, blockBoundsY + (NODE_HEIGHT / 4), xOffset - regionXOffset, 
-							NODE_HEIGHT / 2);  // block/region height is half of a feature's height
-				}
-//				System.out.println("set bounds for region: " + region);
-//				System.out.println("= bounds: " + region.getBounds());
+				// System.out.println("set bounds for region: " + region);
+				// System.out.println("= bounds: " + region.getBounds());
 
 			}
 
-			if (type.trim().equals("I"))
-			{
-				block.setBounds(blockXOffset, blockBoundsY + (NODE_HEIGHT / 2) - 1, xOffset - blockXOffset, 
-						2);  // 2-pixel line for introns
+			if (type.trim().equals("I")) {
+				block.setBounds(blockXOffset, blockBoundsY + (NODE_HEIGHT / 2)
+						- 1, xOffset - blockXOffset, 2); // 2-pixel line for
+															// introns
+			} else if (type.trim().equals("e")) {
+				block.setBounds(regionXOffset,
+						blockBoundsY + (NODE_HEIGHT / 4), xOffset
+								- blockXOffset, NODE_HEIGHT / 2); // block/region
+																	// height is
+																	// half of a
+																	// feature's
+																	// height
 			}
-			else if (type.trim().equals("e"))
-			{
-				block.setBounds(regionXOffset, blockBoundsY + (NODE_HEIGHT / 4), xOffset - blockXOffset, 
-						NODE_HEIGHT / 2);  // block/region height is half of a feature's height
-			}		
-		}			
+		}
+
 	}
-	
+
 	/**
 	 * perform a depth-first search of a SubgeneNetworkView and render all
 	 * Blocks, Regions, Features, Splice Events, and Start sites
+	 * 
 	 * @param view
 	 */
-	public static void renderView (SpliceNetworkView view)
-	{
+	public static void renderView(SpliceNetworkView view) {
 		Block block;
 		Region region;
 		Feature feature;
@@ -141,41 +150,102 @@ public class GraphWalker {
 		Rectangle rect;
 
 		g.setColor(Color.black);
-		
+
 		Iterator<Block> blocks = view.getBlockInterator();
-		while (blocks.hasNext())
-		{
-			
+		while (blocks.hasNext()) {
+
 			block = blocks.next();
 			Iterator<Region> regions = block.getRegionInterator();
-			while (regions.hasNext())
-			{
+			while (regions.hasNext()) {
 				region = regions.next();
 				rect = region.getBounds();
-//				System.out.println ("Drawing region: " + region.getId() + 
-//						" in bounding box " + rect);
+				// System.out.println ("Drawing region: " + region.getId() +
+				// " in bounding box " + rect);
 				g.drawRect(rect.x, rect.y, rect.width, rect.height);
-				
-				
+
 				Iterator<Feature> features = region.getFeatureInterator();
-				while (features.hasNext())
-				{
+				while (features.hasNext()) {
 					feature = features.next();
 					rect = feature.getBounds();
-//					System.out.println ("Drawing feature: " + feature.getId() + 
-//							" in bounding box " + rect);
+					// System.out.println ("Drawing feature: " + feature.getId()
+					// +
+					// " in bounding box " + rect);
 					g.drawRect(rect.x, rect.y, rect.width, rect.height);
 
-//					g.drawString(feature.getId(), rect.x + 1, rect.y + 1);
+					// g.drawString(feature.getId(), rect.x + 1, rect.y + 1);
 					g.setColor(Color.blue);
-					
-					g.drawString(feature.getFeature_id().trim(), 
-							rect.x, rect.y + g.getFont().getSize());
+
+					g.drawString(feature.getFeature_id().trim(), rect.x + 3,
+							rect.y + g.getFont().getSize() + 3);
 					g.setColor(Color.black);
 
 				}
+				// now paint Splice Events
+				Iterator<SpliceEvent> splices = region.listOfSpliceEvents.iterator();
+
+				while (splices.hasNext()) {
+					SpliceEvent splice = splices.next();
+					String blockString = splice.get_toBlock();
+					Block toBlock = view.getBlock(blockString);
+					String regionString = splice.get_toRegion();
+					Region toRegion = toBlock.getRegion(regionString);
+
+					Rectangle regionBounds = view.getBounds(region);
+					if (regionBounds == null) {
+						return;
+					}
+
+					int startX = regionBounds.x + regionBounds.width;
+					int startY = regionBounds.y + regionBounds.height;
+
+					System.out.println("Getting bounds for region: " + toRegion + " with name: " + regionString);
+					Rectangle toRegionBounds = view.getBounds(toRegion);
+					if (toRegionBounds == null) {
+						return;
+					}
+
+					int endX = toRegionBounds.x;
+					int endY = toRegionBounds.y + toRegionBounds.height;
+
+					g.setColor(Color.blue);
+
+					// draw the lines.
+					// consider using Graphics2D so that we have control over width of line
+					g.drawLine(startX, startY, ((startX + endX) / 2), startY + NODE_HEIGHT);
+					g.drawLine(((startX + endX) / 2), startY + NODE_HEIGHT, endX, endY);
+				}
+				
+				// now render Start Site, at this region
+				if (region.containsStartSite())
+				{
+					Rectangle regionBounds = region.getBounds();
+					if (regionBounds == null)
+					{
+						return;
+					}
+
+					int startX = regionBounds.x;
+					int startY = regionBounds.y - START_SITE_HEIGHT; // draw flag above region
+					int endY = regionBounds.y;
+					
+					g.setColor(Color.red);
+					
+					// draw the staff of the flag
+					// consider using Graphics2D so that we have control over width of line
+					g.drawLine(startX, startY, startX, endY);
+					
+					// now draw the flag
+					Polygon p = new Polygon();
+					p.addPoint(startX, startY);
+					p.addPoint(startX + FLAG_WIDTH, startY + (FLAG_HEIGHT / 2));
+					p.addPoint(startX, startY + FLAG_HEIGHT);
+					g.fillPolygon(p);
+					g.setColor(Color.black);
+										
+				}
+
 			}
-		}			
+		}
 	}
 
 }
