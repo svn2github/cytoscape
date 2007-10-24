f4acc6881ed30847eb232e95d6562cef9d68f9fa
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-24 14:36:14 +0000

    Added explicit error code
    git-svn-id: http://chianti.ucsd.edu/svn@11910 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/mskcc/grossb/pathway-commons/src/org/mskcc/pathway_commons/web_service/CPathException.java
+++ csplugins/trunk/mskcc/grossb/pathway-commons/src/org/mskcc/pathway_commons/web_service/CPathException.java
@@ -6,20 +6,98 @@ package org.mskcc.pathway_commons.web_service;
  * @author Ethan Cerami.
  */
 public class CPathException extends Exception {
-	/**
-	 * Constructor.
-	 * @param msg Error message.
-	 */
-	public CPathException(String msg) {
-		super(msg);
-	}
+    /**
+     * Error:  Canceled by User.
+     */
+    public final static int ERROR_CANCELED_BY_USER = 1;
 
-	/**
+    /**
+     * Error:  Unknown Host.
+     */
+    public final static int ERROR_UNKNOWN_HOST = 2;
+
+    /**
+     * Error:  Network IO.
+     */
+    public final static int ERROR_NETWORK_IO = 3;
+
+    /**
+     * Error:  XML Parsing.
+     */
+    public final static int ERROR_XML_PARSING = 4;
+
+    /**
+     * Error:  Web Service API.
+     */
+    public final static int ERROR_WEB_SERVICE_API = 5;
+
+    /**
+     * Error HTTP
+     */
+    public final static int ERROR_HTTP = 6;
+
+    private int errorCode;
+    private String errorDetail;
+
+    /**
 	 * Constructor.
-	 * @param msg Error message.
+     * @param errorCode Error Code.
 	 * @param t Root throwable.
 	 */
-	public CPathException(String msg, Throwable t) {
-		super(msg, t);
+	public CPathException(int errorCode, Throwable t) {
+        super(t);
+        this.errorCode = errorCode;
 	}
+
+    public CPathException (int errorCode, String errorDetail) {
+        this.errorDetail = errorDetail;
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * Gets the Error Code.
+     * @return Error Code.
+     */
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    /**
+     * Gets Error Message.
+     * @return Error Message.
+     */
+    public String getMessage() {
+        String msg = null;
+        switch (errorCode) {
+            case ERROR_CANCELED_BY_USER:
+                msg =  "Canceled by user.";
+                break;
+            case ERROR_UNKNOWN_HOST:
+                msg = "Network error occurred while tring to connect to "
+                        + "remote web service.  Please check your server and network settings, "
+                        + "and try again.";
+                break;
+            case ERROR_NETWORK_IO:
+                msg = "Network error occurred while tring to connect to "
+                        + "remote web service.  Please check your server and network settings, "
+                        + "and try again.";
+                break;
+            case ERROR_XML_PARSING:
+                msg = "Error occurred while trying to parse XML results "
+                    + "retrieved from remote web service.  "
+                    + "Please check your server and network settings, "
+                    + "and try again.";
+                break;
+            case ERROR_HTTP:
+                 msg = "Network error occurred while trying to connect to "
+                        + "remote web service.  (Details:   " + errorDetail + ")";
+                break;
+            case ERROR_WEB_SERVICE_API:
+                msg = "Error occurred while trying to connect to remote web service.  "
+                        + "(Details:  " + errorDetail + ")";
+                break;
+
+        }
+        return msg;
+    }
 }
