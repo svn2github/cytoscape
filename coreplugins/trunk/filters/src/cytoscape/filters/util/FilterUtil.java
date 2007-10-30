ce020d924e49592ce7d1cd6382c66ac8178bfe43
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-30 19:08:22 +0000

    Clean up
    git-svn-id: http://chianti.ucsd.edu/svn@12050 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/filters/src/cytoscape/filters/util/FilterUtil.java
+++ coreplugins/trunk/filters/src/cytoscape/filters/util/FilterUtil.java
@@ -28,6 +28,7 @@ import csplugins.quickfind.util.QuickFind;
 import cytoscape.util.CytoscapeAction;
 import cytoscape.data.CyAttributes;
 import cytoscape.filters.AdvancedSetting;
+import cytoscape.filters.FilterPlugin;
 
 public class FilterUtil {
 	
@@ -42,6 +43,8 @@ public class FilterUtil {
 	public static void doSelection(CompositeFilter pFilter) {
 		System.out.println("Entering FilterUtil.doSelection() ...");
 		
+		return;
+		/*
 		pFilter.apply();
 		
 		CyNetwork network = Cytoscape.getCurrentNetwork(); 
@@ -86,6 +89,7 @@ public class FilterUtil {
 		}
 
 		Cytoscape.getCurrentNetworkView().updateView();
+		*/
 	}
 	
 	
@@ -109,114 +113,19 @@ public class FilterUtil {
 		return attributes;
 	}
 	
-	public static boolean isFilterNameDuplicated(Vector<CompositeFilter> pFilterVect, String pFilterName) {
+	public static boolean isFilterNameDuplicated(String pFilterName) {
 		System.out.println("Entering FilterUtil.isFilterNameDuplicated() ...");
-		if (pFilterVect == null || pFilterVect.size() == 0)
+		if (FilterPlugin.getAllFilterVect() == null || FilterPlugin.getAllFilterVect().size() == 0)
 			return false;
 		
-		for (int i=0; i<pFilterVect.size(); i++) {
-			CompositeFilter theFilter = (CompositeFilter) pFilterVect.elementAt(i);
+		for (int i=0; i<FilterPlugin.getAllFilterVect().size(); i++) {
+			CompositeFilter theFilter = (CompositeFilter) FilterPlugin.getAllFilterVect().elementAt(i);
 			if (pFilterName.equalsIgnoreCase(theFilter.getName().trim())) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	
-	public static CompositeFilter createFilterFromString(Vector pFilterStrVect) {
-		if (pFilterStrVect==null || pFilterStrVect.size() == 0) {
-			return null;
-		}
-		return null;
-		
-/*		
-		//System.out.println("createFilterFromString()\npFilterStrVect.toString() =\n" + pFilterStrVect.toString());
-		
-		CompositeFilter theFilter = new CompositeFilter();
-		String tmpStr;
-		String [] tmpStrArray = null;
-		
-		//name
-		tmpStr = (String) pFilterStrVect.elementAt(0);		
-		tmpStrArray = tmpStr.split("=");
-		theFilter.setName(tmpStrArray[1].trim());
-		//description
-		tmpStr = (String) pFilterStrVect.elementAt(1);		
-		tmpStrArray = tmpStr.split("=");
-		if (tmpStrArray[1].trim().equalsIgnoreCase("null")){
-			theFilter.setDescription(null);	
-		}
-		else {
-			theFilter.setDescription(tmpStrArray[1].trim());			
-		}
-		
-		//AdvancedSettiing
-		AdvancedSetting advSetting = theFilter.getAdvancedSetting();
-
-		//global
-		tmpStr = (String) pFilterStrVect.elementAt(2);		
-		tmpStrArray = tmpStr.split("=");	
-		advSetting.setGlobal((new Boolean(tmpStrArray[1].trim())).booleanValue());			
-		//session
-		tmpStr = (String) pFilterStrVect.elementAt(3);		
-		tmpStrArray = tmpStr.split("=");		
-		advSetting.setSession((new Boolean(tmpStrArray[1].trim())).booleanValue());
-		//node
-		tmpStr = (String) pFilterStrVect.elementAt(4);		
-		tmpStrArray = tmpStr.split("=");
-		advSetting.setNode((new Boolean(tmpStrArray[1].trim())).booleanValue());
-		//edge
-		tmpStr = (String) pFilterStrVect.elementAt(5);		
-		tmpStrArray = tmpStr.split("=");
-		advSetting.setEdge((new Boolean(tmpStrArray[1].trim())).booleanValue());
-		//source
-		tmpStr = (String) pFilterStrVect.elementAt(6);		
-		tmpStrArray = tmpStr.split("=");
-		advSetting.setSource((new Boolean(tmpStrArray[1].trim())).booleanValue());
-		//target
-		tmpStr = (String) pFilterStrVect.elementAt(7);		
-		tmpStrArray = tmpStr.split("=");
-		advSetting.setTarget((new Boolean(tmpStrArray[1].trim())).booleanValue());
-
-		//AND
-		tmpStr = (String) pFilterStrVect.elementAt(8);		
-		tmpStrArray = tmpStr.split("=");
-		advSetting.setRelationAND((new Boolean(tmpStrArray[1].trim())).booleanValue());
-		
-		//OR
-		tmpStr = (String) pFilterStrVect.elementAt(9);		
-		tmpStrArray = tmpStr.split("=");
-		advSetting.setRelationOR((new Boolean(tmpStrArray[1].trim())).booleanValue());
-		
-		for (int i=10; i < pFilterStrVect.size(); i++) {
-			tmpStr = (String) pFilterStrVect.elementAt(i);
-			tmpStrArray = tmpStr.split("=");
-			if (tmpStrArray[0].trim().endsWith("StringFilter")) {
-				tmpStrArray = tmpStrArray[1].split(",");
-				String atomicFilterName = tmpStrArray[0].trim();
-				String serarchStr = tmpStrArray[1].trim();
-				StringFilter stringFilter = new StringFilter(atomicFilterName);
-				stringFilter.setSearchStr(serarchStr);
-				theFilter.getAtomicFilterVect().add(stringFilter);
-			}
-			else { // NumericFilter
-				tmpStrArray = tmpStrArray[1].split(",");
-				String atomicFilterName = tmpStrArray[0].trim();
-				String[] serarchValues = new String[2];
-				serarchValues[0] =tmpStrArray[1].trim();
-				serarchValues[1] =tmpStrArray[2].trim();
-				
-				NumericFilter numericFilter = new NumericFilter(atomicFilterName);
-				numericFilter.setSearchValues(serarchValues);
-				theFilter.getAtomicFilterVect().add(numericFilter);
-			}
-		}
-		
-		return theFilter;
-		*/
-	}
-
 }
 
 
