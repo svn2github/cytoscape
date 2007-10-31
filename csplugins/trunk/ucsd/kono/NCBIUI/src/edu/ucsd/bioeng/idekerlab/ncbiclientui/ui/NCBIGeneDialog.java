92f842b25521c700157e8dda7862a38b88822d6a
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-10-31 23:15:35 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@12068 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/kono/NCBIUI/src/edu/ucsd/bioeng/idekerlab/ncbiclientui/ui/NCBIGeneDialog.java
@@ -0,0 +1,39 @@
+package edu.ucsd.bioeng.idekerlab.ncbiclientui.ui;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+import cytoscape.Cytoscape;
+
+public class NCBIGeneDialog extends JDialog implements PropertyChangeListener {
+	
+	public NCBIGeneDialog() {
+		super(Cytoscape.getDesktop(), false);
+		setTitle("NCBI Entrez Gene");
+		
+		try {
+			JPanel panel = new NCBIGenePanel();
+			
+			add(new NCBIGenePanel());
+			
+			panel.addPropertyChangeListener(this);
+			add(panel);
+			pack();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void propertyChange(PropertyChangeEvent evt) {
+		if(evt.getPropertyName().equals("CLOSE")) {
+			dispose();
+		}
+		
+	}
+
+}
