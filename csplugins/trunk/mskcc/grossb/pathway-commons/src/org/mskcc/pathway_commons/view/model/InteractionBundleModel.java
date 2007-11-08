5218f8d904ec96ed27a0e8c7fdad2f80625dcfe0
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-11-08 21:20:35 +0000

    Refactored to use RecordList Object
    git-svn-id: http://chianti.ucsd.edu/svn@12125 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/mskcc/grossb/pathway-commons/src/org/mskcc/pathway_commons/view/model/InteractionBundleModel.java
+++ csplugins/trunk/mskcc/grossb/pathway-commons/src/org/mskcc/pathway_commons/view/model/InteractionBundleModel.java
@@ -1,30 +1,35 @@
 package org.mskcc.pathway_commons.view.model;
 
+import org.mskcc.pathway_commons.schemas.summary_response.SummaryResponseType;
+
 import javax.swing.table.DefaultTableModel;
 import java.util.Vector;
 import java.util.ArrayList;
 import java.util.Observable;
 
 /**
- * Interaction Bundle Model.
+ * Contains information regarding the currently selected set of interaction bundles.
  *
  * @author Ethan Cerami
  */
 public class InteractionBundleModel extends Observable {
-    private int numInteractions;
+    private RecordList recordList;
 
     /**
-     * Constructor.
+     * Sets the SummaryResponse Object.
+     * @param recordList Record List.
      */
-    public InteractionBundleModel() {}
-
-    public int getNumInteractions() {
-        return numInteractions;
-    }
-
-    public void setNumInteractions(int numInteractions) {
-        this.numInteractions = numInteractions;
+    public void setRecordList (RecordList recordList) {
+        this.recordList = recordList;
         this.setChanged();
         this.notifyObservers();
+    }
+
+    /**
+     * Gets the Record List.
+     * @return RecordList Object.
+     */
+    public RecordList getRecordList() {
+        return recordList;
     }
 }
