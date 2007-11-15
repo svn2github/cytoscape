855d02654d069fd00acff15653bc30402f66c5b7
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-11-15 23:55:54 +0000

    Updated HTML to use new stylesheet
    git-svn-id: http://chianti.ucsd.edu/svn@12188 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/BioPAX/src/org/mskcc/biopax_plugin/view/AboutPanel.java
+++ coreplugins/trunk/BioPAX/src/org/mskcc/biopax_plugin/view/AboutPanel.java
@@ -40,6 +40,7 @@ import java.awt.*;
 import java.util.Properties;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 
 /**
@@ -54,23 +55,21 @@ public class AboutPanel extends JPanel {
 	 * @param title        PlugIn Title.
 	 * @param majorVersion PlugIn Major Version.
 	 * @param minorVersion PlugIn Minor Version.
-	 * @param bgColor      Background Color.
 	 */
-	public AboutPanel(String title, int majorVersion, int minorVersion, Color bgColor) {
-		this.setBackground(bgColor);
+	public AboutPanel(String title, int majorVersion, int minorVersion) {
 		this.setLayout(new BorderLayout());
 
 		JTextPane textPane = new JTextPane();
-		textPane.setEditable(false);
+        textPane.setBorder(new EmptyBorder(5,5,5,5));
+        textPane.setEditable(false);
 		textPane.setContentType("text/html");
-		textPane.setBackground(BioPaxDetailsPanel.BG_COLOR);
 
 		StringBuffer temp = new StringBuffer();
 		temp.append("<HTML><BODY>");
-		temp.append("<TABLE WIDTH=100% CELLPADDING=5 CELLSPACING=5>" + "<TR BGCOLOR='ECE9D8'><TD>");
-		temp.append("<FONT FACE=\"ARIAL\">");
-		temp.append(title + ", Version:  " + majorVersion + ". " + minorVersion);
-		temp.append("<P>");
+        temp.append("<h2>");
+        temp.append(title + ", Version:  " + majorVersion + ". " + minorVersion);
+        temp.append("</h2>");
+        temp.append("<P>");
 
 		Properties cytoProps = CytoscapeInit.getProperties();
 
@@ -84,24 +83,20 @@ public class AboutPanel extends JPanel {
 			} else {
 				temp.append("HTTP Proxy Support:  Not Enabled.");
 			}
-
-			temp.append("<P>");
 		}
 
-		temp.append("Extension released by:  Sander Group, "
-		            + "<A HREF=\"http://www.cbio.mskcc.org/\">"
+		temp.append("<BR><BR>Plugin released by:  Sander Group, "
+		            + "<A class=\"link\" HREF=\"http://www.cbio.mskcc.org/\">"
 		            + "Computational Biology Center</A>, "
-		            + "<A HREF=\"http://www.mskcc.org\">Memorial Sloan-Kettering "
+		            + "<A class=\"link\" HREF=\"http://www.mskcc.org\">Memorial Sloan-Kettering "
 		            + "Cancer Center</A>.");
-		temp.append("<P>For any questions concerning this extension, please "
-		            + "contact:<P>Gary Bader:  baderg AT mskcc.org"
-		            + "<BR>Ethan Cerami:  cerami AT cbio.mskcc.org"
-		            + "<BR>Benjamin Gross:  grossb AT cbio.mskcc.org" + "<P>License:  "
-		            + "This software is made available under the "
-		            + "<A HREF=\"http://www.gnu.org/licenses/lgpl.html\">LGPL "
+		temp.append("<BR><BR>For any questions concerning this plugin, please "
+		            + "contact:<BR><BR><UL><LI> - Gary Bader:  baderg AT mskcc.org"
+		            + "<LI> - Ethan Cerami:  cerami AT cbio.mskcc.org"
+		            + "<LI> - Benjamin Gross:  grossb AT cbio.mskcc.org"
+		            + "<BR><BR>This software is made available under the "
+		            + "<A class=\"link\" HREF=\"http://www.gnu.org/licenses/lgpl.html\">LGPL "
 		            + "(Lesser General Public License)</A>.");
-		temp.append("</FONT>");
-		temp.append("</TD></TR></TABLE>");
 		temp.append("</BODY></HTML>");
 		textPane.setText(temp.toString());
 		textPane.setText(temp.toString());
