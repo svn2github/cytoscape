04337de3c550c5e3a80bbebc4c0c48f9cae6e313
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-12-07 19:35:04 +0000

    clean up
    git-svn-id: http://chianti.ucsd.edu/svn@12486 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/filters/src/cytoscape/filters/InteractionFilter.java
+++ coreplugins/trunk/filters/src/cytoscape/filters/InteractionFilter.java
@@ -36,15 +36,7 @@
 
 package cytoscape.filters;
 
-import giny.model.Edge;
-import giny.model.Node;
-
-import java.util.*;
-
 import cytoscape.CyNetwork;
-import cytoscape.Cytoscape;
-import csplugins.quickfind.util.QuickFind;
-import csplugins.widgets.autocomplete.index.GenericIndex;
 
 
 public class InteractionFilter extends CompositeFilter {
@@ -53,7 +45,7 @@ public class InteractionFilter extends CompositeFilter {
 	public static int NODE_TARGET = 1;
 	public static int NODE_SOURCE_TARGET = 3;
 	
-	protected int nodeType = NODE_SOURCE;
+	protected int nodeType = NODE_UNDEFINED;
 
 	protected boolean isSourceChecked = true;
 	protected boolean isTargetChecked = true;
@@ -85,6 +77,18 @@ public class InteractionFilter extends CompositeFilter {
 			return;
 		}
 		nodeType = pNodeType;
+		if (nodeType == NODE_SOURCE) {
+			isSourceChecked = true;
+			isTargetChecked = false;
+		}
+		else if (nodeType == NODE_TARGET) {
+			isSourceChecked = false;
+			isTargetChecked = true;			
+		}
+		else if (nodeType == NODE_SOURCE_TARGET) {
+			isSourceChecked = true;
+			isTargetChecked = true;					
+		}
 		childChanged = true;
 	}
 
@@ -122,126 +126,17 @@ public class InteractionFilter extends CompositeFilter {
 		}
 	}
 	
-	
-	public BitSet getNodeBits() {
-		apply();
-		return node_bits;
-	}
-	
-	public BitSet getEdgeBits(){
-		apply();
-		return edge_bits;		
-	}
-	
-	
-	public void apply() {
-		if ( !childChanged ) 
-			return;
-
-		//Make sure the pass filter is current
-		if (passFilter == null) {
-			passFilter = new InteractionFilter("None");
-		}
-		
-		if (!passFilter.getName().equalsIgnoreCase("None")) {
-			passFilter.setNetwork(network);
-			passFilter.apply();			
-		}	
-
-		List<Node> nodes_list = null;
-		List<Edge> edges_list=null;
-
-		int objectCount = -1;
-		
-		if (advancedSetting.isNodeChecked()) {
-			nodes_list = network.nodesList();
-			objectCount = nodes_list.size();
-			node_bits = new BitSet(objectCount); // all the bits are false at very beginning
-			
-			for (int i=0; i<objectCount; i++) {
-				if (isHit(nodes_list.get(i))) {
-					node_bits.set(i);
-				}
-			}
-		}
-		else if (advancedSetting.isEdgeChecked()) {
-			edges_list = network.edgesList();
-			objectCount = edges_list.size();
-			edge_bits = new BitSet(objectCount); // all the bits are false at very beginning			
-
-			for (int i=0; i<objectCount; i++) {
-				if (isHit(edges_list.get(i))) {
-					edge_bits.set(i);
-				}
-			}
-		}
-		else {
-			System.out.println("TopologyFilter: objectType is undefined.");
+				
+	public void setNetwork(CyNetwork pNetwork) {
+		if (network != null && network == pNetwork) {
 			return;
 		}
-
-		if (negation) {
-			if (advancedSetting.isNodeChecked()) {
-				node_bits.flip(0, objectCount);
-			}
-			if (advancedSetting.isEdgeChecked()) {
-				edge_bits.flip(0, objectCount);
-			}
+		network = pNetwork;
+		if (passFilter != null) {
+			passFilter.setNetwork(network);			
 		}
 
-		childChanged = false;
-	}
-
-	
-	private boolean isHit(Object pObj) {
-		/*
-		// Get all the neighbors for pNode that pass the given filter
-		HashSet neighborSet = new HashSet();
-		getNeighbors(pObj, neighborSet, withinDistance);
-		
-		//Exclude self from the neighbor
-		if (neighborSet.contains(pObj)) {
-			neighborSet.remove(pObj);
-		}
-
-		// remove all the neighbors that do not pass the given filter
-		if (!passFilter.getName().equalsIgnoreCase("None")) {
-			Object [] nodeArray = neighborSet.toArray();
-			for (int i=0; i< nodeArray.length; i++) {
-				int nodeIndex = network.nodesList().indexOf(nodeArray[i]);
-
-				if (!passFilter.getNodeBits().get(nodeIndex)) {
-					neighborSet.remove(nodeArray[i]);
-				}
-			}							
-		}
-		if (neighborSet.size() < minNeighbors) {
-			return false;
-		}
-		*/
-		return true;
-	}
-	
-	// Get all the neighbors for pNode within pDistance
-	private void getNeighbors(Object pObj, HashSet pNeighborSet, int pDistance) {
-		if (pDistance == 0) {
-			if (!pNeighborSet.contains(pObj)) {
-				pNeighborSet.add(pObj);
-			}
-			return;
-		}
-		
-		List neighbors = network.neighborsList((Node)pObj);
-
-		Iterator nodeIt = neighbors.iterator();
-		while (nodeIt.hasNext()) {
-			Node nextNode = (Node) nodeIt.next();
-
-			if (!pNeighborSet.contains(nextNode)) {
-				pNeighborSet.add(nextNode);
-			}
-			getNeighbors(nextNode, pNeighborSet, pDistance-1);
-		}
+		childChanged();
 	}
 	
 	
@@ -260,52 +155,8 @@ public class InteractionFilter extends CompositeFilter {
 			retStr += "passFilter=" + passFilter.getName()+"\n";						
 		}
 		
-		retStr += "</InteractionFiler>";
+		retStr += "</InteractionFilter>";
 
 		return retStr;
-	}
-	
-	public void setNodeBits(BitSet b) {
-		node_bits = b;
-		//parent.childChanged();
-	}
-
-	public void setEdgeBits(BitSet b) {
-		edge_bits = b;
-		//parent.childChanged();
-	}
-
-	public void setParent(CyFilter p) {
-		parent = p;
-	}
-	public CyFilter getParent() {
-		return parent;
-	}
-		
-	public void setNegation(boolean pNot) {
-		negation = pNot;
-		//getParent().childChanged();
-	}
-	public boolean getNegation() {
-		return negation;
-	}
-
-	public String getName(){
-		return name;
-	}
-	public void setName(String pName){
-		name = pName;
-	}
-
-	public void setNetwork(CyNetwork pNetwork) {
-		if (network != null && network == pNetwork) {
-			return;
-		}
-		network = pNetwork;
-		if (passFilter != null) {
-			passFilter.setNetwork(network);			
-		}
-
-		childChanged();
 	}
 }
