51e181c7d5095a19ea815b2e66326187fe659a26
akuchinsky <akuchinsky@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-12-10 12:03:26 +0000

    added overview menu items
    git-svn-id: http://chianti.ucsd.edu/svn@12505 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/agilent/kuchinsky/NatureProtocolsWorkflow/src/cytoscape/actions/WorkflowLinkToArticleAction.java
@@ -0,0 +1,10 @@
+package cytoscape.actions;
+
+public class WorkflowLinkToArticleAction extends WorkflowPanelAction {
+
+	public WorkflowLinkToArticleAction(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
