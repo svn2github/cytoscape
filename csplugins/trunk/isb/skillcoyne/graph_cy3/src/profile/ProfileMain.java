4e2cfb9cdd6f6d4d962199eec4aaca23fa79a103
skillcoyne <skillcoyne@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2007-12-18 05:09:38 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@12604 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/isb/skillcoyne/graph_cy3/src/profile/ProfileMain.java
+++ csplugins/trunk/isb/skillcoyne/graph_cy3/src/profile/ProfileMain.java
@@ -6,60 +6,47 @@ package profile;
 import org.cytoscape.model.*;
 import org.cytoscape.model.impl.*;
 
+
 /**
  * @author skillcoy
- *
  */
-public class ProfileMain {
-
+public class ProfileMain
+	{
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+		{
 		java.util.Calendar cal = java.util.Calendar.getInstance();
-		System.out.println(cal.getTimeInMillis());
+		System.out.println("START: " + cal.getTimeInMillis());
 		// create 10k nodes and do what exactly??
 		Network network = new NetworkImpl("test");
-
-		
 		Node[] connectingNodes = new Node[5];
-		for (int i=0; i<10000; i++) {
+		for (int i = 0; i < 10000; i++)
+			{
 			Node node = network.addNode(null);
-			System.out.println("Adding node " + i);
-			
-		if (i%5 == 0)
-			connectingNodes[0] = node;
-		if (i%10 == 0)
-			connectingNodes[1] = node;
-		if (i%20 == 0)
-			connectingNodes[2] = node;
-		if (i%50 == 0)
-			connectingNodes[3] = node;
-		if (i%100 == 0)
-			connectingNodes[4] = node;
-		
-		for (int j=0; j<connectingNodes.length; j++) {
-			if (connectingNodes[j] != null)
-				network.addEdge(node, connectingNodes[j], null);
-		}
-			
-
-		}
-		
-		System.out.println(network.getNodes().length);
-
-		int totalEdges = 0;
-		for (Node n: network.getNodes()) {
-			totalEdges += n.getAdjacentEdges().length;
-		}
-		
-		System.out.println("" + totalEdges);
-		
+			if (i % 5 == 0) connectingNodes[0] = node;
+			if (i % 10 == 0) connectingNodes[1] = node;
+			if (i % 20 == 0) connectingNodes[2] = node;
+			if (i % 50 == 0) connectingNodes[3] = node;
+			if (i % 100 == 0) connectingNodes[4] = node;
+			for (int j = 0; j < connectingNodes.length; j++)
+				{
+				if (connectingNodes[j] != null) network.addEdge(node,
+						connectingNodes[j], null);
+				}
+			}
+		// System.out.println("START: " + cal.getTimeInMillis());
 		java.util.Calendar cal2 = java.util.Calendar.getInstance();
-		System.out.println("START: " + cal.getTimeInMillis());
-
 		System.out.println("END: " + cal2.getTimeInMillis());
 
+		System.out.println("Nodes: " + network.getNodes().length);
+		System.out.println("Edges: " + network.getEdges().length);
+		
+//		for (Node node: network.getNodes())
+//			{
+//			System.out.println(node.getAdjacentEdges().length);
+//			break;
+//			}
+		}
 	}
-
-}
