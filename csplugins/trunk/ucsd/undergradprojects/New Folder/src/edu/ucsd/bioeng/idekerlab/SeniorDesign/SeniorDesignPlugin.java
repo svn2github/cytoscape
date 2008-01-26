7982f2e35e29fdb9cea313d192666665bcefff4f
zzhou <zzhou@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-01-26 20:00:17 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@12843 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/undergradprojects/New Folder/src/edu/ucsd/bioeng/idekerlab/SeniorDesign/SeniorDesignPlugin.java
@@ -0,0 +1,57 @@
+package edu.ucsd.bioeng.idekerlab.SeniorDesign;
+
+public class SeniorDesignPlugin implements SeniorDesignPluginInterface
+{
+
+        void getRightClick(Node NodeInfo)
+        {
+                setUpPopup();
+        }
+
+        void setUpPopup()
+        {
+                intializeSwingGui();
+                setUpDefaults();
+
+                //wait for user input
+                while(true)
+                {
+                        userCheckBoxes();
+                        userDropDown();
+                        userStartButtonPress();
+                }
+        }
+
+        void userStartButtonPress()
+        {
+                //start downloads
+                for(each checked database checkbox)
+                {
+                        new SearchThread(Node NodeInfo, String SpeciesName, DatabaseName)
+                }
+        }
+
+        void setUpDefaults()
+        {
+                loadDatabaseNames();
+                scanSpecies();
+        }
+
+        void searchThread(Node NodeInfo, String SpeciesName, DatabaseName) extends Thread
+        {
+                initializeOurWebServiceClient(DatabaseName);
+                getData();
+
+        }
+
+        void initializeOurWebServiceClient(DatabaseName)
+        {
+                //use polymorphism to define getData function
+        }
+
+        void updateNetwork()
+        {
+                //update using CyNetwork / CyNode
+        }
+        }
+}
