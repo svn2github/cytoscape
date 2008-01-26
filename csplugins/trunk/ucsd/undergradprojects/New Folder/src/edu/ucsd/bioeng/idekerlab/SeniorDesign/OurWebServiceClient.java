26bff22182357d687d4a836b2d02ee8bb57c8229
zzhou <zzhou@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-01-26 19:56:48 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@12842 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/undergradprojects/New Folder/src/edu/ucsd/bioeng/idekerlab/SeniorDesign/OurWebServiceClient.java
@@ -0,0 +1,10 @@
+package edu.ucsd.bioeng.idekerlab.SeniorDesign;
+
+public class OurWebServiceClient implements WebServiceClient {
+
+        private void setProperty();
+        private void importNetwork();
+        private void search();
+        private void executeService();
+
+}
