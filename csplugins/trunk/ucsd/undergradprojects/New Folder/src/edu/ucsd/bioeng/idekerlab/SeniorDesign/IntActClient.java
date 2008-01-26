26f919fdbf5086a2e4e6932f96f9d0368b6728f4
zzhou <zzhou@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-01-26 19:38:58 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@12838 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/undergradprojects/New Folder/src/edu/ucsd/bioeng/idekerlab/SeniorDesign/IntActClient.java
@@ -0,0 +1,10 @@
+package edu.ucsd.bioeng.idekerlab.SeniorDesign;
+
+public class IntActClient implements WebServiceClient {
+
+        void setProperty()
+        void importNetwork
+        void search
+        void executeService
+
+}
