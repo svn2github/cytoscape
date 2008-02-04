6355ef80c84792b5ae12a72bf8ab2ae58df1f4aa
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-02-04 17:54:25 +0000

    Added edge colors, directedness, etc.
    git-svn-id: http://chianti.ucsd.edu/svn@12918 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/mskcc/grossb/pathway-commons/src/org/cytoscape/coreplugin/cpath2/cytoscape/BinarySifVisualStyleUtil.java
+++ csplugins/trunk/mskcc/grossb/pathway-commons/src/org/cytoscape/coreplugin/cpath2/cytoscape/BinarySifVisualStyleUtil.java
@@ -18,9 +18,21 @@ import org.mskcc.biopax_plugin.style.BioPaxVisualStyleUtil;
  * @author Ethan Cerami.
  */
 public class BinarySifVisualStyleUtil {
-    public final static String BINARY_SIF_VISUAL_STYLE = "Binary_SIF";
+    public final static String BINARY_SIF_VISUAL_STYLE = "Binary_SIF_Version_1";
 
-	/**
+    public final static String COMPONENT_OF = "Component_Of";
+    public final static String COMPONENT_IN_SAME = "Component_InSame";
+    public final static String SEQUENTIAL_CATALYSIS = "SequentialCatalysis";
+    public final static String CONTROLS_STATE_CHANGE = "Controls_StateChange";
+    public final static String CONTROLS_METABOLIC_CHANGE = "Controls_MetabolicChange";
+    public final static String PARTICIPATES_CONVERSION = "Participates_Conversion";
+    public final static String PARTICIPATES_INTERACTION = "Participates_Interaction";
+    public final static String CO_CONTROL_INDEPENDENT_SIMILAR = "Co-Control-Indepedent_Similar";
+    public final static String CO_CONTROL_INDEPENDENT_ANTI = "Co-Control_Dependent_Anti";
+    public final static String CO_CONTROL_DEPENDENT_SIMILAR = "Co-Control_Dependent_Similar";
+    public final static String CONTROL_DEPENDENT_ANTI = "Control_Dependent_Anti";
+
+    /**
 	 * Constructor.
 	 * If an existing BioPAX Viz Mapper already exists, we use it.
 	 * Otherwise, we create a new one.
@@ -31,34 +43,29 @@ public class BinarySifVisualStyleUtil {
 		VisualMappingManager manager = Cytoscape.getVisualMappingManager();
 		CalculatorCatalog catalog = manager.getCalculatorCatalog();
 
-		VisualStyle visualStyle = catalog.getVisualStyle(BINARY_SIF_VISUAL_STYLE);
-        //  If the BioPAX Visual Style already exists, use this one instead.
-        //  The user may have tweaked the out-of-the box mapping, and we don't
-        //  want to over-ride these tweaks.
-        if (visualStyle == null) {
-            visualStyle = new VisualStyle(BINARY_SIF_VISUAL_STYLE);
+		VisualStyle visualStyle = new VisualStyle(BINARY_SIF_VISUAL_STYLE);
 
-            NodeAppearanceCalculator nac = new NodeAppearanceCalculator();
-            Color color = new Color (255, 153, 153);
-            nac.getDefaultAppearance().set(cytoscape.visual.VisualPropertyType.NODE_FILL_COLOR,
-                    color);
-            nac.getDefaultAppearance().set(cytoscape.visual.VisualPropertyType.NODE_SHAPE,
-                    NodeShape.ELLIPSE);
-            createNodeLabel(nac);
+        NodeAppearanceCalculator nac = new NodeAppearanceCalculator();
+        Color color = new Color (255, 153, 153);
+        nac.getDefaultAppearance().set(cytoscape.visual.VisualPropertyType.NODE_FILL_COLOR,
+                color);
+        nac.getDefaultAppearance().set(cytoscape.visual.VisualPropertyType.NODE_SHAPE,
+                NodeShape.ELLIPSE);
+        createNodeLabel(nac);
 
-            EdgeAppearanceCalculator eac = new EdgeAppearanceCalculator();
-            createEdgeColor (eac);
+        EdgeAppearanceCalculator eac = new EdgeAppearanceCalculator();
+        createEdgeColor (eac);
+        createDirectedEdges (eac);
 
-            visualStyle.setNodeAppearanceCalculator(nac);
-            visualStyle.setEdgeAppearanceCalculator(eac);
-            GlobalAppearanceCalculator gac = new GlobalAppearanceCalculator();
-            gac.setDefaultBackgroundColor(new Color (204, 204, 255));
-            visualStyle.setGlobalAppearanceCalculator(gac);
+        visualStyle.setNodeAppearanceCalculator(nac);
+        visualStyle.setEdgeAppearanceCalculator(eac);
+        GlobalAppearanceCalculator gac = new GlobalAppearanceCalculator();
+        gac.setDefaultBackgroundColor(new Color (204, 204, 255));
+        visualStyle.setGlobalAppearanceCalculator(gac);
 
-            //  The visual style must be added to the Global Catalog
-            //  in order for it to be written out to vizmap.props upon user exit
-            catalog.addVisualStyle(visualStyle);
-        }
+        //  The visual style must be added to the Global Catalog
+        //  in order for it to be written out to vizmap.props upon user exit
+        catalog.addVisualStyle(visualStyle);
         return visualStyle;
     }
 
@@ -68,10 +75,17 @@ public class BinarySifVisualStyleUtil {
 		DiscreteMapping discreteMapping = new DiscreteMapping(Color.BLACK,
             Semantics.INTERACTION, ObjectMapping.EDGE_MAPPING);
 
-        discreteMapping.putMapValue("Component.InSame", Color.RED);
-        discreteMapping.putMapValue("Component.Of", Color.BLUE);
-        discreteMapping.putMapValue("Participates.Conversion", Color.GREEN);
-        discreteMapping.putMapValue("interaction", Color.YELLOW);
+        discreteMapping.putMapValue(COMPONENT_OF, Color.decode("#0000FF"));
+        discreteMapping.putMapValue(COMPONENT_IN_SAME, Color.decode("#00FFFF"));
+        discreteMapping.putMapValue(SEQUENTIAL_CATALYSIS, Color.decode("#808080"));
+        discreteMapping.putMapValue(CONTROLS_STATE_CHANGE, Color.decode("#8B008B"));
+        discreteMapping.putMapValue(CONTROLS_METABOLIC_CHANGE, Color.decode("#008000"));
+        discreteMapping.putMapValue(PARTICIPATES_CONVERSION, Color.decode("#90EE90"));
+        discreteMapping.putMapValue(PARTICIPATES_INTERACTION, Color.decode("#FF00FF"));
+        discreteMapping.putMapValue(CO_CONTROL_INDEPENDENT_SIMILAR, Color.decode("#FFA500"));
+        discreteMapping.putMapValue(CO_CONTROL_INDEPENDENT_ANTI, Color.decode("#FFC0CB"));
+        discreteMapping.putMapValue(CO_CONTROL_DEPENDENT_SIMILAR, Color.decode("#FF0000"));
+        discreteMapping.putMapValue(CONTROL_DEPENDENT_ANTI, Color.decode("#FFFF00"));
 
         // create and set edge label calculator in edge appearance calculator
 		Calculator edgeColorCalculator = new BasicCalculator("Edge Color",
@@ -83,7 +97,25 @@ public class BinarySifVisualStyleUtil {
                Color.BLACK);
 	}
 
-	private static void createNodeLabel(NodeAppearanceCalculator nac) {
+	private static void createDirectedEdges(EdgeAppearanceCalculator eac) {
+		DiscreteMapping discreteMapping = new DiscreteMapping(ArrowShape.NONE,
+            Semantics.INTERACTION, ObjectMapping.EDGE_MAPPING);
+
+        discreteMapping.putMapValue(COMPONENT_OF, ArrowShape.ARROW);
+        discreteMapping.putMapValue(CONTROLS_STATE_CHANGE, ArrowShape.ARROW);
+        discreteMapping.putMapValue(CONTROLS_METABOLIC_CHANGE, ArrowShape.ARROW);
+
+        // create and set edge label calculator in edge appearance calculator
+		Calculator edgeColorCalculator = new BasicCalculator("Edge Source Arrow Shape",
+            discreteMapping, VisualPropertyType.EDGE_TGTARROW_SHAPE);
+		eac.setCalculator(edgeColorCalculator);
+
+		// set default color
+		eac.getDefaultAppearance().set(cytoscape.visual.VisualPropertyType.EDGE_TGTARROW_SHAPE,
+               ArrowShape.NONE);
+	}
+
+    private static void createNodeLabel(NodeAppearanceCalculator nac) {
 		// create pass through mapper for node labels
 		PassThroughMapping passThroughMapping = new PassThroughMapping("",
 		                                                               ObjectMapping.NODE_MAPPING);
