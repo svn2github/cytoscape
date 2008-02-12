c051c1a99195fb52ac276596dc031bf3bbcdc5f9
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-02-12 00:42:12 +0000

    deleting old project
    git-svn-id: http://chianti.ucsd.edu/svn@13024 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- PathwayWalkingPlugin2/src/edu/ucsd/bioeng/idekerlab/SeniorDesign/OurWebServiceClient.java
+++
@@ -1,9 +0,0 @@
-package edu.ucsd.bioeng.idekerlab.SeniorDesign;
-
-public class OurWebServiceClient implements WebServiceClient {
-	private void setProperty();
-	private void importNetwork();
-	private void search();
-	private void executeService();
-
-}
