5dfb6975203655ba8703dd1b7818182ddd93a68c
eatsai <eatsai@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-02-13 06:59:44 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@13035 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/undergradprojects/PathwayWalkingPlugin3/src/edu/ucsd/bioeng/idekerlab/PathwayWalkingPlugin/GUI.java
+++ csplugins/trunk/ucsd/undergradprojects/PathwayWalkingPlugin3/src/edu/ucsd/bioeng/idekerlab/PathwayWalkingPlugin/GUI.java
@@ -1,5 +1,14 @@
 package edu.ucsd.bioeng.idekerlab.PathwayWalkingPlugin;
 
+import static cytoscape.visual.VisualPropertyType.NODE_LABEL;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import giny.model.Edge;
 import giny.model.Node;
 /*
  * GUI.java
@@ -11,15 +20,41 @@ import giny.model.Node;
  *
  * @author  Administrator
  */
+import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
 import cytoscape.data.CyAttributes;
+import cytoscape.data.webservice.DatabaseSearchResult;
 import cytoscape.data.webservice.WebServiceClient;
 import cytoscape.data.webservice.WebServiceClientManager;
-import edu.ucsd.bioeng.idekerlab.intactclient.IntactClient;
-
+import edu.ucsd.bioeng.idekerlab.intactplugin.IntactClient;
+import cytoscape.data.webservice.CyWebServiceEvent.WSEventType;
+import cytoscape.data.webservice.CyWebServiceEvent; 
+import cytoscape.data.webservice.WebServiceClientImpl;
+import cytoscape.layout.Tunable;
+import uk.ac.ebi.intact.binarysearch.wsclient.BinarySearchServiceClient;
+import uk.ac.ebi.intact.binarysearch.wsclient.generated.Alias;
+import uk.ac.ebi.intact.binarysearch.wsclient.generated.BinaryInteraction;
+import uk.ac.ebi.intact.binarysearch.wsclient.generated.Confidence;
+import uk.ac.ebi.intact.binarysearch.wsclient.generated.CrossReference;
+import uk.ac.ebi.intact.binarysearch.wsclient.generated.InteractionDetectionMethod;
+import uk.ac.ebi.intact.binarysearch.wsclient.generated.Interactor;
+import uk.ac.ebi.intact.binarysearch.wsclient.generated.SearchResult;
+import cytoscape.util.ModuleProperties;
+import cytoscape.util.ModulePropertiesImpl;
+import cytoscape.visual.EdgeAppearanceCalculator;
+import cytoscape.visual.GlobalAppearanceCalculator;
+import cytoscape.visual.NodeAppearanceCalculator;
+import cytoscape.visual.NodeShape;
+import cytoscape.visual.VisualPropertyType;
+import cytoscape.visual.VisualStyle;
+import cytoscape.visual.calculators.AbstractCalculator;
+import cytoscape.visual.calculators.NodeCalculator;
+import cytoscape.visual.mappings.PassThroughMapping;
 
 public class GUI extends javax.swing.JFrame {
  
+	private static final String CLIENT_ID = "intact";
+	
     /** Creates new form GUI */
     public GUI(Node nd) {
     	node = nd;
@@ -356,10 +391,108 @@ public class GUI extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
 // TODO add your handling code here:
         System.out.println("Start Search Button Pressed");
-        WebServiceClient try1 = IntactClient.getClient();
-        WebServiceClientManager.registerClient(try1);
+//        WebServiceClient try1 = WebServiceClientManager.getClient("IntAct");
+//        WebServiceClientImpl try1 = WebServiceClientManager.getClient("IntAct");
+        
+        WebServiceClientImpl try2 = (IntactClient) IntactClient.getClient();
+//        System.out.println(try2);
+        
+        try{
+//        	public void search(String id, int i) {}
+//        	try1.execute("executeService", new Class[]{String.class,Integer.class}, new Object[]{"brca2",new Integer(5)});
+
+//			This following line only works if try2 is of type IntActClient
+//        	try2.execute("executeService", new Class[]{CyWebServiceEvent.class}, new Object[]{new CyWebServiceEvent("IntAct", WSEventType.SEARCH_DATABASE, node)});
+        	
+        	
+        	Object blah1 = try2.execute("findBinaryInteractions", new Class[]{String.class}, new Object[]{nodeId});
+
+        	SearchResult result = (SearchResult) blah1;
+
+        	
+        	Cytoscape.firePropertyChange("SEARCH_RESULT", "edu.ucsd.bioeng.idekerlab.intactplugin.IntactClient", new DatabaseSearchResult(result.getTotalCount(), result, WSEventType.IMPORT_NETWORK));
+
+        	CyWebServiceEvent cyweb1 = new CyWebServiceEvent("IntAct", WSEventType.SEARCH_DATABASE, node);
+        	
+        	System.out.println("RESULTS.GETINTERACTIONS() RETURNS...");
+        	System.out.println(result.getInteractions().toString());
+        	System.out.println("END OF RESULTS.GETINTERACTIONS");
+        	
+//			importNetwork(nodeId, null);
+//			System.out.println("Attempting to Import Network");
+//			importNetwork(nodeId, Cytoscape.getCurrentNetwork());
+        	
+        	search(cyweb1.getParameter().toString(), cyweb1);
+        	importNetwork(cyweb1.getParameter(), null);
+			importNetwork(cyweb1.getParameter(), Cytoscape.getCurrentNetwork());
+			
+        } catch(Exception e){
+        	System.out.println(e.toString());
+        }
         System.out.println("After Intact Client was called.");
     }
+    
+    
+	private void setProperty() {
+		props = new ModulePropertiesImpl("edu.ucsd.bioeng.idekerlab.intactplugin.IntactClient", "wsc");
+
+		List<String> searchType = new ArrayList<String>();
+		searchType.add("Lucene");
+		searchType.add("Keyword Search");
+
+		//		props.add(new Tunable("search_type", "The edge attribute that contains the weights",
+		//                Tunable.LIST, searchType));
+		props.add(new Tunable("search_mode", "Enable keyword search", Tunable.BOOLEAN,
+		                      new Boolean(false)));
+
+		props.add(new Tunable("max_interactions", "Maximum number of interactions",
+		                      Tunable.INTEGER, new Integer(500)));
+		props.add(new Tunable("search_depth", "Search depth", Tunable.INTEGER, new Integer(0)));
+		props.add(new Tunable("select_interaction", "Import only selected interactions",
+		                      Tunable.BOOLEAN, new Boolean(false)));
+	}
+	
+	public void executeService(CyWebServiceEvent e) {
+		if (e.getSource().equals(CLIENT_ID)) {
+			if (e.getEventType().equals(WSEventType.IMPORT_NETWORK)) {
+				importNetwork(e.getParameter(), null);
+			} else if (e.getEventType().equals(WSEventType.EXPAND_NETWORK)) {
+				importNetwork(e.getParameter(), Cytoscape.getCurrentNetwork());
+			} else if (e.getEventType().equals(WSEventType.SEARCH_DATABASE)) {
+				
+				search(e.getParameter().toString(), e);
+			}
+		}
+	}
+	
+	private void search(String query, CyWebServiceEvent e) {
+		if (stub == null) {
+			stub = new BinarySearchServiceClient();
+		}
+
+		BinarySearchServiceClient client = (BinarySearchServiceClient) stub;
+		System.out.println("=========CLASS = " + client.getClass());
+		
+		for(int i=1; i<10; i++) {
+		System.out.println("HELP HELP HELP");		
+		}
+		System.out.println("THIS IS THE QUERY:" + query);
+		System.out.println("THIS IS e:" + e);
+		System.out.println("THIS IS e.getParameter: " + e.getParameter());
+		System.out.println("THIS IS instance of Node " + (e.getParameter() instanceof Node));
+		System.out.println("THIS IS instance of String " + (e.getParameter() instanceof String));
+		System.out.println("THIS IS instance of Object " + (e.getParameter() instanceof Object));
+		
+
+		SearchResult result = client.findBinaryInteractions(query);
+		if(e.getNextMove() != null) {
+			Cytoscape.firePropertyChange("SEARCH_RESULT", "edu.ucsd.bioeng.idekerlab.intactplugin.IntactClient", new DatabaseSearchResult(result.getTotalCount(), result, e.getNextMove()));
+		} else {
+			Cytoscape.firePropertyChange("SEARCH_RESULT", "edu.ucsd.bioeng.idekerlab.intactplugin.IntactClient", new DatabaseSearchResult(result.getTotalCount(), result, WSEventType.IMPORT_NETWORK));
+		}
+		
+
+	}
  
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
         System.out.println("Add to existing network");
@@ -376,7 +509,196 @@ public class GUI extends javax.swing.JFrame {
             }
         });
     }
- 
+    
+    Object stub;
+    ModuleProperties props;
+    
+    public void importNetwork(Object searchResult, CyNetwork net) {
+		try {
+			if (stub == null) {
+				stub = new BinarySearchServiceClient();
+			}
+
+			if(searchResult instanceof SearchResult == false) {
+				return;
+			}
+
+			BinarySearchServiceClient client = (BinarySearchServiceClient) stub;
+			SearchResult result = (SearchResult) searchResult;
+			List<BinaryInteraction> binaryInteractions = result.getInteractions();
+
+			final Integer max = (Integer) props.get("max_interactions").getValue();
+			int i = 0;
+			Set<Node> nodes = new HashSet<Node>();
+			Set<Edge> edges = new HashSet<Edge>();
+
+			Node n1 = null;
+			Node n2 = null;
+			Edge e = null;
+
+			List<Alias> alias1;
+			List<Alias> alias2;
+
+			String a1;
+			String a2;
+
+			Interactor a;
+			Interactor b;
+			CyAttributes attr = Cytoscape.getNodeAttributes();
+			CyAttributes edgeAttr = Cytoscape.getEdgeAttributes();
+
+			List<String> nonUP = new ArrayList<String>();
+
+			for (BinaryInteraction bin : binaryInteractions) {
+				// do stuff with interactions here
+				a = bin.getInteractorA();
+				b = bin.getInteractorB();
+
+				List<CrossReference> ids = a.getIdentifiers();
+
+				for (CrossReference ref : ids) {
+					System.out.println("ID A: " + ref.getDatabase() + ": " + ref.getIdentifier()
+							+ ", " + ref.getText());
+
+					if (ref.getDatabase().equals("uniprotkb") == false) {
+						nonUP.add(ref.getDatabase() + ":" + ref.getIdentifier());
+					}
+				}
+
+				String aID = a.getIdentifiers().get(0).getIdentifier();
+
+				List<Alias> al = a.getAliases();
+
+				for (Alias cr : al) {
+					System.out.println("--------Alias A: " + cr.getAliasType() + ": "
+							+ cr.getDbSource() + ", " + cr.getName());
+				}
+
+				List<CrossReference> altA = a.getAlternativeIdentifiers();
+
+				for (CrossReference ref : altA) {
+					attr.setAttribute(aID, "Official Symbol", ref.getIdentifier());
+					System.out.println("########Alt A: " + ref.getDatabase() + ": "
+							+ ref.getIdentifier() + ", " + ref.getText());
+
+					break;
+				}
+
+				ids = b.getIdentifiers();
+
+				for (CrossReference ref : ids) {
+					System.out.println("ID B: " + ref.getDatabase() + ": " + ref.getIdentifier()
+							+ ", " + ref.getText());
+
+					if (ref.getDatabase().equals("uniprotkb") == false) {
+						nonUP.add(ref.getDatabase() + ":" + ref.getIdentifier());
+					}
+				}
+
+				String bID = b.getIdentifiers().get(0).getIdentifier();
+
+				List<Alias> al2 = b.getAliases();
+
+				for (Alias cr : al2) {
+					System.out.println("--------Alias B: " + cr.getAliasType() + ": "
+							+ cr.getDbSource() + ", " + cr.getName());
+				}
+
+				List<CrossReference> altB = b.getAlternativeIdentifiers();
+
+				for (CrossReference ref : altB) {
+					attr.setAttribute(bID, "Official Symbol", ref.getIdentifier());
+					System.out.println("########Alt B: " + ref.getDatabase() + ": "
+							+ ref.getIdentifier() + ", " + ref.getText());
+
+					break;
+				}
+
+				n1 = Cytoscape.getCyNode(a.getIdentifiers().get(0).getIdentifier(), true);
+				n2 = Cytoscape.getCyNode(b.getIdentifiers().get(0).getIdentifier(), true);
+
+				if ((a.getOrganism() != null) && (a.getOrganism().getIdentifiers() != null)
+						&& (a.getOrganism().getIdentifiers().get(0) != null)) {
+					attr.setAttribute(n1.getIdentifier(), "species",
+							a.getOrganism().getIdentifiers().get(0).getText());
+				}
+
+				if ((b.getOrganism() != null) && (b.getOrganism().getIdentifiers() != null)
+						&& (b.getOrganism().getIdentifiers().get(0) != null)) {
+					attr.setAttribute(n2.getIdentifier(), "species",
+							b.getOrganism().getIdentifiers().get(0).getText());
+				}
+
+				if ((bin.getInteractorA().getAliases() != null)
+						&& (bin.getInteractorA().getAliases().size() != 0)) {
+					alias1 = bin.getInteractorA().getAliases();
+				}
+
+				if ((bin.getInteractorB().getAliases() != null)
+						&& (bin.getInteractorB().getAliases().size() != 0)) {
+					alias2 = bin.getInteractorB().getAliases();
+				}
+
+				e = Cytoscape.getCyEdge(n1, n2, "interaction",
+						bin.getInteractionTypes().get(0).getText(), true);
+				System.out.println("-----------> " + bin.getInteractorA() + " interacts with "
+						+ bin.getInteractorB());
+
+				nodes.add(n1);
+				nodes.add(n2);
+				edges.add(e);
+
+				List<Confidence> confs = bin.getConfidenceValues();
+
+				for (Confidence c : confs) {
+					edgeAttr.setAttribute(e.getIdentifier(), "confidence", c.getValue());
+				}
+
+				List<InteractionDetectionMethod> iType = bin.getDetectionMethods();
+
+				for (InteractionDetectionMethod it : iType) {
+					edgeAttr.setAttribute(e.getIdentifier(), "interaction detection method",
+							it.getText());
+				}
+
+				edgeAttr.setAttribute(e.getIdentifier(), "Source Database",
+						bin.getSourceDatabases().get(0).getDatabase());
+
+				i++;
+
+				if (i > max) {
+					break;
+				}
+			}
+
+			for (String n : nonUP) {
+				System.out.println("!!!!!!!!Non UP id = " + n);
+			}
+
+			if (net == null) {
+				Cytoscape.createNetwork(nodes, edges, "IntAct: ", null);
+				Cytoscape.firePropertyChange(Cytoscape.NETWORK_LOADED, null, null);
+			} else {
+				for (Node node : nodes) {
+					net.addNode(node);
+				}
+
+				for (Edge edge : edges) {
+					net.addEdge(edge);
+				}
+
+				net.setSelectedNodeState(nodes, true);
+				Cytoscape.firePropertyChange(Cytoscape.NETWORK_MODIFIED, null, null);
+			}
+
+			Cytoscape.firePropertyChange(Cytoscape.ATTRIBUTES_CHANGED, null, null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+    
     // Variables declaration - do not modify
     private javax.swing.JLabel databaseNameText;
     private javax.swing.JButton jButton1;
