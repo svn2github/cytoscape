166f2abe4cbfaf12afa59d6da2251dd870cedc5f
cerami <cerami@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-02-20 19:54:09 +0000

    Changed to interface
    git-svn-id: http://chianti.ucsd.edu/svn@13118 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/cPath2/src/org/cytoscape/coreplugin/cpath2/web_service/CPathWebService.java
+++ coreplugins/trunk/cPath2/src/org/cytoscape/coreplugin/cpath2/web_service/CPathWebService.java
@@ -13,32 +13,11 @@ import org.cytoscape.coreplugin.cpath2.schemas.search_response.*;
 import org.cytoscape.coreplugin.cpath2.schemas.summary_response.SummaryResponseType;
 
 /**
- * Class for accessing the cPath Web API.
+ * Interface for accessing the cPath Web API.
  *
  * @author Ethan Cerami
  */
-public class CPathWebService {
-    private static ArrayList<CPathWebServiceListener> listeners =
-            new ArrayList<CPathWebServiceListener>();
-    private volatile CPathProtocol protocol;
-    private static CPathWebService webApi;
-
-    /**
-     * Gets Singelton instance of CPath Web API.
-     * @return CPathWebService Object.
-     */
-    public static CPathWebService getInstance() {
-        if (webApi == null) {
-            webApi = new CPathWebService();
-        }
-        return webApi;
-    }
-
-    /**
-     * Private Constructor.
-     */
-    private CPathWebService() {
-    }
+public interface CPathWebService {
 
     /**
      * Searches Physical Entities in cPath Instance.
@@ -49,49 +28,7 @@ public class CPathWebService {
      * @return SearchResponseType Object.
      */
     public SearchResponseType searchPhysicalEntities(String keyword, int ncbiTaxonomyId,
-            int startIndex, TaskMonitor taskMonitor) throws CPathException, EmptySetException {
-
-        // Notify all listeners of start
-        for (int i = listeners.size() - 1; i >= 0; i--) {
-            CPathWebServiceListener listener = listeners.get(i);
-            listener.searchInitiatedForPhysicalEntities(keyword, ncbiTaxonomyId, startIndex);
-        }
-
-        protocol = new CPathProtocol();
-        protocol.setCommand(CPathProtocol.COMMAND_SEARCH);
-        protocol.setOrganism(ncbiTaxonomyId);
-        protocol.setFormat(CPathProtocol.FORMAT_XML);
-        protocol.setQuery(keyword);
-
-        SearchResponseType searchResponse;
-        if (keyword.equalsIgnoreCase("dummy")) {
-            searchResponse = this.createDummySearchResults();
-            searchResponse.setTotalNumHits(10L);
-        } else {
-            String responseXml = protocol.connect(taskMonitor);
-            StringReader reader = new StringReader(responseXml);
-
-            Class[] classes = new Class[2];
-            classes[0] = org.cytoscape.coreplugin.cpath2.schemas.search_response.SearchResponseType.class;
-            classes[1] = org.cytoscape.coreplugin.cpath2.schemas.search_response.ObjectFactory.class;
-            try {
-                JAXBContext jc = JAXBContext.newInstance(classes);
-                Unmarshaller u = jc.createUnmarshaller();
-                JAXBElement element = (JAXBElement) u.unmarshal(reader);
-                searchResponse = (SearchResponseType) element.getValue();
-            } catch(Throwable e){
-                throw new CPathException(CPathException.ERROR_XML_PARSING, e);
-            }
-        }
-
-        //SearchResponseType searchResponse = createDummySearchResults();
-        // Notify all listeners of end
-        for (int i = listeners.size() - 1; i >= 0; i--) {
-            CPathWebServiceListener listener = listeners.get(i);
-            listener.searchCompletedForPhysicalEntities(searchResponse);
-        }
-        return searchResponse;
-    }
+            int startIndex, TaskMonitor taskMonitor) throws CPathException, EmptySetException;
 
     /**
      * Gets parent summaries for specified record.
@@ -103,42 +40,7 @@ public class CPathWebService {
      * @throws EmptySetException    Empty Set Error.
      */
     public SummaryResponseType getParentSummaries (long primaryId, TaskMonitor taskMonitor)
-            throws CPathException, EmptySetException {
-        // Notify all listeners of start
-        for (int i = listeners.size() - 1; i >= 0; i--) {
-            CPathWebServiceListener listener = listeners.get(i);
-            listener.requestInitiatedForParentSummaries(primaryId);
-        }
-
-        protocol = new CPathProtocol();
-        protocol.setCommand(CPathProtocol.COMMAND_GET_PARENTS);
-        protocol.setFormat(CPathProtocol.FORMAT_XML);
-
-        protocol.setQuery(Long.toString(primaryId));
-
-        SummaryResponseType summaryResponse;
-        String responseXml = protocol.connect(taskMonitor);
-        StringReader reader = new StringReader(responseXml);
-
-        Class[] classes = new Class[2];
-        classes[0] = org.cytoscape.coreplugin.cpath2.schemas.summary_response.SummaryResponseType.class;
-        classes[1] = org.cytoscape.coreplugin.cpath2.schemas.summary_response.ObjectFactory.class;
-        try {
-            JAXBContext jc = JAXBContext.newInstance(classes);
-            Unmarshaller u = jc.createUnmarshaller();
-            JAXBElement element = (JAXBElement) u.unmarshal(reader);
-            summaryResponse = (SummaryResponseType) element.getValue();
-        } catch(Throwable e){
-            throw new CPathException(CPathException.ERROR_XML_PARSING, e);
-        }
-
-        // Notify all listeners of end
-        for (int i = listeners.size() - 1; i >= 0; i--) {
-            CPathWebServiceListener listener = listeners.get(i);
-            listener.requestCompletedForParentSummaries(primaryId, summaryResponse);
-        }
-        return summaryResponse;
-    }
+            throws CPathException, EmptySetException;
 
     /**
      * Gets One or more records by Primary ID.
@@ -150,112 +52,38 @@ public class CPathWebService {
      * @throws EmptySetException    Empty Set Error.
      */
     public String getRecordsByIds(long[] ids, String format, TaskMonitor taskMonitor) throws
-            CPathException, EmptySetException {
-        protocol = new CPathProtocol();
-        protocol.setCommand(CPathProtocol.COMMAND_GET_RECORD_BY_CPATH_ID);
-        protocol.setFormat(format);
-        StringBuffer q = new StringBuffer();
-        for (int i=0; i<ids.length; i++) {
-            q.append (Long.toString(ids[i])+",");
-        }
-        protocol.setQuery(q.toString());
-        String xml = protocol.connect(taskMonitor);
-        return xml;
-    }
-
-    /**
-     * Abort the Request.
-     */
-    public void abort() {
-        protocol.abort();
-    }
-
-    private SearchResponseType createDummySearchResults() {
-        SearchResponseType searchResponse = new SearchResponseType();
-        List<ExtendedRecordType> searchHits = searchResponse.getSearchHit();
-        for (int i = 0; i < 10; i++) {
-            ExtendedRecordType searchHit = new ExtendedRecordType();
-            searchHit.setName("Protein " + i);
-
-            OrganismType organism = new OrganismType();
-            organism.setCommonName("Human");
-            organism.setSpeciesName("Homo Sapiens");
-            searchHit.setOrganism(organism);
-
-            List synList = new ArrayList();
-            synList.add("Synonym 1");
-            synList.add("Synonym 2");
-            synList.add("Synonym 3");
-            synList.add("Synonym 4");
-            searchHit.getSynonym().addAll(synList);
-
-            List <XRefType> xrefList = new ArrayList();
-            for (int j=0; j<3; j++) {
-                XRefType xref = new XRefType();
-                xref.setDb("Database_" + j);
-                xref.setId("ID_" + j);
-                xref.setUrl("http://www.yahoo.com");
-                xrefList.add(xref);
-            }
-            searchHit.getXref().addAll(xrefList);
-
-            List extracts = searchHit.getExcerpt();
-            extracts.add ("Vestibulum pharetra <B>laoreet ante</B> dictum dolor sed, "
-                    + "elementum egestas nunc nullam, pede mauris mattis, eros nam, elit "
-                    + "aliquam lorem vestibulum duis a tortor. Adipiscing elit habitant justo, "
-                    + "nonummy nunc wisi eros, dictum eget orci placerat metus vehicula eu.");
-
-            ObjectFactory factory = new ObjectFactory();
-            PathwayListType pathwayListType = factory.createPathwayListType();
-            List <PathwayType> pathwayList = pathwayListType.getPathway();
-            searchHit.setPathwayList(pathwayListType);
-            for (int j = 0; j < 10; j++) {
-                PathwayType pathwaySummary = new PathwayType();
-                pathwaySummary.setName("Pathway " + j + "[" + i + "]");
-                pathwaySummary.setPrimaryId((long) j);
-                DataSourceType dataSource = new DataSourceType();
-                dataSource.setName("Data Source " + j);
-                pathwaySummary.setDataSource(dataSource);
-                pathwayList.add(pathwaySummary);
-            }
-            searchHits.add(searchHit);
-        }
-        return searchResponse;
-    }
+            CPathException, EmptySetException;
 
     /**
      * Gets a list of all Organisms currently available within cPath instance.
      *
      * @return ArrayList of Organism Type Objects.
      */
-    public ArrayList<OrganismType> getOrganismList() {
-        return null;
-    }
+    public ArrayList<OrganismType> getOrganismList();
+
+    /**
+     * Abort the Request.
+     */
+    public void abort();
 
     /**
      * Registers a new listener.
      *
      * @param listener CPathWebService Listener.
      */
-    public void addApiListener(CPathWebServiceListener listener) {
-        listeners.add(listener);
-    }
+    public void addApiListener(CPathWebServiceListener listener);
 
     /**
      * Removes the specified listener.
      *
      * @param listener CPathWebService Listener.
      */
-    public void removeApiListener(CPathWebServiceListener listener) {
-        listeners.remove(listener);
-    }
+    public void removeApiListener(CPathWebServiceListener listener);
 
     /**
      * Gets the list of all registered listeners.
      *
      * @return ArrayList of CPathWebServiceListener Objects.
      */
-    public ArrayList<CPathWebServiceListener> getListeners() {
-        return listeners;
-    }
+    public ArrayList<CPathWebServiceListener> getListeners();
 }
