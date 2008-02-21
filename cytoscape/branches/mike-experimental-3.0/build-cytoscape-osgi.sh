8853fde5f2db5a7a81eb8c607ea2773a2059e705
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-02-21 19:56:52 +0000

    osgi script changes
    git-svn-id: http://chianti.ucsd.edu/svn@13135 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/branches/mike-experimental-3.0/build-cytoscape-osgi.sh
+++ cytoscape/branches/mike-experimental-3.0/build-cytoscape-osgi.sh
@@ -2,37 +2,38 @@
 
 set -e
 
-bnd="java -jar `pwd`/bnd/bnd-0.0.198.jar"
-INSTALL=build/cytoscape-osgi
+BNDDIR=`pwd`/osgi/bnd
+BND="java -jar $BNDDIR/bnd-0.0.198.jar"
+INST_EQINOX=build/equinox-cytoscape
 
 if [[ ! -d build || ! -e cytoscape.jar ]]; then
 	echo "everything needs to be compiled first!"
 	exit 1
 fi
 
-rm -rf $INSTALL
-cp -r osgi-template $INSTALL
+rm -rf $INST_EQINOX
+cp -r osgi/equinox-template $INST_EQINOX
 
-$bnd wrap -properties bnd/cytoscape.bnd cytoscape.jar
-mv bnd/cytoscape.bar $INSTALL/plugins/cytoscape.jar
+$BND wrap -properties $BNDDIR/cytoscape.bnd cytoscape.jar
+mv $BNDDIR/cytoscape.bar $INST_EQINOX/plugins/cytoscape.jar
 
 cd lib
-$bnd wrap *.jar
-mv *.bar ../$INSTALL/plugins
+$BND wrap *.jar
+mv *.bar ../$INST_EQINOX/plugins
 
-cd ../$INSTALL/plugins
+cd ../$INST_EQINOX/plugins
 python -c "import os; [ os.rename(f,'.'.join(f.split('.')[:-1])+'.jar') for f in os.listdir('.') ]"
 
 cd ../../../
 
-$bnd wrap -properties bnd/jaxb-api.bnd lib/jaxb-api.jar
-mv bnd/jaxb-api.bar $INSTALL/plugins/jaxb-api.jar
+$BND wrap -properties $BNDDIR/jaxb-api.bnd lib/jaxb-api.jar
+mv $BNDDIR/jaxb-api.bar $INST_EQINOX/plugins/jaxb-api.jar
 
-$bnd wrap -properties bnd/jaxb-impl.bnd lib/jaxb-impl.jar
-mv bnd/jaxb-impl.bar $INSTALL/plugins/jaxb-impl.jar
+$BND wrap -properties $BNDDIR/jaxb-impl.bnd lib/jaxb-impl.jar
+mv $BNDDIR/jaxb-impl.bar $INST_EQINOX/plugins/jaxb-impl.jar
 
-cp plugins/core/* $INSTALL/old_plugins
+cp plugins/core/* $INST_EQINOX/old_plugins
 
-cp osgi-plugins/* $INSTALL/plugins
+cp osgi-plugins/* $INST_EQINOX/plugins
 
 echo "done"
