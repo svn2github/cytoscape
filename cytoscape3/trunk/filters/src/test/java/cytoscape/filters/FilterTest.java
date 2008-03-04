280f90f591c62a28aa46b3c990db4c0b949a9645
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-03-04 22:28:31 +0000

    added new filters
    git-svn-id: http://chianti.ucsd.edu/svn@13318 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape3/trunk/filters/src/test/java/cytoscape/filters/FilterTest.java
@@ -0,0 +1,76 @@
+package cytoscape.filters;
+
+import org.cytoscape.Edge;
+import org.cytoscape.GraphPerspective;
+import org.cytoscape.Node;
+import cytoscape.Cytoscape;
+import junit.framework.TestCase;
+import org.cytoscape.attributes.CyAttributes;
+import cytoscape.data.Semantics;
+import cytoscape.filters.StringFilter;
+import java.util.BitSet;
+
+import csplugins.quickfind.util.QuickFind;
+import csplugins.quickfind.util.QuickFindFactory;
+import csplugins.quickfind.util.TaskMonitorBase;
+import csplugins.widgets.autocomplete.index.TextIndex;
+
+public class FilterTest extends TestCase {
+	
+	protected void initNetwork() {
+		cyNetwork = Cytoscape.createNetwork("network1");
+		Node node0 = Cytoscape.getCyNode("rain", true);
+		Node node1 = Cytoscape.getCyNode("rainbow", true);
+		Node node2 = Cytoscape.getCyNode("rabbit", true);
+		Node node3 = Cytoscape.getCyNode("yellow", true);
+
+		cyNetwork.addNode(node0);
+		cyNetwork.addNode(node1);
+		cyNetwork.addNode(node2);
+		cyNetwork.addNode(node3);
+		
+		Edge edge0 = Cytoscape.getCyEdge(node0, node1, Semantics.INTERACTION, "pp", true);
+		Edge edge1 = Cytoscape.getCyEdge(node0, node2, Semantics.INTERACTION, "pp", true);
+		Edge edge2 = Cytoscape.getCyEdge(node0, node3, Semantics.INTERACTION, "pp", true);
+		cyNetwork.addEdge(edge0);
+		cyNetwork.addEdge(edge1);
+		cyNetwork.addEdge(edge2);
+
+		//  Create Sample String Attributes
+		CyAttributes nodeAttributes = Cytoscape.getNodeAttributes();
+		nodeAttributes.setAttribute(node0.getIdentifier(), LOCATION, CYTOPLASM);
+		nodeAttributes.setAttribute(node1.getIdentifier(), LOCATION, CYTOPLASM);
+		nodeAttributes.setAttribute(node2.getIdentifier(), LOCATION, NUCLEUS);
+		nodeAttributes.setAttribute(node3.getIdentifier(), LOCATION, NUCLEUS);
+
+		//  Create Sample Integer Attributes
+		nodeAttributes.setAttribute(node0.getIdentifier(), RANK, 4);
+		nodeAttributes.setAttribute(node1.getIdentifier(), RANK, 3);
+		nodeAttributes.setAttribute(node2.getIdentifier(), RANK, 1);
+		nodeAttributes.setAttribute(node3.getIdentifier(), RANK, 2);
+
+		//  Create Sample Double Attributes
+		nodeAttributes.setAttribute(node0.getIdentifier(), SCORE, 45.2);
+		nodeAttributes.setAttribute(node1.getIdentifier(), SCORE, 3.211);
+		nodeAttributes.setAttribute(node2.getIdentifier(), SCORE, 22.2);
+		nodeAttributes.setAttribute(node3.getIdentifier(), SCORE, 2.1);
+
+		//  Create Sample String Attributes
+		CyAttributes edgeAttributes = Cytoscape.getEdgeAttributes();
+		edgeAttributes.setAttribute(edge0.getIdentifier(), PMID, "12345");
+		edgeAttributes.setAttribute(edge1.getIdentifier(), PMID, "12345");
+		edgeAttributes.setAttribute(edge2.getIdentifier(), PMID, "12666");
+	}
+
+	public void testNothing() {
+		// dummy test
+	}
+	
+	protected static final String LOCATION = "location";
+	protected static final String NUCLEUS = "nucleus";
+	protected static final String CYTOPLASM = "cytoplasm";
+	protected static final String RANK = "rank";
+	protected static final String SCORE = "score";
+	protected static final String PMID = "pmid";
+	protected GraphPerspective cyNetwork;
+}
