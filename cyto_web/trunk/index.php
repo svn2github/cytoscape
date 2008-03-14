fcc33df4111eb7ca3ccb7d3599fa97a8a26e26b6
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-03-14 22:42:51 +0000

    removed QC event link
    git-svn-id: http://chianti.ucsd.edu/svn@13535 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/index.php
+++ cyto_web/trunk/index.php
@@ -30,8 +30,6 @@
 				<hr/>
 				<h3><a href="http://chianti.ucsd.edu/Cyto-2_6-beta">Download 2.6 Beta Release!</a></h3>
 				<a href="http://www.cbio.mskcc.org/cytoscape/bugs/set_project.php?project_id=5">Submit Bugs!</a>
-				<br/>
-				<a href="http://cytoscape.org/cgi-bin/moin.cgi/QC_Event">Earn Rewards!</a>
 				</p></div>
 		</div>
 		<? include "nav.php"; ?><? include "detailed_nav.php"; ?>
