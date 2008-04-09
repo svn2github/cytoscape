1c500c9d3ae6a102b94bf195e43cabc9b428a6ff
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-04-09 17:05:03 +0000

    Add some comments
    git-svn-id: http://chianti.ucsd.edu/svn@13737 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/tutorial01/src/cytoscape/tutorial01/Tutorial01.java
+++ csplugins/trunk/ucsd/pwang/tutorial01/src/cytoscape/tutorial01/Tutorial01.java
@@ -13,20 +13,24 @@ import javax.swing.JPanel;
 public class Tutorial01 extends CytoscapePlugin {
 
 	/**
-	 * Creates a new Plugin object.
-	 * 
-	 * @param icon
-	 *            DOCUMENT ME!
-	 * @param csfilter
-	 *            DOCUMENT ME!
+	 * Add a tabbed panel to the control panel
 	 */
 	public Tutorial01() {
 
+		// Three steps
+		
+		// (1) Get the handler to cytoPanel west
 		CytoPanelImp ctrlPanel = (CytoPanelImp) Cytoscape.getDesktop().getCytoPanel(SwingConstants.WEST); 
+		
+		// (2) Create a JPanel object 
 		MyPanel myPanel = new MyPanel(); 
-		ctrlPanel.add("myPanelName", myPanel);
+		
+		// (3) Add the JPanel object to the cytoPanel west
+		ctrlPanel.add("myPanel", myPanel);
 
-		int indexInCytoPanel = ctrlPanel.indexOfComponent("myPanelName");
+
+		// Select the panel after the plugin is initialized
+		int indexInCytoPanel = ctrlPanel.indexOfComponent("myPanel");
 		ctrlPanel.setSelectedIndex(indexInCytoPanel);						
 	}
 	
