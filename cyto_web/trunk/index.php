4d0db04fc9efaed3cda4032fdb89342ce9fa7bad
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-04-11 00:22:51 +0000

    Modified for 2.6.
    git-svn-id: http://chianti.ucsd.edu/svn@13749 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/index.php
+++ cyto_web/trunk/index.php
@@ -1,7 +1,8 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+"http://www.w3.org/TR/html4/loose.dtd">
 <? include "config.php"; ?>
-<! Updated by kono for 2.5 7_23_2007 >
+
+<! Updated by kono for 2.6 4_2_2008 >
 <! Updated by apico with "latest release" variables for future updates (see config.php). 12_21_2006 >
 
 <html>
@@ -10,138 +11,160 @@
 		<title>Cytoscape: Analyzing and Visualizing Network Data</title>
 		<link href="css/cytoscape.css" type="text/css" rel="stylesheet" media="screen">
 		<link href="images/cyto.ico" rel="shortcut icon">
-		<meta name="keywords" content="Cytoscape, visualization, interaction network, software, genetic, gene, 
-        expression, protein interaction, graph, bioinformatics, computational biology, Whitehead Institute, 
-        Institute for Systems Biology, microarray analysis, clustering, pathways, integration, algorithm, 
-        simulated annealing, gene regulation">
+		<meta name="keywords" content="Cytoscape, visualization, interaction network, 
+		software, genetic, gene, expression, protein interaction, graph, bioinformatics, 
+		computational biology, Whitehead Institute, Institute for Systems Biology, 
+		microarray analysis, clustering, pathways, integration, algorithm, 
+		simulated annealing, gene regulation, complex network">
 	</head>
 
 	<body>
-    
-		<! ========== Title and menu bar ============ />
-
+	<div id="container">	
 		<div id="feature">
-			<div class="title">
-				Cytoscape
-			</div>
+			<div class="title">Cytoscape</div>
 			<img src="images/logo.png">
+			
 			<div class="article">
-				<h2><a href="<?= $latest_download_link ?>">Download Cytoscape<?= $latest_version ?>!</a></h2>
+				<h3><a href="<?= $latest_download_link ?>">Download Cytoscape 
+				<?= $latest_version ?>!</a></h3>
+				
 				<a href="<?= $latest_release_notes_link ?>"><?= $latest_version ?> Release Notes &raquo; </a>
-				<hr/>
+				
 				<h3><a href="http://chianti.ucsd.edu/Cyto-2_6-beta">Download 2.6 Beta Release!</a></h3>
 				<a href="http://www.cbio.mskcc.org/cytoscape/bugs/set_project.php?project_id=5">Submit Bugs!</a>
-				</p>
-            </div>
+			</div>
 		</div>
-        
-		<? include "nav.php"; ?><? include "detailed_nav.php"; ?>
 		
-        <!>
-		<! ========== Main Contents ============ >
-		<!>
-		<div id="content">
+		<? include "nav.php"; ?>
+		
+		<? include "detailed_nav.php"; ?>
 
+		
+		<div id="main">
 
 			<div class="item">
-				<h2><i>NEW!</i> &nbsp;&nbsp;Cytoscape 2.5.2</h2>
-				<div class="paragraph">
-				This is a bug-fix release that addresses issues related to XGMML loading and parsing old vizmap.props files.  Enjoy!
+				<h2><i>New!</i> Cytoscape 2.6.0</h2>
+				<a href="screenshots/2_6_ss1.png">
+					<img src="screenshots/2_6_ss1_thumb.png" alt="Cytoscape 2.6 Screenshot" />
+				</a>
+				
+				<div id="paragraph">
+					(Updated 4/10/2008) New features include: <br>
+					<ul id="paragraph">
+						<li><strong>Web Service Client Manager</strong></li>
+						<ul>
+						<li>New framework to integrate web service clients into Cytoscape</li>
+						<li>Web Service client plugins for downloading networks from 
+							PathwayCommons, IntAct, and NCBI Entrez Gene.</li>
+						<li>Annotation import web service plugin for BioMart. This is mainly for 
+							ID translation/synonym mapping</li>
+						</ul>
+						
+						<li><strong>Cytoscape Themes</strong></li>
+						<li><strong>Dynamic Filters</strong></li>
+						<li><strong>Network Manager supports multiple network selection</strong></li>
+						<li><strong>Label Positioning has been improved</strong></li>
+						<li><strong>Session saving occurs in memory</strong></li>
+						<li><strong>XGMML loading/saving optimized</strong></li>
+						<li><strong>Linkout integrated with attribute browser</strong></li>
+						<li><strong>Extra sample Visual Styles using new visual properties</strong></li>
+						<li><strong>Many, many bug fixes!</strong></li>
+					</ul>
 				</div>
 			</div>
 
 
 			<div class="item">
-				<h2>&nbsp;&nbsp;Cytoscape 2.5.1</h2>
-				<div class="paragraph">
-				A point release to address a variety of bugs in the 2.5.0 release.
+				<h2>Cytoscape 2.5.2</h2>
+				<div id="paragraph">
+					This is a bug-fix release that addresses issues related to XGMML 
+					loading and parsing old vizmap.props files.  Enjoy!
+				</div>
+			</div>
+
+			<div class="item">
+				<h2>Cytoscape 2.5.1</h2>
+				<div id="paragraph">
+					A point release to address a variety of bugs in the 2.5.0 release.
 				</div>
 			</div>
 
 
 			<div class="item">
-				<h2>&nbsp;&nbsp;Cytoscape 2.5.0</h2>
-				<a href="screenshots/2_5_ss1.png"><img src="screenshots/2_5_ss1_thumb.png" alt="Cytoscape 2.5 Screenshot" align="left" border="0"> </a>
-				<div class="paragraph">
-					(Updated 7/23/2007) <br>
-					New features include: <br>
-					<ul>
-						<li> New VizMapper User Interface
-							<ul>
-								<li> More intuitive.
-								<li> Continuous mapping editors.
-								<li> Visual editor for default view.
-								<li> Visual mapping browser. 
-								<li> Improved visual legend generator. 
-								<li> Utilities to generate discrete values. 
-							</ul>
-						<li>New Features for Visual Style
-							<ul>
-								<li> Transparency (opactiy) support.
-								<li> Continuous edge width.
-								<li> Color visual property is separated from Arrow and Edge.
-							</ul>
-						<li>New Filter User Interface
-							<ul>
-								<li> Intuitive widgets for basic filters.
-								
-								<li> Suggested search values with indexing.
-								
-								<li> Options to save in session or globally.
-							
-							</ul>
-						<li>Plugin Manager and New Plugin Website
-							<ul>
-								<li> Install/Update/Delete plugins from within Cytoscape.
-								
-								<li> Search for version compatible plugins from any host site.
-								
-								<li> Display list of installed plugins.
-							
-							</ul>
-						<li>Layout customization.
+				<h2>Cytoscape 2.5.0</h2>
+				<a href="screenshots/2_5_ss1.png">
+					<img src="screenshots/2_5_ss1_thumb.png" alt="Cytoscape 2.5 Screenshot" />
+				</a>
+				<div id="paragraph">
+					(Updated 7/23/2007) New features include: <br>
+					<ul id="paragraph">
+						<li><strong>New VizMapper User Interface</strong></li>
+						<ul>
+						<li>More intuitive</li>
+						<li>Continuous mapping editors</li>
+						<li>Visual editor for default view</li>
+						<li>Visual mapping browser</li>
+						<li>Improved visual legend generator</li> 
+						<li>Utilities to generate discrete values</li> 
+						</ul>
 						
-						<li>Undo and Redo.
+						<li><strong>New Features for Visual Style</strong></li>
+						<ul>
+						<li>Transparency (opactiy) support</li>
+						<li>Continuous edge width</li>
+						<li>Color visual property is separated from Arrow and Edge</li>
+						</ul>
 						
-						<li>Group API for plugin developers.
+						<li><strong>New Filter User Interface</strong></li>
+						<ul>
+						<li>Intuitive widgets for basic filters</li>		
+						<li>Suggested search values with indexing</li>
+						<li>Options to save in session or globally.
+						</ul>
 						
-						<li>Node stacking.
+						<li><strong>Plugin Manager and New Plugin Website</strong></li>
+						<ul>
+						<li>Install/Update/Delete plugins from within Cytoscape</li>	
+						<li>Search for version compatible plugins from any host site</li>
+						<li>Display list of installed plugins</li>
+						</ul>
 						
-						<li>Tested on both Java SE 5 and 6
-						
-						<li> Many, many bug fixes! 
-					
+						<li><strong>Layout customization</strong></li>
+						<li><strong>Undo and Redo</strong></li>
+						<li><strong>Group API for plugin developers</strong></li>
+						<li><strong>Node stacking</strong></li>
+						<li><strong>Tested on both Java SE 5 and 6</strong></li>
+						<li><strong>Many, many bug fixes!</strong></li>
 					</ul>
 				</div>
 			</div>
 			<div class="item">
-				<h2>&nbsp;&nbsp;<a href="retreat2007/index.php">Cytoscape Retreat 2007!</a></h2>
-				<a href="retreat2007/venue.php"><img src="retreat2007/images/magere-brug-small.jpg" alt="Amsterdam by night" border="0"> </a>
-				<div class="paragraph">
+				<h2><a href="retreat2007/index.php">Cytoscape Retreat 2007!</a></h2>
+				<a href="retreat2007/venue.php">
+					<img src="retreat2007/images/magere-brug-small.jpg" alt="Amsterdam by night"/>
+				</a>
+				<div id="paragraph">
 						Now in Europe! November 6<sup>th</sup> - 9<sup>th</sup><br>
 					 Including a public symposium on November 8<sup>th</sup>, with a formidable list of confirmed speakers among them
-					<ul>
-						<li>Leroy Hood 
-						
+					<ul id="paragraph">
+						<li>Leroy Hood 	
 						<li>Peter Sorger 
-						
 						<li>Ewan Birney 
-					
 					</ul>
 					 Hosted by the <a href="http://www.humangenetics-amc.nl" target="_blank">Human Genetics Department of the Academic Medical Center</a> in the vibrant historic city of <a href="/retreat2007/venue.php">Amsterdam</a>. </div>
 			</div>
 			<div class="item">
 				<h2>Cytoscape 2.4.1</h2>
-				<div class="paragraph">
+				<div id="paragraph">
 					No new features, but several bugs have been fixed. </div>
 			</div>
 			<div class="item">
 				<h2>Cytoscape 2.4.0 </h2>
 				<a href="screenshots/2_4_ss1.png"><img src="screenshots/2_4_ss1_thumb.png" alt="Cytoscape 2.4.0 Screenshot" align="left" border="0"> </a>
-				<div class="paragraph">
+				<div id="paragraph">
 					(Updated 1/16/2007) <br>
 					New features include: <br>
-					<ul>
+					<ul id="paragraph">
 						<li> Publication quality image generation.
 							<ul>
 								<li> Node label position adjustment.
@@ -178,28 +201,52 @@
 						<li> Many, many bug fixes! 
 					
 					</ul>
-					</ul>
-      See the <a href="cyto_2_4_features.php">Release Notes</a> for more detail. </div>
-			</div>
-			<div class="item">
-				<h2>Publications about Cytoscape</h2>
-				<div class="paragraph">
-					Shannon P, Markiel A, Ozier O, Baliga NS, Wang JT, Ramage D, Amin N, Schwikowski B, Ideker T.<br>
-					<strong>Cytoscape: a software environment for integrated models of biomolecular interaction networks.</strong><br>
-					Genome Research 2003 Nov; 13(11):2498-504<br><br>
+					
+      	See the <a href="cyto_2_4_features.php">Release Notes</a> for more detail. 
+      		</div>
+		</div>
+		
+		<div class="item">
+			<h2>Publications about Cytoscape</h2>
+			
+			<div id="paragraph">
+			<div class="pub">
+				Melissa S Cline, Michael Smoot, Ethan Cerami, Allan Kuchinsky, Nerius Landys, 
+				Chris Workman, Rowan Christmas, Iliana Avila-Campilo, Michael Creech, 
+				Benjamin Gross, Kristina Hanspers, Ruth Isserlin, Ryan Kelley, Sarah Killcoyne, 
+				Samad Lotia, Steven Maere, John Morris, Keiichiro Ono, Vuk Pavlovic, 
+				Alexander R Pico, Aditya Vailaya, Peng-Liang Wang, Annette Adler, Bruce R Conklin, 
+				Leroy Hood, Martin Kuiper, Chris Sander, Ilya Schmulevich, Benno Schwikowski, 
+				Guy J Warner, Trey Ideker & Gary D Bader
+				<h3>Integration of biological networks and gene expression data using Cytoscape</h3>
+				Nature Protocols 2, 2366 - 2382 (2007) Published online: 27 September 2007 
+				| doi:10.1038/nprot.2007.324<br><br>
 					<a href="http://www.genome.org/cgi/content/full/13/11/2498"> [Abstract] </a>
 					<a href="http://www.genome.org/cgi/reprint/13/11/2498"> [PDF] </a>
 					<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids=14597658&amp;dopt=Abstract"> [PubMed entry]</a>.
-				</div>
 			</div>
+			</div>
+			
+			<div id="paragraph">
+			<div class="pub">
+				Shannon P, Markiel A, Ozier O, Baliga NS, Wang JT, Ramage D, Amin N, 
+				Schwikowski B, Ideker T.<br>
+				<h3>Cytoscape: a software environment for integrated models of 
+				biomolecular interaction networks.</h3>
+				Genome Research 2003 Nov; 13(11):2498-504<br><br>
+					<a href="http://www.genome.org/cgi/content/full/13/11/2498"> [Abstract] </a>
+					<a href="http://www.genome.org/cgi/reprint/13/11/2498"> [PDF] </a>
+					<a href="http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?cmd=Retrieve&amp;db=PubMed&amp;list_uids=14597658&amp;dopt=Abstract"> [PubMed entry]</a>.
+			</div>
+			</div>
+		</div>
 			<div class="item">
-				<h2><i>Updated!&nbsp;</i>(July 24 2007)</i>&nbsp;&nbsp;Research using Cytoscape</h2>
+				<h2><i>Updated!&nbsp;</i>(July 24 2007)</i> Research using Cytoscape</h2>
 				
-				<div class="paragraph">
-				
+				<div id="paragraph">
 					<strong>As of July 2007, 281 publications are citing 
 					<a href="http://www.genome.org/cgi/content/full/13/11/2498">Shannon et al. (2003)</a>.</strong><br>
-					<ul>
+					<ul id="paragraph">
 						<li><a href="http://scholar.google.com/scholar?hl=en&lr=&cites=3669641697993554798">View Full Listing at Google Scholar</a></li>
 						<li><a href="http://www.pubmedcentral.nih.gov/tocrender.fcgi?action=cited&artid=403769">Link to Listing at PubMed Central</a></li>
 						<li><a href="http://highwire.stanford.edu/cgi/searchresults?fulltext=cytoscape&andorexactfulltext=and&author1=&pubdate_year=&volume=&firstpage=&src=hw&searchsubmit=redo&resourcetype=1&search=Search&fmonth=Jan&fyear=1844&tmonth=Jul&tyear=2007&fdatedef=1+January+1844&tdatedef=24+Jul+2007">
@@ -214,25 +261,27 @@
 				</div>
 			</div>
 			<div class="item">
-				<div class="paragraph">
+				<div id="paragraph">
 					 Past news articles are available <a href="past_news.php">here.</a></div>
 			</div>
 		</div>
+		
+		
 		<div id="rightbox">
+			<? include "help.php"; ?>
 			<?
-include "help.php";
+			if ($news_option == "atom") {
+				include "feed.php";
+			}else {
+				include "news.php";
+			}
+			?>
 
-if ($news_option == "atom") {
-	include "feed.php";
-} else {
-	include "news.php";
-	
-}
-?><?
-include "community_box.php";
-include "collab.php";
-?></div>
+			<? include "community_box.php"; include "collab.php"; ?>
+
+		</div>
 		<p><? include "footer.php"; ?></p>
+	</div>
 	</body>
 
 </html>
