c7f36c1ed04c1f8f2da00b9b8eaf0a25e45a42bf
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-04-22 21:38:10 +0000

    moved LayoutActivator
    git-svn-id: http://chianti.ucsd.edu/svn@13813 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape3/trunk/automatic.layout/src/main/java/csplugins/layout/LayoutActivator.java
+++ cytoscape3/trunk/automatic.layout/src/main/java/csplugins/layout/LayoutActivator.java
@@ -34,7 +34,7 @@
  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
 
-package org.cytoscape.layout.automatic;
+package csplugins.layout;
 
 import csplugins.layout.JGraphLayoutWrapper;
 import csplugins.layout.algorithms.GroupAttributesLayout;
