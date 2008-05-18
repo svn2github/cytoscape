a7a850b53e9a6821ca6d267e45c1125e38bdd27c
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-05-18 04:51:43 +0000

    Add some comments
    git-svn-id: http://chianti.ucsd.edu/svn@14006 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/tutorial03/src/cytoscape/tutorial03/Tutorial03.java
+++ csplugins/trunk/ucsd/pwang/tutorial03/src/cytoscape/tutorial03/Tutorial03.java
@@ -6,29 +6,26 @@ import cytoscape.plugin.CytoscapePlugin;
 import cytoscape.util.CytoscapeAction;
 import javax.swing.JOptionPane;
 
-
 /**
- * 
+ * This sample plugin shows how to create a submenu and add action listener to it.
+ * After this plugin is deployed (tutorial03.jar) to the plugins directory, 
+ * a menu item "tutorial03" will appear at Plugins menu. Click on the menu
+ * item "tutorial03" (Plugins-->tutorial03), a message dialog will show up.
  */
 public class Tutorial03 extends CytoscapePlugin {
 
-	/**
-	 * Add a menu item "Tutorial03" under menu "Plugins"
-	 * Select it through Plugins-->Tutorial03 will trigger a message box.
-	 */
 	public Tutorial03() {
 		
 		// (1) Create an Action
 		MyPluginMenuAction menuAction = new MyPluginMenuAction(this);
 		// (2) Add the action to Cytoscape menu
 		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction) menuAction);
-
 	}
 	
 	public class MyPluginMenuAction extends CytoscapeAction {
 
 		public MyPluginMenuAction(Tutorial03 myPlugin) {
-			// Add the menu item under menu pulldown "Plugins"
+			// Add the menu item under menu "Plugins"
 			super("Tutorial03");
 			setPreferredMenu("Plugins");
 		}
@@ -36,27 +33,5 @@ public class Tutorial03 extends CytoscapePlugin {
 		public void actionPerformed(ActionEvent e) {
 			JOptionPane.showMessageDialog(Cytoscape.getDesktop(),"MenuItem Plugins->Tutorial03 is selected!");	
 		}
-
-		/**
-		 *  DOCUMENT ME!
-		 *
-		 * @return  DOCUMENT ME!
-		 */
-		public boolean isInToolBar() {
-			return false;
-		}
-
-		/**
-		 *  DOCUMENT ME!
-		 *
-		 * @return  DOCUMENT ME!
-		 */
-		public boolean isInMenuBar() {
-			return true;
-		}
 	}
-
-	
-	
-	
 }
