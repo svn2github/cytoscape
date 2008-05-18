f5916ef636d8c111ba811145f0b54b108274ea58
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-05-18 05:10:25 +0000

    remove Exclude.txt after tar file is created
    git-svn-id: http://chianti.ucsd.edu/svn@14017 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/tarPlugin.sh
+++ csplugins/trunk/ucsd/pwang/tarPlugin.sh
@@ -3,5 +3,6 @@ USAGE="Usage: $0: <dir>"
 test $DIR ||{ echo $USAGE; exit;}
 
 find $DIR  -print | \
-egrep '/\.|*/classes/*|/bin/*|/build/*' > Exclude.txt
-tar cvf $DIR.tar -X Exclude.txt $DIR
+egrep '/\.|*/classes/*|/bin/*|/build/' > Exclude.txt
+tar cvf ./wikizip/$DIR.tar -X Exclude.txt $DIR
+rm Exclude.txt
