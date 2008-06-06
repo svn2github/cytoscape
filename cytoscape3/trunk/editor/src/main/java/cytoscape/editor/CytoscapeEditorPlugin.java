c4ee9874cba64643148539267fae5b934f749228
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-06-06 23:33:05 +0000

    made the editor a bundle
    git-svn-id: http://chianti.ucsd.edu/svn@14156 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape3/trunk/editor/src/main/java/cytoscape/editor/CytoscapeEditorPlugin.java
+++ cytoscape3/trunk/editor/src/main/java/cytoscape/editor/CytoscapeEditorPlugin.java
@@ -43,7 +43,9 @@ package cytoscape.editor;
 
 import cytoscape.Cytoscape;
 import cytoscape.CytoscapeInit;
-import cytoscape.plugin.CytoscapePlugin;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleActivator;
+
 
 
 /**
@@ -53,186 +55,36 @@ import cytoscape.plugin.CytoscapePlugin;
  * @version 1.0
  *
  */
-public class CytoscapeEditorPlugin extends CytoscapePlugin {
-    // MLC 07/20/07:
-    // private static boolean _initialized = false;
-    // MLC 07/20/07:
+public class CytoscapeEditorPlugin implements BundleActivator {
     private static final double VERSION = 2.53;
 
 	/**
 	 * Creates a new CytoscapeEditorPlugin object.
 	 */
-	public CytoscapeEditorPlugin() {
-		// MLC 07/24/06 BEGIN:
-		// CytoscapeEditorManager.log("CytoscapeEditor loaded ");
-		// MainPluginAction mpa = new MainPluginAction();
-		// CytoscapeEditorManager.setRunningEditorFramework(true);
-		// CytoscapeEditorManager.log("Setting up CytoscapeEditor");
-		// mpa.initializeCytoscapeEditor();
-	    // MLC 07/23/07:
-	    //CytoscapeEditorManager.setLoggingEnabled(true);
-		initializeCytoscapeEditor();
-
-		// MLC 07/24/06 END.
-	}
-
-    // MLC 07/20/07 BEGIN:
-    //    // overrides CytoscapePlugin.getPluginInfoObject():
-    //    public PluginInfo getPluginInfoObject() {
-    //        PluginInfo info = new PluginInfo();
-    //        info.setName("CytoscapeEditor");
-    //        info.setDescription("Add nodes and edges to a Cytoscape Network.");
-    ////        info.setCategory(PluginInfo.Category.CORE);
-    ////        info.setPluginVersion("2.5.01");
-    //        info.setCytoscapeVersion("2.5");
-    //        // info.setProjectUrl("http://www.cytoscape.org/download_agilent_literature_search_v2.5.php?file=litsearch_v2.4");
-    //        info.addAuthor("Allan Kuchinsky", "Agilent Labs");
-    //        info.addAuthor("Michael Creech", "Blue Oak Software");
-    //        return info;
-    //    }
-    // MLC 07/20/07 END.
-
-    // MLC 07/20/07 BEGIN:
-
-    //	/**
-    //	 * Overrides CytoscapePlugin.describe():
-    //	 */
-    //	public String describe() {
-    //		return "Add nodes and edges to a Cytoscape Network. ";
-    //	}
-
-	/**
-	 * sets various flags and registers various editors with the CytoscapeEditorManager
-	 *
-	 */
-
-        private void initializeCytoscapeEditor() {
-	// public static void initializeCytoscapeEditor() {
-	    //if (_initialized) {
-	    // return;
-	    // }
-	    // CytoscapeEditorManager.setRunningEditorFramework(true);
-	    // CytoscapeEditorManager.setEditingEnabled(true);
-	    // MLC 07/20/07 END.
+	public void start(BundleContext bc) {
 	    CytoscapeEditorManager.initialize();
 
-		// add default palette-based editor
 		CytoscapeEditorManager.register(CytoscapeEditorManager.DEFAULT_EDITOR_TYPE,
 		                                "cytoscape.editor.event.PaletteNetworkEditEventHandler",
-		                                // AJK: 02/03/06 have Default editor use current visual
-		// style
 		CytoscapeEditorManager.NODE_TYPE, CytoscapeEditorManager.EDGE_TYPE,
 		                                CytoscapeEditorManager.ANY_VISUAL_STYLE);
 
-		// AJK: 12/09/06 SimpleBioMoleculeEditor deleted 
-		/*
-		CytoscapeEditorManager.register("SimpleBioMoleculeEditor",
-		         "cytoscape.editor.event.PaletteNetworkEditEventHandler",
-		         CytoscapeEditorManager.NODE_TYPE, CytoscapeEditorManager.EDGE_TYPE,
-		         MapBioMoleculeEditorToVisualStyle.BIOMOLECULE_VISUAL_STYLE);
 
-		*/
-
-		// AJK: 12/09/06 BEGIN
-		//    register an editor to handle BioPAX visual style
-		//    TODO: this is a short-term contingency, to be overhauled
-		//          when vizmapper is overhauled for Cytoscape 2.5
-		//    	CytoscapeEditorManager.register("cytoscape.editor.editors.SimpleBioPAXEditor",
-		//	            "cytoscape.editor.event.PaletteNetworkEditEventHandler",
-		//	            "biopax.entity_type",   // controlling node attribute
-		//	            "BIOPAX_EDGE_TYPE",          // controlling edge type
-		//	            "BioPAX v 0_5");
-		//       
-
-		// AJK: 12/09/06 END
 		String editorName = CytoscapeEditorManager.DEFAULT_EDITOR_TYPE;
 
 		try {
 			CytoscapeEditor cyEditor = CytoscapeEditorFactory.INSTANCE.getEditor(editorName);
 			CytoscapeEditorManager.setCurrentEditor(cyEditor);
 
-			// MLC 08/06/06:
-			// CytoscapeEditorManager.setDefaultEditor(cyEditor);
 		} catch (InvalidEditorException ex) {
 			CytoscapeEditorManager.log("Error: cannot set up Cytoscape Editor: " + editorName);
 		}
 
 		Cytoscape.getDesktop().setVisualStyle(Cytoscape.getVisualMappingManager()
 		                                               .getCalculatorCatalog()
-		                                               .getVisualStyle( // MLC 08/06/06:
-		                                                                // CytoscapeInit.getDefaultVisualStyle()));
-		                                                                // MLC 08/06/06:
+		                                               .getVisualStyle( 
 		CytoscapeInit.getProperties().getProperty("defaultVisualStyle")));
-		// MLC 07/20/07:
-		// _initialized = true;
 	}
-
-	// MLC 07/24/06 BEGIN:
-	//	public class MainPluginAction extends AbstractAction {
-	//		public MainPluginAction() {
-	//			super("Cytoscape Editor");
-	//		}
-	//
-	//		/**
-	//		 * Gives a description of this plugin.
-	//		 */
-	//		public String describe() {
-	//			StringBuffer sb = new StringBuffer();
-	//			sb.append("Add nodes and edges to a Cytoscape Network. ");
-	//			return sb.toString();
-	//		}
-	//
-	//		/**
-	//		 * This method is called when the user selects the menu item.
-	//		 */
-	//		public void actionPerformed(ActionEvent ae) {
-	//			initializeCytoscapeEditor();
-	//		}
-	//
-	//		/**
-	//		 * sets various flags and registers various editors with the CytoscapeEditorManager
-	//		 *
-	//		 */
-	//		public void initializeCytoscapeEditor() {
-	//			
-	//
-	//			CytoscapeEditorManager.setEditingEnabled(false);
-	//
-	//			CytoscapeEditorManager.initialize();
-	//
-	//			// add default palette-based editor
-	//			CytoscapeEditorManager.register(
-	//					CytoscapeEditorManager.DEFAULT_EDITOR_TYPE,
-	//					"cytoscape.editor.event.PaletteNetworkEditEventHandler",
-	//					// AJK: 02/03/06 have Default editor use current visual
-	//					// style
-	//					CytoscapeEditorManager.NODE_TYPE,
-	//					CytoscapeEditorManager.EDGE_TYPE,
-	//					CytoscapeEditorManager.ANY_VISUAL_STYLE);
-	//
-	//			CytoscapeEditorManager.register("SimpleBioMoleculeEditor",
-	//					"cytoscape.editor.event.PaletteNetworkEditEventHandler",
-	//					CytoscapeEditorManager.NODE_TYPE,
-	//					CytoscapeEditorManager.EDGE_TYPE,
-	//					MapBioMoleculeEditorToVisualStyle.BIOMOLECULE_VISUAL_STYLE);
-	//
-	//			String editorName = CytoscapeEditorManager.DEFAULT_EDITOR_TYPE;
-	//			try {
-	//				CytoscapeEditor cyEditor = CytoscapeEditorFactory.INSTANCE
-	//						.getEditor(editorName);
-	//				CytoscapeEditorManager.setCurrentEditor(cyEditor);
-	//				CytoscapeEditorManager.setDefaultEditor(cyEditor);
-	//			} catch (InvalidEditorException ex) {
-	//				CytoscapeEditorManager.log("Error: cannot set up Cytoscape Editor: "
-	//						+ editorName);
-	//			}
-	//
-	//			Cytoscape.getDesktop().setVisualStyle(
-	//					Cytoscape.getVisualMappingManager()
-	//							.getCalculatorCatalog().getVisualStyle(
-	//									CytoscapeInit.getDefaultVisualStyle()));
-	//		}
-	//
-	//	}
-	// MLC 07/24/06 END.
+	public void stop(BundleContext bc) {
+	}
 }
