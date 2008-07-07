73ad24bbbfb5a6e661f7a13996aa453e7d5529b4
djiao <djiao@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-07-07 15:52:02 +0000

    Cleanup and added license
    git-svn-id: http://chianti.ucsd.edu/svn@14344 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/djiao/CheminfoPlugin/src/core/cytoscape/plugin/cheminfo/ChemInfoPlugin.java
+++ csplugins/trunk/soc/djiao/CheminfoPlugin/src/core/cytoscape/plugin/cheminfo/ChemInfoPlugin.java
@@ -1,11 +1,45 @@
+//$Revision: $ $Author: $ $Date: $
+/*
+  Copyright (c) 2006, 2007, 2008 The Cytoscape Consortium (www.cytoscape.org)
+
+  The Cytoscape Consortium is:
+  - Institute for Systems Biology
+  - University of California San Diego
+  - Memorial Sloan-Kettering Cancer Center
+  - Institut Pasteur
+  - Agilent Technologies
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
 package cytoscape.plugin.cheminfo;
 
 import giny.view.NodeView;
 
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
@@ -13,12 +47,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import javax.swing.JDialog;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
-import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 
 import cytoscape.CyNode;
@@ -30,6 +62,9 @@ import cytoscape.view.CytoscapeDesktop;
 import ding.view.DGraphView;
 import ding.view.NodeContextMenuListener;
 
+/**
+ * This plugin adds cheminformatics tools to Cytoscape.
+ */
 public class ChemInfoPlugin extends CytoscapePlugin implements
 		NodeContextMenuListener, PropertyChangeListener, ActionListener {
 
