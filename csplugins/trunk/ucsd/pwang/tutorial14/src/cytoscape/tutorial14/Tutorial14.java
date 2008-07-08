cdd1a598845410c0190938f0b5ca8d7a42fb59f7
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-07-08 06:27:05 +0000

    Use Mike's sample code
    git-svn-id: http://chianti.ucsd.edu/svn@14357 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/tutorial14/src/cytoscape/tutorial14/Tutorial14.java
+++ csplugins/trunk/ucsd/pwang/tutorial14/src/cytoscape/tutorial14/Tutorial14.java
@@ -1,25 +1,30 @@
 package cytoscape.tutorial14;
 
-import giny.model.Node;
-
-import java.awt.event.ActionEvent;
-import java.util.List;
-
 import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
-import cytoscape.data.CyAttributes;
-import cytoscape.data.readers.VisualStyleBuilder;
+import cytoscape.view.CyNetworkView;
 import cytoscape.plugin.CytoscapePlugin;
-import cytoscape.util.CytoscapeAction;
+import cytoscape.visual.ArrowShape;
+import cytoscape.visual.CalculatorCatalog;
+import cytoscape.visual.EdgeAppearanceCalculator;
 import cytoscape.visual.GlobalAppearanceCalculator;
+import cytoscape.visual.NodeAppearanceCalculator;
 import cytoscape.visual.NodeShape;
 import cytoscape.visual.VisualMappingManager;
 import cytoscape.visual.VisualPropertyType;
 import cytoscape.visual.VisualStyle;
-
+import cytoscape.visual.calculators.BasicCalculator;
+import cytoscape.visual.calculators.Calculator;
+import cytoscape.visual.mappings.BoundaryRangeValues;
+import cytoscape.visual.mappings.ContinuousMapping;
+import cytoscape.visual.mappings.DiscreteMapping;
+import cytoscape.visual.mappings.Interpolator;
+import cytoscape.visual.mappings.LinearNumberToColorInterpolator;
+import cytoscape.visual.mappings.ObjectMapping;
+import cytoscape.visual.mappings.PassThroughMapping;
+import cytoscape.util.CytoscapeAction;
 import java.awt.Color;
-import javax.swing.JOptionPane;
-
+import java.awt.event.ActionEvent;
 
 /**
  * 
@@ -34,7 +39,9 @@ public class Tutorial14 extends CytoscapePlugin {
 		MyPluginAction action = new MyPluginAction(this);
 		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction) action);
 	}
-	
+
+	public static final String vsName = "Example Visual Style";
+
 	public class MyPluginAction extends CytoscapeAction {
 
 		public MyPluginAction(Tutorial14 myPlugin) {
@@ -44,63 +51,104 @@ public class Tutorial14 extends CytoscapePlugin {
 		}
 
 		public void actionPerformed(ActionEvent e) {
-			
-			System.out.println("Tutorial14 is clicked!");
-			
-			// If there is no network, give a message to user
-			CyNetwork network = Cytoscape.getCurrentNetwork();		
-			if (network.getNodeCount() == 0) {
-				JOptionPane.showMessageDialog(Cytoscape.getDesktop(),"There is no node!");
-				return;
+			// get the network and view
+			CyNetwork network = Cytoscape.getCurrentNetwork();
+			CyNetworkView networkView = Cytoscape.getCurrentNetworkView();
+
+			// get the VisualMappingManager and CalculatorCatalog
+			VisualMappingManager manager = Cytoscape.getVisualMappingManager();
+			CalculatorCatalog catalog = manager.getCalculatorCatalog();
+
+			// check to see if a visual style with this name already exists
+			VisualStyle vs = catalog.getVisualStyle(vsName);
+			if (vs == null) {
+				// if not, create it and add it to the catalog
+				vs = createVisualStyle(network);
+				catalog.addVisualStyle(vs);
 			}
 			
-			// Select two nodes
-			Object [] nodeArray = network.nodesList().toArray();
-			List<Node> nodeList = network.nodesList();
-			Node node1 = nodeList.get(0);
-			Node node2 = nodeList.get(1);
+			networkView.setVisualStyle(vs.getName()); // not strictly necessary
 
-			// Create a visual style "myVisualStyle"
-			String styleName = "myVisualStyle";
-			VisualStyleBuilder graphStyle = new VisualStyleBuilder(styleName, false);
-			graphStyle.setNodeSizeLocked(false);
-									
-			// set some visual property for two nodes
-			graphStyle.addProperty(node1.getIdentifier(), VisualPropertyType.NODE_WIDTH, "30");
-			graphStyle.addProperty(node1.getIdentifier(), VisualPropertyType.NODE_FILL_COLOR, "#E1E1E1");
-			graphStyle.addProperty(node1.getIdentifier(), VisualPropertyType.NODE_SHAPE, NodeShape.DIAMOND.getShapeName());
+			// actually apply the visual style
+			manager.setVisualStyle(vs);
+			networkView.redrawGraph(true,true);
+		}
 
-			graphStyle.addProperty(node2.getIdentifier(), VisualPropertyType.NODE_WIDTH, "80");
-			graphStyle.addProperty(node2.getIdentifier(), VisualPropertyType.NODE_FILL_COLOR, "#0000E1");
-			graphStyle.addProperty(node2.getIdentifier(), VisualPropertyType.NODE_SHAPE, NodeShape.TRIANGLE.getShapeName());
+		VisualStyle createVisualStyle(CyNetwork network) {
 
-			// Create the visual style
-			graphStyle.buildStyle();
+			NodeAppearanceCalculator nodeAppCalc = new NodeAppearanceCalculator();
+			EdgeAppearanceCalculator edgeAppCalc = new EdgeAppearanceCalculator();
+			GlobalAppearanceCalculator globalAppCalc = new GlobalAppearanceCalculator(); 
+
+
+			// Passthrough Mapping - set node label 
+			//PassThroughMapping pm = new PassThroughMapping(new String(), "attr2");
+			PassThroughMapping pm = new PassThroughMapping(new String(), "attr2asdf");
+			Calculator nlc = new BasicCalculator("Example Node Label Calculator", 
+			                                     pm, VisualPropertyType.NODE_LABEL);
+			nodeAppCalc.setCalculator(nlc);
+
+
+			// Discrete Mapping - set node shapes 
+			DiscreteMapping disMapping = new DiscreteMapping(NodeShape.RECT,
+			                                                 ObjectMapping.NODE_MAPPING);
+			disMapping.setControllingAttributeName("attr1", network, false);
+			disMapping.putMapValue(new Integer(1), NodeShape.DIAMOND);
+			disMapping.putMapValue(new Integer(2), NodeShape.ELLIPSE);
+			disMapping.putMapValue(new Integer(3), NodeShape.TRIANGLE);
+
+			Calculator shapeCalculator = new BasicCalculator("Example Node Shape Calculator",
+			                                                  disMapping,
+															  VisualPropertyType.NODE_SHAPE);
+			nodeAppCalc.setCalculator(shapeCalculator);
+
+
+			// Continuous Mapping - set node color 
+			ContinuousMapping continuousMapping = new ContinuousMapping(Color.WHITE, 
+	                                                            ObjectMapping.NODE_MAPPING);
+			continuousMapping.setControllingAttributeName("attr3", network, false);
+
+	        Interpolator numToColor = new LinearNumberToColorInterpolator();
+	        continuousMapping.setInterpolator(numToColor);
+
+			Color underColor = Color.GRAY;
+			Color minColor = Color.RED;
+			Color midColor = Color.WHITE;
+			Color maxColor = Color.GREEN;
+			Color overColor = Color.BLUE;
+
+			// Create boundary conditions                  less than,   equals,  greater than
+			BoundaryRangeValues bv0 = new BoundaryRangeValues(underColor, minColor, minColor);
+			BoundaryRangeValues bv1 = new BoundaryRangeValues(midColor, midColor, midColor);
+			BoundaryRangeValues bv2 = new BoundaryRangeValues(maxColor, maxColor, overColor);
+
+	        // Set the attribute point values associated with the boundary values 
+			continuousMapping.addPoint(0.0, bv0);
+			continuousMapping.addPoint(1.0, bv1);
+			continuousMapping.addPoint(2.0, bv2);
 			
-			// Set the background color for this visual style
-			GlobalAppearanceCalculator gac = Cytoscape.getVisualMappingManager().getVisualStyle().getGlobalAppearanceCalculator();
-			gac.setDefaultBackgroundColor(Color.red);
+			Calculator nodeColorCalculator = new BasicCalculator("Example Node Color Calc", 
+			                                                continuousMapping, 
+														 VisualPropertyType.NODE_FILL_COLOR);
+			nodeAppCalc.setCalculator(nodeColorCalculator);
 
-			// Refresh the view
-			Cytoscape.getCurrentNetworkView().redrawGraph(false, true);
-		}
 
-		/**
-		 *  DOCUMENT ME!
-		 *
-		 * @return  DOCUMENT ME!
-		 */
-		public boolean isInToolBar() {
-			return false;
-		}
+			// Discrete Mapping - Set edge target arrow shape	
+			DiscreteMapping arrowMapping = new DiscreteMapping(ArrowShape.NONE,
+			                                                   ObjectMapping.EDGE_MAPPING);
+			arrowMapping.setControllingAttributeName("interaction", network, false);
+			arrowMapping.putMapValue("pp", ArrowShape.ARROW);
+			arrowMapping.putMapValue("pd", ArrowShape.CIRCLE);
 
-		/**
-		 *  DOCUMENT ME!
-		 *
-		 * @return  DOCUMENT ME!
-		 */
-		public boolean isInMenuBar() {
-			return true;
+			Calculator edgeArrowCalculator = new BasicCalculator("Example Edge Arrow Shape Calculator",
+	                                              arrowMapping, VisualPropertyType.EDGE_TGTARROW_SHAPE);
+			edgeAppCalc.setCalculator(edgeArrowCalculator);
+
+
+			// Create the visual style 
+			VisualStyle visualStyle = new VisualStyle(vsName, nodeAppCalc, edgeAppCalc, globalAppCalc);
+
+			return visualStyle;
 		}
 	}
 
