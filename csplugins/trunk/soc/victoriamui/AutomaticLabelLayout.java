0263fdf0ae3cd39709447e5a08cad48384bc2e6b
victoriamui <victoriamui@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-07-23 18:25:48 +0000

    Modified AutomaticLabelLayout.java - Removed unecessary code - Capable of applying plugin on network more than once - Removes label nodes and edges - LEFT: find out why applyLockedLayout doesn't lock the nodes
    git-svn-id: http://chianti.ucsd.edu/svn@14453 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/victoriamui/AutomaticLabelLayout.java
+++ csplugins/trunk/soc/victoriamui/AutomaticLabelLayout.java
@@ -17,12 +17,17 @@ import cytoscape.CyEdge;
 import cytoscape.Cytoscape;
 
 import cytoscape.view.CyNetworkView;
+import cytoscape.ding.DingNetworkView;
+import cytoscape.layout.CyLayouts;
 
 import giny.view.NodeView;
 import giny.view.Label;
+import cytoscape.view.CyNodeView;
 
 import cytoscape.visual.Appearance;
 import cytoscape.visual.VisualPropertyType;
+import cytoscape.visual.LabelPosition;
+
 import java.awt.Color;
 
 import cytoscape.data.Semantics;
@@ -55,11 +60,21 @@ public class AutomaticLabelLayout extends CytoscapePlugin {
 		// The attributes of all nodes in the network
 		CyAttributes nodeAtts = Cytoscape.getNodeAttributes();
 		
+		DingNetworkView dingNetView = new DingNetworkView(
+				Cytoscape.getCurrentNetwork(), 
+				Cytoscape.getCurrentNetwork().getTitle());
+		
 		// The List of static nodes
 		List<CyNode> staticNodes;
 		
+		// The List of static edges
+		List<CyNode> staticEdges;
+		
 		// The List of dynamic nodes
 		List<CyNode> dynamicNodes = new ArrayList<CyNode>();
+		
+		// The List of dynamic edges
+		List<CyEdge> dynamicEdges = new ArrayList<CyEdge>();
 	    
 	    /** 
 	     * Constructs a new Spring Embedded Algorithm object and initializes
@@ -80,17 +95,56 @@ public class AutomaticLabelLayout extends CytoscapePlugin {
 	    	// Get the set of CyNodes --> static nodes
 	    	staticNodes = Cytoscape.getCyNodesList();
 	    	
+	    	// Get the set of CyEdges --> static edges
+	    	staticEdges = Cytoscape.getCyEdgesList();
+	    	
+	    	// Print the set of all static nodes <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<REMOVE
+	    	for(CyNode n : staticNodes) {
+	    		System.out.println("StaticNode: " + n.getIdentifier());
+	    	}
+	    	
 	    	// Create label nodes for each of the node's labels --> dynamic nodes
 	    	createLabelNodes(staticNodes);
+
+	    	// Print the set of all dynamic nodes <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<REMOVE
+	    	for (CyNode m : dynamicNodes) {
+	    		System.out.println("DynamicNode: " + m.getIdentifier());
+	    	}
 	    	
-	    	// Apply Force-directed algorithm to dynamic nodes
-	    	System.out.println(network.getNodeCount());
+	    	// Apply Force-directed algorithm to dynamic nodes while locking static nodes
+	    	CyNode[] staticNodeArr = new CyNode[staticNodes.size()];
+	    	CyEdge[] staticEdgeArr = new CyEdge[staticEdges.size()];
+	    	dingNetView.applyLockedLayout(CyLayouts.getLayout("force-directed"),
+	    			staticNodes.toArray(staticNodeArr), 
+	    			staticEdges.toArray(staticEdgeArr));
+	        	    	
+	    	// Remove the label nodes and reposition the node labels so that
+	    	// they are located at where the node's corresponding label node
+	    	// is currently located.
+	    	removeLabelNodes();
 	    	
-	        // Remove label nodes and edges
+	    	// Print the set of all static nodes <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<REMOVE
+	    	for(CyNode n : staticNodes) {
+	    		System.out.println("FINAL StaticNode: " + n.getIdentifier());
+	    	}
+
+	    	// Print the set of all dynamic nodes <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<REMOVE
+	    	for (CyNode m : dynamicNodes) {
+	    		System.out.println("FINAL DynamicNode: " + m.getIdentifier());
+	    	}
+	    	
+	    	System.out.println("FINAL NUM NODES: " + network.getNodeCount());
+	    	
+	    	// Clean up the lists of static and dynamic nodes and edges
+	    	clear();
 	    	
 	    } //end construct()
 	    
-	    
+	    /**
+	     * Creates label nodes and edges, which connect nodes with their
+	     * corresponding label nodes.
+	     * @param nodes
+	     */
 	    public void createLabelNodes(List<CyNode> nodes) {
 	    	
 	    	Iterator<CyNode> nodeIt = nodes.iterator();
@@ -99,46 +153,82 @@ public class AutomaticLabelLayout extends CytoscapePlugin {
 	    	
 	    	while (nodeIt.hasNext()) {
 	    		CyNode current = nodeIt.next();
-
-	    		Appearance app = new Appearance();
+    			
+    			// Create a new CyNode for the label
+    			labelNode = Cytoscape.getCyNode("child_" + current.getIdentifier(), 
+    					true);
 	    		
-	    		// Create a new label node
-	    		labelNode = Cytoscape.getCyNode(current.toString() + "a", true);
+	    		// Add the label node to the current network
 	    		network.addNode(labelNode);
-	    		
-	    		// Replace the node's label with an empty string
-	    		//current.setIdentifier(""); ---------------------------------------UNCOMMENT
 	    		
 	    		// Create a new edge between the node and its label node
 	    		CyEdge edge = Cytoscape.getCyEdge(current.getIdentifier(), 
 	    				"", labelNode.getIdentifier(), 
 	    				Semantics.INTERACTION);
-	    		network.addEdge(edge);
+	    		network.addEdge(edge); // Add new edge to the current network
+	    		
+	    		// Add the newly created label node and edge to the list
+	    		// of dynamicNodes and dynamicEdges respectively.
 	    		dynamicNodes.add(labelNode);
-	    		
-	    		
-	    		// Make label node's background and border transparent
-	    		// ================== SAND-BOX =================
-	    		CyAttributes nodeAtts = Cytoscape.getNodeAttributes();
-	    		nodeAtts.setAttribute(labelNode.getIdentifier(),
-	    				"node.opacity","0");
-	    		nodeAtts.setAttribute(labelNode.getIdentifier(),
-	    				"node.borderOpacity","0");
-	    		
-	    		// Make the edge connecting the node and its node label transparent
-	    		CyAttributes edgeAtts = Cytoscape.getEdgeAttributes();
-	    		edgeAtts.setAttribute(edge.getIdentifier(), "edge.opacity", "0");
-	    		
-	    		// ================== NODE LABEL SAND-BOX =====================
-	    		System.out.println("node ID: " + current.getIdentifier());
-	    		System.out.println("label node ID: " + labelNode.getIdentifier());
+	    		dynamicEdges.add(edge);
 	    	}
-	    	System.out.println("createLabelNodes: " + network.getNodeCount());
+	    	networkView.updateView();
+	    	networkView.redrawGraph(true, true);
+	    }
+	    
+	    /**
+	     * Removes all label nodes and edges, and repositions their parent 
+	     * node's label at where the label node currently is
+	     */
+	    public void removeLabelNodes() {
+	    	
+    		nodeAtts = Cytoscape.getNodeAttributes();
+	    	LabelPosition lp;    	
+	    	int count = 0;
+	    	
+	    	// - Remove all label nodes
+	    	// - Relocate all node's labels to corresponding label node's
+	    	// current position
+	    	for(CyNode n : dynamicNodes) {
+	    		CyNode parentNode = staticNodes.get(count);
+	    		
+	    		NodeView parentView = networkView.getNodeView(
+	    				parentNode.getRootGraphIndex());
+	    		
+	    		NodeView nView = networkView.getNodeView(n.getRootGraphIndex());
+	    		
+	    		lp = new LabelPosition();
+	    		
+	    		lp.setOffsetX(nView.getXPosition() - parentView.getXPosition());
+	    		lp.setOffsetY(nView.getYPosition() - parentView.getYPosition());
+	    		
+	    		nodeAtts.setAttribute(parentNode.getIdentifier(), 
+	    				"node.labelPosition", lp.shortString());
+	    		
+	    		// Remove the label node
+	    		network.removeNode(n.getRootGraphIndex(), true);
+	    		count++;
+	    	}
+	    	
+	    	// Remove all label edges
+	    	for(CyEdge e : dynamicEdges) {
+	    		network.removeEdge(e.getRootGraphIndex(), true);
+	    	}
+
 	    	networkView.updateView();
 	    	networkView.redrawGraph(true, true);
 	    }
 	    
 	    
+	    /**
+	     * Clears the dynamic and static lists of nodes and edges
+	     */
+	    public void clear() {
+	    	staticNodes.clear();
+	    	staticEdges.clear();
+	    	dynamicNodes.clear();
+	    	dynamicEdges.clear();
+	    }
 	    /** 
 	     * Returns the name to construct property strings 
 	     * for this layout with */
