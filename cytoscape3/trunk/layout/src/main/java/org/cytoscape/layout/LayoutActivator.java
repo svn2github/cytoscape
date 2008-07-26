83bc7b64c20c4dceb83aae76436a901e5f4eea87
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-07-26 00:52:26 +0000

    now using a ServiceTracker
    git-svn-id: http://chianti.ucsd.edu/svn@14495 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape3/trunk/layout/src/main/java/org/cytoscape/layout/LayoutActivator.java
+++ cytoscape3/trunk/layout/src/main/java/org/cytoscape/layout/LayoutActivator.java
@@ -3,7 +3,7 @@ package org.cytoscape.layout;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.ServiceReference;
-import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
 
 import java.util.Hashtable;
 
@@ -12,15 +12,19 @@ public class LayoutActivator implements BundleActivator {
 	public void start(BundleContext bc) {
 		System.out.println("org.cytoscape.layout.LayoutActivator start");
 		try {
-		ServiceReference[] sr = bc.getServiceReferences(CyLayoutAlgorithm.class.getName(), null);
-		if ( sr != null )
-			for (ServiceReference r : sr ) {
-				CyLayoutAlgorithm cla = (CyLayoutAlgorithm)bc.getService(r);
-				String prefMenu = (String)r.getProperty("preferredMenu");
-				if ( prefMenu == null || prefMenu.equals("") )
-					prefMenu = "Cytoscape Layouts";
-				CyLayouts.addLayout(cla, prefMenu);
-			}
+
+			ServiceTracker trac = new ServiceTracker(bc, CyLayoutAlgorithm.class.getName(), null) {
+				 @Override public Object addingService(ServiceReference r) {
+					CyLayoutAlgorithm cla = (CyLayoutAlgorithm)super.addingService(r);
+					String prefMenu = (String)r.getProperty("preferredMenu");
+					if ( prefMenu == null || prefMenu.equals("") )
+						prefMenu = "Cytoscape Layouts";
+					CyLayouts.addLayout(cla, prefMenu);
+					return cla;			 	
+				 }
+			};
+			trac.open();
+
 		} catch (Exception e) { e.printStackTrace(); }
 	}
 
