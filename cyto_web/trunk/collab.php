0ae0d1edf359f373eee359adb39ae5698729622b
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-08-12 22:37:07 +0000

    Link error fixed.
    git-svn-id: http://chianti.ucsd.edu/svn@14642 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/collab.php
+++ cyto_web/trunk/collab.php
@@ -3,24 +3,24 @@
 <h2>The Cytoscape project is an ongoing collaboration between:</h2>
 <div class="navigation">
 <ul>
-	<li><a href="ttp://www.agilent.com"><img alt="Agilent"
-		src="images/logo/agilent_logo_web"></a></li>
+	<li><a href="http://www.agilent.com"><img alt="Agilent"
+		src="images/logo/agilent_logo_web.png"></a></li>
 	<li><a href="http://www.systemsbiology.org"><img alt="ISB"
-		src="images/logo/isb_logo_web"></a></li>
+		src="images/logo/isb_logo_web.png"></a></li>
 	<li><a href="http://cbio.mskcc.org"><img alt="MSKCC"
-		src="images/logo/mskcc_logo_web"></a></li>
+		src="images/logo/mskcc_logo_web.png"></a></li>
 	<li><a href="http://portal.ncibi.org/gateway/index.html"><img
-		alt="NCIBI" src="images/logo/ncibi_logo_web"></a></li>
+		alt="NCIBI" src="images/logo/ncibi_logo_web.png"></a></li>
 	<li><a href="http://www.pasteur.fr"><img alt="Pasteur"
-		src="images/logo/pasteur_logo_web"></a></li>
+		src="images/logo/pasteur_logo_web.png"></a></li>
 	<li><a href="http://www.utoronto.ca/"><img alt="Toronto"
-		src="images/logo/toronto_logo_web"></a></li>
+		src="images/logo/toronto_logo_web.png"></a></li>
 	<li><a href="http://www.ucsd.edu"><img alt="UCSD"
-		src="images/logo/ucsd_logo_web"></a></li>
+		src="images/logo/ucsd_logo_web.png"></a></li>
 	<li><a href="http://www.ucsf.edu/"><img alt="UCSF"
-		src="images/logo/ucsf_logo_web"></a></li>
+		src="images/logo/ucsf_logo_web.png"></a></li>
 	<li><a href="http://www.unilever.com/"><img alt="Unilever"
-		src="images/logo/unilever_logo_web"></a></li>
+		src="images/logo/unilever_logo_web.png"></a></li>
 </ul>
 </div>
 </div>
