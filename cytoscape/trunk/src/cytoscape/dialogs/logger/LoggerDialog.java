af603438d884630eb6b8b0edfbe77ad9c02d1262
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-08-19 19:42:48 +0000

    Change to more readable color for warnings & fixup html coding a little
    git-svn-id: http://chianti.ucsd.edu/svn@14755 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/dialogs/logger/LoggerDialog.java
+++ cytoscape/trunk/src/cytoscape/dialogs/logger/LoggerDialog.java
@@ -142,7 +142,7 @@ public class LoggerDialog extends javax.swing.JDialog implements CyLogHandler
 
 		// Do whatever formatting we need
 		msg = msg.replace("\n","<br>");
-		msg = msg.replace(" ","&nbsp;");
+		msg = msg.replace("\t","&nbsp;&nbsp;&nbsp;&nbsp;");
 
     Messages.add(msg);
     // Make sure it gets added back in
@@ -200,9 +200,9 @@ public class LoggerDialog extends javax.swing.JDialog implements CyLogHandler
     StringBuffer sb = new StringBuffer();
 		String color = "#333333";
 		if (level.equals(LogLevel.LOG_ERROR))
-			color = "red";
+			color = "#ff0000";
 		else if (level.equals(LogLevel.LOG_WARN))
-			color = "orange";
+			color = "#ff4500";
 			
     sb.append("<html><style type='text/css'>");
     sb.append("body,th,td,div,p,h1,h2,li,dt,dd ");
