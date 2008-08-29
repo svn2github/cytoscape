13ff700922d88a9047565f532fcbe9331fb86246
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-08-29 22:25:22 +0000

    fix queryParameter Construction
    git-svn-id: http://chianti.ucsd.edu/svn@14803 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/StringClient/src/org/cytoscape/StringClient/StringStub.java
+++ csplugins/trunk/ucsd/pwang/StringClient/src/org/cytoscape/StringClient/StringStub.java
@@ -1,6 +1,5 @@
 package org.cytoscape.StringClient;
 
-import cytoscape.layout.Tunable;
 import cytoscape.util.ModuleProperties;
 import cytoscape.util.ModulePropertiesImpl;
 
@@ -15,57 +14,46 @@ import cytoscape.util.ModulePropertiesImpl;
 public class StringStub {
 	// Base URL of the String Web Service API
 	private String baseURL = "http://string.embl.de/api/psi-mi/interactions?";
+	//private String baseURL = "http://stitch.embl.de/api/psi-mi/interactions?";
+	
 	private String URLStr= "";
 	public StringStub() {
 	}
 		
 	public void setQueryParam(final ModuleProperties pProps, String pQuery){
-		
+				
 		ModulePropertiesImpl props = (ModulePropertiesImpl) pProps;
 		
 		// Construct queryParam
-		String queryParameter = ""; //"identifier=PGH1_HUMAN&species=auto_detect";
-		
+		String queryParameter = "";
+
+		String[] keywords = pQuery.split("[ \n\t]+");
+		queryParameter = "identifier="; 
+		for (int i=0; i<keywords.length; i++) {
+			queryParameter += keywords[i];
+			if (i < (keywords.length-1)) {
+				queryParameter +=  "%20";
+			}
+		}
+
 		int additional_network_nodes = ((Integer) props.get("additional_network_nodes").getValue()).intValue();
 		int limit = ((Integer) props.get("limit").getValue()).intValue();
-		int network_depath = ((Integer) props.get("network_depth").getValue()).intValue();
+		int network_depth = ((Integer) props.get("network_depth").getValue()).intValue();
 		int required_score = ((Integer) props.get("required_score").getValue()).intValue();
 		String species = ((String) props.get("species").getValue()).toString();
 
-		String[] keywords = pQuery.split("[ |\n]");
-		
-		queryParameter = "identifier="; 
-		for (int i=0; i<keywords.length; i++) {
-			queryParameter += keywords[i] + "%20";			
-		}
-		queryParameter += "&";
+		queryParameter += "&additional_network_nodes="+ additional_network_nodes;
+		queryParameter += "&limit="+ limit;
+		queryParameter += "&network_depth="+ network_depth;
+		queryParameter += "&required_score="+ required_score;
+		queryParameter += "&species="+ species;
 
-		// Construct URLstr -- baseURL + queryParam
-		
-		URLStr = baseURL + queryParameter; //"http://string.embl.de/api/psi-mi/interactions?identifier=PGH1_HUMAN&species=auto_detect";
-		URLStr = "http://string.embl.de/api/psi-mi/interactions?identifier=PGH1_HUMAN&species=auto_detect";
+		// Construct URLstr
+		URLStr = baseURL + queryParameter;
 	}
 
 	public String getURLstr(){
 		//Sample URL: http://string.embl.de/api/psi-mi/interactions?identifier=PGH1_HUMAN&species=auto_detect
 		return URLStr;
-	}
-	
-	public static void main(String[] argv ) {
-		StringStub anInstance = new StringStub();
-		
-		ModulePropertiesImpl props = new ModulePropertiesImpl("String", "wsc");
-
-		props.add(new Tunable("additional_network_nodes", "Additional network nodes", Tunable.INTEGER,
-		                      new Integer(0)));
-		props.add(new Tunable("limit", "limit", Tunable.INTEGER, new Integer(10)));		
-		props.add(new Tunable("network_depth", "Network depth", Tunable.INTEGER, new Integer(1)));
-		props.add(new Tunable("required_score", "Required score", Tunable.INTEGER, new Integer(400)));
-		props.add(new Tunable("species", "species", Tunable.STRING, new String("auto_detect")));
-
-		anInstance.setQueryParam(props, "Ppotein1");
-		
-		String URLstr = anInstance.getURLstr();
-		System.out.println("URLstr = " + URLstr + "\n");
-	}
+	}	
 }
