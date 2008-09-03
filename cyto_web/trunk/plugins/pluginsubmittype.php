6d9877b44d91b5fed09d16279dba5de6c804de23
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-09-03 18:51:25 +0000

    Fix format problem
    git-svn-id: http://chianti.ucsd.edu/svn@14834 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/plugins/pluginsubmittype.php
+++ cyto_web/trunk/plugins/pluginsubmittype.php
@@ -19,25 +19,26 @@
 <div id="topbar">
 	<div class="title">Choose plugin file type</div>
 </div>
-
+<div id="container">
 <?php include "../nav.php"; ?>
 
-
-<body>
 <p>&nbsp;</p>
 <p class="style1">Please choose plugin file type to submit</p>
-<table width="960" height="121" border="0">
+<p class="style1">&nbsp;</p>
+<table width="960" height="204" border="0">
   <tr>
-    <th width="101" ><a href="pluginsubmit.php">Jar file</a></th>
+    <th width="101" height="40" ><a href="pluginsubmit.php">Jar file</a></th>
     <td width="849" ><div align="left">A single <a href="http://www.cytoscape.org/cgi-bin/moin.cgi/Cytoscape_Plugin_Tutorial"> properly formatted plugin jar</a>. We will read (and validate) the plugin metadata (name, description, version, etc.) from the jar.</div></td>
   </tr>
   <tr>
-    <th height="64" scope="row"><a href="pluginsubmitzip.php">Zip file</a></th>
+    <th height="69" scope="row"><a href="pluginsubmitzip.php">Zip file</a></th>
     <td>A file with extension .zip, which should contain the plugin jar and any other necessary data.  The file will be loaded as is and you will need to enter the plugin metadata.</td>
   </tr>
   <tr>
     <th scope="row"><a href="pluginsubmiturl.php">URL</a> </th>
-    <td>Plugin information with project URL, from where user can download the plugin manually </td>
+    <td><p>Plugin information with project URL. Since Cytoscape does not host the Zip/Jar file, they will not appear at the Cytoscape plugin manager. Users should download them manualy by following the project URLs, if they are interested.
+      </p>
+      <p></p></td>
   </tr>
 </table>
 
