e41135eeedf4d3dfeb11ea7b04e4dd3f159988dd
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-09-26 22:18:24 +0000

    Add more fields
    git-svn-id: http://chianti.ucsd.edu/svn@14975 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/AutoBugReport/src/bugreport/BugReport.java
+++ csplugins/trunk/ucsd/pwang/AutoBugReport/src/bugreport/BugReport.java
@@ -3,12 +3,15 @@ package bugreport;
 import java.io.File;
 
 class BugReport {
-	private String bugReporter = "guest";		
-	private String summary = "unknown";
-	private String description = "none";
-	private String cytoscape_version = "3.x";
-	private File[] attachedFiles = null;
-	//private String summary = "unknown";
+	private String bugReporter = "guest";// AutomaticBugReporter		
+	private String summary = "";
+	private String description = "";
+	private String cytoscape_version = "";
+	private File attachedFile = null;
+	private String os = ""; // WIN/LINUX/MAC/OTHER
+	private String os_version = "";
+	private String additionalInfo = "";
+	private String stepsToReproduce = "";
 		
 	public void setBugReporter(String pReporter){
 		bugReporter = pReporter;
@@ -26,11 +29,29 @@ class BugReport {
 		summary = pSummary;
 	}
 
-	public void setAttachedFiles(File[] pAttachedFiles){
-		attachedFiles = pAttachedFiles;
+	public void setOS(String pOS){
+		os = pOS;
+	}
+
+	public void setOSVersion(String pVersion){
+		os_version = pVersion;
+	}
+
+	public void setStepsToReproduce(String pStepsToReproduce){
+		stepsToReproduce = pStepsToReproduce;
+	}
+
+	public void setAdditionalInfo(String pInfo){
+		additionalInfo = pInfo;
+	}
+
+	public void setAttachedFile(File pAttachedFile){
+		attachedFile = pAttachedFile;
 	}
 		
-	//
+	/**
+	 * @return String The reporter account on Cytoscape's Mantis bug tracker.
+	 */
 	public String getBugReporter(){
 		return bugReporter;
 	}
@@ -38,15 +59,53 @@ class BugReport {
 	public String getCytoscapeVersion(){
 		return cytoscape_version;
 	}
-		
+
+	/**
+	 * @return String The stack trace of the exception.
+	 */		
 	public String getDescription() {
 		return description;
 	}
+
+	/**
+	 * @return String Summary (title) of the bug report.
+	 */		
 	public String getSummary(){
 		return summary;
 	}
-		
-	public File[] getAttachedFiles(){
-		return attachedFiles;
+
+	/**
+	 * @return String Operating system -- WIN/LINUX/MAC/OTHER.
+	 */		
+	public String getOS(){
+		return os;
+	}
+
+	/**
+	 * @return String Version of Operating System.
+	 */		
+	public String getOSVersion(){
+		return os_version;
+	}
+	
+	/**
+	 * @return String User's description -- What were you doing when this problem happened?
+	 */
+	public String getAdditionalInfo(){
+		return additionalInfo;
+	}
+
+	/**
+	 * @return String The command list (history) to reproduce the problem
+	 */	
+	public String getStepsToReproduce(){
+		return stepsToReproduce;
+	}
+
+	/**
+	 * @return File A session file.
+	 */
+	public File getAttachedFile(){ // max size: 5M (Mantis 1.1.1)
+		return attachedFile;
 	}				
 }
