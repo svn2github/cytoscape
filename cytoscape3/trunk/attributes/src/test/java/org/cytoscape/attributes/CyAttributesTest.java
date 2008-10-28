8dd1fa7cf3795d0eaef99249e075ae46f91cd292
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-10-28 21:25:52 +0000

    Fixed package name.
    git-svn-id: http://chianti.ucsd.edu/svn@15133 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape3/trunk/attributes/src/test/java/org/cytoscape/attributes/CyAttributesTest.java
+++ cytoscape3/trunk/attributes/src/test/java/org/cytoscape/attributes/CyAttributesTest.java
@@ -34,7 +34,7 @@
  along with this library; if not, write to the Free Software Foundation,
  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
-package cytoscape.data;
+package org.cytoscape.attributes;
 
 import org.cytoscape.attributes.CyAttributes;
 import org.cytoscape.attributes.impl.CyAttributesImpl;
