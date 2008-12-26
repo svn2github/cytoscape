a79d6c1711bdc57403eeffe568d74663f5b562ec
abeld <abeld@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2008-12-26 11:14:00 +0000

    refactored-viewmodel: branch viewmodel bundle to column-oriented-viewmodel to work on column-oriented implementation
    git-svn-id: http://chianti.ucsd.edu/svn@15512 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape3/branches/abeld-gsoc/dev/refactored-viewmodel/column-oriented-viewmodel/src/main/java/org/cytoscape/viewmodel/internal/RowOrientedViewImpl.java
@@ -0,0 +1,180 @@
+/*
+ Copyright (c) 2008, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+package org.cytoscape.viewmodel.internal;
+
+import org.cytoscape.viewmodel.View;
+import org.cytoscape.viewmodel.VisualProperty;
+
+import java.util.HashMap;
+
+
+/**
+ * The base interface that defines the methods used to set visual properties
+ * for nodes, edges, and networks.
+ *
+ * Think of it as a row in the viewmodel table.
+ * 
+ * @param <S> the base (model-level) object for which this is a View. For example, CyNode or CyEdge
+ */
+public class RowOrientedViewImpl<S> implements View<S> {
+	private static final String VP_IS_NULL = "VisualProperty is null";
+
+	private final S source;
+	private final HashMap<VisualProperty<?>, Object> vpValues;
+
+	// note: this surely could be done more efficiently...:
+	private final HashMap<VisualProperty<?>, Boolean> bypassLocks;
+	private final long suid;
+
+	/**
+	 * Creates a new RowOrientedViewImpl object.
+	 *
+	 * @param source  DOCUMENT ME!
+	 */
+	public RowOrientedViewImpl(final S source) {
+		suid = IdFactory.getNextSUID();
+		this.source = source;
+		vpValues = new HashMap<VisualProperty<?>, Object>();
+		bypassLocks = new HashMap<VisualProperty<?>, Boolean>();
+	}
+
+	/**
+	 * The VisualProperty object identifies which visual property to set and the Object
+	 * determines the value.   We should probably consider doing something more type safe like
+	 * what we're doing for Attributes.
+	 *
+	 * @param <T>  DOCUMENT ME!
+	 * @param vp  DOCUMENT ME!
+	 * @param o  DOCUMENT ME!
+	 */
+	public <T> void setVisualProperty(final VisualProperty<T> vp, final T o) {
+		if (vp == null)
+			throw new NullPointerException(VP_IS_NULL);
+
+		final Boolean b = bypassLocks.get(vp);
+
+		if ((b == null) || !b.booleanValue())
+			vpValues.put(vp, o);
+	}
+
+	/**
+	 * Getting visual properties in this way incurs lots of casting. We should probably
+	 * consider doing something more type safe like what we're doing for Attributes.
+	 *
+	 * @param <T>  DOCUMENT ME!
+	 * @param vp  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public <T> T getVisualProperty(final VisualProperty<T> vp) {
+		if (vp == null)
+			throw new NullPointerException(VP_IS_NULL);
+
+		if (vpValues.containsKey(vp))
+			return (T) vpValues.get(vp);
+		else
+
+			return vp.getDefault();
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public S getSource() {
+		return source;
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public long getSUID() {
+		return suid;
+	}
+
+	/**
+	 * Sets ByPass value. This value won't be override by
+	 * VisualStyle or such, until it is cleared.
+	 *
+	 * Note: this should only be used when the user, interactively
+	 * sets a bypass either through the gui or through a scripting
+	 * api. All other access should be done by defining an
+	 * appropriate MappingCalculator.
+	 *
+	 * @param <T>  DOCUMENT ME!
+	 * @param vp  The visualProperty for which to set the value
+	 * @param value the value to set
+	 */
+	public <T> void setLockedValue(final VisualProperty<T> vp, final T value){
+		if (vp == null)
+			throw new NullPointerException(VP_IS_NULL);
+
+		setVisualProperty(vp, value);
+		bypassLocks.put(vp, Boolean.TRUE);
+	}
+
+	/**
+	 * @param vp the VisualProperty
+	 * @return true if current VisualProperty value is locked
+	 */
+	public boolean isValueLocked(final VisualProperty<?> vp){
+		if (vp == null)
+			throw new NullPointerException(VP_IS_NULL);
+
+		final Boolean value = bypassLocks.get(vp);
+
+		if (value == null) {
+			return false;
+		} else {
+			return value.booleanValue();
+		}
+	}
+
+	/**
+	 * Clear value lock for given VisualProperty.
+	 * 
+	 * @param vp the VisualProperty 
+	 */
+	public void clearValueLock(final VisualProperty<?> vp){
+		if (vp == null)
+			throw new NullPointerException(VP_IS_NULL);
+
+		bypassLocks.put(vp, Boolean.FALSE);
+
+	}
+}
