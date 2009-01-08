96e253be655c0a62713b821412cc99d4469af518
Mathieu <Mathieu@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-08 16:15:09 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@15600 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/pasteur/mathieu/Tunables/src/GuiInterception/GuiHandlerFactory.java
+++ csplugins/trunk/pasteur/mathieu/Tunables/src/GuiInterception/GuiHandlerFactory.java
@@ -3,21 +3,19 @@ package GuiInterception;
 import Factory.*;
 import HandlerFactory.HandlerFactory;
 import java.lang.reflect.*;
-import java.security.acl.Group;
 import Tunable.Tunable;
 import Utils.*;
 
 public class GuiHandlerFactory<T> implements HandlerFactory<Guihandler> {
-
 
 	public Guihandler getHandler(Field f, Object o, Tunable t){
 		
 		Class<?> type = f.getType();
 		
 		if(type == Integer.class || type == int.class)
-			return new IntegerHandler(f,o,t);//new BoundedHandler<String>(f,o,t);
+			return new IntegerHandler(f,o,t);
 		if(type == Double.class || type == double.class)
-			return new DoubleHandler(f,o,t);//new BoundedHandler<String>(f,o,t);
+			return new DoubleHandler(f,o,t);
 		if(type == Bounded.class)
 			return new BoundedHandler<String>(f,o,t);		
 		if(type == Boolean.class)
