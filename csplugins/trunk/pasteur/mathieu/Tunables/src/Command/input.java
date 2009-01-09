10e6eb4f3ed42f462066b4c466f90ffecf61ceb8
Mathieu <Mathieu@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-09 17:21:12 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@15620 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/pasteur/mathieu/Tunables/src/Command/input.java
+++ csplugins/trunk/pasteur/mathieu/Tunables/src/Command/input.java
@@ -17,11 +17,11 @@ public class input implements command {
 	@Tunable(description = "String",group="Group2")
 	public String string1 = new String("Cytoscape");
 	@Tunable(description="Bounded",flag=Param.Double,group="Group1")
-	public Bounded boundObject1 = new Bounded("-10","10", true, true);
+	public Bounded bounded1 = new Bounded("-10","10", true, true);
 	@Tunable(description ="Double",group="Group1")
 	public Double double1 = new Double(3.4);
 	@Tunable(description="Bounded",flag=Param.IntegerSlider,group="Group1")
-	public Bounded boundObject2 = new Bounded("-2","40", true, true);
+	public Bounded bounded2 = new Bounded("-2","40", true, true);
 	@Tunable(description="Integer",group="Group1")
 	public Integer integer2 = new Integer(33);
 	@Tunable(description="Boolean",group="Group2")
@@ -40,7 +40,6 @@ public class input implements command {
 	public Float float1 = new Float(64.9084);
 	@Tunable(description ="Long",group="Group4")
 	public Long long1 = new Long(223233333);
-	
 	@Tunable(description="ListSingleSelection with Integers",group="Group5")
 	public ListSingleSelection<Integer> listOfIntegers;
 	
