b95299ff4511f0a8759307e7b1c1b87a7f0af443
apico <apico@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-15 05:51:25 +0000

    new tag
    git-svn-id: http://chianti.ucsd.edu/svn@15664 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/tags/Cyto-2.6.2/src/cytoscape/actions/SelectAdjacentEdgesAction.java
@@ -0,0 +1,111 @@
+/*
+ File: SelectAdjacentEdgesAction.java
+
+ Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+//-------------------------------------------------------------------------
+// $Revision: 11118 $
+// $Date: 2007-07-24 14:42:07 -0700 (Tue, 24 Jul 2007) $
+// $Author: mes $
+//-------------------------------------------------------------------------
+package cytoscape.actions;
+
+import cytoscape.Cytoscape;
+
+import cytoscape.CyNetwork;
+
+import cytoscape.CyNode;
+
+import cytoscape.CyEdge;
+
+import cytoscape.util.CytoscapeAction;
+
+//-------------------------------------------------------------------------
+import java.awt.event.ActionEvent;
+
+import java.util.Set;
+
+import java.util.HashMap;
+
+import javax.swing.event.MenuEvent;
+
+//-------------------------------------------------------------------------
+/**
+ *
+ */
+public class SelectAdjacentEdgesAction extends CytoscapeAction {
+	/**
+	 * Creates a new SelectAdjacentEdgesAction object.
+	 */
+	public SelectAdjacentEdgesAction() {
+		super("Select adjacent edges");
+		setPreferredMenu("Select.Edges");
+		setAcceleratorCombo(java.awt.event.KeyEvent.VK_E, ActionEvent.ALT_MASK);
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param e DOCUMENT ME!
+	 */
+	public void actionPerformed(ActionEvent e) {
+		// GinyUtils.selectAllEdges( Cytoscape.getCurrentNetworkView() );
+		CyNetwork network = Cytoscape.getCurrentNetwork();
+		HashMap<CyEdge,CyEdge> edgeMap = new HashMap<CyEdge,CyEdge>();
+
+		// Get the list of selected nodes
+		for (CyNode node: (Set<CyNode>)network.getSelectedNodes()) {
+			// Get the list of edges connected to this node
+			int[] edgeIndices = network.getAdjacentEdgeIndicesArray(node.getRootGraphIndex(), true, true, true);
+			// For each node, select the appropriate edges
+			if (edgeIndices == null)
+				continue;
+
+			for (int i = 0; i < edgeIndices.length; i++)  {
+				CyEdge edge = (CyEdge)network.getEdge(edgeIndices[i]);
+				edgeMap.put(edge,edge);
+			}
+		}
+		network.setSelectedEdgeState(edgeMap.keySet(), true);
+
+		if (Cytoscape.getCurrentNetworkView() != null) {
+			Cytoscape.getCurrentNetworkView().updateView();
+		}
+	} // action performed
+
+    public void menuSelected(MenuEvent e) {
+        enableForNetwork();
+    }
+}
---
+++ cytoscape/tags/Cyto-2.6.2/src/cytoscape/data/webservice/WebServiceClient.java
@@ -0,0 +1,155 @@
+/*
+ Copyright (c) 2006, 2007, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+package cytoscape.data.webservice;
+
+import cytoscape.data.webservice.WebServiceClientManager.ClientType;
+
+import cytoscape.util.ModuleProperties;
+
+import java.lang.reflect.Method;
+
+import java.util.Collection;
+
+
+/**
+ * Web service client wrapper for Cytoscape.
+ * <p>
+ * Usually, developers first use code generator to create Java code
+ * from WSDL.  The generated code should be wrapped by this interface
+ * to be used in Cytoscape framework.
+ * </p>
+ * All web service clients <strong>must</strong> implement this method.
+ *
+ * @param <S> Client stub object.  This is service dependent.  For example, NCBI's eUtils
+ *  stub has the class EUtilsServiceSoap.
+ *
+ * @author kono
+ * @version 0.5
+ * @since Cytoscape 2.6
+ */
+public interface WebServiceClient<S> {
+	/**
+	 *  Returns client ID.
+	 *  This ID should be unique.
+	 *
+	 * @return  client ID as String.
+	 */
+	public String getClientID();
+
+	/**
+	 *  Returns display name of this client.
+	 *  This is more human readable name for this client.
+	 *
+	 * @return  display name for this client.
+	 */
+	public String getDisplayName();
+
+	/**
+	 *  Returns client type.
+	 *  A client can have multiple types.
+	 *
+	 *  For example, NCBI Client can be used as network import
+	 *  and attribute import, so this array contains both types.
+	 *
+	 * @see cytoscape.data.webservice.WebServiceClientManager.ClientType
+	 *
+	 * @return  Array of client types.
+	 */
+	public ClientType[] getClientType();
+
+	/**
+	 *  Return true if the given ClientType is compatible with this client.
+	 *
+	 *  If a client is used as network importer, isCompatibleType(ClientType.NETWORK)
+	 *  returns true, but isCompatibleType(ClientType.ATTRIBUTE) returns false.
+	 *
+	 * @param ct Client type.
+	 *
+	 * @return  true if compatible.
+	 */
+	public boolean isCompatibleType(ClientType ct);
+
+	/**
+	 *  Get client stub object.
+	 *  All services available from this client will be
+	 *  accessed through this stub.  This will be used when developer wants
+	 *  to access "raw" API of this service.
+	 *
+	 * @return Client stub.  This object type depends on service.
+	 */
+	public S getClientStub();
+
+	/**
+	 * Set stub to this client.
+	 *
+	 * @param stub client stub used in this client.
+	 */
+	public void setClientStub(S clientStub);
+
+	/**
+	 * Get properties used by the Tunable.
+	 *
+	 * @return
+	 */
+	public ModuleProperties getProps();
+
+	/**
+	 * Set props used by Tunable.
+	 *
+	 * @param props Property.
+	 */
+	public void setProps(ModuleProperties props);
+
+	/**
+	 * Returns all available methods accessible through client stub.
+	 *
+	 * @return Collection of methods available through stub.
+	 */
+	public Collection<Method> getAccessibleMethods();
+
+	/**
+	 * Get description for this client.
+	 *
+	 * @return  Description as a string.  Users should write parser for this return value.
+	 */
+	public String getDescription();
+
+	/**
+	 *  Set description for this service.
+	 *
+	 * @param description Description as a String.
+	 */
+	public void setDescription(String description);
+}
