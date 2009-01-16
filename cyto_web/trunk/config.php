ce32d9e11f68aaea66eefea8fb89ecbb515885ec
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-16 00:28:18 +0000

    updated for 2.6.2
    git-svn-id: http://chianti.ucsd.edu/svn@15669 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/config.php
+++ cyto_web/trunk/config.php
@@ -34,13 +34,14 @@ $chianti_base = "http://chianti.ucsd.edu/";
 # to update all refereces to the "latest" version,
 # including release notes and download links
 #################################################
-$latest_version = "2.6.1";
+$latest_version = "2.6.2";
 $latest_release_notes_link = "cyto_2_6_features.php";
-$latest_download_link = "download.php?file=cyto2_6_1";
+$latest_download_link = "download.php?file=cyto2_6_2";
 $latest_manual_pdf = "manual/Cytoscape2_6Manual.pdf";
 $latest_manual_html = "manual/Cytoscape2_6Manual.html";
-$latest_javadoc = $chianti_base."Cyto-2_6_1/javadoc/";
+$latest_javadoc = $chianti_base."Cyto-2_6_2/javadoc/";
 $release_array = array( 
+					  'cyto2_6_2'   => '2.6.2',
 					  'cyto2_6_1'   => '2.6.1',
 					  'cyto2_6_0'   => '2.6.0',
 					  'cyto2_5_2'   => '2.5.2',
@@ -192,6 +193,17 @@ $cyto2_6_1_source_east = $chianti_base."Cyto-2_6_1/cytoscapeSource-v2.6.1.tar.gz
 $cyto2_6_1_mac = $chianti_base."Cyto-2_6_1/Cytoscape_2_6_1_macos.dmg";
 $cyto2_6_1_windows = $chianti_base."Cyto-2_6_1/Cytoscape_2_6_1_windows.exe";
 $cyto2_6_1_linux = $chianti_base."Cyto-2_6_1/Cytoscape_2_6_1_unix.sh";
+
+#################################################
+# Cytoscape 2.6.2 Release File Names
+#################################################
+$cyto2_6_2_gz_east = $chianti_base."Cyto-2_6_2/cytoscape-v2.6.2.tar.gz";
+$cyto2_6_2_zip_east = $chianti_base."Cyto-2_6_2/cytoscape-v2.6.2.zip";
+$cyto2_6_2_source_east = $chianti_base."Cyto-2_6_2/cytoscapeSource-v2.6.2.tar.gz";
+
+$cyto2_6_2_mac = $chianti_base."Cyto-2_6_2/Cytoscape_2_6_2_macos.dmg";
+$cyto2_6_2_windows = $chianti_base."Cyto-2_6_2/Cytoscape_2_6_2_windows.exe";
+$cyto2_6_2_linux = $chianti_base."Cyto-2_6_2/Cytoscape_2_6_2_unix.sh";
 
 #################################################
 # URL for Cytoscape Install Anywhere
