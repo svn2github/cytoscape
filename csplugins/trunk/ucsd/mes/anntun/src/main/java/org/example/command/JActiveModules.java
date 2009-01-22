5ed9768f710ef4d7be24de622700a38c94f5c162
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-22 02:43:24 +0000

    added params to JActiveModules
    git-svn-id: http://chianti.ucsd.edu/svn@15692 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/mes/anntun/src/main/java/org/example/command/JActiveModules.java
+++ csplugins/trunk/ucsd/mes/anntun/src/main/java/org/example/command/JActiveModules.java
@@ -4,14 +4,12 @@ package org.example.command;
 
 import org.example.tunable.Tunable;
 import org.example.tunable.util.*;
+import java.util.*;
 
 public class JActiveModules implements Command {
 
-//	@Tunable(description="Expression Attributes for Analysis")
-//	ListMultipleSelection<String> attrs = new ListMultipleSelection<String>();
-	//attrs.add("gal1RGSig");
-	//attrs.add("gal4RGSig");
-	//attrs.add("gal80RSig");
+	@Tunable(description="Expression Attributes for Analysis")
+	ListMultipleSelection<String> attrs; 
 
 	@Tunable(description="Number of Modules", group={"General Parameters"})
 	BoundedInteger numMod = new BoundedInteger(0,5,1000,false,false);
@@ -25,19 +23,63 @@ public class JActiveModules implements Command {
 	@Tunable(description="Regional Scoring?", group={"General Parameters"})
 	boolean regionalScoring = true;
 
-	@Tunable(description="Search depth", group={"Searching Parameters"})
+	@Tunable(description="Strategy", group={"Strategy"}, flags={"radio"})
+	ListSingleSelection<String> strategy;
+
+	@Tunable(description="Search depth", group={"Strategy","Searching Parameters"})
 	int depth = 1;
 
-	@Tunable(description="Search from selected nodes?", group={"Searching Parameters"})
+	@Tunable(description="Search from selected nodes?", group={"Strategy","Searching Parameters"})
 	boolean searchFromSelected = true;
 
-	@Tunable(description="Consider Max depth from start nodes?", group={"Searching Parameters"})
+	@Tunable(description="Consider Max depth from start nodes?", group={"Strategy","Searching Parameters"})
 	boolean maxDepth = true;
 
-	@Tunable(description="Max depth from start nodes", group={"Searching Parameters"})
+	@Tunable(description="Max depth from start nodes", group={"Strategy","Searching Parameters"})
 	int maxDepthFromStart = 2;
-	
-	
+
+	@Tunable(description="Iterations", group={"Strategy","Annealing Parameters"})
+	BoundedInteger iterations = new BoundedInteger(0,2500,100000000,false,false);
+			
+	@Tunable(description="Start Temp", group={"Strategy","Annealing Parameters"})
+	BoundedDouble startTemp = new BoundedDouble(0.0001,1.0,100.0,false,false);
+
+	@Tunable(description="End Temp", group={"Strategy","Annealing Parameters"})
+	BoundedDouble endTemp = new BoundedDouble(0.0001,0.01,100.0,false,false);
+
+	@Tunable(description="Quenching", group={"Strategy","Annealing Parameters","Annealing Extensions"})
+	boolean quenching = true;
+
+	@Tunable(description="Hubfinding", group={"Strategy","Annealing Parameters","Annealing Extensions"})
+	boolean hubfinding = false;
+
+	@Tunable(description="Hubfinding Value", group={"Strategy","Annealing Parameters","Annealing Extensions"})
+	int hubfindingValue = 10;
+
+	@Tunable(description="Seed Graph Options", group={"Strategy","Annealing Parameters"}, flags={"radio"})
+	ListSingleSelection<String> seedOption;
+
+
+	public JActiveModules() {
+		List<String> l = new ArrayList<String>();
+		l.add("gal1RGSig");
+		l.add("gal4RGSig");
+		l.add("gal80RSig");
+
+		attrs = new ListMultipleSelection<String>(l);
+
+		List<String> s = new ArrayList<String>();
+		s.add("Search");
+		s.add("Anneal");
+
+		strategy = new ListSingleSelection<String>(s);
+
+		List<String> seed = new ArrayList<String>();
+		seed.add("Non-Random Starting Graph");
+		seed.add("Random Based on Current Time");
+
+		seedOption = new ListSingleSelection<String>(seed);
+	}
 
 	public void execute() {
 		System.out.println("Running JActiveModules...");
