c74d0d3fb334da2e732a3342bb109c494bfc7cef
mmichaud <mmichaud@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-27 15:36:44 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@15729 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/pasteur/mathieu/Tunables/src/Command/input.java
+++ csplugins/trunk/pasteur/mathieu/Tunables/src/Command/input.java
@@ -8,27 +8,22 @@ import java.util.ArrayList;
 
 public class input implements command {
 	
-
 	@Tunable(description = "FirstName",group="Name")
 	public String string1 = new String("John");
 	@Tunable(description = "LastName",group="Name")
 	public String string2 = new String("Smith");
-
 	@Tunable(description="Day",group="Today's date",flag=Param.Horizontal)
 	public ListSingleSelection<String> dayNames;
 	@Tunable(description="Month",group="Today's date",flag=Param.Horizontal)
 	public ListMultipleSelection<String> monthNames;
 	@Tunable(description="Number",group="Today's date",flag=Param.Horizontal)
-	public ListSingleSelection<Integer> listOfIntegers;
-	
+	public ListSingleSelection<Integer> listOfIntegers;	
 	@Tunable(description ="Identification Code",group="Other")
 	public Long long1 = new Long((long)223248997);
 	@Tunable(description="Number of children",group="Other")
 	public Integer integer1 = new Integer(0);
-	
 	@Tunable(description="Age",flag={Param.Slider},group="Birth")
 	public BoundedInteger bounded2 = new BoundedInteger(0,18,130,false,true);
-
 	@Tunable(description="How many $ for 1 euro ?",group="Euro/Dollar")
 	public BoundedDouble bounded3 = new BoundedDouble((double)1,(double)1.4,(double)1.8,true,true);
 	@Tunable(description="Do you like Tunables",group="Tunable Test")
