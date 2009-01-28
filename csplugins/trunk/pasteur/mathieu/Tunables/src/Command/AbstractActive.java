886a360e36cfd4d40c7aada9102c4c03f4ac854c
Mathieu <Mathieu@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-28 18:31:38 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@15755 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/pasteur/mathieu/Tunables/src/Command/AbstractActive.java
@@ -0,0 +1,36 @@
+package Command;
+
+import Tunable.Tunable;
+import Utils.*;
+import java.util.*;
+
+public class AbstractActive implements command {
+
+	@Tunable(description="Expression Attributes for Analysis")
+	public ListMultipleSelection<String> attrs; 
+
+	@Tunable(description="Number of Modules", group={"General Parameters"})
+	public BoundedInteger numMod = new BoundedInteger(0,5,1000,false,false);
+
+	@Tunable(description="Overlap Threshold", group={"General Parameters"})
+	public BoundedDouble overlap = new BoundedDouble(0.0,0.8,1.0,false,false);	
+
+	@Tunable(description="Adjust for size?", group={"General Parameters"})
+	public boolean adjustForSize = true;
+
+	@Tunable(description="Regional Scoring?", group={"General Parameters"})
+	public Boolean regionalScoring = new Boolean(true);
+
+	public AbstractActive() {
+        List<String> l = new ArrayList<String>();
+        l.add("gal1RGSig");
+        l.add("gal4RGSig");
+        l.add("gal80RSig");
+
+        attrs = new ListMultipleSelection<String>(l);
+	}
+	
+	public void execute() {
+		System.out.println(this.getClass().getSimpleName()+" has been executed");
+	}
+}
