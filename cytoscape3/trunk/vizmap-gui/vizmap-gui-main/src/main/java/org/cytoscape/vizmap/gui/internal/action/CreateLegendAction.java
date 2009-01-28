96c5b1fbc287d491c6a94c24e3a825613319b61f
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-28 21:59:18 +0000

    removed use of SwingWorker
    git-svn-id: http://chianti.ucsd.edu/svn@15768 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape3/trunk/vizmap-gui/vizmap-gui-main/src/main/java/org/cytoscape/vizmap/gui/internal/action/CreateLegendAction.java
+++ cytoscape3/trunk/vizmap-gui/vizmap-gui-main/src/main/java/org/cytoscape/vizmap/gui/internal/action/CreateLegendAction.java
@@ -3,7 +3,7 @@ package org.cytoscape.vizmap.gui.internal.action;
 import java.awt.event.ActionEvent;
 
 
-import cytoscape.util.SwingWorker;
+//import cytoscape.util.SwingWorker;
 
 public class CreateLegendAction extends AbstractVizMapperAction {
 
@@ -15,17 +15,17 @@ public class CreateLegendAction extends AbstractVizMapperAction {
 	private static final long serialVersionUID = 707566797144402515L;
 
 	public void actionPerformed(ActionEvent e) {
-		final SwingWorker worker = new SwingWorker() {
-			public Object construct() {
+//		final SwingWorker worker = new SwingWorker() {
+//			public Object construct() {
 				LegendDialog ld = new LegendDialog(menuItem,
 						vmm.getVisualStyle());
 				ld.setLocationRelativeTo(menuItem);
 				ld.setVisible(true);
 
-				return null;
-			}
-		};
+//				return null;
+//			}
+//		};
 
-		worker.start();
+//		worker.start();
 	}
 }
