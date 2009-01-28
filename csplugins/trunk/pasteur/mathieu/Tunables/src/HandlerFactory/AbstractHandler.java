da06bb2ae49a61349195cbdf44d9e210a55ae464
Mathieu <Mathieu@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-01-28 18:35:39 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@15760 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/pasteur/mathieu/Tunables/src/HandlerFactory/AbstractHandler.java
@@ -0,0 +1,43 @@
+
+package HandlerFactory;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+
+import Tunable.Tunable;
+
+public abstract class AbstractHandler implements Handler {
+
+	protected Field f;
+	protected Method m;
+	protected Object o;
+	protected Tunable t;
+
+	public AbstractHandler(Field f, Object o, Tunable t) {
+		this.f = f;
+		this.o = o;
+		this.t = t;
+	}
+
+	public AbstractHandler(Method m, Object o, Tunable t) {
+		this.m = m;
+		this.o = o;
+		this.t = t;
+	}
+
+	public Field getField() {
+		return f;
+	}
+
+	public Method getMethod() {
+		return m;
+	}
+
+	public Object getObject() {
+		return o;
+	}
+
+	public Tunable getTunable() {
+		return t;
+	}
+}
--- csplugins/trunk/pasteur/mathieu/Tunables/src/HandlerFactory/Handler.java
+++ csplugins/trunk/pasteur/mathieu/Tunables/src/HandlerFactory/Handler.java
@@ -1,3 +1,12 @@
 package HandlerFactory;
 
-public interface Handler{}
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import Tunable.Tunable;
+
+public interface Handler{
+	Field getField();
+	Method getMethod();
+	Object getObject();
+	Tunable getTunable();
+}
