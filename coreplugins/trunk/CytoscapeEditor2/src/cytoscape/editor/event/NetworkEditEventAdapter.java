46e2da9261db32d51bf92943815370a923f7d560
mcreech <mcreech@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-04 18:12:34 +0000

    Removed use of canvas and view in favor of deriving these from the current network view. Added getCurrentDGraphView().
    git-svn-id: http://chianti.ucsd.edu/svn@15825 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/CytoscapeEditor2/src/cytoscape/editor/event/NetworkEditEventAdapter.java
+++ coreplugins/trunk/CytoscapeEditor2/src/cytoscape/editor/event/NetworkEditEventAdapter.java
@@ -1,10 +1,33 @@
-/*
- * Created on May 17, 2005
- *
- */
+/* -*-Java-*-
+********************************************************************************
+*
+* File:         NetworkEditEventAdapter.java
+* RCS:          $Header: $
+* Description:  
+* Author:       Allan Kuchinsky
+* Created:      Tue May 17 11:17:49 2005
+* Modified:     Wed Feb 04 08:44:13 2009 (Michael L. Creech) creech@w235krbza760
+* Language:     Java
+* Package:      
+* Status:       Experimental (Do Not Distribute)
+*
+* (c) Copyright 2009, Agilent Technologies, all rights reserved.
+*
+********************************************************************************
+*
+* Revisions:
+*
+* Tue Feb 03 11:19:51 2009 (Michael L. Creech) creech@w235krbza760
+*  Removed use of canvas and view in favor of deriving these from the
+*  current network view. Added getCurrentDGraphView().
+********************************************************************************
+*/
+
 package cytoscape.editor.event;
 
+import cytoscape.Cytoscape;
 import cytoscape.editor.CytoscapeEditor;
+import cytoscape.view.CyNetworkView;
 
 import ding.view.DGraphView;
 import ding.view.InnerCanvas;
@@ -20,6 +43,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+// TODO (02/03/09): All deprecated references to setView(), getView(),
+// and start(DGraphView) should be updated along with all higher-level
+// methods that pass unneeded variables DGrapViews to lower-level
+// methods. ALso, this class should probably be turned into an
+// interface or an abstract class and BasicNetworkEditEventHandler
+// become the first full implementation.
 
 /**
  *
@@ -41,9 +70,13 @@ import java.awt.event.MouseMotionListener;
 public class NetworkEditEventAdapter implements MouseListener, MouseMotionListener, ActionListener,
                                                 PhoebeCanvasDropListener, KeyListener,
                                                 cytoscape.data.attr.MultiHashMapListener {
-	protected InnerCanvas canvas;
-	protected DGraphView view;
-	CytoscapeEditor _caller;
+    // MLC 02/03/09 BEGIN:
+    //	protected InnerCanvas canvas;
+    //	protected DGraphView view;
+    // CytoscapeEditor _caller;
+	protected CytoscapeEditor _caller;
+    private boolean started; 
+    // MLC 02/03/09 END.
 
 	/**
 	 * Creates a new NetworkEditEventAdapter object.
@@ -55,51 +88,73 @@ public class NetworkEditEventAdapter implements MouseListener, MouseMotionListen
 	 * starts up the event handler on the input network view adds an input event
 	 * listener to the view's canvas
 	 *
+	 * @deprecated Use no argument start().
 	 * @param view
 	 *            a Cytoscape network view
 	 */
 
-	// public void start(PGraphView view) {
 	public void start(DGraphView view) {
-		this.view = view;
-		this.canvas = view.getCanvas();
-		// AJK: 04/15/06 for Cytoscape 2.3
-		// canvas.addInputEventListener(this);
-		
-		canvas.addMouseListener(this);
-		canvas.addMouseMotionListener(this);
-		canvas.addKeyListener(this);
-
-		// CytoscapeEditorManager.log("Mouse and MotionListeners added to " + canvas);
-		// CytoscapeEditorManager.log("Canvas has total number of Listeners = " +
-		// canvas.getMouseListeners().length);
+	    // MLC 02/03/09 BEGIN:
+	    start();
+	    //	    this.view = view;
+	    //	    this.canvas = view.getCanvas();
+	    //	    // AJK: 04/15/06 for Cytoscape 2.3
+	    //	    // canvas.addInputEventListener(this);
+	    //		
+	    //	    canvas.addMouseListener(this);
+	    //	    canvas.addMouseMotionListener(this);
+	    //	    canvas.addKeyListener(this);
+	    //	    // CytoscapeEditorManager.log("Mouse and MotionListeners added to " + canvas);
+	    //	    // CytoscapeEditorManager.log("Canvas has total number of Listeners = " +
+	    //	    // canvas.getMouseListeners().length);
+	    // MLC 02/03/09 END.
 	}
 
+    // MLC 02/03/09 BEGIN:
+    public void start() {
+	InnerCanvas canvas = getCurrentDGraphView().getCanvas();
+	started = true;
+	canvas.addMouseListener(this);
+	canvas.addMouseMotionListener(this);
+	canvas.addKeyListener(this);
+    }
+
+    // MLC 02/03/09 END.
 	/**
 	 * stops the event handler by removing the input event listener from the
 	 * canvas this is called when the user switches between editors
 	 *
 	 */
 	public void stop() {
-		if (canvas != null) {
-			// AJKL: 04/15/06 for Cytoscape 2.3
-			// canvas.removeInputEventListener(this);
-			canvas.removeMouseListener(this);
-			canvas.removeMouseMotionListener(this);
-			canvas.removeKeyListener(this);
-			this.view = null;
-			this.canvas = null;
+	    // MLC 02/03/09 BEGIN:
+	    // if (canvas != null) {
+	    if (started) {
+		InnerCanvas canvas = getCurrentDGraphView().getCanvas();
+	    // MLC 02/03/09 END.
+		// AJKL: 04/15/06 for Cytoscape 2.3
+		// canvas.removeInputEventListener(this);
+		canvas.removeMouseListener(this);
+		canvas.removeMouseMotionListener(this);
+		canvas.removeKeyListener(this);
+		// MLC 02/03/09 BEGIN:
+		started = false;
+		// this.view = null;
+		// this.canvas = null;
+		// MLC 02/03/09 END.
 		}
-	}
+	    }
 
 	/**
-	 *
+	 * Gets the current canvas based on Cytoscape.getCurrentNetworkView().
 	 * @return the current canvas
 	 */
 
 	// public PCanvas getCanvas() {
 	public InnerCanvas getCanvas() {
-		return canvas;
+	    // MLC 02/03/09 BEGIN:
+	    // return canvas;
+	    return getCurrentDGraphView().getCanvas();
+	    // MLC 02/03/09 END.
 	}
 
 	/**
@@ -253,21 +308,37 @@ public class NetworkEditEventAdapter implements MouseListener, MouseMotionListen
 	}
 
 	/**
+	 * @deprecated Use getCurrentDGraphView().
 	 * @return Returns the view.
 	 */
 
 	// public PGraphView getView() {
+
 	public DGraphView getView() {
-		return view;
+	    // MLC 02/03/09 BEGIN:
+	    return getCurrentDGraphView ();
+	    // return view;
+	    // MLC 02/03/09 END.
 	}
 
 	/**
+	 * @deprecated There is no longer any need to set the view.
 	 * @param view
 	 *            The view to set.
 	 */
-
 	// public void setView(PGraphView view) {
 	public void setView(DGraphView view) {
-		this.view = view;
+	    
+	    // this.view = view;
 	}
+
+    // MLC 02/03/09 BEGIN:
+    // Return the Cytoscape current network view as a DGraphView:
+     public DGraphView getCurrentDGraphView () {
+	 CyNetworkView cnv = Cytoscape.getCurrentNetworkView();
+	 // CytoscapeEditorManager.log ("current newtork view used: " + cnv.getTitle());
+	 return (DGraphView)cnv;	
+     }
+    // MLC 02/03/09 END.
+
 }
