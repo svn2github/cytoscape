98a7f23a8562d468b0133d8bc1fd213345377d2f
mcreech <mcreech@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-07 01:34:41 +0000

    Changed mousePressed() and itemDropped() to use current network view vs cached CytoscapeEditor value, deprecated two argument constructor. Fixed bug whre itemDropped() would allow dropping of a palette entry on a NetworkView that wasn't the current network view.
    git-svn-id: http://chianti.ucsd.edu/svn@15848 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/agilent/creech/HyperEdgeEditor/src/cytoscape/hyperedge/editor/event/HyperEdgeEditEventHandler.java
+++ csplugins/trunk/agilent/creech/HyperEdgeEditor/src/cytoscape/hyperedge/editor/event/HyperEdgeEditEventHandler.java
@@ -6,7 +6,7 @@
 * Description:
 * Author:       Michael L. Creech
 * Created:      Fri Jul 21 11:28:16 2006
-* Modified:     Wed Oct 08 07:06:47 2008 (Michael L. Creech) creech@w235krbza760
+* Modified:     Fri Feb 06 13:03:02 2009 (Michael L. Creech) creech@w235krbza760
 * Language:     Java
 * Package:
 * Status:       Experimental (Do Not Distribute)
@@ -17,6 +17,11 @@
 *
 * Revisions:
 *
+* Wed Jan 28 15:53:50 2009 (Michael L. Creech) creech@w235krbza760
+*  Changed mousePressed() and itemDropped() to use current network
+*  view vs cached CytoscapeEditor value, deprecated two argument constructor.
+*  Fixed bug whre itemDropped() would allow dropping of a palette entry
+*  on a NetworkView that wasn't the current network view.
 * Wed Jul 09 09:56:39 2008 (Michael L. Creech) creech@w235krbza760
 *  Added check that Editor component is active to itemDropped()
 *  to avoid handling events when the editor tab isn't the current tab.
@@ -41,12 +46,13 @@ import cytoscape.hyperedge.editor.HyperEdgeEditor;
 import cytoscape.view.CyNetworkView;
 
 import giny.view.EdgeView;
-
 import phoebe.PhoebeCanvasDropEvent;
 
 import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
+
+import ding.view.InnerCanvas;
 
 
 
@@ -62,9 +68,14 @@ public class HyperEdgeEditEventHandler extends PaletteNetworkEditEventHandler {
     public HyperEdgeEditEventHandler(CytoscapeEditor caller) {
         super(caller);
     }
-
+    /**
+     * @deprecated use  single argument constructor
+     */
     public HyperEdgeEditEventHandler(CytoscapeEditor caller, CyNetworkView view) {
-        super(caller, view);
+	// MLC 02/03/09:
+        // super(caller, view);
+	// MLC 02/03/09:
+        super(caller);
     }
 
     /**
@@ -76,7 +87,7 @@ public class HyperEdgeEditEventHandler extends PaletteNetworkEditEventHandler {
      * dropped from other applications onto the palette.
      *
      */
-    public void itemDropped(PhoebeCanvasDropEvent e) {
+    @Override public void itemDropped(PhoebeCanvasDropEvent e) {
 	// MLC 07/09/08 BEGIN:
 	// TODO: This check should really be avoided by having the editor remove the PhoebeCanvasDropListener
 	//       when the editor looses focus (another tab is clicked on).
@@ -85,6 +96,17 @@ public class HyperEdgeEditEventHandler extends PaletteNetworkEditEventHandler {
 	    return;
 	}
         Point                location = e.getLocation();
+	// MLC 02/03/09 BEGIN:
+	InnerCanvas dropCanvas = (InnerCanvas) e.getSource();
+	InnerCanvas currentCanvas = getCurrentDGraphView().getCanvas();
+	
+	if (dropCanvas != currentCanvas)
+	    {
+		// We are attempting to drop on a NetworkView that
+		// isn't the current NetworkView:
+		return;
+	    }
+	// MLC 02/03/09 END.
         BasicCytoShapeEntity myShape = getShapeEntityForLocation(location,
                                                                  e.getTransferable());
 
@@ -104,7 +126,8 @@ public class HyperEdgeEditEventHandler extends PaletteNetworkEditEventHandler {
             // HEUtils.log("getView() = " + getView());
             // HEUtils.log("getCurrentView() = " +
             //            Cytoscape.getCurrentNetworkView());
-            ((HyperEdgeEditor) get_caller()).determineAction(getView(),
+            // MLC 02/03/09:
+            ((HyperEdgeEditor) get_caller()).determineAction(getCurrentDGraphView(),
                                                              attributeValue,
                                                              location);
         } else {
@@ -144,8 +167,7 @@ public class HyperEdgeEditEventHandler extends PaletteNetworkEditEventHandler {
     //    }
 
     // override mousePressed() in BasicNetworkEditEventHandler:
-    public void mousePressed(MouseEvent e) {
-	// MLC: 07/09/08 BEGIN:
+    @Override public void mousePressed(MouseEvent e) {
 	// TODO: This check should really be avoided by having the editor remove all mouse and key
 	//       listeners when the editor looses focus (another tab is clicked on).
 	//       This is somewhat involved and so is left for when the editor is refactored.
@@ -153,10 +175,13 @@ public class HyperEdgeEditEventHandler extends PaletteNetworkEditEventHandler {
 	    return;
 	}
 	// CytoscapeEditorManager.log("HEE: mousePressed!");
-	// MLC: 07/09/08 END.
-        nextPoint = e.getPoint();
+	// MLC 02/03/09 BEGIN:
+        // nextPoint = e.getPoint();
+        Point2D pressedPoint = e.getPoint();
 
-        EdgeView ev     = getView().getPickedEdgeView(nextPoint);
+        // EdgeView ev     = getView().getPickedEdgeView(nextPoint);
+        EdgeView ev     = getCurrentDGraphView().getPickedEdgeView(pressedPoint);	
+	// MLC 02/03/09 END.
         boolean  onEdge = (ev != null);
 
         if (!onEdge) {
@@ -165,7 +190,10 @@ public class HyperEdgeEditEventHandler extends PaletteNetworkEditEventHandler {
         } else if (onEdge && isEdgeStarted() && !e.isControlDown()) {
             // special use for HyperEdgeEditor.
             // Finish Edge Creation
-            finishEdge(nextPoint, ev);
+	    // MLC 02/03/09:
+            // finishEdge(nextPoint, ev);
+	    // MLC 02/03/09:
+            finishEdge(pressedPoint, ev);
         } else {
             super.mousePressed(e);
         }
