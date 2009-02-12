f46c90601ff4346eea28e3e7594e0aa30ec4280e
mmichaud <mmichaud@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-12 19:33:54 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@15877 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/pasteur/mathieu/Tunables/src/Utils/CheckListManager.java
+++ csplugins/trunk/pasteur/mathieu/Tunables/src/Utils/CheckListManager.java
@@ -6,6 +6,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.DefaultListSelectionModel;
 import javax.swing.JCheckBox;
@@ -17,14 +19,16 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 
-public class CheckListManager<T> extends MouseAdapter implements ListSelectionListener, ActionListener{
+public class CheckListManager<T> extends MouseAdapter implements ActionListener, ListSelectionListener{
 	
     private ListSelectionModel selectionModel = new DefaultListSelectionModel(); 
     private JList list = new JList();
-    ArrayList<T> arrayOut=null;
+    ArrayList<T> arrayOut = new ArrayList<T>();
     ArrayList<Integer> arrayTest=null;
     ListMultipleSelection<T> LMS;
-    int hotspot = new JCheckBox().getPreferredSize().width; 
+    int hotspot = new JCheckBox().getPreferredSize().width;
+    Map<Integer,T> map = new HashMap<Integer,T>();
+    int test;
  
     public CheckListManager(JList list,ListMultipleSelection<T> LMS){
     	this.LMS=LMS;
@@ -41,9 +45,8 @@ public class CheckListManager<T> extends MouseAdapter implements ListSelectionLi
     } 
  
     private void toggleSelection(int index){ 
-        if(index<0) 
+        if(index<0)
             return; 
- 
         if(selectionModel.isSelectedIndex(index)){
             selectionModel.removeSelectionInterval(index, index);}
         else {
@@ -51,39 +54,61 @@ public class CheckListManager<T> extends MouseAdapter implements ListSelectionLi
     }
  
     
-    public ArrayList<T> getArray(){
-        arrayOut = new ArrayList<T>();
-        arrayTest = new ArrayList<Integer>();
-    	for(int i=0;i<LMS.getPossibleValues().size();i++){
-    		if(selectionModel.isSelectedIndex(i))arrayTest.add(i);
+//    public T getValue(){
+//    	return (T) list.getSelectedValue();
+//    }    
+    
+    
+//    public ArrayList<T> getArray(){
+//        arrayOut.removeAll(arrayTest);
+//        arrayTest = new ArrayList<Integer>();
+//    	for(int i=0;i<LMS.getPossibleValues().size();i++){
+//    		if(selectionModel.isSelectedIndex(i))arrayTest.add(i);
+//    	}
+//		if(arrayTest.size()!=0){
+//			for(int i=0;i<arrayTest.size();i++){
+//				list.setSelectedIndex((Integer)arrayTest.get(i));
+//				arrayOut.add(i, (T) list.getSelectedValue());
+//			}
+//    	return arrayOut;
+//		}
+//		else {
+//			return null;
+//		}
+//    }
+   
+    
+    
+   
+    public Map<Integer,T> getMap(){
+    	if(!map.containsKey(list.getSelectedIndex())){
+    		System.out.println("on ajoute = "+list.getSelectedIndex());
+       		map.put(list.getSelectedIndex(),(T)list.getSelectedValue());
     	}
-		if(arrayTest.size()!=0){
-			for(int i=0;i<arrayTest.size();i++){
-				list.setSelectedIndex((Integer)arrayTest.get(i));
-				arrayOut.add(i, (T) list.getSelectedValue());
-			}
-    	return arrayOut;
-		}
-		else {
-			arrayOut=null;
-			return arrayOut;
-		}
+    	else if(map.containsKey(list.getSelectedIndex())&& test!=list.getSelectedIndex()){
+    		map.remove(list.getSelectedIndex());
+    		System.out.println("on retire = "+list.getSelectedIndex());
+    	}
+    	toggleSelection(list.getSelectedIndex());
+    	test=list.getSelectedIndex();
+    	return map;
     }
     
     
     
-    public void mouseClicked(MouseEvent me){ 
+    public void mouseClicked(MouseEvent me){
         int index = list.locationToIndex(me.getPoint()); 
         if(index<0) 
             return; 
         if(me.getX()>list.getCellBounds(index, index).x+hotspot) 
             return; 
-        toggleSelection(index); 
+        toggleSelection(index);
     } 
 
  
     public void valueChanged(ListSelectionEvent e){ 
-        list.repaint(list.getCellBounds(e.getFirstIndex(), e.getLastIndex())); 
+        list.repaint(list.getCellBounds(e.getFirstIndex(), e.getLastIndex()));
+        
     } 
  
 
