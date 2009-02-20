6323f8cc01aaac175e2bf0907c91828f7ad0a582
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-20 23:42:35 +0000

    BookmarkDialogFactory added.  Also, package names for bookmarks are org.cytoscape.property.
    git-svn-id: http://chianti.ucsd.edu/svn@15979 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape3/trunk/application/src/main/java/cytoscape/dialogs/internal/BookmarkDialogFactoryImpl.java
@@ -0,0 +1,27 @@
+package cytoscape.dialogs.internal;
+
+import java.awt.Frame;
+
+import org.cytoscape.property.CyProperty;
+import org.cytoscape.property.bookmark.Bookmarks;
+import org.cytoscape.property.bookmark.BookmarksUtil;
+
+import cytoscape.dialogs.BookmarkDialog;
+import cytoscape.dialogs.BookmarkDialogFactory;
+
+public class BookmarkDialogFactoryImpl implements BookmarkDialogFactory {
+
+	private CyProperty<Bookmarks> bookmarksProp;
+	private BookmarksUtil bkUtil;
+
+	public BookmarkDialogFactoryImpl(CyProperty<Bookmarks> bookmarksProp,
+			BookmarksUtil bkUtil) {
+		this.bookmarksProp = bookmarksProp;
+		this.bkUtil = bkUtil;
+	}
+
+	public BookmarkDialog getBookamrkDialog(Frame parent) {
+		return new BookmarkDialogImpl(parent, bookmarksProp.getProperties(),
+				bkUtil);
+	}
+}
