5b251d81cff1b9b3c4a36f7093bce14866a208b9
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-23 18:14:39 +0000

    Added TaskMonitor
    git-svn-id: http://chianti.ucsd.edu/svn@16010 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/work-api/trunk/src/main/java/org/cytoscape/work/TaskMonitor.java
@@ -0,0 +1,30 @@
+package org.cytoscape.work;
+
+/**
+ * Provided by <code>TaskManager</code> to a <code>Task</code> so that the
+ * <code>Task</code> can modify its user interface during its execution.
+ */
+public interface TaskMonitor
+{
+	/**
+	 * Tells the <code>TaskManager</code> to set the title of the
+	 * <code>Task</code>.
+	 * The title is a succinct description of the <code>Task</code>'s
+	 * purpose.
+	 */
+	public void setTitle(String title);
+
+	/**
+	 * Tells the <code>TaskManager</code> how much progress has been
+	 * completed by the <code>Task</code>.
+	 *
+	 * @param progress Must be between 0.0 and 1.0.
+	 */
+	public void setProgress(double progress);
+
+	/**
+	 * Gives the <code>TaskManager</code> a description of what the
+	 * <code>Task</code> is currently doing.
+	 */
+	public void setStatusMessage(String statusMessage);
+}
