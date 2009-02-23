fc70e035307ec003739b50eab52a4dec26709383
mmichaud <mmichaud@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-23 15:30:07 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@16001 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-swing-impl/trunk/src/main/java/org/cytoscape/work/internal/tunables/utils/ChooseFilePanel.java
+++ core3/work-swing-impl/trunk/src/main/java/org/cytoscape/work/internal/tunables/utils/ChooseFilePanel.java
@@ -9,6 +9,8 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.cytoscape.io.CyFileFilter;
+
 
 
 public class ChooseFilePanel extends JPanel implements ActionListener{
@@ -21,14 +23,14 @@ public class ChooseFilePanel extends JPanel implements ActionListener{
 	File[] myNetworkFile;
 	myFile file;
 	java.util.List<String> paths;
-//	private CyFileFilter[] tempCFF;
+	private CyFileFilter[] tempCFF;
 
 	
 	public ChooseFilePanel(myFile infile){
 	
 		this.file = infile;
 		setLayout(new BorderLayout());
-//		tempCFF = file.getCyFileFilter();
+		tempCFF = file.getCyFileFilter();
 		jtf = new JTextField("Please select a network file...",10);	
 		add(jtf,BorderLayout.WEST);
 		JButton selectbutton = new JButton("select");
