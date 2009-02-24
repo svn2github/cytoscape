2502eb39e97d8a5819f6ea53909f37a64e3a644b
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-24 23:56:37 +0000

    committing this script so that we have a backup plan if APlus flakes out
    git-svn-id: http://chianti.ucsd.edu/svn@16030 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cyto_web/trunk/deploy_enoteca.sh
@@ -0,0 +1,8 @@
+# Syncs latest file changes to cytoscape.org
+# - excludes all CVS directories
+
+#  Configure for production
+ant config_prod
+
+#  Issue rsync command
+rsync --verbose --progress --stats --compress --rsh=/usr/bin/ssh --recursive --size-only --perms --links --exclude ".svn/" --exclude "deploy.sh" --exclude "build.xml" . root@enoteca-5.ucsd.edu:/var/www/html/cytoscape/  
