ce35303a490322dcad46fe339810b76fd062d14c
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-25 23:42:28 +0000

    initial import of a core task implementation bundle
    git-svn-id: http://chianti.ucsd.edu/svn@16041 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/loadnetwork/internal/LoadNetworkFileTaskFactoryImpl.java
@@ -0,0 +1,79 @@
+
+/*
+ File: LoadNetworkTaskFactory.java
+
+ Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+package org.cytoscape.task.loadnetwork.internal;
+
+import cytoscape.CyNetworkManager;
+
+import org.cytoscape.layout.CyLayouts;
+import org.cytoscape.io.read.CyReaderManager;
+import org.cytoscape.view.GraphViewFactory;
+
+import org.cytoscape.work.TaskFactory;
+import org.cytoscape.work.Task;
+
+import org.cytoscape.property.CyProperty;
+
+import java.util.Properties;
+
+
+/**
+ * Task to load a new network.
+ */
+public class LoadNetworkFileTaskFactoryImpl implements TaskFactory {
+
+	private CyReaderManager mgr; 
+	private GraphViewFactory gvf; 
+	private CyLayouts cyl; 
+	private CyNetworkManager netmgr; 
+	private Properties props;
+
+	public LoadNetworkFileTaskFactoryImpl(CyReaderManager mgr, 
+	                                      GraphViewFactory gvf, CyLayouts cyl, 
+										  CyNetworkManager netmgr, CyProperty<Properties> cyProp) {
+		this.mgr = mgr;
+		this.gvf = gvf;
+		this.cyl = cyl;
+		this.netmgr = netmgr;
+		this.props = cyProp.getProperties();
+	}
+
+	public Task getTask() {
+		return new LoadNetworkFileTask(mgr,gvf,cyl,netmgr,props);
+	}
+}
---
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/loadnetwork/internal/LoadNetworkURLTask.java
@@ -0,0 +1,108 @@
+/*
+ File: LoadNetworkURLTask.java
+
+ Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+// $Revision: 8703 $
+// $Date: 2006-11-06 23:17:02 -0800 (Mon, 06 Nov 2006) $
+// $Author: pwang $
+package org.cytoscape.task.loadnetwork.internal;
+
+import cytoscape.CyNetworkManager;
+import cytoscape.util.CyNetworkNaming;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.layout.CyLayouts;
+import org.cytoscape.view.GraphView;
+import org.cytoscape.io.read.CyReaderManager;
+import org.cytoscape.io.read.CyNetworkReader;
+import org.cytoscape.view.GraphViewFactory;
+
+import org.cytoscape.work.TaskMonitor;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+
+/**
+ * Specific instance of AbstractLoadNetworkTask that loads a URL. 
+ */
+public class LoadNetworkURLTask extends AbstractLoadNetworkTask {
+
+//	@Tunable(description="The URL to load")
+	public URL url;
+
+	public LoadNetworkURLTask(CyReaderManager mgr, GraphViewFactory gvf, CyLayouts cyl, CyNetworkManager netmgr, Properties props) {
+		super(mgr,gvf,cyl,netmgr,props);
+	}
+
+	/**
+	 * Executes Task.
+	 */
+	public void run(TaskMonitor taskMonitor) {
+		this.taskMonitor = taskMonitor;
+
+		if (url == null)
+			throw new NullPointerException("network url is null");
+
+		name = url.toString();
+
+		myThread = Thread.currentThread();
+
+		try {
+			taskMonitor.setStatusMessage("Opening URL " + url);
+			reader = mgr.getReader(url);
+
+			if (interrupted)
+				return;
+
+			uri = url.toURI();
+		} catch (Exception e) {
+			uri = null;
+			taskMonitor.setStatusMessage("Unable to connect to URL " + name + ": " + e.getMessage());
+
+			return;
+		}
+
+		if (reader == null) {
+			uri = null;
+			taskMonitor.setStatusMessage( "Unable to connect to URL " + name);
+
+			return;
+		}
+
+		loadNetwork(reader);
+	}
+}
