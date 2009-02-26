4d823add641228a325746a5b29553e4b320c5b36
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-26 19:33:26 +0000

    original creation
    git-svn-id: http://chianti.ucsd.edu/svn@16053 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/pwang/cyprovision/org.eclipse.equinox.p2.ui2/src/org/eclipse/equinox/internal/provisional/p2/ui2/SiteFileFilter.java
@@ -0,0 +1,43 @@
+package org.eclipse.equinox.internal.provisional.p2.ui2;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class SiteFileFilter extends FileFilter {
+  	 public boolean accept(File f) {
+    	    if (f.isDirectory()) {
+    	    	return true;
+    	        }
+
+    	        String extension = getExtension(f);
+    	        if (extension != null) {
+    	    	if (extension.equals("xml")) {
+    	    	        return true;
+    	    	} else {
+    	    	    return false;
+    	    	}
+    	        }
+
+    	        return false;
+    	    }
+
+     	 public String getDescription(){
+     		 return "XML file";
+     	 }
+    	 
+     	 /*
+    	     * Get the extension of a file.
+    	     */  
+    	    public String getExtension(File f) {
+    	        String ext = null;
+    	        String s = f.getName();
+    	        int i = s.lastIndexOf('.');
+
+    	        if (i > 0 &&  i < s.length() - 1) {
+    	            ext = s.substring(i+1).toLowerCase();
+    	        }
+    	        return ext;
+    	    }
+
+}
