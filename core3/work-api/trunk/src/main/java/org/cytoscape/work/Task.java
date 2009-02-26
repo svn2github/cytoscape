f4ed3d732acb9e73cd3166f418b3fd6643ab1fc1
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-26 19:40:02 +0000

    Added some documentation for Task.
    git-svn-id: http://chianti.ucsd.edu/svn@16054 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-api/trunk/src/main/java/org/cytoscape/work/Task.java
+++ core3/work-api/trunk/src/main/java/org/cytoscape/work/Task.java
@@ -15,6 +15,81 @@ package org.cytoscape.work;
  * for the <code>Task</code> that allows the <code>Task</code> to modify
  * the user interface, and run the <code>Task</code> in its own thread.
  *
+ * <p>Some hints for writing a <code>Task</code>:</p>
+ * <p><ul>
+ *
+ * <li>When an exception is thrown, the <code>Task</code>
+ * should not catch it and set a status message or the progress,
+ * even to provide explanatory messages for the user.
+ * A <code>TaskManager</code> can disregard status message
+ * and progress updates once an exception is thrown.</li>
+ *
+ * <li>Any helpful user messages regarding the exception should
+ * be contained solely in the exception. If a <code>Task</code> throws
+ * a low level exception, it should catch it and throw
+ * an exception with a high level description. For example:
+ * <p><code>
+ * try<br>
+ * {<br>
+ *   ...<br>
+ * }<br>
+ * catch (IOException exception) // Low level exception<br>
+ * {<br>
+ *   // Throw a high level exception that gives a high level explanation<br>
+ *   // that makes sense for a non-technical user.<br>
+ *   throw new Exception("Oops! Looks like you specified an invalid file.", exception)<br>
+ * }<br>
+ * </code></p>
+ * Any helpful messages for the user should be contained in
+ * an exception.</li>
+ *
+ * <li>When a <code>Task</code> encounters an error
+ * that is not in the form of an exception, like an invalid
+ * variable or incorrectly formatted parameter,
+ * the <code>Task</code> should not
+ * set the status message giving an explanation of the
+ * error and exit. Instead, it should throw an exception.
+ * <p>The wrong way:</p>
+ * <p><code>
+ * public void run(TaskMonitor taskMonitor)<br>
+ * {<br>
+ *   if (myParameter == null)<br>
+ *   {<br>
+ *     taskMonitor.setStatusMessage("Whoa, looks like you didn't specified the parameter!");<br>
+ *     return;<br>
+ *   }<br>
+ * }<br>
+ * </code></p>
+ * <p>The right way:</p>
+ * <p><code>
+ * public void run(TaskMonitor taskMonitor) throws Exception<br>
+ * {<br>
+ *   if (myParameter == null)<br>
+ *     throw new Exception("Whoa, looks like you didn't specified the parameter!");<br>
+ * }<br>
+ * </code></p>
+ * This is done because it is possible for the <code>TaskManager</code> to close
+ * the <code>Task</code>'s user interface when the <code>Task</code> returns
+ * before the user can read the message. Throwing an exception ensures that
+ * the user will see the message.</li>
+ *
+ * <li>The <code>Task</code> should not set the status message or progress
+ * immediately before the <code>Task</code> finishes. This is because the
+ * <code>TaskManager</code> may close the <code>Task</code>'s user interface
+ * before the user has a chance to read it. For example:
+ * <p><code>
+ * public void run(TaskMonitor taskMonitor) throws Exception<br>
+ * {<br>
+ *   ... // Some complicated calculation<br>
+ *   <br>
+ *   // This is unnecessary:<br>
+ *   taskMonitor.setStatusMessage("We're all done!");<br>
+ *   taskMonitor.setProgress(1.0);<br>
+ * }<br>
+ * </code></p>
+ * </li>
+ * </ul></p>
+ *
  * @author Samad Lotia
  */
 public interface Task
@@ -32,6 +107,10 @@ public interface Task
 	 * @throws Exception The <code>Task</code> is at liberty to
 	 * throw any exceptions in <code>run</code>. The exception is
 	 * caught by <code>TaskManager</code> and is displayed in the interface.
+	 * If a <code>Task</code> does not throw an exception,
+	 * the <code>Task</code> implementation does <i>not</i>
+	 * need to specify the <code>throws Exception</code> clause 
+	 * for the <code>run</code> method.
 	 */
 	public void run(TaskMonitor taskMonitor) throws Exception;
 
