63559552ea2e628d31f322b748b636fe46781a37
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-26 00:17:30 +0000

    Add event handler for add/remove repository sites and commit the change
    git-svn-id: http://chianti.ucsd.edu/svn@16044 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/cyprovision/org.eclipse.equinox.p2.ui2/src/org/eclipse/equinox/internal/provisional/p2/ui2/dialogs/RepositoryManipulationDialog.java
+++ csplugins/trunk/ucsd/pwang/cyprovision/org.eclipse.equinox.p2.ui2/src/org/eclipse/equinox/internal/provisional/p2/ui2/dialogs/RepositoryManipulationDialog.java
@@ -1,19 +1,69 @@
 package org.eclipse.equinox.internal.provisional.p2.ui2.dialogs;
 
 import javax.swing.JDialog;
-import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+
+import java.net.URI;
+import java.util.Hashtable;
+import java.util.Set;
+
 import javax.swing.JButton;
 
-public class RepositoryManipulationDialog extends JDialog implements ActionListener {
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.equinox.internal.p2.ui2.DefaultMetadataURLValidator;
+import org.eclipse.equinox.internal.p2.ui2.ProvUIMessages;
+import org.eclipse.equinox.internal.p2.ui2.model.ElementUtils;
+import org.eclipse.equinox.internal.p2.ui2.model.MetadataRepositoryElement;
+import org.eclipse.equinox.internal.provisional.p2.core.ProvisionException;
+import org.eclipse.equinox.internal.provisional.p2.ui2.UpdateManagerCompatibility;
+import org.eclipse.equinox.internal.provisional.p2.ui2.dialogs.CachedMetadataRepositories;
+import org.eclipse.equinox.internal.provisional.p2.ui2.model.MetadataRepositories;
+import org.eclipse.equinox.internal.provisional.p2.ui2.operations.ProvisioningOperation;
+import org.eclipse.equinox.internal.provisional.p2.ui2.operations.RepositoryOperation;
+import org.eclipse.equinox.internal.provisional.p2.ui2.policy.Policy;
+import org.eclipse.equinox.internal.provisional.p2.ui2.policy.QueryProvider;
+import org.eclipse.equinox.internal.provisional.p2.ui2.policy.RepositoryLocationValidator;
+import org.eclipse.equinox.internal.provisional.p2.ui2.policy.RepositoryManipulator;
+import org.eclipse.osgi.util.NLS;
+//import org.eclipse.swt.custom.BusyIndicator;
+//import org.eclipse.swt.widgets.Shell;
+//import org.eclipse.ui.IWorkbench;
+//import org.eclipse.ui.internal.Model;
 
-	
+import javax.swing.table.*;
+import java.util.Vector;
+
+
+public class RepositoryManipulationDialog extends JDialog implements ActionListener, ListSelectionListener {
+
+	private Policy policy;
+	JDialog parent;
+	RepositoryManipulator manipulator;
+	RepositoryManipulator localCacheRepoManipulator;
+	CachedMetadataRepositories input;
+	boolean changed = false;
+
     /** Creates new form RepositoryManipulationPage */
-    public RepositoryManipulationDialog(JComponent parent, boolean modal) {
-        //super(parent, modal);
+    public RepositoryManipulationDialog(JDialog parent, boolean modal, Policy policy) {
+        super(parent, modal);
+        this.policy = policy;
+        this.parent = parent;
+        this.setTitle("Repository Manipulation");
+        
         initComponents();
 
+		btnRemove.setEnabled(false);
+		btnTestConnection.setEnabled(false);
+		btnEnable.setEnabled(false);
+		//btnOK.setEnabled(false);
+		
         btnOK.addActionListener(this);
         btnCancel.addActionListener(this);
         
@@ -24,24 +74,72 @@ public class RepositoryManipulationDialog extends JDialog implements ActionListe
         btnExport.addActionListener(this);
         btnImport.addActionListener(this);
 
+        init();
+        getInput();
+        input.getChildren(null);
+        
+        MetadataRepositoryElement[] elements = getElements();
+        Vector tableData = new Vector();
+        
+        for (int i=0; i< elements.length; i++){
+        	tableData.add(elements[i]);
+        }
+        
+        SitesTableModel model = new SitesTableModel(tableData);
+        tblSites.setModel(model);
+        
+        tblSites.getSelectionModel().addListSelectionListener(this);
+        
     }
     
+
+    public void valueChanged(ListSelectionEvent e){
+	
+    	if (tblSites.getSelectedRow() == -1) {
+    		btnRemove.setEnabled(false);
+    		btnTestConnection.setEnabled(false);
+    		btnEnable.setEnabled(false);
+    	}
+    	else {
+    		btnRemove.setEnabled(true);
+    		btnTestConnection.setEnabled(true);
+    		btnEnable.setEnabled(true); 		
+    	}
+		//if (changed) {
+    	//	btnOK.setEnabled(true);    			
+		//}
+		//else {
+		//	btnOK.setEnabled(false);
+		//}
+    }
+    
+	public void init() {
+		//noDefaultAndApplyButton();
+		if (policy == null)
+			policy = Policy.getDefault();
+		manipulator = policy.getRepositoryManipulator();
+	}
+
+
     public void actionPerformed(ActionEvent e) {
     	if (e.getSource() instanceof JButton){
     		JButton btn = (JButton) e.getSource();
     		
     		if (btn == btnOK){
     			System.out.println("btnOK is pressed!");
+    			if (changed)
+    				ElementUtils.updateRepositoryUsingElements(getElements(),null);
+    			this.dispose();
     		}
     		else if (btn == btnCancel){
     			System.out.println("btnCancel is pressed!");
     			this.dispose();
     		}
     		else if (btn == btnAdd){
-    			System.out.println("btnAdd is pressed!");
+    			addRepository();
     		}
     		else if (btn == btnRemove){
-    			System.out.println("btnRemove is pressed!");
+    			removeRepositories();
     		}
     		else if (btn == btnTestConnection){
     			System.out.println("btntestConnection is pressed!");
@@ -51,14 +149,217 @@ public class RepositoryManipulationDialog extends JDialog implements ActionListe
     		}
     		else if (btn == btnImport){
     			System.out.println("btnImport is pressed!");
+       			
+    			System.out.println("Number of repository sites = "+ input.cachedElements.size());
+
     		}
     		else if (btn == btnExport){
     			System.out.println("btnExport is pressed!");
     		}
-    		
     	}
     }
+
+/*
+	void importRepositories() {
+		BusyIndicator.showWhile(getShell().getDisplay(), new Runnable() {
+			public void run() {
+				MetadataRepositoryElement[] imported = UpdateManagerCompatibility.importSites(getShell());
+				if (imported.length > 0) {
+					Hashtable repos = getInput().cachedElements;
+					changed = true;
+					for (int i = 0; i < imported.length; i++)
+						repos.put(imported[i].getLocation().toString(), imported[i]);
+					asyncRefresh();
+				}
+			}
+		});
+	}
+	
+	void asyncRefresh() {
+		display.asyncExec(new Runnable() {
+			public void run() {
+				repositoryViewer.refresh();
+			}
+		});
+	}
+*/
+	
+    /*
+	void exportRepositories() {
+		BusyIndicator.showWhile(getShell().getDisplay(), new Runnable() {
+			public void run() {
+				MetadataRepositoryElement[] elements = getSelectedElements();
+				if (elements.length == 0)
+					elements = getElements();
+				UpdateManagerCompatibility.exportSites(getShell(), elements);
+			}
+		});
+	}
+*/
+
+	void removeRepositories() {
+		
+		int selectRowIndex = tblSites.getSelectedRow();
+		if (selectRowIndex ==-1) return;
+		
+		SitesTableModel tblModel = (SitesTableModel)tblSites.getModel();
+		MetadataRepositoryElement element = tblModel.getRowAt(selectRowIndex);
+		
+		int userInput = JOptionPane.showConfirmDialog(this, "Are you sure you want to remove "+ element.getLocation(), "Remove Sites", JOptionPane.YES_NO_OPTION);
+		
+		if (userInput == JOptionPane.NO_OPTION){
+			return;
+		}
+		tblModel.deleteRowAt(selectRowIndex);
+
+		// sync the data in tableModel and in cachedMetaRepository
+		getInput().cachedElements.remove(element.getLocation().toString());
+
+		changed = true;		
+	}
+
     
+	void addRepository() {
+		//System.out.println("btnAddSite is clicked");
+		AddSiteDialog addSiteDlg = new AddSiteDialog(this, true, policy, input);
+		addSiteDlg.setLocationRelativeTo(this);
+		addSiteDlg.setSize(400, 150);
+		addSiteDlg.setVisible(true);	
+		while (addSiteDlg.isVisible()){
+			try {
+				Thread.sleep(2000);				
+			}
+			catch (InterruptedException ie){
+				// do nothing
+			}
+		}
+		SitesTableModel _model =  (SitesTableModel) tblSites.getModel();
+		
+		// sync the data in tableModel and in cachedMetaRepository
+		if (input.cachedElements.size() > _model.getRowCount()) {
+			// Determine which site is teh one just added
+			Hashtable clonedTable = (Hashtable) input.cachedElements.clone();
+			for (int i=0; i< _model.getRowCount(); i++){
+				if (clonedTable.containsKey(_model.getRowAt(i).getLocation().toString())){
+					clonedTable.remove(_model.getRowAt(i).getLocation().toString());
+				}
+			}
+			Object[] keyArray = clonedTable.keySet().toArray();
+			MetadataRepositoryElement newElement = (MetadataRepositoryElement) clonedTable.get(keyArray[0]);
+			_model.addRow(newElement);			
+			changed = true;			
+		}		
+	}
+
+ 
+	CachedMetadataRepositories getInput() {
+		if (input == null)
+			input = new CachedMetadataRepositories(policy);
+		return input;
+	}
+
+	MetadataRepositoryElement[] getElements() {		
+		return (MetadataRepositoryElement[]) getInput().cachedElements.values().toArray(new MetadataRepositoryElement[getInput().cachedElements.size()]);
+	}
+
+	
+	URI[] getKnownRepositories() {
+		MetadataRepositoryElement[] elements = getElements();
+		URI[] locations = new URI[elements.length];
+		for (int i = 0; i < elements.length; i++)
+			locations[i] = elements[i].getLocation();
+		return locations;
+	}
+
+	RepositoryLocationValidator getRepositoryLocationValidator() {
+		DefaultMetadataURLValidator validator = new DefaultMetadataURLValidator() {
+			protected URI[] getKnownLocations() {
+				return getKnownRepositories();
+			}
+		};
+		return validator;
+
+	}
+
+	RepositoryOperation getRepoAddOperation(URI location) {
+		return new RepositoryOperation("Cached add repo operation", new URI[] {location}) { //$NON-NLS-1$
+			protected IStatus doExecute(IProgressMonitor monitor) {
+				for (int i = 0; i < locations.length; i++) {
+					Hashtable elements = getInput().cachedElements;
+					elements.put(locations[i].toString(), new MetadataRepositoryElement(getInput(), locations[i], true));
+
+				}
+				//changed = true;
+				//asyncRefresh();
+				return Status.OK_STATUS;
+			}
+
+			protected IStatus doBatchedExecute(IProgressMonitor monitor) throws ProvisionException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+/////////////////////////////
+	
+    class SitesTableModel extends AbstractTableModel {
+    	
+    	String[] columnNames = {"Name","Location","Enabled"};
+    	private Vector repoElementVect;
+    	
+    	public SitesTableModel(Vector MetadataRepositoryElementVect){
+    		this.repoElementVect = MetadataRepositoryElementVect;
+    	}
+    	
+    	public int getRowCount(){
+    		if (repoElementVect == null){
+    			return 0;
+    		}
+    		return repoElementVect.size();
+    	}
+    	
+    	public int getColumnCount(){
+    		return columnNames.length;
+    	}
+    	
+    	public String getColumnName(int column){
+    		return columnNames[column];
+    	}
+    	
+    	  public Object getValueAt(int row, int column){
+    		  MetadataRepositoryElement rowX = (MetadataRepositoryElement) repoElementVect.elementAt(row);
+      		if (column == 0){
+    			if (rowX.getName().endsWith("- metadata")){
+    				return "";
+    			}
+    			return rowX.getName();
+    		}      		
+    		if (column == 1){
+    			return rowX.getLocation();
+    		}
+    		if (column == 2){
+    			if (rowX.isEnabled())
+    				return "Enabled";
+    			else 
+    				return "Disabled";
+    		}
+    		return "unknown";
+    	  }
+    	  
+    	  public MetadataRepositoryElement getRowAt(int row){
+    		  return (MetadataRepositoryElement) repoElementVect.elementAt(row);
+    	  }
+    	  
+    	  public void deleteRowAt(int row){
+    		  repoElementVect.removeElementAt(row);
+    		  this.fireTableDataChanged();
+    	  }
+
+       	  public void addRow(MetadataRepositoryElement newElement){
+    		  repoElementVect.add(newElement);
+    		  this.fireTableDataChanged();
+    	  }    
+    }
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -185,17 +486,6 @@ public class RepositoryManipulationDialog extends JDialog implements ActionListe
         pack();
     }// </editor-fold>                        
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new RepositoryManipulationDialog(null, true).setVisible(true);
-            }
-        });
-    }
-    
     // Variables declaration - do not modify                     
     private javax.swing.JButton btnAdd;
     private javax.swing.JButton btnCancel;
@@ -211,6 +501,5 @@ public class RepositoryManipulationDialog extends JDialog implements ActionListe
     private javax.swing.JPanel pnlButton_right;
     private javax.swing.JTable tblSites;
     // End of variables declaration                   
-    
-	
+
 }
