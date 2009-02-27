d7365bb78e107f150f3287505ad843fe646d67cc
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-02-27 01:41:46 +0000

    Formatted the code, but also, and importantly, allowed loadTunables to be overridden to support a problem with Tunables and Spring.
    git-svn-id: http://chianti.ucsd.edu/svn@16066 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-api/trunk/src/main/java/org/cytoscape/work/AbstractTunableInterceptor.java
+++ core3/work-api/trunk/src/main/java/org/cytoscape/work/AbstractTunableInterceptor.java
@@ -1,6 +1,43 @@
+
+/*
+ Copyright (c) 2008, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
 package org.cytoscape.work;
 
 import java.lang.reflect.Field;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -9,45 +46,87 @@ import java.util.List;
 import java.util.Map;
 
 
-public abstract class AbstractTunableInterceptor<T extends Handler> implements TunableInterceptor<T>{
-	
+/**
+ * DOCUMENT ME!
+  *
+ * @param <T>  DOCUMENT ME!
+ */
+public abstract class AbstractTunableInterceptor<T extends Handler> implements TunableInterceptor<T> {
+	/**
+	 * DOCUMENT ME!
+	 */
 	protected HandlerFactory<T> factory;
-	protected Map<Object,LinkedHashMap<String,T>> handlerMap;
-	
+
+	/**
+	 * DOCUMENT ME!
+	 */
+	protected Map<Object, LinkedHashMap<String, T>> handlerMap;
+
+	/**
+	 * Creates a new AbstractTunableInterceptor object.
+	 *
+	 * @param tunablehandlerfactory  DOCUMENT ME!
+	 */
 	public AbstractTunableInterceptor(HandlerFactory<T> tunablehandlerfactory) {
 		this.factory = tunablehandlerfactory;
-		handlerMap = new HashMap<Object,LinkedHashMap<String,T>>();
+		handlerMap = new HashMap<Object, LinkedHashMap<String, T>>();
 	}
 
-	public final void loadTunables(Object obj){
-		if ( !handlerMap.containsKey(obj) ) {					//Deleted to get new Panels if we do it many times
-			LinkedHashMap<String,T> handlerList = new LinkedHashMap<String,T>();
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param obj DOCUMENT ME!
+	 */
+	public void loadTunables(Object obj) {
+		//System.out.println("looking at obj: " + obj.getClass().toString());
+		if (!handlerMap.containsKey(obj)) { //Deleted to get new Panels if we do it many times
+
+			LinkedHashMap<String, T> handlerList = new LinkedHashMap<String, T>();
+
 			// Find each public field in the class.
 			for (Field field : obj.getClass().getFields()) {
+				//System.out.println("evaluating: " + field.toString());
 				// See if the field is annotated as a Tunable.
-   				if (field.isAnnotationPresent(Tunable.class)) {
+				if (field.isAnnotationPresent(Tunable.class)) {
 					try {
 						Tunable tunable = field.getAnnotation(Tunable.class);
-						
-						T handler = factory.getHandler(field,obj,tunable);
-						if ( handler != null )handlerList.put( field.getName(),handler );
-						else System.out.println("No handler for type: "+ field.getType().getName());
-					}catch (Throwable ex) {
-						System.out.println("tunable field intercept failed: " + field.toString() );
+
+						T handler = factory.getHandler(field, obj, tunable);
+
+						if (handler != null) {
+							//System.out.println("adding tunable handler for: " + field.toString());
+							handlerList.put(field.getName(), handler);
+						} else
+							System.out.println("No handler for type: " + field.getType().getName());
+					} catch (Throwable ex) {
+						System.out.println("tunable field intercept failed: " + field.toString());
 						ex.printStackTrace();
 					}
 				}
 			}
-			handlerMap.put(obj,handlerList);
-		}													//End of the deleted Loop
-	}	
-	
-	
-	public Map<String,T> getHandlers(Object o) {
-		if ( o == null )
+
+			handlerMap.put(obj, handlerList);
+		} //End of the deleted Loop
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param o DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public Map<String, T> getHandlers(Object o) {
+		if (o == null)
 			return null;
+
 		return handlerMap.get(o);
 	}
 
-	public abstract boolean createUI(Object ... objs);
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public abstract boolean createUI(Object... objs);
 }
