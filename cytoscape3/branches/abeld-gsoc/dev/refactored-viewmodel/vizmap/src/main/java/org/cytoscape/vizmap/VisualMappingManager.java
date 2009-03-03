4bf021544c401a896e58e0cf237b6478edac3aee
abeld <abeld@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-03-03 15:37:02 +0000

    refactored-viewmodel: add a very bare-bones VisualMappingManager api (with implementation) to store network->VisualStyle mapping
    git-svn-id: http://chianti.ucsd.edu/svn@16128 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape3/branches/abeld-gsoc/dev/refactored-viewmodel/vizmap/src/main/java/org/cytoscape/vizmap/VisualMappingManager.java
@@ -0,0 +1,14 @@
+package org.cytoscape.vizmap;
+
+import org.cytoscape.viewmodel.CyNetworkView;
+
+/**
+ * From pre-3.0 functionality, only 'network->VisualStyle' map remains in here.
+ * @author abeld
+ *
+ */
+public interface VisualMappingManager {
+	
+	public void setVisualStyle(VisualStyle vs, CyNetworkView nv);
+	public VisualStyle getVisualStyle(CyNetworkView nv);
+}
