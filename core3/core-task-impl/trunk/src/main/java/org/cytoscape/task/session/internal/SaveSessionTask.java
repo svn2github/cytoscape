eb2ce05666ec84315d67f5af4b265a53c7dc98b5
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-03-03 21:59:31 +0000

    got the session tasks compiling
    git-svn-id: http://chianti.ucsd.edu/svn@16134 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/session/internal/SaveSessionTask.java
@@ -0,0 +1,64 @@
+/*
+ File: SaveSessionTask.java
+
+ Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+package org.cytoscape.task.session.internal;
+
+
+import org.cytoscape.io.write.CyWriterManager;
+import org.cytoscape.io.write.CyWriter;
+
+import org.cytoscape.work.Task;
+import org.cytoscape.work.TaskMonitor;
+
+import org.cytoscape.session.CySessionManager;
+
+import java.io.File;
+
+
+public class SaveSessionTask extends AbstractSaveSessionTask {
+
+	/**
+	 * setAcceleratorCombo(KeyEvent.VK_S, ActionEvent.CTRL_MASK);
+	 */
+	public SaveSessionTask(CySessionManager mgr, CyWriterManager factory) {
+		super(mgr,factory);
+		file = new File(mgr.getCurrentSessionFileName());
+	}
+
+	public void run(TaskMonitor taskMonitor) throws Exception {
+		saveSession(taskMonitor);
+	}
+} 
