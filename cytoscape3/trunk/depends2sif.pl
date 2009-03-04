4849e46646cf2d372ec26181ef0ddbf5de5de5ed
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-03-04 01:55:39 +0000

    corrected the script
    git-svn-id: http://chianti.ucsd.edu/svn@16141 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape3/trunk/depends2sif.pl
+++ cytoscape3/trunk/depends2sif.pl
@@ -1,35 +1,25 @@
 #!/usr/bin/perl
 
-$everything = 0;
-if ( $ARGV[0] eq "-everything" ) {
-	$everything = 1;
-}
 
-$currentModule = "";
-$record = 0;
+%lastModule = ();
 
-open COM, "mvn dependency:list -DexcludeTransitive|" or die "couldn't execute mvn dependency:list\n";
+open COM, "mvn dependency:tree -Dtokens=whitespace|" or die "couldn't execute mvn dependency:tree\n";
 while (<COM>) {
 
-	if ( $_ =~ /Building\s+(\S+)\s+/ ) {
-		$currentModule = $1;
-		$record = 0;
-	} elsif ( $currentModule ne "" &&
-	          $_ =~ /The following files have been resolved/ ) {
-		$record = 1; 
-    } elsif ( $record == 1 &&
-	          $_ =~ /\s+(\S+)\:(\S+)\:\S+\:\S+\:(\S+)/ ) {
+	if ( $_ =~ /^\[INFO\] \[dependency\:tree\$]/ ) {
+		$lastModule = ();	
+    } elsif ( $_ =~ /^\[INFO\] (\S+)\:(\S+)\:\S+\:\S+$/ ) {
 		$org = $1;
 		$depModule = $2;
-		$phase = $3;
+		$lastModule{0} = $org . "." . $depModule;
+    } elsif ( $_ =~ /^\[INFO\] ( +)(\S+)\:(\S+)\:\S+\:\S+\:(\S+)$/ ) {
+		$spaces = (length $1)/3;
+		$org = $2;
+		$depModule = $3;
+		$phase = $4;
+		$lastModule{$spaces} = $org . "." . $depModule;
 		if ( $phase eq "compile" ) {
-			if ( $everything == 1 ) {
-				print "$currentModule	dependsOn	$depModule\n";
-			} else {
-				if ( $org =~ /cytoscape/ ) {
-					print "$currentModule	dependsOn	$depModule\n";
-				}
-			}
+			print "$lastModule{$spaces-1}	dependsOn	$lastModule{$spaces}\n";
 		}
 	}
 }
