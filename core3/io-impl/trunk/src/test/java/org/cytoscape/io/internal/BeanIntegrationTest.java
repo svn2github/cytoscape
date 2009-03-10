c201f1b3f5e4c3eb41b37774985fce2d6f43ab9c
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-03-10 17:58:43 +0000

    Simple integration test has been added.
    git-svn-id: http://chianti.ucsd.edu/svn@16195 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/io-impl/trunk/src/test/java/org/cytoscape/io/internal/BeanIntegrationTest.java
+++ core3/io-impl/trunk/src/test/java/org/cytoscape/io/internal/BeanIntegrationTest.java
@@ -1,9 +1,15 @@
 package org.cytoscape.io.internal;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.cytoscape.io.CyFileFilter;
+import org.cytoscape.io.DataCategory;
 import org.cytoscape.io.read.CyReader;
+import org.cytoscape.io.read.CyReaderFactory;
 import org.cytoscape.io.read.CyReaderManager;
 import org.cytoscape.io.read.internal.AbstractNetworkReader;
+import org.cytoscape.io.read.internal.sif.InteractionsReader;
 import org.cytoscape.model.CyNetworkFactory;
 import org.cytoscape.view.GraphViewFactory;
 import org.junit.Before;
@@ -13,6 +19,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
 
 
 /**
@@ -33,13 +40,19 @@ public class BeanIntegrationTest extends AbstractJUnit4SpringContextTests {
 
 	private CyFileFilter sifFilter;
 	
+	private CyReaderFactory sifFactory;
+	
+	private URI sampleFileLocation1;
+	
 
 	@Before
-	public void initializeTest1() {
+	public void initializeTest1() throws Exception {
+		sampleFileLocation1 = new URI("http://chianti.ucsd.edu/kono/data/galFiltered.sif");
+		
 		sifReader = (CyReader) applicationContext.getBean("sifReader");
 		xgmmlReader = (CyReader) applicationContext.getBean("xgmmlReader");
 		sifFilter = (CyFileFilter) applicationContext.getBean("sifFilter");
-		
+		sifFactory = (CyReaderFactory) applicationContext.getBean("sifReaderFactory");
 		
 		factoryMock = createMock(CyNetworkFactory.class);
 		viewFactoryMock = createMock(GraphViewFactory.class);
@@ -53,9 +66,13 @@ public class BeanIntegrationTest extends AbstractJUnit4SpringContextTests {
 	/**
 	 * Test using mock service objects.
 	 */
-	@Test public void fooManager() {
-		System.out.println("--------------------------------------- Test1 ");
+	@Test public void readerManagerTest() {
+		System.out.println("--------------------------------------- Reader Manager Test Begins");
+		manager.addReaderFactory(sifFactory, null);
+		CyReader reader = manager.getReader(sampleFileLocation1, DataCategory.NETWORK);
 		
-		System.out.println("--------------------------------------- Test12 " + this.applicationContext);
+		assertEquals(reader.getClass(), InteractionsReader.class);
+		
+		System.out.println("--------------------------------------- Reader Obj = " + reader.toString());
 	}
 }
