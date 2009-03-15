54c04d6e53ec09af81a325b3c70f2f6c60b93de0
abeld <abeld@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-03-15 15:22:06 +0000

    vizmap: add VisualMappingManager to hold the CyNetworkView -> VisualStyle mapping
    git-svn-id: http://chianti.ucsd.edu/svn@16273 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/vizmap-impl/trunk/src/main/java/org/cytoscape/vizmap/internal/VisualMappingManagerImpl.java
@@ -0,0 +1,26 @@
+package org.cytoscape.vizmap.internal;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.vizmap.VisualMappingManager;
+import org.cytoscape.vizmap.VisualStyle;
+
+public class VisualMappingManagerImpl implements VisualMappingManager {
+	private HashMap<CyNetworkView, VisualStyle> vsForNetwork;
+	
+	public VisualMappingManagerImpl() {
+		vsForNetwork = new HashMap<CyNetworkView, VisualStyle>();
+	}
+	
+	public VisualStyle getVisualStyle(CyNetworkView nv) {
+		return vsForNetwork.get(nv);
+	}
+
+	public void setVisualStyle(VisualStyle vs, CyNetworkView nv) {
+		// TODO Auto-generated method stub
+		vsForNetwork.put(nv, vs);
+	}
+
+}
