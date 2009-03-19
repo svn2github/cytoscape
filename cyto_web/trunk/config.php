052c4ae7dd51735af71c9e1e44caf80e251e3bde
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-03-19 16:44:26 +0000

    updated config file to reflect new directory locations
    git-svn-id: http://chianti.ucsd.edu/svn@16313 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/config.php
+++ cyto_web/trunk/config.php
@@ -224,8 +224,8 @@ $cyto2_3_1_install_anywhere = "http://chianti.ucsd.edu/Cyto-2_3_1/install.htm";
 #  Do not Modify
 if ($in_production == true) {
 	#  The Real Cytoscape Data File
-	$cyto_data = "/usr/local/www/virtual3/66/175/24/126/data/cyto_data.txt";
-	$litsearch_log = "/usr/local/www/virtual3/66/175/24/126/data/litsearch_log.txt";
+	$cyto_data = "/home/u5/treyideker/data/cyto_data.txt";
+	$litsearch_log = "/home/u5/treyideker/data/litsearch_log.txt";
 } else {
 	#  A Test Data File (Used for local testing purposes only)
 	$cyto_data = "data/cyto_data.txt";
