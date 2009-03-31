2ca8d8d573efa7549346da3d2cc28a93e989c83c
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-03-31 17:24:35 +0000

    added new events
    git-svn-id: http://chianti.ucsd.edu/svn@16364 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/vizmap-api/trunk/src/main/java/org/cytoscape/vizmap/events/SaveVizmapPropsEvent.java
@@ -0,0 +1,10 @@
+
+package org.cytoscape.vizmap.events;
+
+import org.cytoscape.event.CyEvent;
+
+/**
+ * 
+ */
+public interface SaveVizmapPropsEvent extends CyEvent<Object> {
+}
---
+++ core3/vizmap-api/trunk/src/main/java/org/cytoscape/vizmap/events/SaveVizmapPropsListener.java
@@ -0,0 +1,11 @@
+
+package org.cytoscape.vizmap.events;
+
+import org.cytoscape.event.CyListener;
+
+/**
+ * 
+ */
+public interface SaveVizmapPropsListener extends CyListener {
+	public void handleEvent(SaveVizmapPropsEvent e);
+}
