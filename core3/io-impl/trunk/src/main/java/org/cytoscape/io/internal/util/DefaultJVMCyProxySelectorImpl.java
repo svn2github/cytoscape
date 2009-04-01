dc19869eeadeaebcb72f66308b4e24fd69f3c506
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-04-01 17:44:00 +0000

    Added DefaultJVMCyProxySelectorImpl
    git-svn-id: http://chianti.ucsd.edu/svn@16378 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/io-impl/trunk/src/main/java/org/cytoscape/io/internal/util/DefaultJVMCyProxySelectorImpl.java
@@ -0,0 +1,15 @@
+package org.cytoscape.io.internal.util;
+
+import org.cytoscape.io.CyProxySelector;
+
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+
+public class DefaultJVMCyProxySelectorImpl implements CyProxySelector
+{
+	public Proxy select(URI uri)
+	{
+		return ProxySelector.getDefault().select(uri).get(0);
+	}
+}
