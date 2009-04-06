44e410e99e44e4354e4f7a47b28b981d1280c35d
mmichaud <mmichaud@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-04-06 14:54:29 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@16473 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/pasteur/mathieu/tunables2/src/main/java/org/cytoscape/command/proxy.java
+++ csplugins/trunk/pasteur/mathieu/tunables2/src/main/java/org/cytoscape/command/proxy.java
@@ -2,14 +2,12 @@ package org.cytoscape.command;
 
 
 import org.cytoscape.work.Tunable;	
+import org.cytoscape.work.TunableValidator;
 import org.cytoscape.work.Tunable.Param;
 import org.cytoscape.work.util.ListSingleSelection;
 
-public class proxy implements Command{
+public class proxy implements Command,TunableValidator{
 
-//	@TunablesValidator
-//	TunablesValidator validator = new MyValidator();
-	
 	@Tunable(description="Type")
 	public ListSingleSelection<String> type = new ListSingleSelection<String>("direct","http","socks");
 	
@@ -19,15 +17,27 @@ public class proxy implements Command{
 	@Tunable(description="Port",group={""},dependsOn="type!=direct",alignment={Param.horizontal})
 	public int port;
 	
-	public void execute() {
+	public void execute(){
 	}
 	
-//	class MyValidator implements TunablesValidator
-//	{
-//		public String validate()
-//		{
-//			
+	public void validate(String param){
+		if(param.equals("type")){
+			System.out.println("test for " + param);
+		}
+		if(param.equals("hostname")){
+			System.out.println("test for " + param);
+			if(hostname.contains("cache")) System.out.println("and the test is successful");
+		}
+		if(param.equals("port")){
+			System.out.println("test for " + param);
+		}
+//		if(param.contains("cache.pasteur")){
+//			System.out.println("Test Succeeded for hostname");
+//			return true;
 //		}
-//	}
-	
+//		else{
+//			System.out.println("Test Failed");
+//			return false;
+//		}
+	}	
 }
