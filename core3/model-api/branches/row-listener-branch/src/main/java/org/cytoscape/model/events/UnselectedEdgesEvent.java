48c1f65bfb18d8529fcdf80a5939b157b3f6099e
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-04-07 19:24:22 +0000

    creating row listener branch
    git-svn-id: http://chianti.ucsd.edu/svn@16483 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/model-api/branches/row-listener-branch/src/main/java/org/cytoscape/model/events/UnselectedEdgesEvent.java
@@ -0,0 +1,12 @@
+
+
+package org.cytoscape.model.events;
+
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.event.CyEvent;
+import java.util.List;
+
+public interface UnselectedEdgesEvent extends CyEvent<CyNetwork> {
+	public List<CyEdge> getEdgeList();
+}
---
+++ core3/model-api/branches/row-listener-branch/src/test/java/org/cytoscape/event/AbstractCyEventHelperTest.java
@@ -0,0 +1,81 @@
+
+/*
+ Copyright (c) 2008, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+package org.cytoscape.event;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.lang.RuntimeException;
+
+import java.util.List;
+
+
+/**
+ */
+public abstract class AbstractCyEventHelperTest extends TestCase {
+
+	protected CyEventHelper helper;
+	protected StubCyListener service;
+
+	/**
+	 *  DOCUMENT ME!
+	 */
+	public void testSychronous() {
+		// TODO figure out why I need to cast the StubCyEventImpl
+		helper.fireSynchronousEvent((StubCyEvent) new StubCyEventImpl("homer"),
+		                            StubCyListener.class);
+		assertEquals(1, service.getNumCalls());
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 */
+	public void testASychronous() {
+		try {
+			// TODO figure out why I need to cast the StubCyEventImpl
+			helper.fireAsynchronousEvent((StubCyEvent) new StubCyEventImpl("marge"),
+			                             StubCyListener.class);
+			Thread.sleep(500); // TODO is there a better way to wait?
+			assertEquals(1, service.getNumCalls());
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+			fail();
+		}
+	}
+}
