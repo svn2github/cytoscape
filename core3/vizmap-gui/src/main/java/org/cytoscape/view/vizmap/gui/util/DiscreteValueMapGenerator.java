a1d35e5989eaacff97fc163b81cee4d7b24ab19d
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-04-14 18:02:41 +0000

    Discrete mapping generators refactored to support generics.
    git-svn-id: http://chianti.ucsd.edu/svn@16561 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-gui/src/main/java/org/cytoscape/view/vizmap/gui/util/DiscreteValueMapGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.cytoscape.view.vizmap.gui.util;
-
-import java.util.Map;
-import java.util.Set;
-
-public interface DiscreteValueMapGenerator<V> {
-	
-	public Map<?, V> generateMap(Set<V> attributeSet);
-}
