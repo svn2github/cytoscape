bfb9df562786b070795ca71946559399e1b54c68
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-04-14 00:18:04 +0000

    added hide actions as tasks
    git-svn-id: http://chianti.ucsd.edu/svn@16542 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/hide/HideSelectedNodesTask.java
@@ -0,0 +1,68 @@
+/*
+  File: HideSelectedNodesTask.java
+
+  Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+
+  The Cytoscape Consortium is:
+  - Institute for Systems Biology
+  - University of California San Diego
+  - Memorial Sloan-Kettering Cancer Center
+  - Institut Pasteur
+  - Agilent Technologies
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+package org.cytoscape.task.internal.hide;
+
+import cytoscape.CyNetworkManager;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyDataTableUtil;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.work.Task;
+import org.cytoscape.work.TaskMonitor;
+
+import java.util.List;
+
+
+public class HideSelectedNodesTask extends AbstractHideTask {
+
+	public HideSelectedNodesTask(CyNetworkManager netmgr) {
+		super(netmgr);
+	}
+
+	public void run(TaskMonitor e) {
+	
+		final CyNetwork curr = netmgr.getCurrentNetwork();
+		final CyNetworkView view = netmgr.getNetworkView( curr.getSUID() );
+		final List<CyNode> selectedNodes = CyDataTableUtil.getNodesInState(curr,"selected",true);
+
+		HideUtils.setVisibleNodes( selectedNodes, false, view );
+
+        if ( view != null )
+            view.updateView();
+	} 
+}
