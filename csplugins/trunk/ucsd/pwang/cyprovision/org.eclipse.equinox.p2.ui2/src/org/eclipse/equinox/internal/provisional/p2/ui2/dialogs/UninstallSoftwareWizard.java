ab27a3f06d743c41b7ec51093971bde694e2f12c
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-04-17 18:57:59 +0000

    original creation
    git-svn-id: http://chianti.ucsd.edu/svn@16619 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/pwang/cyprovision/org.eclipse.equinox.p2.ui2/src/org/eclipse/equinox/internal/provisional/p2/ui2/dialogs/UninstallSoftwareWizard.java
@@ -0,0 +1,49 @@
+package org.eclipse.equinox.internal.provisional.p2.ui2.dialogs;
+
+import java.awt.Rectangle;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.swing.JOptionPane;
+
+//import org.eclipse.equinox.internal.provisional.p2.ui2.dialogs.InstallNewSoftwareWizard.MyResultProducer;
+import org.eclipse.equinox.internal.provisional.p2.ui2.model.InstalledIUElement;
+import org.netbeans.api.wizard.WizardDisplayer;
+import org.netbeans.spi.wizard.Wizard;
+import org.netbeans.spi.wizard.WizardException;
+import org.netbeans.spi.wizard.WizardPage;
+import org.netbeans.spi.wizard.WizardPage.WizardResultProducer;
+
+public class UninstallSoftwareWizard {
+
+	public UninstallSoftwareWizard(InstallationInformationDialog parent, String profileId, Vector<InstalledIUElement> ius){
+		parent.dispose();
+		WizardPage[] pages = new WizardPage[1];
+
+		UninstallDetailsPage page1 = new UninstallDetailsPage(profileId, ius);
+		pages[0] = page1;
+
+		//Use the utility method to compose a Wizard
+		Wizard wizard = WizardPage.createWizard("Uninstall", pages, new MyResultProducer());
+
+		//And show it on screen
+		Map gatherSettings = (Map) WizardDisplayer.showWizard (wizard, new Rectangle(20,20,800,600));
+
+	}
+
+	class MyResultProducer implements WizardResultProducer {
+		public Object finish(Map wizardData) throws WizardException {
+			//String nameForThing = (String) wizardData.get ("name");
+
+			return new BackgroundUninstall(wizardData);	        
+		}
+
+		//Called when the user presses the cancel button
+		public boolean cancel(Map settings) {
+			//boolean dialogShouldClose = JOptionPane.showConfirmDialog (null, 
+			//   "Are you sure you want to cancel the installation?!!") == JOptionPane.OK_OPTION;
+			return true; //dialogShouldClose;
+		}
+	}
+
+}
