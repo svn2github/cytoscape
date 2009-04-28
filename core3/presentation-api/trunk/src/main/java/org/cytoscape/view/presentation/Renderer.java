39d8648151b8ddefc4b8aa4b8099368143b0454b
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-04-28 20:15:56 +0000

    Renderer interface for pluggable renderer.
    git-svn-id: http://chianti.ucsd.edu/svn@16694 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/presentation-api/trunk/src/main/java/org/cytoscape/view/presentation/Renderer.java
@@ -0,0 +1,20 @@
+package org.cytoscape.view.presentation;
+
+import java.util.Collection;
+
+import org.cytoscape.view.model.VisualProperty;
+
+
+/**
+ * A function to draw a given network object.
+ * Will be used by pluggable-renderer
+ * 
+ */
+public interface Renderer {
+	/**
+	 * Return a list of visual attributes this renderer can use
+	 */
+	public Collection<VisualProperty<?>> supportedVisualProperties();
+	
+	public String getName();
+}
