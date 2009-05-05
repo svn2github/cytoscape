8b46f16cb6bc075c5a092c29dd41ae260efc2fa2
mmichaud <mmichaud@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-05-05 17:17:21 +0000

    []
    git-svn-id: http://chianti.ucsd.edu/svn@16758 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-swing-impl/trunk/src/main/java/org/cytoscape/work/internal/tunables/GuiHandlerFactory.java
+++ core3/work-swing-impl/trunk/src/main/java/org/cytoscape/work/internal/tunables/GuiHandlerFactory.java
@@ -22,6 +22,13 @@ import org.cytoscape.work.util.ListSingleSelection;
 
 
 
+
+/**
+ * Provides a factory to create <code>Guihandler</code> depending on their type
+ * A <code>Guihandler</code> is simply a <code>Handler</code> with the GUI aspect to display the handled object to the user in a proper way.
+ * 
+ * @author pasteur
+ */
 public class GuiHandlerFactory implements HandlerFactory<Guihandler> {
 
 	private Bookmarks bookmarks;
@@ -29,6 +36,12 @@ public class GuiHandlerFactory implements HandlerFactory<Guihandler> {
 //	public FileUtil flUtil;
 //	public StreamUtil stUtil;
 	
+	
+	/**
+	 * creates a new GuiHandlerFactory object
+	 * @param book	informations and properties of the <code>Bookmarks</code> registered
+	 * @param bkUtil object that provides tools to manage the <code>Bookmarks</code>
+	 */
 	public GuiHandlerFactory(CyProperty<Bookmarks> book, BookmarksUtil bkUtil) {
 		this.bookmarks = book.getProperties();
 		this.bkUtil = bkUtil;
@@ -36,12 +49,29 @@ public class GuiHandlerFactory implements HandlerFactory<Guihandler> {
 //		this.stUtil = stUtil;
 	}
 	
+	
+	/**
+	 * To get a <code>Handler</code> for a Method annotated as <code>Tunable</code>
+	 * 
+	 * @param	m	the annotated method
+	 * @param	o	object of the <code>Handler</code>
+	 * @param	t	tunable of the <code>Handler</code>
+	 * @return a <code>Guihandler</code> object depending on the Method's type
+	 */	
 	public Guihandler getHandler(Method m, Object o, Tunable t) {
 		return null;
 	}
 
+	
+	/**
+	 * To get a <code>Handler</code> for a Field annotated as <code>Tunable</code>
+	 * 
+	 * @param	m	the annotated field
+	 * @param	o	object of the <code>Handler</code>
+	 * @param	t	tunable of the <code>Handler</code>
+	 * @return a <code>Guihandler</code> object depending on the Field's type
+	 */
 	public Guihandler getHandler(Field f, Object o, Tunable t){
-		
 		Class<?> type = f.getType();
 		if(type == Integer.class || type == int.class)
 			return new IntegerHandler(f,o,t);
@@ -68,6 +98,7 @@ public class GuiHandlerFactory implements HandlerFactory<Guihandler> {
 		else if(type == ListMultipleSelection.class)
 			return new ListMultipleHandler<String>(f,o,t);
 		else if(type == File.class)
+//			return new FileHandler(f,o,t,flUtil);
 			return new FileHandler(f,o,t);
 		else if(type == URL.class)
 			return new URLHandler(f,o,t,bookmarks,bkUtil);
