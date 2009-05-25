f561bff5ee88d0cab7e5532a5707d06fc936fddb
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-05-25 00:14:56 +0000

    More cleanup from move
    git-svn-id: http://chianti.ucsd.edu/svn@16816 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsf/scooter/chemViz/src/test/chemViz/ChemVizPluginTest.java
+++ csplugins/trunk/ucsf/scooter/chemViz/src/test/chemViz/ChemVizPluginTest.java
@@ -44,10 +44,7 @@ public class ChemVizPluginTest extends TestCase {
 	}
 
 	public void testConvertInchiToSmiles() {
-		String inchi = "InChI=1/C4H10/c1-3-4-2/h3-4H2,1-2H3";
-		String smiles = "CCCC";
-		String result = ChemVizPlugin.convertInchiToSmiles(inchi);
-		assertEquals(smiles, result);
+		fail("Not yet implemented");
 	}
 
 	public void testDepiectInchi() {
