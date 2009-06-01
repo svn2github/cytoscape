44f2d75218707384d4aa7f7df420e7ecb9c8c787
ghuck <ghuck@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-01 19:39:23 +0000

    created a copy of the original code of gpuGraphDrawing
    git-svn-id: http://chianti.ucsd.edu/svn@16855 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/ghuck/gpuGraphDrawing-original/common.h
@@ -0,0 +1,16 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define MAX_REC_LEN 2048
+#define SCREEN_W 1000
+#define SCREEN_H 1000
+#define maxThreadsThisBlock 248
+
+#define max(a,b) (((a)>(b))? a:b)
+#define min(a,b) (((a)<(b))? a:b)
+
+int				EDGE_LEN;
+int				initialNoIterations;
+
+#endif
+
