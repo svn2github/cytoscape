9d3a9e368a693a475ebfde38b9a880cf36dc1655
ghuck <ghuck@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-11 21:50:42 +0000

    Corrected license. copyright.h removed.
    git-svn-id: http://chianti.ucsd.edu/svn@16930 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/ghuck/gpuGraphDrawing/common.h
+++ csplugins/trunk/soc/ghuck/gpuGraphDrawing/common.h
@@ -1,15 +1,24 @@
 /**************************************************************************************
-This code is released under GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007. 
 Copyright (C) Apeksha Godiyal, 2008
 Copyright (C) Gerardo Huck, 2009
 
-See licence.h and copyright.h for more information.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+See licence.h for more information.
 **************************************************************************************/
-
 #include "licence.h"
-#include "copyright.h"
-
-
 
 #ifndef __COMMON_H__
 #define __COMMON_H__
