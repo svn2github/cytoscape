3c90158b0a2baa490b48bb769ed6467f99b29fd0
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-18 01:42:31 +0000

    importing editor
    git-svn-id: http://chianti.ucsd.edu/svn@17003 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/editor-impl/trunk/src/main/java/org/cytoscape/editor/internal/SIFInterpreterTask.java
@@ -0,0 +1,141 @@
+
+package org.cytoscape.editor.internal;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.View;
+import org.cytoscape.work.Task;
+import org.cytoscape.work.TaskMonitor;
+import org.cytoscape.work.Tunable;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SIFInterpreterTask implements Task {
+
+	private CyNetworkView view;
+	private CyNetwork network;
+
+	@Tunable(description="Type in a nodes/edges expression in SIF format, e.g. A inhibits B")
+	public String sifString;
+
+	public SIFInterpreterTask(CyNetworkView view) {
+		this.view = view;
+		network = view.getSource();
+	}
+
+	public void run(TaskMonitor tm) {
+		if ( sifString == null )
+			throw new NullPointerException("SIF input string is null");
+		
+		String[] terms = sifString.split("\\s");
+		if (terms != null) {
+			if (terms.length > 0) {
+				String name1 = terms[0].trim();
+				if (!name1.equals(null)) {
+					
+					CyNode node1 = findNode(terms[0]);
+					if (node1 == null) {
+
+						node1 = network.addNode();
+						node1.attrs().set("name",terms[0]);
+
+						//Cytoscape.getCurrentNetwork().restoreNode(node1);
+
+						//nv1 = view.getNodeView(node1);
+
+						//double[] nextLocn = new double[2];
+						//nextLocn[0] = p.getX();
+						//nextLocn[1] = p.getY();
+						//Cytoscape.getCurrentNetworkView().xformComponentToNodeCoords(nextLocn);
+						//nv1.setOffset(nextLocn[0], nextLocn[1]);
+					} else {
+						//nv1 = view.getNodeView(node1);
+					}
+					System.out.println("Node 1 = " + node1);
+					//System.out.println("NodeView 1 = " + nv1);
+
+				//	double spacing = 3.0 * Cytoscape.getCurrentNetworkView()
+				//			.getNodeView(node1).getWidth();
+
+					if (terms.length == 3) // simple case of 'A interaction B'
+					{
+						CyNode node2 = findNode(terms[2]);
+						if (node2 == null) {
+							node2 = network.addNode();
+							node2.attrs().set("name",terms[2]);
+
+							//Cytoscape.getCurrentNetwork().restoreNode(node2);
+							//nv2 = view.getNodeView(node2);
+
+							//nv2.setOffset(nv1.getXPosition() + spacing, nv1
+							//		.getYPosition());
+						}
+
+						CyEdge edge = network.addEdge(node1, node2, true);
+						edge.attrs().set("name",terms[1]);
+						//Cytoscape.getCurrentNetwork().restoreEdge(edge);
+
+					} else if (terms.length > 3) {
+						// process multiple targets and one source
+						List<View<CyNode>> nodeViews = new ArrayList<View<CyNode>>();
+						String interactionType = terms[1];
+						for (int i = 2; i < terms.length; i++)
+						{
+							CyNode node2 = findNode(terms[i]);
+							if (node2 == null) {
+								node2 = network.addNode();
+								node2.attrs().set("name",terms[i]);
+
+								//Cytoscape.getCurrentNetwork().restoreNode(node2);
+								//nv2 = view.getNodeView(node2);
+
+								//nv2.setOffset(nv1.getXPosition() + spacing, nv1
+								//		.getYPosition());
+							}
+
+							CyEdge edge = network.addEdge(node1, node2, true);
+							edge.attrs().set("name",terms[1]);
+							//doCircleLayout(nodeViews, nv1);
+					}
+				}
+			}
+			view.updateView();	
+		}
+		}
+	}
+	private void doCircleLayout(List<View<CyNode>> nodeViews, View<CyNode> nv1) {
+
+/*
+		// // Compute Radius
+
+		int r = (int) Math.max((nodeViews.size() * nv1.getWidth()) / Math.PI, 30);
+
+		// Compute angle step
+		double phi = (2 * Math.PI) / nodeViews.size();
+
+		// Arrange vertices in a circle
+		for (int i = 0; i < nodeViews.size(); i++) {
+
+			View<CyNode> nv = nodeViews.get(i);
+			nv.setOffset(
+					nv1.getXPosition() + (int) (r * Math.sin(i * phi)), 
+					nv1.getYPosition() + (int) (r * Math.cos(i * phi)));
+		}
+
+		//Cytoscape.redrawGraph(Cytoscape.getCurrentNetworkView());
+*/
+	}
+
+	public void cancel() {
+	}
+
+	private CyNode findNode(String name) {
+		for ( CyNode node : network.getNodeList() ) 
+			if ( node.attrs().get("name",String.class).equals(name) ) 
+				return node;	
+		return null;
+	}
+}
+
