8dd6500dd5e12b1cc0568d2b73ec76ece68790fb
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-23 00:41:55 +0000

    Add menu item: File->Import->Phylogenetic tree...
    git-svn-id: http://chianti.ucsd.edu/svn@17055 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/chinmoy/phylotree/src/org/cytoscape/phylotree/PhylotreePlugin.java
+++ csplugins/trunk/soc/chinmoy/phylotree/src/org/cytoscape/phylotree/PhylotreePlugin.java
@@ -15,14 +15,14 @@ import cytoscape.layout.AbstractLayout;
 import cytoscape.layout.LayoutProperties;
 import cytoscape.layout.Tunable;
 
-import giny.model.GraphPerspective;
 import giny.model.Node;
 import javax.swing.JPanel;
 
-import java.awt.GridLayout;
-import java.awt.Rectangle;
-
 import java.util.Iterator;
+import org.cytoscape.phylotree.actions.MyLayoutAction;
+import org.cytoscape.phylotree.actions.PhyloTreeImportAction;
+
+import org.cytoscape.phylotree.layout.MyLayout;
 /**
  * 
  */
@@ -36,131 +36,13 @@ public class PhylotreePlugin extends CytoscapePlugin {
 	 * 
 	 */
 	public PhylotreePlugin() {
-		CyLayouts.addLayout(new MyLayout(), "PhyloTree Layouts");
+		
+		// (1) add an menuItem: File->Import->Phylogenetic Tree
+		PhyloTreeImportAction menuAction = new PhyloTreeImportAction(this);
+		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction) menuAction);
+
+		//(2) add another menu item: Layout->Phylotree layouts
+		//CyLayouts.addLayout(new MyLayout(), "PhyloTree Layouts");
 	}
 	
-	class MyLayout extends AbstractLayout{
-		/**
-		 * Creates a new layout object.
-		 */
-		public MyLayout() {
-			super();
-			layoutProperties = new LayoutProperties(getName());
-			initialize_properties();
-		}
-		
-		protected void initialize_properties() {
-			layoutProperties.add(new Tunable("groupcount",
-			                                 "Number of random groups ",
-			                                 Tunable.INTEGER, new Integer(2)));
-
-			layoutProperties.initializeProperties();
-
-			updateSettings(true);
-		}
-
-		/**
-		 *  DOCUMENT ME!
-		 */
-		public void updateSettings() {
-			updateSettings(false);
-		}
-
-		/**
-		 *  DOCUMENT ME!
-		 *
-		 * @param force DOCUMENT ME!
-		 */
-		public void updateSettings(boolean force) {
-			layoutProperties.updateValues();
-
-			Tunable t = layoutProperties.get("groupcount");
-
-			if ((t != null) && (t.valueChanged() || force))
-				groupcount = ((Integer) t.getValue()).intValue();
-		}
-
-		/**
-		 * Get the settings panel for this layout
-		 */
-		public JPanel getSettingsPanel() {
-			JPanel panel = new JPanel(new GridLayout(0, 1));
-			panel.add(layoutProperties.getTunablePanel());
-
-			return panel;
-		}
-
-		
-		/**
-		 *  DOCUMENT ME!
-		 */
-		public void revertSettings() {
-			layoutProperties.revertProperties();
-		}
-
-		public LayoutProperties getSettings() {
-			return layoutProperties;
-		}
-
-		/**
-		 *  DOCUMENT ME!
-		 */
-		public void construct() {
-			taskMonitor.setStatus("Initializing");
-			initialize(); // Calls initialize_local
-
-	
-			System.out.println("do layout here: groupcount = "+ groupcount);
-
-			if (groupcount <2) {
-				return;
-			}
-
-			// Get the group center X
-			double[] group_center_x = new double[groupcount];
-			
-			for (int i=0; i<groupcount; i++) {
-				group_center_x[i] = i* maxwidth/(groupcount*2); 
-			}
-			
-			double group_width = (maxwidth/groupcount)*0.6/2;
-						
-			Iterator<Node> it = network.nodesIterator();
-			
-			int group_id = 0;
-			
-			while (it.hasNext()) {
-				if (canceled)
-					return;
-
-				group_id = (int) Math.round((groupcount-1)*Math.random());	
-				
-				double x = group_center_x[group_id] + (Math.random()-0.5)*group_width;
-				
-				Node node = (Node) it.next();
-			
-				//System.out.println(group_id);
-				
-				networkView.getNodeView(node).setXPosition(x);
-			}
-		}
-
-		private double maxwidth = 5000.0;
-
-		/**
-		 * getName is used to construct property strings
-		 * for this layout.
-		 */
-		public  String getName() {
-			return "PhyloTree Layout";
-		}
-
-		/**
-		 * toString is used to get the user-visible name
-		 * of the layout
-		 */
-		public  String toString(){
-			return "PhyloTree Layout";
-		}
-	}
 }
