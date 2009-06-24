075cb9a934a015bb40a20f9b6c2917812119f7df
ghuck <ghuck@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-24 14:45:19 +0000

    Created gpuGraphDrawing.cu file (containing main function)
    Makefile modified to compile gpuGraphDrawing
    
    
    
    
    git-svn-id: http://chianti.ucsd.edu/svn@17070 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/ghuck/gpuGraphDrawing/interface.cu
+++ csplugins/trunk/soc/ghuck/gpuGraphDrawing/interface.cu
@@ -409,47 +409,5 @@ int calculateLayout (globalScope* scope)
 
 
 
-////////////////////////////////////////////////////////////////////////////////
-// Program main
-////////////////////////////////////////////////////////////////////////////////
 
-int main(int argc, char** argv)
-{
-  FILE* from;
-  globalScope *scope;
 
-  // Check number of arguments
-  if (argc < 2) error("Wrong no of args");
-
-  // Create scope
-  scope = globalScopeCreate();
-
-  // Ask for parameters
-  printf("Enter the size of the coarsest graph (Default 50):");          scanf("%d", &(scope->coarseGraphSize));
-  printf("Enter the number of interpolation iterations (Default 50):");  scanf("%d", &(scope->interpolationIterations));
-  printf("Enter the level of convergence (Default 2):");                 scanf("%d", &(scope->levelConvergence));
-  printf("Enter the ideal edge length (Default 5):");                    scanf("%d", &(scope->EDGE_LEN));
-  printf("Enter the initial no of force iterations(Default 300):");      scanf("%d", &(scope->initialNoIterations));
- 
-  // Open file 
-  from=fopen(argv[1],"r");
-  if(!from) error("cannot open 1st file");
-  
-  // Read graph grom file (argv[1])
-  int len = strlen(argv[1]);
-  if((argv[1][len-1]=='l') && (argv[1][len-2]=='m') && (argv[1][len-3]=='g') )
-    readGml(scope, from);
-  else
-    readChaco(scope, from);
-
-  printf ("Finished reading graph!\n");
-
-  calculateLayout (scope);
-
-  printf ("Finished calculationg layout, showing results...\n");
-
-  // Show results in display
-  showGraph (scope, argc, argv);
-    
-  return 0;
-}
