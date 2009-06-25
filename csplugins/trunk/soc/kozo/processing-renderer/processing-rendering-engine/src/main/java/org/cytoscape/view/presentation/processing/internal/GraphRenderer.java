88b5d81ddaf71c44c5e413fd991cd7287da34114
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-25 22:56:22 +0000

    Graph renderer which actually draw the network.
    git-svn-id: http://chianti.ucsd.edu/svn@17101 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/kozo/processing-renderer/processing-rendering-engine/src/main/java/org/cytoscape/view/presentation/processing/internal/GraphRenderer.java
@@ -0,0 +1,15 @@
+package org.cytoscape.view.presentation.processing.internal;
+
+import processing.core.PApplet;
+
+public class GraphRenderer {
+	
+	private PApplet p;
+	
+	
+	public GraphRenderer(PApplet p) {
+		this.p = p;
+	}
+	
+
+}
