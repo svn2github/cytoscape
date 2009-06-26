537fd47590d61aebf2f5eb07771671e3006ca2f0
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-26 23:15:03 +0000

    Removed CyGroupChangeListener, moved it to GroupPanel
    git-svn-id: http://chianti.ucsd.edu/svn@17131 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/mes/groupResults/src/org/cytoscape/groups/results/GroupResultsPlugin.java
+++ csplugins/trunk/ucsd/mes/groupResults/src/org/cytoscape/groups/results/GroupResultsPlugin.java
@@ -9,13 +9,8 @@ import cytoscape.view.cytopanels.CytoPanel;
 import cytoscape.view.cytopanels.CytoPanelState;
 import cytoscape.plugin.CytoscapePlugin;
 
-// Cytoscape group system imports
-import cytoscape.groups.CyGroup;
-import cytoscape.groups.CyGroupManager;
-import cytoscape.groups.CyGroupChangeListener;
-
 public class GroupResultsPlugin extends CytoscapePlugin 
-                               implements CyGroupChangeListener { 
+{ 
 
 	private GroupPanel groupPanel; 
 	private CytoPanel cytoPanel; 
@@ -29,24 +24,7 @@ public class GroupResultsPlugin extends CytoscapePlugin
 		groupPanel = new GroupPanel();
 		cytoPanel = Cytoscape.getDesktop().getCytoPanel(SwingConstants.EAST);
 		cytoPanel.add("Module Finding Results", groupPanel);
-
-		CyGroupManager.addGroupChangeListener(this);
 	}
 
-	public void groupChanged(CyGroup group, CyGroupChangeListener.ChangeType change) { 
-		if ( change == CyGroupChangeListener.ChangeType.GROUP_CREATED ) {
-			groupPanel.groupCreated(group);
-
-			// set visible
-			if ( cytoPanel.getState() == CytoPanelState.HIDE )
-				cytoPanel.setState( CytoPanelState.DOCK );
-		} else if ( change == CyGroupChangeListener.ChangeType.GROUP_DELETED ) {
-			groupPanel.groupRemoved(group);
-		} else if ( change == CyGroupChangeListener.ChangeType.GROUP_MODIFIED ) {
-			groupPanel.groupChanged(group);
-		} else {
-			System.err.println("unsupported change type: " + change);
-		}
-	}
 }
 
