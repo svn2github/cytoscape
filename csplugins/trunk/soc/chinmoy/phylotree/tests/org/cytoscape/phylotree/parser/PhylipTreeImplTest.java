42f35313298453dbe83af5ff64de05c53edc466c
chinmoy <chinmoy@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-26 17:32:24 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@17110 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/chinmoy/phylotree/tests/org/cytoscape/phylotree/parser/PhylipTreeImplTest.java
+++ csplugins/trunk/soc/chinmoy/phylotree/tests/org/cytoscape/phylotree/parser/PhylipTreeImplTest.java
@@ -12,22 +12,52 @@ public class PhylipTreeImplTest extends TestCase {
 	
 	public void setUp() {
 	
-		String treeStr = "XXXX";
-		PhylipTreeImpl treeParser = new PhylipTreeImpl(treeStr);
+		treeStr = "(B:9,(A:8,C:7,E:6),D:5);";
+		treeParser = new PhylipTreeImpl(treeStr);
 	}
 	
 	public void testGetNodeList() {
-		List nodeList = treeParser.getNodeList();
+		List<PhylotreeNode> nodeList = treeParser.getNodeList();
 		
-		assertEquals("","");
+		// Test number of nodes
+		assertEquals(7,nodeList.size());
+		
+		// Test names of first and second to last nodes created
+		assertEquals("E", nodeList.get(0).getName());
+		assertEquals("B",nodeList.get(nodeList.size()-2).getName());
+		
+		 
 	}
 	
 	public void testGetEdges() {
+
+		// Test the number of edges the first parent node has
+		List<PhylotreeNode> nodeList = treeParser.getNodeList();
+		
+		List<PhylotreeEdge> edgeList = treeParser.getEdges(nodeList.get(4));
+		
+		assertEquals(4, edgeList.size());
+		
+	
 		
 	}
 
 	
 	public void testGetEdgeAttribute() {
+		
+		List<PhylotreeNode> nodeList = treeParser.getNodeList();
+		List<PhylotreeEdge> edgeList = treeParser.getEdges(nodeList.get(0));
+		
+		// Test edge lengths of the first node
+		assertEquals(6.0, treeParser.getEdgeAttribute(edgeList.get(0)).get(0));
+		
+		// Test edge lengths of the first parent node
+		edgeList = treeParser.getEdges(nodeList.get(4));
+		
+		assertEquals(6.0, treeParser.getEdgeAttribute(edgeList.get(0)).get(0));
+		assertEquals(7.0, treeParser.getEdgeAttribute(edgeList.get(1)).get(0));
+		assertEquals(8.0, treeParser.getEdgeAttribute(edgeList.get(2)).get(0));
+		assertEquals(0.0, treeParser.getEdgeAttribute(edgeList.get(3)).get(0));
 		
 	}
 
