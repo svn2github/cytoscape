27785a2da4deb67258e591a2fd6e79f019abba48
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-26 23:01:09 +0000

    Builder class to create actual multi-layer network.
    git-svn-id: http://chianti.ucsd.edu/svn@17128 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/MultiLayerNetworkBuilder.java
@@ -0,0 +1,15 @@
+package org.cytoscape.layer.internal.ui;
+
+/**
+ * Build actual network here
+ * 
+ * @author kono
+ *
+ */
+public class MultiLayerNetworkBuilder {
+	
+	public MultiLayerNetworkBuilder() {
+		
+	}
+
+}
