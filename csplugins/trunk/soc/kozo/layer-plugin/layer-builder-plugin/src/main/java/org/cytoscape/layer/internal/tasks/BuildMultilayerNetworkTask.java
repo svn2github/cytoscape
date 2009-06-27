8b489e7d56a47050f5d3bd09cd61829ed3a85332
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-27 08:33:41 +0000

    Incomplete - # 13: Implement Layered Network Data Builder http://cg06.naist.jp/gsoc/ticket/13 just renamed files from Create to Build
    git-svn-id: http://chianti.ucsd.edu/svn@17141 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/BuildMultilayerNetworkTask.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/BuildMultilayerNetworkTask.java
@@ -5,13 +5,13 @@ import org.cytoscape.work.TaskMonitor;
 
 import cytoscape.CyNetworkManager;
 
-public class CreateMultilayerNetworkTask implements Task {
+public class BuildMultilayerNetworkTask implements Task {
 	private CyNetworkManager manager;
 	private TaskMonitor taskMonitor;
 
 	// private LayerFactory lFactory;
 
-	public CreateMultilayerNetworkTask(CyNetworkManager manager) {
+	public BuildMultilayerNetworkTask(CyNetworkManager manager) {
 		this.manager = manager;
 	}
 
--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/BuildMultilayerNetworkTaskFactory.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/BuildMultilayerNetworkTaskFactory.java
@@ -3,7 +3,7 @@ package org.cytoscape.layer.internal.tasks;
 import org.cytoscape.work.Task;
 import org.cytoscape.work.TaskFactory;
 
-public class CreateMultilayerNetworkTaskFactory implements TaskFactory {
+public class BuildMultilayerNetworkTaskFactory implements TaskFactory {
 
 	public Task getTask() {
 		// TODO Auto-generated method stub
