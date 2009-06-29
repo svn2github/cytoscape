703800eb50cc5d45d918eea67442355adf7fd210
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-06-29 23:33:51 +0000

    Test directory.
    git-svn-id: http://chianti.ucsd.edu/svn@17161 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/test/java/org/cytoscape/layer/test/SimpleTest.java
@@ -0,0 +1,23 @@
+package org.cytoscape.layer.test;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void simpleTest() throws Exception {
+		System.out.println("This is a simple test.");
+	}
+
+}
