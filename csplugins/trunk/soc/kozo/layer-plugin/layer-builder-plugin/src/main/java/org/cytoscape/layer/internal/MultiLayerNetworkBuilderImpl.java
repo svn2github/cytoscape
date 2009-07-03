35f6abfbfbdd80399a07af4a590b3a9b7d207a94
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-03 20:12:17 +0000

    added network merge part(refs ticket:13)
    git-svn-id: http://chianti.ucsd.edu/svn@17222 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/MultiLayerNetworkBuilderImpl.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/MultiLayerNetworkBuilderImpl.java
@@ -33,34 +33,60 @@ public class MultiLayerNetworkBuilderImpl implements MultiLayerNetworkBuilder {
 		layeredNetwork.attrs().set("name", "Layered Network");
 
 		// Connect layers here...
-		for(CyNetwork topLayer : layers){
-			for(CyNetwork bottomLayer : layers){
-				for(CyNetwork connector : connectors){
+		for (CyNetwork topLayer : layers) {
+			for (CyNetwork bottomLayer : layers) {
+				for (CyNetwork connector : connectors) {
 					connect(topLayer, bottomLayer, connector);
 				}
 			}
 		}
-		
+
 		manager.addNetwork(layeredNetwork);
 		return layeredNetwork;
 	}
 
-	private void connect(CyNetwork topLayer, CyNetwork connector, CyNetwork bottomLayer) {
+	private void connect(CyNetwork topLayer, CyNetwork connector,
+			CyNetwork bottomLayer) {
 		// Connect them
-		
+
 		// 1st Phase: add all nodes in the top layer
-		for(CyNode cyNode : topLayer.getNodeList()){
+		for (CyNode cyNode : topLayer.getNodeList()) {
 			CyNode newNode = layeredNetwork.addNode();
-			String name = cyNode.attrs().get("name",String.class);
+			String name = cyNode.attrs().get("name", String.class);
 			newNode.attrs().set("name", name);
 		}
-		for(CyEdge edge: topLayer.getEdgeList()) {
-			CyEdge newEdge = layeredNetwork.addEdge(edge.getSource(), edge.getTarget(), true);
-			String name = edge.attrs().get("name",String.class);
+		for (CyEdge edge : topLayer.getEdgeList()) {
+			CyEdge newEdge = layeredNetwork.addEdge(edge.getSource(), edge
+					.getTarget(), true);
+			String name = edge.attrs().get("name", String.class);
 			newEdge.attrs().set("name", name);
 		}
-	
-		
+		// 2nd Phase: append nodes only included in connector
+		for (CyNode cyNode : connector.getNodeList()) {
+			if (layeredNetwork.containsNode(cyNode) == false) {
+				CyNode newNode = layeredNetwork.addNode();
+				String name = cyNode.attrs().get("name", String.class);
+				newNode.attrs().set("name", name);
+			}
+		}
+		for (CyEdge edge : connector.getEdgeList()) {
+			CyEdge newEdge = layeredNetwork.addEdge(edge.getSource(), edge
+					.getTarget(), true);
+			String name = edge.attrs().get("name", String.class);
+			newEdge.attrs().set("name", name);
+		}
+		// 3rd Phase: append nodes only included in bottom layer
+		for (CyNode cyNode : bottomLayer.getNodeList()) {
+			if (layeredNetwork.containsNode(cyNode) == false) {
+				CyNode newNode = layeredNetwork.addNode();
+				String name=cyNode.attrs().get("name", String.class);
+				newNode.attrs().set("name", name);
+			}
+		}
+		for(CyEdge edge: bottomLayer.getEdgeList()){
+			CyEdge newEdge=layeredNetwork.addEdge(edge.getSource(), edge.getTarget(), true);
+			String name =edge.attrs().get("name", String.class);
+			newEdge.attrs().set("name", name);
+		}
 	}
-
 }
