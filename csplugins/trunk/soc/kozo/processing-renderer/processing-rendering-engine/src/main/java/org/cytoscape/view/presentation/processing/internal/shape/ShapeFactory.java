3d015f2f54398bd463b8b0176815e4d939d3615d
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-03 01:47:06 +0000

    Start updating code based on new gestalt style design.
    git-svn-id: http://chianti.ucsd.edu/svn@17211 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/processing-renderer/processing-rendering-engine/src/main/java/org/cytoscape/view/presentation/processing/internal/shape/ShapeFactory.java
+++ csplugins/trunk/soc/kozo/processing-renderer/processing-rendering-engine/src/main/java/org/cytoscape/view/presentation/processing/internal/shape/ShapeFactory.java
@@ -1,43 +1,39 @@
 package org.cytoscape.view.presentation.processing.internal.shape;
 
+import static org.cytoscape.view.presentation.property.ThreeDVisualLexicon.NODE_Z_LOCATION;
+import static org.cytoscape.view.presentation.property.TwoDVisualLexicon.NODE_X_LOCATION;
+import static org.cytoscape.view.presentation.property.TwoDVisualLexicon.NODE_Y_LOCATION;
 import gestalt.p5.GestaltPlugIn;
-import gestalt.render.Drawable;
 import gestalt.shape.AbstractShape;
-import gestalt.shape.Cube;
 import gestalt.shape.DrawableFactory;
 
 import org.cytoscape.model.CyNode;
 import org.cytoscape.view.model.View;
-import org.cytoscape.view.presentation.processing.P5NodePresentation;
-
-import processing.core.PApplet;
-
-import static org.cytoscape.view.presentation.property.ThreeDVisualLexicon.*;
+import org.cytoscape.view.presentation.processing.P5Presentation;
 
 public class ShapeFactory {
-	
+
 	private final GestaltPlugIn p;
-	
+
 	private final DrawableFactory factory;
-	
+
 	public ShapeFactory(GestaltPlugIn p) {
 		this.p = p;
 		this.factory = p.drawablefactory();
 	}
-	
-	public P5NodePresentation getNodeShape(View<CyNode> view) {
-		
-		
+
+	public P5Presentation<CyNode> getNodeShape(View<CyNode> view) {
+
 		/*
 		 * Parse view info
 		 */
 		final Number x = view.getVisualProperty(NODE_X_LOCATION);
 		final Number y = view.getVisualProperty(NODE_Y_LOCATION);
 		final Number z = view.getVisualProperty(NODE_Z_LOCATION);
-		
+
 		AbstractShape gNode = factory.cube();
 		gNode.position(x.floatValue(), y.floatValue(), z.floatValue());
-		
+
 		return null;
 	}
 
