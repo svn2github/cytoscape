ab473cb7ff748d1bcb651d4257acf9d31a96ca6b
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-05 08:11:48 +0000

    deleted default string list for Integrated Networklist(refs ticket:13)
    git-svn-id: http://chianti.ucsd.edu/svn@17231 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/LayerBuilderDialog.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/LayerBuilderDialog.java
@@ -125,8 +125,7 @@ public class LayerBuilderDialog extends JDialog {
 				.createTitledBorder("Integrated Networks"));
 
 		IntegratedNetworkList.setModel(new javax.swing.AbstractListModel() {
-			String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4",
-					"Item 5" };
+			String[] strings = { };
 
 			public int getSize() {
 				return strings.length;
