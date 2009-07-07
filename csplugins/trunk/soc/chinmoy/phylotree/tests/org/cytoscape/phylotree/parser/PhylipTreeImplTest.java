57009ad096455a9edc4297072fad80c93b40f228
chinmoy <chinmoy@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-07 02:57:29 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@17242 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/chinmoy/phylotree/tests/org/cytoscape/phylotree/parser/PhylipTreeImplTest.java
+++ csplugins/trunk/soc/chinmoy/phylotree/tests/org/cytoscape/phylotree/parser/PhylipTreeImplTest.java
@@ -1,19 +1,25 @@
-package org.cytoscape.phylotree.parser;
+package tests.org.cytoscape.phylotree.parser;
 
-import org.cytoscape.phylotree.parser.PhylipTreeImpl;
+import src.org.cytoscape.phylotree.parser.*;
 
 import junit.framework.TestCase;
+
+import java.io.File;
 import java.util.List;
 
 public class PhylipTreeImplTest extends TestCase {
 
 	private String treeStr = null;
 	private PhylipTreeImpl treeParser = null;
+	private PhylipTreeImpl treeParser2 = null;
 	
 	public void setUp() {
 	
 		treeStr = "(B:9,(A:8,C:7,E:6),D:5);";
 		treeParser = new PhylipTreeImpl(treeStr);
+
+		File file2 = new File("phyliptree.phy");
+		treeParser2 = new PhylipTreeImpl(file2);
 	}
 	
 	public void testGetNodeList() {
@@ -26,7 +32,17 @@ public class PhylipTreeImplTest extends TestCase {
 		assertEquals("E", nodeList.get(0).getName());
 		assertEquals("B",nodeList.get(nodeList.size()-2).getName());
 		
-		 
+		nodeList = treeParser2.getNodeList();
+		
+		// Test number of nodes
+		assertEquals(7,nodeList.size());
+		
+		// Test names of first and second to last nodes created
+		assertEquals("E", nodeList.get(0).getName());
+		assertEquals("B",nodeList.get(nodeList.size()-2).getName());
+		
+
+	 
 	}
 	
 	public void testGetEdges() {
@@ -38,6 +54,12 @@ public class PhylipTreeImplTest extends TestCase {
 		
 		assertEquals(4, edgeList.size());
 		
+		// Test the number of edges the first parent node has
+		 nodeList = treeParser2.getNodeList();
+		
+		 edgeList = treeParser2.getEdges(nodeList.get(4));
+		
+		assertEquals(4, edgeList.size());
 	
 		
 	}
@@ -58,6 +80,24 @@ public class PhylipTreeImplTest extends TestCase {
 		assertEquals(7.0, treeParser.getEdgeAttribute(edgeList.get(1)).get(0));
 		assertEquals(8.0, treeParser.getEdgeAttribute(edgeList.get(2)).get(0));
 		assertEquals(0.0, treeParser.getEdgeAttribute(edgeList.get(3)).get(0));
+
+		 nodeList = treeParser2.getNodeList();
+		 edgeList = treeParser2.getEdges(nodeList.get(0));
+		
+		// Test edge lengths of the first node
+		assertEquals(6.0, treeParser.getEdgeAttribute(edgeList.get(0)).get(0));
+		
+		// Test edge lengths of the first parent node
+		edgeList = treeParser.getEdges(nodeList.get(4));
+		
+		assertEquals(6.0, treeParser.getEdgeAttribute(edgeList.get(0)).get(0));
+		assertEquals(7.0, treeParser.getEdgeAttribute(edgeList.get(1)).get(0));
+		assertEquals(8.0, treeParser.getEdgeAttribute(edgeList.get(2)).get(0));
+		assertEquals(0.0, treeParser.getEdgeAttribute(edgeList.get(3)).get(0));
+
+
+
+
 		
 	}
 
