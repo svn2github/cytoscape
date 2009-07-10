999560e02ddc593a00dc3c1370b87541d1848ef1
Srinivasarao <Srinivasarao@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-10 18:35:32 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@17343 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/srinivasarao/search-api/src/test/java/org/cytoscape/search/AbstractEnhancedSearchIndexTest.java
+++ csplugins/trunk/soc/srinivasarao/search-api/src/test/java/org/cytoscape/search/AbstractEnhancedSearchIndexTest.java
@@ -6,9 +6,7 @@ import org.apache.lucene.store.RAMDirectory;
 
 public abstract class AbstractEnhancedSearchIndexTest extends TestCase{
 	EnhancedSearchIndex esi = null;
-	private AbstractEnhancedSearchIndexTest(EnhancedSearchIndex si){
-			esi = si;
-	}
+	
 	public void testgetIndex(){
 		RAMDirectory rd = esi.getIndex();
 		assertTrue(rd instanceof RAMDirectory);
--- csplugins/trunk/soc/srinivasarao/search-api/src/test/java/org/cytoscape/search/AbstractEnhancedSearchTest.java
+++ csplugins/trunk/soc/srinivasarao/search-api/src/test/java/org/cytoscape/search/AbstractEnhancedSearchTest.java
@@ -9,41 +9,39 @@ import org.cytoscape.model.CyNode;
 import org.cytoscape.model.internal.ArrayGraph;
 
 public abstract class AbstractEnhancedSearchTest extends TestCase {
-		private EnhancedSearch es;
-		private CyNetwork net1 = null;
-		protected AbstractEnhancedSearchTest(EnhancedSearch ens){
-			this.es = ens;
-		}
-		protected void defaultSetup(){
-			net1 = new ArrayGraph(new DummyCyEventHelper());
-			CyNode n1 = net1.addNode();
-            CyNode n2 = net1.addNode();
-		}
-		public void testaddNetwork(){
-			if(net1==null)
-				defaultSetup();
-            es.addNetwork(net1);
-            RAMDirectory rd = es.getNetworkIndex(net1);
-            assertTrue(rd instanceof RAMDirectory);
-        }
-		public void testremoveNetwork(){
-			testaddNetwork();
-			es.removeNetworkIndex(net1);
-			RAMDirectory rd = es.getNetworkIndex(net1);
-            assertNull(rd);
-		}
-		public void testgetNetworkIndex(){
-			if(net1==null)
-				defaultSetup();
-			es.addNetwork(net1);
-			RAMDirectory rd = es.getNetworkIndex(net1);
-            assertTrue(rd instanceof RAMDirectory);
-		}
-		public void testgetNetworkIndexStatus(){
-			if(net1==null)
-				defaultSetup();
-			es.addNetwork(net1);
-			String str = es.getNetworkIndexStatus(net1);
-			assertEquals(str,EnhancedSearch.INDEX_SET);
-		}
+	private EnhancedSearch es;
+	private CyNetwork net1 = null;
+	
+	protected void defaultSetup(){
+		net1 = new ArrayGraph(new DummyCyEventHelper());
+		CyNode n1 = net1.addNode();
+		CyNode n2 = net1.addNode();
+	}
+	public void testaddNetwork(){
+		if(net1==null)
+			defaultSetup();
+		es.addNetwork(net1);
+		RAMDirectory rd = es.getNetworkIndex(net1);
+		assertTrue(rd instanceof RAMDirectory);
+	}
+	public void testremoveNetwork(){
+		testaddNetwork();
+		es.removeNetworkIndex(net1);
+		RAMDirectory rd = es.getNetworkIndex(net1);
+		assertNull(rd);
+	}
+	public void testgetNetworkIndex(){
+		if(net1==null)
+			defaultSetup();
+		es.addNetwork(net1);
+		RAMDirectory rd = es.getNetworkIndex(net1);
+		assertTrue(rd instanceof RAMDirectory);
+	}
+	public void testgetNetworkIndexStatus(){
+		if(net1==null)
+			defaultSetup();
+		es.addNetwork(net1);
+		String str = es.getNetworkIndexStatus(net1);
+		assertEquals(str,EnhancedSearch.INDEX_SET);
+	}
 }
