d7ceb8f714d429a1785ee1edf81367793414d1f2
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-14 23:02:26 +0000

    Correct printed message
    git-svn-id: http://chianti.ucsd.edu/svn@17404 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/cyprovision/org.cytoscape.product/src/org/cytoscape/product/Application.java
+++ csplugins/trunk/ucsd/pwang/cyprovision/org.cytoscape.product/src/org/cytoscape/product/Application.java
@@ -18,10 +18,10 @@ public class Application implements IApplication {
 	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
 	 */
 	public Object start(IApplicationContext context) {
-		System.out.println("myequinoxappstub.start()...");
+		System.out.println("org.cytoscape.product.start()...");
 		java.text.DateFormat dateFormat = new java.text.SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         java.util.Date date = new java.util.Date();
-        System.out.println("Current Date Time : " + dateFormat.format(date));
+        System.out.println(" at : " + dateFormat.format(date));
 
 		// Start Cytoscape.application bundle here ??? 
 		//????
@@ -43,6 +43,6 @@ public class Application implements IApplication {
 	 * @see org.eclipse.equinox.app.IApplication#stop()
 	 */
 	public void stop() {
-		//System.out.println("myequinoxappstub.stop()...");
+		System.out.println("org.cytoscape.product.stop()...");
 	}
 }
