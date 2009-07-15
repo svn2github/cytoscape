1bcdb15b27e3247ad01c83127bf918f7936f9930
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-15 05:41:38 +0000

    added custom cell renderer class (refs ticket:18)
    git-svn-id: http://chianti.ucsd.edu/svn@17408 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/ReorderableJList.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/ReorderableJList.java
@@ -1,20 +1,49 @@
 package org.cytoscape.layer.internal.ui;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.dnd.DragGestureEvent;
 import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSource;
 import java.awt.dnd.DragSourceDragEvent;
 import java.awt.dnd.DragSourceDropEvent;
 import java.awt.dnd.DragSourceEvent;
 import java.awt.dnd.DragSourceListener;
+import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
 
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
 
 public class ReorderableJList extends JList implements DragSourceListener,
 		DropTargetListener, DragGestureListener {
+
+	static DataFlavor localObjectFlavor;
+	static {
+		try {
+			localObjectFlavor = new DataFlavor(
+					DataFlavor.javaJVMLocalObjectMimeType);
+		} catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+		}
+	}
+	static DataFlavor[] supportedFlavors = { localObjectFlavor };
+	DragSource dragSource;
+	DropTarget dropTarget;
+	Object dropTargetCell;
+	int draggedIndex = -1;
+
+	public ReorderableJList() {
+		super();
+		setCellRenderer(new ReorderableListCellRenderer());
+
+	}
 
 	public void dragDropEnd(DragSourceDropEvent dsde) {
 		// TODO Auto-generated method stub
@@ -52,8 +81,17 @@ public class ReorderableJList extends JList implements DragSourceListener,
 	}
 
 	public void dragOver(DropTargetDragEvent dtde) {
-		// TODO Auto-generated method stub
-
+		if (dtde.getSource() != dropTarget) {
+			dtde.rejectDrag();
+		}
+		Point dragPoint = dtde.getLocation();
+		int index = locationToIndex(dragPoint);
+		if (index == -1) {
+			dropTargetCell = null;
+		} else {
+			dropTargetCell = getModel().getElementAt(index);
+		}
+		repaint();
 	}
 
 	public void drop(DropTargetDropEvent dtde) {
@@ -69,6 +107,32 @@ public class ReorderableJList extends JList implements DragSourceListener,
 	public void dragGestureRecognized(DragGestureEvent dge) {
 		// TODO Auto-generated method stub
 
+	}
+
+	class ReorderableListCellRenderer extends DefaultListCellRenderer {
+		boolean isTargetCell;
+		boolean isLastItem;
+
+		public ReorderableListCellRenderer() {
+			super();
+		}
+
+		public Component getListCellRendererComponent(JList list, Object value,
+				int index, boolean isSelected, boolean hasFocus) {
+			isTargetCell = (value == dropTargetCell);
+			isLastItem = (index == list.getModel().getSize() - 1);
+			boolean showSelected = isSelected & (dropTargetCell == null);
+			return super.getListCellRendererComponent(list, value, index,
+					showSelected, hasFocus);
+		}
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			if (isTargetCell) {
+				g.setColor(Color.black);
+				g.drawLine(0, 0, getSize().width, 0);
+			}
+		}
 	}
 
 }
