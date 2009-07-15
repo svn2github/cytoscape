da2a6c438104e17b7db3da1e32f73f4e2821275f
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-15 04:20:45 +0000

    ASSIGNED - # 18: Implement ReorderableJList in network builder dialog http://cg06.naist.jp/gsoc/ticket/18 just committed class template
    git-svn-id: http://chianti.ucsd.edu/svn@17407 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/ReorderableJList.java
@@ -0,0 +1,74 @@
+package org.cytoscape.layer.internal.ui;
+
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSourceDragEvent;
+import java.awt.dnd.DragSourceDropEvent;
+import java.awt.dnd.DragSourceEvent;
+import java.awt.dnd.DragSourceListener;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+
+import javax.swing.JList;
+
+public class ReorderableJList extends JList implements DragSourceListener,
+		DropTargetListener, DragGestureListener {
+
+	public void dragDropEnd(DragSourceDropEvent dsde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dragEnter(DragSourceDragEvent dsde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dragExit(DragSourceEvent dse) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dragOver(DragSourceDragEvent dsde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dropActionChanged(DragSourceDragEvent dsde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dragEnter(DropTargetDragEvent dtde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dragExit(DropTargetEvent dte) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dragOver(DropTargetDragEvent dtde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void drop(DropTargetDropEvent dtde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dropActionChanged(DropTargetDragEvent dtde) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dragGestureRecognized(DragGestureEvent dge) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
