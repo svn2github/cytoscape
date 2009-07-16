24fb27c4691c2e7b83940258e647ef0022fc115d
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-16 22:58:53 +0000

    changed !ComboCellRenderer spec(refs ticket:17)
    git-svn-id: http://chianti.ucsd.edu/svn@17445 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/ComboCellRenderer.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/ComboCellRenderer.java
@@ -3,36 +3,47 @@ package org.cytoscape.layer.internal.ui;
 import java.awt.Component;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.ListModel;
 import javax.swing.table.TableCellRenderer;
 
 public class ComboCellRenderer extends JComboBox implements TableCellRenderer {
-	private final JTextField editor;
+	private DefaultComboBoxModel model;
 
-	public ComboCellRenderer() {
+	public ComboCellRenderer(JComboBox base) {
 		super();
-		setEditable(true);
-		setBorder(BorderFactory.createEmptyBorder());
+		this.initialize(base.getModel());
+	}
 
-		editor = (JTextField) getEditor().getEditorComponent();
-		editor.setBorder(BorderFactory.createEmptyBorder());
-		editor.setOpaque(true);
+	public void initialize(ListModel listModel) {
+		Object[] items = new Object[listModel.getSize()];
+		for (int i = 0; i < items.length; i++) {
+			items[i] = listModel.getElementAt(i);
+		}
+		this.model = new DefaultComboBoxModel(items);
+		this.setModel(this.model);
+		this.setBorder(BorderFactory.createEmptyBorder());
 	}
 
 	public Component getTableCellRendererComponent(JTable table, Object value,
 			boolean isSelected, boolean hasFocus, int row, int column) {
 		removeAllItems();
 		if (isSelected) {
-			editor.setForeground(table.getSelectionForeground());
-			editor.setBackground(table.getSelectionBackground());
+			setForeground(table.getSelectionForeground());
+			super.setBackground(table.getSelectionBackground());
 		} else {
-			editor.setForeground(table.getForeground());
-			editor.setBackground(table.getBackground());
+			setForeground(table.getForeground());
+			setBackground(table.getBackground());
 		}
-		addItem((value == null) ? "" : value.toString());
+
+		int index = this.model.getIndexOf(value);
+		if (index != -1) {
+			this.setSelectedIndex(index);
+		} else {
+			this.setSelectedIndex(0);
+		}
 		return this;
 	}
-
 }
