a4b8f14df4a9d6da331b591059633301a946e7b7
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-21 22:17:40 +0000

    Presentation API had been refactored based on the discussion last week.
    git-svn-id: http://chianti.ucsd.edu/svn@17507 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/session-api/trunk/src/main/java/org/cytoscape/session/CyNetworkManager.java
+++ core3/session-api/trunk/src/main/java/org/cytoscape/session/CyNetworkManager.java
@@ -36,13 +36,12 @@
  */
 package org.cytoscape.session;
 
-import org.cytoscape.model.CyNetwork;
-
-import org.cytoscape.view.model.CyNetworkView;
-import org.cytoscape.view.presentation.Renderer;
-
 import java.util.List;
 import java.util.Set;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.presentation.RenderingEngine;
 
 
 /**
@@ -195,6 +194,6 @@ public interface CyNetworkManager {
 	 */
 	public void addNetworkView(CyNetworkView view);
 
-	public Renderer<CyNetworkView> getCurrentPresentation();
-	public void setCurrentPresentation(Renderer<CyNetworkView> renderer);
+	public RenderingEngine getCurrentPresentation();
+	public void setCurrentPresentation(RenderingEngine engine);
 }
