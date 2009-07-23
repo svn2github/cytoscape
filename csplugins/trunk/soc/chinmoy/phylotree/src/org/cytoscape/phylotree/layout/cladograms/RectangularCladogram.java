8107eabc662d0a2e8df56b65f17d2f1ffee6d368
chinmoy <chinmoy@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-23 05:34:51 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@17523 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/chinmoy/phylotree/src/org/cytoscape/phylotree/layout/cladograms/RectangularCladogram.java
@@ -0,0 +1,182 @@
+package org.cytoscape.phylotree.layout.cladograms;
+
+import org.cytoscape.phylotree.layout.CommonFunctions;
+import cytoscape.layout.LayoutProperties;
+import cytoscape.layout.AbstractLayout;
+
+
+import java.awt.GridLayout;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.JPanel;
+import giny.model.Node;
+import giny.model.Edge;
+
+public class RectangularCladogram extends AbstractLayout{
+	
+	static double LEAF_X = 500.0; // X Position where all leaves should be placed
+	static double INTERNODE_DISTANCE = 100.0;
+	
+	private LayoutProperties layoutProperties;
+	
+	CommonFunctions commonFunctions = new CommonFunctions();
+	
+	private int numLeavesVisited = 0; //
+	
+	public RectangularCladogram()
+	{
+		super();
+		layoutProperties = new LayoutProperties(getName());
+		initialize_properties();		
+	}
+	
+	protected void initialize_properties()
+	{	
+		layoutProperties.initializeProperties();
+
+		updateSettings(true);
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 */
+	public void updateSettings() {
+		updateSettings(false);
+	}
+	
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param force DOCUMENT ME!
+	 */
+	public void updateSettings(boolean force) {
+		layoutProperties.updateValues();
+	}
+
+	// UI for getting settings
+	/**
+	 * Get the settings panel for this layout
+	 */
+	public JPanel getSettingsPanel() {
+		JPanel panel = new JPanel(new GridLayout(0, 1));
+		panel.add(layoutProperties.getTunablePanel());
+
+		return panel;
+	}
+
+
+	/**
+	 *  DOCUMENT ME!
+	 */
+	public void revertSettings() {
+		layoutProperties.revertProperties();
+	}
+
+	public LayoutProperties getSettings() {
+		return layoutProperties;
+	}
+
+
+
+	/**
+	 * getName is used to construct property strings
+	 * for this layout.
+	 */
+	public  String getName() {
+		return "rectangular_cladogram";
+	}
+
+	/**
+	 * toString is used to get the user-visible name
+	 * of the layout
+	 */
+	public  String toString(){
+		return "Rectangular Cladogram Layout";
+	}
+
+
+	public void construct() {
+		taskMonitor.setStatus("Initializing");
+		initialize(); 
+
+
+		// Find the root of the tree
+		Node root = commonFunctions.getTreeRoot(network);
+
+		numLeavesVisited = 0;
+
+		// Obtain post order traversal of nodes starting from the root
+		List<Node> postOrderNodes = commonFunctions.postOrderTraverse(network, root);
+
+		// Position each node
+		Iterator<Node> it = postOrderNodes.iterator();
+		while(it.hasNext())
+		{
+			Node node = it.next();
+			
+			// If leaf position it accordingly
+			if(network.getAdjacentEdgeIndicesArray(node.getRootGraphIndex(), false, false, true).length == 0)
+				positionLeaf(node);
+			else
+				positionInternalNode(node);
+				
+				
+			
+		}
+		
+		// Bend each edge to make it look rectangular
+
+		List<Edge> allEdges = network.edgesList();
+		Iterator<Edge> edgesIterator = allEdges.iterator();
+
+		while(edgesIterator.hasNext())
+		{
+
+			Edge edge = edgesIterator.next();
+			
+			commonFunctions.addRectangularBends(network, networkView, edge);
+		}
+
+
+
+
+
+	}
+	
+	
+	/**
+	 * positionLeaf(Node)
+	 * Positions the leaves
+	 */
+	private void positionLeaf(Node node)
+	{
+		numLeavesVisited++;
+		networkView.getNodeView(node).setXPosition(LEAF_X,true);
+		networkView.getNodeView(node).setYPosition(INTERNODE_DISTANCE*numLeavesVisited, true);
+		
+	}
+	
+	/**
+	 * positionInternalNode(Node)
+	 * Positions the internal nodes
+	 */
+	private void positionInternalNode(Node node)
+	{
+		// Find the child horizontally nearest
+		// Get the leaves of the subtree rooted at node
+		List<Node> subtreeLeaves = commonFunctions.getLeaves(network, node);
+
+		// Find the vertical midpoint of the subtree rooted at node
+		double midpointY = commonFunctions.findSubtreeYMidPoint(networkView, subtreeLeaves);
+
+		// Set the positions
+		networkView.getNodeView(node).setYPosition(midpointY,true);
+		networkView.getNodeView(node).setXPosition(LEAF_X-((subtreeLeaves.size()-1))*INTERNODE_DISTANCE, true);
+
+
+	}
+
+	
+
+}
