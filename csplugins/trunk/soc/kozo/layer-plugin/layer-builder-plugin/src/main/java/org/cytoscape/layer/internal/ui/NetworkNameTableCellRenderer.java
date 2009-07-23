6870af3f4d8013fc037a1b3a147af3fd4d55335a
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-23 18:24:57 +0000

    wrote get network name part in !TableCellRenderer (refs ticket:17)
    git-svn-id: http://chianti.ucsd.edu/svn@17528 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/NetworkNameTableCellRenderer.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/ui/NetworkNameTableCellRenderer.java
@@ -9,29 +9,31 @@ import javax.swing.table.TableCellRenderer;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.session.CyNetworkManager;
 
-public class NetworkNameTableCellRenderer extends JLabel implements TableCellRenderer {
+public class NetworkNameTableCellRenderer extends JLabel implements
+		TableCellRenderer {
 
 	private CyNetworkManager manager;
-	
+	private static final String NETWORK_TITLE = "name";
+
 	public NetworkNameTableCellRenderer(CyNetworkManager manager) {
 		this.manager = manager;
 	}
-	
+
 	public Component getTableCellRendererComponent(JTable table, Object value,
 			boolean isSelected, boolean hasFocus, int row, int column) {
-		
+
 		String networkName = "?";
-		
-		if(value instanceof Long) {
-			CyNetwork network = manager.getNetwork((Long)value);
-		
+
+		if (value instanceof Long) {
+			CyNetwork network = manager.getNetwork((Long) value);
+
 			// Get title here!
-			
+			networkName = network.attrs().get(NETWORK_TITLE, String.class);
+
 			this.setText(networkName);
-			
-			
+
 		}
-		
+
 		return this;
 	}
 
