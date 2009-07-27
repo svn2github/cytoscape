c922685c772d767f259f6ff3b9b8f00044b18482
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-27 20:51:28 +0000

    fixed erroneous package name
    git-svn-id: http://chianti.ucsd.edu/svn@17583 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape3/trunk/application/src/main/java/cytoscape/util/swing/CyColorChooser.java
+++ cytoscape3/trunk/application/src/main/java/cytoscape/util/swing/CyColorChooser.java
@@ -34,7 +34,7 @@
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
-package cytoscape.util;
+package cytoscape.util.swing;
 
 import javax.swing.*;
 import java.awt.*;
