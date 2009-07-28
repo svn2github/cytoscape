cf9091c9318e76ebe587b4c63f1b143fa05cd626
chinmoy <chinmoy@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-07-28 03:51:05 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@17591 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/chinmoy/phylotree/src/org/cytoscape/phylotree/PhylotreePlugin.java
+++ csplugins/trunk/soc/chinmoy/phylotree/src/org/cytoscape/phylotree/PhylotreePlugin.java
@@ -1,9 +1,14 @@
 package org.cytoscape.phylotree;
 
+import javax.swing.JMenu;
+
 import cytoscape.Cytoscape;
+import cytoscape.view.CyMenus;
 import cytoscape.plugin.CytoscapePlugin;
 import cytoscape.util.CytoscapeAction;
 import cytoscape.layout.CyLayouts;
+import cytoscape.layout.ui.LayoutMenuManager;
+
 import org.cytoscape.phylotree.actions.PhyloTreeImportAction;
 
 import org.cytoscape.phylotree.layout.cladograms.RadialCladogram;
@@ -29,22 +34,27 @@ public class PhylotreePlugin extends CytoscapePlugin {
 		// (1) add an menuItem: File->Import->Phylogenetic Tree
 		PhyloTreeImportAction menuAction = new PhyloTreeImportAction(this);
 		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction) menuAction);
-
-		
 		
 		//(2) add another menu item: Layout->Phylotree layouts
 		
-
-		CyLayouts.addLayout(new RectangularCladogram(), "PhyloTree Layouts");
-		CyLayouts.addLayout(new SlantedCladogram(), "PhyloTree Layouts");
-		CyLayouts.addLayout(new RadialCladogram(), "PhyloTree Layouts");
-		CyLayouts.addLayout(new CircularCladogram(), "PhyloTree Layouts");
-
-		CyLayouts.addLayout(new RectangularPhylogram(), "PhyloTree Layouts");
-		CyLayouts.addLayout(new SlantedPhylogram(), "PhyloTree Layouts");
-		CyLayouts.addLayout(new RadialPhylogram(), "PhyloTree Layouts");
-		CyLayouts.addLayout(new CircularPhylogram(), "PhyloTree Layouts");
+		JMenu layoutMenu = Cytoscape.getDesktop().getCyMenus().getLayoutMenu();
+		JMenu phyloLayoutMenu = new JMenu("Phylotree Layouts");
+		layoutMenu.add(phyloLayoutMenu);
+		JMenu phylogramLayoutMenu = new JMenu("Phylogram Layouts");
+		JMenu cladogramLayoutMenu = new JMenu("Cladogram Layouts");
+		phyloLayoutMenu.add(phylogramLayoutMenu);		
+		phyloLayoutMenu.add(cladogramLayoutMenu);
 		
+//		CyLayouts.addLayout(new RectangularCladogram(), null);
+//      CyLayouts.addLayout(new SlantedCladogram(), "PhyloTree Layouts");
+//		CyLayouts.addLayout(new RadialCladogram(), "PhyloTree Layouts");
+//		CyLayouts.addLayout(new CircularCladogram(), "PhyloTree Layouts");
+//
+//		CyLayouts.addLayout(new RectangularPhylogram(), "PhyloTree Layouts");
+//		CyLayouts.addLayout(new SlantedPhylogram(), "PhyloTree Layouts");
+//		CyLayouts.addLayout(new RadialPhylogram(), "PhyloTree Layouts");
+//		CyLayouts.addLayout(new CircularPhylogram(), "PhyloTree Layouts");
+//		
 		
 	}
 	
