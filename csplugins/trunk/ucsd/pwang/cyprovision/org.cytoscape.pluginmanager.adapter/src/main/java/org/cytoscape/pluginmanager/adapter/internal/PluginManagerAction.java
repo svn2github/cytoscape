9da73c8bee7a83b1a277fe9b4ea0b22f3e70d132
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-08-05 22:16:09 +0000

    Original creation
    git-svn-id: http://chianti.ucsd.edu/svn@17699 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/pwang/cyprovision/org.cytoscape.pluginmanager.adapter/src/main/java/org/cytoscape/pluginmanager/adapter/internal/PluginManagerAction.java
@@ -0,0 +1,38 @@
+package org.cytoscape.pluginmanager.adapter.internal;
+
+import java.awt.event.ActionEvent;
+
+import org.cytoscape.session.CyNetworkManager;
+import cytoscape.view.CySwingApplication;
+import cytoscape.util.CytoscapeAction;
+import org.eclipse.equinox.internal.p2.ui.sdk.ProvSDKUIActivator;
+
+/**
+ * Internal implementation of our example Spring Bean
+ */
+public class PluginManagerAction extends CytoscapeAction
+{
+	
+	private final static long serialVersionUID = 12023398790248567L;
+	/**
+	 * Creates a new PluginManagerAction object.
+	 */
+	private CySwingApplication desktop;
+	private CyNetworkManager netmgr;
+	
+	public PluginManagerAction(CySwingApplication desktop, CyNetworkManager netmgr ) {
+		super("PluginManager",netmgr);
+		this.desktop = desktop;
+		this.netmgr = netmgr;
+		setPreferredMenu("Plugins");
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param e DOCUMENT ME!
+	 */
+	public void actionPerformed(ActionEvent e) {
+		ProvSDKUIActivator.getInstallNewSoftwareHandler(desktop,netmgr);
+	} 
+}
