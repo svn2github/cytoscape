8693f910a173eabd21b1e24de91e9e150b67b3c2
Srinivasarao <Srinivasarao@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-08-06 06:25:16 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@17710 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/srinivasarao/search-ui/src/main/test/org/cytoscape/search/internal/TestBooleanAttributePanel.java
@@ -0,0 +1,40 @@
+package org.cytoscape.search.internal;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+
+public class TestBooleanAttributePanel {
+
+	private JFrame jf = new JFrame();
+	private BooleanAttributePanel bp = null;
+
+	public TestBooleanAttributePanel() {
+		bp = new BooleanAttributePanel("Attribute 1");
+
+		Timer timer = new Timer();
+		timer.schedule(new RunTimerTask(), 5000);
+	}
+
+	public void createAndShowGUI() {
+		jf.add(bp);
+		jf.setTitle("Boolean Attribute Panel");
+		jf.setLocation(650, 130);
+		jf.setSize(300, 600);
+		jf.setVisible(true);
+	}
+
+	private class RunTimerTask extends TimerTask {
+		public final void run() {
+			bp.getCheckedValues();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestBooleanAttributePanel sp = new TestBooleanAttributePanel();
+		sp.createAndShowGUI();
+
+	}
+}
