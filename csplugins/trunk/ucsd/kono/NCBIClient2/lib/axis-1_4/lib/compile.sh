d5783ff383db859b6a31f3c665696e81fa339945
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-08-06 02:25:36 +0000

    Old libraries deleted.
    git-svn-id: http://chianti.ucsd.edu/svn@17706 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/kono/NCBIClient2/lib/axis-1_4/lib/compile.sh
+++
@@ -1,8 +0,0 @@
-AXIS_LIB=.
-export AXIS_LIB
-
-AXISCP=.:$AXIS_LIB/axis.jar:$AXIS_LIB/commons-discovery-0.2.jar:$AXIS_LIB/commons-logging-1.0.4.jar:$AXIS_LIB/jaxrpc.jar:$AXIS_LIB/saaj.jar:$AXIS_LIB/log4j-1.2.8.jar:$AXIS_LIB/wsdl4j-1.5.1.jar
-
-export AXISCP
-
-javac -J-ms20m -J-mx1000m -classpath $AXISCP /cellar/users/kono/workspace/WebServicePlugin/lib/axis-1_4/lib/uk/ac/ebi/www/ws/services/WSDbfetch/*.java
