90519eaa510786ba9a1a74d825479cdf8c7a4b9d
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-08-17 22:01:40 +0000

    removed comment out part (refs ticket:27)
    git-svn-id: http://chianti.ucsd.edu/svn@17862 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/BuildMultilayerNetworkTaskFactory.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/BuildMultilayerNetworkTaskFactory.java
@@ -5,17 +5,7 @@ import org.cytoscape.work.TaskFactory;
 
 public class BuildMultilayerNetworkTaskFactory implements TaskFactory {
 
-	// private CyNetworkManager manager;
-	// private MultilayerNetworkFactory mlFactory;
-	//
-	// public BuildMultilayerNetworkTaskFactory(CyNetworkManager manager,
-	// MultilayerNetworkFactory mlFactory) {
-	// this.manager=manager;
-	// this.mlFactory=mlFactory;
-	// }
-
 	public Task getTask() {
-		// return new BuildMultilayerNetworkTask(manager, mlFactory);
 		return null;
 	}
 
