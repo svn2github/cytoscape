4fa8f1730e4b76d8607be4a3391030c35bc35a18
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-08-17 22:09:41 +0000

    added javadoc (refs ticket:26)
    git-svn-id: http://chianti.ucsd.edu/svn@17864 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/DisplayMultilayerNetworkBuilderUITaskFactory.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/internal/tasks/DisplayMultilayerNetworkBuilderUITaskFactory.java
@@ -1,25 +1,36 @@
 package org.cytoscape.layer.internal.tasks;
 
+import org.cytoscape.layer.MultiLayerNetworkBuilder;
+import org.cytoscape.session.CyNetworkManager;
 import org.cytoscape.work.Task;
 import org.cytoscape.work.TaskFactory;
 
-import org.cytoscape.layer.MultiLayerNetworkBuilder;
-import org.cytoscape.session.CyNetworkManager;
 import cytoscape.view.CySwingApplication;
 
-public class DisplayMultilayerNetworkBuilderUITaskFactory implements TaskFactory {
-	
+/**
+ * 
+ * The factory class for DisplayMultiLayerNetworkBuilderUITask
+ * 
+ * @author kozo
+ * 
+ */
+public class DisplayMultilayerNetworkBuilderUITaskFactory implements
+		TaskFactory {
+
 	private CyNetworkManager netmgr;
 	private CySwingApplication desktop;
 	private MultiLayerNetworkBuilder builder;
 
-	public DisplayMultilayerNetworkBuilderUITaskFactory(CySwingApplication desktop, CyNetworkManager netmgr, MultiLayerNetworkBuilder builder) {
+	public DisplayMultilayerNetworkBuilderUITaskFactory(
+			CySwingApplication desktop, CyNetworkManager netmgr,
+			MultiLayerNetworkBuilder builder) {
 		this.netmgr = netmgr;
 		this.desktop = desktop;
 		this.builder = builder;
 	}
 
 	public Task getTask() {
-		return new DisplayMultilayerNetworkBuilderUITask(desktop, netmgr, builder);
+		return new DisplayMultilayerNetworkBuilderUITask(desktop, netmgr,
+				builder);
 	}
 }
