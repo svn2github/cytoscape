b3029ac23c55ce2ca4f61e553b6008c973968a42
kozo <kozo@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-08-17 21:10:25 +0000

    added javadoc (refs ticket:26)
    git-svn-id: http://chianti.ucsd.edu/svn@17857 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/MultiLayerNetworkBuilder.java
+++ csplugins/trunk/soc/kozo/layer-plugin/layer-builder-plugin/src/main/java/org/cytoscape/layer/MultiLayerNetworkBuilder.java
@@ -4,11 +4,30 @@ import java.util.List;
 
 import org.cytoscape.model.CyNetwork;
 
+/**
+ * 
+ * A MultiLayerNetworkBuilder is a interface that set multi layer networks and
+ * connector network to connect and build one all layer connected network.
+ */
+
 public interface MultiLayerNetworkBuilder {
-	
+
+	/**
+	 * set layer and connector networks to connect for 3D layer visualization.
+	 * 
+	 * @param layers
+	 *            list of CyNetwork
+	 * @param connectors
+	 *            list of CyNetwork
+	 */
 	public void setSourceNetworks(List<CyNetwork> layers,
 			List<CyNetwork> connectors);
 
+	/**
+	 * build one all layer connected network.
+	 * 
+	 * @return CyNetwork one network connected all layer networks
+	 */
 	public CyNetwork buildLayeredNetwork();
 
 }
