4da1224d9def0ad406cd3c24eb33a4afeb48b431
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-09-14 18:01:26 +0000

    Added io.write.graphics package and a dependency to presentation-api in pom.xml.
    git-svn-id: http://chianti.ucsd.edu/svn@17980 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/io-api/trunk/src/main/java/org/cytoscape/io/write/graphics/CyBitmapGraphicsWriterFactory.java
@@ -0,0 +1,10 @@
+package org.cytoscape.io.write.graphics;
+
+/**
+ * Provides instances of <code>CyBitmapGraphicsWriter</code>
+ * @author Pasteur
+ */
+public interface CyBitmapGraphicsWriterFactory
+{
+	public CyBitmapGraphicsWriter getWriter();
+}
