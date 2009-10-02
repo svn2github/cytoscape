84633300b382e0cc2db1930c1d759d99d7257bce
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-10-02 21:13:11 +0000

    creating branch to experiment with metaedges
    git-svn-id: http://chianti.ucsd.edu/svn@18123 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/model-api/branches/metaedge/src/main/java/org/cytoscape/model/CyNetwork.java
@@ -0,0 +1,253 @@
+
+/*
+ Copyright (c) 2008, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+package org.cytoscape.model;
+
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * CyNetwork is the primary class for algorithm writing.  All algorithms should take a 
+ * CyNetwork as input, and do their best to only use the API of CyNetwork.  CyNetwork
+ * provides a straightforward interface to a simple graph.  For an interface to a
+ * more complicated subgraph, see {@link org.cytoscape.model.subnetwork.CyRootNetwork} 
+ * and {@link org.cytoscape.model.subnetwork.CySubNetwork}, both
+ * of which inherit from CyNetwork.
+ *
+ * The CyNetwork interface provides most of the methods a plugin would need to create
+ * a graph, traverse it, get and set attributes, and inquire about the existence of
+ * nodes and edges.
+ */
+public interface CyNetwork extends Identifiable, GraphObject {
+	/**
+	 * The "USER" CyDataTable is created by default for CyNetworks, CyNodes, and
+	 * CyEdges.  Other CyDataTables may also be associated -- see {@link CyDataTable}
+	 * for more information.  The table should be referenced using this constant:
+	 * <code>CyNetwork.DEFAULT_ATTRS</code>.
+	 */
+	String DEFAULT_ATTRS = "USER";
+
+	/**
+	 * The "HIDDEN" CyDataTable is created by default for CyNetworks, CyNodes, and
+	 * CyEdges.  Other CyDataTables may also be associated -- see {@link CyDataTable}
+	 * for more information.
+	 */
+	String HIDDEN_ATTRS = "HIDDEN";
+
+	/**
+	 * This method is used to create and add a node to this network.
+	 *
+	 * @return the created node
+	 */
+	CyNode addNode();
+
+	/**
+	 * Remove a node from the network and delete the node (if it only exists in 
+	 * this network).  See {@link org.cytoscape.model.subnetwork.CyRootNetwork} 
+	 * for information about having the same node in two networks.
+	 *
+	 * @param node the node to be deleted
+	 * @return true if the node was successfully deleted
+	 */
+	boolean removeNode(CyNode node);
+
+	/**
+	 * This method is used to create and add an edge to this network.
+	 *
+	 * @param source the source (or start) of the edge
+	 * @param target the target (or end) of the edge
+	 * @param isDirected if 'true' this is a directed edge
+	 * @return the created edge
+	 */
+	CyEdge addEdge(CyNode source, CyNode target, boolean isDirected);
+
+	/**
+	 * Remove an edge from the network and delete the edge (if it only exists in
+	 * this network).  See {@link org.cytoscape.model.subnetwork.CyRootNetwork} 
+	 * for information about having the same edge in two networks.
+	 *
+	 * @param edge the edge to be deleted
+	 * @return true if the edge was successfully deleted
+	 */
+	boolean removeEdge(CyEdge edge);
+
+	/**
+	 * Return the number of nodes in this network.
+	 *
+	 * @return the number of nodes
+	 */
+	int getNodeCount();
+
+	/**
+	 * Return the number of edges in this network.
+	 *
+	 * @return the number of edges
+	 */
+	int getEdgeCount();
+
+	/**
+	 * Return a list of the nodes in this network.
+	 *
+	 * @return the node list
+	 */
+	List<CyNode> getNodeList();
+
+	/**
+	 * Return a list of the edges in this network.
+	 *
+	 * @return the edge list
+	 */
+	List<CyEdge> getEdgeList();
+
+	/**
+	 * Determine if this CyNetwork contains a particular node.
+	 *
+	 * @param node the node to check
+	 * @return true if this network contains the node
+	 */
+	boolean containsNode(CyNode node);
+
+	/**
+	 * Determine if this CyNetwork contains a particular edge.
+	 *
+	 * @param edge the edge to check
+	 * @return true if this network contains the edge
+	 */
+	boolean containsEdge(CyEdge edge);
+
+	/**
+	 * Determine if this CyNetwork contains an edge between
+	 * two nodes.  Note that if the edge is directed, the
+	 * source and targets must match.
+	 *
+	 * @param  from the source of the edge
+	 * @param  to the target of the edge
+	 * @return true if this network contains the edge
+	 */
+	boolean containsEdge(CyNode from, CyNode to);
+
+	/**
+	 * Return the CyNode that has the index.
+	 *
+	 * @param index the index of the CyNode to get
+	 * @return the associated CyNode or null if there is no
+	 * node with that index in this network.
+	 */
+	CyNode getNode(int index);
+
+	/**
+	 * Return the CyEdge that has the index.
+	 *
+	 * @param index the index of the CyEdge to get
+	 * @return the associated CyEdge or null if there is no
+	 * edge with that index in this network.
+	 */
+	CyEdge getEdge(int index);
+
+	/**
+	 * Get the list of nodes that neighbor this node where the
+	 * definition of "neighbor" is a node that is connected to this
+	 * node by the passed edgeType.  The {@link CyEdge.Type} enum is
+	 * used to determine whether the list includes undirected, directed,
+	 * incoming, or outgoing edges.
+	 *
+	 * @param node the node whose neighbors we're looking for
+	 * @param edgeType the directionality of the edges we're interested in
+	 * @return the list of nodes that neighbor this node
+	 */
+	List<CyNode> getNeighborList(CyNode node, CyEdge.Type edgeType);
+
+	/**
+	 * Get the list of edges that connect to this node. The {@link CyEdge.Type} enum is
+	 * used to determine whether the list includes undirected, directed,
+	 * incoming, or outgoing edges.
+	 *
+	 * @param node the node whose edges we're looking for
+	 * @param edgeType the directionality of the edges we're interested in
+	 * @return the list of edges that are adjacent to this one
+	 */
+	List<CyEdge> getAdjacentEdgeList(CyNode node, CyEdge.Type edgeType);
+
+	/**
+	 * Get the list of edges that connect two nodes.  The {@link CyEdge.Type} enum is
+	 * used to determine whether the list includes undirected, directed,
+	 * incoming, or outgoing edges.
+	 *
+	 * @param source the source node
+	 * @param target the target node
+	 * @param edgeType the directionality of the edges we're interested in
+	 * @return the list of edges that include source and target and directed edges.
+	 */
+	List<CyEdge> getConnectingEdgeList(CyNode source, CyNode target, CyEdge.Type edgeType);
+
+	/**
+	 * TODO: We should remove this!
+	 * 
+	 * Return the map of {@link CyDataTable}s for this network.  The map is indexed by the
+	 * name of the data table (e.g. "USER")
+	 *
+	 * @return the map of {@link CyDataTable}s for this network
+	 */
+	Map<String,CyDataTable> getNetworkCyDataTables();
+
+	/**
+	 * TODO: We should remove this!
+	 * 
+	 * Return the map of {@link CyDataTable}s for nodes in this network.  The map is indexed by the
+	 * name of the data table (e.g. "USER")
+	 *
+	 * @return the map of {@link CyDataTable}s for nodes in this network
+	 */
+	Map<String,CyDataTable> getNodeCyDataTables();
+
+	/**
+	 * TODO: We should remove this!
+	 * 
+	 * Return the map of {@link CyDataTable}s for edges in this network.  The map is indexed by the
+	 * name of the data table (e.g. "USER")
+	 *
+	 * @return the map of {@link CyDataTable}s for edges in this network
+	 */
+	Map<String,CyDataTable> getEdgeCyDataTables();
+	
+	/**
+	 * 
+	 * @param graphObjectType NODE, EDGE, or NETWORK
+	 * @return the map of {@link CyDataTable}s for the given network object.
+	 */
+	Map<String,CyDataTable> getCyDataTables(String graphObjectType);
+}
---
+++ core3/model-api/branches/metaedge/src/main/java/org/cytoscape/model/subnetwork/CyRootNetwork.java
@@ -0,0 +1,158 @@
+
+/*
+ Copyright (c) 2008, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+package org.cytoscape.model.subnetwork;
+
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+
+import java.util.List;
+
+
+/**
+ * CyRootNetwork is an interface for managing Cytoscape's 
+ * meta-network implementation.  While most applications (and users!)
+ * will treat each {@link CyNetwork} created within Cytoscape
+ * as an independent network, beginning with Cytoscape 3.0, 
+ * Cytoscape has provided a mechanism for implementing a more
+ * complex meta-network structure that can be used for a variety
+ * of other use cases, including implementing subnetworks
+ * and shared nodes between subnetworks.  Beyond the concepts
+ * and methods provided by {@link CyNetwork} a meta-network
+ * adds three new concepts:
+ * <ul><li>A <b>CyRootNetwork</b> is a {@link CyNetwork} that
+ * adds methods for maintaining the meta-network.
+ * All {@link CyNode}s and {@link CyEdge}s in all {@link CySubNetwork}s
+ * that are part of this CyRootNetwork, including all {@link CyMetaNode}s.
+ * <li>A {@link CySubNetwork} is a group of nodes and edges
+ * that are a sub-network of a {@link CyRootNetwork}.  The
+ * {@link CySubNetwork} can be represented within another {@link CySubNetwork}
+ * by a {@link CyMetaNode}.  A {@link CySubNetwork} may be thought
+ * of as a projection of the graph implemented by the <b>CyRootNetwork</b>.
+ * <li>A {@link CyMetaNode} is a {@link CyNode} that represents
+ * a {@link CySubNetwork} in a {@link CyNetwork}.
+ * </ul>
+ * These three additional interfaces can be used to create and
+ * maintain a complex graph structure.  In order to avoid requiring
+ * all plugins and other uses of this package to test for
+ * and support the presence of a meta-network, Cytoscape provides
+ * a {@link CyNetwork} that is a "flattened" version of the
+ * meta-network (essentially all {@link CyNode}s and {@link CyEdge}s
+ * except {@link CyMetaNode}s and their associated {@link CyEdge}s.
+ */
+public interface CyRootNetwork extends CyNetwork {
+
+	/**
+	 * Create a {@link CyMetaNode} and its associated {@link CySubNetwork}.
+	 * The {@link CySubNetwork} contains all of the nodes provided and all
+	 * of the edges connecting those nodes.  For all edges between those nodes
+	 * and other nodes not part of the subnetwork duplicate edges will be
+	 * created between the created {@link CyMetaNode} and the external nodes.
+	 *
+	 * @param sub  the {@link CySubNetwork} this metanode should be added to
+	 *
+	 * @return  The created {@link CyMetaNode}.
+	 */
+	CyMetaNode addMetaNode(CySubNetwork sub);
+
+	/**
+	 * Removes the metanode and the CySubNetwork, not the nodes and edges contained in the
+	 * subnetwork.
+	 *
+	 * @param node  the {@link CyMetaNode} that represents the {@link CySubNetwork} to remove.
+	 */
+	void removeMetaNode(CyMetaNode node);
+
+	/**
+	 * Will return A list of all {@link CyMetaNode}s contained in this root network.
+	 * @return A list of all {@link CyMetaNode}s contained in this root network.
+	 */
+	List<CyMetaNode> getMetaNodeList();
+
+
+	/**
+	 * Return the {@link CyNetwork} that represents the base 
+	 * or "flattened" {@link CyNetwork} that contains all nodes and edges 
+	 * <b>exclusive</b> of any {@link CyMetaNode}s  or {@link CyEdge}s 
+	 * connecting metanodes.  In the case of a {@link CyRootNetwork}
+	 * with no {@link CySubNetwork}s, this contains exactly the same nodes and edges
+	 * as if it were a normal {@link CyNetwork}. 
+	 *
+	 * @return the base network
+	 */
+	CySubNetwork getBaseNetwork();
+
+
+	/**
+	 * Create a subnetwork to be used independently or for creation
+	 * of a {@link CyMetaNode}. The network will include all edges
+	 * from the {@link CyRootNetwork} where both the source <b>AND</b>
+	 * target are contained in the specified list.
+	 *
+	 * @param nodes The nodes contained in this {@link CyRootNetwork}
+	 * that represent the nodes to be included in the {@link CySubNetwork}.
+	 * @return A {@link CySubNetwork} created from the specified nodes.
+	 */
+	CySubNetwork addSubNetwork(List<CyNode> nodes);
+
+	/**
+	 * Create a subnetwork to be used independently or for creation
+	 * of a {@link CyMetaNode}. The network will include only the
+	 * edges specified in the list.
+	 *
+	 * @param nodes The nodes contained in this {@link CyRootNetwork}
+	 * that represent the nodes to be included in the {@link CySubNetwork}.
+	 * @param edges The edges contained in this {@link CyRootNetwork}
+	 * <b>AND</b> that connect specified nodes. Any edges that do not
+	 * connect specified nodes will result in an {@link IllegalArgumentException}.
+	 * @return A {@link CySubNetwork} created from the specified nodes and edges.
+	 */
+	CySubNetwork addSubNetwork(List<CyNode> nodes, List<CyEdge> edges);
+
+	/**
+	 * Will remove the specified {@link CySubNetwork} from this root network.
+	 * @param sub The subnetwork to be removed from this root network.
+	 */
+	void removeSubNetwork(CySubNetwork sub);
+
+	/**
+	 * Return all of the subnetworks that are part of this meta-network.
+	 *
+	 * @return the list of {@link CySubNetwork}s.
+	 */
+	List<CySubNetwork> getSubNetworkList();
+}
