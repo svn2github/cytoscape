222a217fb926ae6b0b3182425aef834d8e8227ab
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-10-09 00:37:15 +0000

    Dependency to swing-layout removed.  Layout code refactored to use Java 6 API.
    git-svn-id: http://chianti.ucsd.edu/svn@18169 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/swing-util/trunk/src/main/java/org/cytoscape/util/swing/JStatusBar.java
+++ core3/swing-util/trunk/src/main/java/org/cytoscape/util/swing/JStatusBar.java
@@ -31,21 +31,25 @@
  You should have received a copy of the GNU Lesser General Public License
  along with this library; if not, write to the Free Software Foundation,
  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
-*/
+ */
 package org.cytoscape.util.swing;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.GroupLayout;
+import javax.swing.JPanel;
+import javax.swing.LayoutStyle;
 
 /**
  * Simple status bar with 3 fields.<br>
- *
+ * 
  * @author kono
- *
+ * 
  */
 public class JStatusBar extends JPanel {
 	private final static long serialVersionUID = 1202339875133611L;
+
 	/**
 	 * Creates a new JStatusBar object.
 	 */
@@ -54,27 +58,30 @@ public class JStatusBar extends JPanel {
 	}
 
 	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @param text DOCUMENT ME!
+	 * DOCUMENT ME!
+	 * 
+	 * @param text
+	 *            DOCUMENT ME!
 	 */
 	public void setLeftLabel(String text) {
 		leftLabel.setText(text);
 	}
 
 	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @param text DOCUMENT ME!
+	 * DOCUMENT ME!
+	 * 
+	 * @param text
+	 *            DOCUMENT ME!
 	 */
 	public void setCenterLabel(String text) {
 		centerLabel.setText(text);
 	}
 
 	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @param text DOCUMENT ME!
+	 * DOCUMENT ME!
+	 * 
+	 * @param text
+	 *            DOCUMENT ME!
 	 */
 	public void setRightLabel(String text) {
 		rightLabel.setText(text);
@@ -93,81 +100,73 @@ public class JStatusBar extends JPanel {
 		leftLabel.setFont(new Font("Sans-Serif", Font.PLAIN, 10));
 		leftLabel.setPreferredSize(new Dimension(20, 20));
 
-		org.jdesktop.layout.GroupLayout jPanel1Layout = new org.jdesktop.layout.GroupLayout(leftPanel);
+		GroupLayout jPanel1Layout = new GroupLayout(leftPanel);
 		leftPanel.setLayout(jPanel1Layout);
-		jPanel1Layout.setHorizontalGroup(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                                              .add(org.jdesktop.layout.GroupLayout.TRAILING,
-		                                                   jPanel1Layout.createSequentialGroup()
-		                                                                .addContainerGap()
-		                                                                .add(leftLabel,
-		                                                                     org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                                     300, Short.MAX_VALUE)
-		                                                                .addContainerGap()));
-		jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                                            .add(jPanel1Layout.createSequentialGroup()
-		                                                              .add(leftLabel)
-		                                                              .addContainerGap(2,
-		                                                                               Short.MAX_VALUE)));
+		jPanel1Layout.setHorizontalGroup(jPanel1Layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING).addGroup(
+				GroupLayout.Alignment.TRAILING,
+				jPanel1Layout.createSequentialGroup().addContainerGap()
+						.addComponent(leftLabel, GroupLayout.DEFAULT_SIZE, 300,
+								Short.MAX_VALUE).addContainerGap()));
+		jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING).addGroup(
+				jPanel1Layout.createSequentialGroup().addComponent(leftLabel)
+						.addContainerGap(2, Short.MAX_VALUE)));
 
 		centerPanel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 		centerLabel.setText("jLabel2");
 
-		org.jdesktop.layout.GroupLayout jPanel2Layout = new org.jdesktop.layout.GroupLayout(centerPanel);
+		GroupLayout jPanel2Layout = new GroupLayout(centerPanel);
 		centerPanel.setLayout(jPanel2Layout);
-		jPanel2Layout.setHorizontalGroup(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                                              .add(jPanel2Layout.createSequentialGroup()
-		                                                                .addContainerGap()
-		                                                                .add(centerLabel,
-		                                                                     org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                                     100, Short.MAX_VALUE)
-		                                                                .addContainerGap()));
-		jPanel2Layout.setVerticalGroup(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                                            .add(centerLabel,
-		                                                 org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                 17, Short.MAX_VALUE));
+		jPanel2Layout.setHorizontalGroup(jPanel2Layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING).addGroup(
+				jPanel2Layout.createSequentialGroup().addContainerGap()
+						.addComponent(centerLabel, GroupLayout.DEFAULT_SIZE,
+								100, Short.MAX_VALUE).addContainerGap()));
+		jPanel2Layout.setVerticalGroup(jPanel2Layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING).addComponent(centerLabel,
+				GroupLayout.DEFAULT_SIZE, 17, Short.MAX_VALUE));
 
 		rightPanel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 		rightLabel.setText("jLabel3");
 
-		org.jdesktop.layout.GroupLayout jPanel3Layout = new org.jdesktop.layout.GroupLayout(rightPanel);
+		GroupLayout jPanel3Layout = new GroupLayout(rightPanel);
 		rightPanel.setLayout(jPanel3Layout);
-		jPanel3Layout.setHorizontalGroup(jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                                              .add(jPanel3Layout.createSequentialGroup()
-		                                                                .addContainerGap()
-		                                                                .add(rightLabel,
-		                                                                     org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                                     130, Short.MAX_VALUE)
-		                                                                .addContainerGap()));
-		jPanel3Layout.setVerticalGroup(jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                                            .add(jPanel3Layout.createSequentialGroup()
-		                                                              .add(rightLabel)
-		                                                              .addContainerGap(2,
-		                                                                               Short.MAX_VALUE)));
+		jPanel3Layout.setHorizontalGroup(jPanel3Layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING).addGroup(
+				jPanel3Layout.createSequentialGroup().addContainerGap()
+						.addComponent(rightLabel, GroupLayout.DEFAULT_SIZE,
+								130, Short.MAX_VALUE).addContainerGap()));
+		jPanel3Layout.setVerticalGroup(jPanel3Layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING).addGroup(
+				jPanel3Layout.createSequentialGroup().addComponent(rightLabel)
+						.addContainerGap(2, Short.MAX_VALUE)));
 
-		org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
+		GroupLayout layout = new GroupLayout(this);
 		this.setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                                .add(layout.createSequentialGroup()
-		                                           .add(leftPanel,
-		                                                org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                Short.MAX_VALUE)
-		                                           .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-		                                           .add(centerPanel,
-		                                                org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                Short.MAX_VALUE)
-		                                           .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-		                                           .add(rightPanel,
-		                                                org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                                Short.MAX_VALUE)));
-		layout.setVerticalGroup(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-		                              .add(leftPanel, 0, 21, Short.MAX_VALUE)
-		                              .add(centerPanel,
-		                                   org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                   org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-		                                   Short.MAX_VALUE).add(rightPanel, 0, 21, Short.MAX_VALUE));
+		layout.setHorizontalGroup(layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING)
+				.addGroup(
+						layout.createSequentialGroup().addComponent(leftPanel,
+								GroupLayout.DEFAULT_SIZE,
+								GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addPreferredGap(
+										LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(centerPanel,
+										GroupLayout.DEFAULT_SIZE,
+										GroupLayout.DEFAULT_SIZE,
+										Short.MAX_VALUE).addPreferredGap(
+										LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(rightPanel,
+										GroupLayout.DEFAULT_SIZE,
+										GroupLayout.DEFAULT_SIZE,
+										Short.MAX_VALUE)));
+		layout.setVerticalGroup(layout.createParallelGroup(
+				GroupLayout.Alignment.LEADING).addComponent(leftPanel, 0, 21,
+				Short.MAX_VALUE).addComponent(centerPanel,
+				GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE,
+				Short.MAX_VALUE).addComponent(rightPanel, 0, 21,
+				Short.MAX_VALUE));
 	} // </editor-fold>
 
 	// Variables declaration - do not modify
