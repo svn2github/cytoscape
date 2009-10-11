0b58d6b5502773c05b736331258a4b9ccc2767f3
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-10-11 02:37:22 +0000

    Added node and edge commands
    git-svn-id: http://chianti.ucsd.edu/svn@18194 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsf/scooter/coreCommands/src/coreCommands/commands/EdgeCommand.java
+++ csplugins/trunk/ucsf/scooter/coreCommands/src/coreCommands/commands/EdgeCommand.java
@@ -32,6 +32,8 @@
  */
 package coreCommands.commands;
 
+import cytoscape.CyEdge;
+import cytoscape.CyNetwork;
 import cytoscape.Cytoscape;
 import cytoscape.command.CyCommand;
 import cytoscape.command.CyCommandException;
@@ -41,10 +43,14 @@ import cytoscape.layout.Tunable;
 import cytoscape.logger.CyLogger;
 import cytoscape.view.CyNetworkView;
 
+import java.io.File;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * XXX FIXME XXX Description 
@@ -56,7 +62,13 @@ public class EdgeCommand extends AbstractCommand {
 		settingsMap = new HashMap();
 		addSetting("import attributes", "file");
 		addSetting("export attributes", "file");
-		addSetting("export attributes", "attribute");
+		// addSetting("export attributes", "attribute");
+		addSetting("select", "edge");
+		addSetting("select", "edgeList");
+		addSetting("deselect", "edge");
+		addSetting("deselect", "edgeList");
+		addSetting("find", "expression");
+		addSetting("get selected", "network", "current");
 
 		// Handle table import????
 	}
@@ -72,6 +84,120 @@ public class EdgeCommand extends AbstractCommand {
 
 	public CyCommandResult execute(String subCommand, Map<String, String>args) throws CyCommandException { 
 		CyCommandResult result = new CyCommandResult();
+
+		// Import edge attributes from a file
+		if ("import attributes".equals(subCommand)) {
+			if (!haveKey(args, "file"))
+				throw new CyCommandException("edge: filename is required to import attributes");
+			String fileName = args.get("file");
+			try {
+				File file = new File(fileName);
+				Cytoscape.loadAttributes(new String[] { file.getAbsolutePath() },
+				                         new String[] {});
+				result.addMessage("edge: attributes imported from "+file.getAbsolutePath());
+			} catch (Exception e) {
+				throw new CyCommandException("edge: unable to import attributes: "+e.getMessage());
+			}
+
+		// Export edge attributes to a file
+		// } else if ("export attributes".equals(subCommand)) {
+
+		// Select some ndoes
+		} else if ("select".equals(subCommand)) {
+			CyNetwork net = getNetwork(args);
+			List<CyEdge> edgeList = getEdgeList(net, result, args);
+			if (edgeList == null)
+				throw new CyCommandException("edge: nothing to select");
+			net.setSelectedEdgeState(edgeList, true);
+			result.addMessage("edge: selected "+edgeList.size()+" edges");
+			if (net == Cytoscape.getCurrentNetwork()) {
+				Cytoscape.getCurrentNetworkView().updateView();
+			}
+
+		// de-select some ndoes
+		} else if ("deselect".equals(subCommand)) {
+			CyNetwork net = getNetwork(args);
+			try {
+				List<CyEdge> edgeList = getEdgeList(net, result, args);
+				net.setSelectedNodeState(edgeList, false);
+				result.addMessage("edge: deselected "+edgeList.size()+" edges");
+			} catch (CyCommandException e) {
+				// deselect everything
+				net.unselectAllEdges();
+				result.addMessage("edge: deselected all edges");
+			}
+			if (net == Cytoscape.getCurrentNetwork()) {
+				Cytoscape.getCurrentNetworkView().updateView();
+			}
+
+		// return the list of currently selected edges
+		} else if ("get selected".equals(subCommand)) {
+			CyNetwork net = getNetwork(args);
+			Set<CyEdge>edges = net.getSelectedEdges();
+			result.addMessage("edge: returned "+edges.size()+" selected edges");
+			result.addResult("edges", makeEdgeList(edges));
+
+		// find edges based on an expression
+		} else if ("find".equals(subCommand)) {
+		}
+
 		return result;
+	}
+
+	private CyNetwork getNetwork(Map<String, String> args) throws CyCommandException {
+		if (!haveKey(args,"network"))
+			return Cytoscape.getCurrentNetwork();
+
+		String netName = args.get("network");
+		CyNetwork net = Cytoscape.getNetwork(netName);
+		if (net == null)
+			throw new CyCommandException("edge: no such network "+netName);
+		return net;
+	}
+
+	private List<CyEdge> getEdgeList(CyNetwork net, CyCommandResult result, Map<String, String> args) 
+                                   throws CyCommandException {
+		if (args == null || args.size() == 0)
+			throw new CyCommandException("edge: either 'edge' or 'edgeList' argument is required");
+
+		List<CyEdge> retList = new ArrayList();
+		if (args.containsKey("edgelist")) {
+			String[] edges = args.get("edgelist").split(",");
+			for (int edgeIndex = 0; edgeIndex < edges.length; edgeIndex++) {
+				addEdge(net, edges[edgeIndex], retList, result);
+			}
+		} else if (args.containsKey("edge")) {
+			String edgeName = args.get("edge");
+			addEdge(net, edgeName, retList, result);
+		} else {
+			throw new CyCommandException("edge: either 'edge' or 'edgeList' argument is required");
+		}
+		return retList;
+	}
+
+	private String makeEdgeList(Collection<CyEdge>edges) {
+		String edgeList = "";
+		if (edges == null || edges.size() == 0)
+			return edgeList;
+
+		for (CyEdge edge: edges) {
+			edgeList += edge.getIdentifier()+",";
+		}
+		return edgeList.substring(0, edgeList.length()-1);
+	}
+
+	private void addEdge(CyNetwork net, String edgeName, List<CyEdge> list, CyCommandResult result) {
+		CyEdge edge = Cytoscape.getRootGraph().getEdge(edgeName);
+		if (edge == null) 
+			result.addError("edge: can't find edge "+edgeName);
+		else
+			list.add(edge);
+		return;
+	}
+
+	private boolean haveKey(Map<String,String>map, String key) {
+		if (map == null || map.size() == 0 || !map.containsKey(key))
+			return false;
+		return true;
 	}
 }
--- csplugins/trunk/ucsf/scooter/coreCommands/src/coreCommands/commands/NodeCommand.java
+++ csplugins/trunk/ucsf/scooter/coreCommands/src/coreCommands/commands/NodeCommand.java
@@ -32,6 +32,8 @@
  */
 package coreCommands.commands;
 
+import cytoscape.CyNetwork;
+import cytoscape.CyNode;
 import cytoscape.Cytoscape;
 import cytoscape.command.CyCommand;
 import cytoscape.command.CyCommandException;
@@ -41,10 +43,13 @@ import cytoscape.layout.Tunable;
 import cytoscape.logger.CyLogger;
 import cytoscape.view.CyNetworkView;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * XXX FIXME XXX Description 
@@ -56,7 +61,13 @@ public class NodeCommand extends AbstractCommand {
 		settingsMap = new HashMap();
 		addSetting("import attributes", "file");
 		addSetting("export attributes", "file");
-		addSetting("export attributes", "attribute");
+		// addSetting("export attributes", "attribute");
+		addSetting("select", "node");
+		addSetting("select", "nodeList");
+		addSetting("deselect", "node");
+		addSetting("deselect", "nodeList");
+		addSetting("find", "expression");
+		addSetting("get selected", "network", "current");
 
 		// Handle table import????
 	}
@@ -72,6 +83,121 @@ public class NodeCommand extends AbstractCommand {
 
 	public CyCommandResult execute(String subCommand, Map<String, String>args) throws CyCommandException { 
 		CyCommandResult result = new CyCommandResult();
+
+		// Import node attributes from a file
+		if ("import attributes".equals(subCommand)) {
+			if (!haveKey(args, "file"))
+				throw new CyCommandException("node: filename is required to import attributes");
+			String fileName = args.get("file");
+			try {
+				File file = new File(fileName);
+				Cytoscape.loadAttributes(new String[] { file.getAbsolutePath() },
+				                         new String[] {});
+				result.addMessage("node: attributes imported from "+file.getAbsolutePath());
+			} catch (Exception e) {
+				throw new CyCommandException("node: unable to import attributes: "+e.getMessage());
+			}
+
+		// Export node attributes to a file
+		// } else if ("export attributes".equals(subCommand)) {
+
+		// Select some ndoes
+		} else if ("select".equals(subCommand)) {
+			CyNetwork net = getNetwork(args);
+			List<CyNode> nodeList = getNodeList(net, result, args);
+			if (nodeList == null)
+				throw new CyCommandException("node: nothing to select");
+			net.setSelectedNodeState(nodeList, true);
+			result.addMessage("node: selected "+nodeList.size()+" nodes");
+			if (net == Cytoscape.getCurrentNetwork()) {
+				Cytoscape.getCurrentNetworkView().updateView();
+			}
+
+		// de-select some ndoes
+		} else if ("deselect".equals(subCommand)) {
+			CyNetwork net = getNetwork(args);
+			try {
+				List<CyNode> nodeList = getNodeList(net, result, args);
+				net.setSelectedNodeState(nodeList, false);
+				result.addMessage("node: deselected "+nodeList.size()+" nodes");
+			} catch (CyCommandException e) {
+				// deselect everything
+				net.unselectAllNodes();
+				result.addMessage("node: deselected all nodes");
+			} 
+
+			if (net == Cytoscape.getCurrentNetwork()) {
+				Cytoscape.getCurrentNetworkView().updateView();
+			}
+
+		// return the list of currently selected nodes
+		} else if ("get selected".equals(subCommand)) {
+			CyNetwork net = getNetwork(args);
+			Set<CyNode>nodes = net.getSelectedNodes();
+			result.addMessage("node: returned "+nodes.size()+" selected nodes");
+			result.addResult("nodes", makeNodeList(nodes));
+
+		// find nodes based on an expression
+		} else if ("find".equals(subCommand)) {
+		}
+
 		return result;
+	}
+
+	private CyNetwork getNetwork(Map<String, String> args) throws CyCommandException {
+		if (!haveKey(args, "network"))
+			return Cytoscape.getCurrentNetwork();
+
+		String netName = args.get("network");
+		CyNetwork net = Cytoscape.getNetwork(netName);
+		if (net == null)
+			throw new CyCommandException("node: no such network "+netName);
+		return net;
+	}
+
+	private List<CyNode> getNodeList(CyNetwork net, CyCommandResult result, Map<String, String> args) 
+	                                 throws CyCommandException {
+		if (args == null || args.size() == 0)
+			throw new CyCommandException("node: either 'node' or 'nodeList' argument is required");
+
+		List<CyNode> retList = new ArrayList();
+		if (args.containsKey("nodelist")) {
+			String[] nodes = args.get("nodelist").split(",");
+			for (int nodeIndex = 0; nodeIndex < nodes.length; nodeIndex++) {
+				addNode(net, nodes[nodeIndex], retList, result);
+			}
+		} else if (args.containsKey("node")) {
+			String nodeName = args.get("node");
+			addNode(net, nodeName, retList, result);
+		} else {
+			throw new CyCommandException("node: either 'node' or 'nodeList' argument is required");
+		}
+		return retList;
+	}
+
+	private String makeNodeList(Collection<CyNode>nodes) {
+		String nodeList = "";
+		if (nodes == null || nodes.size() == 0)
+			return nodeList;
+
+		for (CyNode node: nodes) {
+			nodeList += node.getIdentifier()+",";
+		}
+		return nodeList.substring(0, nodeList.length()-1);
+	}
+
+	private void addNode(CyNetwork net, String nodeName, List<CyNode> list, CyCommandResult result) {
+		CyNode node = Cytoscape.getCyNode(nodeName, false);
+		if (node == null) 
+			result.addError("node: can't find node "+nodeName);
+		else
+			list.add(node);
+		return;
+	}
+
+	private boolean haveKey(Map<String,String>map, String key) {
+		if (map == null || map.size() == 0 || !map.containsKey(key))
+			return false;
+		return true;
 	}
 }
