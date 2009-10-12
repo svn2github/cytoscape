3dde9926ec3c834527cec9769ddfd84ef719480c
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-10-12 13:39:43 +0000

    Fixed import of CyNetworkView in CyNetworkViewReaderFactory
    git-svn-id: http://chianti.ucsd.edu/svn@18195 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/io-api/branches/samad/src/main/java/org/cytoscape/io/read/CyNetworkViewReaderFactory.java
+++ core3/io-api/branches/samad/src/main/java/org/cytoscape/io/read/CyNetworkViewReaderFactory.java
@@ -3,7 +3,7 @@ package org.cytoscape.io.read;
 import java.io.InputStream;
 import org.cytoscape.io.CyFileFilterable;
 import org.cytoscape.work.Task;
-import org.cytoscape.model.CyNetworkView;
+import org.cytoscape.view.model.CyNetworkView;
 
 public interface CyNetworkViewReaderFactory extends CyFileFilterable {
 
