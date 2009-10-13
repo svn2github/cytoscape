44976d6fd3910f060c4d18fa0f5de343bf88c5cf
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-10-13 16:02:31 +0000

    Added CyDataTableMapperTaskFactoryImpl
    git-svn-id: http://chianti.ucsd.edu/svn@18214 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/core-task-impl/branches/samad/src/main/java/org/cytoscape/task/internal/loaddatatable/CyDataTableMapperTaskFactoryImpl.java
@@ -0,0 +1,108 @@
+package org.cytoscape.task.internal.loaddatatable;
+
+import java.util.List;
+import java.util.Map;
+import org.cytoscape.task.CyDataTableMapperTaskFactory;
+import org.cytoscape.model.CyRow;
+import org.cytoscape.model.CyDataTable;
+import org.cytoscape.work.Task;
+import org.cytoscape.work.TaskMonitor;
+import org.cytoscape.work.TaskFactory;
+
+public class CyDataTableMapperTaskFactoryImpl implements CyDataTableMapperTaskFactory 
+{
+	CyDataTable source = null;
+	String sourceColumn = null;
+	List<Long> sourceRows = null;
+
+	CyDataTable target = null;
+	String targetColumn = null;
+	List<Long> targetRows = null;
+
+	public CyDataTableMapperTaskFactoryImpl()
+	{
+	}
+
+	public void setSource(CyDataTable source)
+	{
+		this.source = source;
+	}
+
+	public void setSourceColumn(String sourceColumn)
+	{
+		this.sourceColumn = sourceColumn;
+	}
+
+	public void setSourceRows(List<Long> sourceRows)
+	{
+		this.sourceRows = sourceRows;
+	}
+
+	public void setTarget(CyDataTable target)
+	{
+		this.target = target;
+	}
+	
+	public void setTargetColumn(String targetColumn)
+	{
+		this.targetColumn = targetColumn;
+	}
+
+	public void setTargetRows(List<Long> targetRows)
+	{
+		this.targetRows = targetRows;
+	}
+
+	public Task getTask()
+	{
+		return new Task()
+		{
+			boolean cancel = false;
+
+			public void run(TaskMonitor monitor)
+			{
+				// Init columns in target
+				monitor.setStatusMessage("Creating columns in target data table");
+				Map<String,Class<?>> sourceColumnTypes = source.getColumnTypeMap();
+				for (String sourceColumnName : sourceColumnTypes.keySet())
+				{
+					if (sourceColumnName.equals(sourceColumn))
+						continue;
+
+					target.createColumn(sourceColumnName, sourceColumnTypes.get(sourceColumnName), true);
+				}
+
+				// Copy contents
+				monitor.setStatusMessage("Mapping contents between tables");
+				for (int i = 0; i < sourceRows.size(); i++)
+				{
+					monitor.setProgress(i / ((double) sourceRows.size()));
+					CyRow sourceRow = source.getRow(sourceRows.get(i));
+					for (int j = 0; j < targetRows.size(); j++)
+					{
+						CyRow targetRow = target.getRow(targetRows.get(j));
+						processRow(sourceRow, targetRow);
+					}
+				}
+			}
+
+			public void cancel()
+			{
+				cancel = true;
+			}
+
+			private void processRow(CyRow sourceRow, CyRow targetRow)
+			{
+				if (!sourceRow.get(sourceColumn, Object.class).equals(targetRow.get(targetColumn, Object.class)))
+					return;
+				Map<String,Object> sourceValues = sourceRow.getAllValues();
+				for (String sourceColumnName : sourceValues.keySet())
+				{
+					if (sourceColumnName.equals(sourceColumn))
+						continue;
+					targetRow.set(sourceColumnName, sourceValues.get(sourceColumnName));
+				}
+			}
+		};
+	}
+}
