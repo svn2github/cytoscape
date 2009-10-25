4728337e2c7c0bdc29e17fba6ee937a25b68126e
slotia <slotia@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-10-25 15:33:09 +0000

    Updated CyDataTableMapperTaskFactory
    git-svn-id: http://chianti.ucsd.edu/svn@18297 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/core-task-api/trunk/src/main/java/org/cytoscape/task/CyDataTableMapperTaskFactory.java
+++ core3/core-task-api/trunk/src/main/java/org/cytoscape/task/CyDataTableMapperTaskFactory.java
@@ -1,6 +1,5 @@
 package org.cytoscape.task;
 
-import java.util.List;
 import org.cytoscape.model.CyDataTable;
 import org.cytoscape.work.TaskFactory;
 
@@ -8,8 +7,6 @@ public interface CyDataTableMapperTaskFactory  extends TaskFactory
 {
 	public void setSource(CyDataTable source);
 	public void setSourceColumn(String sourceColumn);
-	public void setSourceRows(List<Long> sourceRows);
 	public void setTarget(CyDataTable target);
 	public void setTargetColumn(String targetColumn);
-	public void setTargetRows(List<Long> targetRows);
 }
