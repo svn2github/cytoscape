d7e8afcac40bbc463a37e19af6636f0260865ce7
ajk <ajk@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-10-30 23:48:16 +0000

    working with dataflavors
    git-svn-id: http://chianti.ucsd.edu/svn@18339 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/editor-impl/trunk/src/main/java/org/cytoscape/editor/internal/DropListenerTask.java
+++ core3/editor-impl/trunk/src/main/java/org/cytoscape/editor/internal/DropListenerTask.java
@@ -1,6 +1,7 @@
 package org.cytoscape.editor.internal;
 
 import java.awt.Point;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 
 import org.cytoscape.task.AbstractNetworkViewTask;
@@ -35,7 +36,16 @@ public class DropListenerTask extends AbstractNetworkViewTask {
 		System.out.println ("DropListenerTask: transferrable = " + t + 
 				", location = " + pt);
 		
-		
+		DataFlavor[] dfl = t.getTransferDataFlavors();
+
+		for (DataFlavor d : dfl) {
+	
+			System.out.println("Item dropped of Mime Type: " + d.getMimeType());
+			System.out.println("Mime subtype is:  " + d.getSubType());
+			System.out.println("Mime class is: " + d.getRepresentationClass());
+
+			Class mimeClass = d.getRepresentationClass();
+		}
 	}
 
 
