a8101effa83ea4a41696f2d033f7a2e00dda66f2
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-11-06 20:56:49 +0000

    More
    git-svn-id: http://chianti.ucsd.edu/svn@18386 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-swing-impl/trunk/src/main/java/org/cytoscape/work/internal/tunables/GuiTunableInterceptor.java
+++ core3/work-swing-impl/trunk/src/main/java/org/cytoscape/work/internal/tunables/GuiTunableInterceptor.java
@@ -36,7 +36,6 @@ import org.cytoscape.work.spring.SpringTunableInterceptor;
  * </pre></p>
  * 
  * @author pasteur
- *
  */
 public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> {
 
@@ -64,12 +63,12 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 	 * @param parent component for the <code>Guihandlers</code>'s panels
 	 */
 	public void setParent(Object o) {
-		if(o instanceof JPanel)
+		if (o instanceof JPanel)
 			this.parent = (Component)o;
-		else throw new IllegalArgumentException("Not a JPanel");
+		else
+			throw new IllegalArgumentException("Not a JPanel");
 	}
 	
-
 	
 	//get the value(Handle) of the Tunable if its JPanel is enabled(Dependency) and check if we have to validate the values of tunables
 	/**
@@ -85,11 +84,11 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 	 * </pre></p>
 	 * @return success or not of the <code>TunableValidator</code> validate method
 	 */
-	public boolean handle(){
-		for(Guihandler h: lh)h.handleDependents();
+	public boolean handle() {
+		for (Guihandler h : lh)
+			h.handleDependents();
 		return validateTunableInput();
 	}
-
 	
 	
 	//Create the GUI
@@ -102,17 +101,17 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 	 * 
 	 * @return if new values has been successfully set
 	 */
-	public boolean createUI(Object... proxyObjs){
-		this.objs = convertSpringProxyObjs( proxyObjs );
+	public boolean createUI(Object... proxyObjs) {
+		this.objs = convertSpringProxyObjs(proxyObjs);
 		lh = new ArrayList<Guihandler>();
-		for ( Object o : objs ) {
-			if ( !handlerMap.containsKey( o ) )
+		for (Object o : objs) {
+			if (!handlerMap.containsKey(o))
 				throw new IllegalArgumentException("No Tunables exist for Object yet!");
-			lh.addAll( handlerMap.get(o).values() );
+			lh.addAll(handlerMap.get(o).values());
 		}
 
-		if ( lh.size() <= 0 ){
-			if(parent != null){
+		if (lh.size() <= 0) {
+			if (parent != null) {
 				((JPanel)parent).removeAll();
 				((JPanel)parent).repaint();
 				//parent = null;
@@ -120,77 +119,86 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 			return true;			
 		}
 
-		if ( !panelMap.containsKey( lh ) ) {
+		if (!panelMap.containsKey(lh)) {
 			final String MAIN = " ";
-			Map<String, JPanel> panels = new HashMap<String,JPanel>();
-			panels.put(MAIN,createJPanel(MAIN,null,null,Param.hidden));
-
+			Map<String, JPanel> panels = new HashMap<String, JPanel>();
+			panels.put(MAIN, createJPanel(MAIN,null,null,Param.hidden));
 			
 			// construct the GUI
 			for (Guihandler gh : lh) {
 				// hook up dependency listeners
 				String dep = gh.getDependency();
-				if ( dep != null && !dep.equals("") ) {
-					for ( Guihandler gh2 : lh ) {
-						if ( gh2.getName().equals(dep) ) {
+				if (dep != null && !dep.equals("")) {
+					for (Guihandler gh2 : lh) {
+						if (gh2.getName().equals(dep)) {
 							gh2.addDependent(gh);
 							break;
 						}
 					}
 				}
-
 				
-				//Get informations about the Groups and alignment from Tunables Annotations in order to create the proper GUI
-				Map<String,Param> groupAlignement = new HashMap<String,Param>();
+				// Get information about the Groups and alignment from Tunables Annotations in order to create the proper GUI
+				Map<String,Param> groupAlignment = new HashMap<String,Param>();
 				Map<String,Param> groupTitles = new HashMap<String,Param>();
 				
 				String[] group = gh.getTunable().group();
 				Param[] alignments = gh.getTunable().alignment();
 				Param[] titles = gh.getTunable().groupTitles();
 				
-				if(group.length==alignments.length)for(int i = 0; i < group.length; i++)groupAlignement.put(group[i], alignments[i]);
-				if(group.length>alignments.length){
-					for(int i = 0; i < alignments.length; i++)groupAlignement.put(group[i], alignments[i]);
-					for(int i=alignments.length;i<group.length;i++)groupAlignement.put(group[i], Param.vertical);
+				if (group.length <= alignments.length) {
+					for (int i = 0; i < group.length; i++)
+						groupAlignment.put(group[i], alignments[i]);
 				}
-				if(alignments.length>group.length)for(int i = 0; i < group.length; i++)groupAlignement.put(group[i], alignments[i]);
-				
-				if(group.length==titles.length)for(int i = 0; i < group.length; i++)groupTitles.put(group[i], titles[i]);
-				if(group.length>titles.length){
-					for(int i = 0; i < titles.length; i++)groupTitles.put(group[i], titles[i]);
-					for(int i=titles.length;i<group.length;i++)groupTitles.put(group[i], Param.displayed);
+				else {
+					for (int i = 0; i < alignments.length; i++)
+						groupAlignment.put(group[i], alignments[i]);
+
+					// Default alignment is "vertical."
+					for (int i = alignments.length; i < group.length; i++)
+						groupAlignment.put(group[i], Param.vertical);
 				}
-				if(titles.length>group.length)for(int i = 0; i < group.length; i++)groupTitles.put(group[i], titles[i]);
 				
-				
+				if (group.length <= titles.length) {
+					for (int i = 0; i < group.length; i++)
+						groupTitles.put(group[i], titles[i]);
+				}
+				else {
+					for (int i = 0; i < titles.length; i++)
+						groupTitles.put(group[i], titles[i]);
+
+					// Default group titles setting is "displayed."
+					for (int i = titles.length; i < group.length; i++)
+						groupTitles.put(group[i], Param.displayed);
+				}
 				
 				// find the proper group to put the handler panel in given the Alignment/Group parameters
 				String lastGroup = MAIN;
 				String groupNames = null;
-				for ( String g : group ) {
-					if(g.equals(""))throw new IllegalArgumentException("The group's name cannot be set to \"\"");
+				for (String g : group) {
+					if (g.equals(""))
+						throw new IllegalArgumentException("A group's name must not be \"\"!");
 					groupNames = groupNames + g;
-					if ( !panels.containsKey(groupNames) ) {
-						panels.put(groupNames,createJPanel(g,gh,groupAlignement.get(g),groupTitles.get(g)));						
-						panels.get(lastGroup).add( panels.get(groupNames), gh.getTunable().xorKey() );
+					if (!panels.containsKey(groupNames)) {
+						panels.put(groupNames, createJPanel(g, gh, groupAlignment.get(g), groupTitles.get(g)));						
+						panels.get(lastGroup).add(panels.get(groupNames), gh.getTunable().xorKey());
 					}
 					lastGroup = groupNames;
 				}
 				panels.get(lastGroup).add(gh.getJPanel());
 			}
-			panelMap.put(lh,panels.get(MAIN));
+			panelMap.put(lh, panels.get(MAIN));
 		}
 
-		// get the gui into the proper state
-		for ( Guihandler h : lh ) 
+		// Get the GUI into the proper state
+		for (Guihandler h : lh) 
 			h.notifyDependents();
 
 		//if no parent is defined, then create a new JDialog to display the Tunables' panels
-		if(parent == null){
+		if (parent == null) {
 			displayOptionPanel();
 			return newValuesSet;
 		}
-		else{//else add them to the "parent" JPanel
+		else { //else add them to the "parent" JPanel
 			((JPanel)parent).removeAll();
 			((JPanel)parent).add(panelMap.get(lh));
 			parent.repaint();
@@ -203,7 +211,7 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 	/**
 	 * Creation of a JPanel that will contain panels of <code>Guihandler</code>
 	 * This panel will have special features like, ability to collapse, ability to be displayed depending on another panel
-	 * A layout will be set for this <i>"container"</i> of panels(horizontally or vertically), and a title(displayed or not)
+	 * A layout will be set for this <i>"container"</i> of panels (horizontally or vertically), and a title (displayed or not)
 	 * 
 	 * 
 	 * @param title of the panel
@@ -213,34 +221,32 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 	 * 
 	 * @return a container for <code>Guihandler</code>' panels with special features if it is requested, or a simple one if not
 	 */
-	private JPanel createJPanel(String title, Guihandler gh,Param alignment,Param groupTitle) {
-		if ( gh == null ) 
-			return getSimplePanel(title,alignment,groupTitle);
+	private JPanel createJPanel(final String title, final Guihandler gh, final Param alignment, final Param groupTitle) {
+		if (gh == null) 
+			return getSimplePanel(title,alignment, groupTitle);
 
-		// See if we need to create a XOR panel
-		if ( gh.getTunable().xorChildren() ) {
-			JPanel p = new XorPanel(title,gh);
+		// See if we need to create an XOR panel
+		if (gh.getTunable().xorChildren()) {
+			JPanel p = new XorPanel(title, gh);
 			return p;
-		} else {
+		}
+		else {
 			// Figure out if the collapsable flag is set
-			for ( Param s : gh.getTunable().flag() ) {
-				if(s.equals(Param.collapsed)){
-					return new CollapsablePanel(title,false);
-				}
-				else if(s.equals(Param.uncollapsed)){
-					return new CollapsablePanel(title,true);
-				}
+			for (Param s : gh.getTunable().flag()) {
+				if (s.equals(Param.collapsed))
+					return new CollapsablePanel(title, false);
+				else if (s.equals(Param.uncollapsed))
+					return new CollapsablePanel(title, true);
 			}
 			// We're not collapsable, so return a normal jpanel
-			return getSimplePanel(title,alignment,groupTitle);
+			return getSimplePanel(title,alignment, groupTitle);
 		}
 	}
 	
 	
-	
 	/**
 	 * Creation of a JPanel that will contain panels of <code>Guihandler</code>
-	 * A layout will be set for this <i>"container"</i> of panels(horizontally or vertically), and a title(displayed or not)
+	 * A layout will be set for this <i>"container"</i> of panels (horizontally or vertically), and a title (displayed or not)
 	 * 
 	 * @param title of the panel
 	 * @param alignment the way the panels will be set in this <i>"container</i> panel 
@@ -248,14 +254,17 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 	 * 
 	 * @return a container for <code>Guihandler</code>' panels
 	 */
-	private JPanel getSimplePanel(String title,Param alignment,Param groupTitle) {
+	private JPanel getSimplePanel(final String title, final Param alignment, final Param groupTitle) {
 		JPanel outPanel = new JPanel();
 		TitledBorder titleborder = BorderFactory.createTitledBorder(title);
 		titleborder.setTitleColor(Color.BLUE);
 		
-		if(groupTitle == Param.displayed || groupTitle == null) outPanel.setBorder(titleborder);
-		if(alignment == Param.vertical || alignment == null) outPanel.setLayout(new BoxLayout(outPanel,BoxLayout.PAGE_AXIS));
-		else if(alignment == Param.horizontal)outPanel.setLayout(new BoxLayout(outPanel,BoxLayout.LINE_AXIS));
+		if (groupTitle == Param.displayed || groupTitle == null)
+			outPanel.setBorder(titleborder);
+		if (alignment == Param.vertical || alignment == null)
+			outPanel.setLayout(new BoxLayout(outPanel, BoxLayout.PAGE_AXIS));
+		else if (alignment == Param.horizontal)
+			outPanel.setLayout(new BoxLayout(outPanel, BoxLayout.LINE_AXIS));
 		return outPanel;
 	}
 
@@ -265,24 +274,24 @@ public class GuiTunableInterceptor extends SpringTunableInterceptor<Guihandler> 
 	 * 
 	 * Set the new <i>"value"</i> to <code>Tunable</code> object if the user clicked on <i>OK</i>, and if the validate method from <code>TunableValidator</code> interface succeeded
 	 */
-	private void displayOptionPanel(){
-		Object[] buttons = {"OK","Cancel"};
-		int n = JOptionPane.showOptionDialog(parent,
-		    panelMap.get(lh),
+	private void displayOptionPanel() {
+		Object[] buttons = {"OK", "Cancel"};
+		int n = JOptionPane.showOptionDialog(parent, panelMap.get(lh),
 		    "Set Parameters",
 		    JOptionPane.YES_NO_CANCEL_OPTION,
 		    JOptionPane.PLAIN_MESSAGE,
 		    null,
 		    buttons,
 		    buttons[0]);
-		if ( n == JOptionPane.OK_OPTION ){
-			for ( Guihandler h : lh )h.handleDependents();
+		if (n == JOptionPane.OK_OPTION) {
+			for (Guihandler h : lh)
+				h.handleDependents();
 			validateTunableInput();
 		}
-		else newValuesSet = false;
+		else
+			newValuesSet = false;
 	}
-	
-	
+
 	
 	/**
 	 * Check if the conditions set in validate method from <code>TunableValidator</code> are met
