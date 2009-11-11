1484ed57e29cfbb82419a9a5c688d60b02f6366a
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-11-11 00:30:14 +0000

    Reformat the output text
    git-svn-id: http://chianti.ucsd.edu/svn@18414 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/plugins/displayreleasenote.php
+++ cyto_web/trunk/plugins/displayreleasenote.php
@@ -5,6 +5,10 @@
 	<title>Cytoscape 2.x Plugins</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="/cyto_web/css/cytoscape.css">
 	<link rel="shortcut icon" href="/cyto_web/images/cyto.ico">
+	<STYLE type="text/css">
+  		DIV.mypars {text-align: left}
+    </STYLE>
+
 </head>
 <body bgcolor="#ffffff">
 
@@ -14,16 +18,47 @@
 
 <?php include "../nav.php"; ?>
 
+
 <?php
-$data = base64_decode(($_GET['data']));
-$data = unserialize($data);
+include 'clean.inc';
+$version_id = cleanInt($_GET['version_id']);
+
+include 'db.inc';
+
+// Connect to the MySQL DBMS
+if (!($connection = @ mysql_pconnect($dbServer, $dbUser, $dbPass)))
+    showerror();
+
+// Use the CyPluginDB database
+if (!mysql_select_db($dbName, $connection))
+   showerror();
+
+$query = "SELECT name, version, release_note FROM plugin_list, plugin_version where plugin_list.plugin_auto_id = plugin_version.plugin_id and version_auto_id = $version_id";
+
+// Run the query
+if (!($resultArray= @ mysql_query ($query, $connection)))
+   showerror();
+
+$row= @mysql_fetch_array($resultArray);
+$name=$row['name'];
+$version=$row['version'];
+
+$release_note=stripslashes($row['release_note']);
+
+$pattern = '/\n/';
+$replacement = '<br>';
+   
+$release_note = preg_replace($pattern, $replacement, $release_note);
+
+$release_note = '<br>'.$release_note;
 
 ?>
 <br>
-<div id="indent">
-	<p><strong>Name:</strong> <?php echo $data[0] ?></p>
-	<p><strong>Version: </strong><?php echo $data[1] ?></p>
-	<p><big><b>Release Note:</b></big><br><?php echo $data[2] ?></p>
+
+<div id="indent" class="mypars">
+	<p><strong>Plugin Name:</strong> <?php echo $name ?></p>
+	<p><strong>Version: </strong><?php echo $version ?></p>
+	<p><big><b>Release Note:</b></big><br><?php echo $release_note ?></p>
   <p>&nbsp;</p>
 </div>
 
