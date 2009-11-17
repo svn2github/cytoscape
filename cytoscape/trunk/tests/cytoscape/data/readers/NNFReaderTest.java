32c642df3966e4f0639f1b2bd8958c7c1efe664c
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-11-17 23:21:11 +0000

    Completed NNFReader tests.
    git-svn-id: http://chianti.ucsd.edu/svn@18453 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/tests/cytoscape/data/readers/NNFReaderTest.java
+++ cytoscape/trunk/tests/cytoscape/data/readers/NNFReaderTest.java
@@ -1,44 +1,101 @@
 package cytoscape.data.readers;
 
 import java.io.File;
-
 import junit.framework.TestCase;
+import java.io.IOException;
+
 
 public class NNFReaderTest extends TestCase {
-
 	private static final String FILE_LOCATION = "testData/NNFData/";
+
 	
 	public void setUp() throws Exception {
-		
+		/* Intentionally empty! */
 	}
+
 
 	public void tearDown() throws Exception {
-
+		/* Intentionally empty! */
 	}
+
 
 	public void testGood1() throws Exception {
 		final NNFReader reader = new NNFReader(FILE_LOCATION + "good1.nnf");
-		reader.read();
+		try {
+			reader.read();
+		}
+		catch (final IOException e) {
+			assertTrue(false); // We should never get here!
+		}
 	}
 	
 	
 	public void testGood2() {
-		assertTrue(true);
+		final NNFReader reader = new NNFReader(FILE_LOCATION + "good2.nnf");
+		try {
+			reader.read();
+		}
+		catch (final IOException e) {
+			assertTrue(false); // We should never get here!
+		}
 	}
 	
 	
 	public void testGood3() {
-		assertTrue(true);
+		final NNFReader reader = new NNFReader(FILE_LOCATION + "good3.nnf");
+		try {
+			reader.read();
+		}
+		catch (final IOException e) {
+			assertTrue(false); // We should never get here!
+		}
 	}
 	
 	
 	public void testGood4() {
-		assertTrue(true);
+		final NNFReader reader = new NNFReader(FILE_LOCATION + "good4.nnf");
+		try {
+			reader.read();
+		}
+		catch (final IOException e) {
+			assertTrue(false); // We should never get here!
+		}
 	}
 	
 	
 	public void testGood5() {
-		assertTrue(true);
+		final NNFReader reader = new NNFReader(FILE_LOCATION + "good5.nnf");
+		try {
+			reader.read();
+		}
+		catch (final IOException e) {
+			assertTrue(false); // We should never get here!
+		}
 	}
 	
+	
+	public void testBad1() {
+		final NNFReader reader = new NNFReader(FILE_LOCATION + "bad1.nnf");
+		try {
+			reader.read();
+		}
+		catch (final IOException e) {
+			return;
+		}
+
+		assertTrue(false); // We should never get here!
+	}
+	
+	
+	public void testBad2() {
+		final NNFReader reader = new NNFReader(FILE_LOCATION + "bad2.nnf");
+		try {
+			reader.read();
+		}
+		catch (final IOException e) {
+			return;
+		}
+
+		assertTrue(false); // We should never get here!
+	}
 }
