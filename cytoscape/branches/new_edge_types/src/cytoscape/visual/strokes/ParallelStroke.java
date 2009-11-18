c25bb44be5c69cac14b10eebd76a4df26e683738
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-11-18 16:53:28 +0000

    changed back to simple equals rather than rounded ends and advance overlap
    git-svn-id: http://chianti.ucsd.edu/svn@18467 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/branches/new_edge_types/src/cytoscape/visual/strokes/ParallelStroke.java
+++ cytoscape/branches/new_edge_types/src/cytoscape/visual/strokes/ParallelStroke.java
@@ -8,7 +8,7 @@ import java.awt.geom.GeneralPath;
 public class ParallelStroke extends ShapeStroke {
 
 	public ParallelStroke(float width, String name) {
-		super( new Shape[] { getParallelStroke(width) }, 0.75f, name, width );
+		super( new Shape[] { getParallelStroke(width) }, 1f, name, width );
 		this.name = name;
 		this.width = width;
 	}
@@ -20,23 +20,17 @@ public class ParallelStroke extends ShapeStroke {
 	static Shape getParallelStroke(final float width) {
 		GeneralPath shape = new GeneralPath();
 
-		// Instead of drawing a simple "=" sign with two parallel
-		// rectanges, I'm drawing rectangles with rounded short ends.
-		// This coupled the overlap specified in the advance above,
-		// means that when these lines are curved, they appear to 
-		// bend slightly more gracefully.
-
 		shape.moveTo(0f,-0.5f*width);
 		shape.lineTo(1f,-0.5*width);
-		shape.quadTo(1.5f, -0.75f*width, 1f,-1f*width);
+		shape.lineTo(1f,-1f*width);
 		shape.lineTo(0f,-1f*width);
-		shape.quadTo(-0.5f,-0.75*width,0f,-0.5f*width);
+		shape.lineTo(0f,-0.5f*width);
 
 		shape.moveTo(0f,0.5f*width);
 		shape.lineTo(1f,0.5*width);
-		shape.quadTo(1.5f, 0.75f, 1f,1f*width);
+		shape.lineTo(1f,1f*width);
 		shape.lineTo(0f,1f*width);
-		shape.quadTo(-0.5f, 0.75f*width, 0f,0.5f*width);
+		shape.lineTo(0f,0.5f*width);
 
 		return shape;
 	}
