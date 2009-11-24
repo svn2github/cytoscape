b279688c0e260410acb06a4d458229d1d3659e84
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-11-24 00:14:35 +0000

    Fixed broken test.
    git-svn-id: http://chianti.ucsd.edu/svn@18553 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/tests/cytoscape/util/NestedNetworkImageManagerTest.java
+++ cytoscape/trunk/tests/cytoscape/util/NestedNetworkImageManagerTest.java
@@ -1,36 +1,43 @@
 package cytoscape.util;
 
 import junit.framework.TestCase;
+import java.util.List;
 import cytoscape.Cytoscape;
+import cytoscape.CyNode;
 import cytoscape.data.readers.NNFReader;
 
+
 public class NestedNetworkImageManagerTest extends TestCase {
-	
 	private static final String FILE_LOCATION = "testData/NNFData/";
+
 
 	protected void setUp() throws Exception {
 		super.setUp();
+
 		// Instantiate Nested Network Image Manager singleton
 		NestedNetworkImageManager.getNetworkImageGenerator();
 	}
 
+
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
+
 	
 	public void testNestedNetworkImageManager() throws Exception {
 		TestUtil.destroyNetworksEdgesAndNodes();
+
 		// Load nested network
 		final NNFReader reader = new NNFReader(FILE_LOCATION + "good3.nnf");
 		reader.read();
 		
-		assertNotNull(Cytoscape.getCyNode("M1"));
+		final CyNode m1 = Cytoscape.getCyNode("M1");
+		assertNotNull(m1);
 		assertNotNull(Cytoscape.getCyNode("M2"));
 		assertNotNull(Cytoscape.getCyNode("M3"));
 		assertEquals(3, NestedNetworkImageManager.getNetworkImageGenerator().getImageCount());
-		
-		TestUtil.destroyNetworksEdgesAndNodes();
-		//assertEquals(0, NestedNetworkImageManager.getNetworkImageGenerator().getImageCount());	
-	}
 
+		m1.setNestedNetwork(null);
+		assertEquals(2, NestedNetworkImageManager.getNetworkImageGenerator().getImageCount());	
+	}
 }
