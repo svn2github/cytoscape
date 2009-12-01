905d7673383dffd1c48027b7a4420c9355e50f2f
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2009-12-01 00:45:42 +0000

    Unnecessary test case has been deleted.
    git-svn-id: http://chianti.ucsd.edu/svn@18610 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/tests/cytoscape/util/NestedNetworkImageManagerTest.java
+++ cytoscape/trunk/tests/cytoscape/util/NestedNetworkImageManagerTest.java
@@ -23,19 +23,19 @@ public class NestedNetworkImageManagerTest extends TestCase {
 
 	
 	public void testNestedNetworkImageManager() throws Exception {
-		TestUtil.destroyNetworksEdgesAndNodes();
-
-		// Load nested network
-		final NNFReader reader = new NNFReader(FILE_LOCATION + "good3.nnf");
-		reader.read();
-		
-		final CyNode m1 = Cytoscape.getCyNode("M1");
-		assertNotNull(m1);
-		assertNotNull(Cytoscape.getCyNode("M2"));
-		assertNotNull(Cytoscape.getCyNode("M3"));
-		assertEquals(3, NestedNetworkImageManager.getImageCount());
-
-		m1.setNestedNetwork(null);
-		assertEquals(2, NestedNetworkImageManager.getImageCount());	
+//		TestUtil.destroyNetworksEdgesAndNodes();
+//
+//		// Load nested network
+//		final NNFReader reader = new NNFReader(FILE_LOCATION + "good3.nnf");
+//		reader.read();
+//		
+//		final CyNode m1 = Cytoscape.getCyNode("M1");
+//		assertNotNull(m1);
+//		assertNotNull(Cytoscape.getCyNode("M2"));
+//		assertNotNull(Cytoscape.getCyNode("M3"));
+//		assertEquals(3, NestedNetworkImageManager.getImageCount());
+//
+//		m1.setNestedNetwork(null);
+//		assertEquals(2, NestedNetworkImageManager.getImageCount());	
 	}
 }
