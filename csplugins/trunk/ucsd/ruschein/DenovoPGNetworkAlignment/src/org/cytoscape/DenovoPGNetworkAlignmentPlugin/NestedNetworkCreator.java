da44c2e7aa8b8f3f51a70e33f540e6f11d9e069c
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-01-13 00:51:04 +0000

    Overview network generator works.  Need to add nested network for each node.
    git-svn-id: http://chianti.ucsd.edu/svn@18886 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/ruschein/DenovoPGNetworkAlignment/src/org/cytoscape/DenovoPGNetworkAlignmentPlugin/NestedNetworkCreator.java
+++ csplugins/trunk/ucsd/ruschein/DenovoPGNetworkAlignment/src/org/cytoscape/DenovoPGNetworkAlignmentPlugin/NestedNetworkCreator.java
@@ -1,14 +1,12 @@
 package org.cytoscape.DenovoPGNetworkAlignmentPlugin;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import networks.denovoPGNetworkAlignment.BFEdge;
 import networks.linkedNetworks.TypedLinkEdge;
 import networks.linkedNetworks.TypedLinkNetwork;
-import networks.linkedNetworks.TypedLinkNode;
 import networks.linkedNetworks.TypedLinkNodeModule;
 import cytoscape.CyEdge;
 import cytoscape.CyNetwork;
@@ -16,7 +14,8 @@ import cytoscape.CyNode;
 import cytoscape.Cytoscape;
 import cytoscape.data.CyAttributes;
 import cytoscape.data.Semantics;
-
+import cytoscape.layout.CyLayouts;
+import cytoscape.view.CyNetworkView;
 
 /**
  * Creates an overview network for the detected complexes and nested networks
@@ -30,50 +29,74 @@ class NestedNetworkCreator {
 	 * Instantiates an overview network of complexes (modules) and one nested
 	 * network for each node in the overview network.
 	 */
-	NestedNetworkCreator(final TypedLinkNetwork<TypedLinkNodeModule<String, BFEdge>, BFEdge> networkOfModules) {
+	NestedNetworkCreator(
+			final TypedLinkNetwork<TypedLinkNodeModule<String, BFEdge>, BFEdge> networkOfModules) {
 		moduleToCyNodeMap = new HashMap<TypedLinkNodeModule<String, BFEdge>, CyNode>();
 
-		overviewNetwork = Cytoscape.createNetwork(findNextAvailableNetworkName("Complex Search Results: " + new java.util.Date()),
-							  /* create_view = */true);
+		overviewNetwork = Cytoscape.createNetwork(
+				findNextAvailableNetworkName("Complex Search Results: "
+						+ new java.util.Date()),
+				/* create_view = */true);
 
 		final CyAttributes nodeAttribs = Cytoscape.getNodeAttributes();
 		final CyAttributes edgeAttribs = Cytoscape.getEdgeAttributes();
 
 		int nodeIndex = 1;
-		for (final TypedLinkEdge<TypedLinkNodeModule<String, BFEdge>, BFEdge> edge : networkOfModules.edges()) {
-			final TypedLinkNodeModule<String, BFEdge> sourceModule = edge.source().value();
+		for (final TypedLinkEdge<TypedLinkNodeModule<String, BFEdge>, BFEdge> edge : networkOfModules
+				.edges()) {
+			final TypedLinkNodeModule<String, BFEdge> sourceModule = edge
+					.source().value();
 			CyNode sourceNode = moduleToCyNodeMap.get(sourceModule);
 			if (sourceNode == null) {
-				final String nodeName = findNextAvailableNodeName("Complex" + nodeIndex);
-				sourceNode = Cytoscape.getCyNode(nodeName, /* create = */ true);
+				final String nodeName = findNextAvailableNodeName("Complex"
+						+ nodeIndex);
+				sourceNode = Cytoscape.getCyNode(nodeName, /* create = */true);
 				moduleToCyNodeMap.put(sourceModule, sourceNode);
 				overviewNetwork.addNode(sourceNode);
 				final Set<String> genes = sourceModule.getMemberValues();
-				nodeAttribs.setAttribute(sourceNode.getIdentifier(), "gene count", Integer.valueOf(genes.size()));
-				nodeAttribs.setAttribute(sourceNode.getIdentifier(), "score", Double.valueOf(sourceModule.score()));
+				nodeAttribs.setAttribute(sourceNode.getIdentifier(),
+						"gene count", Integer.valueOf(genes.size()));
+				nodeAttribs.setAttribute(sourceNode.getIdentifier(), "score",
+						Double.valueOf(sourceModule.score()));
 
 				++nodeIndex;
 			}
 
-			final TypedLinkNodeModule<String, BFEdge> targetModule = edge.target().value();
+			final TypedLinkNodeModule<String, BFEdge> targetModule = edge
+					.target().value();
 			CyNode targetNode = moduleToCyNodeMap.get(targetModule);
 			if (targetNode == null) {
-				final String nodeName = findNextAvailableNodeName("Complex" + nodeIndex);
-				targetNode = Cytoscape.getCyNode(nodeName, /* create = */ true);
+				final String nodeName = findNextAvailableNodeName("Complex"
+						+ nodeIndex);
+				targetNode = Cytoscape.getCyNode(nodeName, /* create = */true);
 				moduleToCyNodeMap.put(targetModule, targetNode);
 				overviewNetwork.addNode(targetNode);
 				final Set<String> genes = targetModule.getMemberValues();
-				nodeAttribs.setAttribute(targetNode.getIdentifier(), "gene count", Integer.valueOf(genes.size()));
-				nodeAttribs.setAttribute(targetNode.getIdentifier(), "score", Double.valueOf(targetModule.score()));
+				nodeAttribs.setAttribute(targetNode.getIdentifier(),
+						"gene count", Integer.valueOf(genes.size()));
+				nodeAttribs.setAttribute(targetNode.getIdentifier(), "score",
+						Double.valueOf(targetModule.score()));
 
 				++nodeIndex;
 			}
 
-			final CyEdge newEdge = Cytoscape.getCyEdge(sourceNode, targetNode, Semantics.INTERACTION, "complex-complex",
-								   /* create = */true);
+			final CyEdge newEdge = Cytoscape.getCyEdge(sourceNode, targetNode,
+					Semantics.INTERACTION, "complex-complex",
+					/* create = */true);
 
-			edgeAttribs.setAttribute(newEdge.getIdentifier(), "edge score", Double.valueOf(edge.value().link()));
+			edgeAttribs.setAttribute(newEdge.getIdentifier(), "edge score",
+					Double.valueOf(edge.value().link()));
+			overviewNetwork.addEdge(newEdge);
+			
 		}
+		
+		// Visualize
+		final CyNetworkView targetView = Cytoscape.getNetworkView(overviewNetwork.getIdentifier());
+		targetView.setVisualStyle("default");
+		Cytoscape.getVisualMappingManager().setVisualStyle("default");
+		targetView.applyLayout(CyLayouts.getDefaultLayout());
+		targetView.redrawGraph(false, true);
+		
 	}
 
 	CyNetwork getOverviewNetwork() {
@@ -115,7 +138,7 @@ class NestedNetworkCreator {
 	private String findNextAvailableNodeName(final String initialPreference) {
 		// Try the preferred choice first:
 		CyNode node = Cytoscape
-			.getCyNode(initialPreference, /* create = */false);
+				.getCyNode(initialPreference, /* create = */false);
 		if (node == null)
 			return initialPreference;
 
