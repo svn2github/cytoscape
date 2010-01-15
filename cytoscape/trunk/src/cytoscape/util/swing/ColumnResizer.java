a90b3166994c342afedceae4a572bbaa20ee52a6
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-01-15 23:56:15 +0000

    Add a new method
    git-svn-id: http://chianti.ucsd.edu/svn@18944 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/util/swing/ColumnResizer.java
+++ cytoscape/trunk/src/cytoscape/util/swing/ColumnResizer.java
@@ -40,6 +40,7 @@ import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
+import java.util.HashMap;
 
 
 /**
@@ -106,4 +107,58 @@ public class ColumnResizer {
 			column.setPreferredWidth(maxwidth + 20);
 		} // for col
 	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param table DOCUMENT ME!
+	 */
+	public static HashMap<String, Integer> getColumnPreferredWidths(JTable table) {
+		
+		HashMap<String, Integer> retMap = new HashMap<String, Integer>();
+		
+		// strategy - get max width for cells in column and
+		// make that the preferred width
+		TableColumnModel columnModel = table.getColumnModel();
+
+		for (int col = 0; col < table.getColumnCount(); col++) {
+			int maxwidth = 0;
+
+			for (int row = 0; row < table.getRowCount(); row++) {
+				TableCellRenderer rend = table.getCellRenderer(row, col);
+				Object value = table.getValueAt(row, col);
+				Component comp = rend.getTableCellRendererComponent(table, value, false, false,
+				                                                    row, col);
+				maxwidth = Math.max(comp.getPreferredSize().width, maxwidth);
+			} // for row
+
+			/*
+			 * this version of the width set considers the column header's
+			 * preferred width too
+			 */
+			TableColumn column = columnModel.getColumn(col);
+			TableCellRenderer headerRenderer = column.getHeaderRenderer();
+
+			if (headerRenderer == null)
+				headerRenderer = table.getTableHeader().getDefaultRenderer();
+
+			Object headerValue = column.getHeaderValue();
+			Component headerComp = headerRenderer.getTableCellRendererComponent(table, headerValue,
+			                                                                    false, false, 0, col);
+			maxwidth = Math.max(maxwidth, headerComp.getPreferredSize().width);
+
+			/*
+			 * If the value is too big, adjust to fixed maximum val.
+			 */
+			if (DEFLMAX_WIDTH < maxwidth) {
+				maxwidth = DEFLMAX_WIDTH;
+			}
+
+			retMap.put(headerValue.toString(), new Integer(maxwidth + 20));			
+		} // for col
+		
+		return retMap;
+	}
+
+
 }
