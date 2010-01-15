6d5c43a6f34393ac2095788bc347c070d9258637
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-01-15 21:55:34 +0000

    Edges will be added.
    git-svn-id: http://chianti.ucsd.edu/svn@18937 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/ruschein/BipartiteVisualiser/src/org/cytoscape/BipartiteVisualiserPlugin/duallayout/LayoutEngine.java
+++ csplugins/trunk/ucsd/ruschein/BipartiteVisualiser/src/org/cytoscape/BipartiteVisualiserPlugin/duallayout/LayoutEngine.java
@@ -1,12 +1,22 @@
 package org.cytoscape.BipartiteVisualiserPlugin.duallayout;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import giny.model.Edge;
+import giny.model.Node;
 import giny.view.EdgeView;
+import cytoscape.CyEdge;
 import cytoscape.CyNetwork;
+import cytoscape.CyNode;
 import cytoscape.Cytoscape;
+import cytoscape.layout.CyLayouts;
 import cytoscape.task.TaskMonitor;
+import cytoscape.view.CyNetworkView;
 
 public class LayoutEngine {
-	
+
 	private final EdgeView edgeView;
 
 	private final CyNetwork parentNetwork;
@@ -15,33 +25,60 @@ public class LayoutEngine {
 
 	public LayoutEngine(final EdgeView edgeView, final CyNetwork parentNetwork,
 			final CyNetwork network1, final CyNetwork network2) {
-		
+
 		this.edgeView = edgeView;
 		this.parentNetwork = parentNetwork;
 		this.network1 = network1;
 		this.network2 = network2;
 
 	}
-	
+
 	public void doLayout(TaskMonitor taskMonitor) {
 		if (taskMonitor != null)
 			taskMonitor.setPercentCompleted(0);
-		
+
 		// Create network
-		int[] nodes1 = network1.getNodeIndicesArray();
-		int[] nodes2 = network2.getNodeIndicesArray();
+		CyNetwork result = Cytoscape.createNetwork(network1
+				.getNodeIndicesArray(), network1.getEdgeIndicesArray(),
+				network1.getTitle() + " <--> " + network2.getTitle(), parentNetwork, false);
+		int[] nodes = network2.getNodeIndicesArray();
+		int[] edges = network2.getEdgeIndicesArray();
+
+		for (int i = 0; i < nodes.length; i++)
+			result.addNode(nodes[i]);
+		for (int i = 0; i < edges.length; i++)
+			result.addEdge(edges[i]);
 		
-			
-		
+		addEdges(result);
+		performLayout(result);
+
 	}
+
 	
-	private void pickEdges() {
+	private void addEdges(final CyNetwork result) {
+		@SuppressWarnings("unchecked")
+		final Set<CyNode> nodesInResult = new HashSet<CyNode>((List<CyNode>)result.nodesList());
 		
-		
+		for (CyNode node: nodesInResult) {
+			// Get the list of edges connected to this node
+			int[] edgeIndices = parentNetwork.getAdjacentEdgeIndicesArray(node.getRootGraphIndex(), true, true, true);
+			// For each node, select the appropriate edges
+			if (edgeIndices == null)
+				continue;
+
+			for (int i = 0; i < edgeIndices.length; i++)  {
+				final Edge edge = parentNetwork.getEdge(edgeIndices[i]);
+				if (nodesInResult.contains(edge.getSource()) && nodesInResult.contains(edge.getTarget()))
+					result.addEdge(edge);
+				
+			}
+		}
 	}
-	
-	private void performeLayout() {
-		
+
+	private void performLayout(CyNetwork result) {
+		final CyNetworkView view = Cytoscape.createNetworkView(result);
+		CyLayouts.getDefaultLayout().doLayout(view);
+
 	}
 
 }
