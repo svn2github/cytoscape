904db1c679130b2b3b7252b1278707ab864ec3dd
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-01-21 21:18:41 +0000

    Added Cytoscape launcher support to our .sh and .bat scripts.
    git-svn-id: http://chianti.ucsd.edu/svn@18988 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/resources/bin/cytoscape.sh
+++ cytoscape/trunk/resources/bin/cytoscape.sh
@@ -6,5 +6,5 @@
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./plugins
 
-java -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd -Xss5M -Xmx512M -jar cytoscape.jar cytoscape.CyMain -p plugins "$@"
+java -d64 -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd -Xss5M -Xmx512M -jar cytoscape.jar cytoscape.launcher.CytoscapeLauncher -p plugins "$@"
 
