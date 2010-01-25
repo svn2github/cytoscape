fc26f15fa362405d6ed006acd0f8af7f95fb735a
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-01-25 23:04:19 +0000

    Final, cleaned-up versions of the scripts to generate cytoscape.vmoptions.
    git-svn-id: http://chianti.ucsd.edu/svn@19017 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/tools/gen_vmoptions.bat
+++ cytoscape/trunk/tools/gen_vmoptions.bat
@@ -1,23 +1,23 @@
-ECHO OFF
+@ECHO OFF
 
-REM Generates the cytoscape.vmoptions file based on whether
-REM we're dealing with a 32 bit or 64 bit JVM.
+:: Generates the cytoscape.vmoptions file based on whether
+:: we're dealing with a 32 bit or 64 bit JVM.
 
 java -version 2>&1 | FINDSTR /I 64-Bit > NUL:
-GOTO label%ERRORLEVEL%
+IF NOT %errorlevel% == 0 GOTO 32bit 
 
-REM 64-bit JVM
-:label0
-	echo -Xms250m > cytoscape.vmoptions 
-	echo -Xmx20000m >> cytoscape.vmoptions 
+
+:64bit
+	echo -Xms100m > cytoscape.vmoptions 
+	echo -Xmx2000000m >> cytoscape.vmoptions 
 	GOTO end:
 
 
-REM 32-Bit JVM
-:label1
+:32bit
 	echo -Xms50m > cytoscape.vmoptions 
 	echo -Xmx1550m >> cytoscape.vmoptions 
 
 
 :end
- 
+set errorlevel=0
+
--- cytoscape/trunk/tools/gen_vmoptions.sh
+++ cytoscape/trunk/tools/gen_vmoptions.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 # Generates the cytoscape.vmoptions file
-if `java -version 2>&1 | grep -- 32-Bit > /dev/null`; then
-    echo -Xms50m > cytoscape.vmoptions
-    echo -Xmx1550m >> cytoscape.vmoptions
-elif `java -version 2>&1 | grep -- 64-Bit > /dev/null`; then
+if `java -version 2>&1 | grep -- 64-Bit > /dev/null`; then
     echo -Xms250m > cytoscape.vmoptions
     echo -Xmx20000m >> cytoscape.vmoptions
-else
+else # Assume a 32 bit JVM.
     echo -Xms50m > cytoscape.vmoptions
     echo -Xmx1550m >> cytoscape.vmoptions
 fi
