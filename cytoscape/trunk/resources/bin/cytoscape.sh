e777ae30211d0abd7e9028d15e1cc95c0cb11411
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-01-26 00:21:00 +0000

    Fixed another path problem.  => It should now be possible to invoke cytoscape.sh from any location.
    git-svn-id: http://chianti.ucsd.edu/svn@19022 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/resources/bin/cytoscape.sh
+++ cytoscape/trunk/resources/bin/cytoscape.sh
@@ -7,13 +7,13 @@
 script_path=$(dirname -- $0)
 
 # Attempt to generate cytoscape.vmoptions if it doesn't exist!
-if [ ! -e $script_path/cytoscape.vmoptions  -a  -x $script_path/gen_vmoptions.sh ]; then
+if [ ! -e cytoscape.vmoptions  -a  -x $script_path/gen_vmoptions.sh ]; then
     $script_path/gen_vmoptions.sh
 fi
 
-if [ -r $script_path/cytoscape.vmoptions ]; then
+if [ -r cytoscape.vmoptions ]; then
     java -d64 -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd \
-	`cat $script_path/cytoscape.vmoptions` -cp $script_path/cytoscape.jar cytoscape.CyMain \
+	`cat cytoscape.vmoptions` -cp $script_path/cytoscape.jar cytoscape.CyMain \
 	-p $script_path/plugins "$@"
 else # Just use sensible defaults.
     java -d64 -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd -Xss100M -Xmx1550M \
