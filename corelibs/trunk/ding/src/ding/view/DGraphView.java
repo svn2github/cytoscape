9d43e2853a832f6f3fbde2a668eca36632b5b2ce
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-02-05 00:31:52 +0000

    made DEF_SNAPSHOT_SIZE protected and not final, so that DingNetworkView could update it based on a property found in cytoscape.props
    git-svn-id: http://chianti.ucsd.edu/svn@19205 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- corelibs/trunk/ding/src/ding/view/DGraphView.java
+++ corelibs/trunk/ding/src/ding/view/DGraphView.java
@@ -98,7 +98,7 @@ public class DGraphView implements GraphView, Printable {
 	boolean calledFromGetSnapshot = false;
 
 	// Size of snapshot image
-	private static final int DEF_SNAPSHOT_SIZE = 400;
+	protected static int DEF_SNAPSHOT_SIZE = 400;
 	
 	static final float DEFAULT_ANCHOR_SIZE = 9.0f;
 	static final Paint DEFAULT_ANCHOR_SELECTED_PAINT = Color.red;
