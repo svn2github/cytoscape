5c8f63ed21d40d0cba3b282c6a320f52505291ec
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-02-09 19:38:35 +0000

    commented out some printlns
    git-svn-id: http://chianti.ucsd.edu/svn@19262 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/trunk/TableImport/src/edu/ucsd/bioeng/coreplugin/tableImport/ui/DataTypeUtil.java
+++ coreplugins/trunk/TableImport/src/edu/ucsd/bioeng/coreplugin/tableImport/ui/DataTypeUtil.java
@@ -55,8 +55,8 @@ class DataTypeUtil {
 	static void guessTypes(final TableModel model, final String tableName, 
 	                       Map<String,Byte[]> dataTypeMap) {
 
-		System.out.println("model row count: " + model.getRowCount() );
-		System.out.println("model column count: " + model.getColumnCount() );
+		//System.out.println("model row count: " + model.getRowCount() );
+		//System.out.println("model column count: " + model.getColumnCount() );
 
 		// 0 = Boolean,  1 = Integer,  2 = Double,  3 = String
 		final Integer[][] typeChecker = new Integer[4][model.getColumnCount()];
@@ -72,7 +72,7 @@ class DataTypeUtil {
 		for (int i = 0; i < model.getRowCount(); i++) {
 			for (int j = 0; j < model.getColumnCount(); j++) {
 				cell = (String) model.getValueAt(i, j);
-				System.out.print( i + " " + j + " " + cell + " ");
+				//System.out.print( i + " " + j + " " + cell + " ");
 
 				boolean found = false;
 
@@ -82,7 +82,7 @@ class DataTypeUtil {
 					     falsePattern.matcher(cell).matches() ) {
 						typeChecker[0][j]++;
 						found = true;
-						System.out.println("boolean");
+						//System.out.println("boolean");
 					} else {
 
 						// integers
@@ -90,7 +90,7 @@ class DataTypeUtil {
 							Integer.valueOf(cell);
 							typeChecker[1][j]++;
 							found = true;
-						System.out.println("integer");
+						//System.out.println("integer");
 						} catch (NumberFormatException e) {
 						}
 			
@@ -100,7 +100,7 @@ class DataTypeUtil {
 							Double.valueOf(cell);
 							typeChecker[2][j]++;
 							found = true;
-						System.out.println("float");
+						//System.out.println("float");
 						} catch (NumberFormatException e) {
 						}
 					}
@@ -109,7 +109,7 @@ class DataTypeUtil {
 				// default to string
 				if (found == false) {
 					typeChecker[3][j]++;
-						System.out.println("string");
+						//System.out.println("string");
 				}
 			}
 		}
@@ -125,14 +125,14 @@ class DataTypeUtil {
 			int maxIndex = 0;
 
 			for (int j = 0; j < 4; j++) {
-				System.out.println("col: " + i + " byte: " + j  + " count: " + typeChecker[j][i]);
+				//System.out.println("col: " + i + " byte: " + j  + " count: " + typeChecker[j][i]);
 				if (maxVal < typeChecker[j][i]) {
 					maxVal = typeChecker[j][i];
 					maxIndex = j;
 				}
 			}
 	
-			System.out.println("  max index: " + maxIndex);
+			//System.out.println("  max index: " + maxIndex);
 
 			if (maxIndex == 0)
 				dataType[i] = CyAttributes.TYPE_BOOLEAN;
@@ -143,7 +143,7 @@ class DataTypeUtil {
 			else
 				dataType[i] = CyAttributes.TYPE_STRING;
 
-			System.out.println("  resulting data type: " + dataType[i]);
+			//System.out.println("  resulting data type: " + dataType[i]);
 		}
 
 		dataTypeMap.put(tableName, dataType);
