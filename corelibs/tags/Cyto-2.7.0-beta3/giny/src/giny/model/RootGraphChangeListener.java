f431c0fbf0b236bdfc96af7b8dcc1cd0cc45cb01
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-02-10 21:35:52 +0000

    tagging release Cyto-2.7.0-beta3
    git-svn-id: http://chianti.ucsd.edu/svn@19280 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ corelibs/tags/Cyto-2.7.0-beta3/giny/src/giny/model/RootGraphChangeListener.java
@@ -0,0 +1,14 @@
+package giny.model;
+
+import java.util.EventListener;
+
+public interface RootGraphChangeListener
+  extends EventListener {
+
+  /**
+   * Invoked when a RootGraph to which this RootGraphChangeListener listens
+   * changes.
+   */
+  public void rootGraphChanged ( RootGraphChangeEvent event );
+
+} // interface RootGraphChangeListener
