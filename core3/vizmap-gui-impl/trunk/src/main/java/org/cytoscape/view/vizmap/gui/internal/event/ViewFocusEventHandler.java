202870cc615a9e4c7e1d02d6a458b26434fa3670
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-02-19 01:53:41 +0000

    removed dependency on an internal class
    git-svn-id: http://chianti.ucsd.edu/svn@19382 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-gui-impl/trunk/src/main/java/org/cytoscape/view/vizmap/gui/internal/event/ViewFocusEventHandler.java
+++ core3/vizmap-gui-impl/trunk/src/main/java/org/cytoscape/view/vizmap/gui/internal/event/ViewFocusEventHandler.java
@@ -4,15 +4,15 @@ import java.beans.PropertyChangeEvent;
 
 import org.cytoscape.view.vizmap.VisualStyle;
 
-import cytoscape.internal.view.NetworkPanel;
+//import cytoscape.internal.view.NetworkPanel;
 
 public class ViewFocusEventHandler extends AbstractVizMapEventHandler {
 
 	@Override
 	public void processEvent(PropertyChangeEvent e) {
 
-		if (e.getSource().getClass() != NetworkPanel.class)
-			return;
+//		if (e.getSource().getClass() != NetworkPanel.class)
+//			return;
 
 		// Get visual style for the selected netwrok view.
 		final VisualStyle vs = vmm.getVisualStyle(cyNetworkManager
