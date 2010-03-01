def07377a82950fd0b5cd4c613c55c0082b34370
mcreech <mcreech@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-03-01 22:45:27 +0000

    Many changes including: 1) Added saving of proxy related Cytoscape Properties in cytoscape.props to allow persistence of proxy information without having to create and restore a session.    Other temporarily changed properties that exist at the time of saving are NOT saved--only the proxy information is added to the existing save cytoscape.props, since the user may not want the other properties saved. 2) Added automatic testing of proxy settings.    When Update is pressed, a check is made to ensure a site on the Internet can be reached. Success and failure are noted with a dialog and failure will allow user an opportunity to change his/her proxy settings. 3) Added various TODO: notes in the code and did a bit of package cleanup.
    git-svn-id: http://chianti.ucsd.edu/svn@19478 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/dialogs/preferences/ProxyServerDialog.java
+++ cytoscape/trunk/src/cytoscape/dialogs/preferences/ProxyServerDialog.java
@@ -27,23 +27,40 @@
 */
 package cytoscape.dialogs.preferences;
 
-import cytoscape.Cytoscape;
-import cytoscape.CytoscapeInit;
-
-import cytoscape.util.ProxyHandler;
-
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Properties;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+import cytoscape.Cytoscape;
+import cytoscape.CytoscapeInit;
+import cytoscape.logger.CyLogger;
+import cytoscape.util.ProxyHandler;
 
 
 /**
@@ -52,9 +69,11 @@ import javax.swing.JOptionPane;
 public class ProxyServerDialog extends JDialog implements ActionListener, ItemListener {
 
 	private static final long serialVersionUID = -2693844068486336199L;
+        private static final String   WELL_KNOWN_URL   = "http://google.com";
+        private static final String PROPERTIES_FILE_NAME = "cytoscape.props";
 
 	/** Creates new form URLimportAdvancedDialog */
-	public ProxyServerDialog(javax.swing.JFrame pParent) {
+	public ProxyServerDialog(JFrame pParent) {
 		super(pParent, true);
 		this.setTitle("Proxy Server Setting");
 		this.setLocationRelativeTo(pParent);
@@ -63,52 +82,58 @@ public class ProxyServerDialog extends JDialog implements ActionListener, ItemLi
 		initValues();
 	}
 
-	private javax.swing.JButton btnCancel;
-	private javax.swing.JButton btnUpdate;
-	private javax.swing.JCheckBox chbUseProxy;
-	private javax.swing.JComboBox cmbType;
-	private javax.swing.JPanel jPanel1;
-	private javax.swing.JPanel jPanel2;
-	private javax.swing.JPanel jPanel3;
-	private javax.swing.JLabel lbHost;
-	private javax.swing.JLabel lbPort;
-	private javax.swing.JLabel lbType;
-	private javax.swing.JLabel lbUseProxy;
-	private javax.swing.JTextField tfHost;
-	private javax.swing.JTextField tfPort;
-    
+	private JButton btnCancel;
+        // TODO: Change the update button only be enabled when there has
+        //       been a change that needs updating.
+	private JButton btnUpdate;
+	private JCheckBox chbUseProxy;
+	private JComboBox cmbType;
+	private JPanel jPanel1;
+	private JPanel jPanel2;
+	private JPanel jPanel3;
+	private JLabel lbHost;
+	private JLabel lbPort;
+	private JLabel lbType;
+	private JLabel lbUseProxy;
+	private JTextField tfHost;
+	private JTextField tfPort;
+        private boolean _lastUseProxy;
+        private String  _lastProxyHost;
+        private String  _lastProxyPort;
+        private String  _lastProxyType;
+
 	private void initComponents() {
 		
-		java.awt.GridBagConstraints gridBagConstraints;
+		GridBagConstraints gridBagConstraints;
 
-		jPanel3 = new javax.swing.JPanel();
-		lbUseProxy = new javax.swing.JLabel();
-		chbUseProxy = new javax.swing.JCheckBox();
-		jPanel1 = new javax.swing.JPanel();
-		btnUpdate = new javax.swing.JButton();
-		btnCancel = new javax.swing.JButton();
-		jPanel2 = new javax.swing.JPanel();
-		lbType = new javax.swing.JLabel();
-		lbHost = new javax.swing.JLabel();
-		lbPort = new javax.swing.JLabel();
-		cmbType = new javax.swing.JComboBox();
-		tfHost = new javax.swing.JTextField();
-		tfPort = new javax.swing.JTextField();
+		jPanel3 = new JPanel();
+		lbUseProxy = new JLabel();
+		chbUseProxy = new JCheckBox();
+		jPanel1 = new JPanel();
+		btnUpdate = new JButton();
+		btnCancel = new JButton();
+		jPanel2 = new JPanel();
+		lbType = new JLabel();
+		lbHost = new JLabel();
+		lbPort = new JLabel();
+		cmbType = new JComboBox();
+		tfHost = new JTextField();
+		tfPort = new JTextField();
 
-		getContentPane().setLayout(new java.awt.GridBagLayout());
+		getContentPane().setLayout(new GridBagLayout());
 
-		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		lbUseProxy.setText("Use Proxy");
 		jPanel3.add(lbUseProxy);
 
-		chbUseProxy.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
-		chbUseProxy.setMargin(new java.awt.Insets(0, 0, 0, 0));
+		chbUseProxy.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		chbUseProxy.setMargin(new Insets(0, 0, 0, 0));
 		jPanel3.add(chbUseProxy);
 
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridx = 0;
-		gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-		gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+		gridBagConstraints.insets = new Insets(5, 5, 5, 5);
 		getContentPane().add(jPanel3, gridBagConstraints);
 
 		btnUpdate.setText("Update");
@@ -117,56 +142,56 @@ public class ProxyServerDialog extends JDialog implements ActionListener, ItemLi
 		btnCancel.setText("Cancel");
 		jPanel1.add(btnCancel);
 
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridy = 3;
-		gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
-		gridBagConstraints.insets = new java.awt.Insets(10, 0, 0, 0);
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		gridBagConstraints.insets = new Insets(10, 0, 0, 0);
 		getContentPane().add(jPanel1, gridBagConstraints);
 
-		jPanel2.setLayout(new java.awt.GridBagLayout());
+		jPanel2.setLayout(new GridBagLayout());
 
-		jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Proxy Settings"));
+		jPanel2.setBorder(BorderFactory.createTitledBorder("Proxy Settings"));
 		lbType.setText("Type");
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridy = 1;
 		jPanel2.add(lbType, gridBagConstraints);
 
 		lbHost.setText("Host name");
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridy = 1;
 		jPanel2.add(lbHost, gridBagConstraints);
 
 		lbPort.setText("Port");
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridy = 1;
 		jPanel2.add(lbPort, gridBagConstraints);
 
-		cmbType.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "HTTP", "SOCKS" }));
-		cmbType.setMinimumSize(new java.awt.Dimension(61, 18));
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		cmbType.setModel(new DefaultComboBoxModel(new String[] { "HTTP", "SOCKS" }));
+		cmbType.setMinimumSize(new Dimension(61, 18));
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridy = 2;
-		gridBagConstraints.insets = new java.awt.Insets(5, 10, 10, 5);
+		gridBagConstraints.insets = new Insets(5, 10, 10, 5);
 		jPanel2.add(cmbType, gridBagConstraints);
 
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridy = 2;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
 		gridBagConstraints.weightx = 1.0;
-		gridBagConstraints.insets = new java.awt.Insets(5, 5, 10, 5);
+		gridBagConstraints.insets = new Insets(5, 5, 10, 5);
 		jPanel2.add(tfHost, gridBagConstraints);
 
 		final int tfPortHeight = (int)tfHost.getMinimumSize().getHeight();
-		tfPort.setMinimumSize(new java.awt.Dimension(43, tfPortHeight));
-		tfPort.setPreferredSize(new java.awt.Dimension(43, tfPortHeight));
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		tfPort.setMinimumSize(new Dimension(43, tfPortHeight));
+		tfPort.setPreferredSize(new Dimension(43, tfPortHeight));
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridy = 2;
-		gridBagConstraints.insets = new java.awt.Insets(5, 5, 10, 10);
+		gridBagConstraints.insets = new Insets(5, 5, 10, 10);
 		jPanel2.add(tfPort, gridBagConstraints);
 
-		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 1;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+		gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
 		gridBagConstraints.weightx = 1.0;
 		getContentPane().add(jPanel2, gridBagConstraints);
 
@@ -181,30 +206,42 @@ public class ProxyServerDialog extends JDialog implements ActionListener, ItemLi
 	} // </editor-fold>
 
 	private void initValues() {
-		Proxy p = ProxyHandler.getProxyServer();
+	    // initially set the last values as they are read from the
+	    // ProxyHandler. We will compare these to the final values
+	    // to see if anything changed.
+	    _lastUseProxy = false;
+	    _lastProxyHost = "";
+	    _lastProxyPort = "";
+	    _lastProxyType = "";
 
-		chbUseProxy.setSelected(true);
-		cmbType.setEnabled(true);
-		tfHost.setEnabled(true);
-		tfPort.setEnabled(true);
-
-		if (p == null) {
-			tfHost.setText("");
-			tfPort.setText("");
-			chbUseProxy.setSelected(false);
-    		cmbType.setEnabled(false);
-    		tfHost.setEnabled(false);
-    		tfPort.setEnabled(false);
-			return;
-		} else if (p.type() == Proxy.Type.HTTP) {
-			cmbType.setSelectedItem("HTTP");
+	    Proxy p = ProxyHandler.getProxyServer();
+	    chbUseProxy.setSelected(true);
+	    cmbType.setEnabled(true);
+	    tfHost.setEnabled(true);
+	    tfPort.setEnabled(true);
+	    
+	    if ((p != null) && (p.type() != Proxy.Type.DIRECT)) {
+		_lastUseProxy = true;
+		if (p.type() == Proxy.Type.HTTP) {
+		    _lastProxyType = "HTTP";
+		    cmbType.setSelectedItem(_lastProxyType);
 		} else if (p.type() == Proxy.Type.SOCKS) {
-			cmbType.setSelectedItem("SOCKS");
+		    _lastProxyType = "SOCKS";
+		    cmbType.setSelectedItem(_lastProxyType);
 		}
 
 		InetSocketAddress address = (InetSocketAddress) p.address();
-		tfHost.setText(address.getHostName());
-		tfPort.setText(new Integer(address.getPort()).toString());
+		_lastProxyHost = address.getHostName();
+		_lastProxyPort = new Integer(address.getPort()).toString();
+	    } else {
+		chbUseProxy.setSelected(false);
+		cmbType.setEnabled(false);
+		tfHost.setEnabled(false);
+		tfPort.setEnabled(false);
+	    }
+	    tfHost.setText(_lastProxyHost);
+	    tfPort.setText(_lastProxyPort);
+
 	}
 
 	/**
@@ -216,7 +253,7 @@ public class ProxyServerDialog extends JDialog implements ActionListener, ItemLi
 		
         Object _actionObject = e.getSource();
 
-        if (_actionObject instanceof javax.swing.JCheckBox)
+        if (_actionObject instanceof JCheckBox)
         {
         	if (chbUseProxy.isSelected()) { // UseProxy is checked
             	// enable the setting panel
@@ -239,6 +276,7 @@ public class ProxyServerDialog extends JDialog implements ActionListener, ItemLi
 	 *
 	 * @param e DOCUMENT ME!
 	 */
+        // TODO: Split each action into a separate private class so _btn and _actionObject are not needed:
 	public void actionPerformed(ActionEvent e) {
 		Object _actionObject = e.getSource();
 
@@ -302,9 +340,108 @@ public class ProxyServerDialog extends JDialog implements ActionListener, ItemLi
 		CytoscapeInit.getProperties().setProperty(ProxyHandler.PROXY_PORT_PROPERTY_NAME, tfPort.getText());
 		CytoscapeInit.getProperties()
 		             .setProperty(ProxyHandler.PROXY_TYPE_PROPERTY_NAME, cmbType.getSelectedItem().toString());
-
-		Cytoscape.firePropertyChange(Cytoscape.PREFERENCES_UPDATED, null, null);
-
-		return true;
+		return saveProxyChangesAndTestWhenNeeded ();
 	}
+
+    // Return true iff something with the proxy settings has changes
+    // since this dialog was constructed:
+    private boolean hasProxyValuesChanged () {
+	return ((_lastUseProxy != chbUseProxy.isSelected()) ||
+		(!_lastProxyType.equals (cmbType.getSelectedItem().toString())) ||
+		(!_lastProxyPort.equals (tfPort.getText())) ||
+		(!_lastProxyHost.equals (tfHost.getText().trim())));
+    }
+
+
+    // TODO: This method is using knowledge of how properties are loaded
+    // and saved. A better approach is to add some property management
+    // classes or methods that encapsulate this behavior. This also
+    // applies to the PreferencesDialog.
+
+    private boolean saveProxyChangesAndTestWhenNeeded () {
+
+	if (!hasProxyValuesChanged ()) {
+	    // nothing requires saving, but test the Proxy:
+	    return performProxyTest();
+	}
+	// To avoid saving potentially unwanted, temporary properties,
+	// we can't just save the existing in-memory
+	// properties. Instead, we must read the last saved properties
+	// into a new Properties object, add the proxy values, and
+	// save the Properties back out. If the user saved the in-memory
+	// properties, that will be fine also, since the proxy info
+	// has been updated there.
+
+	Properties inMemoryProps = CytoscapeInit.getProperties();
+	Properties propsToSave = new Properties();
+	// fill in propsToSave with the last read properties (NOT the
+	// existing in-memory properties):
+	CytoscapeInit.loadStaticProperties (PROPERTIES_FILE_NAME, propsToSave);
+	// Now add in-memory values to our Properties to save:
+	propsToSave.setProperty (ProxyHandler.PROXY_HOST_PROPERTY_NAME,
+				 inMemoryProps.getProperty(ProxyHandler.PROXY_HOST_PROPERTY_NAME,null));
+	propsToSave.setProperty (ProxyHandler.PROXY_PORT_PROPERTY_NAME,
+				 inMemoryProps.getProperty(ProxyHandler.PROXY_PORT_PROPERTY_NAME,null));
+	propsToSave.setProperty (ProxyHandler.PROXY_TYPE_PROPERTY_NAME,
+				 inMemoryProps.getProperty(ProxyHandler.PROXY_TYPE_PROPERTY_NAME,null));
+	// Now save the updated properties back out:
+	try {
+	    File file = CytoscapeInit.getConfigFile(PROPERTIES_FILE_NAME);
+	    FileOutputStream output = null;
+	    try {
+		output = new FileOutputStream(file);
+		propsToSave.store(output, "Cytoscape Property File");
+		CyLogger.getLogger().info("wrote Cytoscape properties file to: " + file.getAbsolutePath());
+	    }
+	    finally {
+		if (output != null) {
+		    output.close();
+		}
+	    }
+	} catch (Exception ex) {
+	    CyLogger.getLogger().error("Could not write the " + PROPERTIES_FILE_NAME + " file!", ex);
+	}
+	// Even if the properties were not sucessfully written, signal
+	// they changed in memory. The ProxyHandler will use this
+	// event to update the real Proxy:
+	Cytoscape.firePropertyChange(Cytoscape.PREFERENCES_UPDATED, null, null);
+	return performProxyTest ();
+    }
+
+    private boolean performProxyTest () {
+	if (testNetConnection (ProxyHandler.getProxyServer(), 2000)) {
+	    JOptionPane.showMessageDialog (this,
+					   "We successfully connected to " + WELL_KNOWN_URL + " using your new settings!");
+	    return true;
+	} else {
+	    JOptionPane.showMessageDialog (this,
+					   "<HTML>We could <STRONG>not connect</STRONG> to " + WELL_KNOWN_URL + " using your new settings.");
+	    return false;
+	}
+    }
+
+    // Attempt to connect to a well known site (e.g., google.com):
+    // return true if we successfully connected to the Internet to
+    // a well know site. Return false otherwise.
+    private boolean testNetConnection(final Proxy proxy, final int ms_timeout) {
+        try {
+            final URL           u  = new URL(WELL_KNOWN_URL);
+            URLConnection uc = null;
+	    if (proxy != null) {
+		uc = u.openConnection(proxy);
+	    } else {
+		uc = u.openConnection();
+	    }
+            uc.setAllowUserInteraction(false);
+            uc.setUseCaches(false); // don't use a cached page
+            uc.setConnectTimeout(ms_timeout);
+            uc.connect();
+            return true;
+        } catch (IOException e) {
+            // we couldn't connect:
+            return false;
+        }
+    }
+
+
 }
