c3fe3635bffca1c75ef0085dc34dd43a740e2474
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-03-02 00:42:05 +0000

    tagging release Cyto-2.7.0-beta4
    git-svn-id: http://chianti.ucsd.edu/svn@19484 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/tags/Cyto-2.7.0-beta4/src/cytoscape/visual/strokes/ForwardSlashStroke.java
@@ -0,0 +1,13 @@
+
+
+package cytoscape.visual.strokes;
+
+import static cytoscape.visual.LineStyle.FORWARD_SLASH;
+
+public class ForwardSlashStroke extends PipeStroke {
+
+	public ForwardSlashStroke(float width, Type offsetType) {
+		super(width,offsetType,FORWARD_SLASH);
+	}
+}
+
