d6e49ef9ca4d7dfcfa26d59c60ffe794306c287c
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-03-09 20:53:00 +0000

    Branch for Custom Graphics visual property.
    git-svn-id: http://chianti.ucsd.edu/svn@19545 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/branches/cytoscape_28_custom_graphics/src/cytoscape/visual/ui/VizMapperProperty.java
@@ -0,0 +1,49 @@
+package cytoscape.visual.ui;
+
+import com.l2fprod.common.propertysheet.DefaultProperty;
+
+
+/**
+ * Extended version of DefaultProperty which accepts one more value as hidden
+ * object.
+ *
+ * @author kono
+ *
+ */
+public class VizMapperProperty extends DefaultProperty {
+    private Object hiddenObject;
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param obj DOCUMENT ME!
+     */
+    public void setHiddenObject(Object obj) {
+        this.hiddenObject = obj;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     */
+    public Object getHiddenObject() {
+        return hiddenObject;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param object DOCUMENT ME!
+     */
+    public void readFromObject(Object object) {
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param object DOCUMENT ME!
+     */
+    public void writeToObject(Object object) {
+    }
+}
