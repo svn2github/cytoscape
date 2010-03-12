2b66c35c5c7931c2c072f2079968d87544319d01
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-03-12 22:46:43 +0000

    tagging release Cyto-2.7.0
    git-svn-id: http://chianti.ucsd.edu/svn@19600 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/tags/Cyto-2.7.0/resources/bin/gen_vmoptions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Generates the cytoscape.vmoptions file
+if `java -version 2>&1 | grep -- 64-Bit > /dev/null`; then # We have a 64 bit JVM.
+    echo -Xms20m >  cytoscape.vmoptions
+    echo -Xmx20g >> cytoscape.vmoptions
+    echo -d64       >> cytoscape.vmoptions
+else # Assume a 32 bit JVM.
+    echo -Xms10m   >  cytoscape.vmoptions
+    echo -Xmx1550m >> cytoscape.vmoptions
+fi
+
+# Shared JVM options
+echo -Dswing.aatext=true               >> cytoscape.vmoptions
+echo -Dawt.useSystemAAFontSettings=lcd >> cytoscape.vmoptions
+
+exit 0
---
+++ cytoscape/tags/Cyto-2.7.0/src/cytoscape/visual/NodeAppearance.java
@@ -0,0 +1,91 @@
+/*
+ File: NodeAppearance.java
+
+ Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
+
+ The Cytoscape Consortium is:
+ - Institute for Systems Biology
+ - University of California San Diego
+ - Memorial Sloan-Kettering Cancer Center
+ - Institut Pasteur
+ - Agilent Technologies
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
+
+package cytoscape.visual;
+
+/**
+ * Objects of this class hold data describing the appearance of a Node.
+ */
+public class NodeAppearance extends Appearance {
+
+	/**
+	 * Constructor.
+	 */
+	public NodeAppearance() {
+		super();
+	}
+
+	private VisualPropertyDependency dep;
+
+	/**
+	 * @deprecated This exists only for backwards compatibility.  Do not use it!
+     * Will be removed Jan 2011.
+	 */
+	@Deprecated
+	NodeAppearance(VisualPropertyDependency dep) {
+		super();
+		this.dep = dep;
+	}
+
+
+	public Object clone() {
+		NodeAppearance ga = new NodeAppearance();
+		ga.copy(this);
+		return ga;
+	}
+
+    /**
+     * Returns whether or not the node height and width are locked.
+     * @return Whether or not the node height and width are locked.     
+     * @deprecated Use VisualStyle.getDependency().check(VisualPropertyDependency.Definition.NODE_SIZE_LOCKED) instead. 
+     * Will be removed Jan 2011.
+     */
+    @Deprecated
+    public boolean getNodeSizeLocked() {
+        return dep.check(VisualPropertyDependency.Definition.NODE_SIZE_LOCKED);
+    }
+
+    /**
+     * Sets whether or not the node height and width are locked.     
+     * @deprecated Use VisualStyle.getDependency().set(VisualPropertyDependency.Definition.NODE_SIZE_LOCKED,b) instead. 
+     * Will be removed Jan 2011.
+     */
+    @Deprecated
+    public void setNodeSizeLocked(boolean b) {
+       	dep.set(VisualPropertyDependency.Definition.NODE_SIZE_LOCKED,b); 
+    }
+}
