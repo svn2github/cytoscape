32f72d34d01cc29677c72c9d8e6d1d1ae316674c
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-03-23 17:24:41 +0000

    tagging release Cyto-2.7.0
    git-svn-id: http://chianti.ucsd.edu/svn@19691 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/tags/Cyto-2.7.0/src/cytoscape/view/NetworkTreeTableModel.java
@@ -0,0 +1,118 @@
+package cytoscape.view;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.Icon;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import cytoscape.CyNetwork;
+import cytoscape.Cytoscape;
+import cytoscape.util.swing.AbstractTreeTableModel;
+
+/**
+ * Inner class that extends the AbstractTreeTableModel
+ */
+public final class NetworkTreeTableModel extends AbstractTreeTableModel {
+
+	private List<ColumnTypes> columnNames;
+	private final Map<String, Icon> networkIcons;
+
+	public NetworkTreeTableModel(Object root) {
+		super(root);
+		columnNames = new ArrayList<ColumnTypes>();
+		columnNames.add(ColumnTypes.NETWORK);
+		columnNames.add(ColumnTypes.NODES);
+		columnNames.add(ColumnTypes.EDGES);
+
+		networkIcons = new HashMap<String, Icon>();
+	}
+
+	public void addColumn(final ColumnTypes model, final int idx) {
+		columnNames.add(idx, model);
+	}
+
+	public void removeColumn(final int idx) {
+		columnNames.remove(idx);
+	}
+
+	public Object getChild(Object parent, int index) {
+		final Enumeration<?> tree_node_enum = ((DefaultMutableTreeNode) getRoot())
+				.breadthFirstEnumeration();
+
+		while (tree_node_enum.hasMoreElements()) {
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree_node_enum
+					.nextElement();
+
+			if (node == parent) {
+				return node.getChildAt(index);
+			}
+		}
+
+		return null;
+	}
+
+	public int getChildCount(Object parent) {
+		Enumeration<?> tree_node_enum = ((DefaultMutableTreeNode) getRoot())
+				.breadthFirstEnumeration();
+
+		while (tree_node_enum.hasMoreElements()) {
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree_node_enum
+					.nextElement();
+
+			if (node == parent) {
+				return node.getChildCount();
+			}
+		}
+
+		return 0;
+	}
+
+	public int getColumnCount() {
+		return columnNames.size();
+	}
+
+	public String getColumnName(int column) {
+		return columnNames.get(column).getDisplayName();
+	}
+
+	public Class<?> getColumnClass(int column) {
+		return columnNames.get(column).getType();
+	}
+
+	public Object getValueAt(Object node, int column) {
+		if (columnNames.get(column).equals(ColumnTypes.NETWORK))
+			return ((DefaultMutableTreeNode) node).getUserObject();
+		else if (columnNames.get(column).equals(ColumnTypes.NODES)) {
+			CyNetwork cyNetwork = Cytoscape.getNetwork(((NetworkTreeNode) node)
+					.getNetworkID());
+
+			return "" + cyNetwork.getNodeCount() + "("
+					+ cyNetwork.getSelectedNodes().size() + ")";
+		} else if (columnNames.get(column).equals(ColumnTypes.EDGES)) {
+			CyNetwork cyNetwork = Cytoscape.getNetwork(((NetworkTreeNode) node)
+					.getNetworkID());
+
+			return "" + cyNetwork.getEdgeCount() + "("
+					+ cyNetwork.getSelectedEdges().size() + ")";
+		} else if (columnNames.get(column).equals(ColumnTypes.NETWORK_ICONS)) {
+
+			return networkIcons.get(((NetworkTreeNode) node).getNetworkID());
+		}
+
+		return "";
+	}
+
+	public void setValueAt(Object aValue, Object node, int column) {
+		if (columnNames.get(column).equals(ColumnTypes.NETWORK)) {
+			((DefaultMutableTreeNode) node).setUserObject(aValue);
+		} else if (columnNames.get(column).equals(ColumnTypes.NETWORK_ICONS)
+				&& aValue instanceof Icon) {
+			networkIcons.put(((NetworkTreeNode) node).getNetworkID(),
+					(Icon) aValue);
+		}
+	}
+}
