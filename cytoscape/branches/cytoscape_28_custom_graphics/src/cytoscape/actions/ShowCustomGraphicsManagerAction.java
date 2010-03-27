68414dda03619de2f2149dbbba897e753e193289
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-03-27 09:12:51 +0000

    Reader added (not working)
    git-svn-id: http://chianti.ucsd.edu/svn@19743 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/branches/cytoscape_28_custom_graphics/src/cytoscape/actions/ShowCustomGraphicsManagerAction.java
+++ cytoscape/branches/cytoscape_28_custom_graphics/src/cytoscape/actions/ShowCustomGraphicsManagerAction.java
@@ -7,13 +7,13 @@ import cytoscape.util.CytoscapeAction;
 import cytoscape.visual.customgraphic.ui.CustomGraphicsManagerDialog;
 
 public class ShowCustomGraphicsManagerAction extends CytoscapeAction {
-	
+
 	private static final long serialVersionUID = 5876533870116518191L;
-	
+
 	private static final String TITLE = "Open Custom Graphics Manager...";
 
 	private CustomGraphicsManagerDialog manager;
-	
+
 	/**
 	 * Creates a new SetVisualPropertiesAction object.
 	 */
@@ -24,17 +24,20 @@ public class ShowCustomGraphicsManagerAction extends CytoscapeAction {
 	}
 
 	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @param e DOCUMENT ME!
+	 * DOCUMENT ME!
+	 * 
+	 * @param e
+	 *            DOCUMENT ME!
 	 */
 	public void actionPerformed(ActionEvent e) {
 		// Add custom graphics manager
-		if(manager == null)
-			manager = new CustomGraphicsManagerDialog(Cytoscape.getDesktop(), false);
-		
+
+		if (manager == null)
+			manager = new CustomGraphicsManagerDialog(Cytoscape.getDesktop(),
+					false);
+
 		manager.setLocationRelativeTo(Cytoscape.getDesktop());
 		manager.setVisible(true);
-		
+
 	}
 }
