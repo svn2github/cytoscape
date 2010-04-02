21e43efaef85d39d1d213f73a518c58da4c6c881
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-04-02 23:38:32 +0000

    Use absolute URLs instead of relative URLs
    git-svn-id: http://chianti.ucsd.edu/svn@19800 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/nav.php
+++ cyto_web/trunk/nav.php
@@ -1,12 +1,12 @@
 <div id="nav">
-	<div class="item"><a href="index.php">Home</a></div>
-	<div class="item"><a href="features2.php">Introduction</a></div>
-	<div class="item"><a href="screenshots.php">Screenshots</a></div>
-	<div class="item"><a href="plugins.php">Plugins</a></div>
-	<div class="item"><a href="community.php">Community</a></div>
-	<div class="item"><a href="download_list.php">All Releases</a></div>
-	<div class="item"><a href="people.php">Dev Team</a></div>
-	<div class="item"><a href="links.php">Links</a></div>
+	<div class="item"><a href="http://cytoscape.org/index.php">Home</a></div>
+	<div class="item"><a href="http://cytoscape.org/features2.php">Introduction</a></div>
+	<div class="item"><a href="http://cytoscape.org/screenshots.php">Screenshots</a></div>
+	<div class="item"><a href="http://cytoscape.org/plugins.php">Plugins</a></div>
+	<div class="item"><a href="http://cytoscape.org/community.php">Community</a></div>
+	<div class="item"><a href="http://cytoscape.org/download_list.php">All Releases</a></div>
+	<div class="item"><a href="http://cytoscape.org/people.php">Dev Team</a></div>
+	<div class="item"><a href="http://cytoscape.org/links.php">Links</a></div>
 	<div class="item"><a href="http://cbio.mskcc.org/cytoscape/bugs/set_project.php?project_id=5">Bug Tracker</a></div>
 	<div class="itemlast"><a href="http://cytoscape.wodaklab.org/wiki/user_faq">FAQ</a></div>
 </div>
