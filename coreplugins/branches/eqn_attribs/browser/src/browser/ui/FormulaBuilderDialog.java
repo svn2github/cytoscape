74f9e1011342e9e720594d151849edcd527d32eb
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-04-07 22:54:20 +0000

    Work in progress.
    git-svn-id: http://chianti.ucsd.edu/svn@19836 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/branches/eqn_attribs/browser/src/browser/ui/FormulaBuilderDialog.java
+++ coreplugins/branches/eqn_attribs/browser/src/browser/ui/FormulaBuilderDialog.java
@@ -28,55 +28,161 @@
 package browser.ui;
 
 
+import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Frame;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import browser.DataObjectType;
 import browser.DataTableModel;
 
+import cytoscape.data.eqn_attribs.AttribFunction;
+import cytoscape.data.eqn_attribs.AttribParser;
+import cytoscape.data.eqn_attribs.Parser;
+
+import org.jdesktop.layout.GroupLayout;
+
 
 public class FormulaBuilderDialog extends JDialog {
-	private JButton OKButton;
+	private JComboBox functionComboBox = null;
+	private JLabel usageLabel = null;
+	private JTextField formulaTextField = null;
+	private JButton okButton = null;
+	private JButton cancelButton = null;
+	private AttribFunction function = null;
+	private Map<String, AttribFunction> stringToFunctionMap;
 
 
 	public FormulaBuilderDialog(final DataTableModel tableModel, final DataObjectType tableObjectType,
 	                            final Frame parent)
 	{
 		super(parent);
-		final FormulaBuilderPanel formulaBuilderPanel = new FormulaBuilderPanel(tableModel, tableObjectType);
 		this.setTitle("Creating a formula for: " + tableObjectType.getDisplayName());
-		this.setAlwaysOnTop(true);
-		
-		setLayout(new GridBagLayout());
-		
-		OKButton = new JButton("OK");
-		OKButton.setPreferredSize(new Dimension(70, 20));
 
-		GridBagConstraints gridBagConstraints = new GridBagConstraints();
-
-		gridBagConstraints = new GridBagConstraints();
-		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 0;
-		gridBagConstraints.fill = GridBagConstraints.BOTH;
-		gridBagConstraints.weightx = 1.0;
-		gridBagConstraints.weighty = 1.0;
-		gridBagConstraints.insets = new java.awt.Insets(10, 10, 10, 10);
-		add(formulaBuilderPanel, gridBagConstraints);
-
-		pack();
-		this.setModal(false);
+		this.stringToFunctionMap = new HashMap<String, AttribFunction>();
 		
-		OKButton.addActionListener(new ActionListener() {
+		final Container panel = getContentPane();
+		final GroupLayout groupLayout = new GroupLayout(panel);
+		panel.setLayout(groupLayout);
+
+		initFunctionComboBox(panel);
+		initUsageLabel(panel);
+		initFormulaTextField(panel);
+		initOkButton(panel);
+		initCancelButton(panel);
+
+		setSize(600, 150);
+
+		initLayout(groupLayout);
+	}
+
+	private void initFunctionComboBox(final Container panel) {
+		functionComboBox = new JComboBox();
+		panel.add(functionComboBox);
+		functionComboBox.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					functionSelected();
+				}
+			});
+
+		final AttribParser parser = Parser.getParser();
+		final Set<AttribFunction> functions = parser.getRegisteredFunctions();
+		final String[] functionNames = new String[functions.size()];
+		int index = 0;
+		for (final AttribFunction function : functions) {
+			functionNames[index] = function.getName() + ": " + function.getFunctionSummary();
+			stringToFunctionMap.put(functionNames[index], function);
+			++index;
+		}
+
+		Arrays.sort(functionNames);
+		for (final String functionName : functionNames)
+			functionComboBox.addItem(functionName);
+
+		functionComboBox.setEditable(false);
+
+		if (functionComboBox.getItemCount() != 0) {
+			functionComboBox.setSelectedIndex(0);
+			function = stringToFunctionMap.get(functionNames[0]);
+		}
+	}
+
+	private void initUsageLabel(final Container panel) {
+		usageLabel = new JLabel();
+		panel.add(usageLabel);
+		if (function != null)
+			usageLabel.setText(function.getUsageDescription());
+	}
+
+	private void initFormulaTextField(final Container panel) {
+		formulaTextField = new JTextField(40);
+		panel.add(formulaTextField);
+		formulaTextField.setEditable(false);
+		if (function != null)
+			formulaTextField.setText(function.getName() + "(");
+	}
+
+	private void initOkButton(final Container panel) {
+		okButton = new JButton("OK");
+		panel.add(okButton);
+		okButton.setEnabled(false);
+		okButton.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					dispose();
 				}
 			});
+	}
+
+	private void initCancelButton(final Container panel) {
+		cancelButton = new JButton("Cancel");
+		panel.add(okButton);
+		cancelButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+	}
+
+	private void functionSelected() {
+		final String funcName = (String)functionComboBox.getSelectedItem();
+		if (funcName == null || formulaTextField == null || usageLabel == null)
+			return;
+
+		function = stringToFunctionMap.get(funcName);
+		formulaTextField.setText(function.getName() + "(");
+		usageLabel.setText(function.getUsageDescription());
+	}
+
+	private void initLayout(final GroupLayout groupLayout) {
+		// 1. vertical layout
+		groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
+					     .add(functionComboBox)
+					     .add(usageLabel)
+					     .add(formulaTextField)
+					     .add(groupLayout.createParallelGroup(GroupLayout.BASELINE)
+						       .add(okButton)
+						       .add(cancelButton)));
+
+		// 2. horizontal layout
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(GroupLayout.CENTER)
+					       .add(functionComboBox)
+					       .add(usageLabel)
+					       .add(formulaTextField)
+					       .add(groupLayout.createSequentialGroup()
+							 .add(okButton)
+							 .add(cancelButton)));
 	}
 }
