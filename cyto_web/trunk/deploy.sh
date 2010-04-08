270f330aa93d6a661106f94738cb55e173c38b72
apico <apico@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-04-08 00:52:55 +0000

    added prompt for sourceforge username
    git-svn-id: http://chianti.ucsd.edu/svn@19838 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/deploy.sh
+++ cyto_web/trunk/deploy.sh
@@ -4,6 +4,10 @@
 #  Configure for production
 ant config_prod
 
+# Collect user name
+echo "Sourceforge username: "
+read name
+
 #  Issue rsync command
-rsync -av --exclude ".svn/" --exclude "deploy.sh" --exclude "deploy_enoteca.sh" --exclude "build.xml" . mes5k,cytoscape@web.sourceforge.net:htdocs/
+rsync -av --exclude ".svn/" --exclude "deploy.sh" --exclude "deploy_enoteca.sh" --exclude "build.xml" . $name,cytoscape@web.sourceforge.net:htdocs/
 
