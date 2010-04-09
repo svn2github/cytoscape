e84b3821c369c5444243512e89ee02747a2682c9
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-04-09 16:09:58 +0000

    Work in progress.
    git-svn-id: http://chianti.ucsd.edu/svn@19856 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/branches/eqn_attribs/browser/src/browser/ui/FormulaBuilderDialog.java
+++ coreplugins/branches/eqn_attribs/browser/src/browser/ui/FormulaBuilderDialog.java
@@ -46,7 +46,9 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -55,6 +57,8 @@ import browser.DataTableModel;
 
 import cytoscape.data.eqn_attribs.AttribFunction;
 import cytoscape.data.eqn_attribs.AttribParser;
+import cytoscape.data.eqn_attribs.AttribToken;
+import cytoscape.data.eqn_attribs.AttribTokeniser;
 import cytoscape.data.eqn_attribs.EquationUtil;
 import cytoscape.data.eqn_attribs.Parser;
 
@@ -172,16 +176,36 @@ public class FormulaBuilderDialog extends JDialog {
 					final String currentFormula = formulaTextField.getText();
 					final String constExpr = constantValuesTextField.getText();
 					if (constExpr != null && !constExpr.isEmpty()) {
+						final List<Class> possibleArgTypes = getPossibleNextArgumentTypes();
+						if (!expressionIsValid(possibleArgTypes, constExpr))
+							return;
+
 						formulaTextField.setText(currentFormula + constExpr);
 						constantValuesTextField.setText("");
 					}
 					else {
 						final String attribName = (String)attribNamesComboBox.getSelectedItem();
-						formulaTextField.setText(currentFormula + EquationUtil.attribNameAsReference(attribName));
+						formulaTextField.setText(currentFormula
+						                         + EquationUtil.attribNameAsReference(attribName));
 					}
 				}
 			});
 		argumentPanel.add(addButton);
+	}
+
+	private boolean expressionIsValid(final List<Class> validArgTypes, final String expression) {
+		final StringBuilder errorMessage = new StringBuilder(30);
+		final Class expressionType = parseSimpleExpression(expression, errorMessage);
+		if (expression == null) {
+			displayErrorMessage(errorMessage.toString());
+			return false;
+		}
+
+		if (validArgTypes.contains(expressionType))
+			return true;
+
+		displayErrorMessage("Expression is of an incompatible data type!");
+		return false;
 	}
 
 	/**
@@ -189,17 +213,22 @@ public class FormulaBuilderDialog extends JDialog {
 	 *  arguments) attribute names.
 	 */
 	private void updateAttribNamesComboBox() {
-		// Get the set of allowed types for the next argument:
-		final Class[] leadingArgsAsArray = new Class[leadingArgs.size()];
-		leadingArgs.toArray(leadingArgsAsArray);
-		final List<Class> possibleArgTypes = function.getPossibleArgTypes(leadingArgsAsArray);
-		
+		final List<Class> possibleArgTypes = getPossibleNextArgumentTypes();
 		final ArrayList possibleAttribNames = new ArrayList(20);
 		for (final String attribName : attribNamesAndTypes.keySet()) {
 			final Class attribType = attribNamesAndTypes.get(attribName);
 			if (isTypeCompatible(possibleArgTypes, attribType))
 				attribNamesComboBox.addItem(attribName);
 		}
+	}
+
+	/**
+	 *  @returns the set of allowed types for the next argument or null if no additional argument is valid
+	 */
+	private List<Class> getPossibleNextArgumentTypes() {
+		final Class[] leadingArgsAsArray = new Class[leadingArgs.size()];
+		leadingArgs.toArray(leadingArgsAsArray);
+		return function.getPossibleArgTypes(leadingArgsAsArray);
 	}
 
 	private boolean isTypeCompatible(final List<Class> allowedArgumentTypes, final Class attribType) {
@@ -260,5 +289,116 @@ public class FormulaBuilderDialog extends JDialog {
 					       .add(groupLayout.createSequentialGroup()
 							 .add(okButton)
 							 .add(cancelButton)));
+	}
+
+	/**
+	 *  Parses very simple expressions of the form: "constant" or "constant1 operator constant2".
+	 *  @returns either the type of the expression if there was no error or null if there was an error
+	 *  In case of an error "errorMessage" will contain the error message after the call.
+	 */
+	private Class parseSimpleExpression(final String expression, final StringBuilder errorMessage) {
+		final AttribTokeniser scanner = new AttribTokeniser(expression);
+
+		AttribToken token1 = scanner.getToken();
+		if (token1 == AttribToken.ERROR) {
+			errorMessage.append(scanner.getErrorMsg());
+			return null;
+		}
+
+		AttribToken token2 = scanner.getToken();
+		if (token2 == AttribToken.ERROR) {
+			errorMessage.append(scanner.getErrorMsg());
+			return null;
+		}
+
+		if (token2 == AttribToken.EOS) {
+			switch (token1) {
+			case STRING_CONSTANT:
+				return String.class;
+			case FLOAT_CONSTANT:
+				return Double.class;
+			case BOOLEAN_CONSTANT:
+				return Boolean.class;
+			default:
+				errorMessage.append("Invalid argument \"" + expression
+				                    + "\"!\nPlease enter a constant or a constant followed by an operator and another constant.");
+				return null;
+			}
+		}
+
+		if (token2.isStringOperator()) {
+			if (token1 != AttribToken.STRING_CONSTANT) {
+				errorMessage.append("String operator applied to a non-String operand!");
+				return null;
+			}
+
+			token2 = scanner.getToken();
+			if (token2 != AttribToken.STRING_CONSTANT) {
+				errorMessage.append("String operator applied to a non-String operand!");
+				return null;
+			}
+
+			token2 = scanner.getToken();
+			if (token2 != AttribToken.EOS) {
+				errorMessage.append("Invalid string expression!");
+				return null;
+			}
+
+			return String.class;
+		}
+
+		if (token2.isArithmeticOperator()) {
+			if (token1 != AttribToken.FLOAT_CONSTANT) {
+				errorMessage.append("Numeric operator applied to a non-numeric operand!");
+				return null;
+			}
+
+			token2 = scanner.getToken();
+			if (token2 != AttribToken.FLOAT_CONSTANT) {
+				errorMessage.append("Numeric operator applied to a non-numeric operand!");
+				return null;
+			}
+
+			token2 = scanner.getToken();
+			if (token2 != AttribToken.EOS) {
+				errorMessage.append("Invalid arithmetic expression!");
+				return null;
+			}
+
+			return Double.class;
+		}
+
+		if (token2.isComparisonOperator()) {
+			final AttribToken operatorToken = token2;
+
+			token2 = scanner.getToken();
+			if (token1 != token2) {
+				errorMessage.append("Comparsion operator applied to two incompatible operands!");
+				return null;
+			}
+
+			if (token1 == AttribToken.BOOLEAN_CONSTANT && token2 == AttribToken.BOOLEAN_CONSTANT) {
+				if (operatorToken != AttribToken.EQUAL && operatorToken != AttribToken.NOT_EQUAL) {
+					errorMessage.append("Only equality or inequality comparisions are valid for booelan operands!");
+					return null;
+				}
+				return Boolean.class;
+			}
+
+			if (token1 != AttribToken.FLOAT_CONSTANT && token1 != AttribToken.STRING_CONSTANT) {
+				errorMessage.append("Comparsion are only allowed between 2 string values or 2 numeric values!");
+				return null;
+			}
+
+			return Boolean.class;
+		}
+
+		errorMessage.append("Invalid expression!");
+		return null;
+	}
+
+	private static void displayErrorMessage(final String errorMessage) {
+		JOptionPane.showMessageDialog(new JFrame(), errorMessage, "Error",
+		                              JOptionPane.ERROR_MESSAGE);
 	}
 }
