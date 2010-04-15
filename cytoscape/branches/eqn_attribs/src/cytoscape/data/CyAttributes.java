8cfba53d70f66274db33c2f699d654c926198ec4
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-04-15 19:47:12 +0000

    Updated the copyright message.
    git-svn-id: http://chianti.ucsd.edu/svn@19906 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/branches/eqn_attribs/src/cytoscape/data/CyAttributes.java
+++ cytoscape/branches/eqn_attribs/src/cytoscape/data/CyAttributes.java
@@ -1,14 +1,7 @@
 /*
   File: CyAttributes.java
 
-  Copyright (c) 2006, The Cytoscape Consortium (www.cytoscape.org)
-
-  The Cytoscape Consortium is:
-  - Institute for Systems Biology
-  - University of California San Diego
-  - Memorial Sloan-Kettering Cancer Center
-  - Institut Pasteur
-  - Agilent Technologies
+  Copyright (c) 2006, 2010, The Cytoscape Consortium (www.cytoscape.org)
 
   This library is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published
