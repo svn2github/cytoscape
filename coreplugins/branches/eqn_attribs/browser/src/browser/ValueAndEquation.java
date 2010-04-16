fddba691ba96f05f29ef2e8e893becea808d9544
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-04-16 16:10:31 +0000

    Removed incorrect copyright years.
    git-svn-id: http://chianti.ucsd.edu/svn@19923 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/branches/eqn_attribs/browser/src/browser/ValueAndEquation.java
+++ coreplugins/branches/eqn_attribs/browser/src/browser/ValueAndEquation.java
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2006, 2007, 2010 The Cytoscape Consortium (www.cytoscape.org)
+ Copyright (c) 2010 The Cytoscape Consortium (www.cytoscape.org)
 
  This library is free software; you can redistribute it and/or modify it
  under the terms of the GNU Lesser General Public License as published
