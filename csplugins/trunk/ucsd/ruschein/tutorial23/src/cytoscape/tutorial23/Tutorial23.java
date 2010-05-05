520d79f21095c6e1a75403fd38f539f5e936c42c
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-05-05 22:52:42 +0000

    Final simplified version.
    git-svn-id: http://chianti.ucsd.edu/svn@20086 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/ruschein/tutorial23/src/cytoscape/tutorial23/Tutorial23.java
+++ csplugins/trunk/ucsd/ruschein/tutorial23/src/cytoscape/tutorial23/Tutorial23.java
@@ -16,23 +16,7 @@ import cytoscape.util.CytoscapeAction;
 
 public class Tutorial23 extends CytoscapePlugin {
 	public Tutorial23() {
-		// Create an Action, add the action to Cytoscape menu
-		MyPluginAction action = new MyPluginAction(this);
-		Cytoscape.getDesktop().getCyMenus().addCytoscapeAction((CytoscapeAction) action);
-	}
-	
-	public class MyPluginAction extends CytoscapeAction {
-		public MyPluginAction(final Tutorial23 myPlugin) {
-			// Add the menu item under menu pulldown "Plugins"
-			super("Tutorial23 - Attribute Function Example");
-			setPreferredMenu("Plugins");
-		}
-		
-		public void actionPerformed(ActionEvent e) {
-			final AttribParser theParser = Parser.getParser();
-			theParser.registerFunction(new IXor());
-			JOptionPane.showMessageDialog(null, "The integer-XOR attribute function IXOR(i1,i2) has been registered!",
-			                              "Plugin Result", JOptionPane.INFORMATION_MESSAGE);
-		}
+		final AttribParser theParser = Parser.getParser();
+		theParser.registerFunction(new IXor());
 	}	
 }
