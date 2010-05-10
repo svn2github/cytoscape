18ee47952dee4d8f115c296598a32607874e57a8
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-05-10 21:26:41 +0000

    Scaling enum.
    git-svn-id: http://chianti.ucsd.edu/svn@20167 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/pwang/jActiveModules3/src/csplugins/jActiveModules/dialogs/ScalingMethodX.java
@@ -0,0 +1,25 @@
+package csplugins.jActiveModules.dialogs;
+
+
+public enum ScalingMethodX {
+	NONE("none (prescaled)"),
+	LINEAR_LOWER("linear/lower"),
+	LINEAR_UPPER("linear/upper"),
+	RANK_LOWER("rank/lower"),
+	RANK_UPPER("rank/upper");
+
+	private String displayString;
+
+	ScalingMethodX(final String displayString) { this.displayString = displayString; }
+
+	public String getDisplayString() { return displayString; }
+
+	static public ScalingMethodX getEnumValue(final String displayString) {
+		for (final ScalingMethodX method : ScalingMethodX.values()) {
+			if (method.getDisplayString().equals(displayString))
+				return method;
+		}
+
+		throw new IllegalStateException("unknown string representation: \"" + displayString + "\"!");
+	}
+}
