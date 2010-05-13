365e6e12eafaf0029712152c4c5a8e3f83050a8b
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-05-13 21:42:07 +0000

    more complete unit tests
    git-svn-id: http://chianti.ucsd.edu/svn@20212 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- corelibs/trunk/render.stateful/tests/cytoscape/render/stateful/GraphRendererTest.java
+++ corelibs/trunk/render.stateful/tests/cytoscape/render/stateful/GraphRendererTest.java
@@ -52,18 +52,8 @@ public class GraphRendererTest extends TestCase {
 		grafx = new GraphGraphics( img, false);
 	}
 
-/*
-    	boolean GraphRenderer.computeEdgeEndpoints(final GraphGraphics grafx,
-                                                     final float[] srcNodeExtents,
-                                                     final byte srcNodeShape, final byte srcArrow,
-                                                     final float srcArrowSize, EdgeAnchors anchors,
-                                                     final float[] trgNodeExtents,
-                                                     final byte trgNodeShape, final byte trgArrow,
-                                                     final float trgArrowSize,
-                                                     final float[] rtnValSrc,
-                                                     final float[] rtnValTrg);
-*/
-	public void testComputeEdgeEndpoints() {
+	// normal without anchors
+	public void testComputeEdgeEndpoints_1() {
 		float[] srcNodeExtents = {10.0f,10.0f,20.0f,20.0f};
 		float[] trgNodeExtents = {110.0f,110.0f,120.0f,120.0f};
 		float[] rtnValSrc = new float[2];
@@ -76,15 +66,209 @@ public class GraphRendererTest extends TestCase {
 										  GraphGraphics.ARROW_DELTA,7.0f,
 										  rtnValSrc,rtnValTrg);
 
-		System.out.println("source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
-		System.out.println("target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
-		//source X: 18.535534  Y: 18.535534
-		//target X: 110.0  Y: 110.0
-		assertEquals( rtnValSrc[0], 18.5355f, 0.001f );
-		assertEquals( rtnValSrc[1], 18.5355f, 0.001f );
-		assertEquals( rtnValTrg[0], 110.000f, 0.001f );
-		assertEquals( rtnValTrg[1], 110.000f, 0.001f );
+		System.out.println("1 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("1 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		//1 source X: 18.535534  Y: 18.535534
+		//1 target X: 110.0  Y: 110.0
+		assertEquals( 18.5355f, rtnValSrc[0], 0.001f );
+		assertEquals( 18.5355f, rtnValSrc[1], 0.001f );
+		assertEquals( 110.0f, rtnValTrg[0], 0.001f );
+		assertEquals( 110.0f, rtnValTrg[1], 0.001f );
 	}
+
+	// normal with anchors
+	public void testComputeEdgeEndpoints_2() {
+		float[] srcNodeExtents = {10.0f,10.0f,20.0f,20.0f};
+		float[] trgNodeExtents = {110.0f,110.0f,120.0f,120.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_TRIANGLE,
+                                          GraphGraphics.ARROW_NONE,5.0f,
+										  new SingleEdgeAnchor(new float[]{100.0f,100.0f}),
+										  trgNodeExtents,GraphGraphics.SHAPE_VEE,
+										  GraphGraphics.ARROW_TEE,7.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("2 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("2 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 2 source X: 20.0  Y: 20.0
+		// 2 target X: 110.0  Y: 110.0
+		assertEquals( 20.0f, rtnValSrc[0], 0.001f );
+		assertEquals( 20.0f, rtnValSrc[1], 0.001f );
+		assertEquals( 110.0f, rtnValTrg[0], 0.001f );
+		assertEquals( 110.0f, rtnValTrg[1], 0.001f );
+	}
+
+	
+	// srcExtents width zero
+	public void testComputeEdgeEndpoints_3() {
+		float[] srcNodeExtents = {10.0f,10.0f,10.0f,20.0f};
+		float[] trgNodeExtents = {110.0f,110.0f,120.0f,120.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_TRIANGLE,
+                                          GraphGraphics.ARROW_NONE,5.0f,
+										  null /*anchors*/,
+										  trgNodeExtents,GraphGraphics.SHAPE_VEE,
+										  GraphGraphics.ARROW_TEE,0.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("3 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("3 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 3 source X: 10.0  Y: 15.0
+		// 3 target X: 110.22727  Y: 110.454544
+		assertEquals( 10.0f, rtnValSrc[0], 0.001f );
+		assertEquals( 15.0f, rtnValSrc[1], 0.001f );
+		assertEquals( 110.227f, rtnValTrg[0], 0.001f );
+		assertEquals( 110.455f, rtnValTrg[1], 0.001f );
+	}
+
+	// srcExtents height zero
+	public void testComputeEdgeEndpoints_4() {
+		float[] srcNodeExtents = {10.0f,10.0f,20.0f,10.0f};
+		float[] trgNodeExtents = {110.0f,110.0f,120.0f,120.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_TRIANGLE,
+                                          GraphGraphics.ARROW_NONE,5.0f,
+										  new SingleEdgeAnchor(new float[]{75.0f,150.0f}),
+										  trgNodeExtents,GraphGraphics.SHAPE_VEE,
+										  GraphGraphics.ARROW_TEE,7.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("4 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("4 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 4 source X: 15.0  Y: 10.0
+		// 4 target X: 113.26087  Y: 116.521736
+		assertEquals( 15.0f, rtnValSrc[0], 0.001f );
+		assertEquals( 10.0f, rtnValSrc[1], 0.001f );
+		assertEquals( 113.261f, rtnValTrg[0], 0.001f );
+		assertEquals( 116.522f, rtnValTrg[1], 0.001f );
+	}
+	
+	// trgExtents width zero
+	public void testComputeEdgeEndpoints_5() {
+		float[] srcNodeExtents = {10.0f,10.0f,20.0f,20.0f};
+		float[] trgNodeExtents = {110.0f,110.0f,110.0f,120.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_HEXAGON,
+                                          GraphGraphics.ARROW_DIAMOND,8.0f,
+										  null /*anchors*/,
+										  trgNodeExtents,GraphGraphics.SHAPE_OCTAGON,
+										  GraphGraphics.ARROW_ARROWHEAD,57.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("5 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("5 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 5 source X: 17.938145  Y: 18.092783
+		// 5 target X: 110.0  Y: 115.0
+		assertEquals( 17.938f, rtnValSrc[0], 0.001f );
+		assertEquals( 18.093f, rtnValSrc[1], 0.001f );
+		assertEquals( 110.0f, rtnValTrg[0], 0.001f );
+		assertEquals( 115.0f, rtnValTrg[1], 0.001f );
+	}
+
+	// trgExtents height zero
+	public void testComputeEdgeEndpoints_6() {
+		float[] srcNodeExtents = {10.0f,10.0f,20.0f,20.0f};
+		float[] trgNodeExtents = {110.0f,110.0f,120.0f,110.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_PARALLELOGRAM,
+                                          GraphGraphics.ARROW_HALF_BOTTOM,1.0f,
+										  new SingleEdgeAnchor(new float[]{0.0f,50.0f}),
+										  trgNodeExtents,GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
+										  GraphGraphics.ARROW_HALF_TOP,17.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("6 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("6 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 6 source X: 13.125  Y: 19.375
+		// 6 target X: 115.0  Y: 110.0
+		assertEquals( 13.125f, rtnValSrc[0], 0.001f );
+		assertEquals( 19.375f, rtnValSrc[1], 0.001f );
+		assertEquals( 115.0f, rtnValTrg[0], 0.001f );
+		assertEquals( 110.0f, rtnValTrg[1], 0.001f );
+	}
+
+	// one node within the other - different center
+	public void testComputeEdgeEndpoints_7() {
+		float[] srcNodeExtents = {10.0f,10.0f,20.0f,20.0f};
+		float[] trgNodeExtents = {12.0f,12.0f,17.0f,17.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_PARALLELOGRAM,
+                                          GraphGraphics.ARROW_HALF_BOTTOM,1.0f,
+										  new SingleEdgeAnchor(new float[]{0.0f,50.0f}),
+										  trgNodeExtents,GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
+										  GraphGraphics.ARROW_HALF_TOP,17.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("7 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("7 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 7 source X: 13.125  Y: 19.375
+		// 7 target X: 13.478873  Y: 17.0
+		assertEquals( 13.125f, rtnValSrc[0], 0.001f );
+		assertEquals( 19.375f, rtnValSrc[1], 0.001f );
+		assertEquals( 13.479f, rtnValTrg[0], 0.001f );
+		assertEquals( 17.0f, rtnValTrg[1], 0.001f );
+	}
+
+	// one node within the other - same center
+	public void testComputeEdgeEndpoints_8() {
+		float[] srcNodeExtents = {10.0f,10.0f,20.0f,20.0f};
+		float[] trgNodeExtents = {12.0f,12.0f,18.0f,18.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_PARALLELOGRAM,
+                                          GraphGraphics.ARROW_HALF_BOTTOM,1.0f,
+										  new SingleEdgeAnchor(new float[]{0.0f,50.0f}),
+										  trgNodeExtents,GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
+										  GraphGraphics.ARROW_HALF_TOP,17.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("8 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("8 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 8 source X: 13.125  Y: 19.375
+		// 8 target X: 13.714286  Y: 18.0
+		assertEquals( 13.125f, rtnValSrc[0], 0.001f );
+		assertEquals( 19.375f, rtnValSrc[1], 0.001f );
+		assertEquals( 13.714f, rtnValTrg[0], 0.001f );
+		assertEquals( 18.0f, rtnValTrg[1], 0.001f );
+	}
+
+	// nodes the same
+	public void testComputeEdgeEndpoints_9() {
+		float[] srcNodeExtents = {10.0f,10.0f,20.0f,20.0f};
+		float[] trgNodeExtents = {10.0f,10.0f,20.0f,20.0f};
+		float[] rtnValSrc = new float[2];
+		float[] rtnValTrg = new float[2];
+		boolean ret = GraphRenderer.computeEdgeEndpoints(grafx,
+		                                  srcNodeExtents,GraphGraphics.SHAPE_PARALLELOGRAM,
+                                          GraphGraphics.ARROW_HALF_BOTTOM,1.0f,
+										  new SingleEdgeAnchor(new float[]{0.0f,50.0f}),
+										  trgNodeExtents,GraphGraphics.SHAPE_ROUNDED_RECTANGLE,
+										  GraphGraphics.ARROW_HALF_TOP,17.0f,
+										  rtnValSrc,rtnValTrg);
+
+		System.out.println("9 source X: " + rtnValSrc[0] + "  Y: " + rtnValSrc[1]);
+		System.out.println("9 target X: " + rtnValTrg[0] + "  Y: " + rtnValTrg[1]);
+		// 9 source X: 13.125  Y: 19.375
+		// 9 target X: 12.857142  Y: 20.0
+		assertEquals( 13.125f, rtnValSrc[0], 0.001f );
+		assertEquals( 19.375f, rtnValSrc[1], 0.001f );
+		assertEquals( 12.857f, rtnValTrg[0], 0.001f );
+		assertEquals( 20.0f, rtnValTrg[1], 0.001f );
+	}
+
 
 	private class SingleEdgeAnchor implements EdgeAnchors {
 		private float[] pt;	
