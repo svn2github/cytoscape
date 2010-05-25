4157d0bf3382ce549515a3a7cf6abebe4f7b2366
ghuck <ghuck@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-05-25 14:08:14 +0000

    Added Spring Embedded Labels Layout files (copied from Biolayouts)
    git-svn-id: http://chianti.ucsd.edu/svn@20297 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/ghuck/LabelPositioningPlugin/plugin/src/LabelLayoutPlugin.java
+++ csplugins/trunk/soc/ghuck/LabelPositioningPlugin/plugin/src/LabelLayoutPlugin.java
@@ -1,22 +1,21 @@
-/**********************************************
-  Copyright (C) Victoria Mui, 2008
-  Copyright (C) Gerardo Huck, 2010
-
-
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published 
-  by the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
-  
-  You should have received a copy of the GNU Lesser General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**********************************************/
+/**
+* Copyright (C) Victoria Mui, 2008
+* Copyright (C) Gerardo Huck, 2010
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*  
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*  
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 
 package cytoscape.layout.label;
@@ -68,8 +67,12 @@ public class LabelLayoutPlugin extends CytoscapePlugin {
      */
 	public LabelLayoutPlugin() {
 
-		// Add this layout to the Layout menu under "Label Layouts".
+		// Adds LabelForce-DirectedLayout to the Layout menu under "Label Layouts".
 		CyLayouts.addLayout(new LabelForceDirectedLayout(), 
+				"Label Layouts");
+
+		// Adds LabelBioLayoutKKAlgorithm to the Layout menu under "Label Layouts".
+		CyLayouts.addLayout(new LabelBioLayoutKKAlgorithm(true), 
 				"Label Layouts");
 
 	}
