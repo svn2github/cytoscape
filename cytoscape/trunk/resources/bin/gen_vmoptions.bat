f40562e6546e0eb224802bcc744d0d9f34d23c2a
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-06-01 23:06:19 +0000

    New attempt.
    git-svn-id: http://chianti.ucsd.edu/svn@20396 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/resources/bin/gen_vmoptions.bat
+++ cytoscape/trunk/resources/bin/gen_vmoptions.bat
@@ -3,8 +3,10 @@
 :: Generates the Cytoscape.vmoptions file based on whether
 :: we're dealing with a 32 bit or 64 bit JVM.
 
-java -version 2>&1 | FINDSTR /I 64-Bit > NUL:
-IF NOT %errorlevel% == 0 GOTO 32bit 
+java -version 2>&1 | FINDSTR /I 64-Bit > %TMP%\MATCH.TXT
+IF /F %%A in ('DIR %TMP%\MATCH.TXT') DO (
+	IF %%~zA LSS 1 GOTO 32bit
+)
 
 
 :64bit
