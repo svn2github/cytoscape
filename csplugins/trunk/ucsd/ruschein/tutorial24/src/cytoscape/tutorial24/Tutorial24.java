5f87546954dfacfacb19cd39b755d2290f9ec810
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-06-09 15:30:19 +0000

    Removed junk due to this having started out as a copy of tutorial 23.
    git-svn-id: http://chianti.ucsd.edu/svn@20477 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/ruschein/tutorial24/src/cytoscape/tutorial24/Tutorial24.java
+++ csplugins/trunk/ucsd/ruschein/tutorial24/src/cytoscape/tutorial24/Tutorial24.java
@@ -4,14 +4,10 @@
 package cytoscape.tutorial24;
 
 
-import cytoscape.data.eqn_attribs.AttribParser;
-import cytoscape.data.eqn_attribs.Parser;
 import cytoscape.plugin.CytoscapePlugin;
 
 
 public class Tutorial24 extends CytoscapePlugin {
 	public Tutorial24() {
-		final AttribParser theParser = Parser.getParser();
-		theParser.registerFunction(new IXor());
 	}	
 }
