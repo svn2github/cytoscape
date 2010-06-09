8bd8eb785099dff333ee82a1e47e879802a504b2
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-06-09 15:55:28 +0000

    First completely working help tutorial.
    git-svn-id: http://chianti.ucsd.edu/svn@20478 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/ruschein/tutorial24/src/cytoscape/tutorial24/Tutorial24.java
+++ csplugins/trunk/ucsd/ruschein/tutorial24/src/cytoscape/tutorial24/Tutorial24.java
@@ -5,9 +5,32 @@ package cytoscape.tutorial24;
 
 
 import cytoscape.plugin.CytoscapePlugin;
+import cytoscape.view.CyHelpBroker;
+
+import java.net.URL;
+
+import javax.help.HelpSet;
 
 
 public class Tutorial24 extends CytoscapePlugin {
 	public Tutorial24() {
+		addHelp();
 	}	
+
+	/**
+	 *  Hook plugin help into the Cytoscape main help system:
+	 */
+	private void addHelp() {
+		final String HELP_SET_NAME = "/help/jhelpset";
+		final ClassLoader classLoader = Tutorial24.class.getClassLoader();
+		URL helpSetURL;
+		try {
+			helpSetURL = HelpSet.findHelpSet(classLoader, HELP_SET_NAME);
+			final HelpSet newHelpSet = new HelpSet(classLoader, helpSetURL);
+			if (!CyHelpBroker.addHelpSet(newHelpSet))
+				System.err.println("Tutorial24: Failed to add help set!");
+		} catch (final Exception e) {
+			System.err.println("Tutorial24: Could not find help set: \"" + HELP_SET_NAME + "!");
+		}
+	}
 }
