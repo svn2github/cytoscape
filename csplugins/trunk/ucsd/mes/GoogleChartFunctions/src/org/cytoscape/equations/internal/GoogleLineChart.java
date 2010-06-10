37d7a6fb588b942c057a727a5b0e36494e6cc83a
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-06-10 00:50:30 +0000

    prototype of google chart api based functions
    git-svn-id: http://chianti.ucsd.edu/svn@20488 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/mes/GoogleChartFunctions/src/org/cytoscape/equations/internal/GoogleLineChart.java
@@ -0,0 +1,186 @@
+/*
+  Copyright (c) 2010, The Cytoscape Consortium (www.cytoscape.org)
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+package org.cytoscape.equations.internal;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import org.cytoscape.equations.Function;
+import org.cytoscape.equations.FunctionUtil;
+import cytoscape.util.ProbabilityScaler;
+import cytoscape.util.ScalingMethod;
+
+import static com.googlecode.charts4j.Color.*;
+import com.googlecode.charts4j.*;
+
+public class GoogleLineChart implements Function {
+	/**
+	 *  Used to parse the function string.  This name is treated in a case-insensitive manner!
+	 *  @return the name by which you must call the function when used in an attribute equation.
+	 */
+	public String getName() { return "GLINECHART"; }
+
+	/**
+	 *  Used to provide help for users.
+	 *  @return a description of what this function does
+	 */
+	public String getFunctionSummary() { return "Creates a line chart URL based on the supplied arguments."; }
+
+	/**
+	 *  Used to provide help for users.
+	 *  @return a description of how to use this function
+	 */
+	public String getUsageDescription() { return "Call this with \"GLINECHART(title,list)\""; }
+
+	public Class getReturnType() { return String.class; }
+
+	/**
+	 *  @return String.class or null if the args passed in have the wrong arity or a type mismatch was found
+	 */
+	public Class validateArgTypes(final Class[] argTypes) {
+		if (argTypes.length != 2)
+			return null;
+
+		if ( argTypes[0] != String.class )
+			return null;
+
+		if ( !FunctionUtil.isSomeKindOfList(argTypes[1]) )
+			return null;
+
+//		if ( !FunctionUtil.isSomeKindOfList(argTypes[1]) )
+//			return null;
+
+		return String.class;
+	}
+
+	/**
+	 *  @param args the function arguments which must be either one or two objects of type Double
+	 *  @return the result of the function evaluation which is the natural logarithm of the first argument
+	 *  @throws ArithmeticException 
+	 *  @throws IllegalArgumentException thrown if any of the arguments is not of type Boolean
+	 */
+	private double[] extractDoubles(Object o) {
+		List<Double> ld = (List<Double>)o;
+
+		float[] data = new float[ld.size()];
+		int i = 0;
+		for ( Double d : ld )
+			data[i++] = d.floatValue();
+
+		float[] data2 = ProbabilityScaler.scale((float[])data,ScalingMethod.LINEAR_LOWER,new StringBuilder());
+		double[] ret = new double[data2.length];
+		i = 0;
+		for (float f : data2)
+			ret[i++] = 100.0 * f;  // scaled by 100 for Google
+
+		return ret; 
+	}
+
+	public Object evaluateFunction(final Object[] args) 
+		throws IllegalArgumentException, ArithmeticException {
+		final String title = FunctionUtil.getArgAsString(args[0]);
+		final double[] data1 = extractDoubles( args[1] );
+
+
+        Line line1 = Plots.newLine(Data.newData(data1), Color.newColor("CA3D05"), "Data 1");
+        line1.setLineStyle(LineStyle.newLineStyle(3, 1, 0));
+//        line1.addShapeMarkers(Shape.DIAMOND, Color.newColor("CA3D05"), 12);
+//        line1.addShapeMarkers(Shape.DIAMOND, Color.WHITE, 8);
+
+//        Line line2 = Plots.newLine(Data.newData(competition), SKYBLUE, "Competition.com");
+//        line2.setLineStyle(LineStyle.newLineStyle(3, 1, 0));
+//        line2.addShapeMarkers(Shape.DIAMOND, SKYBLUE, 12);
+//        line2.addShapeMarkers(Shape.DIAMOND, Color.WHITE, 8);
+
+
+        // Defining chart.
+//        LineChart chart = GCharts.newLineChart(line1, line2);
+        LineChart chart = GCharts.newLineChart(line1);
+        chart.setSize(100, 100);
+        chart.setTitle(title, WHITE, 10);
+//        chart.addHorizontalRangeMarker(40, 60, Color.newColor(RED, 30));
+//        chart.addVerticalRangeMarker(70, 90, Color.newColor(GREEN, 30));
+//        chart.setGrid(25, 25, 3, 2);
+
+        // Defining axis info and styles
+        AxisStyle axisStyle = AxisStyle.newAxisStyle(BLACK, 8, AxisTextAlignment.CENTER);
+        AxisLabels xAxis = AxisLabelsFactory.newAxisLabels("a", "b", "c", "d", "e", "f", "g");
+        xAxis.setAxisStyle(axisStyle);
+
+//        AxisLabels xAxis2 = AxisLabelsFactory.newAxisLabels("2007", "2007", "2008", "2008", "2008");
+//        xAxis2.setAxisStyle(axisStyle);
+
+//        AxisLabels xAxis3 = AxisLabelsFactory.newAxisLabels("Month", 50.0);
+//        xAxis3.setAxisStyle(AxisStyle.newAxisStyle(WHITE, 14, AxisTextAlignment.CENTER));
+
+        AxisLabels yAxis = AxisLabelsFactory.newAxisLabels("", "1", "2", "3", "4");
+        yAxis.setAxisStyle(axisStyle);
+
+//        AxisLabels yAxis2 = AxisLabelsFactory.newAxisLabels("Hits", 50.0);
+//        yAxis2.setAxisStyle(AxisStyle.newAxisStyle(WHITE, 14, AxisTextAlignment.CENTER));
+//        yAxis2.setAxisStyle(axisStyle);
+
+        // Adding axis info to chart.
+        chart.addXAxisLabels(xAxis);
+//        chart.addXAxisLabels(xAxis2);
+//        chart.addXAxisLabels(xAxis3);
+        chart.addYAxisLabels(yAxis);
+//        chart.addYAxisLabels(yAxis2);
+
+        // Defining background and chart fills.
+//        chart.setBackgroundFill(Fills.newSolidFill(Color.newColor("1F1D1D")));
+//        LinearGradientFill fill = Fills.newLinearGradientFill(0, Color.newColor("363433"), 100);
+//        fill.addColorAndOffset(Color.newColor("2E2B2A"), 0);
+//        chart.setAreaFill(fill);
+
+		return chart.toURLString(); 
+	}
+
+	/**
+	 *  Used with the equation builder.
+	 *
+	 *  @param leadingArgs the types of the arguments that have already been selected by the user.
+	 *  @return the set of arguments (must be a collection of String.class, Long.class, Double.class,
+	 *           Boolean.class and List.class) that are candidates for the next argument.  An empty
+	 *           set indicates that no further arguments are valid.
+	 */
+	public List<Class> getPossibleArgTypes(final Class[] leadingArgs) {
+		if (leadingArgs.length == 2)
+			return null;
+
+		final List<Class> possibleNextArgs = new ArrayList<Class>();
+
+		if (leadingArgs.length == 0) {
+			possibleNextArgs.add(String.class);
+		} else if (leadingArgs.length == 1) {
+			possibleNextArgs.add(List.class);
+		}
+
+		return possibleNextArgs;
+	}
+}
