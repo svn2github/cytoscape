f65578199f88a4fc8e302db8cd45c2119af07e66
ghuck <ghuck@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-06-14 21:26:54 +0000

    almost finished Label Partition Implementation. TODO: Change the use of LabelPosition (now deprecated).
    git-svn-id: http://chianti.ucsd.edu/svn@20537 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- coreplugins/branches/labelLayout/AutomaticLayout/src/csplugins/layout/LayoutLabelPartition.java
+++ coreplugins/branches/labelLayout/AutomaticLayout/src/csplugins/layout/LayoutLabelPartition.java
@@ -44,38 +44,155 @@ public class LayoutLabelPartition extends LayoutPartition {
     protected ArrayList<LayoutNode> allLayoutNodesArray;
     protected ArrayList<LayoutEdge> allLayoutEdgesArray;
 
+    protected double weightCoefficient = 1.0;
+    protected boolean moveNodes = false;
+    protected boolean selectedOnly = false;
+
     public LayoutLabelPartition(int nodeCount, int edgeCount) {
 	super(nodeCount, edgeCount);
     }
 
 
-    public LayoutLabelPartition createLabelPartition(LayoutPartition part) {
+    public LayoutLabelPartition(LayoutPartition part, 
+				double weightCoefficient, 
+				boolean moveNodes, 
+				boolean selectedOnly) {
 
-	// Create new empty partition
-	LayoutLabelPartition newPart = new LayoutLabelPartition(part.size(),
-								part.getEdgeList().size());
+	super(part.size(), part.getEdgeList().size());
+
+	this.weightCoefficient = weightCoefficient;
+	this.moveNodes = moveNodes;
+	this.selectedOnly = selectedOnly;
 
 	// Copy fields from part
-	newPart.nodeList = (ArrayList<LayoutNode>) part.nodeList.clone();
-	newPart.edgeList = (ArrayList<LayoutEdge>) part.edgeList.clone();
-	newPart.nodeToLayoutNode = (HashMap<CyNode,LayoutNode>) part.nodeToLayoutNode.clone();
-	newPart.partitionNumber = part.partitionNumber;
-	newPart.edgeWeighter = part.edgeWeighter;
+	this.nodeList = (ArrayList<LayoutNode>) part.nodeList.clone();
+	this.edgeList = (ArrayList<LayoutEdge>) part.edgeList.clone();
+	this.nodeToLayoutNode = (HashMap<CyNode,LayoutNode>) part.nodeToLayoutNode.clone();
+	this.partitionNumber = part.partitionNumber;
+	this.edgeWeighter = part.edgeWeighter;
 
-	newPart.maxX = part.maxX;
-	newPart.maxY = part.maxY;
-	newPart.minX = part.minX;
-	newPart.minY = part.minY;
-	newPart.width = part.width;
-	newPart.height = part.height;
+	this.maxX = part.maxX;
+	this.maxY = part.maxY;
+	this.minX = part.minX;
+	this.minY = part.minY;
+	this.width = part.width;
+	this.height = part.height;
 	
-	newPart.averageX = part.averageX;
-	newPart.averageY = part.averageY;
+	this.averageX = part.averageX;
+	this.averageY = part.averageY;
 	
-	newPart.lockedNodes = part.lockedNodes;
+	this.lockedNodes = part.lockedNodes;
 
-	// Returns the already initialized label layoutPartition
-	return newPart;
+	// Initialize 'label' fields
+	labelToParentMap = new HashMap<LayoutNode,LayoutNode>(part.size());
+       	allLayoutNodesArray = new ArrayList<LayoutNode>(part.size());
+	allLayoutEdgesArray = new ArrayList<LayoutEdge>(part.size());
+	
+	// This method handles the creation of LayoutLabelNodes, the mappings, etc
+	this.initializeLabels();
+
+	// Calculate weights for this partition using an overriding method
+	this.calculateEdgeWeights();
+    }
+
+    /**
+     * Handles the initization of all the infraestructure needed in order to use a 
+     * LayoutLabelPartition to layout labels, such as Label nodes creation, etc.
+     */
+    protected void initializeLabels() {	
+
+	for (LayoutNode ln: nodeList ) {
+
+	    // Creates a new LabelNode, child of ln
+	    LayoutLabelNodeImpl labelNode = new LayoutLabelNodeImpl((LayoutNodeImpl) ln);
+
+	    /* Unlock labelNode if:
+	     * - algorithm is to be applied to the entire network
+	     * - algorithm is to be applied to the selected nodes only, and ln
+	     * is selected
+	     * 
+	     * Unlock ln if:
+	     * - either of the above conditions is true, and the user has
+	     * specified that the network nodes are to be moved as well
+	     * 
+	     * Lock labelNode and/or ln otherwise. */
+	    if (!selectedOnly || !(ln.isLocked()) ) {
+		
+		labelNode.unLock();
+
+		if (!moveNodes && ln.isLocked()) {
+		    ln.unLock();
+		    lockedNodes--;
+		} 
+	    } else {
+		    labelNode.lock();
+		    lockedNodes++;
+	    }
+
+	    // Add labelNode -> ln 
+	    labelToParentMap.put(labelNode, ln);
+
+	    // Creates label Edge: ln <---> labelNode
+	    LayoutEdge labelEdge = new LayoutEdge();
+	    labelEdge.addNodes(ln, labelNode);
+
+	    // Adds it to edgeList
+	    edgeList.add(labelEdge);
+	}
+
+	// Adds all LabelNodes to nodeList
+	nodeList.addAll(labelToParentMap.keySet());
+
+    }
+
+    public void calculateEdgeWeights() {
+	
+	// -- First set labelEdges weights --
+
+	// Calculate maximum preexisting weight
+	Double maxWeight = new Double(Double.MIN_VALUE);
+
+	ListIterator<LayoutEdge>iter = edgeList.listIterator();
+
+	while (iter.hasNext()) {
+	    LayoutEdge edge = iter.next();
+	    
+	    // Only consider non label edges
+	    if (labelToParentMap.keySet().contains(edge.getSource()) || 
+		labelToParentMap.keySet().contains(edge.getTarget()) ) {
+		continue;
+	    }
+
+	    if (  edge.getWeight() > maxWeight ) {
+		maxWeight = edge.getWeight();
+	    }
+	}
+
+	// Value which will be used as weight
+	double weight = maxWeight *  weightCoefficient;
+
+	// Set all labelEdge weights
+	ListIterator<LayoutEdge>iter2 = edgeList.listIterator();
+
+	while (iter2.hasNext()) {
+	    LayoutEdge edge = iter2.next();
+	    
+	    // Only consider label edges
+	    if (labelToParentMap.keySet().contains(edge.getSource()) || 
+		labelToParentMap.keySet().contains(edge.getTarget()) ) {
+		
+		edge.setWeight(weight);		
+	    }
+	}
+
+
+	// -- Then call parent method to calculate all weights --
+	super.calculateEdgeWeights();
+    }
+
+
+    public Map<LayoutNode,LayoutNode> getLabelToParentMap() {
+	return labelToParentMap;
     }
 
 }
