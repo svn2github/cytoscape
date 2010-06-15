ea0b6fd2038fe53f816f3d56b323acc3a9a08493
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-06-15 21:21:07 +0000

    Added an additional test case for the RankScaler class.
    git-svn-id: http://chianti.ucsd.edu/svn@20547 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/tests/cytoscape/util/RankScalerTest.java
+++ cytoscape/trunk/tests/cytoscape/util/RankScalerTest.java
@@ -5,12 +5,12 @@ import junit.framework.*;
 
 
 public class RankScalerTest extends TestCase {
-	public void test() {
-		final Scaler linearScaler = new RankScaler();
+	public void test1() {
+		final Scaler rankScaler = new RankScaler();
 		final double[] unscaledValues = new double[] { 3.0, 10.2, 5.4, 3.0, 7.7 };
 		final double MIN = 0.0;
 		final double MAX = 1.0;
-		final double[] scaledValues = linearScaler.scale(unscaledValues, MIN, MAX);
+		final double[] scaledValues = rankScaler.scale(unscaledValues, MIN, MAX);
 		boolean foundMin = false;
 		boolean foundMax = false;
 		for (final double d : scaledValues) {
@@ -22,5 +22,17 @@ public class RankScalerTest extends TestCase {
 		}
 		assertFalse(foundMin);
 		assertFalse(foundMax);
+	}
+
+	public void test2() {
+		final Scaler rankScaler = new RankScaler();
+		final double[] unscaledValues = new double[] { 3.0, 3.0, 1.4, 7.7 };
+		final double MIN = 0.0;
+		final double MAX = 1.0;
+		final double[] scaledValues = rankScaler.scale(unscaledValues, MIN, MAX);
+		assertEquals(0.5, scaledValues[0], 1e-10);
+		assertEquals(0.5, scaledValues[1], 1e-10);
+		assertEquals(0.125, scaledValues[2], 1e-10);
+		assertEquals(0.875, scaledValues[3], 1e-10);
 	}
 }
