aa1a738474aecce44033de774d498a63aa2dcea1
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-06-15 22:59:38 +0000

    Add a meta tag no-index for search engine
    git-svn-id: http://chianti.ucsd.edu/svn@20552 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cyto_web/trunk/plugins1.php
+++ cyto_web/trunk/plugins1.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+	<meta name="robots" content="noindex" />
 	<title>Cytoscape 1.1 PlugIns</title> 
 	<link rel="stylesheet" type="text/css" media="screen" href="css/cytoscape.css">
 	<link rel="shortcut icon" href="images/cyto.ico">
@@ -10,8 +11,8 @@
 <table id="feature" border="0" cellpadding="0" cellspacing="0" summary="">
 	<tbody>
 		<tr>
-			<td width="10">
-				&nbsp; 
+			<td width="10">&nbsp;
+				 
 			</td>
 			<td valign="center">
 				<h1>Cytoscape 1.1 PlugIns</h1> 
