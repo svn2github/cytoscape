6b916a66bae8978e7b05b889e5b01807b2c789c4
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-07-07 20:21:29 +0000

    Javadoc comments added.
    git-svn-id: http://chianti.ucsd.edu/svn@20842 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/visual/customgraphic/CyCustomGraphics.java
+++ cytoscape/trunk/src/cytoscape/visual/customgraphic/CyCustomGraphics.java
@@ -10,7 +10,7 @@ public interface CyCustomGraphics <T> {
 	/**
 	 * Immutable session-unique identifier of image generated in constructor.
 	 * 
-	 * NOT globally unique.
+	 * NOT globally unique.  Uniqueness is guaranteed in a session.
 	 * 
 	 * @return Immutable ID as Long.
 	 */
@@ -18,12 +18,18 @@ public interface CyCustomGraphics <T> {
 	
 	/**
 	 * Display name is a simple description of this image object.
-	 * 
 	 * May not be unique and mutable.
 	 * 
 	 * @return display name as String.
 	 */
 	public String getDisplayName();
+	
+	
+	/**
+	 * Set human readable display name.
+	 * 
+	 * @param displayName
+	 */
 	public void setDisplayName(final String displayName);
 	
 	
@@ -36,22 +42,45 @@ public interface CyCustomGraphics <T> {
 	 */
 	public Collection<T> getLayers();
 	
+	
 	/**
-	 * Return size of current object.
+	 * Returns width of current object.
 	 * 
 	 * @return
 	 */
 	public int getWidth();
+	
+	
+	/**
+	 * Returns height of current object.
+	 * 
+	 * @return
+	 */
 	public int getHeight();
 	
+	
+	/**
+	 * Set width of Custom Graphics.
+	 * 
+	 * @param width
+	 */
 	public void setWidth(final int width);
+	
+	/**
+	 * Set height of Custom Graphics.
+	 * 
+	 * @param height
+	 */
 	public void setHeight(final int height);
 	
 	public float getFitRatio();
 	public void setFitRatio(float ratio);
 	
 	/**
-	 * From layers of graphics objects, render scaled Image object. 
+	 * From layers of graphics objects, render scaled Image object.
+	 * Usually done by Java2D low level code. 
+	 * 
+	 * Usually, the image returned by this method is used in GUI components (as icons).
 	 * 
 	 * @return rendered image object.
 	 */
