2e419a48b1d668e49d139820d65621243c50b9d1
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-07-21 22:03:16 +0000

    added several new getters
    git-svn-id: http://chianti.ucsd.edu/svn@20985 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/plugin-impl/trunk/src/main/java/org/cytoscape/plugin/internal/CyPluginAdapterImpl.java
+++ core3/plugin-impl/trunk/src/main/java/org/cytoscape/plugin/internal/CyPluginAdapterImpl.java
@@ -1,44 +1,108 @@
 
 package org.cytoscape.plugin.internal;
 
-import org.cytoscape.model.CyNetworkFactory;
-import org.cytoscape.model.CyDataTableFactory;
-import org.cytoscape.view.model.CyNetworkViewFactory;
-import org.cytoscape.work.TaskManager;
 import org.cytoscape.plugin.CyPluginAdapter;
+
+//
+// Keep these alphabetical by class name
+//
+import org.cytoscape.model.CyDataTableFactory;
+import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.view.layout.CyLayouts;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.session.CyNetworkManager;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.model.subnetwork.CyRootNetworkFactory;
+import org.cytoscape.session.CySessionManager;
+import org.cytoscape.view.vizmap.gui.editor.EditorManager;
+import org.cytoscape.view.presentation.PresentationFactory;
+import org.cytoscape.work.TaskManager;
+import org.cytoscape.view.vizmap.VisualMappingManager;
 
 
 /**
- * A Java-only api providing access to cytoscape functionality.
+ * An implementation of CyPluginAdapter
  */
 public class CyPluginAdapterImpl implements CyPluginAdapter {
 
-	CyNetworkFactory netFac;
-	CyDataTableFactory tabFac;
-	CyNetworkViewFactory viewFac;
-	TaskManager tm;
+	//
+	// Since there are a lot of fields, keep them
+	// in alphabetical order to maintain some order.
+	// Always make the field name same as the class
+	// name, but with a lower case first letter.
+	//
+	private final CyDataTableFactory cyDataTableFactory;
+	private final CyEventHelper cyEventHelper;
+	private final CyLayouts cyLayouts;
+	private final CyNetworkFactory cyNetworkFactory;
+	private final CyNetworkManager cyNetworkManager;
+	private final CyNetworkViewFactory cyNetworkViewFactory;
+	private final CyRootNetworkFactory cyRootNetworkFactory;
+	private final CySessionManager cySessionManager;
+	private final EditorManager editorManager;
+	private final PresentationFactory presentationFactory;
+	private final TaskManager taskManager;
+	private final VisualMappingManager visualMappingManager;
 
-	CyPluginAdapterImpl(CyNetworkFactory netFac, CyDataTableFactory tabFac,
-	           CyNetworkViewFactory viewFac, TaskManager tm ) {
-		this.netFac = netFac;
-		this.tabFac = tabFac;
-		this.viewFac = viewFac;
-		this.tm = tm;
+	//
+	// Since this is implementation code, there shouldn't be a
+	// a problem adding new arguments as needed.  Therefore, to
+	// maintain sanity, keep the arguments in alphabetical order.
+	//
+	CyPluginAdapterImpl( 
+	                    CyDataTableFactory cyDataTableFactory,
+	                    CyEventHelper cyEventHelper,
+	                    CyLayouts cyLayouts,
+                     	CyNetworkFactory cyNetworkFactory,
+                     	CyNetworkManager cyNetworkManager,
+                     	CyNetworkViewFactory cyNetworkViewFactory,
+                     	CyRootNetworkFactory cyRootNetworkFactory,
+                     	CySessionManager cySessionManager,
+                     	EditorManager editorManager,
+                     	PresentationFactory presentationFactory,
+                     	TaskManager taskManager,
+                     	VisualMappingManager visualMappingManager
+					    ) {
+		this.cyDataTableFactory = cyDataTableFactory;
+		this.cyEventHelper = cyEventHelper;
+		this.cyLayouts = cyLayouts;
+		this.cyNetworkFactory = cyNetworkFactory;
+		this.cyNetworkManager = cyNetworkManager;
+		this.cyNetworkViewFactory = cyNetworkViewFactory;
+		this.cyRootNetworkFactory = cyRootNetworkFactory;
+		this.cySessionManager = cySessionManager;
+		this.editorManager = editorManager;
+		this.presentationFactory = presentationFactory;
+		this.taskManager = taskManager;
+		this.visualMappingManager = visualMappingManager;
 	}
 
-	public CyNetworkFactory getCyNetworkFactory() {
-		return netFac;
-	}
 
-	public CyDataTableFactory getCyDataTableFactory(){
-		return tabFac;
-	} 
+	//
+	// May as well keep the methods alphabetical too!
+	// 
 
-	public CyNetworkViewFactory getCyNetworkViewFactory(){
-		return viewFac;
-	}
+	public CyDataTableFactory getCyDataTableFactory() { return cyDataTableFactory; } 
 
-	public TaskManager getTaskManager(){
-		return tm;
-	}
+	public CyEventHelper getCyEventHelper() { return cyEventHelper; } 
+
+	public CyLayouts getCyLayouts() { return cyLayouts; } 
+
+	public CyNetworkFactory getCyNetworkFactory() { return cyNetworkFactory; }
+
+	public CyNetworkManager getCyNetworkManager() { return cyNetworkManager; } 
+
+	public CyNetworkViewFactory getCyNetworkViewFactory() { return cyNetworkViewFactory; }
+
+	public CyRootNetworkFactory getCyRootNetworkFactory() { return cyRootNetworkFactory; } 
+
+	public CySessionManager getCySessionManager() { return cySessionManager; } 
+
+	public EditorManager getEditorManager() { return editorManager; } 
+
+	public PresentationFactory getPresentationFactory() { return presentationFactory; }
+
+	public TaskManager getTaskManager() { return taskManager; }
+
+	public VisualMappingManager getVisualMappingManager() { return visualMappingManager; } 
 }
