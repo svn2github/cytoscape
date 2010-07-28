042b1829b42c7a6f9d83b0765e612b1dd7c83795
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-07-28 17:02:03 +0000

    Works on Win, try on Linux
    git-svn-id: http://chianti.ucsd.edu/svn@21045 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/src/cytoscape/CytoscapeInit.java
+++ cytoscape/trunk/src/cytoscape/CytoscapeInit.java
@@ -455,15 +455,27 @@ public class CytoscapeInit implements PropertyChangeListener {
 
 					// The following code has problem to load session file from command line on Windows
 					// because prefix driver letter "C:\" or "D:\\" was removed from the absolute path
-					//final File shortName = new File(sessionFile);
-					//final String absolutePath = shortName.getAbsolutePath();
-					//final URL sessionURL = new URL("file://" + absolutePath.replace("%", "%25"));
-					//reader = new CytoscapeSessionReader(sessionURL);
-					//sessionName = shortName.getName();
+					final File shortName = new File(sessionFile);
+					final String absolutePath = shortName.getAbsolutePath();
 					
+					
+					System.out.println("absolutePath = "+absolutePath);
+					
+					final URL sessionURL = new URL("file:/" + absolutePath.replace("%", "%25"));
+					
+					System.out.println("sessionURL =" + sessionURL.toString());
+					
+					reader = new CytoscapeSessionReader(sessionURL);
+					
+					//sessionName = shortName.getName();
+					/*
 					final File _sessionFile = new File(sessionFile);
 					reader = new CytoscapeSessionReader(_sessionFile.toURL());
+					
+					System.out.println("_sessionFile.toURL().toString() ="+_sessionFile.toURL().toString());
+					
 					sessionName = _sessionFile.getName();
+					*/
 				}
 
 				if (reader != null) {
