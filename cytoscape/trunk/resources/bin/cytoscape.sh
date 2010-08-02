c5dd2e40b06bc1fb151f310d4136447a9a305b2f
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-08-02 16:54:26 +0000

    Fixed running cytoscape.sh from a directory that is not the installation directory.
    git-svn-id: http://chianti.ucsd.edu/svn@21116 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/resources/bin/cytoscape.sh
+++ cytoscape/trunk/resources/bin/cytoscape.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #
 # Run cytoscape from a jar file
-# this is a UNIX-only version
+# This script is a UNIX-only (i.e. Linux, Mac OS, etc.) version
 #-------------------------------------------------------------------------------
 
-script_path=$(dirname -- $0)
+script_path="$(dirname -- $0)"
 
 # Attempt to generate Cytoscape.vmoptions if it doesn't exist!
-if [ ! -e Cytoscape.vmoptions  -a  -x $script_path/gen_vmoptions.sh ]; then
-    $script_path/gen_vmoptions.sh
+if [ ! -e "$script_path/Cytoscape.vmoptions"  -a  -x "$script_path/gen_vmoptions.sh" ]; then
+    "$script_path/gen_vmoptions.sh"
 fi
 
 if [ -r Cytoscape.vmoptions ]; then
-    java `cat Cytoscape.vmoptions` -jar $script_path/cytoscape.jar -p $script_path/plugins "$@"
+    java `cat "$script_path/Cytoscape.vmoptions"` -jar "$script_path/cytoscape.jar" -p "$script_path/plugins" "$@"
 else # Just use sensible defaults.
     java -d64 -Dswing.aatext=true -Dawt.useSystemAAFontSettings=lcd -Xss10M -Xmx1550M \
-	-jar $script_path/cytoscape.jar -p $script_path/plugins "$@"
+	-jar "$script_path/cytoscape.jar" -p "$script_path/plugins" "$@"
 fi
 
--- cytoscape/trunk/resources/bin/gen_vmoptions.sh
+++ cytoscape/trunk/resources/bin/gen_vmoptions.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # Generates the Cytoscape.vmoptions file
+
+script_path="$(dirname -- $0)"
+
 if `java -version 2>&1 | grep -- 64-Bit > /dev/null`; then # We have a 64 bit JVM.
-    echo -Xms20m >  Cytoscape.vmoptions
-    echo -Xmx20g >> Cytoscape.vmoptions
-    echo -d64       >> Cytoscape.vmoptions
+    echo -Xms20m >  "$script_path/Cytoscape.vmoptions"
+    echo -Xmx20g >> "$script_path/Cytoscape.vmoptions"
+    echo -d64       >> "$script_path/Cytoscape.vmoptions"
 else # Assume a 32 bit JVM.
-    echo -Xms10m   >  Cytoscape.vmoptions
-    echo -Xmx1550m >> Cytoscape.vmoptions
+    echo -Xms10m   >  "$script_path/Cytoscape.vmoptions"
+    echo -Xmx1550m >> "$script_path/Cytoscape.vmoptions"
 fi
 
 # Shared JVM options
-echo -Dswing.aatext=true               >> Cytoscape.vmoptions
-echo -Dawt.useSystemAAFontSettings=lcd >> Cytoscape.vmoptions
+echo -Dswing.aatext=true               >> "$script_path/Cytoscape.vmoptions"
+echo -Dawt.useSystemAAFontSettings=lcd >> "$script_path/Cytoscape.vmoptions"
 
 exit 0
