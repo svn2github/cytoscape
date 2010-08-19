6ed6354bf84a4672366d636c097f68725f87ccbd
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-08-19 17:34:59 +0000

    moved manifest generation code into integration-test-support bundle
    git-svn-id: http://chianti.ucsd.edu/svn@21470 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/viewmodel-impl/trunk/src/integration/java/org/cytoscape/view/ITViewModelImpl.java
+++ core3/viewmodel-impl/trunk/src/integration/java/org/cytoscape/view/ITViewModelImpl.java
@@ -1,7 +1,5 @@
 package org.cytoscape.view;
 
-import java.util.jar.Manifest;
-
 import org.cytoscape.integration.AbstractIntegrationTester;
 import org.cytoscape.view.model.CyNetworkViewFactory;
 import org.cytoscape.view.model.RootVisualLexicon;
@@ -27,29 +25,9 @@ public class ITViewModelImpl extends AbstractIntegrationTester {
 				new String[] {
 					"rootVisualLexicon", "cyNetworkViewFactory" }, 
 				new Class[] {
-					RootVisualLexicon.class, CyNetworkViewFactory.class });
+					RootVisualLexicon.class, CyNetworkViewFactory.class },
+				new String[] { "org.cytoscape.view.model" }
+				);
 	}
 
-	/**
-	 * Provide correct metadata for on-the-fly bundle
-	 * 
-	 */
-	@Override
-	protected Manifest getManifest() {
-		// let the testing framework create/load the manifest
-		Manifest mf = super.getManifest();
-		
-		// add import statement
-		String original = mf.getMainAttributes().getValue("Import-Package");
-		mf.getMainAttributes().putValue("Import-Package",
-				original + ",org.cytoscape.view.model");
-
-		for (Object key : mf.getMainAttributes().keySet()) {
-			System.out.print("############ MF KEY = " + key);
-			System.out.println(", VAL = "
-					+ mf.getMainAttributes().getValue(key.toString()));
-		}
-
-		return mf;
-	}
 }
