92130de55a2179866a382d4b2a46d78a2ed112da
akuchinsky <akuchinsky@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-08-28 09:42:35 +0000

    Updated header to show Cytoscape copyrright
    git-svn-id: http://chianti.ucsd.edu/svn@21609 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/application/src/main/java/cytoscape/actions/DeleteAction.java
+++ cytoscape/trunk/application/src/main/java/cytoscape/actions/DeleteAction.java
@@ -1,31 +1,34 @@
-/* -*-Java-*-
- ********************************************************************************
- *
- * File:         DeleteAction.java
- * RCS:          $Header: $
- * Description:
- * Author:       Allan Kuchinsky
- * Created:      Tue May 24 06:54:43 2005
- * Modified:     Tue Jan 09 05:16:48 2007 (Michael L. Creech) creech@w235krbza760
- * Language:     Java
- * Package:
- * Status:       Experimental (Do Not Distribute)
- *
- * (c) Copyright 2006, Agilent Technologies, all rights reserved.
- *
- ********************************************************************************
- *
- * Revisions:
- *
-* Fri May 11 16:58:47 2007 (Michael L. Creech) creech@w235krbza760
-*  Removed unneeded imports and update some generics.
- * Tue Jan 09 05:15:18 2007 (Michael L. Creech) creech@w235krbza760
- *  Added setup of Delete keyboard accelerator to constructors.
- * Wed Dec 27 06:54:56 2006 (Michael L. Creech) creech@w235krbza760
- *  Removed use of _title and removed 2 parameter constructor, removed unused
- *  local variables.
- ********************************************************************************
+/*
+ File: DeleteAction.java
+
+ Copyright (c) 2006, 2010, The Cytoscape Consortium (www.cytoscape.org)
+
+ This library is free software; you can redistribute it and/or modify it
+ under the terms of the GNU Lesser General Public License as published
+ by the Free Software Foundation; either version 2.1 of the License, or
+ any later version.
+
+ This library is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ documentation provided hereunder is on an "as is" basis, and the
+ Institute for Systems Biology and the Whitehead Institute
+ have no obligations to provide maintenance, support,
+ updates, enhancements or modifications.  In no event shall the
+ Institute for Systems Biology and the Whitehead Institute
+ be liable to any party for direct, indirect, special,
+ incidental or consequential damages, including lost profits, arising
+ out of the use of this software and its documentation, even if the
+ Institute for Systems Biology and the Whitehead Institute
+ have been advised of the possibility of such damage.  See
+ the GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
+
+//---------------------------------------------------------------------------
 package cytoscape.actions;
 
 import giny.model.GraphObject;
