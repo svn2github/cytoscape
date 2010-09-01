d34448ce557876df50b5e9b1b5c0d418560247b1
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-09-01 23:53:13 +0000

    moved and commented on SetCurrentNetwork tasks
    git-svn-id: http://chianti.ucsd.edu/svn@21662 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/setcurrent/SetCurrentNetworkTask.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/setcurrent/SetCurrentNetworkTask.java
@@ -1,4 +1,4 @@
-package org.cytoscape.task.internal.loadnetwork;
+package org.cytoscape.task.internal.setcurrent;
 
 
 import org.cytoscape.model.CyNetwork;
@@ -6,7 +6,11 @@ import org.cytoscape.session.CyNetworkManager;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
 
-
+/**
+ * This class exists for (possible) use in headless mode.  The associated 
+ * TaskFactory should not be registered in Swing mode, since this task doesn't 
+ * make sense in GUI mode.
+ */
 public class SetCurrentNetworkTask extends AbstractTask {
 
 	CyNetworkManager netmgr;
@@ -18,6 +22,8 @@ public class SetCurrentNetworkTask extends AbstractTask {
 
 
 	public void run(TaskMonitor taskMonitor) throws Exception {
+		// TODO Verify that we want an essentially random network and that this
+		// task shouldn't be NetworkTask instead.
 		this.taskMonitor = taskMonitor;
 		Object[] setNetworks = netmgr.getNetworkSet().toArray();
 		netmgr.setCurrentNetwork(((CyNetwork) setNetworks[setNetworks.length-1]).getSUID());
--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/setcurrent/SetCurrentNetworkTaskFactoryImpl.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/setcurrent/SetCurrentNetworkTaskFactoryImpl.java
@@ -1,20 +1,23 @@
-package org.cytoscape.task.internal.loadnetwork;
+package org.cytoscape.task.internal.setcurrent;
 
 
 import org.cytoscape.session.CyNetworkManager;
 import org.cytoscape.work.TaskFactory;
 import org.cytoscape.work.TaskIterator;
 
-
+// TODO Verify that we need this class in headless mode!
+/**
+ * This TaskFactory is for headless mode and not GUI mode. This
+ * factory shouldn't be registered by the swing GUI as it doesn't
+ * make sense in that context.
+ */
 public class SetCurrentNetworkTaskFactoryImpl implements TaskFactory {
 
 	private CyNetworkManager netmgr;
 
-
 	public SetCurrentNetworkTaskFactoryImpl(CyNetworkManager netmgr) {
 		this.netmgr = netmgr;
 	}
-
 
 	public TaskIterator getTaskIterator() {
 		return new TaskIterator(new SetCurrentNetworkTask(netmgr));
