3a3f3a547871e74dc001e8b89e4b61ed72236a59
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-09-10 20:31:35 +0000

    got export graphics task registering properly
    git-svn-id: http://chianti.ucsd.edu/svn@21798 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/exportgraphics/ExportNetworkImageTaskFactory.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/exportgraphics/ExportNetworkImageTaskFactory.java
@@ -26,7 +26,7 @@
  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
-package org.cytoscape.task.internal.loadnetwork;
+package org.cytoscape.task.internal.exportgraphics;
 
 import org.cytoscape.task.AbstractNetworkViewTaskFactory;
 import org.cytoscape.io.write.ViewWriterManager;
