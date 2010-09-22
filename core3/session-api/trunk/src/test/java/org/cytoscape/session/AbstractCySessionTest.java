30946016f79a736fe8223978fb11452281523267
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-09-22 23:31:20 +0000

    Added actual tests
    git-svn-id: http://chianti.ucsd.edu/svn@22025 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/session-api/trunk/src/test/java/org/cytoscape/session/AbstractCySessionTest.java
+++ core3/session-api/trunk/src/test/java/org/cytoscape/session/AbstractCySessionTest.java
@@ -1,17 +1,61 @@
 package org.cytoscape.session;
 
 import static org.junit.Assert.*;
+
+import org.cytoscape.session.CySession;
 import org.junit.Before;
 import org.junit.Test;
 
 public abstract class AbstractCySessionTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+
+	protected CySession session;
 
 	
 	@Test
-	public void testSomething() {
+	public void testSessionName() {
+		assertNotNull(session);
+		assertNotNull(session.getSessionName());
+		assertFalse(session.getSessionName().equals(""));
+	}
+	
+	@Test
+	public void testGetNetworks() {
+		assertNotNull(session);
+		assertNotNull(session.getNetworks());
+	}
+	
+	@Test
+	public void testGetNetworkViews() {
+		assertNotNull(session);
+		assertNotNull(session.getNetworkViews());
+	}
+	
+	
+	@Test
+	public void testGetTables() {
+		assertNotNull(session);
+		assertNotNull(session.getTables());
+	}
+	
+	@Test
+	public void testGetVisualStyles() {
+		assertNotNull(session);
+		assertNotNull(session.getVisualStyles());
+	}
+	
+	@Test
+	public void testGetProperties() {
+		assertNotNull(session);
+		assertNotNull(session.getProperties());
+		assertNotNull(session.getProperties().get("cytoscape"));
+		assertNotNull(session.getProperties().get("vizmap"));
+	}
+	
+	@Test
+	public void testGetFilename() {
+		assertNotNull(session);
+		assertNotNull(session.getFileName());
+		assertFalse(session.getSessionName().equals(""));
 	}
 }
