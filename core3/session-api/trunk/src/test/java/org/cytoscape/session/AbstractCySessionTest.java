a60d9046830af71acc7d896db0063fccc4445550
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-09-22 23:11:03 +0000

    added first abstract test skeleton
    git-svn-id: http://chianti.ucsd.edu/svn@22022 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/session-api/trunk/src/test/java/org/cytoscape/session/AbstractCySessionTest.java
@@ -0,0 +1,17 @@
+package org.cytoscape.session;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class AbstractCySessionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+	@Test
+	public void testSomething() {
+	}
+}
