ec49631640a73782342cfb6c4b90447db15f9368
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-09-23 18:24:21 +0000

    added first pass at an archetype
    git-svn-id: http://chianti.ucsd.edu/svn@22030 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ cytoscape/trunk/archetypes/sample-plugin/src/main/resources/archetype-resources/src/main/java/MyAction.java
@@ -0,0 +1,32 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${packageInPathFormat};
+
+import cytoscape.Cytoscape;
+import cytoscape.util.CytoscapeAction;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+
+/**
+ * A simple action.  Change the names as appropriate and
+ * then fill in your expected behavior in the actionPerformed()
+ * method.
+ */
+public class MyAction extends CytoscapeAction {
+
+		public MyAction() {
+			// Give your action a name here
+			super("Sample plugin");
+
+			// Set the menu you'd like here.  Plugins don't need
+			// to live in the Plugins menu, so choose whatever
+			// is appropriate!
+	        setPreferredMenu("Plugins");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			// Do something useful here!
+			JOptionPane.showMessageDialog(Cytoscape.getDesktop(),"Executed the Sample plugin!");	
+		}
+}
