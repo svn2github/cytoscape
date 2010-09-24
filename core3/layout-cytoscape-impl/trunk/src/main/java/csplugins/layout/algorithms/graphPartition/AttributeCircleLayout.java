1537d240cf2e0876047ba45c18de896389aef990
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-09-24 21:45:17 +0000

    Refactored
    git-svn-id: http://chianti.ucsd.edu/svn@22051 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/layout-cytoscape-impl/trunk/src/main/java/csplugins/layout/algorithms/graphPartition/AttributeCircleLayout.java
+++ core3/layout-cytoscape-impl/trunk/src/main/java/csplugins/layout/algorithms/graphPartition/AttributeCircleLayout.java
@@ -40,17 +40,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.cytoscape.view.layout.AbstractGraphPartition;
+//import org.cytoscape.view.layout.AbstractGraphPartition;
+import org.cytoscape.view.layout.AbstractLayout;
 import org.cytoscape.view.layout.LayoutNode;
-import org.cytoscape.view.layout.LayoutPartition;
+//import org.cytoscape.view.layout.LayoutPartition;
+import org.cytoscape.work.TaskIterator;
 import org.cytoscape.work.Tunable;
+import org.cytoscape.work.TunableValidator;
 import org.cytoscape.work.undo.UndoSupport;
 
 
 /**
  *
  */
-public class AttributeCircleLayout extends AbstractGraphPartition {
+public class AttributeCircleLayout extends AbstractLayout implements TunableValidator {
 	@Tunable(description="The attribute to use for the layout")
 	public String attribute = null;
 	@Tunable(description="The attribute namespace to use for the layout")
@@ -77,6 +80,21 @@ public class AttributeCircleLayout extends AbstractGraphPartition {
 		initialize(true);
 	}
 
+	
+
+	//TODO 
+	public boolean tunablesAreValid(final Appendable errMsg) {
+		return true;
+	}
+
+	public TaskIterator getTaskIterator() {
+		
+		return new TaskIterator(new AttributeCircleLayoutTask(networkView, getName(), selectedOnly, staticNodes,
+				attribute,namespace,spacing,supportNodeAttributes));
+	}
+
+	
+	
 	/**
 	 *  DOCUMENT ME!
 	 *
@@ -158,88 +176,4 @@ public class AttributeCircleLayout extends AbstractGraphPartition {
 			return "attribute-circle";
 	}
 
-	/**
-	 *  DOCUMENT ME!
-	 *
-	 * @param partition DOCUMENT ME!
-	 */
-	public void layoutPartion(LayoutPartition partition) {
-		// just add the unlocked nodes
-		List<LayoutNode> nodes = new ArrayList<LayoutNode>();
-		for ( LayoutNode ln : partition.getNodeList() ) {
-			if ( !ln.isLocked() ) {
-				nodes.add(ln);
-			}
-		}
-
-		int count = nodes.size(); 
-		int r = (int) Math.sqrt(count);
-		r *= spacing;
-
-		if (this.attribute != null && count > 0) {
-			Class<?> klass = nodes.get(0).getNode().getCyRow(namespace).contains(attribute); 
-			if (klass != null && Comparable.class.isAssignableFrom(klass)){
-				// FIXME: I assume this would be better, but get type errors if I try:
-				//Class<Comparable<?>> kasted = (Class<Comparable<?>>) klass;
-				//Collections.sort(nodes, new AttributeComparator<Comparable<?>>(kasted));
-				Collections.sort(nodes, new AttributeComparator(klass));
-			} else {
-				/* FIXME Error! */
-			}
-		}
-
-		// Compute angle step
-		double phi = (2 * Math.PI) / count; 
-
-		partition.resetNodes(); // We want to figure out our mins & maxes anew
-		                        // Arrange vertices in a circle
-
-		for (int i = 0; i < count; i++) {
-			LayoutNode node = (LayoutNode) nodes.get(i);
-			double x = r + (r * Math.sin(i * phi));
-			double y = r + (r * Math.cos(i * phi));
-			node.setX(x);
-			node.setY(y);
-			partition.moveNodeToLocation(node);
-		}
-	}
-	private class AttributeComparator<T extends Comparable<T>> implements Comparator<LayoutNode> {
-		Class<T> klass;
-		private AttributeComparator(Class<T> klass) {
-			this.klass = klass;
-		}
-		
-		public int compare(LayoutNode o1, LayoutNode o2) {
-			T v1 = o1.getNode().getCyRow(namespace).get(attribute, klass);
-			T v2 = o2.getNode().getCyRow(namespace).get(attribute, klass);
-			if (String.class.isAssignableFrom(klass)){ // i.e. if klass _is_ String.class
-				String s1 = String.class.cast(v1);
-				String s2 = String.class.cast(v2);
-				if ((s1 != null) && (s2 != null))
-					return s1.compareToIgnoreCase(s2);
-				else if ((s1 == null) && (s2 != null))
-					return -1;
-				else if ((s1 == null) && (s2 == null))
-					return 0;
-				else if ((s1 != null) && (s2 == null))
-					return 1;
-				
-			} else {
-				return compareEvenIfNull(v1, v2);
-			}
-
-			return 0; // can't happen anyway
-		}
-
-		public int compareEvenIfNull(T v1, T v2){
-			if ((v1 != null) && (v2 != null))
-				return v1.compareTo(v2);
-			else if ((v1 == null) && (v2 != null))
-				return -1;
-			else if ((v1 == null) && (v2 == null))
-				return 0;
-			else // if ((v1 != null) && (v2 == null)) // this is the only possibility
-				return 1;
-		}
-	}
 }
