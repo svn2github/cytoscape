86e8194431108f7e124a5acebe6da6d2493d1d07
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-01 23:26:20 +0000

    Fixed some Javadoc errors.
    git-svn-id: http://chianti.ucsd.edu/svn@22129 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/io-api/trunk/src/main/java/org/cytoscape/io/FileIOFactory.java
+++ core3/io-api/trunk/src/main/java/org/cytoscape/io/FileIOFactory.java
@@ -1,9 +1,9 @@
 package org.cytoscape.io;
 
+
 /**
- * An interface extended by {@link org.cytoscape.io.read.CyReader} and
- * {@link org.cytoscape.io.read.CyWriter} the provides basic information about
- * the types of files supported.
+ * An interface extended by various reader and writer factories.
+ * It the provides basic information about the types of supported files.
  */
 public interface FileIOFactory {
 
