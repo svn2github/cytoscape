1c8d003c5d3000b6141b54450ffcf56541cedcb1
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-04 22:36:36 +0000

    initial import
    git-svn-id: http://chianti.ucsd.edu/svn@22141 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/property-api/trunk/src/main/java/org/cytoscape/property/CyProperty.java
@@ -0,0 +1,17 @@
+package org.cytoscape.property;
+
+/**
+ * A general property service interface for providing access to different types
+ * of property objects as OSGi services. The type P is generally one  of three types: 
+ * {@link java.util.Properties}, 
+ * {@link org.cytoscape.property.bookmark.Bookmarks}, or
+ * {@link org.cytoscape.property.session.Cysession}, although it is possible for 
+ * property objects of other types to be registered in this way as well.
+ */
+public interface CyProperty<P> {
+
+	/**
+	 * @return A property object to type P.
+	 */
+	public P getProperties();
+}
