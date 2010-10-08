cfd5b784d766392aea7c0bee95a772364573a267
Christian Lopes <Christian Lopes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-08 14:30:47 +0000

    Fixed some broken links.
    git-svn-id: http://chianti.ucsd.edu/svn@22188 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscapeweb/trunk/website/src/php/content/tutorial.php
+++ cytoscapeweb/trunk/website/src/php/content/tutorial.php
@@ -59,24 +59,24 @@
     
     <p>Now that you are able to embed Cytoscape Web into a page, you can use the Javascript API to
     interact with it.  You have already used the class representing the
-    <a href="/documentation/cytoscape_web">embedded visualisation</a> in the
+    <a href="/documentation/visualization">embedded visualisation</a> in the
     first example.  What remains is to interact with the visualisation once it has been drawn.</p>
     
     <p>The main thing to keep in mind is that you can not interact with most of Cytoscape Web
     until the graph is drawn.  Thus, you can interact with Cytoscape Web by using the
-    <a href="/documentation/cytoscape_web#ready">ready</a> callback function, which is called
+    <a href="/documentation/visualization#ready">ready</a> callback function, which is called
     when Cytoscape Web is finished drawing the graph.</p>
     
     <p>This example interacts with Cytoscape Web by getting attributes values that were set for the
     nodes and edges in the graph.  This is achieved by registering with the
-    <a href="/documentation/cytoscape_web#addListener">addListener</a> function for click events.</p>
+    <a href="/documentation/visualization#addListener">addListener</a> function for click events.</p>
     
     <?php print_code("file/example_code/interacting.html"); ?>
     
     <?php embed_code("file/example_code/interacting.html"); ?>
     
     <p>Now that you know how to initialise and interact with Cytoscape Web, you can look to the
-    <a href="/documentation/cytoscape_web">API reference</a> to customise
+    <a href="/documentation/visualization">API reference</a> to customise
     Cytoscape Web exactly to your liking.  Have fun!</p>
     
     <p>If you would like a bit more in the way of instruction, see the next section on how to
@@ -88,7 +88,7 @@
     
     <p>Visual styles configure the way that the graph is visually displayed.  You can create a
     visual style statically or programattically, by setting the visual style at initialisation or
-    by using the <a href="/documention/cytoscape_web#visualStyle">visualStyle</a> function.</p>
+    by using the <a href="/documentation#section/visualStyle">visualStyle</a> function.</p>
     
     <p>This example changes the visual style of the graph from the previous examples.</p>
     
@@ -103,7 +103,7 @@
     <p>This example has used only a few visual properties for the sake of simplicity.  However,
     there are many visual properties that exist in Cytoscape Web that can be used to control
     exactly how things are visually displayed.  Take a look at the
-    <a href="/documentation/cytoscape_web#visualStyle">visualStyle</a>
+    <a href="/documentation#section/visualStyle">visualStyle</a>
     function for more information.</p>
     
     
@@ -113,7 +113,7 @@
     
     <p>This tutorial should allow you to have Cytoscape Web up and running and interacting with
     other components in your page.  You should now be sufficiently capable of using the
-    <a href="/documentation/cytoscape_web">API reference</a> to customise Cytoscape Web to your
+    <a href="/documentation/visualization">API reference</a> to customise Cytoscape Web to your
     specific needs.</p>
     
     <p>If you still have questions, take a look at the <a href="faq">FAQ</a>.  Most likely,
