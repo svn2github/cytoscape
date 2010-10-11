940c5d7bd3fc11f0ad84383305057701ca6d902e
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-11 19:59:22 +0000

    Now creates the .cytoscape directory if it doesn't already exist.
    git-svn-id: http://chianti.ucsd.edu/svn@22202 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/distribution/src/main/bin/gen_vmoptions.bat
+++ cytoscape/trunk/distribution/src/main/bin/gen_vmoptions.bat
@@ -3,6 +3,11 @@
 :: Generates the Cytoscape.vmoptions file based on whether
 :: we're dealing with a 32 bit or 64 bit JVM.
 
+:: Create the .cytoscape directory if it doesn't already exist
+if exist %HOMEPATH%\.cytoscape goto dot_cytoscape_exists
+mkdir %HOMEPATH%\.cytoscape
+:dot_cytoscape_exists
+
 if exist findstr.out del findstr.out
 java -version 2>&1 | findstr /I 64-Bit > findstr.out
 for /f %%i in ('dir /b findstr.out') do if %%~zi equ 0 goto 32bit
