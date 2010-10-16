370319a0ebb7737f2a6f8fa984460c89ad0963b7
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-16 00:08:48 +0000

    Original creation
    git-svn-id: http://chianti.ucsd.edu/svn@22269 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/core-task-impl/trunk/src/test/java/org/cytoscape/task/internal/hide/UnHideAllTaskFactoryTest.java
@@ -0,0 +1,28 @@
+package org.cytoscape.task.internal.hide;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.work.Task;
+import org.cytoscape.work.TaskIterator;
+import org.junit.Test;
+
+public class UnHideAllTaskFactoryTest {
+	@Test
+	public void testRun() throws Exception {
+
+		CyNetworkView view = mock(CyNetworkView.class);
+
+		UnHideAllTaskFactory factory = new UnHideAllTaskFactory();
+		factory.setNetworkView(view);
+		
+		TaskIterator ti = factory.getTaskIterator();
+		assertNotNull(ti);
+		
+		assertTrue( ti.hasNext() );
+		Task t = ti.next();
+		assertNotNull( t );				
+	}	
+}
