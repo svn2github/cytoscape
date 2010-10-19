212b10ca0b31873d3bf5d48036405f1098772c34
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-19 20:17:16 +0000

    Add doc
    git-svn-id: http://chianti.ucsd.edu/svn@22294 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/io-api/trunk/src/main/java/org/cytoscape/io/FileIOFactory.java
+++ core3/io-api/trunk/src/main/java/org/cytoscape/io/FileIOFactory.java
@@ -3,10 +3,12 @@ package org.cytoscape.io;
 
 /**
  * An interface extended by various reader and writer factories.
- * It the provides basic information about the types of supported files.
+ * It provides basic information about the types of supported files.
  */
 public interface FileIOFactory {
-
+	/**
+	 * Return the file filter associated with the IOFactory
+	 */
 	public CyFileFilter getCyFileFilter();
 
 }
