837b6b478b1382f86a69735b6a9a761df3245ad5
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-19 17:40:53 +0000

    Moved interface tests to abstract class
    git-svn-id: http://chianti.ucsd.edu/svn@22288 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/plugin-impl/trunk/src/test/java/org/cytoscape/plugin/internal/CyPluginAdapterImplTest.java
+++ core3/plugin-impl/trunk/src/test/java/org/cytoscape/plugin/internal/CyPluginAdapterImplTest.java
@@ -11,17 +11,12 @@ import org.cytoscape.view.model.CyNetworkViewFactory;
 import org.cytoscape.view.presentation.RenderingEngineFactory;
 import org.cytoscape.view.vizmap.VisualMappingManager;
 import org.cytoscape.work.TaskManager;
-import org.junit.Test;
-import junit.framework.*;
-import org.cytoscape.plugin.CyPluginAdapter;
-
+import org.cytoscape.plugin.CyPluginAdapterTest;
 import static org.mockito.Mockito.*;
 
-public class CyPluginAdapterImplTest extends TestCase {
+public class CyPluginAdapterImplTest extends CyPluginAdapterTest {
 	
-	CyPluginAdapter adapter;
-	
-	private void defaultSetUp() {
+	public void setUp() {
 
 		CyTableFactory cyTableFactory = mock(CyTableFactory.class);
 		CyEventHelper cyEventHelper = mock(CyEventHelper.class);
@@ -50,90 +45,4 @@ public class CyPluginAdapterImplTest extends TestCase {
 			    );
 	}
 	
-	@Test
-	public void testGetCyDataTableFactory(){ 
-		defaultSetUp();
-		assertNotNull("dataTable exists",adapter.getCyDataTableFactory());
-		assertTrue("CyTableFactory is expected", adapter.getCyDataTableFactory() instanceof CyTableFactory);
-	}
-	
-	@Test
-	public void testGetCyEventHelper() 
-	{ 
-		defaultSetUp();
-		assertNotNull("dataTable exists",adapter.getCyEventHelper());
-		assertTrue("CyEventHelper is expected", adapter.getCyEventHelper() instanceof CyEventHelper);
-	} 
-
-	@Test
-	public void testGetCyLayouts() 
-	{ 
-		defaultSetUp();
-		assertNotNull("CyLayouts exists",adapter.getCyLayouts());
-		assertTrue("CyLayouts is expected", adapter.getCyLayouts()instanceof CyLayouts);
-	} 
-
-	@Test
-	public void testGetCyNetworkFactory() 
-	{ 
-		defaultSetUp();
-		assertNotNull("CyLayouts exists",adapter.getCyNetworkFactory());
-		assertTrue("CyLayouts is expected", adapter.getCyNetworkFactory()instanceof CyNetworkFactory);
-	}
-
-	@Test
-	public void testGetCyNetworkManager() 
-	{ 
-		defaultSetUp();
-		assertNotNull("CyNetworkManager exists",adapter.getCyNetworkManager());
-		assertTrue("CyNetworkManger is expected", adapter.getCyNetworkManager()instanceof CyNetworkManager);
-	} 
-
-	@Test
-	public void testGetCyNetworkViewFactory() 
-	{
-		defaultSetUp();
-		assertNotNull("CyLayouts exists",adapter.getCyNetworkViewFactory());
-		assertTrue("CyNetworkViewFactory is expected", adapter.getCyNetworkViewFactory()instanceof CyNetworkViewFactory);
-	}
-
-	@Test
-	public void testGetCyRootNetworkFactory() 
-	{
-		defaultSetUp();
-		assertNotNull("CyLayouts exists",adapter.getCyRootNetworkFactory());
-		assertTrue("CyRootNetworkFactory is expected", adapter.getCyRootNetworkFactory()instanceof CyRootNetworkFactory);
-	} 
-
-	@Test
-	public void testGetCySessionManager() 
-	{ 
-		defaultSetUp();
-		assertNotNull("CyLayouts exists",adapter.getCySessionManager());
-		assertTrue("CySessionManager is expected", adapter.getCySessionManager()instanceof CySessionManager);
-	} 
-
-	@Test
-	public void testGetPresentationFactory() 
-	{ 
-		defaultSetUp();
-		assertNotNull("PresentationFactory exists",adapter.getPresentationFactory());
-		assertTrue("PresentationFactory is expected", adapter.getPresentationFactory()instanceof RenderingEngineFactory);
-	}
-
-	@Test
-	public void testGetTaskManager() 
-	{ 
-		defaultSetUp();
-		assertNotNull("TaskManager exists",adapter.getTaskManager());
-		assertTrue("TaskManager is expected", adapter.getTaskManager()instanceof TaskManager);
-	}
-
-	@Test
-	public void testGetVisualMappingManager() 
-	{ 
-		defaultSetUp();
-		assertNotNull("VisualMappingManager exists",adapter.getVisualMappingManager());
-		assertTrue("VisualMappingManager is expected", adapter.getVisualMappingManager()instanceof VisualMappingManager);
-	} 
 }
