c2c2da1f547deba38e09be00bcf0e874a1c9e69e
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-20 17:39:40 +0000

    Initial implementation of SBML reader
    git-svn-id: http://chianti.ucsd.edu/svn@22308 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/sbml-impl/src/test/java/org/cytoscape/sbml/internal/SBMLNetworkViewReaderTest.java
@@ -0,0 +1,84 @@
+package org.cytoscape.sbml.internal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.cytoscape.io.read.CyNetworkViewReader;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
+import org.cytoscape.test.support.NetworkTestSupport;
+import org.cytoscape.test.support.NetworkViewTestSupport;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.work.TaskIterator;
+import org.cytoscape.work.TaskMonitor;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@SuppressWarnings("nls")
+public class SBMLNetworkViewReaderTest {
+	@Mock TaskMonitor taskMonitor;
+	
+	private CyNetworkFactory networkFactory;
+	private CyNetworkViewFactory networkViewFactory;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		
+		networkFactory = new NetworkTestSupport().getNetworkFactory();
+		networkViewFactory = new NetworkViewTestSupport().getNetworkViewFactory();
+	}
+	
+	
+	CyNetworkViewReader createReader(File file) throws IOException {
+		SBMLNetworkViewReader reader = new SBMLNetworkViewReader(new FileInputStream(file), networkFactory, networkViewFactory);
+		reader.setTaskIterator(new TaskIterator(reader));
+		return reader;
+	}
+
+	@Test
+	public void testSBMLLevel2() throws Exception {
+		File file = new File("src/test/resources/BIOMD0000000003.xml");
+		CyNetworkViewReader reader = createReader(file);
+		reader.run(taskMonitor);
+		CyNetworkView[] views = reader.getNetworkViews();
+		
+		assertNotNull(views);
+		assertEquals(1, views.length);
+		
+		CyNetworkView view = views[0];
+		assertNotNull(view);
+		
+		CyNetwork model = view.getModel();
+		assertNotNull(model);
+		
+		assertEquals(10, model.getNodeCount());
+		assertEquals(8, model.getEdgeCount());
+		
+		CyNode cyclin = findNodeById("C", model);
+		assertNotNull(cyclin);
+		CyRow attributes = cyclin.attrs();
+		assertEquals("Cyclin", attributes.get(SBMLNetworkViewReader.NODE_NAME_ATTR_LABEL, String.class));
+		assertEquals((Double) 0.01, attributes.get(SBMLNetworkViewReader.SBML_INITIAL_CONCENTRATION_ATTR, Double.class));
+	}
+
+	private CyNode findNodeById(String sbmlId, CyNetwork network) {
+		for (CyNode node : network.getNodeList()) {
+			CyRow attributes = node.attrs();
+			String id = attributes.get(SBMLNetworkViewReader.SBML_ID_ATTR, String.class);
+			if (id.equals(sbmlId)) {
+				return node;
+			}
+		}
+		return null;
+	}
+}
