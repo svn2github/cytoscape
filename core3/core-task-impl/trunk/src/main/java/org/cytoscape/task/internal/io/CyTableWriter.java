1de88fd0717f4b403713d4d6274febedee005a9f
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-25 18:21:17 +0000

    Moved from io-api bundle
    git-svn-id: http://chianti.ucsd.edu/svn@22361 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/io/CyTableWriter.java
@@ -0,0 +1,37 @@
+
+package org.cytoscape.task.internal.io;
+
+import org.cytoscape.model.CyTable;
+import org.cytoscape.io.CyFileFilter;
+import org.cytoscape.io.write.CyTableWriterFactory;
+import org.cytoscape.io.write.CyTableWriterManager;
+import org.cytoscape.io.write.CyWriter;
+
+import java.io.File;
+
+/**
+ * A utility Task implementation specifically for writing {@link org.cytoscape.model.CyTable} objects.
+ */
+public final class CyTableWriter extends AbstractCyWriter<CyTableWriterManager> {
+
+	private final CyTable table;
+
+	/**
+	 * @param writerManager The {@link org.cytoscape.io.write.CyTableWriterManager} used to determine which 
+	 * {@link org.cytoscape.io.write.CyTableWriterFactory} to use to write the file.
+	 * @param table The {@link org.cytoscape.model.CyTable} to be written out. 
+ 	 */
+    public CyTableWriter(CyTableWriterManager writerManager, CyTable table ) {
+		super(writerManager);
+		if ( table == null )
+			throw new NullPointerException("Table is null");
+		this.table = table;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected CyWriter getWriter(CyFileFilter filter, File file)  throws Exception{
+		return writerManager.getWriter(table,filter,file);
+	}
+}
