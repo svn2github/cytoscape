ff8a48257c6b2d31fa8050705df239d15d258e85
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-29 00:15:20 +0000

    [maven-release-plugin]  copy for tag viewmodel-impl-3.0.0-alpha1
    git-svn-id: http://chianti.ucsd.edu/svn@22637 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/viewmodel-impl/tags/viewmodel-impl-3.0.0-alpha1/src/integration/java/org/cytoscape/view/ITViewModelImpl.java
@@ -0,0 +1,32 @@
+package org.cytoscape.view;
+
+import org.cytoscape.integration.AbstractIntegrationTester;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+
+/**
+ * Integration test for viewmodel-impl bundle.
+ * 
+ * @author kono
+ * 
+ */
+public class ITViewModelImpl extends AbstractIntegrationTester {
+
+	public ITViewModelImpl() {
+		super("org.cytoscape.viewmodel-impl", 
+				new String[] {
+					"org.cytoscape, event-api, 1.0-SNAPSHOT",
+					"org.cytoscape, event-impl, 1.0-SNAPSHOT",
+					"org.cytoscape, model-api, 1.0-SNAPSHOT",
+					"org.cytoscape, integration-test-support, 1.0-SNAPSHOT",
+					"org.cytoscape, service-util, 1.0-SNAPSHOT",
+					"org.cytoscape, viewmodel-api, 1.0-SNAPSHOT",
+					"org.cytoscape, viewmodel-impl, 1.0-SNAPSHOT", }, 
+				new String[] {
+					"cyNetworkViewFactory" }, 
+				new Class[] {
+					CyNetworkViewFactory.class },
+				new String[] { "org.cytoscape.view.model" }
+				);
+	}
+
+}
---
+++ core3/viewmodel-impl/tags/viewmodel-impl-3.0.0-alpha1/src/main/java/org/cytoscape/view/model/internal/NodeViewImpl.java
@@ -0,0 +1,30 @@
+package org.cytoscape.view.model.internal;
+
+import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.model.events.NodeViewChangeMicroListener;
+
+public class NodeViewImpl extends ViewImpl<CyNode> {
+
+	private final CyNetworkView parent;
+	
+	public NodeViewImpl(CyNode model, CyEventHelper cyEventHelper, CyNetworkView parent) {
+		super(model, cyEventHelper);
+		this.parent = parent;
+	}
+	
+	@Override
+	public <T, V extends T> void setVisualProperty(
+			VisualProperty<? extends T> vp, V value) {
+		
+		if(value == null)
+			this.visualProperties.remove(vp);
+		else
+			this.visualProperties.put(vp, value);
+		
+		cyEventHelper.getMicroListener(NodeViewChangeMicroListener.class, parent).nodeVisualPropertySet(this, vp, value);
+	}
+
+}
