2714f22e45f174a7d59bde0777611c733eb3c251
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-10-29 00:49:44 +0000

    [maven-release-plugin]  copy for tag plugin-impl-3.0.0-alpha1
    git-svn-id: http://chianti.ucsd.edu/svn@22643 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/plugin-impl/tags/plugin-impl-3.0.0-alpha1/src/main/java/org/cytoscape/plugin/internal/CyPluginAdapterImpl.java
@@ -0,0 +1,115 @@
+
+package org.cytoscape.plugin.internal;
+
+import org.cytoscape.plugin.CyPluginAdapter;
+
+//
+// Keep these alphabetical by class name
+//
+import org.cytoscape.model.CyTableFactory;
+import org.cytoscape.event.CyEventHelper;
+//import org.cytoscape.view.layout.CyLayouts;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.model.subnetwork.CyRootNetworkFactory;
+import org.cytoscape.session.CyApplicationManager;
+import org.cytoscape.session.CySessionManager;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.view.model.CyNetworkViewManager;
+import org.cytoscape.view.presentation.RenderingEngineFactory;
+import org.cytoscape.work.TaskManager;
+import org.cytoscape.view.vizmap.VisualMappingManager;
+
+
+/**
+ * An implementation of CyPluginAdapter
+ */
+public class CyPluginAdapterImpl implements CyPluginAdapter {
+
+	//
+	// Since there are a lot of fields, keep them
+	// in alphabetical order to maintain some order.
+	// Always make the field name same as the class
+	// name, but with a lower case first letter.
+	//
+	private final CyTableFactory cyTableFactory;
+	private final CyEventHelper cyEventHelper;
+//	private final CyLayouts cyLayouts;
+	private final CyNetworkFactory cyNetworkFactory;
+	private final CyNetworkManager cyNetworkManager;
+	private final CyNetworkViewFactory cyNetworkViewFactory;
+	private final CyRootNetworkFactory cyRootNetworkFactory;
+	private final CySessionManager cySessionManager;
+	private final RenderingEngineFactory presentationFactory;
+	private final TaskManager taskManager;
+	private final VisualMappingManager visualMappingManager;
+	private final CyNetworkViewManager networkViewManager;
+	private final CyApplicationManager applicationManager;
+
+	//
+	// Since this is implementation code, there shouldn't be a
+	// a problem adding new arguments as needed.  Therefore, to
+	// maintain sanity, keep the arguments in alphabetical order.
+	//
+	CyPluginAdapterImpl( 
+	                    CyTableFactory cyTableFactory,
+	                    CyEventHelper cyEventHelper,
+//	                    CyLayouts cyLayouts,
+			    CyNetworkFactory cyNetworkFactory,
+			    CyNetworkManager cyNetworkManager,
+			    CyNetworkViewFactory cyNetworkViewFactory,
+			    CyRootNetworkFactory cyRootNetworkFactory,
+			    CySessionManager cySessionManager,
+			    RenderingEngineFactory presentationFactory,
+			    TaskManager taskManager,
+			    VisualMappingManager visualMappingManager,
+			    CyNetworkViewManager networkViewManager,
+			    CyApplicationManager applicationManager
+					    )
+	{
+		this.cyTableFactory = cyTableFactory;
+		this.cyEventHelper = cyEventHelper;
+//		this.cyLayouts = cyLayouts;
+		this.cyNetworkFactory = cyNetworkFactory;
+		this.cyNetworkManager = cyNetworkManager;
+		this.cyNetworkViewFactory = cyNetworkViewFactory;
+		this.cyRootNetworkFactory = cyRootNetworkFactory;
+		this.cySessionManager = cySessionManager;
+		this.presentationFactory = presentationFactory;
+		this.taskManager = taskManager;
+		this.visualMappingManager = visualMappingManager;
+		this.networkViewManager = networkViewManager;
+		this.applicationManager = applicationManager;
+	}
+
+
+	//
+	// May as well keep the methods alphabetical too!
+	// 
+
+	public CyTableFactory getCyTableFactory() { return cyTableFactory; } 
+
+	public CyEventHelper getCyEventHelper() { return cyEventHelper; } 
+
+//	public CyLayouts getCyLayouts() { return cyLayouts; } 
+
+	public CyNetworkFactory getCyNetworkFactory() { return cyNetworkFactory; }
+
+	public CyNetworkManager getCyNetworkManager() { return cyNetworkManager; } 
+
+	public CyNetworkViewFactory getCyNetworkViewFactory() { return cyNetworkViewFactory; }
+
+	public CyRootNetworkFactory getCyRootNetworkFactory() { return cyRootNetworkFactory; } 
+
+	public CySessionManager getCySessionManager() { return cySessionManager; } 
+
+	public RenderingEngineFactory getPresentationFactory() { return presentationFactory; }
+
+	public TaskManager getTaskManager() { return taskManager; }
+
+	public VisualMappingManager getVisualMappingManager() { return visualMappingManager; }
+
+	public CyNetworkViewManager getCyNetworkViewManager() { return networkViewManager; }
+
+	public CyApplicationManager getCyApplicationManager() { return applicationManager; }
+}
---
+++ core3/plugin-impl/tags/plugin-impl-3.0.0-alpha1/src/test/java/org/cytoscape/plugin/internal/CyPluginAdapterImplTest.java
@@ -0,0 +1,57 @@
+package org.cytoscape.plugin.internal;
+
+import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyTableFactory;
+import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.model.subnetwork.CyRootNetworkFactory;
+import org.cytoscape.session.CyApplicationManager;
+import org.cytoscape.session.CySessionManager;
+//import org.cytoscape.view.layout.CyLayouts;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.view.model.CyNetworkViewManager;
+import org.cytoscape.view.presentation.RenderingEngineFactory;
+import org.cytoscape.view.vizmap.VisualMappingManager;
+import org.cytoscape.work.TaskManager;
+import org.cytoscape.plugin.CyPluginAdapterTest;
+
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+
+public class CyPluginAdapterImplTest extends CyPluginAdapterTest {
+
+	@Before
+	public void setUp() {
+
+		CyTableFactory cyTableFactory = mock(CyTableFactory.class);
+		CyEventHelper cyEventHelper = mock(CyEventHelper.class);
+//		CyLayouts cyLayouts = mock(CyLayouts.class);
+		CyNetworkFactory cyNetworkFactory = mock(CyNetworkFactory.class);
+		CyNetworkManager cyNetworkManager = mock(CyNetworkManager.class);
+		CyNetworkViewFactory cyNetworkViewFactory = mock(CyNetworkViewFactory.class);
+		CyRootNetworkFactory cyRootNetworkFactory = mock(CyRootNetworkFactory.class);
+		CySessionManager cySessionManager = mock(CySessionManager.class);
+		RenderingEngineFactory presentationFactory = mock(RenderingEngineFactory.class);
+		TaskManager taskManager = mock(TaskManager.class);
+		VisualMappingManager visualMappingManager = mock(VisualMappingManager.class);
+		CyNetworkViewManager networkViewManager = mock(CyNetworkViewManager.class);
+		CyApplicationManager applicationManager = mock(CyApplicationManager.class);
+
+		adapter = new CyPluginAdapterImpl( 
+                cyTableFactory,
+                cyEventHelper,
+//                cyLayouts,
+             	cyNetworkFactory,
+             	cyNetworkManager,
+             	cyNetworkViewFactory,
+             	cyRootNetworkFactory,
+             	cySessionManager,
+             	presentationFactory,
+             	taskManager,
+             	visualMappingManager,
+		networkViewManager,
+		applicationManager
+			    );
+	}
+	
+}
