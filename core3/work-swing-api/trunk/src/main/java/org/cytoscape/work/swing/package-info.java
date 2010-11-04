23782bd13dc84d2b457b7bc0b26881f534974141
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-04 17:35:24 +0000

    Fixed a typo.
    git-svn-id: http://chianti.ucsd.edu/svn@22719 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/work-swing-api/trunk/src/main/java/org/cytoscape/work/swing/package-info.java
@@ -0,0 +1,5 @@
+/**
+ *  This packages provides Swing-specific specialisations of concepts from work-api  This includes
+ *  {@link TaskFactory}, {@link TaskManager}. and {@link TaskIterator}.
+ */
+package org.cytoscape.work.swing;
