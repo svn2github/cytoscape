8a51564bdb8f9e0279c8b6db57a2469b99188135
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-08 23:37:55 +0000

    Patched up to make up for the recent directory reorg.
    git-svn-id: http://chianti.ucsd.edu/svn@22763 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/equations-api/trunk/src/main/java/org/cytoscape/equations/Node.java
+++ core3/equations-api/trunk/src/main/java/org/cytoscape/equations/Node.java
@@ -27,12 +27,10 @@
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
-package org.cytoscape.equations.parse_tree;
+package org.cytoscape.equations;
 
 
 import java.util.Stack;
-
-import org.cytoscape.equations.CodeAndSourceLocation;
 
 
 /**
