d5dfab70f185ca861579d0795fa96cb9b654d444
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-09 18:35:23 +0000

    A port from Cytoscape 2.8.
    git-svn-id: http://chianti.ucsd.edu/svn@22773 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/equations-impl/trunk/src/main/java/org/cytoscape/equations/internal/Parser.java
@@ -0,0 +1,113 @@
+/*
+  File: Parser.java
+
+  Copyright (c) 2010, The Cytoscape Consortium (www.cytoscape.org)
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License, or
+  any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+  MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+  documentation provided hereunder is on an "as is" basis, and the
+  Institute for Systems Biology and the Whitehead Institute
+  have no obligations to provide maintenance, support,
+  updates, enhancements or modifications.  In no event shall the
+  Institute for Systems Biology and the Whitehead Institute
+  be liable to any party for direct, indirect, special,
+  incidental or consequential damages, including lost profits, arising
+  out of the use of this software and its documentation, even if the
+  Institute for Systems Biology and the Whitehead Institute
+  have been advised of the possibility of such damage.  See
+  the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+package org.cytoscape.equations.internal;
+
+
+import org.cytoscape.equations.EqnParser;
+import org.cytoscape.equations.internal.builtins.*;
+
+
+/**
+ *  A singleton EqnParser
+ */
+public class Parser {
+	static private EqnParser eqnParser;
+
+	synchronized static public EqnParser getParser() {
+		if (eqnParser == null) {
+			eqnParser = new EqnParserImpl();
+
+			eqnParser.registerFunction(new Abs());
+			eqnParser.registerFunction(new ACos());
+			eqnParser.registerFunction(new ASin());
+			eqnParser.registerFunction(new And());
+			eqnParser.registerFunction(new ATan2());
+			eqnParser.registerFunction(new Average());
+			eqnParser.registerFunction(new BList());
+			eqnParser.registerFunction(new Combin());
+			eqnParser.registerFunction(new Concatenate());
+			eqnParser.registerFunction(new Cos());
+			eqnParser.registerFunction(new Cosh());
+			eqnParser.registerFunction(new Count());
+			eqnParser.registerFunction(new Degrees());
+			eqnParser.registerFunction(new org.cytoscape.equations.internal.builtins.Error());
+			eqnParser.registerFunction(new Exp());
+			eqnParser.registerFunction(new First());
+			eqnParser.registerFunction(new FList());
+			eqnParser.registerFunction(new GeoMean());
+			eqnParser.registerFunction(new HarMean());
+			eqnParser.registerFunction(new If());
+			eqnParser.registerFunction(new IList());
+			eqnParser.registerFunction(new Largest());
+			eqnParser.registerFunction(new Last());
+			eqnParser.registerFunction(new Left());
+			eqnParser.registerFunction(new Len());
+			eqnParser.registerFunction(new ListToString());
+			eqnParser.registerFunction(new Ln());
+			eqnParser.registerFunction(new Log());
+			eqnParser.registerFunction(new Lower());
+			eqnParser.registerFunction(new Max());
+			eqnParser.registerFunction(new Median());
+			eqnParser.registerFunction(new Mid());
+			eqnParser.registerFunction(new Min());
+			eqnParser.registerFunction(new Mod());
+			eqnParser.registerFunction(new Mode());
+			eqnParser.registerFunction(new Not());
+			eqnParser.registerFunction(new NormDist());
+			eqnParser.registerFunction(new Now());
+			eqnParser.registerFunction(new Nth());
+			eqnParser.registerFunction(new Or());
+			eqnParser.registerFunction(new Permut());
+			eqnParser.registerFunction(new Pi());
+			eqnParser.registerFunction(new Product());
+			eqnParser.registerFunction(new Radians());
+			eqnParser.registerFunction(new Right());
+			eqnParser.registerFunction(new Round());
+			eqnParser.registerFunction(new Sign());
+			eqnParser.registerFunction(new Sin());
+			eqnParser.registerFunction(new Sinh());
+			eqnParser.registerFunction(new SList());
+			eqnParser.registerFunction(new StDev());
+			eqnParser.registerFunction(new Sqrt());
+			eqnParser.registerFunction(new Substitute());
+			eqnParser.registerFunction(new Sum());
+			eqnParser.registerFunction(new Tan());
+			eqnParser.registerFunction(new Tanh());
+			eqnParser.registerFunction(new Text());
+			eqnParser.registerFunction(new Today());
+			eqnParser.registerFunction(new Trunc());
+			eqnParser.registerFunction(new Upper());
+			eqnParser.registerFunction(new Value());
+			eqnParser.registerFunction(new Var());
+		}
+
+		return eqnParser;
+	}
+}
