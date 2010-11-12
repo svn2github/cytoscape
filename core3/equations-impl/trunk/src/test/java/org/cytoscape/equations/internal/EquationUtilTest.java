8e3659d4ad8ce08050e735ee7b8a0da38189d716
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-12 21:40:09 +0000

    fixed package name and added ignores
    git-svn-id: http://chianti.ucsd.edu/svn@22825 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/equations-impl/trunk/src/test/java/org/cytoscape/equations/internal/EquationUtilTest.java
+++ core3/equations-impl/trunk/src/test/java/org/cytoscape/equations/internal/EquationUtilTest.java
@@ -27,7 +27,7 @@
   along with this library; if not, write to the Free Software Foundation,
   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
-package cytoscape.equations.internal;
+package org.cytoscape.equations.internal;
 
 
 import junit.framework.*;
