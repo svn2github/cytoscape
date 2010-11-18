901d03ce971e7ee69ec3cdb500893afef18fe8f7
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-18 22:53:43 +0000

    added genomespace libs
    git-svn-id: http://chianti.ucsd.edu/svn@22909 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/mes/genomespace-libs-plugin/src/main/java/cytoscape/genomespace/libs/GenomeSpaceLibsPlugin.java
@@ -0,0 +1,14 @@
+package cytoscape.genomespace.libs;
+
+import cytoscape.plugin.CytoscapePlugin;
+
+
+/**
+ * A nothing plugin that only exists to establish the libraries 
+ * included in this plugin in the Cytoscape classloader. 
+ */
+public class GenomeSpaceLibsPlugin extends CytoscapePlugin {
+	public GenomeSpaceLibsPlugin() {
+		super();
+	}
+}
