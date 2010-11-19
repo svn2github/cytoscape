e906496dd0d64f6b154f05ad72fed98f2481c4cb
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-19 23:33:14 +0000

    integration tests now configured through maven
    git-svn-id: http://chianti.ucsd.edu/svn@22950 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-impl/trunk/it/src/test/java/org/cytoscape/view/vizmap/ServiceConfigurationTest.java
+++ core3/vizmap-impl/trunk/it/src/test/java/org/cytoscape/view/vizmap/ServiceConfigurationTest.java
@@ -1,47 +1,22 @@
 package org.cytoscape.view.vizmap; 
 
-import static org.ops4j.pax.exam.CoreOptions.*;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.Inject;
-import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-
 
 import org.cytoscape.integration.ServiceTestSupport;
 
-//@RunWith(MavenConfiguredJUnit4TestRunner.class)
-@RunWith(JUnit4TestRunner.class)
-public class ServiceConfigurationTest extends ServiceTestSupport {
+import org.osgi.framework.Bundle;
 
-	@Configuration
-	public static Option[] configuration() {      
-		return options(felix(), 
-		               profile("spring.dm"), 
-		               provision( 
-		                         mavenBundle().groupId("org.cytoscape").artifactId("vizmap-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("vizmap-impl").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("viewmodel-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("viewmodel-impl").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("model-impl").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("model-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("event-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("event-impl").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("presentation-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("integration-test-support").versionAsInProject()
-		));
-	} 
+@RunWith(MavenConfiguredJUnit4TestRunner.class)
+public class ServiceConfigurationTest extends ServiceTestSupport {
 
 	@Test
 	public void testExpectedServices() {
+		for ( Bundle b : bundleContext.getBundles() )
+			System.out.println("XXXXXXXXXX bundle: " + b.getSymbolicName());
 		checkService(VisualMappingManager.class);
 		checkService(VisualStyleFactory.class);
 	}
