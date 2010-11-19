ecf87de1dace11a43393a3c42e175f27c1a80afa
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-19 00:56:31 +0000

    cleaned up to use integration test support
    git-svn-id: http://chianti.ucsd.edu/svn@22917 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/model-impl/trunk/it/src/test/java/org/cytoscape/model/ServiceConfigurationTest.java
+++ core3/model-impl/trunk/it/src/test/java/org/cytoscape/model/ServiceConfigurationTest.java
@@ -12,51 +12,16 @@ import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
 
 
 import org.cytoscape.model.subnetwork.CyRootNetworkFactory;
-import org.cytoscape.event.CyEventHelper;
-import java.util.Properties; 
-//import org.cytoscape.integration.ServiceTestSupport;
+import org.cytoscape.integration.ServiceTestSupport;
 
 //@RunWith(MavenConfiguredJUnit4TestRunner.class)
 @RunWith(JUnit4TestRunner.class)
-public class ServiceConfigurationTest /*extends ServiceTestSupport*/ {
-
-    /**
-     * The OSGi BundleContext made available for additional testing.
-     */
-    @Inject
-    protected BundleContext bundleContext;
-
-    /**
-     * Asserts that a service of the specified type exists.
-     * @param clazz The service interface type to be checked. 
-     */
-    protected void checkService(Class<?> clazz) {
-        checkService(clazz,1000);
-    }
-
-    /**
-     * Asserts that a service of the specified type exists.
-     * @param clazz The service interface type to be checked. 
-     * @param waitTime The time the service tracker should wait to 
-     * find the specified service in milliseconds.
-     */
-    protected void checkService(Class<?> clazz, int waitTime) {
-        try {
-            ServiceTracker tracker = new ServiceTracker(bundleContext, clazz.getName(), null);
-            tracker.open();
-            Object service = tracker.waitForService(waitTime);
-            tracker.close();
-            assertNotNull(service);
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
-    }
+public class ServiceConfigurationTest extends ServiceTestSupport {
 
 	@Configuration
 	public static Option[] configuration() {      
@@ -66,17 +31,17 @@ public class ServiceConfigurationTest /*extends ServiceTestSupport*/ {
 		                         mavenBundle().groupId("org.cytoscape").artifactId("model-impl").versionAsInProject(),
 		                         mavenBundle().groupId("org.cytoscape").artifactId("model-api").versionAsInProject(),
 		                         mavenBundle().groupId("org.cytoscape").artifactId("event-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("event-impl").versionAsInProject()
+		                         mavenBundle().groupId("org.cytoscape").artifactId("event-impl").versionAsInProject(),
+		                         mavenBundle().groupId("org.cytoscape").artifactId("integration-test-support").versionAsInProject()
 		));
 	}  
 
 	@Test
 	public void testExpectedServices() {
-		for ( Bundle b : bundleContext.getBundles() )
-			System.out.println("bundle: " + b.getSymbolicName());
 		checkService(CyNetworkFactory.class);
 		checkService(CyTableFactory.class);
 		checkService(CyRootNetworkFactory.class);
 		checkService(CyTableManager.class);
+		checkService(CyNetworkManager.class);
 	}
 }
