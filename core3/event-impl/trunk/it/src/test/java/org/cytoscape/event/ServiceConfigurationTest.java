86c8b3d9e96623d43900b1994c10fe6edd33a1f7
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-19 20:45:02 +0000

    integration tests now configured through maven
    git-svn-id: http://chianti.ucsd.edu/svn@22939 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/event-impl/trunk/it/src/test/java/org/cytoscape/event/ServiceConfigurationTest.java
+++ core3/event-impl/trunk/it/src/test/java/org/cytoscape/event/ServiceConfigurationTest.java
@@ -1,37 +1,15 @@
 package org.cytoscape.event; 
 
-import static org.ops4j.pax.exam.CoreOptions.*;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.Inject;
-import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-
 
 import org.cytoscape.integration.ServiceTestSupport;
 
-//@RunWith(MavenConfiguredJUnit4TestRunner.class)
-@RunWith(JUnit4TestRunner.class)
+@RunWith(MavenConfiguredJUnit4TestRunner.class)
 public class ServiceConfigurationTest extends ServiceTestSupport {
-
-	@Configuration
-	public static Option[] configuration() {      
-		return options(felix(), 
-		               profile("spring.dm"), 
-		               provision( 
-		                         mavenBundle().groupId("org.cytoscape").artifactId("event-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("event-impl").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("integration-test-support").versionAsInProject()
-		));
-	}  
 
 	@Test
 	public void testExpectedServices() {
