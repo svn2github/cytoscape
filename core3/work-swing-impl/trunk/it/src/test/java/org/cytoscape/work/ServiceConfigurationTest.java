5b8f07afdbe4d3bdcc0005b9c7b69e286ae69712
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-20 00:49:32 +0000

    refactored integration tests so that they're configured from maven
    git-svn-id: http://chianti.ucsd.edu/svn@22954 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-swing-impl/trunk/it/src/test/java/org/cytoscape/work/ServiceConfigurationTest.java
+++ core3/work-swing-impl/trunk/it/src/test/java/org/cytoscape/work/ServiceConfigurationTest.java
@@ -1,51 +1,18 @@
 package org.cytoscape.work; 
 
-import static org.ops4j.pax.exam.CoreOptions.*;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.Inject;
-import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-
 
 import org.cytoscape.work.swing.GUITaskManager;
 import org.cytoscape.work.swing.GUITunableInterceptor;
 import org.cytoscape.work.undo.UndoSupport;
 import org.cytoscape.integration.ServiceTestSupport;
 
-//@RunWith(MavenConfiguredJUnit4TestRunner.class)
-@RunWith(JUnit4TestRunner.class)
+@RunWith(MavenConfiguredJUnit4TestRunner.class)
 public class ServiceConfigurationTest extends ServiceTestSupport {
-
-	@Configuration
-	public static Option[] configuration() {      
-		return options(felix(), 
-		               profile("spring.dm"), 
-		               provision( 
-		                         mavenBundle().groupId("org.cytoscape").artifactId("model-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("viewmodel-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("presentation-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("vizmap-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("event-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("session-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("work-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("work-swing-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("work-swing-impl").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("property-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("property-impl").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("io-api").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("work-spring-hack").versionAsInProject(),
-		                         mavenBundle().groupId("org.cytoscape").artifactId("integration-test-support").versionAsInProject()
-		));
-	}  
 
 	@Test
 	public void testExpectedServices() {
