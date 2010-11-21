db8709b50518ca111aa03af5868796827c3b9396
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-21 18:49:57 +0000

    updated API
    git-svn-id: http://chianti.ucsd.edu/svn@22969 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/dnd-api/trunk/src/main/java/org/cytoscape/dnd/DropNetworkViewTaskFactory.java
+++ core3/dnd-api/trunk/src/main/java/org/cytoscape/dnd/DropNetworkViewTaskFactory.java
@@ -2,7 +2,7 @@
 package org.cytoscape.dnd;
 
 
-import java.awt.Point;
+import java.awt.geom.Point2D;
 import java.awt.datatransfer.Transferable;
 
 import org.cytoscape.view.model.CyNetworkView;
@@ -17,7 +17,8 @@ public interface DropNetworkViewTaskFactory extends NetworkViewTaskFactory {
 	/**
 	 * Sets the drop information for a TaskFactory. 
 	 * @param t The transferable object that was dropped.
-	 * @param pt The point at which the object was dropped.
+	 * @param javaPt The raw Java point at which the object was dropped.
+	 * @param xformPt The drop point transformed into Cytoscape coordinates.
 	 */
-	public void setDropInformation (Transferable t, Point pt);
+	public void setDropInformation (Transferable t, Point2D javaPt, Point2D xformPt);
 }
--- core3/dnd-api/trunk/src/main/java/org/cytoscape/dnd/DropNodeViewTaskFactory.java
+++ core3/dnd-api/trunk/src/main/java/org/cytoscape/dnd/DropNodeViewTaskFactory.java
@@ -2,7 +2,7 @@
 package org.cytoscape.dnd;
 
 
-import java.awt.Point;
+import java.awt.geom.Point2D;
 import java.awt.datatransfer.Transferable;
 
 import org.cytoscape.task.NodeViewTaskFactory;
@@ -16,7 +16,8 @@ public interface DropNodeViewTaskFactory extends NodeViewTaskFactory {
 	/**
 	 * Sets the drop information for a TaskFactory. 
 	 * @param t The transferable object that was dropped.
-	 * @param pt The point at which the object was dropped.
+	 * @param javaPt The raw coordinate point at which the object was dropped.
+	 * @param xformPt The drop point transformed into Cytoscape coordinates. 
 	 */
-	public void setDropInformation (Transferable t, Point pt);
+	public void setDropInformation (Transferable t, Point2D javaPt, Point2D xformPt);
 }
