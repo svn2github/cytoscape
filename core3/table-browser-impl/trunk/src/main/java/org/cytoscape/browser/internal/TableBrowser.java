314b1e53b9c12c525c2215af2a702c225b6479ec
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-11-23 00:23:56 +0000

    Work in progress.
    git-svn-id: http://chianti.ucsd.edu/svn@22990 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/table-browser-impl/trunk/src/main/java/org/cytoscape/browser/internal/TableBrowser.java
+++ core3/table-browser-impl/trunk/src/main/java/org/cytoscape/browser/internal/TableBrowser.java
@@ -2,7 +2,9 @@ package org.cytoscape.browser.internal;
 
 
 import java.awt.Component;
+import java.awt.Dimension;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.Icon;
 
 import org.cytoscape.application.swing.CytoPanelComponent;
@@ -11,16 +13,28 @@ import org.cytoscape.application.swing.CytoPanelName;
 
 @SuppressWarnings("serial")
 public class TableBrowser extends JPanel implements CytoPanelComponent {
+	public TableBrowser() {
+		super();
+
+		setPreferredSize(new Dimension(0, 100));
+		add(new JLabel("Here's the table browser!"));
+
+		setVisible(true);
+System.err.println("========================================== after setVisible()!");
+	}
+
 	/**
 	 * Returns the Component to be added to the CytoPanel. 
 	 * @return The Component to be added to the CytoPanel. 
 	 */
+	@Override
 	public Component getComponent() { return this; }
 
 	/**
 	 * Returns the name of the CytoPanel that this component should be added to.
 	 * @return the name of the CytoPanel that this component should be added to.
 	 */
+	@Override
 	public CytoPanelName getCytoPanelName() {
 		return CytoPanelName.SOUTH;
 	}
@@ -29,10 +43,12 @@ public class TableBrowser extends JPanel implements CytoPanelComponent {
 	 * Returns the title of the tab within the CytoPanel for this component.
 	 * @return the title of the tab within the CytoPanel for this component.
 	 */
+	@Override
 	public String getTitle() { return "Table Browser"; }
 
 	/**
 	 * @return null
 	 */
+	@Override
 	public Icon getIcon() { return null; }
 }
