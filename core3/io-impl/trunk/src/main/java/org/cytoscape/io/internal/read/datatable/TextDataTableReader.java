dd602a1494198979b4dc36f1e2b887d6f2b319e5
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-12-08 23:27:39 +0000

    Fixed an off-by-one error.
    git-svn-id: http://chianti.ucsd.edu/svn@23137 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/io-impl/trunk/src/main/java/org/cytoscape/io/internal/read/datatable/TextDataTableReader.java
+++ core3/io-impl/trunk/src/main/java/org/cytoscape/io/internal/read/datatable/TextDataTableReader.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -77,7 +78,7 @@ public class TextDataTableReader extends AbstractTableReader {
 		final CyTable table = tableFactory.createTable(tableName, columnNames[0], 
 		                                                   String.class, true);
 		
-		for ( String col : columnNames ) 
+		for (String col : Arrays.copyOfRange(columnNames, 1, columnNames.length))
 			table.createColumn(col, String.class);
 
 		return table;
