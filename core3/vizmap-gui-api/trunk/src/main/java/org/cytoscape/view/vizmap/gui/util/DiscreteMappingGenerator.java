7838fd1f52c5e97321d0971fa3cd10b2bcccfe1b
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-12-09 16:51:29 +0000

    New method had been added for type checking.
    git-svn-id: http://chianti.ucsd.edu/svn@23144 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-gui-api/trunk/src/main/java/org/cytoscape/view/vizmap/gui/util/DiscreteMappingGenerator.java
+++ core3/vizmap-gui-api/trunk/src/main/java/org/cytoscape/view/vizmap/gui/util/DiscreteMappingGenerator.java
@@ -5,5 +5,14 @@ import java.util.Set;
 
 public interface DiscreteMappingGenerator<V> {
 	
-	public <T> Map<T, V> generateMap(Set<T> attributeSet);
+	/**
+	 * For a given set of attribute values, create discrete mapping.
+	 * 
+	 * @param <T>
+	 * @param attributeSet
+	 * @return
+	 */
+	public <T> Map<T, V> generateMap(final Set<T> attributeSet);
+	
+	public Class<V> getDataType();
 }
