4167806077aa17b0f872a9645729278640e00832
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-12-10 22:39:04 +0000

    Work in progress.
    git-svn-id: http://chianti.ucsd.edu/svn@23170 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/table-browser-impl/branches/experiment1/src/main/java/org/cytoscape/browser/internal/BrowserTableModel.java
+++ core3/table-browser-impl/branches/experiment1/src/main/java/org/cytoscape/browser/internal/BrowserTableModel.java
@@ -1,6 +1,9 @@
 package org.cytoscape.browser.internal;
 
 
+import java.util.List;
+import java.util.Map;
+
 import javax.swing.table.AbstractTableModel;
 
 import org.cytoscape.model.CyTable;
@@ -20,20 +23,41 @@ public class BrowserTableModel extends AbstractTableModel
 	}
 
 	public int getRowCount() {
-		return 100;
+		return table.getRowCount();
 	}
 
 	public int getColumnCount() {
-		return 10;
+		return table.getColumnTypeMap().size();
 	}
 
-	public Object getValueAt(int row, int column) {
-		return row + "," + column;
+	public Object getValueAt(final int row, final int column) {
+		final List primaryKeyValues = table.getColumnValues(table.getPrimaryKey(),
+								    table.getPrimaryKeyType());
+
+		// Column 0 is always the primary key:
+		if (column == 0)
+			return primaryKeyValues.get(row);
+
+		final Map<String, Class<?>> columnNameToTypeMap = table.getColumnTypeMap();
+		final String primaryKey = table.getPrimaryKey();
+		int i = 1;
+		for (String columnName : columnNameToTypeMap.keySet()) {
+			if (columnName.equals(primaryKey))
+				continue;
+			if (column == i)
+				return table.getRow(primaryKeyValues.get(row))
+					.get(columnName, columnNameToTypeMap.get(columnName));
+			++i;
+		}
+
+		return new IllegalStateException("We should *never* get here!");
 	}
 
 	public void handleEvent(final ColumnCreatedEvent e) {
+		fireTableStructureChanged();
 	}
 
 	public void handleEvent(final ColumnDeletedEvent e) {
+		fireTableStructureChanged();
 	}
 }
