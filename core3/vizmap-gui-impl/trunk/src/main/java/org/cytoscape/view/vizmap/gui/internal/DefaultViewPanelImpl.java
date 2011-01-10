a8bd1a23b0936edcebadb47e5197f4cf39f7194c
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-10 17:57:31 +0000

    Use private table instead of public one for the dummy network.
    git-svn-id: http://chianti.ucsd.edu/svn@23370 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-gui-impl/trunk/src/main/java/org/cytoscape/view/vizmap/gui/internal/DefaultViewPanelImpl.java
+++ core3/vizmap-gui-impl/trunk/src/main/java/org/cytoscape/view/vizmap/gui/internal/DefaultViewPanelImpl.java
@@ -119,7 +119,7 @@ public class DefaultViewPanelImpl extends JPanel implements DefaultViewPanel {
 		this.selectedManager = selectedManager;
 
 		// Create dummy view.
-		final CyNetwork dummyNet = cyNetworkFactory.getInstance();
+		final CyNetwork dummyNet = cyNetworkFactory.getInstanceWithPrivateTables();
 
 		final CyNode source = dummyNet.addNode();
 		final CyNode target = dummyNet.addNode();
