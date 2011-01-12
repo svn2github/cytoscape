b4b7f87e45dce89dc6fe85f4d0933ce892c4c492
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-12 18:11:04 +0000

    Separated out equation-related bits.
    git-svn-id: http://chianti.ucsd.edu/svn@23424 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/model-impl/trunk/impl/src/main/java/org/cytoscape/model/internal/CyTableImpl.java
+++ core3/model-impl/trunk/impl/src/main/java/org/cytoscape/model/internal/CyTableImpl.java
@@ -29,23 +29,15 @@ package org.cytoscape.model.internal;
 
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
-import org.cytoscape.equations.BooleanList;
-import org.cytoscape.equations.DoubleList;
 import org.cytoscape.equations.Equation;
-import org.cytoscape.equations.IdentDescriptor;
 import org.cytoscape.equations.Interpreter;
-import org.cytoscape.equations.LongList;
-import org.cytoscape.equations.StringList;
 
 import org.cytoscape.event.CyEventHelper;
 
@@ -56,9 +48,6 @@ import org.cytoscape.model.events.ColumnCreatedEvent;
 import org.cytoscape.model.events.ColumnDeletedEvent;
 import org.cytoscape.model.events.RowSetMicroListener;
 import org.cytoscape.model.events.RowCreatedMicroListener;
-
-import org.cytoscape.model.internal.tsort.TopoGraphNode;
-import org.cytoscape.model.internal.tsort.TopologicalSort;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -279,7 +268,12 @@ public class CyTableImpl implements CyTable {
 		for (final Object suid : vals.keySet()) {
 			final Object value = vals.get(suid);
 			if (value instanceof Equation) {
-				final Object eqnValue = evalEquation((Equation)value, suid, columnName);
+				final StringBuilder errorMsg = new StringBuilder();
+				final Object eqnValue =
+					EqnSupport.evalEquation((Equation)value, suid, interpreter,
+								currentlyActiveAttributes,
+								columnName, errorMsg, this);
+				lastInternalError = errorMsg.toString();
 				if (eqnValue == null)
 					throw new IllegalStateException("can't convert an equation to a value!");
 				l.add(type.cast(eqnValue));
@@ -337,8 +331,9 @@ public class CyTableImpl implements CyTable {
 		if (keys == null)
 			return new HashSet<CyRow>();
 
-		final Set<CyRow> matchingRows = new HashSet<CyRow>(keys.size());
-		for (final Object key : keys)
+		final Set<Object> unmodifiableKeys = Collections.unmodifiableSet(keys);
+		final Set<CyRow> matchingRows = new HashSet<CyRow>(unmodifiableKeys.size());
+		for (final Object key : unmodifiableKeys)
 			matchingRows.add(rows.get(key));
 
 		return matchingRows;
@@ -368,13 +363,19 @@ public class CyTableImpl implements CyTable {
 
 		final Class targetType = types.get(columnName);
 		if (targetType.isAssignableFrom(value.getClass())
-		    || scalarEquationIsCompatible(value, targetType))
+		    || EqnSupport.scalarEquationIsCompatible(value, targetType))
 		{
 			if (value instanceof Equation) {
 				final Equation equation = (Equation)value;
 				// TODO this is an implicit addRow - not sure if we want to refactor this or not
 				keyToValueMap.put(key, equation);
-				final Object eqnValue = evalEquation(equation, key, columnName);
+
+				final StringBuilder errorMsg = new StringBuilder();
+				final Object eqnValue =
+					EqnSupport.evalEquation(equation, key, interpreter,
+								currentlyActiveAttributes, columnName,
+								errorMsg, this);
+				lastInternalError = errorMsg.toString();
 				if (eqnValue == null)
 					logger.warn("attempted premature evaluation evaluation for "
 						    + equation);
@@ -421,25 +422,6 @@ public class CyTableImpl implements CyTable {
 		keys.add(key);
 	}
 
-	private static boolean scalarEquationIsCompatible(final Object equationCandidate,
-							  final Class targetType)
-	{
-		if (!(equationCandidate instanceof Equation))
-			return false;
-
-		final Equation equation = (Equation)equationCandidate;
-		final Class<?> eqnReturnType = equation.getType();
-
-		if (targetType == Double.class || targetType == Boolean.class
-		    || targetType == Integer.class || targetType == Long.class)
-			return eqnReturnType == Double.class || eqnReturnType == Long.class
-			       || eqnReturnType == Boolean.class;
-		else if (targetType == String.class)
-			return true; // Everything can be turned into a String!
-		else
-			return false;
-	}
-
 	private void setListX(final Object key, final String columnName, final Object value) {
 		if (value instanceof List) {
 			final List list = (List)value;
@@ -449,7 +431,8 @@ public class CyTableImpl implements CyTable {
 			throw new IllegalArgumentException("value is a " + value.getClass().getName()
 							   + " and not a List for column '"
 							   + columnName + "'!");
-		else if (!listEquationIsCompatible((Equation)value, listElementTypes.get(columnName)))
+		else if (!EqnSupport.listEquationIsCompatible((Equation)value,
+							      listElementTypes.get(columnName)))
 			throw new IllegalArgumentException(
 				"value is not a List equation of a compatible type for column '"
 				+ columnName + "'!");
@@ -460,31 +443,18 @@ public class CyTableImpl implements CyTable {
 		final Object oldValue = keyToValueMap.get(key);
 		keyToValueMap.put(key, value);
 		final Object newValue;
-		if (value instanceof Equation)
-			newValue = evalEquation((Equation)value, suid, columnName);
-		else {
+		if (value instanceof Equation) {
+			final StringBuilder errorMsg = new StringBuilder();
+			newValue = EqnSupport.evalEquation((Equation)value, suid, interpreter,
+							   currentlyActiveAttributes, columnName,
+							   errorMsg, this);
+			lastInternalError = errorMsg.toString();
+		} else {
 			newValue = value;
 			addToReverseMap(columnName, key, oldValue, value);
 		}
 		eventHelper.getMicroListener(RowSetMicroListener.class,
 					     getRow(key)).handleRowSet(columnName, newValue, value);
-	}
-
-	private static boolean listEquationIsCompatible(final Equation equation,
-							final Class listElementType)
-	{
-		final Class<?> eqnReturnType = equation.getType();
-		if (eqnReturnType == BooleanList.class)
-			return listElementType == Boolean.class;
-		if (eqnReturnType == DoubleList.class)
-			return listElementType == Double.class;
-		if (eqnReturnType == StringList.class)
-			return listElementType == String.class;
-		if (eqnReturnType == LongList.class)
-			return listElementType == Long.class;
-		// TODO: Add support for a hypothetical IntegerList type.
-
-		return false;
 	}
 
 	private void unSetX(final Object key, final String columnName) {
@@ -512,7 +482,7 @@ public class CyTableImpl implements CyTable {
 			valueTokeysMap.remove(value);
 	}
 
-	private Object getValueOrEquation(final Object key, final String columnName) {
+	Object getValueOrEquation(final Object key, final String columnName) {
 		if (columnName.equals(primaryKey))
 			return key;
 
@@ -533,41 +503,31 @@ public class CyTableImpl implements CyTable {
 			return null;
 
 		if (vl instanceof Equation) {
-			final Object result = evalEquation((Equation)vl, key, columnName);
-			return result == null ? null : (T)convertEqnResultToColumnType(type, result);
+			final StringBuilder errorMsg = new StringBuilder();
+			final Object result =
+				EqnSupport.evalEquation((Equation)vl, key, interpreter,
+							currentlyActiveAttributes, columnName,
+							errorMsg, this);
+			lastInternalError = errorMsg.toString();
+			return result == null ? null : (T)EqnSupport.convertEqnResultToColumnType(type, result);
 		} else
 			return type.cast(vl);
 	}
 
-	private Object convertEqnResultToColumnType(final Class<?> columnType, final Object result) {
-		final Class<?> resultType = result.getClass();
-		if (resultType == columnType)
-			return result;
-
-		if (columnType == String.class)
-			return result.toString();
-
-		if (columnType == Double.class && resultType == Long.class)
-			return (double)(Long)result;
-
-		if (columnType == Boolean.class && resultType == Long.class)
-			return (Long)result == 0 ? Boolean.FALSE : Boolean.TRUE;
-
-		if (columnType == Boolean.class && resultType == Double.class)
-			return (Double)result == 0.0 ? Boolean.FALSE : Boolean.TRUE;
-
-		throw new IllegalArgumentException("unexpected equation result type " + resultType
-						   + " for a column of type " + columnType + "!");
-	}
-
-	private Object getValue(Object key, String columnName) {
+	Object getValue(Object key, String columnName) {
 		final Object vl = getValueOrEquation(key, columnName);
 		if (vl == null)
 			return null;
 
-		if (vl instanceof Equation)
-			return evalEquation((Equation)vl, key, columnName);
-		else
+		if (vl instanceof Equation) {
+			final StringBuilder errorMsg = new StringBuilder();
+			final Object value =
+				EqnSupport.evalEquation((Equation)vl, key, interpreter,
+							currentlyActiveAttributes, columnName,
+							errorMsg, this);
+			lastInternalError = errorMsg.toString();
+			return value;
+		} else
 			return vl;
 	}
 
@@ -590,7 +550,12 @@ public class CyTableImpl implements CyTable {
 			return null;
 
 		if (vl instanceof Equation) {
-			final Object result = evalEquation((Equation)vl, key, columnName);
+			final StringBuilder errorMsg = new StringBuilder();
+			final Object result =
+				EqnSupport.evalEquation((Equation)vl, key, interpreter,
+							currentlyActiveAttributes, columnName,
+							errorMsg, this);
+			lastInternalError = errorMsg.toString();
 			return (List)result;
 		} else
 			return (List)vl;
@@ -710,236 +675,5 @@ public class CyTableImpl implements CyTable {
 				builder.append(key + " = " + allVal.get(key) + ", ");
 			return builder.toString();
 		}
-	}
-
-	private Object evalEquation(final Equation equation, final Object key,
-				    final String columnName)
-	{
-		if (currentlyActiveAttributes.contains(columnName)) {
-			currentlyActiveAttributes.clear();
-			lastInternalError = "Recursive equation evaluation of \"" + columnName + "\"!";
-			return null;
-		} else
-			currentlyActiveAttributes.add(columnName);
-
-		final Collection<String> attribReferences = equation.getVariableReferences();
-
-		final Map<String, IdentDescriptor> nameToDescriptorMap = new TreeMap<String, IdentDescriptor>();
-		for (final String attribRef : attribReferences) {
-			if (attribRef.equals("ID")) {
-				nameToDescriptorMap.put("ID", new IdentDescriptor(key));
-				continue;
-			}
-
-			final Object attribValue = getValue(key, attribRef);
-			if (attribValue == null) {
-				currentlyActiveAttributes.clear();
-				lastInternalError = "Missing value for referenced attribute \"" + attribRef + "\"!";
-				logger.warn("Missing value for \"" + attribRef
-				            + "\" while evaluating an equation (ID:" + key
-				            + ", attribute name:" + columnName + ")");
-				return null;
-			}
-
-			try {
-				nameToDescriptorMap.put(attribRef, new IdentDescriptor(attribValue));
-			} catch (final Exception e) {
-				currentlyActiveAttributes.clear();
-				lastInternalError = "Bad attribute reference to \"" + attribRef + "\"!";
-				logger.warn("Bad attribute reference to \"" + attribRef
-				            + "\" while evaluating an equation (ID:" + key
-				            + ", attribute name:" + columnName + ")");
-				return null;
-			}
-		}
-
-		try {
-			final Object result = interpreter.execute(equation, nameToDescriptorMap);
-			currentlyActiveAttributes.remove(columnName);
-			return result;
-		} catch (final Exception e) {
-			currentlyActiveAttributes.clear();
-			lastInternalError = e.getMessage();
-			logger.warn("Error while evaluating an equation: " + e.getMessage() + " (ID:"
-			            + key + ", attribute name:" + columnName + ")");
-			return null;
-		}
-	}
-
-	/**
-	 *  @return an in-order list of attribute names that will have to be evaluated before "columnName" can be evaluated
-	 */
-	private List<String> topoSortAttribReferences(final Object key, final String columnName) {
-		final Object equationCandidate = getValueOrEquation(key, columnName);
-		if (!(equationCandidate instanceof Equation))
-			return new ArrayList<String>();
-
-		final Equation equation = (Equation)equationCandidate;
-		final Set<String> attribReferences = equation.getVariableReferences();
-		if (attribReferences.size() == 0)
-			return new ArrayList<String>();
-
-		final Set<String> alreadyProcessed = new TreeSet<String>();
-		alreadyProcessed.add(columnName);
-		final List<TopoGraphNode> dependencies = new ArrayList<TopoGraphNode>();
-		for (final String attribReference : attribReferences)
-                        followReferences(key, attribReference, alreadyProcessed, dependencies);
-
-
-		final List<TopoGraphNode> topoOrder = TopologicalSort.sort(dependencies);
-		final List<String> retVal = new ArrayList<String>();
-		for (final TopoGraphNode node : topoOrder) {
-			final AttribTopoGraphNode attribTopoGraphNode = (AttribTopoGraphNode)node;
-			final String nodeName = attribTopoGraphNode.getNodeName();
-			if (nodeName.equals(columnName))
-				return retVal;
-			else
-				retVal.add(nodeName);
-		}
-
-		// We should never get here because "columnName" should have been found in the for-loop above!
-		throw new IllegalStateException("\"" + columnName
-		                                + "\" was not found in the toplogical order, which should be impossible!");
-	}
-
-	/**
-	 *  Helper function for topoSortAttribReferences() performing a depth-first search of equation evaluation dependencies.
-	 */
-	private void followReferences(final Object key, final String columnName, final Collection<String> alreadyProcessed,
-	                              final Collection<TopoGraphNode> dependencies)
-	{
-		// Already visited this attribute?
-		if (alreadyProcessed.contains(columnName))
-			return;
-
-		alreadyProcessed.add(columnName);
-		final Object equationCandidate = getValueOrEquation(key, columnName);
-		if (!(equationCandidate instanceof Equation))
-			return;
-
-		final Equation equation = (Equation)equationCandidate;
-		final Set<String> attribReferences = equation.getVariableReferences();
-		for (final String attribReference : attribReferences)
-			followReferences(key, attribReference, alreadyProcessed, dependencies);
-	}
-
-	/**
-	 *  @return "x" truncated using Excel's notion of truncation.
-	 */
-	private static double excelTrunc(final double x) {
-		final boolean isNegative = x < 0.0;
-		return Math.round(x + (isNegative ? +0.5 : -0.5));
-	}
-
-	/**
-	 *  @return "d" converted to an Integer using Excel rules, should the number be outside the
-	 *          range of an int, null will be returned
-	 */
-	private static Integer doubleToInteger(final double d) {
-		if (d > Integer.MAX_VALUE || d < Integer.MIN_VALUE)
-			return null;
-
-		double x = ((Double)d).intValue();
-		if (x != d && x < 0.0)
-			--x;
-
-		return (Integer)(int)x;
-	}
-
-	/**
-	 *  @return "l" converted to an Integer using Excel rules, should the number be outside the
-	 *          range of an int, null will be returned
-	 */
-	private static Integer longToInteger(final double l) {
-		if (l >= Integer.MIN_VALUE && l <= Integer.MAX_VALUE)
-			return (Integer)(int)l;
-
-		return null;
-	}
-
-	/**
-	 *  @return "equationValue" interpreted according to Excel rules as an integer or null if
-	 *          that is not possible
-	 */
-	private Integer convertEqnRetValToInteger(final String id, final String columnName,
-						  final Object equationValue)
-	{
-		if (equationValue.getClass() == Double.class) {
-			final Integer retVal = doubleToInteger((Double)equationValue);
-			if (retVal == null)
-				logger.warn("Cannot convert a floating point value ("
-					    + equationValue + ") to an integer!  (ID:" + id
-					    + ", attribute name:" + columnName + ")");
-			return retVal;
-		}
-		else if (equationValue.getClass() == Long.class) {
-			final Integer retVal = longToInteger((Long)equationValue);
-			if (retVal == null)
-				logger.warn("Cannot convert a large integer (long) value ("
-					    + equationValue + ") to an integer! (ID:" + id
-					    + ", attribute name:" + columnName + ")");
-			return retVal;
-		}
-		else if (equationValue.getClass() == Boolean.class) {
-			final Boolean boolValue = (Boolean)equationValue;
-			return (Integer)(boolValue ? 1 : 0);
-		}
-		else
-			throw new IllegalStateException("we should never get here!");
-	}
-
-	/**
-	 *  @return "equationValue" interpreted according to Excel rules as a double or null if that is not possible
-	 */
-	private Double convertEqnRetValToDouble(final String id, final String columnName, final Object equationValue) {
-		if (equationValue.getClass() == Double.class)
-			return (Double)equationValue;
-		else if (equationValue.getClass() == Long.class)
-			return (double)(Long)(equationValue);
-		else if (equationValue.getClass() == Boolean.class) {
-			final Boolean boolValue = (Boolean)equationValue;
-			return boolValue ? 1.0 : 0.0;
-		}
-		else if (equationValue.getClass() == String.class) {
-			final String valueAsString = (String)equationValue;
-			try {
-				return Double.parseDouble(valueAsString);
-			} catch (final NumberFormatException e) {
-				logger.warn("Cannot convert a string (\"" + valueAsString
-				            + "\") to a floating point value! (ID:" + id
-                                            + ", attribute name:" + columnName + ")");
-				return null;
-			}
-		}
-		else
-			throw new IllegalStateException("we should never get here!");
-	}
-
-	/**
-	 *  @return "equationValue" interpreted according to Excel rules as a boolean
-	 */
-	private Boolean convertEqnRetValToBoolean(final String id, final String columnName, final Object equationValue) {
-		if (equationValue.getClass() == Double.class)
-			return (Double)equationValue != 0.0;
-		else if (equationValue.getClass() == Long.class)
-			return (Long)(equationValue) != 0L;
-		else if (equationValue.getClass() == Boolean.class) {
-			return (Boolean)equationValue;
-		}
-		else if (equationValue.getClass() == String.class) {
-			final String stringValue = (String)equationValue;
-			if (stringValue.compareToIgnoreCase("true") == 0)
-				return true;
-			else if (stringValue.compareToIgnoreCase("false") == 0)
-				return false;
-			else {
-				logger.warn("Cannot convert a string (\"" + stringValue
-				            + "\") to a boolean value! (ID:" + id
-                                            + ", attribute name:" + columnName + ")");
-				return null;
-			}
-		}
-		else
-			throw new IllegalStateException("we should never get here!");
 	}
 }
