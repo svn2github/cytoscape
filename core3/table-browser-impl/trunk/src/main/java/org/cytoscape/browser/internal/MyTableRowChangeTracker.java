0a849da723650622644e7c4de0ed6dbed9841471
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-18 17:21:37 +0000

    Final changes before moving it into model-api.
    git-svn-id: http://chianti.ucsd.edu/svn@23488 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/table-browser-impl/trunk/src/main/java/org/cytoscape/browser/internal/MyTableRowChangeTracker.java
+++ core3/table-browser-impl/trunk/src/main/java/org/cytoscape/browser/internal/MyTableRowChangeTracker.java
@@ -8,23 +8,25 @@ import org.cytoscape.model.CyRow;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyTableRowChangeTracker;
 import org.cytoscape.model.CyTableRowChangeTracker.RowUpdate;
+import org.cytoscape.service.util.CyServiceRegistrar;
 
 
 class MyTableRowChangeTracker extends CyTableRowChangeTracker {
 	private final BrowserTableModel browserTableModel;
 
 	MyTableRowChangeTracker(final CyTable table, final CyEventHelper eventHelper,
+				final CyServiceRegistrar serviceRegistrar,
 				final BrowserTableModel browserTableModel)
 	{
-		super(table, eventHelper);
+		super(table, eventHelper, serviceRegistrar);
 		this.browserTableModel = browserTableModel;
 	}
 
-	public void rowCreated(final CyRow newRow) {
+	public final void rowCreated(final CyRow newRow) {
 		browserTableModel.rowCreated(newRow);
 	}
 
-	public void rowsUpdated(final List<RowUpdate> updates) {
+	public final void rowsUpdated(final List<RowUpdate> updates) {
 		for (final RowUpdate update : updates)
 			browserTableModel.handleRowUpdate(update.getRow(), update.getColumnName(),
 							  update.getNewValue(),
