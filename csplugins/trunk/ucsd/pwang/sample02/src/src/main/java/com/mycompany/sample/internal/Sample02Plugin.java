34b58b58566fcaf3327bf101e0b367d5c6be4c1b
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-25 05:23:21 +0000

    got plugin working with new plugin-api
    git-svn-id: http://chianti.ucsd.edu/svn@23597 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/sample02/src/src/main/java/com/mycompany/sample/internal/Sample02Plugin.java
+++ csplugins/trunk/ucsd/pwang/sample02/src/src/main/java/com/mycompany/sample/internal/Sample02Plugin.java
@@ -4,6 +4,7 @@ package com.mycompany.sample.internal;
 import org.cytoscape.plugin.CyPluginAdapter;
 import com.mycompany.sample.internal.action.Sample02Action;
 import org.cytoscape.plugin.CyPlugin;
+import org.cytoscape.application.swing.CySwingApplication;
 
 /**
  * An implementation of CyPluginAdapter
@@ -14,7 +15,7 @@ public class Sample02Plugin extends CyPlugin {
 	public Sample02Plugin(CyPluginAdapter a){
 		super(a);
 		
-		new Sample02Action(a);
+		a.getCySwingApplication().addAction(new Sample02Action(a));
 		
 	}
 }
