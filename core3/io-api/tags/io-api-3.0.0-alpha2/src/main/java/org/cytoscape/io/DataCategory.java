138f7eb4b4bcbebcefc3f0882b3857a336038eea
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-28 01:29:05 +0000

    [maven-release-plugin]  copy for tag io-api-3.0.0-alpha2
    git-svn-id: http://chianti.ucsd.edu/svn@23720 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/io-api/tags/io-api-3.0.0-alpha2/src/main/java/org/cytoscape/io/DataCategory.java
@@ -0,0 +1,36 @@
+package org.cytoscape.io;
+
+/**
+ * An enum that captures the types of data the IO package can read and write.
+ */
+public enum DataCategory {
+	/** 
+	* Data to be import/export is NETWORK
+	*/ 
+	NETWORK, 
+	
+	/** 
+	* Data to be import/export is Table
+	*/ 
+	TABLE, 
+	
+	/** 
+	* Data to be import/export is Image
+	*/ 
+	IMAGE, 
+	
+	/** 
+	* Data to be import/export is Properties
+	*/ 
+	PROPERTIES, 
+	
+	/** 
+	* Data to be import/export is CySession
+	*/ 
+	SESSION, 
+	
+	/** 
+	* Data to be import/export is not specified (unknown)
+	*/ 
+	UNSPECIFIED;
+}
