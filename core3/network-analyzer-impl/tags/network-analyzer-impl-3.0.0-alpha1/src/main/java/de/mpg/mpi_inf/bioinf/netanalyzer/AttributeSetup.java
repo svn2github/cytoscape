ba82282578fdd9e7ba17093ee5eaf2e5d71cc6f6
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-28 19:02:31 +0000

    [maven-release-plugin]  copy for tag network-analyzer-impl-3.0.0-alpha1
    git-svn-id: http://chianti.ucsd.edu/svn@23797 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/network-analyzer-impl/tags/network-analyzer-impl-3.0.0-alpha1/src/main/java/de/mpg/mpi_inf/bioinf/netanalyzer/AttributeSetup.java
@@ -0,0 +1,57 @@
+package de.mpg.mpi_inf.bioinf.netanalyzer;
+
+
+import org.cytoscape.model.CyTable;
+
+
+final class AttributeSetup {
+
+	private AttributeSetup() {}
+
+	static void createDirectedNodeAttributes(CyTable nodeTable) {
+		createCommonNodeAttributes(nodeTable);
+		createAttr(nodeTable,"dal", Integer.class);
+		createAttr(nodeTable,"din", Integer.class);
+		createAttr(nodeTable,"dou", Integer.class);
+		createAttr(nodeTable,"nbt", Double.class);
+		createAttr(nodeTable,"nco", Double.class);
+	}
+
+	static void createUndirectedNodeAttributes(CyTable nodeTable) {
+		createCommonNodeAttributes(nodeTable);
+		createAttr(nodeTable,"deg", Integer.class);
+		createAttr(nodeTable,"nbt", Double.class); 
+		createAttr(nodeTable,"nco", Double.class);
+		createAttr(nodeTable,"nde", Integer.class);
+		createAttr(nodeTable,"nue", Integer.class);
+		createAttr(nodeTable,"rad", Double.class);
+		createAttr(nodeTable,"tco", Double.class);
+	}
+
+	private static void createCommonNodeAttributes(CyTable nodeTable) {
+		createAttr(nodeTable,"apl", Double.class); 
+		createAttr(nodeTable,"cco", Double.class);
+		createAttr(nodeTable,"clc", Double.class);
+		createAttr(nodeTable,"isn", Boolean.class);
+		createAttr(nodeTable,"pmn", Integer.class);
+		createAttr(nodeTable,"slo", Integer.class);
+		createAttr(nodeTable,"spl", Integer.class);
+		createAttr(nodeTable,"stress", Long.class);
+	}
+
+	static void createEdgeAttributes(CyTable edgeTable) {
+		createAttr(edgeTable,"ebt",Double.class);
+	}
+
+	private static void createAttr(CyTable table, String col, Class<?> newType) {
+		final Class<?> existingType = table.getType(col);
+		if ( existingType == newType )
+			return;
+		else if ( existingType == null )
+			table.createColumn(col,newType);
+		else
+			throw new IllegalArgumentException("trying to set table column: " + col + 
+			                                   " to type: " + newType + " when it already " +
+											   " has a type of: " + existingType);
+	}
+}
