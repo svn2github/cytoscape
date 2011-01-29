b53c5bc834cc9f649090ba6622dd0c249a2f0713
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-01-29 02:14:18 +0000

    [maven-release-plugin]  copy for tag filter-impl-3.0.0-alpha1
    git-svn-id: http://chianti.ucsd.edu/svn@23960 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/filter-impl/tags/filter-impl-3.0.0-alpha1/src/main/java/org/cytoscape/filter/internal/filters/util/VisualPropertyUtil.java
@@ -0,0 +1,20 @@
+package org.cytoscape.filter.internal.filters.util;
+
+import org.cytoscape.view.model.View;
+import org.cytoscape.view.model.VisualLexicon;
+import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.model.Visualizable;
+import org.cytoscape.view.presentation.property.TwoDVisualLexicon;
+
+public class VisualPropertyUtil {
+	@SuppressWarnings("unchecked")
+	public static <T> T get(VisualLexicon lexicon, View<?> view, String id, VisualProperty<Visualizable> root, Class<T> type) {
+		for (VisualProperty<?> property : lexicon.getAllDescendants(root)) {
+			if (!property.getIdString().equals(id)) {
+				continue;
+			}
+			return (T) view.getVisualProperty(property);
+		}
+		return null;
+	}
+}
