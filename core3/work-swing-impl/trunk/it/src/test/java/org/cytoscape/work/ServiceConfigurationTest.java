dc180312228a6eabf7f9c020d2f0d9e7120bf561
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-21 05:21:01 +0000

    refactored to use mock services rather than impl bundles
    git-svn-id: http://chianti.ucsd.edu/svn@24202 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-swing-impl/trunk/it/src/test/java/org/cytoscape/work/ServiceConfigurationTest.java
+++ core3/work-swing-impl/trunk/it/src/test/java/org/cytoscape/work/ServiceConfigurationTest.java
@@ -11,9 +11,22 @@ import org.cytoscape.work.swing.GUITunableInterceptor;
 import org.cytoscape.work.undo.UndoSupport;
 import org.cytoscape.integration.ServiceTestSupport;
 
+import org.cytoscape.property.CyProperty;
+import org.cytoscape.property.bookmark.BookmarksUtil;
+
+import java.util.Properties;
+
 @RunWith(MavenConfiguredJUnit4TestRunner.class)
 public class ServiceConfigurationTest extends ServiceTestSupport {
 
+	@Before
+	public void setup() {
+		Properties p = new Properties();
+		p.setProperty("cyPropertyName","bookmarks");
+		registerMockService(CyProperty.class,p);
+		registerMockService(BookmarksUtil.class);
+	}
+	
 	@Test
 	public void testExpectedServices() {
 		checkService(GUITaskManager.class);
