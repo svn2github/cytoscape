6033c09ff2bb3e7bd065dd71b721acb25fed52b6
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-21 04:56:46 +0000

    refactored for new mock service option
    git-svn-id: http://chianti.ucsd.edu/svn@24200 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-impl/trunk/it/src/test/java/org/cytoscape/view/vizmap/ServiceConfigurationTest.java
+++ core3/vizmap-impl/trunk/it/src/test/java/org/cytoscape/view/vizmap/ServiceConfigurationTest.java
@@ -8,15 +8,35 @@ import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
 
 import org.cytoscape.integration.ServiceTestSupport;
 
-import org.osgi.framework.Bundle;
+import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.view.presentation.RenderingEngineManager;
+import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
+
+import java.util.Properties;
 
 @RunWith(MavenConfiguredJUnit4TestRunner.class)
 public class ServiceConfigurationTest extends ServiceTestSupport {
 
+	@Before
+	public void setup() {
+		registerMockService(CyEventHelper.class);
+		registerMockService(RenderingEngineManager.class);
+
+		Properties p1 = new Properties();
+		p1.setProperty("mapping.type","discrete");
+		registerMockService(VisualMappingFunctionFactory.class, p1);
+
+		Properties p2 = new Properties();
+		p2.setProperty("mapping.type","continuous");
+		registerMockService(VisualMappingFunctionFactory.class, p2);
+
+		Properties p3 = new Properties();
+		p3.setProperty("mapping.type","passthrough"); 
+		registerMockService(VisualMappingFunctionFactory.class, p3);
+	}
+
 	@Test
 	public void testExpectedServices() {
-		for ( Bundle b : bundleContext.getBundles() )
-			System.out.println("XXXXXXXXXX bundle: " + b.getSymbolicName());
 		checkService(VisualMappingManager.class);
 		checkService(VisualStyleFactory.class);
 		checkService(VisualStyleSerializer.class);
