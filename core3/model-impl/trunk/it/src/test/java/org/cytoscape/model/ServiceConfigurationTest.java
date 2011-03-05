028b797c1fbae63e98a55362ef2d55ce3dcc7a74
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-03-05 21:44:47 +0000

    now registering additional necessary services
    git-svn-id: http://chianti.ucsd.edu/svn@24315 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/model-impl/trunk/it/src/test/java/org/cytoscape/model/ServiceConfigurationTest.java
+++ core3/model-impl/trunk/it/src/test/java/org/cytoscape/model/ServiceConfigurationTest.java
@@ -10,6 +10,8 @@ import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
 import org.cytoscape.model.subnetwork.CyRootNetworkFactory;
 import org.cytoscape.integration.ServiceTestSupport;
 import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.equations.Interpreter;
+import org.cytoscape.service.util.CyServiceRegistrar;
 
 @RunWith(MavenConfiguredJUnit4TestRunner.class)
 public class ServiceConfigurationTest extends ServiceTestSupport {
@@ -17,6 +19,8 @@ public class ServiceConfigurationTest extends ServiceTestSupport {
 	@Before 
 	public void setup() {
 		registerMockService(CyEventHelper.class);
+		registerMockService(Interpreter.class);
+		registerMockService(CyServiceRegistrar.class);
 	}
 
 	@Test
