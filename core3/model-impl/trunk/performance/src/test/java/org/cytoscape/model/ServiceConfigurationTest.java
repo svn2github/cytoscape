caf3a618dbbbc8844586d912dbf0718ad8fa6d5c
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-03-21 21:23:02 +0000

    Increased time to wait for OSGi services.
    git-svn-id: http://chianti.ucsd.edu/svn@24523 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/model-impl/trunk/performance/src/test/java/org/cytoscape/model/ServiceConfigurationTest.java
+++ core3/model-impl/trunk/performance/src/test/java/org/cytoscape/model/ServiceConfigurationTest.java
@@ -15,12 +15,18 @@ import org.cytoscape.event.CyEventHelper;
 import org.cytoscape.event.DummyCyEventHelper;
 import org.cytoscape.equations.Interpreter;
 import org.cytoscape.integration.ServiceTestSupport;
+import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.model.events.RowCreatedMicroListener;
 import org.cytoscape.model.events.RowSetMicroListener;
 import org.cytoscape.service.util.CyServiceRegistrar;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 @RunWith(MavenConfiguredJUnit4TestRunner.class)
@@ -40,7 +46,7 @@ public class ServiceConfigurationTest extends ServiceTestSupport {
 		// Obtain a CyNetworkFactory service:
 		CyNetworkFactory factory = null;
 		try {
-			final int WAIT_TIME = 2000; // seconds
+			final int WAIT_TIME = 4000; // seconds
 			factory = (CyNetworkFactory)tracker.waitForService(WAIT_TIME);
 		} catch (final InterruptedException ie) {
 			fail("Did not get an instance of a CyNetworkFactory service within the specified amount of time!");
@@ -49,5 +55,32 @@ public class ServiceConfigurationTest extends ServiceTestSupport {
 
 		final CyNetwork network = factory.getInstance();
 		assertNotNull(network);
+
+		final int NODE_COUNT = 50000;
+		final List<CyNode> nodes = new ArrayList<CyNode>(NODE_COUNT);
+		for (int i = 0; i < NODE_COUNT; ++i)
+			nodes.add(network.addNode());
+
+		boolean isDirected = true;
+		final Random rand = new Random(1234L);
+
+		final int EDGE_COUNT = 100000;
+		final List<CyEdge> edges = new ArrayList<CyEdge>(EDGE_COUNT);
+		for (int i = 0; i < EDGE_COUNT; ++i) {
+			final CyNode source = nodes.get(rand.nextInt(NODE_COUNT));
+			final CyNode target = nodes.get(rand.nextInt(NODE_COUNT));
+			edges.add(network.addEdge(source, target, isDirected));
+			isDirected = !isDirected;
+		}
+
+		for (int i = 0; i < 1000; ++i)
+			network.getNeighborList(nodes.get(i), CyEdge.Type.ANY);
+		for (int i = 0; i < 1000; ++i)
+			network.getAdjacentEdgeList(nodes.get(i), CyEdge.Type.ANY);
+		for (int i = 0; i < 1000; ++i) {
+			final CyNode source = nodes.get(i);
+			final CyNode target = nodes.get(i + 1000);
+			network.getConnectingEdgeList(source, target, CyEdge.Type.ANY);
+		}
 	}
 }
