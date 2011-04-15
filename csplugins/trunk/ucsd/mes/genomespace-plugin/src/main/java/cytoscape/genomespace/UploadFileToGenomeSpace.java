c690567375d857bfcc36927d342981de04557276
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-04-15 20:19:27 +0000

    Made it work and look great.
    git-svn-id: http://chianti.ucsd.edu/svn@24768 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/mes/genomespace-plugin/src/main/java/cytoscape/genomespace/UploadFileToGenomeSpace.java
+++ csplugins/trunk/ucsd/mes/genomespace-plugin/src/main/java/cytoscape/genomespace/UploadFileToGenomeSpace.java
@@ -1,15 +1,17 @@
 package cytoscape.genomespace;
 
+
 import cytoscape.Cytoscape;
+import cytoscape.logger.CyLogger;
 import cytoscape.util.CytoscapeAction;
 import cytoscape.util.FileUtil;
-import cytoscape.logger.CyLogger;
-import java.awt.event.ActionEvent;
-import javax.swing.JOptionPane;
 
-import java.io.File;
 import java.awt.FileDialog;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.Date;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 import org.genomespace.datamanager.core.GSFileMetadata;
 import org.genomespace.datamanager.core.GSFileMetadataImpl;
@@ -17,13 +19,13 @@ import org.genomespace.client.DataManagerClient;
 import org.genomespace.client.GsSession;
 import org.genomespace.client.User;
 
+
 /**
  * A simple action.  Change the names as appropriate and
  * then fill in your expected behavior in the actionPerformed()
  * method.
  */
 public class UploadFileToGenomeSpace extends CytoscapeAction {
-
 	private static final long serialVersionUID = 9988760123456789L;
 	private static final CyLogger logger = CyLogger.getLogger(UploadFileToGenomeSpace.class);
 
@@ -34,25 +36,33 @@ public class UploadFileToGenomeSpace extends CytoscapeAction {
 		// Set the menu you'd like here.  Plugins don't need
 		// to live in the Plugins menu, so choose whatever
 		// is appropriate!
-        setPreferredMenu("Plugins.GenomeSpace");
+		setPreferredMenu("Plugins.GenomeSpace");
 	}
 
 	public void actionPerformed(ActionEvent e) {
-
 		try {
-		File f =  FileUtil.getFile("",FileDialog.LOAD);
-		if ( f == null )
-			return;
+			File f =  FileUtil.getFile("Select file to upload", FileDialog.LOAD);
+			if (f == null)
+				return;
 
-		GsSession client = GSUtils.getSession(); 
-		DataManagerClient dmc = client.getDataManagerClient();
+			GsSession client = GSUtils.getSession(); 
+			DataManagerClient dmc = client.getDataManagerClient();
 
-		// TODO add metadata info?
-		dmc.uploadFile(f, new GSFileMetadataImpl());
-	
-		} catch (Exception ex) {
-			logger.error("GenomeSpace failed",ex);
+			final String targetDirectoryPath =
+				dmc.listDefaultDirectory().getDirectory().getPath();
+			final GSFileMetadata uploadedFileMetadata =
+				dmc.uploadFile(f, targetDirectoryPath, f.getName());
+			if (uploadedFileMetadata != null)
+				JOptionPane.showMessageDialog(
+						Cytoscape.getDesktop(),
+						f.getName() + " successfully uploaded to GenomeSpace!",
+						 "Information", JOptionPane.INFORMATION_MESSAGE);
+			
+		} catch (final Exception ex) {
+			logger.error("GenomeSpace failed", ex);
+			JOptionPane.showMessageDialog(Cytoscape.getDesktop(),
+						      ex.getMessage(), "GenomeSpace Error",
+						      JOptionPane.ERROR_MESSAGE);
 		}
 	}
-
 }
