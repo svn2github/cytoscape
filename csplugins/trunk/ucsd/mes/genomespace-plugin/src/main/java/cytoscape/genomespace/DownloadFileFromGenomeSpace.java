d2433cdc13f52b582044b72afe6dd345769aa46b
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-04-15 22:23:35 +0000

    Spiffed up file downloading!
    git-svn-id: http://chianti.ucsd.edu/svn@24769 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/mes/genomespace-plugin/src/main/java/cytoscape/genomespace/DownloadFileFromGenomeSpace.java
+++ csplugins/trunk/ucsd/mes/genomespace-plugin/src/main/java/cytoscape/genomespace/DownloadFileFromGenomeSpace.java
@@ -1,22 +1,26 @@
 package cytoscape.genomespace;
 
+
 import cytoscape.Cytoscape;
+import cytoscape.genomespace.filechoosersupport.GenomeSpaceFileSystemView;
+import cytoscape.logger.CyLogger;
 import cytoscape.util.CytoscapeAction;
 import cytoscape.util.FileUtil;
-import cytoscape.logger.CyLogger;
+
+import java.awt.FileDialog;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.List;
+import java.util.Collection;
+import java.util.Map;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
-import java.io.File;
-import java.awt.FileDialog;
-import java.util.List;
-import java.util.Map;
-import java.util.Collection;
-
-import org.genomespace.datamanager.core.GSFileMetadata;
-import org.genomespace.client.GsSession;
 import org.genomespace.client.DataManagerClient;
+import org.genomespace.client.GsSession;
 import org.genomespace.client.User;
+import org.genomespace.datamanager.core.GSFileMetadata;
+
 
 /**
  * A simple action.  Change the names as appropriate and
@@ -24,7 +28,6 @@ import org.genomespace.client.User;
  * method.
  */
 public class DownloadFileFromGenomeSpace extends CytoscapeAction {
-
 	private static final long serialVersionUID = 7777788473487659L;
 	private static final CyLogger logger = CyLogger.getLogger(DownloadFileFromGenomeSpace.class);
 
@@ -35,31 +38,45 @@ public class DownloadFileFromGenomeSpace extends CytoscapeAction {
 		// Set the menu you'd like here.  Plugins don't need
 		// to live in the Plugins menu, so choose whatever
 		// is appropriate!
-        setPreferredMenu("Plugins.GenomeSpace");
+		setPreferredMenu("Plugins.GenomeSpace");
 	}
 
 	public void actionPerformed(ActionEvent e) {
-
 		try {
+			GsSession client = GSUtils.getSession(); 
+			DataManagerClient dmc = client.getDataManagerClient();
 
-		GsSession client = GSUtils.getSession(); 
-		DataManagerClient dmc = client.getDataManagerClient();
+			final JFileChooser chooser = new JFileChooser(new GenomeSpaceFileSystemView(dmc));
+			int returnVal = chooser.showDialog(Cytoscape.getDesktop(), "Download");
+			if (returnVal != JFileChooser.APPROVE_OPTION)
+				return;
+			final String selectedFile = chooser.getSelectedFile().getName();
 
-		// list the files present for this user
-		Map<String,GSFileMetadata> files = GSUtils.getFileNameMap( dmc.listDefaultDirectory().getContents() );
-
-		String selectedFile = getSelectedFile( files.keySet() ); 
-
-		// Download the file back from GenomeSpace
-		if (selectedFile != null && files.get(selectedFile) != null) {
-			logger.info("Downloading " + files.get(selectedFile));
-			File localCopy = new File(selectedFile);
-			dmc.downloadFile(files.get(selectedFile), localCopy, true);
-			logger.info("\t saved to: " + localCopy.getAbsolutePath());
-		}
-	
+			// Download the file from GenomeSpace:
+			if (selectedFile != null) {
+				logger.info("Downloading " + selectedFile);
+				final String path =
+					dmc.listDefaultDirectory().getDirectory().getPath()
+					+ "/" + selectedFile;
+				final GSFileMetadata downloadFileMetadata =
+					dmc.getMetadata(path);
+				final JFileChooser saveChooser = new JFileChooser();
+				final File defaultSaveFile =
+					new File(System.getProperty("user.home") + File.separator
+						 + selectedFile);
+				saveChooser.setSelectedFile(defaultSaveFile);
+				returnVal = saveChooser.showSaveDialog(Cytoscape.getDesktop());
+				if (returnVal != JFileChooser.APPROVE_OPTION)
+					return;
+				final File downloadTarget = saveChooser.getSelectedFile();
+				dmc.downloadFile(downloadFileMetadata, downloadTarget, true);
+				logger.info("Saved downloaded file as " + downloadTarget);
+			}
 		} catch (Exception ex) {
 			logger.error("GenomeSpace failed",ex);
+			JOptionPane.showMessageDialog(Cytoscape.getDesktop(),
+						      ex.getMessage(), "GenomeSpace Error",
+						      JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
