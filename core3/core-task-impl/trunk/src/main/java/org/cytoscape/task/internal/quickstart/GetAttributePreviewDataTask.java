2cd42b3173d5e6678ffcb498a85c991d4ece2ef5
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-04-26 00:03:52 +0000

    Create message for table-Load-Summary
    git-svn-id: http://chianti.ucsd.edu/svn@24834 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/GetAttributePreviewDataTask.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/GetAttributePreviewDataTask.java
@@ -6,17 +6,20 @@ import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyColumn;
+import java.util.Iterator;
 
 ///////
 public class GetAttributePreviewDataTask extends AbstractTask {
 	private String[] previewKey;
 	private String[][] previewData;
 	private CyTableReader reader;
+	private QuickStartState state;
 	
-	public GetAttributePreviewDataTask(CyTableReader reader, String[] previewKey, String[][] previewData){
+	public GetAttributePreviewDataTask(QuickStartState state, CyTableReader reader, String[] previewKey, String[][] previewData){
 		this.reader = reader;
 		this.previewKey = previewKey;
 		this.previewData = previewData;
+		this.state = state;
 	}
 	
 	@Override
@@ -37,6 +40,17 @@ public class GetAttributePreviewDataTask extends AbstractTask {
 		
 		for (int i=0; i< colCount; i++){
 			this.previewData[i][0] = values.get(i).toString();
-		}		
+		}
+		
+		//
+		String msg = "column names: ";
+		Iterator<CyColumn> it = tbls[0].getColumns().iterator();
+		while (it.hasNext()){
+			CyColumn col = it.next();
+			msg += col.getName();
+		}
+		msg += "\nTotal rows: "+tbls[0].getRowCount();
+		
+		state.setTableLoadSummaryMessage(msg);
 	}
 }
