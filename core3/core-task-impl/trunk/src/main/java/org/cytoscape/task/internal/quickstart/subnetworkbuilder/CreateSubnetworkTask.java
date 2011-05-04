54d74409d5e67e53c3257747eed1ed57f9c4272f
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-05-04 22:27:29 +0000

    First version supporting other ID sets (gene symbol/ensemble) in subnetwork builder.
    git-svn-id: http://chianti.ucsd.edu/svn@24928 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/subnetworkbuilder/CreateSubnetworkTask.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/subnetworkbuilder/CreateSubnetworkTask.java
@@ -1,7 +1,10 @@
 package org.cytoscape.task.internal.quickstart.subnetworkbuilder;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
@@ -9,99 +12,129 @@ import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyTableEntry;
 import org.cytoscape.model.events.RowsAboutToChangeEvent;
 import org.cytoscape.model.events.RowsFinishedChangingEvent;
+import org.cytoscape.task.internal.quickstart.IDType;
 import org.cytoscape.task.internal.select.SelectFirstNeighborsTask;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.Task;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.work.Tunable;
+import org.cytoscape.work.util.ListSingleSelection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CreateSubnetworkTask extends AbstractTask {
 
-    private static final Logger logger = LoggerFactory.getLogger(CreateSubnetworkTask.class);
+	private static final Logger logger = LoggerFactory.getLogger(CreateSubnetworkTask.class);
 
-    static final String QUERY_GENE_ATTR_NAME = "Gene Type";
-    static final String SEARCH_GENE_ATTR_NAME = "Search Term";
+	static final String QUERY_GENE_ATTR_NAME = "Gene Type";
+	static final String SEARCH_GENE_ATTR_NAME = "Search Term";
 
-    @Tunable(description = "Enter list of genes you are interested in (NCBI Entrez Gene ID)")
-    public String queryGenes;
+	@Tunable(description = "Enter list of genes you are interested in (should be separated by space)")
+	public String queryGenes;
 
-    private final SubnetworkBuilderUtil util;
-    private final SubnetworkBuilderState state;
+	@Tunable(description = "Select ID Type")
+	public ListSingleSelection<IDType> selection = new ListSingleSelection<IDType>(IDType.GENE_SYMBOL, IDType.ENSEMBL,
+			IDType.ENTREZ_GENE, IDType.UNIPROT);
 
-    CreateSubnetworkTask(final SubnetworkBuilderUtil util, final SubnetworkBuilderState state) {
-	this.util = util;
-	this.state = state;
-    }
+	private final SubnetworkBuilderUtil util;
+	private final SubnetworkBuilderState state;
 
-    @Override
-    public void run(TaskMonitor taskMonitor) throws Exception {
-	taskMonitor.setStatusMessage("Searching related genes in parent network...");
-	taskMonitor.setProgress(0.0);
-	final String[] genes = queryGenes.split("\\s+");
-	logger.debug("Got gene list: " + genes.length);
-	for (final String gene : genes) {
-	    logger.debug("Gene: " + gene);
+	CreateSubnetworkTask(final SubnetworkBuilderUtil util, final SubnetworkBuilderState state) {
+		this.util = util;
+		this.state = state;
 	}
 
-	selectGenes(genes);
-	
-	taskMonitor.setProgress(1.0);
+	@Override
+	public void run(TaskMonitor taskMonitor) throws Exception {
+		taskMonitor.setStatusMessage("Searching related genes in parent network...");
+		taskMonitor.setProgress(-1);
 
-    }
+		final IDType selected = selection.getSelectedValue();
 
-    private void selectGenes(final String[] genes) {
-	final CyNetwork target = util.appManager.getCurrentNetwork();
-	final CyTable nodeTable = target.getDefaultNodeTable();
-
-	if (nodeTable.getColumn(QUERY_GENE_ATTR_NAME) == null)
-	    nodeTable.createColumn(QUERY_GENE_ATTR_NAME, String.class, false);
-	if (nodeTable.getColumn(SEARCH_GENE_ATTR_NAME) == null)
-	    nodeTable.createColumn(SEARCH_GENE_ATTR_NAME, String.class, false);
-
-	boolean found = false;
-	final List<String> geneList = Arrays.asList(genes);
-	try {
-	    util.eventHelper.fireSynchronousEvent(new RowsAboutToChangeEvent(this, nodeTable));
-
-	    List<CyNode> nodeList = target.getNodeList();
-	    for (final CyNode node : nodeList) {
-		final String nodeName = node.getCyRow().get(CyTableEntry.NAME, String.class);
+		final List<String> geneList;
 		
-		if(geneList.contains(nodeName) && state.getDiseaseGenes().contains(nodeName)) {
-		    node.getCyRow().set(CyNetwork.SELECTED, true);
-		    node.getCyRow().set(QUERY_GENE_ATTR_NAME, "query and disease");
-		    node.getCyRow().set(SEARCH_GENE_ATTR_NAME, state.getSearchTerms());
-		    found = true;
-		} else if (geneList.contains(nodeName)) {
-		    node.getCyRow().set(CyNetwork.SELECTED, true);
-		    node.getCyRow().set(QUERY_GENE_ATTR_NAME, "query");
-		    found = true;
-		} else if (state.getDiseaseGenes().contains(nodeName)) {
-		    node.getCyRow().set(CyNetwork.SELECTED, true);
-		    node.getCyRow().set(QUERY_GENE_ATTR_NAME, "disease");
-		    node.getCyRow().set(SEARCH_GENE_ATTR_NAME, state.getSearchTerms());
+		if (selected == IDType.ENTREZ_GENE) {
+			final String[] genes = queryGenes.split("\\s+");
+			logger.debug("Got gene list: " + genes.length);
+			for (final String gene : genes) {
+				logger.debug("Gene: " + gene);
+			}
+			geneList = Arrays.asList(genes);
+		} else {
+			geneList = new ArrayList<String>(convert(selected));
 		}
-	    }
-	} finally {
-	    util.eventHelper.fireSynchronousEvent(new RowsFinishedChangingEvent(this, nodeTable));
+
+		
+		selectGenes(geneList);
+
+		taskMonitor.setProgress(1.0);
+
 	}
 
-	if (!found) {
-	    logger.error("Query genes were not found in the interactome.");
-	    return;
+	private Set<String> convert(IDType selected) throws IOException {
+		final boolean isGeneSymbol;
+
+		if (selected == IDType.GENE_SYMBOL)
+			isGeneSymbol = true;
+		else
+			isGeneSymbol = false;
+
+		final NCBISearchClient client = new NCBISearchClient();
+
+		return client.convert(queryGenes, isGeneSymbol);
 	}
 
-	this.insertTasksAfterCurrentTask(new BuildVisualStyleTask(util));
-	
-	this.insertTasksAfterCurrentTask(util.getApplLayoutTask());
-	
-	Task createNetworkTask = util.getNewNetworkSelectedNodesOnlyTask(target);
-	this.insertTasksAfterCurrentTask(createNetworkTask);
+	private void selectGenes(final List<String> geneList) {
+		final CyNetwork target = util.appManager.getCurrentNetwork();
+		final CyTable nodeTable = target.getDefaultNodeTable();
 
-	SelectFirstNeighborsTask nextTask = new SelectFirstNeighborsTask(target, util.networkViewManager,
-		util.eventHelper);
-	this.insertTasksAfterCurrentTask(nextTask);
-    }
+		if (nodeTable.getColumn(QUERY_GENE_ATTR_NAME) == null)
+			nodeTable.createColumn(QUERY_GENE_ATTR_NAME, String.class, false);
+		if (nodeTable.getColumn(SEARCH_GENE_ATTR_NAME) == null)
+			nodeTable.createColumn(SEARCH_GENE_ATTR_NAME, String.class, false);
+
+		boolean found = false;
+		
+		try {
+			util.eventHelper.fireSynchronousEvent(new RowsAboutToChangeEvent(this, nodeTable));
+
+			List<CyNode> nodeList = target.getNodeList();
+			for (final CyNode node : nodeList) {
+				final String nodeName = node.getCyRow().get(CyTableEntry.NAME, String.class);
+
+				if (geneList.contains(nodeName) && state.getDiseaseGenes().contains(nodeName)) {
+					node.getCyRow().set(CyNetwork.SELECTED, true);
+					node.getCyRow().set(QUERY_GENE_ATTR_NAME, "query and disease");
+					node.getCyRow().set(SEARCH_GENE_ATTR_NAME, state.getSearchTerms());
+					found = true;
+				} else if (geneList.contains(nodeName)) {
+					node.getCyRow().set(CyNetwork.SELECTED, true);
+					node.getCyRow().set(QUERY_GENE_ATTR_NAME, "query");
+					found = true;
+				} else if (state.getDiseaseGenes().contains(nodeName)) {
+					node.getCyRow().set(CyNetwork.SELECTED, true);
+					node.getCyRow().set(QUERY_GENE_ATTR_NAME, "disease");
+					node.getCyRow().set(SEARCH_GENE_ATTR_NAME, state.getSearchTerms());
+				}
+			}
+		} finally {
+			util.eventHelper.fireSynchronousEvent(new RowsFinishedChangingEvent(this, nodeTable));
+		}
+
+		if (!found) {
+			logger.error("Query genes were not found in the interactome.");
+			return;
+		}
+
+		this.insertTasksAfterCurrentTask(new BuildVisualStyleTask(util));
+
+		this.insertTasksAfterCurrentTask(util.getApplLayoutTask());
+
+		Task createNetworkTask = util.getNewNetworkSelectedNodesOnlyTask(target);
+		this.insertTasksAfterCurrentTask(createNetworkTask);
+
+		SelectFirstNeighborsTask nextTask = new SelectFirstNeighborsTask(target, util.networkViewManager,
+				util.eventHelper);
+		this.insertTasksAfterCurrentTask(nextTask);
+	}
 }
--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/subnetworkbuilder/SearchRelatedGenesTask.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/subnetworkbuilder/SearchRelatedGenesTask.java
@@ -10,36 +10,36 @@ import org.slf4j.LoggerFactory;
 
 public class SearchRelatedGenesTask extends AbstractTask {
 
-    private static final Logger logger = LoggerFactory.getLogger(SearchRelatedGenesTask.class);
+	private static final Logger logger = LoggerFactory.getLogger(SearchRelatedGenesTask.class);
 
-    @Tunable(description = "Gene Ontology")
-    public String go;
+	@Tunable(description = "Gene Ontology")
+	public String go;
 
-    @Tunable(description = "Disease/Phynotype")
-    public String phynotype;
+	@Tunable(description = "Disease/Phynotype")
+	public String phynotype;
 
-    private final SubnetworkBuilderState state;
-    private final SubnetworkBuilderUtil util;
+	private final SubnetworkBuilderState state;
+	private final SubnetworkBuilderUtil util;
 
-    SearchRelatedGenesTask(final SubnetworkBuilderUtil util, final SubnetworkBuilderState state) {
-	this.state = state;
-	this.util = util;
-    }
+	SearchRelatedGenesTask(final SubnetworkBuilderUtil util, final SubnetworkBuilderState state) {
+		this.state = state;
+		this.util = util;
+	}
 
-    @Override
-    public void run(TaskMonitor tm) throws Exception {
-	tm.setStatusMessage("Searching NCBI Gene Database...");
+	@Override
+	public void run(TaskMonitor tm) throws Exception {
+		tm.setStatusMessage("Searching NCBI Gene Database...");
 
-	tm.setProgress(0.0);
-	
-	insertTasksAfterCurrentTask(new CreateSubnetworkTask(util, state));
-	
-	final NCBISearchClient client = new NCBISearchClient();
+		tm.setProgress(0.0);
 
-	final Set<String> idSet = client.search(phynotype, go);
-	state.setDiseaseGenes(idSet);
-	state.setSearchTerms(phynotype + "," + go);
-	
-	logger.info("NCBI Gene database returns " + idSet.size() + " gene IDs.");
-    }
+		insertTasksAfterCurrentTask(new CreateSubnetworkTask(util, state));
+
+		final NCBISearchClient client = new NCBISearchClient();
+
+		final Set<String> idSet = client.search(phynotype, go);
+		state.setDiseaseGenes(idSet);
+		state.setSearchTerms(phynotype + "," + go);
+
+		logger.info("NCBI Gene database returns " + idSet.size() + " gene IDs.");
+	}
 }
