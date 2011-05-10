76bba2b6ba5f21543a24730432e2d26cce7fee2d
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-05-10 20:51:15 +0000

    First complete plugin.
    git-svn-id: http://chianti.ucsd.edu/svn@25000 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/ruschein/SimpleCytoscape3Plugin/org/cytoscape/plugin/example/MenuAction.java
+++ csplugins/trunk/ucsd/ruschein/SimpleCytoscape3Plugin/org/cytoscape/plugin/example/MenuAction.java
@@ -3,16 +3,33 @@ package org.cytoscape.plugin.example;
 
 import java.awt.event.ActionEvent;
 import org.cytoscape.application.swing.AbstractCyAction;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
 import org.cytoscape.plugin.CyPluginAdapter;
+import org.cytoscape.view.model.CyNetworkView;
+import static org.cytoscape.view.presentation.property.MinimalVisualLexicon.NODE_VISIBLE;
 
 
 public class MenuAction extends AbstractCyAction {
+	private final CyPluginAdapter adapter;
+
 	public MenuAction(CyPluginAdapter adapter) {
 		super("Hide unconnected nodes", adapter.getCyApplicationManager());
+		this.adapter = adapter;
 		setPreferredMenu("Select");
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("Jello mol;d!");
+		final CyNetwork currentNetwork = adapter.getCyApplicationManager().getCurrentNetwork();
+		if (currentNetwork == null)
+			return;
+
+		final CyNetworkView currentNetworkView = adapter.getCyApplicationManager().getCurrentNetworkView();
+
+		for (CyNode node : currentNetwork.getNodeList()) {
+			if (currentNetwork.getNeighborList(node, CyEdge.Type.ANY).isEmpty())
+				currentNetworkView.getNodeView(node).setVisualProperty(NODE_VISIBLE, false);
+		}
 	}
 }
