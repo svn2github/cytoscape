1eacb8e2c38aa465701ce681e2404e27b811138f
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-05-11 22:36:31 +0000

    Logger dependency had been added.
    git-svn-id: http://chianti.ucsd.edu/svn@25013 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/archetypes/trunk/bundle-plugin/src/main/resources/archetype-resources/src/main/java/internal/Cy3PluginImpl.java
+++ core3/archetypes/trunk/bundle-plugin/src/main/resources/archetype-resources/src/main/java/internal/Cy3PluginImpl.java
@@ -3,12 +3,21 @@
 #set( $symbol_escape = '\' )
 package ${package}.internal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ${package}.Cy3Plugin;
 
 public class Cy3PluginImpl implements Cy3Plugin {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Cy3PluginImpl.class);
+	
+	public Cy3PluginImpl() {
+		logger.debug("An instance of Cy3PluginImpl has been created.");
+	}
 
 	public void doSomething() {
-		
+		// Write your function here.
 	}
 
 }
