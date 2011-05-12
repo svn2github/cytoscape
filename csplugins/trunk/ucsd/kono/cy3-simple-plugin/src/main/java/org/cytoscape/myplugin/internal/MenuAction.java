1ac718b8b13c81a2b4157a45c0fda88a383387e4
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-05-12 19:48:20 +0000

    Working version of sample plugin for tutorial.
    git-svn-id: http://chianti.ucsd.edu/svn@25028 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/kono/cy3-simple-plugin/src/main/java/org/cytoscape/myplugin/internal/MenuAction.java
+++ csplugins/trunk/ucsd/kono/cy3-simple-plugin/src/main/java/org/cytoscape/myplugin/internal/MenuAction.java
@@ -15,21 +15,21 @@ public class MenuAction extends AbstractCyAction {
 	private static final long serialVersionUID = -7457271379655577597L;
 
 	public MenuAction(final CyApplicationManager applicationManager) {
-		super("Hide unconnected nodes", applicationManager);
+		super("Hide unconnected nodes (OSGi bundle version)", applicationManager);
 		setPreferredMenu("Select");
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		final CyNetwork currentNetwork = this.applicationManager
-				.getCurrentNetwork();
-		if (currentNetwork == null)
+
+		final CyNetworkView currentNetworkView = applicationManager.getCurrentNetworkView();
+		if(currentNetworkView == null)
 			return;
-
-		final CyNetworkView currentNetworkView = applicationManager
-				.getCurrentNetworkView();
-
-		for (CyNode node : currentNetwork.getNodeList()) {
-			if (currentNetwork.getNeighborList(node, CyEdge.Type.ANY).isEmpty())
+		
+		// View is always associated with its model.
+		final CyNetwork network = currentNetworkView.getModel();
+		
+		for (CyNode node : network.getNodeList()) {
+			if (network.getNeighborList(node, CyEdge.Type.ANY).isEmpty())
 				currentNetworkView.getNodeView(node).setVisualProperty(
 						MinimalVisualLexicon.NODE_VISIBLE, false);
 		}
