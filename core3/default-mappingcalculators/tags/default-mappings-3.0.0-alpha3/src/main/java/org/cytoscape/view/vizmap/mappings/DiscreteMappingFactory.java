81934224d4c719ff5574b8e3607b9bc34607b422
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-05-14 21:37:50 +0000

    [maven-release-plugin]  copy for tag default-mappings-3.0.0-alpha3
    git-svn-id: http://chianti.ucsd.edu/svn@25159 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/default-mappingcalculators/tags/default-mappings-3.0.0-alpha3/src/main/java/org/cytoscape/view/vizmap/mappings/DiscreteMappingFactory.java
@@ -0,0 +1,26 @@
+package org.cytoscape.view.vizmap.mappings;
+
+import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.vizmap.VisualMappingFunction;
+import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
+
+public class DiscreteMappingFactory implements
+		VisualMappingFunctionFactory {
+
+	@Override
+	public <K, V> VisualMappingFunction<K, V> createVisualMappingFunction(final String attributeName,
+			Class<K> attrValueType, final VisualProperty<V> vp) {
+		
+		return new DiscreteMapping<K, V>(attributeName, attrValueType, vp);
+	}
+	
+	@Override public String toString() {
+		return DiscreteMapping.DISCRETE;
+	}
+
+	@Override
+	public Class<?> getMappingFunctionType() {
+		return DiscreteMapping.class;
+	}
+
+}
