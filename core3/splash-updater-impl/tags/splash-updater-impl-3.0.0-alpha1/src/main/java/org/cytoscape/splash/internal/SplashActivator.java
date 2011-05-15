df83ceeb147789e00b32d43d3d2855a19bb39574
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-05-15 00:29:03 +0000

    [maven-release-plugin]  copy for tag splash-updater-impl-3.0.0-alpha1
    git-svn-id: http://chianti.ucsd.edu/svn@25356 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/splash-updater-impl/tags/splash-updater-impl-3.0.0-alpha1/src/main/java/org/cytoscape/splash/internal/SplashActivator.java
@@ -0,0 +1,32 @@
+package org.cytoscape.splash.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkListener;
+import org.osgi.framework.BundleListener;
+import org.springframework.osgi.context.event.OsgiBundleApplicationContextListener;
+import java.util.Properties;
+
+/**
+ * Meant to be run at startLevel 1 so that the splash screen pops up before all
+ * other bundles start loading.  
+ */
+public final class SplashActivator implements BundleActivator {
+
+    /**
+     * Called whenever the OSGi framework starts our bundle
+     */
+    public void start( BundleContext bc ) throws Exception {
+		SplashManipulator splash = new SplashManipulator();
+		bc.addFrameworkListener(splash);
+		bc.addBundleListener(splash);
+		bc.registerService(OsgiBundleApplicationContextListener.class.getName(),splash,new Properties());
+    }
+
+    /**
+     * Called whenever the OSGi framework stops our bundle
+     */
+    public void stop( BundleContext bc ) throws Exception {
+    }
+}
+
