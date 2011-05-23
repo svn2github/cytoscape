f62a0c17cf800bedee51842c70db6f2ea3762a28
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-05-23 23:54:32 +0000

    Adding tests to check visual style functionality.  Still there are several bugs in apply() method.
    git-svn-id: http://chianti.ucsd.edu/svn@25513 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-api/trunk/src/test/java/org/cytoscape/view/vizmap/AbstractVisualStyleTest.java
+++ core3/vizmap-api/trunk/src/test/java/org/cytoscape/view/vizmap/AbstractVisualStyleTest.java
@@ -1,15 +1,33 @@
 package org.cytoscape.view.vizmap;
 
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 
+import java.awt.Color;
+import java.awt.Paint;
+import java.util.Properties;
+
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyTable;
+import org.cytoscape.property.CyProperty;
+import org.cytoscape.test.support.NetworkViewTestSupport;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.View;
 import org.cytoscape.view.model.VisualLexicon;
+import org.cytoscape.view.presentation.property.MinimalVisualLexicon;
+import org.cytoscape.view.vizmap.mappings.DiscreteMapping;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public abstract class AbstractVisualStyleTest {
-
+	
+	protected static final String attrName = "Sample attr 1";
 	
 	protected VisualStyle style;
 	
@@ -18,6 +36,53 @@ public abstract class AbstractVisualStyleTest {
 	
 	protected VisualLexicon lexicon;
 	
+	protected DiscreteMapping<String, Paint> colorMapping1;
+	protected DiscreteMapping<String, Paint> colorMapping2;
+	
+	
+	// Simple test network
+	protected CyNode node1;
+	protected CyNode node2;
+	protected CyNode node3;
+	protected CyEdge edge;
+	protected CyNetwork network;
+	protected CyNetworkView networkView;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		final Class<String> type = String.class;
+		colorMapping1 = new DiscreteMapping<String, Paint>(attrName, type,
+				MinimalVisualLexicon.NODE_PAINT);
+		colorMapping2 = new DiscreteMapping<String, Paint>(attrName, type,
+				MinimalVisualLexicon.NODE_FILL_COLOR);
+		
+		colorMapping1.putMapValue("red", Color.RED);
+		colorMapping1.putMapValue("green", Color.GREEN);
+		colorMapping1.putMapValue("blue", Color.BLUE);
+		
+		colorMapping2.putMapValue("red", Color.RED);
+		colorMapping2.putMapValue("green", Color.GREEN);
+		colorMapping2.putMapValue("blue", Color.BLUE);
+		
+		CyProperty<Properties> cyProperties = mock(CyProperty.class);
+		NetworkViewTestSupport nvts = new NetworkViewTestSupport(cyProperties);
+		network = nvts.getNetworkFactory().getInstance();
+		
+		node1 = network.addNode();
+		node2 = network.addNode();
+		node3 = network.addNode();
+		
+		edge = network.addEdge(node1, node2, true);
+		CyTable nodeTable = network.getDefaultNodeTable();
+		nodeTable.createColumn(attrName, String.class, true);
+		nodeTable.getRow(node1.getSUID()).set(attrName, "red");
+		nodeTable.getRow(node2.getSUID()).set(attrName, "green");
+		nodeTable.getRow(node3.getSUID()).set(attrName, "foo");
+		
+		networkView = nvts.getNetworkViewFactory().getNetworkView(network);
+	}
 
 	@After
 	public void tearDown() throws Exception {
@@ -29,10 +94,48 @@ public abstract class AbstractVisualStyleTest {
 		assertNotNull(style);
 		assertNotNull(originalTitle);
 		assertNotNull(newTitle);
+		assertNotNull(network);
+		assertNotNull(networkView);
+		assertNotNull(colorMapping1);
+		assertNotNull(networkView.getNodeView(node1));
+		assertNotNull(networkView.getNodeView(node2));
+		assertNotNull(networkView.getNodeView(node3));
 		
+		assertEquals("red", networkView.getModel().getDefaultNodeTable().getRow(node1.getSUID()).get(attrName, String.class));
+		assertEquals("green", networkView.getModel().getDefaultNodeTable().getRow(node2.getSUID()).get(attrName, String.class));
+		assertEquals("foo", networkView.getModel().getDefaultNodeTable().getRow(node3.getSUID()).get(attrName, String.class));
+
 		// Test title
 		assertEquals(originalTitle, style.getTitle());
 		style.setTitle(newTitle);
 		assertEquals(newTitle, style.getTitle());
+		
+		style.addVisualMappingFunction(colorMapping1);
+		style.addVisualMappingFunction(colorMapping2);
+		style.setDefaultValue(MinimalVisualLexicon.NODE_PAINT, Color.BLACK);
+		style.setDefaultValue(MinimalVisualLexicon.NODE_FILL_COLOR, Color.PINK);
+		
+		final Paint defaultNodeColor = MinimalVisualLexicon.NODE_FILL_COLOR.getDefault();
+		final View<CyNode> nodeView1 = networkView.getNodeView(node1);
+		final View<CyNode> nodeView2 = networkView.getNodeView(node2);
+		final View<CyNode> nodeView3 = networkView.getNodeView(node3);
+		assertEquals(defaultNodeColor, nodeView2.getVisualProperty(MinimalVisualLexicon.NODE_FILL_COLOR));
+		
+		assertEquals(2, style.getAllVisualMappingFunctions().size());
+		style.apply(networkView);
+		assertEquals(Color.PINK, style.getDefaultValue(MinimalVisualLexicon.NODE_FILL_COLOR));
+		
+		
+		Paint prop1 = nodeView1.getVisualProperty(MinimalVisualLexicon.NODE_FILL_COLOR);
+//		assertEquals(Color.RED, nodeView1.getVisualProperty(MinimalVisualLexicon.NODE_FILL_COLOR));
+//		
+//		assertEquals(Color.PINK, networkView.getNodeView(node3).getVisualProperty(MinimalVisualLexicon.NODE_FILL_COLOR));
+//		assertEquals(Color.GREEN, networkView.getNodeView(node2).getVisualProperty(MinimalVisualLexicon.NODE_FILL_COLOR));
+//		
+//		assertEquals(Color.RED, networkView.getNodeView(node1).getVisualProperty(MinimalVisualLexicon.NODE_PAINT));
+		
+		//assertEquals(Color.BLACK, networkView.getNodeView(node3));
+		
 	}
+	
 }
--- core3/vizmap-impl/trunk/impl/src/test/java/org/cytoscape/view/vizmap/VisualStyleTest.java
+++ core3/vizmap-impl/trunk/impl/src/test/java/org/cytoscape/view/vizmap/VisualStyleTest.java
@@ -1,7 +1,12 @@
 package org.cytoscape.view.vizmap;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
+import java.util.Collection;
+
+import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.presentation.property.MinimalVisualLexicon;
+import org.cytoscape.view.presentation.property.NullVisualProperty;
 import org.cytoscape.view.vizmap.internal.VisualLexiconManager;
 import org.cytoscape.view.vizmap.internal.VisualStyleFactoryImpl;
 import org.junit.After;
@@ -11,9 +16,19 @@ public class VisualStyleTest extends AbstractVisualStyleTest {
 
 	@Before
 	public void setUp() throws Exception {
-
+		super.setUp();
+		
 		// Create root node.
 		final VisualLexiconManager lexManager = mock(VisualLexiconManager.class);
+		
+		// Create root node.
+		final NullVisualProperty minimalRoot = new NullVisualProperty("MINIMAL_ROOT", "Minimal Root Visual Property");
+		final MinimalVisualLexicon minimalLex = new MinimalVisualLexicon(minimalRoot);		
+		final Collection<VisualProperty<?>> nodeVP = minimalLex.getAllDescendants(MinimalVisualLexicon.NODE);
+		final Collection<VisualProperty<?>> edgeVP = minimalLex.getAllDescendants(MinimalVisualLexicon.EDGE);
+		when(lexManager.getNodeVisualProperties()).thenReturn(nodeVP);
+		when(lexManager.getEdgeVisualProperties()).thenReturn(edgeVP);
+		
 		final VisualStyleFactoryImpl visualStyleFactory = new VisualStyleFactoryImpl(lexManager);
 		originalTitle = "Style 1";
 		newTitle = "Style 2";
