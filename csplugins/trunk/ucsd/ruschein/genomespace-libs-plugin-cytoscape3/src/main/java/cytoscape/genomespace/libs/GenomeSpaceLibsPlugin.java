0c09dc245b5765782c0a0ea5d758e7df6279ab97
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-06-07 20:24:12 +0000

    Removed dependencies on the Cytoscape 2.8 plug-in API.
    git-svn-id: http://chianti.ucsd.edu/svn@25664 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/ruschein/genomespace-libs-plugin-cytoscape3/src/main/java/cytoscape/genomespace/libs/GenomeSpaceLibsPlugin.java
+++ csplugins/trunk/ucsd/ruschein/genomespace-libs-plugin-cytoscape3/src/main/java/cytoscape/genomespace/libs/GenomeSpaceLibsPlugin.java
@@ -1,14 +1,12 @@
 package cytoscape.genomespace.libs;
 
-import cytoscape.plugin.CytoscapePlugin;
 
 
 /**
  * A nothing plugin that only exists to establish the libraries 
  * included in this plugin in the Cytoscape classloader. 
  */
-public class GenomeSpaceLibsPlugin extends CytoscapePlugin {
+public class GenomeSpaceLibsPlugin {
 	public GenomeSpaceLibsPlugin() {
-		super();
 	}
 }
