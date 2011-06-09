4b2c3e4376bf76dba95fa20dc2c0464b2e3c114b
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-06-09 01:54:48 +0000

    Code had been re-formated because it was broken.  NO FUNCTIONAL CHANGES.
    git-svn-id: http://chianti.ucsd.edu/svn@25686 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/ding-presentation-impl/trunk/src/main/java/org/cytoscape/ding/impl/FlagAndSelectionHandler.java
+++ core3/ding-presentation-impl/trunk/src/main/java/org/cytoscape/ding/impl/FlagAndSelectionHandler.java
@@ -62,134 +62,134 @@ import org.slf4j.LoggerFactory;
  */
 public class FlagAndSelectionHandler implements GraphViewChangeListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(FlagAndSelectionHandler.class);
+	private static final Logger logger = LoggerFactory.getLogger(FlagAndSelectionHandler.class);
 
-    private final GraphView view;
-    private final CyEventHelper eventHelper;
+	private final GraphView view;
+	private final CyEventHelper eventHelper;
 
-    /**
-     * Standard constructor takes the flag filter and the view that should be
-     * synchronized. On construction, this object will synchronize the filter
-     * and view by turning on flags or selections that are currently on in one
-     * of the two objects.
-     */
-    public FlagAndSelectionHandler(final GraphView view, final CyEventHelper eventHelper) {
-	this.view = view;
-	this.eventHelper = eventHelper;
+	/**
+	 * Standard constructor takes the flag filter and the view that should be
+	 * synchronized. On construction, this object will synchronize the filter
+	 * and view by turning on flags or selections that are currently on in one
+	 * of the two objects.
+	 */
+	public FlagAndSelectionHandler(final GraphView view, final CyEventHelper eventHelper) {
+		this.view = view;
+		this.eventHelper = eventHelper;
 
-	syncFilterAndView();
-	view.addGraphViewChangeListener(this);
-    }
-
-    private Set<CyNode> getSelectedNodes() {
-	final Set<CyNode> selectedNodes = new HashSet<CyNode>();
-
-	for (final CyNode n : view.getNetwork().getNodeList())
-	    if (n.getCyRow().get(CyNetwork.SELECTED, Boolean.class))
-		selectedNodes.add(n);
-
-	return selectedNodes;
-    }
-
-    private Set<CyEdge> getSelectedEdges() {
-	final Set<CyEdge> selectedEdges = new HashSet<CyEdge>();
-
-	for (final CyEdge n : view.getNetwork().getEdgeList())
-	    if (n.getCyRow().get(CyNetwork.SELECTED, Boolean.class))
-		selectedEdges.add(n);
-
-	return selectedEdges;
-    }
-
-    /**
-     * Synchronizes the filter and view of this object by selecting every object
-     * that is currently flagged and vice versa.
-     */
-    private void syncFilterAndView() {
-	final Set<CyNode> flaggedNodes = getSelectedNodes();
-	final Set<CyEdge> flaggedEdges = getSelectedEdges();
-
-	final List<CyNode> selectedNodes = view.getSelectedNodes();
-	final List<CyEdge> selectedEdges = view.getSelectedEdges();
-
-	// select all nodes that are flagged but not currently selected
-	for (final CyNode node : flaggedNodes) {
-	    final NodeView nv = view.getNodeView(node);
-
-	    if ((nv == null) || nv.isSelected())
-		continue;
-
-	    nv.setSelected(true);
+		syncFilterAndView();
+		view.addGraphViewChangeListener(this);
 	}
 
-	// select all edges that are flagged but not currently selected
-	for (final CyEdge edge : flaggedEdges) {
-	    final EdgeView ev = view.getEdgeView(edge);
+	private Set<CyNode> getSelectedNodes() {
+		final Set<CyNode> selectedNodes = new HashSet<CyNode>();
 
-	    if ((ev == null) || ev.isSelected())
-		continue;
+		for (final CyNode n : view.getNetwork().getNodeList())
+			if (n.getCyRow().get(CyNetwork.SELECTED, Boolean.class))
+				selectedNodes.add(n);
 
-	    ev.setSelected(true);
+		return selectedNodes;
 	}
 
-	// flag all nodes that are selected but not currently flagged
-	select(selectedNodes, true);
+	private Set<CyEdge> getSelectedEdges() {
+		final Set<CyEdge> selectedEdges = new HashSet<CyEdge>();
 
-	// flag all edges that are selected but not currently flagged
-	select(selectedEdges, true);
-    }
+		for (final CyEdge n : view.getNetwork().getEdgeList())
+			if (n.getCyRow().get(CyNetwork.SELECTED, Boolean.class))
+				selectedEdges.add(n);
 
-    private void select(final Collection<? extends CyTableEntry> nodesOrEdges, final boolean selected) {
-	if (nodesOrEdges.isEmpty())
-	    return;
-
-	final CyTable table = nodesOrEdges.iterator().next().getCyRow().getTable();
-	try {
-	    eventHelper.fireSynchronousEvent(new RowsAboutToChangeEvent(this, table));
-
-	    for (final CyTableEntry nodeOrEdge : nodesOrEdges)
-		nodeOrEdge.getCyRow().set(CyNetwork.SELECTED, selected);
-	} finally {
-	    eventHelper.fireSynchronousEvent(new RowsFinishedChangingEvent(this, table));
-	}
-    }
-
-    /**
-     * Responds to selection events from the view by setting the matching
-     * flagged state in the SelectFilter object.
-     */
-    public void graphViewChanged(final GraphViewChangeEvent event) {
-
-	// GINY bug: the event we get frequently has the correct indices
-	// but incorrect Node and Edge objects. For now we get around this
-	// by converting indices to graph objects ourselves
-
-	final long start = System.currentTimeMillis();
-
-	if (event.isNodesSelectedType()) {
-	    final CyNode[] selectedNodes = event.getSelectedNodes();
-	    select(Arrays.asList(selectedNodes), true);
-	} else if (event.isNodesUnselectedType() || event.isNodesHiddenType()) {
-	    final CyNode[] objIndecies;
-	    if (event.isNodesUnselectedType())
-		objIndecies = event.getUnselectedNodes();
-	    else
-		objIndecies = event.getHiddenNodes();
-
-	    select(Arrays.asList(objIndecies), false);
-	} else if (event.isEdgesSelectedType()) {
-	    final CyEdge[] objIndecies = event.getSelectedEdges();
-	    select(Arrays.asList(objIndecies), true);
-	} else if (event.isEdgesUnselectedType() || event.isEdgesHiddenType()) {
-	    final CyEdge[] objIndecies;
-	    if (event.isEdgesUnselectedType())
-		objIndecies = event.getUnselectedEdges();
-	    else
-		objIndecies = event.getHiddenEdges();
-
-	    select(Arrays.asList(objIndecies), false);
+		return selectedEdges;
 	}
 
-	logger.debug("Finished selection: Time = " + (System.currentTimeMillis() - start) + " msec.");
-    }
+	/**
+	 * Synchronizes the filter and view of this object by selecting every object
+	 * that is currently flagged and vice versa.
+	 */
+	private void syncFilterAndView() {
+		final Set<CyNode> flaggedNodes = getSelectedNodes();
+		final Set<CyEdge> flaggedEdges = getSelectedEdges();
+
+		final List<CyNode> selectedNodes = view.getSelectedNodes();
+		final List<CyEdge> selectedEdges = view.getSelectedEdges();
+
+		// select all nodes that are flagged but not currently selected
+		for (final CyNode node : flaggedNodes) {
+			final NodeView nv = view.getNodeView(node);
+
+			if ((nv == null) || nv.isSelected())
+				continue;
+
+			nv.setSelected(true);
+		}
+
+		// select all edges that are flagged but not currently selected
+		for (final CyEdge edge : flaggedEdges) {
+			final EdgeView ev = view.getEdgeView(edge);
+
+			if ((ev == null) || ev.isSelected())
+				continue;
+
+			ev.setSelected(true);
+		}
+
+		// flag all nodes that are selected but not currently flagged
+		select(selectedNodes, true);
+
+		// flag all edges that are selected but not currently flagged
+		select(selectedEdges, true);
+	}
+
+	private void select(final Collection<? extends CyTableEntry> nodesOrEdges, final boolean selected) {
+		if (nodesOrEdges.isEmpty())
+			return;
+
+		final CyTable table = nodesOrEdges.iterator().next().getCyRow().getTable();
+		try {
+			eventHelper.fireSynchronousEvent(new RowsAboutToChangeEvent(this, table));
+
+			for (final CyTableEntry nodeOrEdge : nodesOrEdges)
+				nodeOrEdge.getCyRow().set(CyNetwork.SELECTED, selected);
+		} finally {
+			eventHelper.fireSynchronousEvent(new RowsFinishedChangingEvent(this, table));
+		}
+	}
+
+	/**
+	 * Responds to selection events from the view by setting the matching
+	 * flagged state in the SelectFilter object.
+	 */
+	public void graphViewChanged(final GraphViewChangeEvent event) {
+
+		// GINY bug: the event we get frequently has the correct indices
+		// but incorrect Node and Edge objects. For now we get around this
+		// by converting indices to graph objects ourselves
+
+		final long start = System.currentTimeMillis();
+
+		if (event.isNodesSelectedType()) {
+			final CyNode[] selectedNodes = event.getSelectedNodes();
+			select(Arrays.asList(selectedNodes), true);
+		} else if (event.isNodesUnselectedType() || event.isNodesHiddenType()) {
+			final CyNode[] objIndecies;
+			if (event.isNodesUnselectedType())
+				objIndecies = event.getUnselectedNodes();
+			else
+				objIndecies = event.getHiddenNodes();
+
+			select(Arrays.asList(objIndecies), false);
+		} else if (event.isEdgesSelectedType()) {
+			final CyEdge[] objIndecies = event.getSelectedEdges();
+			select(Arrays.asList(objIndecies), true);
+		} else if (event.isEdgesUnselectedType() || event.isEdgesHiddenType()) {
+			final CyEdge[] objIndecies;
+			if (event.isEdgesUnselectedType())
+				objIndecies = event.getUnselectedEdges();
+			else
+				objIndecies = event.getHiddenEdges();
+
+			select(Arrays.asList(objIndecies), false);
+		}
+
+		logger.debug("Finished selection: Time = " + (System.currentTimeMillis() - start) + " msec.");
+	}
 }
