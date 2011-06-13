8bb530ee949323727c7d3b3cbbbcb267384cf1eb
Christian Lopes <Christian Lopes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-06-13 18:58:25 +0000

    Created vizmap export/import  tasks.
    git-svn-id: http://chianti.ucsd.edu/svn@25732 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/exportvizmap/ExportVizmapTaskFactory.java
@@ -0,0 +1,23 @@
+package org.cytoscape.task.internal.exportvizmap;
+
+import org.cytoscape.io.write.VizmapWriterManager;
+import org.cytoscape.task.internal.io.VizmapWriter;
+import org.cytoscape.view.vizmap.VisualMappingManager;
+import org.cytoscape.work.TaskFactory;
+import org.cytoscape.work.TaskIterator;
+
+public class ExportVizmapTaskFactory implements TaskFactory {
+
+	private final VizmapWriterManager writerManager;
+	private final VisualMappingManager vmMgr;
+
+	public ExportVizmapTaskFactory(VizmapWriterManager writerManager, VisualMappingManager vmMgr) {
+		this.writerManager = writerManager;
+		this.vmMgr = vmMgr;
+	}
+	
+	@Override
+	public TaskIterator getTaskIterator() {
+		return new TaskIterator(new VizmapWriter(writerManager, vmMgr));
+	}
+}
---
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/io/VizmapWriter.java
@@ -0,0 +1,28 @@
+package org.cytoscape.task.internal.io;
+
+import java.io.File;
+import java.util.Set;
+
+import org.cytoscape.io.CyFileFilter;
+import org.cytoscape.io.write.CyWriter;
+import org.cytoscape.io.write.VizmapWriterManager;
+import org.cytoscape.view.vizmap.VisualMappingManager;
+import org.cytoscape.view.vizmap.VisualStyle;
+
+public class VizmapWriter extends TunableAbstractCyWriter<VizmapWriterManager> {
+
+	private final VisualMappingManager vmMgr;
+
+	public VizmapWriter(VizmapWriterManager writerManager, VisualMappingManager vmMgr) {
+		super(writerManager);
+		if (vmMgr == null) throw new NullPointerException("VisualMappingManager is null");
+		this.vmMgr = vmMgr;
+	}
+
+	@Override
+	protected CyWriter getWriter(CyFileFilter filter, File file) throws Exception {
+		Set<VisualStyle> styles = vmMgr.getAllVisualStyles();
+
+		return writerManager.getWriter(styles, filter, file);
+	}
+}
