a87bf6aa57b5f3c41c8dc6e7c9223503d14d0bed
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-06-23 00:10:38 +0000

    setting the parent for the quickstart dialog
    git-svn-id: http://chianti.ucsd.edu/svn@25885 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/swing-application-impl/trunk/src/main/java/org/cytoscape/internal/QuickStartStartup.java
+++ core3/swing-application-impl/trunk/src/main/java/org/cytoscape/internal/QuickStartStartup.java
@@ -2,6 +2,7 @@ package org.cytoscape.internal;
 
 import org.cytoscape.application.swing.events.CytoscapeStartEvent;
 import org.cytoscape.application.swing.events.CytoscapeStartListener;
+import org.cytoscape.application.swing.CySwingApplication;
 import org.cytoscape.work.TaskFactory;
 import org.cytoscape.work.swing.GUITaskManager;
 
@@ -12,13 +13,16 @@ public class QuickStartStartup implements CytoscapeStartListener {
 
 	private TaskFactory quickStartTaskFactory;
 	private GUITaskManager guiTaskManager;
+	private CySwingApplication swingApp;
 	
-	public QuickStartStartup(TaskFactory quickStartTaskFactory, GUITaskManager guiTaskManager){	
+	public QuickStartStartup(TaskFactory quickStartTaskFactory, GUITaskManager guiTaskManager, CySwingApplication swingApp){	
 		this.quickStartTaskFactory = quickStartTaskFactory;
 		this.guiTaskManager = guiTaskManager;
+		this.swingApp = swingApp;
 	}
 	
 	public void handleEvent(CytoscapeStartEvent e){
-		this.guiTaskManager.execute(this.quickStartTaskFactory);
+		guiTaskManager.setParent(swingApp.getJFrame());
+		guiTaskManager.execute(quickStartTaskFactory);
 	}	
 }
