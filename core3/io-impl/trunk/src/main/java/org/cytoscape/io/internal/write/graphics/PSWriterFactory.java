6e1cbb8e19bc4831d7d3bd1284542c0f6ddf10e5
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-06-24 18:13:44 +0000

    PS writer fixed.  Missing freehep artifact had been added to the wrapper repository.
    git-svn-id: http://chianti.ucsd.edu/svn@25917 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/io-impl/trunk/src/main/java/org/cytoscape/io/internal/write/graphics/PSWriterFactory.java
+++ core3/io-impl/trunk/src/main/java/org/cytoscape/io/internal/write/graphics/PSWriterFactory.java
@@ -2,15 +2,13 @@ package org.cytoscape.io.internal.write.graphics;
 
 import org.cytoscape.io.CyFileFilter;
 import org.cytoscape.io.write.CyWriter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class PSWriterFactory extends AbstractPresentationWriterFactory {
-
+	
 	public PSWriterFactory(final CyFileFilter fileFilter) {
 		super(fileFilter);
-	}
+ 	}
 
 	@Override
 	public CyWriter getWriterTask() {
