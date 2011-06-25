ebc3c36dcede39a70959ef59b86b430f6aa5c470
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-06-25 00:25:33 +0000

    Added back ability to calculate the amount of physical memory and use that for java startup options
    git-svn-id: http://chianti.ucsd.edu/svn@25937 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- cytoscape/trunk/distribution/src/main/bin/cytoscape.sh
+++ cytoscape/trunk/distribution/src/main/bin/cytoscape.sh
@@ -9,9 +9,13 @@ if [ -h $script_path ]; then
 	script_path="$(readlink $script_path)"
 fi
 
-
 #vm_options_path=$HOME/.cytoscape
 vm_options_path=$script_path
+
+# Attempt to generate Cytoscape.vmoptions if it doesn't exist!
+if [ ! -e "$vm_options_path/Cytoscape.vmoptions"  -a  -x "$script_path/gen_vmoptions.sh" ]; then
+    "$script_path/gen_vmoptions.sh"
+fi
 
 if [ -r $vm_options_path/Cytoscape.vmoptions ]; then
     java `cat "$vm_options_path/Cytoscape.vmoptions"` -jar "$script_path/cytoscape.jar" -p "$script_path/plugins" "$@"
