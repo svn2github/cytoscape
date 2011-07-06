b930c2ffbe5e2e306837a522356f436d8131c125
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-07-06 16:19:30 +0000

    now properly exporting services
    git-svn-id: http://chianti.ucsd.edu/svn@26060 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/vizmap-impl/trunk/it/src/test/java/org/cytoscape/view/vizmap/ServiceConfigurationTest.java
+++ core3/vizmap-impl/trunk/it/src/test/java/org/cytoscape/view/vizmap/ServiceConfigurationTest.java
@@ -17,23 +17,12 @@ public class ServiceConfigurationTest extends ServiceTestSupport {
 	public void setup() {
 		registerMockService(CyEventHelper.class);
 		registerMockService(RenderingEngineManager.class);
-
-		Properties p1 = new Properties();
-		p1.setProperty("mapping.type","discrete");
-		registerMockService(VisualMappingFunctionFactory.class, p1);
-
-		Properties p2 = new Properties();
-		p2.setProperty("mapping.type","continuous");
-		registerMockService(VisualMappingFunctionFactory.class, p2);
-
-		Properties p3 = new Properties();
-		p3.setProperty("mapping.type","passthrough"); 
-		registerMockService(VisualMappingFunctionFactory.class, p3);
 	}
 
 	@Test
 	public void testExpectedServices() {
 		checkService(VisualMappingManager.class);
 		checkService(VisualStyleFactory.class);
+		checkService(VisualMappingFunctionFactory.class);
 	}
 }
