961783edb2d8dfaae8fa683ba548300da4fd33a5
komal <komal@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-07-12 15:36:00 +0000

    git-svn-id: http://chianti.ucsd.edu/svn@26147 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/komal/ExportToCWPlugin/src/main/java/org/cytoscape/komal/BuildHtml.java
@@ -0,0 +1,36 @@
+package main.java.org.cytoscape.komal;
+
+import java.util.*;
+
+import cytoscape.logger.CyLogger;
+
+public class BuildHtml extends ExportToCWPlugin {
+
+    private Formatter x;
+    String nameofwebpage = HtmlPath;
+
+    public void openFile() {
+        try {
+            x = new Formatter(nameofwebpage);
+             CyLogger.getLogger().info("HTML " + HtmlPath + " CREATED");
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        }
+    }
+
+    public void addRecords(String a) {
+        x.format("%s", a);
+        CyLogger.getLogger().info("RECORDS ADDED TO "+HtmlPath );
+    }
+
+    public String completeurl() {
+        CyLogger.getLogger().info("HTML NAME AFTER ADD RECORD: " + nameofwebpage);
+        return nameofwebpage;
+
+    }
+
+    public void closeFile() {
+
+        x.close();
+    }
+}
