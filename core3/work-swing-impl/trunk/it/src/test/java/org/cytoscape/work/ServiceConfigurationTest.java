93c1aaeda7a6a682803762670d0837211ddb99b0
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-07-12 17:52:26 +0000

    Fixed some integration test failures.
    git-svn-id: http://chianti.ucsd.edu/svn@26150 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/work-swing-impl/trunk/it/src/test/java/org/cytoscape/work/ServiceConfigurationTest.java
+++ core3/work-swing-impl/trunk/it/src/test/java/org/cytoscape/work/ServiceConfigurationTest.java
@@ -1,4 +1,4 @@
-package org.cytoscape.work; 
+package org.cytoscape.work;
 
 
 import java.util.Properties;
@@ -6,6 +6,7 @@ import java.util.Properties;
 import org.cytoscape.integration.ServiceTestSupport;
 import org.cytoscape.property.BasicCyProperty;
 import org.cytoscape.property.CyProperty;
+import org.cytoscape.property.bookmark.Bookmarks;
 import org.cytoscape.property.bookmark.BookmarksUtil;
 import org.cytoscape.work.swing.GUITaskManager;
 import org.cytoscape.work.swing.GUITunableHandlerFactory;
@@ -25,16 +26,21 @@ public class ServiceConfigurationTest extends ServiceTestSupport {
 	public void setup() {
 		Properties coreP = new Properties();
 		coreP.setProperty("cyPropertyName","coreSettings");
-		
+
 		Properties p = new Properties();
 		p.setProperty("cyPropertyName","bookmarks");
 
-		CyProperty<Properties> cyProp = new BasicCyProperty(new Properties(), CyProperty.SavePolicy.CONFIG_DIR);
+		CyProperty<Properties> cyProp =
+			new BasicCyProperty(new Properties(), CyProperty.SavePolicy.CONFIG_DIR);
 		registerMockService(CyProperty.class, cyProp, coreP);
-		registerMockService(CyProperty.class, cyProp, p);
+
+		BookmarksCyProperty cyBookProp =
+			new BookmarksCyProperty(new Bookmarks(), CyProperty.SavePolicy.CONFIG_DIR);
+		registerMockService(CyProperty.class, cyBookProp, p);
+
 		registerMockService(BookmarksUtil.class);
 	}
-	
+
 	@Test
 	public void testExpectedServices() {
 		checkService(GUITaskManager.class);
@@ -44,4 +50,34 @@ public class ServiceConfigurationTest extends ServiceTestSupport {
 		checkService(GUITunableInterceptor.class);
 		checkService(UndoSupport.class);
 	}
+}
+
+
+/**
+ * A simple implementation of CyProperty&lt;Properties&gt; suitable for
+ * general purpose use.
+ */
+final class BookmarksCyProperty implements CyProperty<Bookmarks> {
+        private final Bookmarks bookmarks;
+        private final CyProperty.SavePolicy savePolicy;
+
+        public BookmarksCyProperty(final Bookmarks bookmarks, final CyProperty.SavePolicy savePolicy)
+	{
+                if (bookmarks == null)
+			throw new NullPointerException("\"bookmarks\" parameter is null!");
+                if (savePolicy == null)
+                        throw new NullPointerException("\"savePolicy\" parameter is null!");
+
+                this.bookmarks = bookmarks;
+                this.savePolicy = savePolicy;
+        }
+
+        public Bookmarks getProperties() {
+                return bookmarks;
+
+        }
+
+        public CyProperty.SavePolicy getSavePolicy() {
+                return savePolicy;
+        }
 }
