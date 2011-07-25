6ece78af8a550bb4091fde0adc342effa2bc5292
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-07-25 18:18:13 +0000

    Source code had been re-formatted because it was broken.  No functional changes.
    git-svn-id: http://chianti.ucsd.edu/svn@26261 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/subnetworkbuilder/SubnetworkBuilderUtil.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/quickstart/subnetworkbuilder/SubnetworkBuilderUtil.java
@@ -18,7 +18,6 @@ import org.cytoscape.task.internal.creation.NewNetworkSelectedNodesOnlyTask;
 import org.cytoscape.task.internal.quickstart.ImportNetworkFromPublicDataSetTask;
 import org.cytoscape.task.internal.quickstart.datasource.InteractionFilePreprocessor;
 import org.cytoscape.view.layout.CyLayoutAlgorithmManager;
-import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.model.CyNetworkViewFactory;
 import org.cytoscape.view.model.CyNetworkViewManager;
 import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
@@ -28,93 +27,92 @@ import org.cytoscape.work.Task;
 
 public class SubnetworkBuilderUtil {
 
-    private CyNetworkReaderManager mgr;
-    private CyNetworkManager netmgr;
-    protected final CyNetworkViewManager networkViewManager;
-    private Properties props;
-    private StreamUtil streamUtil;
+	private CyNetworkReaderManager mgr;
+	private CyNetworkManager netmgr;
+	protected final CyNetworkViewManager networkViewManager;
+	private Properties props;
+	private StreamUtil streamUtil;
 
-    protected final CyEventHelper eventHelper;
-    protected final CyApplicationManager appManager;
+	protected final CyEventHelper eventHelper;
+	protected final CyApplicationManager appManager;
 
-    private CyNetworkNaming cyNetworkNaming;
+	private CyNetworkNaming cyNetworkNaming;
 
-    private final CyRootNetworkFactory crnf;
-    private final CyNetworkViewFactory cnvf;
+	private final CyRootNetworkFactory crnf;
+	private final CyNetworkViewFactory cnvf;
 
-    private final Set<InteractionFilePreprocessor> processors;
+	private final Set<InteractionFilePreprocessor> processors;
 
-    VisualStyleBuilder vsBuilder;
-    final VisualMappingManager vmm;
+	VisualStyleBuilder vsBuilder;
+	final VisualMappingManager vmm;
 
-    // For mapping generator
-    final VisualStyleFactory vsFactory;
-    private VisualMappingFunctionFactory discFactory;
-    private VisualMappingFunctionFactory ptFactory;
-    private final CyLayoutAlgorithmManager layouts;
+	// For mapping generator
+	final VisualStyleFactory vsFactory;
+	private VisualMappingFunctionFactory discFactory;
+	private VisualMappingFunctionFactory ptFactory;
+	private final CyLayoutAlgorithmManager layouts;
 
-    public SubnetworkBuilderUtil(CyNetworkReaderManager mgr, CyNetworkManager netmgr,
-	    final CyNetworkViewManager networkViewManager, CyProperty<Properties> cyProps,
-	    CyNetworkNaming cyNetworkNaming, StreamUtil streamUtil, final CyEventHelper eventHelper,
-	    final CyApplicationManager appManager, CyRootNetworkFactory crnf, CyNetworkViewFactory cnvf,
-	    VisualMappingManager vmm, final VisualStyleFactory vsFactory, final CyLayoutAlgorithmManager layouts) {
+	public SubnetworkBuilderUtil(CyNetworkReaderManager mgr, CyNetworkManager netmgr,
+			final CyNetworkViewManager networkViewManager, CyProperty<Properties> cyProps,
+			CyNetworkNaming cyNetworkNaming, StreamUtil streamUtil, final CyEventHelper eventHelper,
+			final CyApplicationManager appManager, CyRootNetworkFactory crnf, CyNetworkViewFactory cnvf,
+			VisualMappingManager vmm, final VisualStyleFactory vsFactory, final CyLayoutAlgorithmManager layouts) {
 
-	this.mgr = mgr;
-	this.netmgr = netmgr;
-	this.networkViewManager = networkViewManager;
-	this.crnf = crnf;
-	this.cnvf = cnvf;
-	this.vmm = vmm;
-	this.props = cyProps.getProperties();
-	this.cyNetworkNaming = cyNetworkNaming;
-	this.streamUtil = streamUtil;
-	this.processors = new HashSet<InteractionFilePreprocessor>();
-	this.eventHelper = eventHelper;
-	this.appManager = appManager;
-	this.vsFactory = vsFactory;
-	this.layouts = layouts;
-    }
-
-    public void addProcessor(InteractionFilePreprocessor processor, Map props) {
-	if (processor != null)
-	    this.processors.add(processor);
-    }
-
-    public void removeProcessor(InteractionFilePreprocessor processor, Map props) {
-	if (processor != null)
-	    this.processors.remove(processor);
-
-    }
-
-    public Task getWebServiceImportTask() {
-	return new ImportNetworkFromPublicDataSetTask(processors, mgr, netmgr, networkViewManager, props,
-		cyNetworkNaming, streamUtil);
-    }
-
-    Task getNewNetworkSelectedNodesOnlyTask(final CyNetwork network) {
-	return new NewNetworkSelectedNodesOnlyTask(network, crnf, cnvf, netmgr, networkViewManager, cyNetworkNaming,
-		vmm, appManager);
-    }
-    
-    Task getApplLayoutTask() {
-	return new ApplyLayoutTask(this, layouts);
-    }
-
-    public void addFactory(VisualMappingFunctionFactory factory, Map props) {
-
-
-	if (factory.toString().startsWith("Discrete Mapping"))
-	    discFactory = factory;
-	else if (factory.toString().startsWith("Passthrough Mapping"))
-	    ptFactory = factory;
-
-	if (discFactory != null && ptFactory != null) {
-	    this.vsBuilder = new VisualStyleBuilder(vsFactory, discFactory, ptFactory);
+		this.mgr = mgr;
+		this.netmgr = netmgr;
+		this.networkViewManager = networkViewManager;
+		this.crnf = crnf;
+		this.cnvf = cnvf;
+		this.vmm = vmm;
+		this.props = cyProps.getProperties();
+		this.cyNetworkNaming = cyNetworkNaming;
+		this.streamUtil = streamUtil;
+		this.processors = new HashSet<InteractionFilePreprocessor>();
+		this.eventHelper = eventHelper;
+		this.appManager = appManager;
+		this.vsFactory = vsFactory;
+		this.layouts = layouts;
 	}
-    }
 
-    void removeFactory(VisualMappingFunctionFactory factory, Map props) {
+	public void addProcessor(InteractionFilePreprocessor processor, Map props) {
+		if (processor != null)
+			this.processors.add(processor);
+	}
 
-    }
+	public void removeProcessor(InteractionFilePreprocessor processor, Map props) {
+		if (processor != null)
+			this.processors.remove(processor);
+
+	}
+
+	public Task getWebServiceImportTask() {
+		return new ImportNetworkFromPublicDataSetTask(processors, mgr, netmgr, networkViewManager, props,
+				cyNetworkNaming, streamUtil);
+	}
+
+	Task getNewNetworkSelectedNodesOnlyTask(final CyNetwork network) {
+		return new NewNetworkSelectedNodesOnlyTask(network, crnf, cnvf, netmgr, networkViewManager, cyNetworkNaming,
+				vmm, appManager);
+	}
+
+	Task getApplLayoutTask() {
+		return new ApplyLayoutTask(this, layouts);
+	}
+
+	public void addFactory(VisualMappingFunctionFactory factory, Map props) {
+
+		if (factory.toString().startsWith("Discrete Mapping"))
+			discFactory = factory;
+		else if (factory.toString().startsWith("Passthrough Mapping"))
+			ptFactory = factory;
+
+		if (discFactory != null && ptFactory != null) {
+			this.vsBuilder = new VisualStyleBuilder(vsFactory, discFactory, ptFactory);
+		}
+	}
+
+	void removeFactory(VisualMappingFunctionFactory factory, Map props) {
+
+	}
 
 }
