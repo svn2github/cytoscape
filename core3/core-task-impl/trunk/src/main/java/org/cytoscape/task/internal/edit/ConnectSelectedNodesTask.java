ed2a96d4c08018bf7e2f906ab7d4f9e538e26111
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-08-02 19:18:48 +0000

    Added undo/redo support for the connecting of selected nodes.
    git-svn-id: http://chianti.ucsd.edu/svn@26354 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/edit/ConnectSelectedNodesTask.java
+++ core3/core-task-impl/trunk/src/main/java/org/cytoscape/task/internal/edit/ConnectSelectedNodesTask.java
@@ -1,5 +1,7 @@
 package org.cytoscape.task.internal.edit;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.cytoscape.event.CyEventHelper;
@@ -12,16 +14,20 @@ import org.cytoscape.model.CyTableEntry;
 import org.cytoscape.model.CyTableUtil;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
+import org.cytoscape.work.undo.UndoSupport;
+
 
 public class ConnectSelectedNodesTask extends AbstractTask {
-
 	// TODO: is it sufficient to create undirected edge only?
-	private static final String INTERACTION = "undirected";
-
+	static final String INTERACTION = "undirected";
+	private final UndoSupport undoSupport;
 	private final CyNetwork network;
 	private final CyEventHelper eventHelper;
-	
-	public ConnectSelectedNodesTask(final CyNetwork network, final CyEventHelper eventHelper) {
+
+	public ConnectSelectedNodesTask(final UndoSupport undoSupport, final CyNetwork network,
+	                                final CyEventHelper eventHelper)
+	{
+		this.undoSupport = undoSupport;
 		if (network == null)
 			throw new NullPointerException("Network is null.");
 		this.network = network;
@@ -30,27 +36,30 @@ public class ConnectSelectedNodesTask extends AbstractTask {
 
 	@Override
 	public void run(TaskMonitor taskMonitor) throws Exception {
-
 		final CyTable nodeTable = network.getDefaultNodeTable();
 		final CyTable edgeTable = network.getDefaultEdgeTable();
-			final List<CyNode> selectedNodes = CyTableUtil.getNodesInState(network, CyNetwork.SELECTED, true);
-			for (CyNode source : selectedNodes) {
-				for (CyNode target : selectedNodes) {
-					if (source != target) {
-						final List<CyNode> sourceNeighborList = network.getNeighborList(source, Type.ANY);
-						if (sourceNeighborList.contains(target) == false) {
-							// connect it
-							final CyEdge newEdge = network.addEdge(source, target, false);
-							newEdge.getCyRow().set(
-									CyTableEntry.NAME,
-									source.getCyRow().get(CyTableEntry.NAME, String.class) + " (" + INTERACTION + ") "
-											+ target.getCyRow().get(CyTableEntry.NAME, String.class));
-							newEdge.getCyRow().set(CyEdge.INTERACTION, INTERACTION);
+		final List<CyNode> selectedNodes = CyTableUtil.getNodesInState(network, CyNetwork.SELECTED, true);
 
-						}
+		final List<CyEdge> newEdges = new ArrayList<CyEdge>();
+		for (final CyNode source : selectedNodes) {
+			for (final CyNode target : selectedNodes) {
+				if (source != target) {
+					final List<CyNode> sourceNeighborList = network.getNeighborList(source, Type.ANY);
+					if (!sourceNeighborList.contains(target)) {
+						// connect it
+						final CyEdge newEdge = network.addEdge(source, target, false);
+						newEdges.add(newEdge);
+						newEdge.getCyRow().set(
+							CyTableEntry.NAME,
+							source.getCyRow().get(CyTableEntry.NAME, String.class) + " (" + INTERACTION + ") "
+							+ target.getCyRow().get(CyTableEntry.NAME, String.class));
+						newEdge.getCyRow().set(CyEdge.INTERACTION, INTERACTION);
 					}
 				}
 			}
-	}
+		}
 
+		undoSupport.getUndoableEditSupport().postEdit(
+			new ConnectSelectedNodesEdit(network, newEdges));
+	}
 }
