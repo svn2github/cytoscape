9eb3e46aebe1020f2ea3f34ebf89fb0f71208eeb
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-08-12 19:33:02 +0000

    added combo box
    git-svn-id: http://chianti.ucsd.edu/svn@26543 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/mes/LAFDebug-spring/src/main/java/millhouseyyy/internal/LAFDebug.java
+++ csplugins/trunk/ucsd/mes/LAFDebug-spring/src/main/java/millhouseyyy/internal/LAFDebug.java
@@ -7,10 +7,17 @@ public class LAFDebug {
 
 	public LAFDebug() {
 		JFrame frame = new JFrame();
+
 		JPanel panel = new JPanel();
 		panel.setPreferredSize(new Dimension(100,100));
+
 		JButton button = new JButton("Using Spring-DM");
 		panel.add(button);
+
+		Object[] names = new Object[]{"homer","marge","lisa"};
+		JComboBox combo = new JComboBox(names);
+		panel.add(combo);
+
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
