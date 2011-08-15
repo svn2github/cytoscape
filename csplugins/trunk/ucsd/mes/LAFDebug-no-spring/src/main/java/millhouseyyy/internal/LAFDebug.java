a12ae81a8e42ecf46b4b61bfcf50c7cb2989eda1
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-08-15 17:34:02 +0000

    added jcombo box to produce problem
    git-svn-id: http://chianti.ucsd.edu/svn@26556 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/mes/LAFDebug-no-spring/src/main/java/millhouseyyy/internal/LAFDebug.java
+++ csplugins/trunk/ucsd/mes/LAFDebug-no-spring/src/main/java/millhouseyyy/internal/LAFDebug.java
@@ -7,12 +7,19 @@ public class LAFDebug {
 
 	public LAFDebug() {
 		JFrame frame = new JFrame();
+
 		JPanel panel = new JPanel();
 		panel.setPreferredSize(new Dimension(100,100));
+
 		JButton button = new JButton("NO spring");
 		panel.add(button);
+
+		Object[] names = new Object[]{"homer","marge","lisa"};
+		JComboBox combo = new JComboBox(names);
+		panel.add(combo);
+
 		frame.add(panel);
 		frame.pack();
-//		frame.setVisible(true);
+		frame.setVisible(true);
 	}
 }
