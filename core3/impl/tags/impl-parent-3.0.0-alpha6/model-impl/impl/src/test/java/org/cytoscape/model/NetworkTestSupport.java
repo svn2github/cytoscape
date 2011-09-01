f2ab71638ee6e5f8fc1f9f276795b55071c0a973
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-09-01 20:25:02 +0000

    [maven-release-plugin]  copy for tag impl-parent-3.0.0-alpha6
    git-svn-id: http://chianti.ucsd.edu/svn@26689 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/impl/tags/impl-parent-3.0.0-alpha6/model-impl/impl/src/test/java/org/cytoscape/model/NetworkTestSupport.java
@@ -0,0 +1,45 @@
+package org.cytoscape.model;
+
+
+import org.cytoscape.equations.Interpreter;
+import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.event.DummyCyEventHelper;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyTableFactory;
+
+import org.cytoscape.model.internal.CyNetworkFactoryImpl;
+import org.cytoscape.model.internal.CyTableFactoryImpl;
+import org.cytoscape.model.internal.CyTableManagerImpl;
+import org.cytoscape.service.util.CyServiceRegistrar;
+
+import static org.mockito.Mockito.*;
+
+
+public class NetworkTestSupport {
+
+	protected CyNetworkFactory networkFactory;
+	protected CyEventHelper eventHelper;
+	protected CyTableManagerImpl tableMgr;
+
+	public NetworkTestSupport() {
+		eventHelper = new DummyCyEventHelper();
+		tableMgr = mock(CyTableManagerImpl.class); 
+		final CyServiceRegistrar serviceRegistrar = mock(CyServiceRegistrar.class);
+		final CyTableFactoryImpl tableFactory =
+			new CyTableFactoryImpl(eventHelper, mock(Interpreter.class), serviceRegistrar);
+		networkFactory =
+			new CyNetworkFactoryImpl(eventHelper, tableMgr, tableFactory,
+			                         serviceRegistrar);
+	}
+
+	public CyNetwork getNetwork() {
+		return networkFactory.getInstance();
+	}
+
+	public CyNetworkFactory getNetworkFactory() {
+		return networkFactory;	
+	}
+}
+
+
