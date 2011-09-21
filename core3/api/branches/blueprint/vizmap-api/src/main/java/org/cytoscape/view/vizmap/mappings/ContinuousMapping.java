da75f3918debf899c5934ec05569248fe6372d68
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-09-21 17:36:04 +0000

    making blueprint branch
    git-svn-id: http://chianti.ucsd.edu/svn@26909 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/api/branches/blueprint/vizmap-api/src/main/java/org/cytoscape/view/vizmap/mappings/ContinuousMapping.java
@@ -0,0 +1,42 @@
+package org.cytoscape.view.vizmap.mappings;
+
+import java.util.List;
+
+import org.cytoscape.view.vizmap.VisualMappingFunction;
+
+public interface ContinuousMapping<K, V> extends VisualMappingFunction<K,V>{
+
+	public static final String CONTINUOUS = "Continuous Mapping";
+
+	/**
+	 * Gets all Data Points.
+	 * 
+	 * @return List of ContinuousMappingPoint objects.
+	 */
+	public List<ContinuousMappingPoint<K, V>> getAllPoints();
+
+	/**
+	 * Adds a New Data Point.
+	 */
+	public void addPoint(K value, BoundaryRangeValues<V> brv);
+
+	/**
+	 * Removes a Point from the List.
+	 */
+	public void removePoint(int index);
+
+	/**
+	 * Gets Total Point Count.
+	 */
+	public int getPointCount();
+
+	/**
+	 * Gets Specified Point.
+	 *  
+	 * @param index
+	 *            Index Value.
+	 * @return ContinuousMappingPoint.
+	 */
+	public ContinuousMappingPoint<K, V> getPoint(int index);
+
+}
