268de16ec3ae4d7ab26747036617171afe05c3c5
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-10-05 15:16:08 +0000

    Renamed freehep.sh to freehep-vectorgraphics.sh Added freehep-io.sh
    git-svn-id: http://chianti.ucsd.edu/svn@27063 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/toronto/jm/cytoscape-temp/freehep-io.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FREEHEP_VERSION=2.1
+
+mkdir build
+pushd build
+
+curl -OL https://github.com/freehep/freehep-io/tarball/freehep-io-${FREEHEP_VERSION} || exit
+
+tar xvzf freehep-io-${FREEHEP_VERSION} || exit
+pushd freehep-freehep-io-*
+
+mvn clean package || exit
+
+for JAR in target/*.jar
+do
+    bnd wrap ${JAR}
+done
+
+for BAR in */target/*.bar
+do
+    ARTIFACT_ID=$(dirname $(dirname ${BAR}))
+    mvn deploy:deploy-file -DgroupId=cytoscape-temp -DartifactId=${ARTIFACT_ID} -Dversion=${FREEHEP_VERSION} -Dpackaging=jar -Dfile=${BAR} -DrepositoryId=thirdparty -Durl=http://code.cytoscape.org/nexus/content/repositories/thirdparty/
+done
+
+popd
+
+popd
+rm -rf build
