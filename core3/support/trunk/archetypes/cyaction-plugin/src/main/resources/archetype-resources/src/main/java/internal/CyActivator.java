afef0b9f8eebd4a797448ea3c49c84fd4a7d752a
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-10-05 20:06:35 +0000

    Updated cyaction-plugin archetype - Removed Eclipse configuration (M2E plugin generates this) - Ported to use CyActivator instead of Spring - Added compilation configuration to pom.xml so that Eclipse treats this as a Java project
    git-svn-id: http://chianti.ucsd.edu/svn@27067 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ core3/support/trunk/archetypes/cyaction-plugin/src/main/resources/archetype-resources/src/main/java/internal/CyActivator.java
@@ -0,0 +1,20 @@
+package ${package}.internal;
+
+import java.util.Properties;
+
+import org.cytoscape.application.CyApplicationManager;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.BundleContext;
+
+public class CyActivator extends AbstractCyActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		CyApplicationManager applicationManager = (CyApplicationManager) getService(context, CyApplicationManager.class);
+		MenuAction action = new MenuAction(applicationManager, "Hello World Plugin");
+		
+		Properties properties = new Properties();
+		registerAllServices(context, action, properties);
+	}
+
+}
