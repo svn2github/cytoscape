adbc5e20242fd6f9a998e8a8426dfbc421470a3b
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-02 19:10:14 +0000

    Add comments
    git-svn-id: http://chianti.ucsd.edu/svn@27378 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/jActiveModules31/src/main/java/csplugins/jActiveModules/ActiveModulesPanelSelectedListener.java
+++ csplugins/trunk/ucsd/pwang/jActiveModules31/src/main/java/csplugins/jActiveModules/ActiveModulesPanelSelectedListener.java
@@ -5,17 +5,19 @@ import org.cytoscape.application.swing.events.CytoPanelComponentSelectedListener
 
 import csplugins.jActiveModules.dialogs.ActivePathsParameterPanel;
 
+
+// Note: THis class does nothing, should be removed.
 public class ActiveModulesPanelSelectedListener implements CytoPanelComponentSelectedListener {
 	private ActivePathsParameterPanel mainPanel;
 
-	public ActiveModulesPanelSelectedListener(ActivePathsParameterPanel mainPanel) {
+	public ActiveModulesPanelSelectedListener(ActivePathsParameterPanel mainPanel, int c) {
 		this.mainPanel = mainPanel;
 	}
 	
 	@Override
 	public void handleEvent(CytoPanelComponentSelectedEvent e) {
 		if (e.getCytoPanel().getSelectedComponent() == mainPanel) {
-			mainPanel.handlePanelSelected();
+			//mainPanel.handlePanelSelected();
 		}
 	}
 }
