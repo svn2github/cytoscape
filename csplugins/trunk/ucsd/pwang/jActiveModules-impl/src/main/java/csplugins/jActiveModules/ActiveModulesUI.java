1e16ef1d396c469b1eaaa07bc64b4e7272af8a1b
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-05 00:47:52 +0000

    Comment out unused code
    git-svn-id: http://chianti.ucsd.edu/svn@27412 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/jActiveModules-impl/src/main/java/csplugins/jActiveModules/ActiveModulesUI.java
+++ csplugins/trunk/ucsd/pwang/jActiveModules-impl/src/main/java/csplugins/jActiveModules/ActiveModulesUI.java
@@ -44,10 +44,10 @@ import java.util.Properties;
 /**
  * UI for Active Modules. Manages the various menu items
  */
-public class ActiveModulesUI extends AbstractCyAction implements CytoscapeStartListener {
+public class ActiveModulesUI extends AbstractCyAction { //implements CytoscapeStartListener {
 	protected ActivePaths activePaths;
 	protected ActivePathFinderParameters apfParams;
-	protected ThreadExceptionHandler xHandler;
+	//protected ThreadExceptionHandler xHandler;
 
 	private ActivePathsParameterPanel mainPanel;
 
@@ -71,7 +71,7 @@ public class ActiveModulesUI extends AbstractCyAction implements CytoscapeStartL
 		this.apfParams = apfParams;
 		this.apfParams.reloadExpressionAttributes();
 		
-		AttrChangeListener acl = new AttrChangeListener();
+		//AttrChangeListener acl = new AttrChangeListener();
 		//Cytoscape.getPropertyChangeSupport().addPropertyChangeListener(
 		//		Cytoscape.ATTRIBUTES_CHANGED, acl);
 		//Cytoscape.getNodeAttributes().getMultiHashMapDefinition()
@@ -120,46 +120,46 @@ public class ActiveModulesUI extends AbstractCyAction implements CytoscapeStartL
 		}
 	}
 
-	public void handleEvent(CytoscapeStartEvent e){
-		//evt.getPropertyName() == Cytoscape.CYTOSCAPE_INITIALIZED
-		if ( apfParams.getRun()) {
-			if (apfParams.getRandomizeExpression()) {
-				startRandomizeAndRun(mainPanel.getTargetNetwork());
-			} else {
-				activePaths = new ActivePaths(mainPanel.getTargetNetwork(),
-						apfParams, this);
-				Thread t = new Thread(activePaths);
-				t.start();
-			}
-		}
-	}
+//	public void handleEvent(CytoscapeStartEvent e){
+//		//evt.getPropertyName() == Cytoscape.CYTOSCAPE_INITIALIZED
+//		if ( apfParams.getRun()) {
+//			if (apfParams.getRandomizeExpression()) {
+//				startRandomizeAndRun(mainPanel.getTargetNetwork());
+//			} else {
+//				activePaths = new ActivePaths(mainPanel.getTargetNetwork(),
+//						apfParams, this);
+//				Thread t = new Thread(activePaths);
+//				t.start();
+//			}
+//		}
+//	}
 	
 
 	/**
 	 * Action to allow the user to change the current options for running
 	 * jActiveModules, with a gui interface
 	 */
-	protected class SetParametersAction extends AbstractAction {
-		private static final long serialVersionUID = -7836213413255212288L;
-
-		public SetParametersAction() {
-			super("jActiveModules");
-		}
-
-		public void actionPerformed(ActionEvent e) {
-			if (apfParams.getPossibleExpressionAttributes().size() == 0) {
-				JOptionPane
-						.showMessageDialog(ServicesUtil.cySwingApplicationServiceRef.getJFrame(),
-								"JActiveModules cannot start because it cannot find\n"
-										+ "any p-value attributes! JActiveModules requires at\n"
-										+ "least one node attribute with values ranging between\n"
-										+ "0 and 1 of type 'float' (i.e. decimal or real number).\n"
-										+ "Please load an appropriate attribute and try again.",
-								"jActiveModules", JOptionPane.ERROR_MESSAGE);
-				return;
-			}
-
-			
+//	protected class SetParametersAction extends AbstractAction {
+//		private static final long serialVersionUID = -7836213413255212288L;
+//
+//		public SetParametersAction() {
+//			super("jActiveModules");
+//		}
+//
+//		public void actionPerformed(ActionEvent e) {
+//			if (apfParams.getPossibleExpressionAttributes().size() == 0) {
+//				JOptionPane
+//						.showMessageDialog(ServicesUtil.cySwingApplicationServiceRef.getJFrame(),
+//								"JActiveModules cannot start because it cannot find\n"
+//										+ "any p-value attributes! JActiveModules requires at\n"
+//										+ "least one node attribute with values ranging between\n"
+//										+ "0 and 1 of type 'float' (i.e. decimal or real number).\n"
+//										+ "Please load an appropriate attribute and try again.",
+//								"jActiveModules", JOptionPane.ERROR_MESSAGE);
+//				return;
+//			}
+//
+//			
 //			final CytoPanel cytoPanel = Cytoscape.getDesktop().getCytoPanel(
 //					SwingConstants.WEST);
 //			
@@ -174,15 +174,13 @@ public class ActiveModulesUI extends AbstractCyAction implements CytoscapeStartL
 //			}
 //			cytoPanel.setSelectedIndex(index);
 //			//cytoPanel.setState(CytoPanelState.DOCK);
-			
-		}
-	}
+//			
+//		}
+//	}
 
 	public void startFindActivePaths(CyNetwork network) {
 		try {
-			
-			activePaths = new ActivePaths(network, apfParams, this);
-			
+			activePaths = new ActivePaths(network, this.apfParams, this);
 		} catch (final Exception e) {
 			e.printStackTrace(System.err);
 			JOptionPane.showMessageDialog(ServicesUtil.cySwingApplicationServiceRef.getJFrame(),
@@ -190,93 +188,97 @@ public class ActiveModulesUI extends AbstractCyAction implements CytoscapeStartL
 					"Error", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
-		Thread t = new Thread(activePaths);
-		t.setUncaughtExceptionHandler(xHandler);
-		t.start();
+		
+//		Thread t = new Thread(activePaths);
+//		t.setUncaughtExceptionHandler(xHandler);
+//		t.start();
+		
+		ActivePathsTaskFactory factory = new ActivePathsTaskFactory(activePaths);
+		ServicesUtil.taskManagerServiceRef.execute(factory);
 	}
 
 	/**
 	 * This action will run activePaths with the current parameters
 	 */
-	protected class FindActivePathsAction extends AbstractAction {
+//	protected class FindActivePathsAction extends AbstractAction {
+//
+//		FindActivePathsAction() {
+//			super("Active Modules: Find Modules");
+//		}
+//
+//		public void actionPerformed(ActionEvent ae) {
+//			startFindActivePaths(mainPanel.getTargetNetwork());
+//		}
+//	}
 
-		FindActivePathsAction() {
-			super("Active Modules: Find Modules");
-		}
-
-		public void actionPerformed(ActionEvent ae) {
-			startFindActivePaths(mainPanel.getTargetNetwork());
-		}
-	}
-
-	protected class ThreadExceptionHandler implements
-			Thread.UncaughtExceptionHandler {
-		public void uncaughtException(Thread t, Throwable e) {
-			e.printStackTrace(System.err);
-			logger.warn("Non-fatal exception in Thread " + t.getName(), e);
-			logger.warn("The previous exception was non-fatal - Don't panic!");
-			JOptionPane.showMessageDialog(ServicesUtil.cySwingApplicationServiceRef.getJFrame(),
-					"Error running jActiveModules (2)!  " + e.getMessage(),
-					"Error", JOptionPane.ERROR_MESSAGE);
-		}
-	}
+//	protected class ThreadExceptionHandler implements
+//			Thread.UncaughtExceptionHandler {
+//		public void uncaughtException(Thread t, Throwable e) {
+//			e.printStackTrace(System.err);
+//			logger.warn("Non-fatal exception in Thread " + t.getName(), e);
+//			logger.warn("The previous exception was non-fatal - Don't panic!");
+//			JOptionPane.showMessageDialog(ServicesUtil.cySwingApplicationServiceRef.getJFrame(),
+//					"Error running jActiveModules (2)!  " + e.getMessage(),
+//					"Error", JOptionPane.ERROR_MESSAGE);
+//		}
+//	}
 
 	/**
 	 * This action will generate a score for the currently selected nodes in the
 	 * view
 	 */
-	protected class ScoreSubComponentAction extends AbstractAction {
+//	protected class ScoreSubComponentAction extends AbstractAction {
+//
+//		ScoreSubComponentAction() {
+//			super("Active Modules: Score Selected Nodes");
+//		}
+//
+//		public void actionPerformed(ActionEvent e) {
+//			activePaths = new ActivePaths(mainPanel.getTargetNetwork(), apfParams, ActiveModulesUI.this);
+//			activePaths.scoreActivePath();
+//		}
+//	}
 
-		ScoreSubComponentAction() {
-			super("Active Modules: Score Selected Nodes");
-		}
+//	public void startRandomizeAndRun(CyNetwork network) {
+//		activePaths = new ActivePaths(network, apfParams, ActiveModulesUI.this);
+//		Thread t = new ScoreDistributionThread(ServicesUtil.cySwingApplicationServiceRef.getJFrame(), network, activePaths, apfParams);
+//		t.setUncaughtExceptionHandler(xHandler);
+//		t.start();
+//	}
 
-		public void actionPerformed(ActionEvent e) {
-			activePaths = new ActivePaths(mainPanel.getTargetNetwork(), apfParams, ActiveModulesUI.this);
-			activePaths.scoreActivePath();
-		}
-	}
-
-	public void startRandomizeAndRun(CyNetwork network) {
-		activePaths = new ActivePaths(network, apfParams, ActiveModulesUI.this);
-		Thread t = new ScoreDistributionThread(ServicesUtil.cySwingApplicationServiceRef.getJFrame(), network, activePaths, apfParams);
-		t.setUncaughtExceptionHandler(xHandler);
-		t.start();
-	}
-
-	public class RandomizeAndRunAction extends AbstractAction {
-
-		public RandomizeAndRunAction() {
-			super("Active Modules: Score Distribution");
-		}
-
-		public void actionPerformed(ActionEvent e) {
-			startRandomizeAndRun(mainPanel.getTargetNetwork());
-		}
-	}
+//	public class RandomizeAndRunAction extends AbstractAction {
+//
+//		public RandomizeAndRunAction() {
+//			super("Active Modules: Score Distribution");
+//		}
+//
+//		public void actionPerformed(ActionEvent e) {
+//			startRandomizeAndRun(mainPanel.getTargetNetwork());
+//		}
+//	}
 
 	/**
 	 * This is used to update the expression attributes in the params object so
 	 * that they match those that exist in CyAttributes.
 	 */
-	protected class AttrChangeListener implements PropertyChangeListener { //,
-			//MultiHashMapDefinitionListener {
-
-		public void propertyChange(PropertyChangeEvent e) {
-			//if (e.getPropertyName().equals(Cytoscape.ATTRIBUTES_CHANGED))
-			//	apfParams.reloadExpressionAttributes();
-		}
-
-		/**
-		 * There is no point in listening to attributeDefined events because
-		 * this only defines the attr and when this is fired, no attr values
-		 * actually exist.
-		 */
-		public void attributeDefined(String attributeName) {
-		}
-
-		public void attributeUndefined(String attributeName) {
-			apfParams.reloadExpressionAttributes();
-		}
-	}
+//	protected class AttrChangeListener implements PropertyChangeListener { //,
+//			//MultiHashMapDefinitionListener {
+//
+//		public void propertyChange(PropertyChangeEvent e) {
+//			//if (e.getPropertyName().equals(Cytoscape.ATTRIBUTES_CHANGED))
+//			//	apfParams.reloadExpressionAttributes();
+//		}
+//
+//		/**
+//		 * There is no point in listening to attributeDefined events because
+//		 * this only defines the attr and when this is fired, no attr values
+//		 * actually exist.
+//		 */
+//		public void attributeDefined(String attributeName) {
+//		}
+//
+//		public void attributeUndefined(String attributeName) {
+//			apfParams.reloadExpressionAttributes();
+//		}
+//	}	
 }
