d4b4ba794e4e91812dc46b2cb8ea73d2009c25d5
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-12-20 20:20:12 +0000

    updated
    git-svn-id: http://chianti.ucsd.edu/svn@27843 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- new_cytoscape_website/bugreport/bugreport_schema/bugDB_schema.sql
+++ new_cytoscape_website/bugreport/bugreport_schema/bugDB_schema.sql
@@ -5,11 +5,12 @@ USE bugs;
 
 CREATE TABLE bugs (
   bug_auto_id int(11) NOT NULL auto_increment,
-  reporter_id int(11) NOT NULL;
+  reporter_id int(11) NOT NULL,
   description text NOT NULL,
   remote_host varchar(60) default NULL,
   ip_address varchar(20) default NULL,
   sysdat date default NULL,
+  editdat date default NULL,
   PRIMARY KEY  (bug_auto_id)
 );
 
@@ -23,17 +24,16 @@ CREATE TABLE reporter (
 
 
 CREATE TABLE attached_files (
-  attached_file_auto_id int(11) NOT NULL auto_increment,
+  file_auto_id int(11) NOT NULL auto_increment,
   file_data longblob,
-  file_type enum('jar','zip') default NULL,
   file_name varchar(100) default NULL,
   md5 varchar(40),
-  PRIMARY KEY  (plugin_file_auto_id)
+  PRIMARY KEY  (file_auto_id)
 );
 
 
 CREATE TABLE bug_file (
-  bug_id int(11) default NULL,
-  file_id int(11) default NULL,
+  bug_id int(11) NOT NULL,
+  file_id int(11) NOT NULL
 );
 
