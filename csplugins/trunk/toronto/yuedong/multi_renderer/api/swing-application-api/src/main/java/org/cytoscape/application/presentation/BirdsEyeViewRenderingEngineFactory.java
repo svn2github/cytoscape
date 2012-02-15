aa3e3dd1877e67b541a0e42ba9cb135370fa2a9e
paperwing <paperwing@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-02-15 18:51:10 +0000

    Added RenderingEngineFactory declarations for swing-application-api
    git-svn-id: http://chianti.ucsd.edu/svn@28278 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/toronto/yuedong/multi_renderer/api/swing-application-api/src/main/java/org/cytoscape/application/presentation/BirdsEyeViewRenderingEngineFactory.java
@@ -0,0 +1,13 @@
+package org.cytoscape.application.presentation;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.view.presentation.RenderingEngine;
+import org.cytoscape.view.presentation.RenderingEngineFactory;
+
+/**
+ * This interface represents a {@link RenderingEngineFactory} used specifically
+ * for generating {@link RenderingEngine} objects for the bird's eye view.
+ */
+public interface BirdsEyeViewRenderingEngineFactory extends
+		RenderingEngineFactory<CyNetwork> {
+}
---
+++ csplugins/trunk/toronto/yuedong/multi_renderer/api/swing-application-api/src/main/java/org/cytoscape/application/presentation/MainViewRenderingEngineFactory.java
@@ -0,0 +1,14 @@
+package org.cytoscape.application.presentation;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.view.presentation.RenderingEngine;
+import org.cytoscape.view.presentation.RenderingEngineFactory;
+
+/**
+ * This interface represents a {@link RenderingEngineFactory} used specifically
+ * for generating {@link RenderingEngine} objects for the main network viewing
+ * window.
+ */
+public interface MainViewRenderingEngineFactory extends
+		RenderingEngineFactory<CyNetwork> {
+}
