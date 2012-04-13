9769e63d71ee2d18dbb34fb67a2a23bd522984b1
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-13 23:58:12 +0000

    fixes #734 Utility method had been moved to public API and Rendering Engine developer can add new Visual Properties as a part of Basic set.
    git-svn-id: http://chianti.ucsd.edu/svn@28841 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/api/trunk/vizmap-gui-api/src/main/java/org/cytoscape/view/vizmap/gui/util/PropertySheetUtil.java
+++ core3/api/trunk/vizmap-gui-api/src/main/java/org/cytoscape/view/vizmap/gui/util/PropertySheetUtil.java
@@ -1,4 +1,4 @@
-package org.cytoscape.view.vizmap.gui.internal;
+package org.cytoscape.view.vizmap.gui.util;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -6,6 +6,10 @@ import java.util.Set;
 import org.cytoscape.view.model.VisualProperty;
 import org.cytoscape.view.presentation.property.BasicVisualLexicon;
 
+/**
+ * Utility class to provide access to basic set of Visual Properties.
+ *
+ */
 public final class PropertySheetUtil {
 	
 	private static final Set<VisualProperty<?>> BASIC_PROPS = new HashSet<VisualProperty<?>>();
@@ -29,20 +33,50 @@ public final class PropertySheetUtil {
 		
 		BASIC_PROPS.add(BasicVisualLexicon.NETWORK_BACKGROUND_PAINT);
 	}
-	
-	public static boolean isBasic(final VisualProperty<?> vp) {
+
+
+	/**
+	 * Check whether given Visual Property is categorized as Basic or not.
+	 * @param vp Visual Property to be checked
+	 * 
+	 * @return true if it is a part of preset basic visual properties.
+	 */
+	public static final boolean isBasic(final VisualProperty<?> vp) {
 		if(BASIC_PROPS.contains(vp))
 			return true;
 		else
 			return false;
 	}
 	
-	public static boolean isAdvancedMode() {
+	/**
+	 * Check the status of display mode
+	 * 
+	 * @return true if it is in advanced mode.
+	 */
+	public static final boolean isAdvancedMode() {
 		return mode;
 	}
 	
-	public static void setMode(boolean advanced) {
+	/**
+	 * Switch the current display mode.
+	 * 
+	 * @param advanced If true, it is in advanced mode.
+	 */
+	public static final void setMode(boolean advanced) {
 		mode = advanced;
 	}
-
+	
+	
+	/**
+	 * Add a new basic visual property.  This will be used by Rendering Engine developers.
+	 * 
+	 * @param vp New Visual Property to be set as basic set.
+	 */
+	public static final void addBasicVisualProperty(final VisualProperty<?> vp) {
+		// Ignore null values.
+		if(vp == null)
+			return;
+		
+		BASIC_PROPS.add(vp);
+	}
 }
