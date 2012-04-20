e8d5c1c26063fbca7efa562b1eafbae893ce4196
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-20 04:18:17 +0000

    add some sample code
    git-svn-id: http://chianti.ucsd.edu/svn@28895 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/ucsd/pwang/sandbox2/src/main/java/org/cytoscape/sandbox/SandboxAction.java
+++ csplugins/trunk/ucsd/pwang/sandbox2/src/main/java/org/cytoscape/sandbox/SandboxAction.java
@@ -1,11 +1,17 @@
 package org.cytoscape.sandbox;
 
 import cytoscape.CyNode;
+import cytoscape.CyEdge;
+
 import cytoscape.Cytoscape;
 import cytoscape.util.CytoscapeAction;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import giny.model.Node;
 
 import cytoscape.CyNetwork;
 import cytoscape.view.CyNetworkView;
@@ -35,21 +41,184 @@ public class SandboxAction extends CytoscapeAction {
 			System.out.println( "\nMenuItem sandbox is clicked!\n ");
 			
 			CyAttributes nodeAttrs = Cytoscape.getNodeAttributes();			
-			Iterator<CyNode> it = Cytoscape.getCurrentNetwork().nodesIterator();
+			CyAttributes edgeAttrs = Cytoscape.getEdgeAttributes();			
+
+			CyNetwork network = Cytoscape.getCurrentNetwork();
 			
-			
+			HashSet<Node> selectedNodeSet = new HashSet<Node>();
+									
+			Iterator<CyNode> it = network.nodesIterator();			
 			while (it.hasNext()){
 				CyNode node = it.next();
 
-				String name = nodeAttrs.getAttribute(node.getIdentifier(), "canonicalName").toString();
-
-				if (name.equalsIgnoreCase("ASDF")){
-					
-				}
+				String name = nodeAttrs.getAttribute(node.getIdentifier(), "Name").toString();
 				
-				System.out.println( "\tname = "+ name);
+				// start of a case
+				if (name.equalsIgnoreCase("Diabetes Mellitus, Type 1")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=27){ //13/27
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+
+				// start of a case
+				if (name.equalsIgnoreCase("Diabetes Mellitus, Type 2")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=65){ //45/65
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+
+				// start of a case
+				if (name.equalsIgnoreCase("Obesity")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=37){//20/37
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+				
+				// start of a case
+				if (name.equalsIgnoreCase("Hypertension")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=52){//17/52
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+				
+				// start of a case
+				if (name.equalsIgnoreCase("Cardiovascular Diseases")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=21){//15/21
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+				
+				// start of a case
+				if (name.equalsIgnoreCase("Coronary Artery Disease")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=21){//16/21
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+
+				// start of a case
+				if (name.equalsIgnoreCase("Alzheimer Disease")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=24){//18/24
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+				// start of a case
+				if (name.equalsIgnoreCase("Metabolic Syndrome X")){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=10){ //7/10
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // End of a case
+
+				
+				//
+				// start of a case
+				if (name.equalsIgnoreCase("Dyslipidemias") ){
+					// get the edges for this disease node					
+					List<CyEdge> edgeList = network.getAdjacentEdgesList(node, true, true, true);
+					
+					//System.out.println( "\tedgelist.size() = "+ edgeList.size());
+					// compare edge attribute -- asssociationCount and make selection
+					Iterator<CyEdge> edgeIt = edgeList.iterator();
+					while (edgeIt.hasNext()){
+						CyEdge edge = edgeIt.next();						
+						int assCount  = edgeAttrs.getIntegerAttribute(edge.getIdentifier(), "AsssociationCount");						
+						if (assCount >=7) { // 4/7
+							selectedNodeSet.add(edge.getSource());
+							selectedNodeSet.add(edge.getTarget());							
+						}
+					}					
+				} // end of a case
+				
 			}
 
-			
+			network.setSelectedNodeState(selectedNodeSet, true);
+			Cytoscape.getNetworkView(network.getIdentifier()).updateView();
 		}
 }
