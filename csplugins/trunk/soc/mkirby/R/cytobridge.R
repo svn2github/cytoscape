a2807ffe9b77900630c58950dc800c6b3bf7b932
mkirby <mkirby@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-05-30 22:54:53 +0000

    mkirby: Got socket communication between R and Java working.
    git-svn-id: http://chianti.ucsd.edu/svn@29401 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- csplugins/trunk/soc/mkirby/R/cytobridge.R
+++ csplugins/trunk/soc/mkirby/R/cytobridge.R
@@ -1,9 +1,9 @@
 #make your igraph
-g1 <- graph( c( 0,1, 1,2, 2,2, 2,3 ) )
+g1 <- graph.ring(100000)
 
 #The Cytobridge class
 setClass(Class="cytobridge", 
-	   representation=representation(a="ANY"),
+	   representation=representation(a="ANY", s="ANY"),
 	   prototype=prototype(a=graph(c(0,1))),
 	   validity=function(object) {
 	       if(class(object@a)!="igraph") {
@@ -23,8 +23,29 @@ setGeneric(name="getGraph", def=function(x) standardGeneric("getGraph"))
 setGeneric(name="update", def=function(x) standardGeneric("update"))
         setMethod(f="update", signature="cytobridge",
             definition=function(x) {
-                print(V(getGraph(x)))
+ 		    socket <- make.socket("localhost", "4444")
+    		    on.exit(close.socket(socket))
+		    write.socket(socket, paste(as.character(x@s),toString(x@a),"endRSend"))
+		    close.socket(socket)
         })
 
+setGeneric(name="connect", def=function(x) standardGeneric("connect"))
+        setMethod(f="connect", signature="cytobridge",
+            definition=function(x) {
+		    x@s <- 1234 #some unique id
+		    return(x)
+        })
+
+setGeneric(name="close", def=function(x) standardGeneric("close"))
+        setMethod(f="close", signature="cytobridge",
+            definition=function(x) {
+		    socket <- make.socket("localhost", "4444")
+    		    on.exit(close.socket(socket))
+		    write.socket(socket, "die")
+		    close.socket(socket)
+                x@s <- 0 #resets id
+        })
 
 test <- new("cytobridge",a=g1)
+test <- connect(test)
+update(test)
