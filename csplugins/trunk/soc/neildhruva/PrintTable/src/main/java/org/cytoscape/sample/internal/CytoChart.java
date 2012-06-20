31e29188fb7b16c8b29ee8c2dce26fdcf059e408
neildhruva <neildhruva@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-06-20 08:24:23 +0000

    Added a sample graph in CytoChart.java to be displayed in the Table View panel
    git-svn-id: http://chianti.ucsd.edu/svn@29636 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/soc/neildhruva/PrintTable/src/main/java/org/cytoscape/sample/internal/CytoChart.java
@@ -0,0 +1,33 @@
+package org.cytoscape.sample.internal;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class CytoChart {
+
+	private JFreeChart chart;
+    private ChartPanel myChart;
+    private DefaultCategoryDataset dataset;
+    
+	public CytoChart() {
+		
+		dataset = new DefaultCategoryDataset();			
+	}
+	
+	public ChartPanel createChart(){
+		dataset.setValue(6, "Profit", "Jane");
+		dataset.setValue(7, "Profit", "Tom");
+		dataset.setValue(8, "Profit", "Jill");
+		dataset.setValue(5, "Profit", "John");
+		dataset.setValue(12, "Profit", "Fred");
+		chart = ChartFactory.createBarChart("Comparison between Salesman",
+		"Salesman", "Profit", dataset, PlotOrientation.VERTICAL,
+		false, true, false);
+		myChart = new ChartPanel(chart);
+		myChart.setMouseWheelEnabled(true);
+		return myChart;
+	}
+}
