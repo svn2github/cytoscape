4321cb047f770a4aed1d53da1f2c19ff2eec93de
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-08-29 19:33:05 +0000

    Fixes #1124: We now have a proper progress bar on the splash screen
    git-svn-id: http://chianti.ucsd.edu/svn@30289 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/support/trunk/splash-updater-impl/src/main/java/org/cytoscape/splash/internal/SplashActivator.java
+++ core3/support/trunk/splash-updater-impl/src/main/java/org/cytoscape/splash/internal/SplashActivator.java
@@ -2,10 +2,6 @@ package org.cytoscape.splash.internal;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkListener;
-import org.osgi.framework.BundleListener;
-//import org.springframework.osgi.context.event.OsgiBundleApplicationContextListener;
-import java.util.Properties;
 
 /**
  * Meant to be run at startLevel 1 so that the splash screen pops up before all
@@ -17,10 +13,9 @@ public final class SplashActivator implements BundleActivator {
      * Called whenever the OSGi framework starts our bundle
      */
     public void start( BundleContext bc ) throws Exception {
-		SplashManipulator splash = new SplashManipulator();
-		bc.addFrameworkListener(splash);
+		SplashManipulator splash = new SplashManipulator(bc);
+		bc.addFrameworkListener(splash);	
 		bc.addBundleListener(splash);
-//		bc.registerService(OsgiBundleApplicationContextListener.class.getName(),splash,new Properties());
     }
 
     /**
