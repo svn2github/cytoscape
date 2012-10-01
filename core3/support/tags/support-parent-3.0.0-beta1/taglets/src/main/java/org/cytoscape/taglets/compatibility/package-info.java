80b58eebbf9c69ded3baef10176fdf8cf44336ce
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-10-01 22:17:24 +0000

    Removed broken tag version.
    git-svn-id: http://chianti.ucsd.edu/svn@30562 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/support/tags/support-parent-3.0.0-beta1/taglets/src/main/java/org/cytoscape/taglets/compatibility/package-info.java
+++
@@ -1,7 +0,0 @@
-
-/**
- * This package contains a variety of JavaDoc Taglet classes used to 
- * help document the Cytoscape public API for backwards compatibility
- * policies.
- */
-package org.cytoscape.taglets.compatibility;
