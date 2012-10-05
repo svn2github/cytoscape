bb3fa43aa715aa6e01980d8a8e0e2f1970c56850
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-10-05 22:05:45 +0000

    Fixes #1499: Applied fix to user-beta1 branch
    git-svn-id: http://chianti.ucsd.edu/svn@30614 0ecc0d97-ab19-0410-9704-bfe1a75892f5

--- core3/gui-distribution/branches/user-beta1/launcher/src/main/java/org/cytoscape/launcher/internal/Launcher.java
+++ core3/gui-distribution/branches/user-beta1/launcher/src/main/java/org/cytoscape/launcher/internal/Launcher.java
@@ -1,5 +1,7 @@
 package org.cytoscape.launcher.internal;
 
+import java.io.File;
+
 import org.apache.karaf.main.Main;
 
 public class Launcher {
@@ -7,9 +9,38 @@ public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 		startupArguments = args;
+		setDefaultSystemProperties();
+		createConfigurationDirectory();
 		Main.main(args);
 	}
 	
+	private static void setDefaultSystemProperties() {
+		String userHome = System.getProperty("user.home");
+		if (System.getProperty("karaf.data") == null) {
+			System.setProperty("karaf.data", join(File.separator, userHome, "CytoscapeConfiguration", "3", "karaf_data"));
+		}
+	}
+
+	private static String join(String separator, String... parts) {
+		StringBuilder builder = new StringBuilder();
+		boolean isFirst = true;
+		for (String part : parts) {
+			if (!isFirst) {
+				builder.append(separator);
+			} else {
+				isFirst = false;
+			}
+			builder.append(part);
+		}
+		return builder.toString();
+	}
+	
+	private static void createConfigurationDirectory() {
+		String userHome = System.getProperty("user.home");
+		File karafData = new File(join(File.separator, userHome, "CytoscapeConfiguration", "3"));
+		karafData.mkdirs();
+	}
+
 	public static String[] getStartupArguments() {
 		String[] result = new String[startupArguments.length];
 		System.arraycopy(startupArguments, 0, result, 0, startupArguments.length);
