b395716788840735074cadf9f1cad0440bae4a96
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2013-02-22 19:45:09 +0000

    Original creation
    git-svn-id: http://chianti.ucsd.edu/svn@31134 0ecc0d97-ab19-0410-9704-bfe1a75892f5

---
+++ csplugins/trunk/ucsd/kono/bingo-impl/src/main/java/bingo/internal/CyActivator.java
@@ -0,0 +1,22 @@
+package bingo.internal;
+
+import bingo.internal.BingoPluginAction;
+import org.cytoscape.application.swing.CyAction;
+import org.osgi.framework.BundleContext;
+import org.cytoscape.service.util.AbstractCyActivator;
+import java.util.Properties;
+import org.cytoscape.app.swing.CySwingAppAdapter;
+
+public class CyActivator extends AbstractCyActivator {
+	public CyActivator() {
+		super();
+	}
+
+	public void start(BundleContext bc) {
+
+		CySwingAppAdapter adapter = getService(bc,CySwingAppAdapter.class);
+		
+		BingoPluginAction bingoPluginAction = new BingoPluginAction(adapter);		
+		registerService(bc,bingoPluginAction,CyAction.class, new Properties());
+	}
+}
